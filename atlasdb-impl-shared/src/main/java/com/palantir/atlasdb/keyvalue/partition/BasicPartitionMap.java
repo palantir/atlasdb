44f51a2caf2e94e281eebf1bae679d8c343d24fc
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-08-04 21:32:44 -0700

    Refactor

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/partition/BasicPartitionMap.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/partition/BasicPartitionMap.java
@@ -1,6 +1,5 @@
 package com.palantir.atlasdb.keyvalue.partition;
 
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.NavigableSet;
@@ -11,8 +10,8 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.LinkedHashMultimap;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
@@ -20,66 +19,67 @@ import com.google.common.primitives.UnsignedBytes;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.RangeRequest;
-import com.palantir.atlasdb.keyvalue.api.RangeRequest.Builder;
 import com.palantir.atlasdb.keyvalue.api.RangeRequests;
 import com.palantir.atlasdb.keyvalue.partition.api.PartitionMap;
-
 
 public final class BasicPartitionMap implements PartitionMap {
 
     private static final Logger log = LoggerFactory.getLogger(BasicPartitionMap.class);
     private final QuorumParameters quorumParameters;
     private final CycleMap<byte[], KeyValueService> ring;
+    private final Map<KeyValueService, String> rackByKvs;
     private final Set<KeyValueService> services;
+    private final Set<String> racks;
 
-    //*** Construction ****************************************************************************
+    // *** Construction ****************************************************************************
     private BasicPartitionMap(QuorumParameters quorumParameters,
-                              NavigableMap<byte[], KeyValueService> ring) {
-        // Ensure there are actually enough kv stores.
-        // TODO: Add rack info
-        Preconditions.checkArgument(quorumParameters.getReplicationFactor() <= ring.keySet().size());
-        // Careful with instruction order here
+                              NavigableMap<byte[], KeyValueService> ring,
+                              Map<KeyValueService, String> rackByKvs) {
         this.quorumParameters = quorumParameters;
         this.ring = CycleMap.wrap(ring);
-        this.services = Sets.newHashSet(ring.values());
-        Preconditions.checkArgument(isRingValid());
+        this.rackByKvs = rackByKvs;
+        this.services = ImmutableSet.<KeyValueService> builder().addAll(ring.values()).build();
+        this.racks = ImmutableSet.<String> builder().addAll(rackByKvs.values()).build();
+
+        Preconditions.checkArgument(quorumParameters.getReplicationFactor() <= racks.size());
     }
 
-    /* This map CAN contain duplicate values (virtual partitions). However it is the callers responsibility
-     * to not have same kvs repeated after less than rf points.
-     */
-    public static BasicPartitionMap create(QuorumParameters quorumParameters, NavigableMap<byte[], KeyValueService> ring) {
-        return new BasicPartitionMap(quorumParameters, ring);
+    public static BasicPartitionMap create(QuorumParameters quorumParameters,
+                                           NavigableMap<byte[], KeyValueService> ring,
+                                           Map<KeyValueService, String> rackByKvs) {
+        return new BasicPartitionMap(quorumParameters, ring, rackByKvs);
     }
-    //*********************************************************************************************
 
-    //*** Helper methods **************************************************************************
-    // Make sure that the data is not replicated onto the original machine
-    private boolean isRingValid() {
-        int repf = quorumParameters.getReplicationFactor();
-        ArrayList<KeyValueService> lastKvs = Lists.newArrayList();
-        for (Map.Entry<byte[], KeyValueService> e : ring.entrySet()) {
-            if (lastKvs.contains(e.getValue())) {
-                return false;
-            }
-            if (lastKvs.size() == repf) {
-                lastKvs.remove(0);
-            }
-            lastKvs.add(e.getValue());
+    public static BasicPartitionMap create(QuorumParameters quorumParameters,
+                                           NavigableMap<byte[], KeyValueService> ring) {
+        Map<KeyValueService, String> rackByKvs = Maps.newHashMap();
+        // Assume each kvs to be in separate rack if no info is available.
+        for (KeyValueService kvs : ImmutableSet.<KeyValueService> builder().addAll(ring.values()).build()) {
+            rackByKvs.put(kvs, new Integer(kvs.hashCode()).toString());
         }
-        return true;
+        return create(quorumParameters, ring, rackByKvs);
     }
 
+    // *********************************************************************************************
+
+    // *** Helper methods **************************************************************************
     private Set<KeyValueService> getServicesHavingRow(byte[] key) {
         Set<KeyValueService> result = Sets.newHashSet();
+        Set<String> racks = Sets.newHashSet();
         byte[] point = key;
-        for (int i = 0; i < quorumParameters.getReplicationFactor(); ++i) {
+        while (result.size() < quorumParameters.getReadFactor()) {
             point = ring.nextKey(point);
-            result.add(ring.get(point));
+            KeyValueService kvs = ring.get(point);
+            String rack = rackByKvs.get(kvs);
+            if (!racks.contains(rack)) {
+                result.add(ring.get(point));
+                racks.add(rack);
+            }
         }
         return result;
     }
-    //*********************************************************************************************
+
+    // *********************************************************************************************
 
     @Override
     public Multimap<ConsistentRingRangeRequest, KeyValueService> getServicesForRangeRead(String tableName,
@@ -89,69 +89,53 @@ public final class BasicPartitionMap implements PartitionMap {
         }
         Multimap<ConsistentRingRangeRequest, KeyValueService> result = LinkedHashMultimap.create();
 
-        // This is either the original ring, or its reversed view (in case of reversed range req)
-        CycleMap<byte[], KeyValueService> rangeRing = ring;
-        if (range.isReverse()) {
-            rangeRing = rangeRing.descendingMap();
-        }
-
-        byte[] key = range.getStartInclusive();
+        byte[] rangeStart = range.getStartInclusive();
         if (range.getStartInclusive().length == 0) {
-            key = RangeRequests.getFirstRowName();
+            rangeStart = RangeRequests.getFirstRowName();
         }
 
-        // Note that there is no wrapping around when travering the circle with the key.
-        // Wrap around can happen when gathering further kvss for a given key.
-        while (range.inRange(key)) {
-            Set<KeyValueService> services = Sets.newHashSet();
+        // Note that there is no wrapping around when traversing the circle with the key.
+        // Ie. the range does not go over through "zero" of the ring.
+        while (range.inRange(rangeStart)) {
 
-            // Setup the range
-            Builder builder = range.isReverse() ? RangeRequest.reverseBuilder() : RangeRequest.builder();
-            builder = builder.startRowInclusive(key);
-            if (rangeRing.higherKey(key) != null) {
-                byte[] topOfRange = rangeRing.higherKey(key);
-                if (range.inRange(topOfRange)) {
-                    builder = builder.endRowExclusive(topOfRange);
-                } else {
-                    builder = builder.endRowExclusive(range.getEndExclusive());
-                }
-            } else {
-                builder = builder.endRowExclusive(range.getEndExclusive());
-            }
-            ConsistentRingRangeRequest crrr = ConsistentRingRangeRequest.of(builder.build());
-
-            // Find kvss having this range
-            byte[] kvsKey = rangeRing.nextKey(key);
-            while (services.size() < quorumParameters.getReplicationFactor()) {
-                // TODO: Add logic here to skip over same rack
-                services.add(rangeRing.get(kvsKey));
-                kvsKey = rangeRing.nextKey(kvsKey);
+            // Setup the consistent subrange
+            byte[] rangeEnd = ring.higherKey(rangeStart);
+            if (rangeEnd == null || !range.inRange(rangeEnd)) {
+                rangeEnd = range.getEndExclusive();
             }
 
-            result.putAll(crrr, services);
+            ConsistentRingRangeRequest crrr = ConsistentRingRangeRequest.of(RangeRequest.builder(
+                    range.isReverse()).startRowInclusive(rangeStart).endRowExclusive(rangeEnd).build());
+
+            // We have now the "consistent" subrange which means that
+            // every service having the (inclusive) start row will also
+            // have all the other rows belonging to this range.
+            // No other services will have any of these rows.
+            result.putAll(crrr, getServicesHavingRow(rangeStart));
 
             // Proceed with next range
-            key = rangeRing.higherKey(key);
+            rangeStart = ring.higherKey(rangeStart);
             // We are out of ranges to consider.
-            if (key == null) {
+            if (rangeStart == null) {
                 break;
             }
         }
-
         return result;
     }
 
     @Override
     public Map<KeyValueService, NavigableSet<byte[]>> getServicesForRowsRead(String tableName,
-                                                                         Iterable<byte[]> rows) {
+                                                                             Iterable<byte[]> rows) {
         Map<KeyValueService, NavigableSet<byte[]>> result = Maps.newHashMap();
         for (byte[] row : rows) {
             Set<KeyValueService> services = getServicesHavingRow(row);
             for (KeyValueService kvs : services) {
                 if (!result.containsKey(kvs)) {
-                    result.put(kvs, Sets.<byte[]>newTreeSet(UnsignedBytes.lexicographicalComparator()));
+                    result.put(
+                            kvs,
+                            Sets.<byte[]> newTreeSet(UnsignedBytes.lexicographicalComparator()));
                 }
-                assert(!result.get(kvs).contains(row));
+                assert !result.get(kvs).contains(row);
                 result.get(kvs).add(row);
             }
         }
@@ -166,9 +150,9 @@ public final class BasicPartitionMap implements PartitionMap {
             Set<KeyValueService> services = getServicesHavingRow(e.getKey().getRowName());
             for (KeyValueService kvs : services) {
                 if (!result.containsKey(kvs)) {
-                    result.put(kvs, Maps.<Cell, Long>newHashMap());
+                    result.put(kvs, Maps.<Cell, Long> newHashMap());
                 }
-                assert(result.get(kvs).containsKey(e.getKey()) == false);
+                assert !result.get(kvs).containsKey(e.getKey());
                 result.get(kvs).put(e.getKey(), e.getValue());
             }
         }
@@ -176,16 +160,15 @@ public final class BasicPartitionMap implements PartitionMap {
     }
 
     @Override
-    public Map<KeyValueService, Set<Cell>> getServicesForCellsRead(String tableName,
-                                                                   Set<Cell> cells) {
+    public Map<KeyValueService, Set<Cell>> getServicesForCellsRead(String tableName, Set<Cell> cells) {
         Map<KeyValueService, Set<Cell>> result = Maps.newHashMap();
         for (Cell cell : cells) {
             Set<KeyValueService> services = getServicesHavingRow(cell.getRowName());
             for (KeyValueService kvs : services) {
                 if (!result.containsKey(kvs)) {
-                    result.put(kvs, Sets.<Cell>newHashSet());
+                    result.put(kvs, Sets.<Cell> newHashSet());
                 }
-                assert(result.get(kvs).contains(cell) == false);
+                assert (result.get(kvs).contains(cell) == false);
                 result.get(kvs).add(cell);
             }
         }
@@ -200,9 +183,9 @@ public final class BasicPartitionMap implements PartitionMap {
             Set<KeyValueService> services = getServicesHavingRow(e.getKey().getRowName());
             for (KeyValueService kvs : services) {
                 if (!result.containsKey(kvs)) {
-                    result.put(kvs, Maps.<Cell, byte[]>newHashMap());
+                    result.put(kvs, Maps.<Cell, byte[]> newHashMap());
                 }
-                assert(!result.get(kvs).containsKey(e.getKey()));
+                assert (!result.get(kvs).containsKey(e.getKey()));
                 result.get(kvs).put(e.getKey(), e.getValue());
             }
         }
@@ -212,18 +195,7 @@ public final class BasicPartitionMap implements PartitionMap {
     @Override
     public Map<KeyValueService, Set<Cell>> getServicesForCellsWrite(String tableName,
                                                                     Set<Cell> cells) {
-        Map<KeyValueService, Set<Cell>> result = Maps.newHashMap();
-        for (Cell cell : cells) {
-            Set<KeyValueService> services = getServicesHavingRow(cell.getRowName());
-            for (KeyValueService kvs : services) {
-                if (!result.containsKey(kvs)) {
-                    result.put(kvs, Sets.<Cell>newHashSet());
-                }
-                assert(!result.get(kvs).contains(cell));
-                result.get(kvs).add(cell);
-            }
-        }
-        return result;
+        return getServicesForCellsRead(tableName, cells);
     }
 
     public <T> Map<KeyValueService, Multimap<Cell, T>> getServicesForWrite(String tableName,
@@ -232,11 +204,11 @@ public final class BasicPartitionMap implements PartitionMap {
         for (Map.Entry<Cell, T> e : keys.entries()) {
             Set<KeyValueService> services = getServicesHavingRow(e.getKey().getRowName());
             for (KeyValueService kvs : services) {
-               if (!result.containsKey(kvs)) {
-                   result.put(kvs, HashMultimap.<Cell, T>create());
-               }
-               assert(!result.get(kvs).containsEntry(e.getKey(), e.getValue()));
-               result.get(kvs).put(e.getKey(), e.getValue());
+                if (!result.containsKey(kvs)) {
+                    result.put(kvs, HashMultimap.<Cell, T> create());
+                }
+                assert (!result.get(kvs).containsEntry(e.getKey(), e.getValue()));
+                result.get(kvs).put(e.getKey(), e.getValue());
             }
         }
         return result;
