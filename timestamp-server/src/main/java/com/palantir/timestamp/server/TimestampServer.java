b1a8c9adc229c48541930dad44b391936d608159
John Carrino <john.carrino@gmail.com>
2015-08-07 14:26:34 -0700

    make progress on server with leadership election

--- timestamp-server/src/main/java/com/palantir/timestamp/server/TimestampServer.java
+++ timestamp-server/src/main/java/com/palantir/timestamp/server/TimestampServer.java
@@ -15,9 +15,33 @@
  */
 package com.palantir.timestamp.server;
 
+import java.util.concurrent.ExecutorService;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Supplier;
+import com.google.common.collect.ImmutableList;
+import com.palantir.atlasdb.client.TextDelegateDecoder;
+import com.palantir.common.concurrent.PTExecutors;
+import com.palantir.leader.PaxosLeaderElectionService;
+import com.palantir.leader.PingableLeader;
+import com.palantir.leader.proxy.AwaitingLeadershipProxy;
+import com.palantir.lock.RemoteLockService;
+import com.palantir.lock.impl.LockServiceImpl;
+import com.palantir.paxos.PaxosAcceptor;
+import com.palantir.paxos.PaxosAcceptorImpl;
+import com.palantir.paxos.PaxosLearner;
+import com.palantir.paxos.PaxosLearnerImpl;
+import com.palantir.paxos.PaxosProposer;
+import com.palantir.paxos.PaxosProposerImpl;
 import com.palantir.timestamp.InMemoryTimestampService;
+import com.palantir.timestamp.RateLimitedTimestampService;
+import com.palantir.timestamp.TimestampService;
 import com.palantir.timestamp.server.config.TimestampServerConfiguration;
 
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import feign.jaxrs.JAXRSContract;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 
@@ -27,10 +51,63 @@ public class TimestampServer extends Application<TimestampServerConfiguration> {
         new TimestampServer().run(args);
     }
 
+    private final ExecutorService executor = PTExecutors.newCachedThreadPool();
+
     @Override
     public void run(TimestampServerConfiguration configuration, Environment environment) throws Exception {
-        environment.jersey().register(new InMemoryTimestampService());
+    	String extServer = configuration.servers.iterator().next();
+    	PaxosLearner learner = PaxosLearnerImpl.newLearner(configuration.learnerLogDir);
+    	PaxosAcceptor acceptor = PaxosAcceptorImpl.newAcceptor(configuration.acceptorLogDir);
+        environment.jersey().register(acceptor);
+        environment.jersey().register(learner);
+    	ObjectMapper mapper = new ObjectMapper();
+        PingableLeader extLeader = Feign.builder()
+                .decoder(new TextDelegateDecoder(new JacksonDecoder()))
+                .encoder(new JacksonEncoder(mapper))
+                .contract(new JAXRSContract())
+                .target(PingableLeader.class, extServer);
+        PaxosLearner extLearner = Feign.builder()
+                .decoder(new TextDelegateDecoder(new JacksonDecoder()))
+                .encoder(new JacksonEncoder(mapper))
+                .contract(new JAXRSContract())
+                .target(PaxosLearner.class, extServer);
+        PaxosAcceptor extAcceptor = Feign.builder()
+                .decoder(new TextDelegateDecoder(new JacksonDecoder()))
+                .encoder(new JacksonEncoder(mapper))
+                .contract(new JAXRSContract())
+                .target(PaxosAcceptor.class, extServer);
+
+        PaxosProposer proposer = PaxosProposerImpl.newProposer(
+        		learner,
+        		ImmutableList.<PaxosAcceptor>of(acceptor),
+        		ImmutableList.<PaxosLearner>of(learner),
+        		configuration.quorumSize,
+        		executor);
+        PaxosLeaderElectionService leader = new PaxosLeaderElectionService(
+                proposer,
+                learner,
+                ImmutableList.of(extLeader),
+                ImmutableList.of(extAcceptor),
+                ImmutableList.of(extLearner),
+                executor,
+                0,
+                0,
+                0);
+
+        environment.jersey().register(leader);
+        TimestampService timestamp = AwaitingLeadershipProxy.newProxyInstance(TimestampService.class, new Supplier<TimestampService>() {
+            @Override
+            public TimestampService get() {
+                // TODO: actually use persistent TS
+                return new RateLimitedTimestampService(new InMemoryTimestampService(), 0L);
+            }
+        }, leader);
+
+        RemoteLockService lock = AwaitingLeadershipProxy.newProxyInstance(RemoteLockService.class, new Supplier<RemoteLockService>() {
+            @Override
+            public RemoteLockService get() {
+                return LockServiceImpl.create();
+            }
+        }, leader);
     }
-
-
 }
