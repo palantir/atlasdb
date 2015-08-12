8593f34d467b7149a7180043c5322ca18755b664
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-08-12 16:34:17 -0700

    Use embedded Postgres for unit testing.

--- atlasdb-rdbms/src/test/java/com/palantir/atlasdb/keyvalue/rdbms/PostgresKeyValueServiceTest.java
+++ atlasdb-rdbms/src/test/java/com/palantir/atlasdb/keyvalue/rdbms/PostgresKeyValueServiceTest.java
@@ -1,38 +1,60 @@
 package com.palantir.atlasdb.keyvalue.rdbms;
 
-import javax.sql.DataSource;
+import java.io.IOException;
 
+import org.junit.AfterClass;
 import org.postgresql.jdbc2.optional.PoolingDataSource;
 
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.impl.AbstractAtlasDbKeyValueServiceTest;
+import com.palantir.common.base.Throwables;
+
+import ru.yandex.qatools.embed.postgresql.PostgresExecutable;
+import ru.yandex.qatools.embed.postgresql.PostgresProcess;
+import ru.yandex.qatools.embed.postgresql.PostgresStarter;
+import ru.yandex.qatools.embed.postgresql.config.PostgresConfig;
 
 public class PostgresKeyValueServiceTest extends AbstractAtlasDbKeyValueServiceTest {
 
-    private KeyValueService kvs;
+    private static final String DB_NAME = "test";
 
-    public static DataSource getTestPostgresDataSource() {
-        PoolingDataSource pgDataSource = new PoolingDataSource();
-        pgDataSource.setDatabaseName("test");
-        pgDataSource.setUser("test");
-        pgDataSource.setPassword("password");
-        return pgDataSource;
+    final static PostgresStarter<PostgresExecutable, PostgresProcess> runtime;
+    final static PostgresConfig config;
+    final static PostgresExecutable exec;
+    final static PostgresProcess process;
+    final static PoolingDataSource dataSource;
+    static {
+        try {
+            runtime = PostgresStarter.getDefaultInstance();
+            config = PostgresConfig.defaultWithDbName(DB_NAME);
+            exec = runtime.prepare(config);
+            process = exec.start();
+            dataSource = new PoolingDataSource();
+            dataSource.setDatabaseName(config.storage().dbName());
+        } catch (IOException e) {
+            throw Throwables.throwUncheckedException(e);
+        }
     }
 
-    public static PostgresKeyValueService newTestInstance() {
-        PostgresKeyValueService ret = new PostgresKeyValueService(getTestPostgresDataSource());
-        ret.initializeFromFreshInstance();
-        return ret;
+    private KeyValueService kvs;
+
+    @AfterClass
+    public static void shutdownEmbeddedPostgres() {
+        process.stop();
+    }
+
+    public static PostgresKeyValueService newEmbeddedInstance() {
+        PostgresKeyValueService kvs = new PostgresKeyValueService(dataSource);
+        kvs.initializeFromFreshInstance();
+        return kvs;
     }
 
     @Override
     protected KeyValueService getKeyValueService() {
         if (kvs == null) {
-            KeyValueService ret = new PostgresKeyValueService(getTestPostgresDataSource());
-            ret.initializeFromFreshInstance();
+            KeyValueService ret = newEmbeddedInstance();
             kvs = ret;
         }
         return kvs;
     }
-
 }
