29e60e5011c083d9771ad24f47a0cb4f1907d198
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-08-13 22:04:00 -0700

    Add gcsentinevalues test.

--- atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/keyvalue/impl/AbstractAtlasDbKeyValueServiceTest.java
+++ atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/keyvalue/impl/AbstractAtlasDbKeyValueServiceTest.java
@@ -490,6 +490,28 @@ public abstract class AbstractAtlasDbKeyValueServiceTest {
         }
     }
 
+    @Test
+    public void testAddGCSentinelValues() {
+        putTestDataForMultipleTimestamps();
+        Cell cell = Cell.create(row0, column0);
+
+        Multimap<Cell, Long> timestampsBefore = keyValueService.getAllTimestamps(TEST_TABLE, ImmutableSet.of(cell), Long.MAX_VALUE);
+        assertEquals(2, timestampsBefore.size());
+        assertTrue(!timestampsBefore.containsEntry(cell, Value.INVALID_VALUE_TIMESTAMP));
+
+        keyValueService.addGarbageCollectionSentinelValues(TEST_TABLE, ImmutableSet.of(cell));
+
+        Multimap<Cell, Long> timestampsAfter1 = keyValueService.getAllTimestamps(TEST_TABLE, ImmutableSet.of(cell), Long.MAX_VALUE);
+        assertEquals(3, timestampsAfter1.size());
+        assertTrue(timestampsAfter1.containsEntry(cell, Value.INVALID_VALUE_TIMESTAMP));
+
+        keyValueService.addGarbageCollectionSentinelValues(TEST_TABLE, ImmutableSet.of(cell));
+
+        Multimap<Cell, Long> timestampsAfter2 = keyValueService.getAllTimestamps(TEST_TABLE, ImmutableSet.of(cell), Long.MAX_VALUE);
+        assertEquals(3, timestampsAfter2.size());
+        assertTrue(timestampsAfter2.containsEntry(cell, Value.INVALID_VALUE_TIMESTAMP));
+    }
+
     private void putTestDataForMultipleTimestamps() {
         keyValueService.put(TEST_TABLE,
                 ImmutableMap.of(Cell.create(row0, column0), value0_t0), TEST_TIMESTAMP);
