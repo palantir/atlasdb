e5a4ac16e5d862743647edb39be76cc5ce67b32c
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-08-20 14:13:23 -0700

    Add byte[] key deserializer.

---
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/keyvalue/remoting/BytesAsKeyDeserializer.java
@@ -0,0 +1,25 @@
+package com.palantir.atlasdb.keyvalue.remoting;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.KeyDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+final class BytesAsKeyDeserializer extends KeyDeserializer {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final static BytesAsKeyDeserializer instance = new BytesAsKeyDeserializer();
+
+    @Override
+    public Object deserializeKey(String key, DeserializationContext ctxt) throws IOException,
+            JsonProcessingException {
+        return mapper.readValue(key, byte[].class);
+    }
+
+    public static BytesAsKeyDeserializer instance() {
+        return instance;
+    }
+
+}
