a5c8c9492318097493cc65b837d4cf18184cedc0
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-08-20 14:11:27 -0700

    Rename CellKeySerializer to SaneKeySerializer.

--- atlasdb-client/src/main/java/com/palantir/atlasdb/keyvalue/remoting/SaneAsKeySerializer.java
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/keyvalue/remoting/SaneAsKeySerializer.java
@@ -7,19 +7,18 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.palantir.atlasdb.keyvalue.api.Cell;
 
-final class CellAsKeySerializer extends JsonSerializer<Cell> {
+final class SaneAsKeySerializer extends JsonSerializer<Object> {
 
     private final ObjectMapper mapper = new ObjectMapper();
-    private CellAsKeySerializer() { }
-    private static final CellAsKeySerializer instance = new CellAsKeySerializer();
-    public static CellAsKeySerializer instance() {
+    private SaneAsKeySerializer() { }
+    private static final SaneAsKeySerializer instance = new SaneAsKeySerializer();
+    public static SaneAsKeySerializer instance() {
         return instance;
     }
 
     @Override
-    public void serialize(Cell value, JsonGenerator gen, SerializerProvider serializers)
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers)
             throws IOException, JsonProcessingException {
         gen.writeFieldName(mapper.writeValueAsString(value));
     }
