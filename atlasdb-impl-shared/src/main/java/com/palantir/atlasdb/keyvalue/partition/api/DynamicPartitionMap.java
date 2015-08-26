dbcab9cb6f7d494e2d0f7cb5ef4ec03c92eb642b
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-08-26 11:45:18 -0700

    Fix DynamicPartitionMap API.

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/partition/api/DynamicPartitionMap.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/partition/api/DynamicPartitionMap.java
@@ -5,10 +5,5 @@ import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 public interface DynamicPartitionMap extends PartitionMap {
     void addEndpoint(byte[] key, KeyValueService kvs, String rack);
     void removeEndpoint(byte[] key, KeyValueService kvs, String rack);
-
-    // Block until all scheduled removals complete.
-    void syncRemovedEndpoints();
-
-    // Check if all scheduled removals are finished.
-    boolean removalInProgress();
+    long getVersion();
 }
