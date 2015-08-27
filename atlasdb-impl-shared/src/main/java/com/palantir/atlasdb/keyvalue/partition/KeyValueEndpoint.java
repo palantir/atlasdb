ac8d070fb043c2f47acd960e8e40297bc637de73
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-08-26 18:25:43 -0700

    Begin KVE serialization

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/partition/KeyValueEndpoint.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/partition/KeyValueEndpoint.java
@@ -1,10 +1,13 @@
 package com.palantir.atlasdb.keyvalue.partition;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.google.common.base.Function;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 
+@JsonTypeInfo(use=Id.CLASS, property="@class")
 public interface KeyValueEndpoint {
+    KeyValueService keyValueService();
+    PartitionMapService partitionMapService();
     <T> T run(Function<KeyValueService, T> task);
-//    String getRack();
-//    String getUri();
 }
