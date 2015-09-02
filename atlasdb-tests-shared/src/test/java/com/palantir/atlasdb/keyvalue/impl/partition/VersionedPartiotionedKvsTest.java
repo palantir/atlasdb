d7723bc4e4f6cdd34e9fa23b9e8cdc61f8b58a45
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-09-02 12:04:19 -0700

    Some refactor of VersionedPartitionedKvsTest.

--- atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/keyvalue/impl/partition/VersionedPartiotionedKvsTest.java
+++ atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/keyvalue/impl/partition/VersionedPartiotionedKvsTest.java
@@ -57,68 +57,65 @@ public class VersionedPartiotionedKvsTest extends AbstractAtlasDbKeyValueService
      *
      */
 
-    RemoteEndpoint ept1 = new RemoteEndpoint(new InMemoryKeyValueService(false), new PartitionMapServiceImpl());
-    RemoteEndpoint ept2 = new RemoteEndpoint(new InMemoryKeyValueService(false), new PartitionMapServiceImpl());
-    RemoteEndpoint ept3 = new RemoteEndpoint(new InMemoryKeyValueService(false), new PartitionMapServiceImpl());
-    RemoteEndpoint ept4 = new RemoteEndpoint(new InMemoryKeyValueService(false), new PartitionMapServiceImpl());
+    private static int NUM_EPTS = 4;
+    RemoteEndpoint[] epts = new RemoteEndpoint[NUM_EPTS]; {
+        for (int i=0; i<NUM_EPTS; ++i) {
+            epts[i] = new RemoteEndpoint(new InMemoryKeyValueService(false), new PartitionMapServiceImpl());
+        }
+    };
 
-    SimpleKeyValueEndpoint kve1;
-    SimpleKeyValueEndpoint kve2;
-    SimpleKeyValueEndpoint kve3;
-    SimpleKeyValueEndpoint kve4;
+    SimpleKeyValueEndpoint[] skves = new SimpleKeyValueEndpoint[NUM_EPTS];
 
     NavigableMap<byte[], KeyValueEndpoint> ring;
     DynamicPartitionMapImpl pmap;
     PartitionedKeyValueService pkvs;
 
-    @Rule public DropwizardClientRule kvsRule1 = ept1.kvs.rule;
-    @Rule public DropwizardClientRule kvsRule2 = ept2.kvs.rule;
-    @Rule public DropwizardClientRule kvsRule3 = ept3.kvs.rule;
-    @Rule public DropwizardClientRule kvsRule4 = ept4.kvs.rule;
-    @Rule public DropwizardClientRule pmsRule1 = ept1.pms.rule;
-    @Rule public DropwizardClientRule pmsRule2 = ept2.pms.rule;
-    @Rule public DropwizardClientRule pmsRule3 = ept3.pms.rule;
-    @Rule public DropwizardClientRule pmsRule4 = ept4.pms.rule;
+    @Rule public DropwizardClientRule kvsRule1 = epts[0].kvs.rule;
+    @Rule public DropwizardClientRule kvsRule2 = epts[1].kvs.rule;
+    @Rule public DropwizardClientRule kvsRule3 = epts[2].kvs.rule;
+    @Rule public DropwizardClientRule kvsRule4 = epts[3].kvs.rule;
+    @Rule public DropwizardClientRule pmsRule1 = epts[0].pms.rule;
+    @Rule public DropwizardClientRule pmsRule2 = epts[1].pms.rule;
+    @Rule public DropwizardClientRule pmsRule3 = epts[2].pms.rule;
+    @Rule public DropwizardClientRule pmsRule4 = epts[3].pms.rule;
 
     public void setUpPrivate() {
-        kve1 = new SimpleKeyValueEndpoint(ept1.kvs.rule.baseUri().toString(), ept1.pms.rule.baseUri().toString());
-        kve2 = new SimpleKeyValueEndpoint(ept2.kvs.rule.baseUri().toString(), ept2.pms.rule.baseUri().toString());
-        kve3 = new SimpleKeyValueEndpoint(ept3.kvs.rule.baseUri().toString(), ept3.pms.rule.baseUri().toString());
-        kve4 = new SimpleKeyValueEndpoint(ept4.kvs.rule.baseUri().toString(), ept4.pms.rule.baseUri().toString());
+        for (int i=0; i<NUM_EPTS; ++i) {
+            skves[i] = new SimpleKeyValueEndpoint(epts[i].kvs.rule.baseUri().toString(), epts[i].pms.rule.baseUri().toString());
+        }
 
         ring = Maps.newTreeMap(UnsignedBytes.lexicographicalComparator());
-        ring.put(new byte[] {0},       kve1);
-        ring.put(new byte[] {0, 0},    kve2);
-        ring.put(new byte[] {0, 0, 0}, kve3);
-        // Do not insert kve4 - it will be used later to test addEndpoint
+        ring.put(new byte[] {0},       skves[0]);
+        ring.put(new byte[] {0, 0},    skves[1]);
+        ring.put(new byte[] {0, 0, 0}, skves[2]);
+        // Do not insert skves[3] - it will be used later to test addEndpoint
 
         pmap = DynamicPartitionMapImpl.create(ring);
+        // We do not tolerate failures in this test. It is important since the
+        // non-critical operations are done asynchronously and might not finishi
+        // before checking the results.
         pkvs = PartitionedKeyValueService.create(new QuorumParameters(3, 3, 3), pmap);
 
         // Push the map to all the endpoints
-        kve1.partitionMapService().updateMap(pmap);
-        kve2.partitionMapService().updateMap(pmap);
-        kve3.partitionMapService().updateMap(pmap);
+        for (int i=0; i<NUM_EPTS-1; ++i) {
+            skves[i].partitionMapService().updateMap(pmap);
+        }
     }
 
     @Before
     public void setupHacks() {
-        Utils.setupRuleHacks(kvsRule1);
-        Utils.setupRuleHacks(kvsRule2);
-        Utils.setupRuleHacks(kvsRule3);
-        Utils.setupRuleHacks(kvsRule4);
-        Utils.setupRuleHacks(pmsRule1);
-        Utils.setupRuleHacks(pmsRule2);
-        Utils.setupRuleHacks(pmsRule3);
-        Utils.setupRuleHacks(kvsRule4);
+        for (int i=0; i<NUM_EPTS; ++i) {
+            Utils.setupRuleHacks(epts[i].kvs.rule);
+            Utils.setupRuleHacks(epts[i].pms.rule);
+        }
     }
 
     @Test
     public void testVersionTooOld() {
     	pmap.setVersion(1L);
-    	kve1.partitionMapService().updateMap(pmap);
+    	skves[0].partitionMapService().updateMap(pmap);
     	pmap.setVersion(0L);
-    	assertEquals(1L, kve1.partitionMapService().getMapVersion());
+    	assertEquals(1L, skves[0].partitionMapService().getMapVersion());
     	try {
     		pkvs.createTable("TABLE_NAME_2", 12345);
     		// This has to throw since table metadata is to be
@@ -134,13 +131,13 @@ public class VersionedPartiotionedKvsTest extends AbstractAtlasDbKeyValueService
     public void testAddEndpoint() {
         Map<Cell, Value> emptyResult = ImmutableMap.<Cell, Value>of();
 
-        kve4.partitionMapService().updateMap(pkvs.getPartitionMap());
-        pkvs.getPartitionMap().addEndpoint(new byte[] {(byte)0xff, 0, 0, 0}, kve4, "", false);
+        skves[NUM_EPTS - 1].partitionMapService().updateMap(pkvs.getPartitionMap());
+        pkvs.getPartitionMap().addEndpoint(new byte[] {(byte)0xff, 0, 0, 0}, skves[NUM_EPTS-1], "", false);
         pkvs.getPartitionMap().syncAddEndpoint();
-        kve4.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve1.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve2.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve3.partitionMapService().updateMap(pkvs.getPartitionMap());
+        skves[NUM_EPTS - 1].partitionMapService().updateMap(pkvs.getPartitionMap());
+        for (int i=0; i<NUM_EPTS - 1; ++i) {
+            skves[i].partitionMapService().updateMap(pkvs.getPartitionMap());
+        }
 
         Map<Cell, Long> cells0 = ImmutableMap.of(Cell.create(row0, column0), TEST_TIMESTAMP + 1);
         Map<Cell, byte[]> values0 = ImmutableMap.of(Cell.create(row0, column0), value00);
@@ -157,16 +154,15 @@ public class VersionedPartiotionedKvsTest extends AbstractAtlasDbKeyValueService
         assertEquals(1L, pkvs.getPartitionMap().getVersion());
         pkvs.put(TEST_TABLE, values0, TEST_TIMESTAMP);
 
-        assertEquals(result0, ept1.kvs.delegate.get(TEST_TABLE, cells0));
-        assertEquals(result0, ept2.kvs.delegate.get(TEST_TABLE, cells0));
-        assertEquals(result0, ept3.kvs.delegate.get(TEST_TABLE, cells0));
-        assertEquals(result0, ept4.kvs.delegate.get(TEST_TABLE, cells0));
+        for (int i=0; i<NUM_EPTS; ++i) {
+            assertEquals(result0, epts[i].kvs.delegate.get(TEST_TABLE, cells0));
+        }
 
         pkvs.getPartitionMap().finalizeAddEndpoint(new byte[] {(byte)0xff, 0, 0, 0});
-        kve4.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve1.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve2.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve3.partitionMapService().updateMap(pkvs.getPartitionMap());
+        skves[NUM_EPTS - 1].partitionMapService().updateMap(pkvs.getPartitionMap());
+        for (int i=0; i<NUM_EPTS - 1; ++i) {
+            skves[i].partitionMapService().updateMap(pkvs.getPartitionMap());
+        }
 
         Map<Cell, Long> cells1 = ImmutableMap.of(Cell.create(row0, column1), TEST_TIMESTAMP + 1);
         Map<Cell, byte[]> values1 = ImmutableMap.of(Cell.create(row0, column1), value01);
@@ -174,16 +170,16 @@ public class VersionedPartiotionedKvsTest extends AbstractAtlasDbKeyValueService
 
         // This time without a forced updateMap
         assertEquals(2L, pkvs.getPartitionMap().getVersion());
-        assertEquals(2L, kve1.partitionMapService().getMapVersion());
-        assertEquals(2L, kve2.partitionMapService().getMapVersion());
-        assertEquals(2L, kve3.partitionMapService().getMapVersion());
-        assertEquals(2L, kve4.partitionMapService().getMapVersion());
+        for (int i=0; i<NUM_EPTS; ++i) {
+            assertEquals(2L, skves[i].partitionMapService().getMapVersion());
+        }
+
         pkvs.put(TEST_TABLE, values1, TEST_TIMESTAMP);
 
-        assertEquals(result1, ept1.kvs.delegate.get(TEST_TABLE, cells1));
-        assertEquals(result1, ept2.kvs.delegate.get(TEST_TABLE, cells1));
-        assertEquals(emptyResult, ept3.kvs.delegate.get(TEST_TABLE, cells1));
-        assertEquals(result1, ept4.kvs.delegate.get(TEST_TABLE, cells1));
+        assertEquals(result1, epts[0].kvs.delegate.get(TEST_TABLE, cells1));
+        assertEquals(result1, epts[1].kvs.delegate.get(TEST_TABLE, cells1));
+        assertEquals(emptyResult, epts[2].kvs.delegate.get(TEST_TABLE, cells1));
+        assertEquals(result1, epts[3].kvs.delegate.get(TEST_TABLE, cells1));
     }
 
     @Test
@@ -192,44 +188,42 @@ public class VersionedPartiotionedKvsTest extends AbstractAtlasDbKeyValueService
         Map<Cell, Value> emptyResult = ImmutableMap.<Cell, Value>of();
 
         // First add the endpoint so that we can remove one
-        kve4.partitionMapService().updateMap(pkvs.getPartitionMap());
+        skves[NUM_EPTS - 1].partitionMapService().updateMap(pkvs.getPartitionMap());
 
-        pkvs.getPartitionMap().addEndpoint(new byte[] {(byte)0xff, 0, 0, 0}, kve4, "", false);
+        pkvs.getPartitionMap().addEndpoint(new byte[] {(byte)0xff, 0, 0, 0}, skves[NUM_EPTS - 1], "", false);
         pkvs.getPartitionMap().syncAddEndpoint();
-        kve4.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve1.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve2.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve3.partitionMapService().updateMap(pkvs.getPartitionMap());
+        skves[NUM_EPTS - 1].partitionMapService().updateMap(pkvs.getPartitionMap());
+        for (int i=0; i<NUM_EPTS - 1; ++i) {
+            skves[i].partitionMapService().updateMap(pkvs.getPartitionMap());
+        }
 
         pkvs.getPartitionMap().finalizeAddEndpoint(new byte[] {(byte)0xff, 0, 0, 0});
-        kve4.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve1.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve2.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve3.partitionMapService().updateMap(pkvs.getPartitionMap());
+        skves[NUM_EPTS - 1].partitionMapService().updateMap(pkvs.getPartitionMap());
+        for (int i=0; i<NUM_EPTS - 1; ++i) {
+            skves[i].partitionMapService().updateMap(pkvs.getPartitionMap());
+        }
 
         pkvs.createTable(TEST_TABLE, 12345);
 
         pkvs.getPartitionMap().removeEndpoint(new byte[] {0, 0}, false);
         pkvs.getPartitionMap().syncRemoveEndpoint();
-        kve1.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve2.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve3.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve4.partitionMapService().updateMap(pkvs.getPartitionMap());
+        for (int i=0; i<NUM_EPTS; ++i) {
+            skves[i].partitionMapService().updateMap(pkvs.getPartitionMap());
+        }
 
         Map<Cell, Long> cells0 = ImmutableMap.of(Cell.create(row0, column0), TEST_TIMESTAMP + 1);
         Map<Cell, byte[]> values0 = ImmutableMap.of(Cell.create(row0, column0), value00);
         Map<Cell, Value> result0 = ImmutableMap.of(Cell.create(row0, column0), Value.create(value00, TEST_TIMESTAMP));
 
         pkvs.put(TEST_TABLE, values0, TEST_TIMESTAMP);
-        assertEquals(result0, ept1.kvs.delegate.get(TEST_TABLE, cells0));
-        assertEquals(result0, ept2.kvs.delegate.get(TEST_TABLE, cells0));
-        assertEquals(result0, ept3.kvs.delegate.get(TEST_TABLE, cells0));
-        assertEquals(result0, ept4.kvs.delegate.get(TEST_TABLE, cells0));
+        for (int i=0; i<NUM_EPTS; ++i) {
+            assertEquals(result0, epts[i].kvs.delegate.get(TEST_TABLE, cells0));
+        }
 
         pkvs.getPartitionMap().finalizeRemoveEndpoint(new byte[] {0, 0});
-        kve1.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve3.partitionMapService().updateMap(pkvs.getPartitionMap());
-        kve4.partitionMapService().updateMap(pkvs.getPartitionMap());
+        skves[0].partitionMapService().updateMap(pkvs.getPartitionMap());
+        skves[2].partitionMapService().updateMap(pkvs.getPartitionMap());
+        skves[3].partitionMapService().updateMap(pkvs.getPartitionMap());
 
         Map<Cell, Long> cells1 = ImmutableMap.of(Cell.create(row0, column1), TEST_TIMESTAMP + 1);
         Map<Cell, byte[]> values1 = ImmutableMap.of(Cell.create(row0, column1), value01);
@@ -237,10 +231,10 @@ public class VersionedPartiotionedKvsTest extends AbstractAtlasDbKeyValueService
 
         pkvs.put(TEST_TABLE, values1, TEST_TIMESTAMP);
 
-        assertEquals(result1, ept1.kvs.delegate.get(TEST_TABLE, cells1));
-        assertEquals(emptyResult, ept2.kvs.delegate.get(TEST_TABLE, cells1));
-        assertEquals(result1, ept3.kvs.delegate.get(TEST_TABLE, cells1));
-        assertEquals(result1, ept4.kvs.delegate.get(TEST_TABLE, cells1));
+        assertEquals(result1, epts[0].kvs.delegate.get(TEST_TABLE, cells1));
+        assertEquals(emptyResult, epts[1].kvs.delegate.get(TEST_TABLE, cells1));
+        assertEquals(result1, epts[2].kvs.delegate.get(TEST_TABLE, cells1));
+        assertEquals(result1, epts[3].kvs.delegate.get(TEST_TABLE, cells1));
     }
 
     @Override
