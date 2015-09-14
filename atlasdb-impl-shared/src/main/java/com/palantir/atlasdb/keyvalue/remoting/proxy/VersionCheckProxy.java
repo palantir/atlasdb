18fd0a24723988d22adb48a8fc4023ff0521bb62
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-09-14 13:15:34 -0700

    Exempt some methods from version check to fix the issue with table metadata cache.

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/remoting/proxy/VersionCheckProxy.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/remoting/proxy/VersionCheckProxy.java
@@ -4,12 +4,14 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Supplier;
+import com.google.common.collect.ImmutableSet;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.partition.exception.VersionTooOldException;
 import com.palantir.atlasdb.keyvalue.remoting.RemotingKeyValueService.HOLDER;
@@ -40,13 +42,47 @@ public class VersionCheckProxy implements InvocationHandler {
         this.delegate = delegate;
     }
 
+
+    /*
+     * Table metadata methods are version check-exempt because the metadata
+     * is stored on all enpoints anyway.
+     *
+     * This is necessary because the metadata skips transaction manager and
+     * does not retry automatically on retryable exception.
+     *
+     * Some other "technical" methods are also exempt.
+     */
+    private static final boolean isMethodVersionExempts(Method method) {
+        return method.getDeclaringClass() == KeyValueService.class && EXEMPT_METHODS.contains(method.getName());
+    }
+
+    private static final Set<String> EXEMPT_METHODS = ImmutableSet.<String>builder()
+            // Table and table metadata methods
+            .add("dropTable")
+            .add("createTable")
+            .add("createTables")
+            .add("truncateTable")
+            .add("truncateTables")
+            .add("getAllTableNames")
+            .add("getAllTableNames")
+            .add("getMetadataForTable")
+            .add("getMetadataForTables")
+            .add("putMetadataForTable")
+            .add("putMetadataForTables")
+            // Technical methods
+            .add("close")
+            .add("teardown")
+            .add("initializeFromFreshInstance")
+            .add("compactInternally")
+            .build();
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
         ServiceContext<Long> remoteClientCtx = RemoteContextHolder.INBOX.getProviderForKey(HOLDER.PM_VERSION);
 
         // Only check the version for the interface methods.
-        if (method.getDeclaringClass() == KeyValueService.class) {
+        if (method.getDeclaringClass() == KeyValueService.class && !isMethodVersionExempts(method)) {
             Long clientVersion = remoteClientCtx.get();
             Long serverVersion = Preconditions.checkNotNull(serverVersionProvider.get());
             if (serverVersion < 0L) {
