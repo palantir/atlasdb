2c56c17ae6f0cdc1834e619dcffebf4aa95491e0
Hubert Tarasiuk <htarasiuk@palantir.com>
2015-09-25 11:17:37 -0700

    Remove ignore annotation from snapshot transaction tests.

--- atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/transaction/impl/SnapshotTransactionTest.java
+++ atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/transaction/impl/SnapshotTransactionTest.java
@@ -88,7 +88,6 @@ import com.palantir.lock.LockRefreshToken;
 import com.palantir.lock.LockRequest;
 import com.palantir.lock.LockService;
 
-@Ignore
 public class SnapshotTransactionTest extends AtlasDbTestCase {
     private class UnstableKeyValueService extends ForwardingKeyValueService {
         private final KeyValueService delegate;
