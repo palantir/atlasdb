126b42d1d2072d0680705919d2045b01fa32b41c
Bobby Ullman <bullman@palantir.com>
2015-10-05 11:15:12 -0700

    exposed some of persistent timestamps internals

--- timestamp-impl/src/main/java/com/palantir/timestamp/PersistentTimestampService.java
+++ timestamp-impl/src/main/java/com/palantir/timestamp/PersistentTimestampService.java
@@ -50,7 +50,7 @@ public class PersistentTimestampService implements TimestampService {
     private Clock clock;
     private long lastAllocatedTime;
 
-    public static TimestampService create(TimestampBoundStore tbs) {
+    public static PersistentTimestampService create(TimestampBoundStore tbs) {
         PersistentTimestampService ts = new PersistentTimestampService(
                 tbs,
                 tbs.getUpperLimit(),
@@ -64,12 +64,12 @@ public class PersistentTimestampService implements TimestampService {
     }
 
     @VisibleForTesting
-    protected static TimestampService create(TimestampBoundStore tbs, Clock clock) {
+    protected static PersistentTimestampService create(TimestampBoundStore tbs, Clock clock) {
         PersistentTimestampService ts = new PersistentTimestampService(tbs, tbs.getUpperLimit(), clock);
         return init(ts);
     }
 
-    private static TimestampService init(PersistentTimestampService ts) {
+    private static PersistentTimestampService init(PersistentTimestampService ts) {
         ts.allocateMoreTimestamps();
         return ts;
     }
@@ -82,6 +82,10 @@ public class PersistentTimestampService implements TimestampService {
         isAllocationTaskSubmitted = new AtomicBoolean(false);
         this.clock = clock;
         lastAllocatedTime = clock.getTimeMillis();
+    }
+
+    public long getUpperLimitTimestampToHandOutInclusive() {
+        return upperLimitToHandOutInclusive.get();
     }
 
     private synchronized void allocateMoreTimestamps() {
