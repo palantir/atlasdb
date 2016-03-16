f658958db7b7369243297eb27a6b807979d9d6b2
Alessandra Loro <aloro@palantir.com>
2016-03-16 16:07:49 -0700

    RegenerateCodeForSchema: changing to System call to logger calls

--- atlasdb-exec/src/main/java/com/palantir/atlasdb/clis/RegenerateCodeForSchemas.java
+++ atlasdb-exec/src/main/java/com/palantir/atlasdb/clis/RegenerateCodeForSchemas.java
@@ -21,6 +21,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.slf4j.Logger;
+
 import com.google.common.collect.Lists;
 import com.palantir.atlasdb.schema.AtlasSchema;
 import com.palantir.atlasdb.schema.SweepSchema;
@@ -30,18 +32,19 @@ import com.palantir.atlasdb.schema.SweepSchema;
  *
  */
 public class RegenerateCodeForSchemas {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(RegenerateCodeForSchemas.class);
 
     public static void main(String[] args) {
         List<AtlasSchema> schemas = Lists.newArrayList(SweepSchema.INSTANCE);
 
         for (AtlasSchema schema : schemas) {
             try {
-                System.out.println("Attempting to generate source code for schema: " + schema.getClass().getCanonicalName());
+                log.info("Attempting to generate source code for schema: " + schema.getClass().getCanonicalName());
 
                 Path location = Paths.get(schema.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
-                System.out.println("Generating source directory related to classes in " + location);
+                log.info("Generating source directory related to classes in " + location);
                 if (location.toString().endsWith("jar")) {
-                    System.err.println("You need to set your classpaths to not come from jars / ivy / whatever.");
+                    log.error("You need to set your classpaths to not come from jars / ivy / whatever.");
                     System.exit(1);
                 }
 
@@ -51,14 +54,14 @@ public class RegenerateCodeForSchemas {
                 }
 
                 if (!Files.exists(sourceDirLocation)) {
-                    System.out.println("Didn't find anything at " + sourceDirLocation + ", trying another directory...");
+                    log.info("Didn't find anything at " + sourceDirLocation + ", trying another directory...");
                     sourceDirLocation = resolveIdeaSourceLocation(sourceDirLocation);
                     if (!Files.exists(sourceDirLocation)) {
-                        System.err.println("Could not find a matching source code directory for this project.");
+                        log.error("Could not find a matching source code directory for this project.");
                         continue;
                     }
                 }
-                System.out.println("Placing source in: " + sourceDirLocation + "\n");
+                log.info("Placing source in: " + sourceDirLocation + "\n");
                 schema.getLatestSchema().renderTables(sourceDirLocation.toFile());
             } catch (IOException e) {
                 e.printStackTrace();
