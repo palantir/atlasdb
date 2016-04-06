50670781f790058360755458f2d515348558a5ac
Alessandra Loro <aloro@palantir.com>
2016-04-06 14:53:16 +0100

    #310 Moved Cassandra docker-container. Added docker plugin script to run internally

---
+++ scripts/ete.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+cd $(dirname $0)/..
+
+./gradlew dockerPrepare --parallel --console=plain
+
+cd docker-containers 
+
+function on_exit() {
+    docker-compose stop -t 1
+}
+
+docker-compose rm -f
+docker-compose build
+docker-compose up -t 0 --no-build -d 
+trap "on_exit" EXIT
+docker-compose logs &
+docker-compose run --rm ete ./gradlew atlasdb-cassandra-tests:test --console=plain --info --continue 
