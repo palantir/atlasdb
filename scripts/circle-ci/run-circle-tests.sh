488aa00307d5eb6067cb39e6d288805cf359890e
Joe Lea <joeleapublic@gmail.com>
2016-05-11 13:16:36 +0100

    Make circle scripts clearer

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -2,11 +2,16 @@
 
 set -x
 
+function checkDocsBuild {
+  cd docs/
+  make html
+}
+
 CASSANDRA=':atlasdb-cassandra-tests:check'
 SHARED=':atlasdb-tests-shared:check'
 
 case $CIRCLE_NODE_INDEX in
     0) ./gradlew --continue check -x $CASSANDRA -x $SHARED ;;
     1) ./gradlew --continue --parallel $CASSANDRA --console=plain ;;
-    2) ./gradlew --continue --parallel $SHARED && cd docs/; make html ;;
+    2) ./gradlew --continue --parallel $SHARED && checkDocsBuild ;;
 esac
