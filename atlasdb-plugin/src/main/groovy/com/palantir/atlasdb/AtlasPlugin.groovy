c768340c423cd196eed67b0b28d0b98418226760
Bobby Ullman <bullman@palantir.com>
2016-05-16 10:20:51 -0700

    add basic plugin setup and sls test

--- atlasdb-plugin/src/main/groovy/com/palantir/atlasdb/AtlasPlugin.groovy
+++ atlasdb-plugin/src/main/groovy/com/palantir/atlasdb/AtlasPlugin.groovy
@@ -1,11 +1,15 @@
 package com.palantir.atlasdb
 
+import com.palantir.gradle.javadist.JavaDistributionPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
 
 class AtlasPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
+        project.plugins.apply(JavaPlugin)
+        project.plugins.apply(JavaDistributionPlugin)
         project.extensions.create("atlasdb", AtlasPluginExtension)
     }
 }
