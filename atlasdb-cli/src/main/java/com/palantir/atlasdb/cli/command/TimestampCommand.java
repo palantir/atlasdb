1256fe1d455b1b740acb96c9b6b3fcd08673896a
Tom Petracca <tpetracca@palantir.com>
2016-05-18 15:25:10 -0400

    fresh by default; option to write output to file

--- atlasdb-cli/src/main/java/com/palantir/atlasdb/cli/command/TimestampCommand.java
+++ atlasdb-cli/src/main/java/com/palantir/atlasdb/cli/command/TimestampCommand.java
@@ -15,57 +15,80 @@
  */
 package com.palantir.atlasdb.cli.command;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Set;
+
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 
+import com.google.common.base.Throwables;
+import com.google.common.collect.Sets;
 import com.palantir.atlasdb.cleaner.KeyValueServicePuncherStore;
 import com.palantir.atlasdb.cli.services.AtlasDbServices;
 
 import io.airlift.airline.Command;
 import io.airlift.airline.Option;
 
-@Command(name = "timestamp", description = "Get timestamp information")
+@Command(name = "timestamp", description = "Fetches a timestamp. By default"
+        + " this will be a fresh timestamp unless otherwise specified.")
 public class TimestampCommand extends SingleBackendCommand {
-    
-    @Option(name = {"-f", "--fresh"},
-            description = "Get a fresh timestamp")
-    private boolean fresh;
-    
+
     @Option(name = {"-i", "--immutable"},
-    		description = "Get the current immutable timestamp")
+    		description = "Get the current immutable timestamp, instead of a fresh one.")
     private boolean immutable;
     
     @Option(name = {"-d", "--date-time"},
-            description = "Return the earliest approximate wall clock datetime at which the immutable timestamp" +
-                    " could have been used in a transaction.  Requires --immutable to be set")
+            description = "Return the earliest approximate wall clock datetime at which the chosen timestamp" +
+                    " could have been used in a transaction.")
     private boolean dateTime;
+
+    @Option(name = {"-f", "--file"},
+            title = "TIMESTAMP_FILE",
+            description = "Write the timestamp returned by this command to the specified file.")
+    private File file;
+    
+    private static final String IMMUTABLE_STRING = "Immutable";
+    private static final String FRESH_STRING = "Fresh";
     
 	@Override
 	public int execute(AtlasDbServices services) {
-	    if(dateTime && !immutable) {
-	        System.err.println("You can't use --date-time without also using --immutable");
-	        return 1;
+	    long timestamp;
+	    String name;
+	    if (immutable) {
+    	    timestamp = services.getTransactionManager().getImmutableTimestamp();
+    	    name = IMMUTABLE_STRING;
+	    } else {
+	        timestamp = services.getTimestampService().getFreshTimestamp();
+	        name = FRESH_STRING;
 	    }
-	    
-	    long latestTimestamp = services.getTimestampService().getFreshTimestamp();
+	    System.out.printf("The %s timestamp is: %d\n", name, timestamp);
 
-        if (fresh || !(fresh || immutable)) {
-            System.out.println("Fresh timestamp is: " + latestTimestamp);
+	    String stringTime = null;
+        if (dateTime) {
+            long timeMillis = KeyValueServicePuncherStore.getMillisForTimestamp(
+                    services.getKeyValueService(), timestamp);
+            DateTime dt = new DateTime(timeMillis);
+            stringTime = ISODateTimeFormat.dateTimeNoMillis().print(dt);
+            System.out.printf("Wall clock datetime of %s timestamp is: %s\n", name, stringTime);
         }
 
-        if (immutable) {
-        	long immutableTimestamp = services.getTransactionManager().getImmutableTimestamp();
-            System.out.println("Most recent immutable timestamp is: " + immutableTimestamp);
-            
+        if (file != null) {
+            Set<String> lines = Sets.newHashSet(Long.toString(timestamp));
             if (dateTime) {
-                long timeMillis = KeyValueServicePuncherStore.getMillisForTimestamp(
-                        services.getKeyValueService(), immutableTimestamp);
-                DateTime dt = new DateTime(timeMillis);
-                String stringTime = ISODateTimeFormat.dateTimeNoMillis().print(dt);
-                System.out.println("Wall clock datetime of immutable timestamp is: " + stringTime);
+                lines.add(stringTime);
+            }
+            try {
+                Files.write(file.toPath(), lines, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                System.err.printf("IOException thrown writing %s timestamp to file: %s\n", name, file.getPath());
+                Throwables.propagate(e);
             }
         }
 
+        System.out.println("Timestamp command completed succesfully.");
         return 0;
 	}
 }
