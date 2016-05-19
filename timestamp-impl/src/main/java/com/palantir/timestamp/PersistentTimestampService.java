f93b512a0f888339799c897f35fb83c20edce00a
George Brova <gbrova@palantir.com>
2016-05-19 15:47:36 +0100

    Revert "Fix potential test flakiness by keeping the order the same as before, but making sure that we always set the submitted flag back to false"
    This reverts commit b1a554eab67c65709493d0412fdd89063842feb6.

--- timestamp-impl/src/main/java/com/palantir/timestamp/PersistentTimestampService.java
+++ timestamp-impl/src/main/java/com/palantir/timestamp/PersistentTimestampService.java
@@ -98,38 +98,34 @@ public class PersistentTimestampService implements TimestampService {
 
     volatile Throwable allocationFailure = null;
     private void submitAllocationTask() {
-        if (isAllocationTaskSubmitted.compareAndSet(false, true)) {
-            if (isAllocationRequired(lastReturnedTimestamp.get(), upperLimitToHandOutInclusive.get())) {
-                final Exception createdException = new Exception("allocation task called from here");
-                executor.submit(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            if (allocationFailure instanceof MultipleRunningTimestampServiceError) {
-                                // We cannot allocate timestamps anymore because another server is running.
-                                return;
-                            }
-                            allocateMoreTimestamps();
-                            lastAllocatedTime = clock.getTimeMillis();
-                            allocationFailure = null;
-                        } catch (Throwable e) { // (authorized)
-                            createdException.initCause(e);
-                            if (allocationFailure != null
-                                    && e.getClass().equals(allocationFailure.getClass())) {
-                                // QA-75825: don't keep logging error if we keep failing to allocate.
-                                log.info("Throwable while allocating timestamps.", createdException);
-                            } else {
-                                log.error("Throwable while allocating timestamps.", createdException);
-                            }
-                            allocationFailure = e;
-                        } finally {
-                            isAllocationTaskSubmitted.set(false);
+        if (isAllocationRequired(lastReturnedTimestamp.get(), upperLimitToHandOutInclusive.get()) && isAllocationTaskSubmitted.compareAndSet(false, true)) {
+            final Exception createdException = new Exception("allocation task called from here");
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (allocationFailure instanceof MultipleRunningTimestampServiceError) {
+                            // We cannot allocate timestamps anymore because another server is running.
+                            return;
                         }
+                        allocateMoreTimestamps();
+                        lastAllocatedTime = clock.getTimeMillis();
+                        allocationFailure = null;
+                    } catch (Throwable e) { // (authorized)
+                        createdException.initCause(e);
+                        if (allocationFailure != null
+                                && e.getClass().equals(allocationFailure.getClass())) {
+                            // QA-75825: don't keep logging error if we keep failing to allocate.
+                            log.info("Throwable while allocating timestamps.", createdException);
+                        } else {
+                            log.error("Throwable while allocating timestamps.", createdException);
+                        }
+                        allocationFailure = e;
+                    } finally {
+                        isAllocationTaskSubmitted.set(false);
                     }
-                });
-            } else {
-                isAllocationTaskSubmitted.set(false);
-            }
+                }
+            });
         }
     }
 
