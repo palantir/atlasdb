3d316f6d26ecf1efbb15eedbb91b46b8159d82d4
George Brova <gbrova@palantir.com>
2016-05-19 15:51:37 +0100

    Verify storeUpperLimit was invoked at least 2 times instead of exactly 2 times

--- timestamp-impl/src/test/java/com/palantir/timestamp/PersistentTimestampServiceTest.java
+++ timestamp-impl/src/test/java/com/palantir/timestamp/PersistentTimestampServiceTest.java
@@ -18,6 +18,7 @@ package com.palantir.timestamp;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -90,7 +91,7 @@ public class PersistentTimestampServiceTest {
         Thread.sleep(10);
         persistentTimestampService.getFreshTimestamp();
         Thread.sleep(10);
-        verify(timestampBoundStore, times(2)).storeUpperLimit(anyLong());
+        verify(timestampBoundStore, atLeast(2)).storeUpperLimit(anyLong());
     }
 
     @Test
