54e439e904b774995e420a8689fa9aefcd3f6605
John Carrino <carrino@palantir.com>
2016-05-26 10:41:36 -0700

    round out imcomplete comment

--- atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/transaction/impl/AbstractSerializableTransactionTest.java
+++ atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/transaction/impl/AbstractSerializableTransactionTest.java
@@ -467,7 +467,7 @@ public abstract class AbstractSerializableTransactionTest extends AbstractTransa
         BatchingVisitables.copyToList(t1.getRange(TEST_TABLE, RangeRequest.builder().retainColumns(ImmutableList.of(PtBytes.toBytes("col1"))).build()));
         get(t1, "row1", "col2");
 
-        // We need to do at least one put so we don't get caught by the
+        // We need to do at least one put so we don't get caught by the read only code path
         put(t1, "row22", "col2", initialValue);
 
         t1.commit();
@@ -486,7 +486,7 @@ public abstract class AbstractSerializableTransactionTest extends AbstractTransa
         BatchingVisitables.copyToList(t1.getRange(TEST_TABLE, RangeRequest.builder().retainColumns(ImmutableList.of(PtBytes.toBytes("col1"))).build()));
         BatchingVisitables.copyToList(t1.getRange(TEST_TABLE, RangeRequest.builder().retainColumns(ImmutableList.of(PtBytes.toBytes("col2"))).build()));
 
-        // We need to do at least one put so we don't get caught by the
+        // We need to do at least one put so we don't get caught by the read only code path
         put(t1, "row22", "col2", initialValue);
 
         t1.commit();
