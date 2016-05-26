370463adf5e6a91dd3b4ac7f653277cb36d4108b
Chris Lockfort <clockfort@palantir.com>
2016-05-26 08:58:17 -0700

    backport circle-ci configuration to pre-lts-support

---
+++ scripts/circle-ci/publish-github-page.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -x -e
+
+# Clone gh-pages into build
+cd docs/
+rm -rf build/
+git clone git@github.com:palantir/atlasdb.git -b gh-pages build
+
+# Rebuild the docs into the repo
+make html || { echo "doc build failed, build should not pass"; exit 1; }
+
+cd build/
+# Just to report status of repo in the build output
+git status
+git config user.email "pd-atlasdb-team@palantir.com"
+git config user.name $CIRCLE_PROJECT_USERNAME
+
+# Add and commmit changes to gh-pages
+git add .
+
+if ! git commit -m "circle-publish: [skip ci] from $CIRCLE_BRANCH by build $CIRCLE_BUILD_NUM, can be found at $CIRCLE_BUILD_URL"; then
+    echo "no docs changes, skipping deployment";
+    exit 0;
+fi
+
+git push origin gh-pages:gh-pages
