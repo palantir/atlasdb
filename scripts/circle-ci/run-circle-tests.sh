9eff147c437cba7705868c8115341f49aff3c18c
Chris Lockfort <clockfort@palantir.com>
2016-05-26 09:05:18 -0700

    remove docs from circle-ci tests

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -2,16 +2,11 @@
 
 set -x
 
-function checkDocsBuild {
-  cd docs/
-  make html
-}
-
 CASSANDRA=':atlasdb-cassandra-tests:check'
 SHARED=':atlasdb-tests-shared:check'
 
 case $CIRCLE_NODE_INDEX in
     0) ./gradlew --continue check -x $CASSANDRA -x $SHARED ;;
     1) ./gradlew --continue --parallel $CASSANDRA --console=plain ;;
-    2) ./gradlew --continue --parallel $SHARED && checkDocsBuild ;;
+    2) ./gradlew --continue --parallel $SHARED ;;
 esac
