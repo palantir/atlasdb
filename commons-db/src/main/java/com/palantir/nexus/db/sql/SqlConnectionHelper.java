0c39965ca2ae790e89bcde3cea2600448045b487
David Cohen <dcohen@palantir.com>
2016-05-27 18:16:18 -0700

    Unused import

--- commons-db/src/main/java/com/palantir/nexus/db/sql/SqlConnectionHelper.java
+++ commons-db/src/main/java/com/palantir/nexus/db/sql/SqlConnectionHelper.java
@@ -22,7 +22,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
