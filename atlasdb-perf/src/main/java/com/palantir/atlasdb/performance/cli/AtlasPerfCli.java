6eab66a3858322c6ea2d349d88722efa60280c3c
Bobby Ullman <bullman@palantir.com>
2016-06-16 15:34:49 -0700

    add gradlew building of cli script

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/cli/AtlasPerfCli.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/cli/AtlasPerfCli.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.palantir.atlasdb.performance;
+package com.palantir.atlasdb.performance.cli;
 
 import java.util.concurrent.Callable;
 
