000b9b3cd61ee2dddf2179d7900ec2e04102723a
Ben Grabham <bgrabham@palantir.com>
2016-06-23 15:28:45 +0100

    Add copyright notices

--- atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/todo/TodoEteTest.java
+++ atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/todo/TodoEteTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.palantir.atlasdb.ete.todo;
 
 import static org.hamcrest.MatcherAssert.assertThat;
