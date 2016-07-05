a9ab04d3bee8f8e2319b91e1a57c6645bf0061c5
Tom Petracca <tpetracca@palantir.com>
2016-07-05 18:29:32 -0400

    fix cli tests

--- atlasdb-cli/src/test/java/com/palantir/atlasdb/cli/command/TestTimestampCommand.java
+++ atlasdb-cli/src/test/java/com/palantir/atlasdb/cli/command/TestTimestampCommand.java
@@ -21,15 +21,25 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
+import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.Uninterruptibles;
+import com.palantir.atlasdb.AtlasDbConstants;
+import com.palantir.atlasdb.cleaner.GlobalClock;
+import com.palantir.atlasdb.cleaner.KeyValueServicePuncherStore;
+import com.palantir.atlasdb.cleaner.Puncher;
+import com.palantir.atlasdb.cleaner.PuncherStore;
+import com.palantir.atlasdb.cleaner.SimplePuncher;
 import com.palantir.atlasdb.cli.command.timestamp.FetchTimestamp;
 import com.palantir.atlasdb.cli.runner.InMemoryTestRunner;
 import com.palantir.atlasdb.cli.runner.SingleBackendCliTestRunner;
@@ -37,6 +47,7 @@ import com.palantir.atlasdb.cli.services.AtlasDbServicesFactory;
 import com.palantir.atlasdb.cli.services.DaggerTestAtlasDbServices;
 import com.palantir.atlasdb.cli.services.ServicesConfigModule;
 import com.palantir.atlasdb.cli.services.TestAtlasDbServices;
+import com.palantir.common.time.Clock;
 import com.palantir.lock.LockClient;
 import com.palantir.lock.LockDescriptor;
 import com.palantir.lock.LockMode;
@@ -116,46 +127,63 @@ public class TestTimestampCommand {
             TimestampService tss = services.getTimestampService();
             LockClient client = services.getTestLockClient();
 
+            // this is a really hacky way of forcing a punch to test the datetime output
+            Clock clock = GlobalClock.create(rls);
+            long prePunch = clock.getTimeMillis();
+            Uninterruptibles.sleepUninterruptibly(2, TimeUnit.MILLISECONDS);
+            long punchTs = tss.getFreshTimestamps(1000).getUpperBound();
+            PuncherStore puncherStore = KeyValueServicePuncherStore.create(services.getKeyValueService());
+            Puncher puncher = SimplePuncher.create(puncherStore, clock, Suppliers.ofInstance(AtlasDbConstants.DEFAULT_TRANSACTION_READ_TIMEOUT));
+            puncher.punch(punchTs);
+            Uninterruptibles.sleepUninterruptibly(2, TimeUnit.MILLISECONDS);
+            long postPunch = clock.getTimeMillis();
+
             long immutableTs = tss.getFreshTimestamp();
             LockRequest request = LockRequest.builder(ImmutableSortedMap.of(
                     lock, LockMode.WRITE))
                     .withLockedInVersionId(immutableTs).doNotBlock().build();
             LockRefreshToken token = rls.lock(client.getClientId(), request);
             long lastFreshTs = tss.getFreshTimestamps(1000).getUpperBound();
-            runAndVerify(runner, tss, isImmutable, isToFile, immutableTs, lastFreshTs);
+            runAndVerify(runner, tss, isImmutable, isToFile, immutableTs, lastFreshTs, prePunch, postPunch);
 
             rls.unlock(token);
             lastFreshTs = tss.getFreshTimestamps(1000).getUpperBound();
             // there are no locks so we now expect immutable to just be a fresh 
-            runAndVerify(runner, tss, false, isToFile, lastFreshTs, lastFreshTs);
+            runner.freshCommand();
+            runAndVerify(runner, tss, false, isToFile, lastFreshTs, lastFreshTs, prePunch, postPunch);
         }
     }
 
     private void runAndVerify(SingleBackendCliTestRunner runner, TimestampService tss,
-            boolean isImmutable, boolean isToFile, long immutableTs, long lastFreshTs) 
-                    throws IOException {
+            boolean isImmutable, boolean isToFile, long immutableTs, long lastFreshTs,
+            long prePunch, long postPunch) throws IOException {
         // prep
         if (isToFile && TIMESTAMP_FILE.exists()) {
             TIMESTAMP_FILE.delete();
         }
 
         // run the stuff
-        long timestamp;
-        String datetime;
         Scanner scanner = new Scanner(runner.run(true, false));
-        if (!isToFile) {
-            timestamp = Long.parseLong(scanner.findInLine("\\d+"));
-            datetime = scanner.findInLine("\\d+.*").trim();
-        } else {
+        // get timestamp from stdout
+        String line = scanner.findInLine(".*timestamp\\sis\\:\\s(\\d+.*)");
+        scanner.nextLine();
+        String[] parts = line.split(" ");
+        long timestamp = Long.parseLong(parts[parts.length-1]);
+        // get datetime from stdout
+        line = scanner.findInLine(".*Wall\\sclock\\sdatetime.*\\s(\\d+.*)");
+        parts = line.split(" ");
+        DateTime datetime = ISODateTimeFormat.dateTime().parseDateTime(parts[parts.length-1]);
+        // get timestamp from file
+        if (isToFile) {
             Preconditions.checkArgument(TIMESTAMP_FILE.exists(), "Timestamp file doesn't exist.");
             List<String> lines = Files.readAllLines(TIMESTAMP_FILE.toPath(), StandardCharsets.UTF_8);
             timestamp = Long.parseLong(lines.get(0));
-            datetime = lines.get(1).trim();
         }
         scanner.close();
 
         // verify correctness
-        ISODateTimeFormat.dateTimeNoMillis().parseDateTime(datetime);
+        Preconditions.checkArgument(datetime.isAfter(prePunch));
+        Preconditions.checkArgument(datetime.isBefore(postPunch));
         if (isImmutable) {
             Preconditions.checkArgument(timestamp == immutableTs);
             Preconditions.checkArgument(timestamp < lastFreshTs);
