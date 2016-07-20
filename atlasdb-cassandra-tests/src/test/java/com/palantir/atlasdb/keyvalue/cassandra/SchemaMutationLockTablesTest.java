2762bab5ff6365971b245b48e60ea37ad2052a75
Himangi Saraogi <hsaraogi@palantir.com>
2016-07-20 14:41:11 +0100

    Create SchemaMutationLockTables to separate lock table logic

---
+++ atlasdb-cassandra-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/SchemaMutationLockTablesTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2016 Palantir Technologies
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.keyvalue.cassandra;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfigManager;
+import com.palantir.atlasdb.keyvalue.api.KeyValueService;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+
+public class SchemaMutationLockTablesTest {
+    private KeyValueService keyValueService;
+    private ExecutorService executorService;
+    CassandraKeyValueServiceConfigManager configManager = CassandraKeyValueServiceConfigManager.createSimpleManager(CassandraTestSuite.CASSANDRA_KVS_CONFIG);
+    private CassandraClientPool clientPool = new CassandraClientPool(CassandraTestSuite.CASSANDRA_KVS_CONFIG);
+
+    @Before
+    public void setupKVS() {
+        keyValueService = getKeyValueService();
+        executorService = Executors.newFixedThreadPool(4);
+    }
+
+    @After
+    public void cleanUp() {
+        executorService.shutdown();
+    }
+
+    private KeyValueService getKeyValueService() {
+        return CassandraKeyValueService.create(
+                configManager, CassandraTestSuite.LEADER_CONFIG);
+    }
+
+    @Test
+    public void shouldReturnALockTableIfNoneExist() {
+        SchemaMutationLockTables lockTables = new SchemaMutationLockTables(clientPool, configManager);
+
+        assertThat(lockTables.getOnlyTable(), isA(TableReference.class));
+    }
+}
---
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/SchemaMutationLockTables.java
@@ -0,0 +1,57 @@
+package com.palantir.atlasdb.keyvalue.cassandra;
+
+import java.util.UUID;
+
+import org.apache.cassandra.thrift.Cassandra;
+import org.apache.cassandra.thrift.CfDef;
+import org.apache.thrift.TException;
+
+import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfig;
+import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfigManager;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.common.base.FunctionCheckedException;
+import com.palantir.common.base.Throwables;
+
+public class SchemaMutationLockTables {
+    private final CassandraClientPool clientPool;
+    private CassandraKeyValueServiceConfigManager configManager;
+
+    public SchemaMutationLockTables(CassandraClientPool clientPool, CassandraKeyValueServiceConfigManager configManager) {
+        this.clientPool = clientPool;
+        this.configManager = configManager;
+    }
+
+    private TableReference createLockTable() {
+        try {
+            return clientPool.run(createInternalLockTable);
+        } catch (Exception e) {
+            throw Throwables.throwUncheckedException(e);
+        }
+    }
+
+    private final FunctionCheckedException<Cassandra.Client, TableReference, Exception> createInternalLockTable = client -> {
+        String lockTableName = (HiddenTables.LOCK_TABLE_PREFIX + UUID.randomUUID()).replace('-','_');;
+        TableReference lockTable = TableReference.createWithEmptyNamespace(lockTableName);
+        createTableInternal(client, lockTable);
+        return lockTable;
+    };
+
+    private void createTableInternal(Cassandra.Client client, TableReference tableRef) throws TException {
+        CassandraKeyValueServiceConfig config = configManager.getConfig();
+        CfDef cf = CassandraConstants.getStandardCfDef(config.keyspace(), internalTableName(tableRef));
+        client.system_add_column_family(cf);
+        CassandraKeyValueServices.waitForSchemaVersions(client, tableRef.getQualifiedName(), config.schemaMutationTimeoutMillis());
+    }
+
+    public static String internalTableName(TableReference tableRef) {
+        String tableName = tableRef.getQualifiedName();
+        if (tableName.startsWith("_")) {
+            return tableName;
+        }
+        return tableName.replaceFirst("\\.", "__");
+    }
+
+    public TableReference getOnlyTable() {
+        return createLockTable();
+    }
+}
