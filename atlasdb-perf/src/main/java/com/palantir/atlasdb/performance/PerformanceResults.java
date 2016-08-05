bf72a7cacd4893887cc0e0c5fb7a91f76dd7227f
Misha Wakerman <mwakerman@palantir.com>
2016-08-05 14:32:20 -0700

    Benchmark results are exported in a new JSON format.

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/PerformanceResults.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/PerformanceResults.java
@@ -5,9 +5,16 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import org.immutables.value.Value;
 import org.openjdk.jmh.results.RunResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 public class PerformanceResults {
 
@@ -19,33 +26,37 @@ public class PerformanceResults {
 
     public void writeToFile(File file) throws IOException {
         try (BufferedWriter fout = new BufferedWriter(new FileWriter(file))) {
-            fout.write("date, suite, benchmark, backend, runs, units\n");
-            for (RunResult r : results) {
+            List<ImmutablePerformanceResult> newResults = results.stream().map(r -> {
                 String[] benchmarkParts = r.getParams().getBenchmark().split("\\.");
                 String benchmarkSuite = benchmarkParts[benchmarkParts.length - 2];
                 String benchmarkName = benchmarkParts[benchmarkParts.length - 1];
-                fout.write(
-                        String.format("%d, %s, %s, %s, [%s], %s\n",
-                                System.currentTimeMillis(),
-                                benchmarkSuite,
-                                benchmarkName,
-                                r.getParams().getParam(BenchmarkParam.BACKEND.getKey()),
-                                getResultsFromRun(r)
-                                        .stream()
-                                        .map(d -> Double.toString(d))
-                                        .collect(Collectors.joining(" ")),
-                                r.getParams().getTimeUnit())
-                );
-            }
+                return ImmutablePerformanceResult.builder()
+                        .date(System.currentTimeMillis())
+                        .suite(benchmarkSuite)
+                        .benchmark(benchmarkName)
+                        .backend(r.getParams().getParam((BenchmarkParam.BACKEND.getKey())))
+                        .samples(r.getPrimaryResult().getStatistics().getN())
+                        .std(r.getPrimaryResult().getStatistics().getStandardDeviation())
+                        .mean(r.getPrimaryResult().getStatistics().getMean())
+                        .units(r.getParams().getTimeUnit())
+                        .build();
+            }).collect(Collectors.toList());
+            new ObjectMapper().writeValue(fout, newResults);
         }
     }
 
-    private Collection<Double> getResultsFromRun(RunResult r) {
-        return r.getBenchmarkResults()
-                .stream()
-                .flatMap(rb -> rb.getIterationResults().stream())
-                .map(ir -> ir.getPrimaryResult().getScore())
-                .collect(Collectors.toList());
+    @JsonDeserialize(as = ImmutablePerformanceResult.class)
+    @JsonSerialize(as = ImmutablePerformanceResult.class)
+    @Value.Immutable
+    static abstract class PerformanceResult {
+        public abstract long date();
+        public abstract String suite();
+        public abstract String benchmark();
+        public abstract String backend();
+        public abstract long samples();
+        public abstract double std();
+        public abstract double mean();
+        public abstract TimeUnit units();
     }
 
 }
