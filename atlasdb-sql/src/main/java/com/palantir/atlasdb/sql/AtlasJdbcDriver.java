8263e81338571190c910c52467aa9e8cffd4db16
Bobby Ullman <bullman@palantir.com>
2016-08-08 13:19:26 -0700

    unimplemented boilerplate for jdbc driver

---
+++ atlasdb-sql/src/main/java/com/palantir/atlasdb/sql/AtlasJdbcDriver.java
@@ -0,0 +1,46 @@
+package com.palantir.atlasdb.sql;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class AtlasJdbcDriver implements Driver {
+    @Override
+    public Connection connect(String url, Properties info) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean acceptsURL(String url) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+        return new DriverPropertyInfo[0];
+    }
+
+    @Override
+    public int getMajorVersion() {
+        return 0;
+    }
+
+    @Override
+    public int getMinorVersion() {
+        return 0;
+    }
+
+    @Override
+    public boolean jdbcCompliant() {
+        return false;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+}
