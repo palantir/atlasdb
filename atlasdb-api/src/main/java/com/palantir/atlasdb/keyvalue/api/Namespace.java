ca8d1f086974da0b18020195577448dba10f146f
Ben Grabham <bgrabham@palantir.com>
2016-08-08 16:42:17 -0700

    Clean-up some code, fix an EMPTY_NAMESPACE bug when serialising/deserialising

--- atlasdb-api/src/main/java/com/palantir/atlasdb/keyvalue/api/Namespace.java
+++ atlasdb-api/src/main/java/com/palantir/atlasdb/keyvalue/api/Namespace.java
@@ -69,7 +69,7 @@ public final class Namespace {
 
     @JsonIgnore
     public boolean isEmptyNamespace() {
-        return this == EMPTY_NAMESPACE;
+        return this.equals(EMPTY_NAMESPACE);
     }
 
     public String getName() {
