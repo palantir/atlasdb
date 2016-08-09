576cef25fe4345201234afb6a315a4770624f467
Pavel Zheltov <pzheltov@palantir.com>
2016-08-09 14:37:52 -0700

    add dependency on atlasdb-dbkvs for atlasdb-cli
    * add a test of connection against an atlas on postgres
      (ignored because it's not an ete test; don't want to ensure postgres is running)

--- atlasdb-sql/src/test/java/com/palantir/atlasdb/sql/ConnectionTest.java
+++ atlasdb-sql/src/test/java/com/palantir/atlasdb/sql/ConnectionTest.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,18 +13,29 @@ public class ConnectionTest {
     private Logger log = LoggerFactory.getLogger(ConnectionTest.class);
 
     @Test
-    public void testConnection() throws ClassNotFoundException, SQLException {
+    public void testInMemoryConnection() throws ClassNotFoundException, SQLException {
+        connect("memoryTestConfig.yml");
+    }
+
+    // TODO: start postgres in a docker container
+    @Test @Ignore
+    public void testPostgresConnection() throws ClassNotFoundException, SQLException {
+        connect("postgresTestConfig.yml");
+    }
+
+    private void connect(String name) throws ClassNotFoundException, SQLException {
         Connection connection = null;
         try {
             Class.forName(AtlasJdbcDriver.class.getName());
-            final java.net.URL resource = ConnectionTest.class.getClassLoader().getResource("memoryTestConfig.yml");
+            final java.net.URL resource = ConnectionTest.class.getClassLoader().getResource(name);
+            assert resource != null;
             final String URL = "jdbc:atlas?configFile=" + resource.getFile();
             log.debug("url {}", URL);
             connection = DriverManager.getConnection(URL);
         } finally {
             if (connection != null) {
                 connection.close();
-           }
+            }
         }
     }
 }
