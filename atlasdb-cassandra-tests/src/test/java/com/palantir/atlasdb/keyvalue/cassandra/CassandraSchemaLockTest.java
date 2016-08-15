5416c798b45052ca0d11523e201286666abfd17c
Himangi Saraogi <hsaraogi@palantir.com>
2016-08-15 15:13:29 +0100

    CassandraSchemaLockTest: Automated test for detecting multiple table creation error

--- atlasdb-cassandra-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraSchemaLockTest.java
+++ atlasdb-cassandra-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraSchemaLockTest.java
@@ -15,11 +15,15 @@
  */
 package com.palantir.atlasdb.keyvalue.cassandra;
 
-import static com.google.common.base.Optional.absent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
@@ -32,7 +36,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.jayway.awaitility.Awaitility;
 import com.jayway.awaitility.Duration;
@@ -44,9 +47,7 @@ import com.palantir.atlasdb.config.ImmutableLeaderConfig;
 import com.palantir.atlasdb.config.LeaderConfig;
 import com.palantir.atlasdb.encoding.PtBytes;
 import com.palantir.atlasdb.keyvalue.api.Cell;
-import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
-import com.palantir.atlasdb.keyvalue.api.Value;
 import com.palantir.docker.compose.DockerComposition;
 import com.palantir.docker.compose.connection.DockerPort;
 import com.palantir.docker.compose.connection.waiting.HealthCheck;
@@ -111,24 +112,13 @@ public class CassandraSchemaLockTest {
                 .until(canCreateKeyValueService());
     }
 
-    /*
-    To correctly detect duplicate tables:
-     1. run this test
-     2. after table ns.table1 has been created, check your Cassandra logs for mismatch exceptions: "docker logs seed | grep mismatch"
-     3. restart your docker machine: "docker restart <container_id>
-     4. check the list of tables in the data directory: "docker exec -it seed  ls /var/lib/cassandra/data/atlasdb && echo "\n" && docker exec -it cassandra1 ls /var/lib/cassandra/data/atlasdb && echo "\n" && docker exec -it cassandra2  ls /var/lib/cassandra/data/atlasdb"
-     If you see exceptions in step 2, then step 4 should yield multiple tables of the form "ns__table1*" on at least one node.
-     */
     @Test
-    public void shouldCreateTablesWithMultipleCassandraNodes() throws Exception {
-
+    public void shouldCreateTablesConsistentlyWithMultipleCassandraNodes() throws Exception {
         TableReference table1 = TableReference.createFromFullyQualifiedName("ns.table1");
 
         CyclicBarrier barrier = new CyclicBarrier(32);
-
-
         try {
-            for(int i=0; i < 100; i++) {
+            for(int i=0; i < 32; i++) {
                 async(() -> {
                     CassandraKeyValueService keyValueService = CassandraKeyValueService.create(CONFIG_MANAGER, Optional.absent());
                     barrier.await();
@@ -139,18 +129,28 @@ public class CassandraSchemaLockTest {
         } catch (Exception e) {
             throw e;
         }
-
-        KeyValueService keyValueService = CassandraKeyValueService.create(CONFIG_MANAGER, absent());
-
         executorService.shutdown();
         executorService.awaitTermination(5L, TimeUnit.MINUTES);
 
-        keyValueService.put(table1, ImmutableMap.of(TABLE_CELL, TABLE_VALUE), 127L);
-        Map<Cell, Value> cellValueMap = keyValueService.get(table1, ImmutableMap.of(Cell.create(PtBytes.toBytes("row0"), PtBytes.toBytes("col0")), 130L));
-
-        System.out.println("We got from table1 : " + cellValueMap.size());
+        checkErrorInCassandraLogs();
     }
 
+    private void checkErrorInCassandraLogs() {
+        File logsDirectory = new File("container-logs-multinode");
+        boolean anyFileContainsError = Arrays.stream(logsDirectory.listFiles()).anyMatch(file -> {
+            try {
+                return containsErrorLine(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        });
+        assertThat(anyFileContainsError, is(false));
+    }
+
+    private boolean containsErrorLine(File file) throws IOException {
+        return Files.lines(Paths.get(file.getAbsolutePath())).anyMatch(line -> line.contains("Column family ID mismatch"));
+    }
 
     private static Callable<Boolean> canCreateKeyValueService() {
         return () -> {
