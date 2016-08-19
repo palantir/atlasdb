ec9c96b34548339523cda69273b22706e8d7993d
George Brova <gbrova@palantir.com>
2016-08-19 20:03:14 +0100

    Add simple test for multiple columns

--- atlasdb-cassandra-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/AllCellsPerRowPagerTest.java
+++ atlasdb-cassandra-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/AllCellsPerRowPagerTest.java
@@ -18,23 +18,18 @@
 package com.palantir.atlasdb.keyvalue.cassandra;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.core.StringEndsWith.endsWith;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -42,7 +37,6 @@ import org.apache.cassandra.thrift.Column;
 import org.apache.cassandra.thrift.ColumnOrSuperColumn;
 import org.apache.cassandra.thrift.CqlResult;
 import org.apache.cassandra.thrift.CqlRow;
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
@@ -59,10 +53,11 @@ public class AllCellsPerRowPagerTest {
 
     private static final TableReference DEFAULT_TABLE = TableReference.fromString("tr");
     private static final String DEFAULT_COLUMN_NAME = "col1";
+    private static final String OTHER_COLUMN_NAME = "otherColumn";
     private static final String PREVIOUS_COLUMN_NAME = "don't care";
     private static final long PREVIOUS_TIMESTAMP = 23;
-    private static final List<ColumnOrSuperColumn> PREVIOUS_PAGE = ImmutableList.of(makeColumnOrSuperColumn(PREVIOUS_COLUMN_NAME, PREVIOUS_TIMESTAMP));
 
+    private static final List<ColumnOrSuperColumn> PREVIOUS_PAGE = ImmutableList.of(makeColumnOrSuperColumn(PREVIOUS_COLUMN_NAME, PREVIOUS_TIMESTAMP));
     private AllCellsPerRowPager pager = new AllCellsPerRowPager(
             executor,
             rowKey,
@@ -132,6 +127,21 @@ public class AllCellsPerRowPagerTest {
                 pageSize);
     }
 
+    @Test
+    public void getNextPageShouldSpanMultipleColumns() {
+        CqlRow row1 = makeCqlRow(DEFAULT_COLUMN_NAME, 10L);
+        CqlRow row2 = makeCqlRow(OTHER_COLUMN_NAME, 20L);
+
+        allQueriesWithColumnAndTimestampReturn(ImmutableList.of(row1));
+        allQueriesWithColumnReturn(ImmutableList.of(row2));
+
+        List<ColumnOrSuperColumn> nextPage = pager.getNextPage(PREVIOUS_PAGE);
+        nextPage.get(0);
+
+        assertColumnOrSuperColumnHasCorrectNameAndTimestamp(nextPage.get(0), DEFAULT_COLUMN_NAME, 10L);
+        assertColumnOrSuperColumnHasCorrectNameAndTimestamp(nextPage.get(1), OTHER_COLUMN_NAME, 20L);
+    }
+
     private void verifySingletonListIsReturnedCorrectly(Supplier<List<ColumnOrSuperColumn>> method, long expectedTimestamp) {
         List<ColumnOrSuperColumn> page = method.get();
 
@@ -168,6 +178,24 @@ public class AllCellsPerRowPagerTest {
         when(executor.getColAndTimestampForNextColumn(any(TableReference.class), anyString(), anyString(), anyInt())).thenReturn(noResults);
     }
 
+    private void allQueriesSimpleReturn(List<CqlRow> rows) {
+        CqlResult cqlResult = mock(CqlResult.class);
+        when(cqlResult.getRows()).thenReturn(rows);
+        when(executor.getColAndTimestamp(any(TableReference.class), anyString(), anyInt())).thenReturn(cqlResult);
+    }
+
+    private void allQueriesWithColumnAndTimestampReturn(ImmutableList<CqlRow> rows) {
+        CqlResult cqlResult = mock(CqlResult.class);
+        when(cqlResult.getRows()).thenReturn(rows);
+        when(executor.getColAndTimestampForColumnAndTimestamp(any(TableReference.class), anyString(), anyString(), anyLong(), anyInt())).thenReturn(cqlResult);
+    }
+
+    private void allQueriesWithColumnReturn(ImmutableList<CqlRow> rows) {
+        CqlResult result2 = mock(CqlResult.class);
+        when(result2.getRows()).thenReturn(rows);
+        when(executor.getColAndTimestampForNextColumn(any(TableReference.class), anyString(), anyString(), anyInt())).thenReturn(result2);
+    }
+
     private void assertColumnOrSuperColumnHasCorrectNameAndTimestamp(ColumnOrSuperColumn columnOrSuperColumn, String expectedName, long expectedTs) {
         Pair<byte[], Long> nameAndTimestamp = CassandraKeyValueServices.decomposeName(columnOrSuperColumn.getColumn());
         String colName = PtBytes.toString(nameAndTimestamp.getLhSide());
@@ -175,7 +203,6 @@ public class AllCellsPerRowPagerTest {
 
         long timestamp = nameAndTimestamp.getRhSide();
         assertThat(timestamp, equalTo(expectedTs));
-
     }
 
     private ByteBuffer toByteBuffer(String str) {
