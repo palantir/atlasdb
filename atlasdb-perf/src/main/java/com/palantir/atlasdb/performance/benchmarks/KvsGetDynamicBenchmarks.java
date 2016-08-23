22532f486675bba826ea7cdd2f2589150fabf54b
Bobby Ullman <bullman@palantir.com>
2016-08-22 18:08:29 -0700

    fix checkstyle rule failures caused by merge

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/KvsGetDynamicBenchmarks.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/KvsGetDynamicBenchmarks.java
@@ -1,7 +1,23 @@
+/**
+ * Copyright 2016 Palantir Technologies
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.palantir.atlasdb.performance.benchmarks;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -49,7 +65,7 @@ public class KvsGetDynamicBenchmarks {
     private static final long DUMMY_TIMESTAMP = 1L;
     private static final long READ_TIMESTAMP = 2L;
 
-    private final int NUM_COLS = 50000;
+    private static final int NUM_COLS = 50000;
 
     private KeyValueServiceConnector connector;
     private KeyValueService kvs;
@@ -60,18 +76,18 @@ public class KvsGetDynamicBenchmarks {
     private Map<Cell, Long> firstCell2ReadTimestamp;
 
     @Setup
-    public void setup(KeyValueServiceConnector connector) throws UnsupportedEncodingException {
-        this.connector = connector;
-        kvs = connector.connect();
+    public void setup(KeyValueServiceConnector conn) throws UnsupportedEncodingException {
+        this.connector = conn;
+        kvs = conn.connect();
         tableRef1 = KvsBenchmarks.createTableWithDynamicColumns(kvs, TABLE_NAME_1, ROW_COMPONENT, COLUMN_COMPONENT);
         byte[] rowBytes = ROW_COMPONENT.getBytes("UTF-8");
-        Map<Cell,byte[]> values = Maps.newHashMap();
+        Map<Cell, byte[]> values = Maps.newHashMap();
         allCells2ReadTimestamp = Maps.newHashMap();
-        firstCell2ReadTimestamp = ImmutableMap.of(Cell.create(rowBytes, ("col_0").getBytes("UTF-8")), READ_TIMESTAMP);
+        firstCell2ReadTimestamp = ImmutableMap.of(Cell.create(rowBytes, "col_0".getBytes("UTF-8")), READ_TIMESTAMP);
         for (int i = 0; i < NUM_COLS; i++) {
-            Cell c = Cell.create(rowBytes, ("col_"+i).getBytes("UTF-8"));
-            values.put(c, Ints.toByteArray(i));
-            allCells2ReadTimestamp.put(c, READ_TIMESTAMP);
+            Cell cell = Cell.create(rowBytes, ("col_" + i).getBytes("UTF-8"));
+            values.put(cell, Ints.toByteArray(i));
+            allCells2ReadTimestamp.put(cell, READ_TIMESTAMP);
         }
         kvs.put(tableRef1, values, DUMMY_TIMESTAMP);
     }
@@ -86,14 +102,20 @@ public class KvsGetDynamicBenchmarks {
     @Benchmark
     public Map<Cell, Value> getAllColumnsExplicitly() {
         Map<Cell, Value> result = kvs.get(tableRef1, allCells2ReadTimestamp);
-        KvsBenchmarks.validate(result.size() == NUM_COLS, "Should be %s columns, but were: %s", NUM_COLS, result.size());
+        KvsBenchmarks.validate(result.size() == NUM_COLS,
+                "Should be %s columns, but were: %s", NUM_COLS, result.size());
         return result;
     }
 
     @Benchmark
     public Map<Cell, Value> getAllColumnsImplicitly() throws UnsupportedEncodingException {
-        Map<Cell, Value> result = kvs.getRows(tableRef1, Collections.singleton(ROW_COMPONENT.getBytes("UTF-8")), ColumnSelection.all(), READ_TIMESTAMP);
-        KvsBenchmarks.validate(result.size() == NUM_COLS, "Should be %s columns, but were: %s", NUM_COLS, result.size());
+        Map<Cell, Value> result = kvs.getRows(
+                tableRef1,
+                Collections.singleton(ROW_COMPONENT.getBytes("UTF-8")),
+                ColumnSelection.all(),
+                READ_TIMESTAMP);
+        KvsBenchmarks.validate(result.size() == NUM_COLS,
+                "Should be %s columns, but were: %s", NUM_COLS, result.size());
         return result;
     }
 
