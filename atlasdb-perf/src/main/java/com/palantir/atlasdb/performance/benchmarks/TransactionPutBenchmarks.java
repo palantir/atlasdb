778fa6f387dc08bfb738361c511d32d1f5d31abf
Bobby Ullman <bullman@palantir.com>
2016-08-24 14:12:01 -0700

    add transaction put benchmarks

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/TransactionPutBenchmarks.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/TransactionPutBenchmarks.java
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.palantir.atlasdb.performance.benchmarks;
@@ -29,9 +28,7 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
-import com.google.common.collect.Maps;
 import com.palantir.atlasdb.keyvalue.api.Cell;
-import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.performance.benchmarks.table.EmptyTables;
 
 /**
@@ -44,32 +41,26 @@ import com.palantir.atlasdb.performance.benchmarks.table.EmptyTables;
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @Warmup(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 1, time = 30, timeUnit = TimeUnit.SECONDS)
-public class KvsPutBenchmarks {
+public class TransactionPutBenchmarks {
 
-    private static final long DUMMY_TIMESTAMP = 1L;
     private static final int BATCH_SIZE = 250;
 
     @Benchmark
     public Map<Cell, byte[]> singleRandomPut(EmptyTables tables) {
-        Map<Cell, byte[]> batch = tables.generateBatchToInsert(1);
-        tables.getKvs().put(EmptyTables.TABLE_REF_1, batch, DUMMY_TIMESTAMP);
-        return batch;
+        return tables.getTransactionManager().runTaskThrowOnConflict(txn -> {
+            Map<Cell, byte[]> batch = tables.generateBatchToInsert(1);
+            txn.put(EmptyTables.TABLE_REF_1, batch);
+            return batch;
+        });
     }
 
     @Benchmark
     public Map<Cell, byte[]> batchRandomPut(EmptyTables tables) {
-        Map<Cell, byte[]> batch = tables.generateBatchToInsert(BATCH_SIZE);
-        tables.getKvs().put(EmptyTables.TABLE_REF_1, batch, DUMMY_TIMESTAMP);
-        return batch;
-    }
-
-    @Benchmark
-    public Map<TableReference, Map<Cell, byte[]>> batchRandomMultiPut(EmptyTables tables) {
-        Map<TableReference, Map<Cell, byte[]>> multiPutMap = Maps.newHashMap();
-        multiPutMap.put(EmptyTables.TABLE_REF_1, tables.generateBatchToInsert(BATCH_SIZE));
-        multiPutMap.put(EmptyTables.TABLE_REF_2, tables.generateBatchToInsert(BATCH_SIZE));
-        tables.getKvs().multiPut(multiPutMap, DUMMY_TIMESTAMP);
-        return multiPutMap;
+        return tables.getTransactionManager().runTaskThrowOnConflict(txn -> {
+            Map<Cell, byte[]> batch = tables.generateBatchToInsert(BATCH_SIZE);
+            txn.put(EmptyTables.TABLE_REF_1, batch);
+            return batch;
+        });
     }
 
 }
