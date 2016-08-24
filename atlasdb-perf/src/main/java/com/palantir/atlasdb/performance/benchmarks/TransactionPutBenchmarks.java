db2f049413226d8f9f5a13cce8c31ae2b2ad88b5
Bobby Ullman <bullman@palantir.com>
2016-08-24 14:12:01 -0700

    add transaction put benchmarks

---
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/TransactionPutBenchmarks.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright 2016 Palantir Technologies
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.performance.benchmarks;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.palantir.atlasdb.keyvalue.api.Cell;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.performance.backend.AtlasDbServicesConnector;
+import com.palantir.atlasdb.services.AtlasDbServices;
+
+/**
+ * Performance benchmarks for KVS put operations.
+ *
+ * @author mwakerman
+ */
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.SampleTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 1, time = 30, timeUnit = TimeUnit.SECONDS)
+public class TransactionPutBenchmarks {
+
+    private static final String TABLE_NAME_1 = "performance.table1";
+    private static final String TABLE_NAME_2 = "performance.table2";
+    private static final String ROW_COMPONENT = "key";
+    private static final String COLUMN_NAME = "value";
+    private static final byte [] COLUMN_NAME_IN_BYTES = COLUMN_NAME.getBytes(StandardCharsets.UTF_8);
+
+    private static final int VALUE_BYTE_ARRAY_SIZE = 100;
+    private static final int KEY_BYTE_ARRAY_SIZE = 32;
+    private static final long VALUE_SEED = 279L;
+    private static final int BATCH_SIZE = 250;
+
+    private Random random = new Random(VALUE_SEED);
+
+    private AtlasDbServicesConnector connector;
+    private AtlasDbServices services;
+
+    private TableReference tableRef1;
+    private TableReference tableRef2;
+
+    @Setup
+    public void setup(AtlasDbServicesConnector conn) {
+        this.connector = conn;
+        this.services = conn.connect();
+        this.tableRef1 = Benchmarks.createTable(services.getKeyValueService(), TABLE_NAME_1, ROW_COMPONENT, COLUMN_NAME);
+        this.tableRef2 = Benchmarks.createTable(services.getKeyValueService(), TABLE_NAME_2, ROW_COMPONENT, COLUMN_NAME);
+    }
+
+    @TearDown
+    public void cleanup() throws Exception {
+        this.services.getKeyValueService().dropTables(Sets.newHashSet(tableRef1, tableRef2));
+        this.connector.close();
+        this.tableRef1 = null;
+        this.tableRef2 = null;
+    }
+
+    @Benchmark
+    public byte[] singleRandomPut() {
+        return services.getTransactionManager().runTaskThrowOnConflict(txn -> {
+            byte[] value = generateValue();
+            txn.put(this.tableRef1, ImmutableMap.of(Cell.create(generateKey(), COLUMN_NAME_IN_BYTES), value));
+            return value;
+        });
+    }
+
+    @Benchmark
+    public Map<Cell, byte[]> batchRandomPut() {
+        return services.getTransactionManager().runTaskThrowOnConflict(txn -> {
+            Map<Cell, byte[]> batch = generateBatch(BATCH_SIZE);
+            txn.put(this.tableRef1, batch);
+            return batch;
+        });
+    }
+
+    private byte[] generateValue() {
+        byte[] value = new byte[VALUE_BYTE_ARRAY_SIZE];
+        random.nextBytes(value);
+        return value;
+    }
+
+    private byte[] generateKey() {
+        byte[] key = new byte[KEY_BYTE_ARRAY_SIZE];
+        random.nextBytes(key);
+        return key;
+    }
+
+    private Map<Cell, byte[]> generateBatch(int size) {
+        Map<Cell, byte[]> map = Maps.newHashMapWithExpectedSize(size);
+        for (int j = 0; j < size; j++) {
+            byte[] key = generateKey();
+            byte[] value = generateValue();
+            map.put(Cell.create(key, COLUMN_NAME_IN_BYTES), value);
+        }
+        return map;
+    }
+
+}
