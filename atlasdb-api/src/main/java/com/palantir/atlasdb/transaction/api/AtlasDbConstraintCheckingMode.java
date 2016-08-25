4c65441debe58a119104cdcbf7263734d8bdb9d8
sstecko <sstecko@palantir.com>
2016-08-25 14:25:11 -0700

    Bugfix/fixnewcheckstyle (#848)
    * Fix checkstyle and findbugs.
    
    * fix checkstyle: short param names

--- atlasdb-api/src/main/java/com/palantir/atlasdb/transaction/api/AtlasDbConstraintCheckingMode.java
+++ atlasdb-api/src/main/java/com/palantir/atlasdb/transaction/api/AtlasDbConstraintCheckingMode.java
@@ -27,7 +27,6 @@ public enum AtlasDbConstraintCheckingMode {
         NO
     }
 
-<<<<<<< 7033b8fc57203bf309772ac48101c6126fb91d56
     private final ShouldThrow shouldThrow;
 
     AtlasDbConstraintCheckingMode(ShouldThrow shouldThrow) {
@@ -36,7 +35,6 @@ public enum AtlasDbConstraintCheckingMode {
 
     public boolean shouldThrowException() {
         return shouldThrow == ShouldThrow.YES;
-=======
     private final ShouldThrow shouldThrowException;
 
     AtlasDbConstraintCheckingMode(ShouldThrow shouldThrowException) {
@@ -45,7 +43,6 @@ public enum AtlasDbConstraintCheckingMode {
 
     public boolean shouldThrowException() {
         return shouldThrowException == ShouldThrow.YES;
->>>>>>> merge develop into perf cli branch (#820)
     }
 
 }
