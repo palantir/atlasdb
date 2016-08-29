13a7a41fae4c943a3311c4973115e6689e999b1f
Bobby Ullman <robert.j.ullman@gmail.com>
2016-08-29 14:14:46 -0700

    refactor atlasdb services connector (#863)
    * refactor atlasdb services connector
    
    also adds awaitility call required to let C* docker setup finish in some
    enviornments
    
    * fix checkstyle

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/backend/KeyValueServiceType.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/backend/KeyValueServiceType.java
@@ -18,10 +18,15 @@
 package com.palantir.atlasdb.performance.backend;
 
 import java.net.InetSocketAddress;
-import java.util.function.Function;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableSet;
+import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfig;
+import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfigManager;
 import com.palantir.atlasdb.cassandra.ImmutableCassandraCredentialsConfig;
 import com.palantir.atlasdb.cassandra.ImmutableCassandraKeyValueServiceConfig;
+import com.palantir.atlasdb.config.ImmutableLeaderConfig;
+import com.palantir.atlasdb.keyvalue.cassandra.CassandraKeyValueService;
 import com.palantir.atlasdb.keyvalue.dbkvs.ImmutableDbKeyValueServiceConfig;
 import com.palantir.atlasdb.keyvalue.dbkvs.ImmutablePostgresDdlConfig;
 import com.palantir.atlasdb.spi.KeyValueServiceConfig;
@@ -29,52 +34,15 @@ import com.palantir.nexus.db.pool.config.ImmutableMaskedValue;
 import com.palantir.nexus.db.pool.config.ImmutablePostgresConnectionConfig;
 
 public enum KeyValueServiceType {
-    POSTGRES(addr -> ImmutableDbKeyValueServiceConfig.builder()
-            .ddl(ImmutablePostgresDdlConfig.builder().build())
-            .connection(
-                    ImmutablePostgresConnectionConfig.builder()
-                            .host(addr.getHostString())
-                            .port(5432)
-                            .dbName("atlas")
-                            .dbLogin("palantir")
-                            .dbPassword(ImmutableMaskedValue.of("palantir"))
-                            .build()
-            ).build(),
-            5432,
-            "postgres-docker-compose.yml"),
-    CASSANDRA(addr -> ImmutableCassandraKeyValueServiceConfig.builder()
-            .addServers(addr)
-            .poolSize(20)
-            .keyspace("atlasdb")
-            .credentials(ImmutableCassandraCredentialsConfig.builder()
-                    .username("cassandra")
-                    .password("cassandra")
-                    .build())
-            .ssl(false)
-            .replicationFactor(1)
-            .mutationBatchCount(10000)
-            .mutationBatchSizeBytes(10000000)
-            .fetchBatchCount(1000)
-            .safetyDisabled(false)
-            .autoRefreshNodes(false)
-            .build(),
-            9160,
-            "cassandra-docker-compose.yml");
+    POSTGRES(5432, "postgres-docker-compose.yml"),
+    CASSANDRA(9160, "cassandra-docker-compose.yml");
 
-    private final Function<InetSocketAddress, KeyValueServiceConfig> addressToKvsConfigFunction;
     private final int kvsPort;
     private final String dockerComposeFileName;
 
-    KeyValueServiceType(Function<InetSocketAddress, KeyValueServiceConfig> addressToKvsConfigFunction,
-            int kvsPort,
-            String dockerComposeFileName) {
-        this.addressToKvsConfigFunction = addressToKvsConfigFunction;
+    KeyValueServiceType(int kvsPort, String dockerComposeFileName) {
         this.kvsPort = kvsPort;
         this.dockerComposeFileName = dockerComposeFileName;
-    }
-
-    public KeyValueServiceConfig getKeyValueServiceConfig(InetSocketAddress docker) {
-        return addressToKvsConfigFunction.apply(docker);
     }
 
     public String getDockerComposeResourceFileName() {
@@ -84,4 +52,65 @@ public enum KeyValueServiceType {
     public int getKeyValueServicePort() {
         return kvsPort;
     }
+
+    public static KeyValueServiceConfig getKeyValueServiceConfig(KeyValueServiceType type, InetSocketAddress addr) {
+        switch (type) {
+            case POSTGRES:
+                return ImmutableDbKeyValueServiceConfig.builder()
+                        .ddl(ImmutablePostgresDdlConfig.builder().build())
+                        .connection(
+                                ImmutablePostgresConnectionConfig.builder()
+                                        .host(addr.getHostString())
+                                        .port(5432)
+                                        .dbName("atlas")
+                                        .dbLogin("palantir")
+                                        .dbPassword(ImmutableMaskedValue.of("palantir"))
+                                        .build()
+                        ).build();
+            case CASSANDRA:
+                return ImmutableCassandraKeyValueServiceConfig.builder()
+                        .addServers(addr)
+                        .poolSize(20)
+                        .keyspace("atlasdb")
+                        .credentials(ImmutableCassandraCredentialsConfig.builder()
+                                .username("cassandra")
+                                .password("cassandra")
+                                .build())
+                        .ssl(false)
+                        .replicationFactor(1)
+                        .mutationBatchCount(10000)
+                        .mutationBatchSizeBytes(10000000)
+                        .fetchBatchCount(1000)
+                        .safetyDisabled(false)
+                        .autoRefreshNodes(false)
+                        .build();
+            default:
+                throw new UnsupportedOperationException("Unable to get the KVS config for " + type);
+        }
+    }
+
+    public static boolean canConnect(KeyValueServiceType type, InetSocketAddress addr) {
+        switch (type) {
+            case POSTGRES:
+                return true;
+            case CASSANDRA:
+                try {
+                    CassandraKeyValueService.create(
+                            CassandraKeyValueServiceConfigManager.createSimpleManager(
+                                    (CassandraKeyValueServiceConfig) getKeyValueServiceConfig(type, addr)),
+                            Optional.of(ImmutableLeaderConfig
+                                    .builder()
+                                    .quorumSize(1)
+                                    .localServer(addr.getHostString())
+                                    .leaders(ImmutableSet.of(addr.getHostString()))
+                                    .build()));
+                    return true;
+                } catch (Exception e) {
+                    return false;
+                }
+            default:
+                throw new UnsupportedOperationException("Trying to check connection for unknown KVS " + type);
+        }
+    }
+
 }
