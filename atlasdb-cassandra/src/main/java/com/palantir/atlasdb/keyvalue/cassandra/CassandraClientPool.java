8b6d2b8a29bb7187d5d330fdeac2fe6204713948
George Brova <gbrova@palantir.com>
2016-08-30 21:52:56 +0100

    Remove extraneous comment

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraClientPool.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraClientPool.java
@@ -427,7 +427,6 @@ public class CassandraClientPool {
         while (true) {
             CassandraClientPoolingContainer hostPool = currentPools.get(specifiedHost);
 
-            // TODO: why did it throw
             if (blacklistedHosts.containsKey(specifiedHost) || hostPool == null || shouldRetryOnDifferentHost) {
                 log.warn("Randomly redirected a query intended for host {}.", specifiedHost);
                 hostPool = getRandomGoodHost();
