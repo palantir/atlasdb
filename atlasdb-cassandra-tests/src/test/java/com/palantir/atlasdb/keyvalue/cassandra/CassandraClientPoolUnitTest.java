bc363c43d96131c42c0c4e8cad24110df4a6795f
George Brova <gbrova@palantir.com>
2016-09-02 13:43:27 +0100

    Clean up ClientPoolUnitTest

--- atlasdb-cassandra-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraClientPoolUnitTest.java
+++ atlasdb-cassandra-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraClientPoolUnitTest.java
@@ -34,52 +34,50 @@ public class CassandraClientPoolUnitTest {
     public static final int POOL_REFRESH_INTERVAL_SECONDS = 10;
     public static final int DEFAULT_PORT = 5000;
     public static final int OTHER_PORT = 6000;
-    public static final String HOST_1 = "1.0.0.0";
-    public static final String HOST_2 = "2.0.0.0";
-    public static final String HOST_3 = "3.0.0.0";
+    public static final String HOSTNAME_1 = "1.0.0.0";
+    public static final String HOSTNAME_2 = "2.0.0.0";
+    public static final String HOSTNAME_3 = "3.0.0.0";
 
     @Test
     public void shouldReturnAddressForSingleHostInPool() throws UnknownHostException {
-        InetSocketAddress host = new InetSocketAddress(HOST_1, DEFAULT_PORT);
+        InetSocketAddress host = new InetSocketAddress(HOSTNAME_1, DEFAULT_PORT);
         CassandraClientPool cassandraClientPool = clientPoolWithServersInCurrentPool(ImmutableSet.of(host));
 
-        InetSocketAddress resolvedHost = cassandraClientPool.getAddressForHost(host.getHostName());
+        InetSocketAddress resolvedHost = cassandraClientPool.getAddressForHost(HOSTNAME_1);
 
         assertThat(resolvedHost, equalTo(host));
     }
 
     @Test
     public void shouldReturnAddressForSingleServer() throws UnknownHostException {
-        InetSocketAddress host = new InetSocketAddress(HOST_1, DEFAULT_PORT);
+        InetSocketAddress host = new InetSocketAddress(HOSTNAME_1, DEFAULT_PORT);
         CassandraClientPool cassandraClientPool = clientPoolWithServers(ImmutableSet.of(host));
 
-        InetSocketAddress resolvedHost = cassandraClientPool.getAddressForHost(host.getHostName());
+        InetSocketAddress resolvedHost = cassandraClientPool.getAddressForHost(HOSTNAME_1);
 
         assertThat(resolvedHost, equalTo(host));
     }
 
     @Test
     public void shouldUseCommonPortIfThereIsOnlyOneAndNoAddressMatches() throws UnknownHostException {
-        InetSocketAddress host1 = new InetSocketAddress(HOST_1, DEFAULT_PORT);
-        InetSocketAddress host2 = new InetSocketAddress(HOST_2, DEFAULT_PORT);
+        InetSocketAddress host1 = new InetSocketAddress(HOSTNAME_1, DEFAULT_PORT);
+        InetSocketAddress host2 = new InetSocketAddress(HOSTNAME_2, DEFAULT_PORT);
         CassandraClientPool cassandraClientPool = clientPoolWithServers(ImmutableSet.of(host1, host2));
 
-        InetSocketAddress resolvedHost = cassandraClientPool.getAddressForHost(HOST_3);
+        InetSocketAddress resolvedHost = cassandraClientPool.getAddressForHost(HOSTNAME_3);
 
-        assertThat(resolvedHost, equalTo(new InetSocketAddress(HOST_3, DEFAULT_PORT)));
+        assertThat(resolvedHost, equalTo(new InetSocketAddress(HOSTNAME_3, DEFAULT_PORT)));
     }
 
 
     @Test(expected = UnknownHostException.class)
     public void shouldThrowIfPortsAreNotTheSameAddressDoesNotMatch() throws UnknownHostException {
-        InetSocketAddress host1 = new InetSocketAddress(HOST_1, DEFAULT_PORT);
-        InetSocketAddress host2 = new InetSocketAddress(HOST_2, OTHER_PORT);
+        InetSocketAddress host1 = new InetSocketAddress(HOSTNAME_1, DEFAULT_PORT);
+        InetSocketAddress host2 = new InetSocketAddress(HOSTNAME_2, OTHER_PORT);
 
         CassandraClientPool cassandraClientPool = clientPoolWithServers(ImmutableSet.of(host1, host2));
 
-        InetSocketAddress resolvedHost = cassandraClientPool.getAddressForHost(HOST_3);
-
-        assertThat(resolvedHost, equalTo(host1));
+        cassandraClientPool.getAddressForHost(HOSTNAME_3);
     }
 
     private CassandraClientPool clientPoolWithServers(ImmutableSet<InetSocketAddress> servers) {
