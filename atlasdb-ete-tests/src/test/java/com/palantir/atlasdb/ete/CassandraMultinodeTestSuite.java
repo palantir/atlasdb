844be92bf3449b6ec4068e78848dd51b98b87557
George Brova <gbrova@palantir.com>
2016-09-08 21:34:42 +0100

    Pass available clients into EteSetup
    We could this information already exists in the docker-compose.yml
    file, but parsing that is more work.
    We were already hard-coding these before, in the (overridden)
    createTimestampClient method).

--- atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/CassandraMultinodeTestSuite.java
+++ atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/CassandraMultinodeTestSuite.java
@@ -15,10 +15,14 @@
  */
 package com.palantir.atlasdb.ete;
 
+import java.util.List;
+
 import org.junit.ClassRule;
 import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+import com.google.common.collect.ImmutableList;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -26,6 +30,9 @@ import org.junit.runners.Suite;
         DropwizardEteTest.class
 })
 public class CassandraMultinodeTestSuite extends EteSetup {
+    private static final List<String> CLIENTS = ImmutableList.of("ete1", "ete2", "ete3");
+
     @ClassRule
-    public static final RuleChain COMPOSITION_SETUP = EteSetup.setupComposition("cassandra-ha", "docker-compose.cassandra.yml");
+    public static final RuleChain COMPOSITION_SETUP =
+            EteSetup.setupComposition("cassandra-ha", "docker-compose.cassandra.yml", CLIENTS);
 }
