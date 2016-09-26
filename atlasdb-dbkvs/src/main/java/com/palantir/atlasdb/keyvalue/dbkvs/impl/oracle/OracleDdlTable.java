48448c6fb08f420ccd894b7086d4e315bdd101c8
Himangi Saraogi <hsaraogi@palantir.com>
2016-09-26 19:48:36 +0100

    shorten table name to meet Oracle limit

--- atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/oracle/OracleDdlTable.java
+++ atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/oracle/OracleDdlTable.java
@@ -81,10 +81,6 @@ public class OracleDdlTable implements DbDdlTable {
                     "ORA-00955");
         }
 
-        System.out.println("INSERT INTO " + getPrefixedMetadataTableName() + " (table_name, table_size) VALUES (?, ?)"
-                + tableRef.getQualifiedName()
-                + (needsOverflow ? TableSize.OVERFLOW.getId() : TableSize.RAW.getId()));
-
         conns.get().insertOneUnregisteredQuery(
                 "INSERT INTO " + getPrefixedMetadataTableName() + " (table_name, table_size) VALUES (?, ?)",
                 tableRef.getQualifiedName(),
