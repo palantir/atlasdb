7ed48f510a6bc3094d092e1b123fc233cc7ec4f4
Glenn Sheasby <gsheasby@palantir.com>
2016-09-30 15:22:07 +0100

    Only run the test we're debugging

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -8,31 +8,32 @@ function checkDocsBuild {
 }
 
 # Container 1
-CONTAINER_1=(':atlasdb-cassandra-integration-tests:check')
+#CONTAINER_1=(':atlasdb-cassandra-integration-tests:check')
 
-# Container 2
-CONTAINER_2=(':atlasdb-ete-tests:check')
+# Container 0
+CONTAINER_0=(':atlasdb-ete-tests:test')
 
 #Container 3
-CONTAINER_3=(':atlasdb-timelock-ete:check' ':lock-impl:check' ':atlasdb-dbkvs-tests:check' ':atlasdb-tests-shared:check' ':atlasdb-ete-test-utils:check')
+#CONTAINER_3=(':atlasdb-timelock-ete:check' ':lock-impl:check' ':atlasdb-dbkvs-tests:check' ':atlasdb-tests-shared:check' ':atlasdb-ete-test-utils:check')
 
-CONTAINER_4=(':atlasdb-dropwizard-tests:check')
+#CONTAINER_4=(':atlasdb-dropwizard-tests:check')
 
-CONTAINER_5=(':atlasdb-dbkvs:check' ':atlasdb-cassandra-multinode-tests:check' ':atlasdb-impl-shared:check')
+#CONTAINER_5=(':atlasdb-dbkvs:check' ':atlasdb-cassandra-multinode-tests:check' ':atlasdb-impl-shared:check')
 
 # Container 0 - runs tasks not found in the below containers
-CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}"  "${CONTAINER_5[@]}")
+#CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}"  "${CONTAINER_5[@]}")
 
-for task in "${CONTAINER_0_EXCLUDE[@]}"
-do
-    CONTAINER_0_EXCLUDE_ARGS="$CONTAINER_0_EXCLUDE_ARGS -x $task"
-done
+#for task in "${CONTAINER_0_EXCLUDE[@]}"
+#do
+#    CONTAINER_0_EXCLUDE_ARGS="$CONTAINER_0_EXCLUDE_ARGS -x $task"
+#done
 
 case $CIRCLE_NODE_INDEX in
-    0) ./gradlew --profile --continue check $CONTAINER_0_EXCLUDE_ARGS ;;
-    1) ./gradlew --profile --continue --parallel ${CONTAINER_1[@]} ;;
-    2) ./gradlew --profile --continue --parallel ${CONTAINER_2[@]} ;;
-    3) ./gradlew --profile --continue ${CONTAINER_3[@]} && checkDocsBuild ;;
-    4) ./gradlew --profile --continue --parallel ${CONTAINER_4[@]} ;;
-    5) ./gradlew --profile --continue --parallel ${CONTAINER_5[@]} ;;
+    0) ./gradlew --profile --continue ${CONTAINER_0[@]} ;;
+#    0) ./gradlew --profile --continue check $CONTAINER_0_EXCLUDE_ARGS ;;
+#    1) ./gradlew --profile --continue --parallel ${CONTAINER_1[@]} ;;
+#    2) ./gradlew --profile --continue --parallel ${CONTAINER_2[@]} ;;
+#    3) ./gradlew --profile --continue ${CONTAINER_3[@]} && checkDocsBuild ;;
+#    4) ./gradlew --profile --continue --parallel ${CONTAINER_4[@]} ;;
+#    5) ./gradlew --profile --continue --parallel ${CONTAINER_5[@]} ;;
 esac
