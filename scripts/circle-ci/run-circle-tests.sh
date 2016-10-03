3d3d9f6b0550f1a0962f33894fa93888d7159acf
George Brova <gbrova@palantir.com>
2016-10-03 13:13:46 -0400

    Add ':atlasdb-dropwizard-bundle:check' to CONTAINER_5

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -18,7 +18,7 @@ CONTAINER_3=(':atlasdb-timelock-ete:check' ':lock-impl:check' ':atlasdb-dbkvs-te
 
 CONTAINER_4=(':atlasdb-dropwizard-tests:check')
 
-CONTAINER_5=(':atlasdb-dbkvs:check' ':atlasdb-cassandra-multinode-tests:check' ':atlasdb-impl-shared:check')
+CONTAINER_5=(':atlasdb-dbkvs:check' ':atlasdb-cassandra-multinode-tests:check' ':atlasdb-impl-shared:check' ':atlasdb-dropwizard-bundle:check')
 
 # Container 0 - runs tasks not found in the below containers
 CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}"  "${CONTAINER_5[@]}")
