3eaaf9b50889122855cd6b83ecd5f3c51a506fb2
John Boreiko <jboreiko@palantir.com>
2016-10-11 10:58:39 +0100

    Fix url and update message

--- scripts/circle-ci/pr_changelog_status_check.sh
+++ scripts/circle-ci/pr_changelog_status_check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 CHANGELOG="docs/source/release_notes/release-notes.rst"
 
 if [ -z "$GITHUB_AUTH_TOKEN" ];
@@ -14,7 +16,7 @@ CHANGELOG_MODIFIED=$(git diff origin/develop --name-only | grep $CHANGELOG -q >/
 
 if [ $CHANGELOG_MODIFIED -ne 0 ];
 then
-    curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "failure", "description": "No modifications to $CHANGELOG were found.", "context" : "changelog"}' "https://github.com/api/v3/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
+    curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "failure", "description": "No modifications to release_notes.rst were found.", "context" : "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
 else
-    curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "success", "description": "$CHANGELOG updated.", "context": "changelog"}' "https://github.com/api/v3/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
+    curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "success", "description": "release_notes.rst updated.", "context": "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
 fi
