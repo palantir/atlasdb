095baaf704d92e85a15d3c9aef37418b1fcaa90f
George Brova <gbrova@palantir.com>
2016-10-12 09:23:08 +0100

    Move Circle containers

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -11,9 +11,9 @@ CONTAINER_1=(':atlasdb-cassandra-integration-tests:check')
 
 CONTAINER_2=(':atlasdb-ete-tests:check')
 
-CONTAINER_3=(':atlasdb-timelock-ete:check' ':lock-impl:check' ':atlasdb-dbkvs-tests:check' ':atlasdb-tests-shared:check' ':atlasdb-ete-test-utils:check' ':atlasdb-cassandra:check' ':atlasdb-api:check')
+CONTAINER_3=(':atlasdb-timelock-ete:check' ':lock-impl:check' ':atlasdb-dbkvs-tests:check' ':atlasdb-ete-test-utils:check' ':atlasdb-cassandra:check' ':atlasdb-api:check')
 
-CONTAINER_4=(':atlasdb-dbkvs:check' ':atlasdb-cassandra-multinode-tests:check' ':atlasdb-impl-shared:check' ':atlasdb-dropwizard-bundle:check')
+CONTAINER_4=(':atlasdb-dbkvs:check' ':atlasdb-cassandra-multinode-tests:check' ':atlasdb-impl-shared:check' ':atlasdb-dropwizard-bundle:check' ':atlasdb-tests-shared:check')
 
 # Container 0 - runs tasks not found in the below containers
 CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}")
@@ -26,7 +26,7 @@ done
 case $CIRCLE_NODE_INDEX in
     0) ./gradlew --profile --continue check $CONTAINER_0_EXCLUDE_ARGS ;;
     1) ./gradlew --profile --continue --parallel ${CONTAINER_1[@]} ;;
-    2) ./gradlew --profile --continue --parallel ${CONTAINER_2[@]} ;;
+    2) ./gradlew --profile --continue ${CONTAINER_2[@]} ;;
     3) ./gradlew --profile --continue ${CONTAINER_3[@]} && checkDocsBuild ;;
     4) ./gradlew --profile --continue --parallel ${CONTAINER_4[@]} ;;
 esac
