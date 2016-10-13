46608f720f7aaa7bae8c0bb1797693d8dcca7d50
Glenn Sheasby <gsheasby@palantir.com>
2016-10-13 14:08:20 +0100

    Extract CassandraTimestampDao

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTimestampBoundStore.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTimestampBoundStore.java
@@ -14,17 +14,11 @@
  * limitations under the License.
  */
 package com.palantir.atlasdb.keyvalue.cassandra;
-import java.nio.ByteBuffer;
-
 import javax.annotation.concurrent.GuardedBy;
 
 import org.apache.cassandra.thrift.CASResult;
 import org.apache.cassandra.thrift.Column;
 import org.apache.cassandra.thrift.ColumnOrSuperColumn;
-import org.apache.cassandra.thrift.ColumnPath;
-import org.apache.cassandra.thrift.ConsistencyLevel;
-import org.apache.cassandra.thrift.NotFoundException;
-import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,8 +40,6 @@ import com.palantir.timestamp.TimestampBoundStore;
 
 public final class CassandraTimestampBoundStore implements TimestampBoundStore {
     private static final Logger log = LoggerFactory.getLogger(CassandraTimestampBoundStore.class);
-
-    private static final long CASSANDRA_TIMESTAMP = 0L;
     private static final String ROW_AND_COLUMN_NAME = "ts";
 
     public static final TableMetadata TIMESTAMP_TABLE_METADATA = new TableMetadata(
@@ -61,28 +53,28 @@ public final class CassandraTimestampBoundStore implements TimestampBoundStore {
             ConflictHandler.IGNORE_ALL);
 
     private static final long INITIAL_VALUE = 10000L;
+    private final CassandraTimestampDao cassandraTimestampDao;
 
     @GuardedBy("this")
     private long currentLimit = -1;
     @GuardedBy("this")
     private Throwable lastWriteException = null;
-    private final CassandraClientPool clientPool;
 
     public static TimestampBoundStore create(CassandraKeyValueService kvs) {
         kvs.createTable(AtlasDbConstants.TIMESTAMP_TABLE, TIMESTAMP_TABLE_METADATA.persistToBytes());
-        return new CassandraTimestampBoundStore(kvs.clientPool);
+        CassandraClientPool clientPool = kvs.clientPool;
+        Preconditions.checkNotNull(clientPool, "clientPool cannot be null");
+        CassandraTimestampDao dao = new CassandraTimestampDao(clientPool);
+        return new CassandraTimestampBoundStore(dao);
     }
 
-    private CassandraTimestampBoundStore(CassandraClientPool clientPool) {
-        this.clientPool = Preconditions.checkNotNull(clientPool, "clientPool cannot be null");
+    private CassandraTimestampBoundStore(CassandraTimestampDao dao) {
+        this.cassandraTimestampDao = dao;
     }
 
     @Override
     public synchronized long getUpperLimit() {
-        ByteBuffer rowName = getRowName();
-        ColumnPath columnPath = new ColumnPath(AtlasDbConstants.TIMESTAMP_TABLE.getQualifiedName());
-        columnPath.setColumn(getColumnName());
-        ColumnOrSuperColumn result = getStoredLimit(rowName, columnPath);
+        ColumnOrSuperColumn result = cassandraTimestampDao.getStoredLimit();
         if (result == null) {
             setInitialValue();
             return INITIAL_VALUE;
@@ -90,18 +82,6 @@ public final class CassandraTimestampBoundStore implements TimestampBoundStore {
         Column column = result.getColumn();
         currentLimit = PtBytes.toLong(column.getValue());
         return currentLimit;
-    }
-
-    private ColumnOrSuperColumn getStoredLimit(ByteBuffer rowName, ColumnPath columnPath) {
-        return clientPool.runWithRetry(client -> {
-            try {
-                return client.get(rowName, columnPath, ConsistencyLevel.LOCAL_QUORUM);
-            } catch (NotFoundException e) {
-                return null;
-            } catch (Exception e) {
-                throw Throwables.throwUncheckedException(e);
-            }
-        });
     }
 
     private void setInitialValue() {
@@ -116,7 +96,7 @@ public final class CassandraTimestampBoundStore implements TimestampBoundStore {
     private void cas(Long oldVal, long newVal) {
         final CASResult result;
         try {
-            result = checkAndSet(oldVal, newVal);
+            result = cassandraTimestampDao.checkAndSet(oldVal, newVal);
         } catch (Exception e) {
             lastWriteException = e;
             throw Throwables.throwUncheckedException(e);
@@ -135,41 +115,5 @@ public final class CassandraTimestampBoundStore implements TimestampBoundStore {
             lastWriteException = null;
             currentLimit = newVal;
         }
-    }
-
-    private CASResult checkAndSet(Long oldVal, long newVal) {
-        CASResult result;
-        result = clientPool.runWithRetry(client -> {
-            try {
-                return client.cas(
-                        getRowName(),
-                        AtlasDbConstants.TIMESTAMP_TABLE.getQualifiedName(),
-                        oldVal == null ? ImmutableList.of() : ImmutableList.of(makeColumn(oldVal)),
-                        ImmutableList.of(makeColumn(newVal)),
-                        ConsistencyLevel.SERIAL,
-                        ConsistencyLevel.EACH_QUORUM);
-            } catch (TException e) {
-                throw Throwables.throwUncheckedException(e);
-            }
-        });
-        return result;
-    }
-
-    private Column makeColumn(long ts) {
-        Column col = new Column();
-        col.setName(getColumnName());
-        col.setValue(PtBytes.toBytes(ts));
-        col.setTimestamp(CASSANDRA_TIMESTAMP);
-        return col;
-    }
-
-    private static byte[] getColumnName() {
-        return CassandraKeyValueServices
-                .makeCompositeBuffer(PtBytes.toBytes(ROW_AND_COLUMN_NAME), CASSANDRA_TIMESTAMP)
-                .array();
-    }
-
-    private static ByteBuffer getRowName() {
-        return ByteBuffer.wrap(PtBytes.toBytes(ROW_AND_COLUMN_NAME));
     }
 }
---
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTimestampDao.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2016 Palantir Technologies
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.atlasdb.keyvalue.cassandra;
+
+import java.nio.ByteBuffer;
+
+import org.apache.cassandra.thrift.CASResult;
+import org.apache.cassandra.thrift.Column;
+import org.apache.cassandra.thrift.ColumnOrSuperColumn;
+import org.apache.cassandra.thrift.ColumnPath;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.apache.cassandra.thrift.NotFoundException;
+import org.apache.thrift.TException;
+
+import com.google.common.collect.ImmutableList;
+import com.palantir.atlasdb.AtlasDbConstants;
+import com.palantir.atlasdb.encoding.PtBytes;
+import com.palantir.common.base.Throwables;
+
+class CassandraTimestampDao {
+    private static final long CASSANDRA_TIMESTAMP = 0L;
+
+    private final CassandraClientPool clientPool;
+
+    CassandraTimestampDao(CassandraClientPool clientPool) {
+        this.clientPool = clientPool;
+    }
+
+    ColumnOrSuperColumn getStoredLimit() {
+        ByteBuffer rowName = getRowName();
+        ColumnPath columnPath = new ColumnPath(AtlasDbConstants.TIMESTAMP_TABLE.getQualifiedName());
+        columnPath.setColumn(getColumnName());
+        return clientPool.runWithRetry(client -> {
+            try {
+                return client.get(rowName, columnPath, ConsistencyLevel.LOCAL_QUORUM);
+            } catch (NotFoundException e) {
+                return null;
+            } catch (Exception e) {
+                throw Throwables.throwUncheckedException(e);
+            }
+        });
+    }
+
+    CASResult checkAndSet(Long oldVal, long newVal) {
+        return clientPool.runWithRetry(client -> {
+            try {
+                return client.cas(
+                        getRowName(),
+                        AtlasDbConstants.TIMESTAMP_TABLE.getQualifiedName(),
+                        oldVal == null ? ImmutableList.of()
+                                : ImmutableList.of(makeColumn(oldVal)),
+                        ImmutableList.of(makeColumn(newVal)),
+                        ConsistencyLevel.SERIAL,
+                        ConsistencyLevel.EACH_QUORUM);
+            } catch (TException e) {
+                throw Throwables.throwUncheckedException(e);
+            }
+        });
+    }
+
+    private Column makeColumn(long ts) {
+        Column col = new Column();
+        col.setName(getColumnName());
+        col.setValue(PtBytes.toBytes(ts));
+        col.setTimestamp(CASSANDRA_TIMESTAMP);
+        return col;
+    }
+
+    private static byte[] getColumnName() {
+        return CassandraKeyValueServices
+                .makeCompositeBuffer(PtBytes.toBytes(ROW_AND_COLUMN_NAME), CASSANDRA_TIMESTAMP)
+                .array();
+    }
+
+    private static ByteBuffer getRowName() {
+        return ByteBuffer.wrap(PtBytes.toBytes(ROW_AND_COLUMN_NAME));
+    }
+}
