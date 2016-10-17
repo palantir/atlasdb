9731389b114ace765326417895609be633ba3c46
George Brova <gbrova@palantir.com>
2016-10-17 20:52:31 +0100

    Add tests to verify batching

--- atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/SweepTaskRunnerImplTest.java
+++ atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/SweepTaskRunnerImplTest.java
@@ -20,15 +20,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyCollection;
 import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anySet;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableList;
@@ -37,14 +45,19 @@ import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Multimap;
+import com.google.common.primitives.UnsignedBytes;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
+import com.palantir.atlasdb.keyvalue.api.Namespace;
+import com.palantir.atlasdb.keyvalue.api.RangeRequest;
 import com.palantir.atlasdb.keyvalue.api.RowResult;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.keyvalue.api.Value;
 import com.palantir.atlasdb.protos.generated.TableMetadataPersistence.SweepStrategy;
 import com.palantir.atlasdb.sweep.sweepers.ConservativeSweeper;
 import com.palantir.atlasdb.sweep.sweepers.Sweeper;
 import com.palantir.atlasdb.sweep.sweepers.ThoroughSweeper;
+import com.palantir.atlasdb.transaction.impl.SweepStrategyManager;
 import com.palantir.atlasdb.transaction.impl.TransactionConstants;
 import com.palantir.atlasdb.transaction.service.TransactionService;
 import com.palantir.common.base.ClosableIterators;
@@ -56,21 +69,27 @@ public class SweepTaskRunnerImplTest {
     private static final long HIGH_COMMIT_TS = 102L;
     private static final long VALID_TIMESTAMP = 123L;
 
+    private static final TableReference TABLE_REFERENCE = TableReference.create(Namespace.create("ns"), "testTable");
     private static final Cell SINGLE_CELL = Cell.create(
             "cellRow".getBytes(StandardCharsets.UTF_8),
             "cellCol".getBytes(StandardCharsets.UTF_8));
+
+    public static final long OLD_START_TS = 10L;
+    public static final long COMMIT_TS = 30L;
+    public static final long NEW_START_TS = 20L;
 
     private final KeyValueService mockKvs = mock(KeyValueService.class);
     private final Supplier<Long> mockImmutableTimestampSupplier = mock(Supplier.class);
     private final Supplier<Long> mockUnreadableTimestampSupplier = mock(Supplier.class);
     private final TransactionService mockTransactionService = mock(TransactionService.class);
     private final CellsSweeper mockCellsSweeper = mock(CellsSweeper.class);
+    private final SweepStrategyManager mockSweepStrategyManager = mock(SweepStrategyManager.class);
     private final SweepTaskRunnerImpl sweepTaskRunner = new SweepTaskRunnerImpl(
             mockKvs,
             mockUnreadableTimestampSupplier,
             mockImmutableTimestampSupplier,
             mockTransactionService,
-            null,
+            mockSweepStrategyManager,
             mockCellsSweeper);
     private final Sweeper thoroughSweeper = new ThoroughSweeper(mockKvs, mockImmutableTimestampSupplier);
     private final Sweeper conservativeSweeper = new ConservativeSweeper(
@@ -231,6 +250,67 @@ public class SweepTaskRunnerImplTest {
         Multimap<Cell, Long> timestampsToSweep = cellsToSweep.timestampsAsMultimap();
 
         assertThat(timestampsToSweep.get(SINGLE_CELL)).contains(LOW_START_TS);
+    }
+
+    @Test
+    public void cellSweeperCallsShouldBatchRows() {
+        int numBatches = 3;
+        int maxBatchSize = SweepTaskRunnerImpl.CELL_BATCH_SIZE;
+        int numRows = numBatches * maxBatchSize;
+        List<RowResult<Set<Long>>> timestamps = makeTimestamps(numRows, 1);
+
+        verifyBatching(numBatches, maxBatchSize, timestamps);
+    }
+
+    @Test
+    public void cellSweeperCallsShouldBatchColumns() {
+        int numBatches = 3;
+        int maxBatchSize = SweepTaskRunnerImpl.CELL_BATCH_SIZE;
+        int numCols = numBatches * maxBatchSize;
+        List<RowResult<Set<Long>>> timestamps = makeTimestamps(1, numCols);
+
+        verifyBatching(numBatches, maxBatchSize, timestamps);
+    }
+
+    private void verifyBatching(int batches, int maxBatchSize, List<RowResult<Set<Long>>> timestamps) {
+        ImmutableMap<Long, Long> startToCommitTs = ImmutableMap.of(OLD_START_TS, COMMIT_TS, NEW_START_TS, COMMIT_TS);
+        when(mockTransactionService.get(any(Iterable.class))).thenReturn(startToCommitTs);
+        when(mockKvs.getMetadataForTable(TABLE_REFERENCE)).thenReturn(new byte[1]);
+        when(mockSweepStrategyManager.get()).thenReturn(ImmutableMap.of());
+        when(mockImmutableTimestampSupplier.get()).thenReturn(100L);
+        when(mockUnreadableTimestampSupplier.get()).thenReturn(100L);
+        when(mockKvs.getRangeOfTimestamps(eq(TABLE_REFERENCE), any(RangeRequest.class), anyLong()))
+                .thenReturn(ClosableIterators.wrap(timestamps.iterator()));
+
+        sweepTaskRunner.run(TABLE_REFERENCE, 1000, null);
+
+        verifyAllCallsRespectBatching(batches, maxBatchSize);
+    }
+
+    private void verifyAllCallsRespectBatching(int batches, int maxBatchSize) {
+        ArgumentCaptor<Multimap> captor = ArgumentCaptor.forClass(Multimap.class);
+        verify(mockCellsSweeper, times(batches)).sweepCells(eq(TABLE_REFERENCE), captor.capture(), anySet());
+
+        for (Multimap capturedMap : captor.getAllValues()) {
+            assertThat(capturedMap.size()).isLessThanOrEqualTo(maxBatchSize);
+        }
+    }
+
+    private List<RowResult<Set<Long>>> makeTimestamps(int rows, int cols) {
+        ImmutableList.Builder<RowResult<Set<Long>>> rowsBuilder = ImmutableList.builder();
+        Set<Long> timestampsSet = ImmutableSet.of(OLD_START_TS, NEW_START_TS);
+        for (int row = 0; row < rows; row++) {
+            SortedMap<byte[], Set<Long>> columns =
+                    new ConcurrentSkipListMap<>(UnsignedBytes.lexicographicalComparator());
+            for (int col = 0; col < cols; col++) {
+                byte[] colName = ("col" + col).getBytes(StandardCharsets.UTF_8);
+                columns.put(colName, timestampsSet);
+            }
+
+            byte[] rowName = ("row" + row).getBytes(StandardCharsets.UTF_8);
+            rowsBuilder.add(RowResult.create(rowName, columns));
+        }
+        return rowsBuilder.build();
     }
 
     private Multimap<Cell, Long> twoCommittedTimestampsForSingleCell() {
