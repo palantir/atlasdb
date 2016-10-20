068dc3113f7ee5e0779f7a896e8da3032c629d5b
George Brova <gbrova@palantir.com>
2016-10-20 23:53:22 +0100

    Attempt the KVS deletions in the background

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/persistentlock/LockStore.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/persistentlock/LockStore.java
@@ -16,6 +16,9 @@
 package com.palantir.atlasdb.persistentlock;
 
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -35,6 +38,7 @@ public final class LockStore {
     private static final Logger log = LoggerFactory.getLogger(LockStore.class);
 
     private final KeyValueService keyValueService;
+    private final ExecutorService backgroundCleanExecutor = Executors.newSingleThreadExecutor();
 
     private LockStore(KeyValueService keyValueService) {
         this.keyValueService = keyValueService;
@@ -63,7 +67,7 @@ public final class LockStore {
                 .filter(LockEntry::tombstoned)
                 .collect(Collectors.toSet());
 
-        attemptToCleanupTombstonedEntries(tombstonedLockEntries);
+        backgroundCleanupTombstonedEntries(tombstonedLockEntries);
 
         return Sets.difference(allLockEntriesIncludingTombstones, tombstonedLockEntries);
     }
@@ -76,6 +80,10 @@ public final class LockStore {
     public void insertLockEntry(LockEntry lockEntry) {
         log.debug("Attempting to acquire persistent lock {}", lockEntry);
         keyValueService.put(AtlasDbConstants.PERSISTED_LOCKS_TABLE, lockEntry.insertionMap(), LOCKS_TIMESTAMP);
+    }
+
+    private void backgroundCleanupTombstonedEntries(Set<LockEntry> tombstonedLockEntries) {
+        backgroundCleanExecutor.submit(() -> attemptToCleanupTombstonedEntries(tombstonedLockEntries));
     }
 
     private void attemptToCleanupTombstonedEntries(Set<LockEntry> tombstonedLockEntries) {
@@ -92,5 +100,10 @@ public final class LockStore {
                     + "See debug-level logging for the exact error. ");
             log.debug("Failed to clean up a tombstoned persistent lock entry for the following reason: ", e);
         }
+    }
+
+    public void shutdownBackgroundCleaner() throws InterruptedException {
+        backgroundCleanExecutor.shutdown();
+        backgroundCleanExecutor.awaitTermination(10, TimeUnit.SECONDS);
     }
 }
