fe1451df19cda5602bfe4f31a80c23eb9ee71492
John Boreiko <jboreiko@palantir.com>
2016-10-22 17:10:33 +0100

    Fix pr script as it had whitespace problem and a spelling mistake

--- scripts/circle-ci/pr_changelog_status_check.sh
+++ scripts/circle-ci/pr_changelog_status_check.sh
@@ -14,9 +14,9 @@ cd $(dirname $0)
 CURRENT_REF=$(git log -1 --format="%H")
 CHANGELOG_COMMIT_FLAG=$(git log origin/develop..HEAD --pretty=format:%B | grep '\[no release notes\]' -q >/dev/null)$?
 
-if [ $CHANGELOG_COMMIT_FLAG -eq 0];
+if [ $CHANGELOG_COMMIT_FLAG -eq 0 ];
 then
-    curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "success", "description": "by passed with commit flag", "context": "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
+    curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "success", "description": "Bypassed with commit flag", "context": "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
 else
     CHANGELOG_MODIFIED=$(git log origin/develop..HEAD --name-only --pretty=format: | grep $CHANGELOG -q >/dev/null)$?
     if [ $CHANGELOG_MODIFIED -ne 0 ];
