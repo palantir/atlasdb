1400a04c7a284fde991b2582404f824d32006dc6
John Boreiko <jboreiko@palantir.com>
2016-10-24 17:10:59 +0100

    Refactor requests to have proper order and remove unnecessary redirect of stout / sterr

--- scripts/circle-ci/pr_changelog_status_check.sh
+++ scripts/circle-ci/pr_changelog_status_check.sh
@@ -12,13 +12,13 @@ fi
 cd $(dirname $0)
 
 CURRENT_REF=$(git log -1 --format="%H")
-CHANGELOG_COMMIT_FLAG=$(git log origin/develop..HEAD --pretty=format:%B | grep '\[no release notes\]' -q >/dev/null)$?
+CHANGELOG_COMMIT_FLAG=$(git log --pretty=format:%B origin/develop..HEAD | grep '\[no release notes\]' -q)$?
 
 if [ $CHANGELOG_COMMIT_FLAG -eq 0 ];
 then
     curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "success", "description": "Bypassed with commit flag", "context": "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
 else
-    CHANGELOG_MODIFIED=$(git log origin/develop..HEAD --name-only --pretty=format: | grep $CHANGELOG -q >/dev/null)$?
+    CHANGELOG_MODIFIED=$(git log --name-only --pretty=format: origin/develop..HEAD | grep $CHANGELOG -q)$?
     if [ $CHANGELOG_MODIFIED -ne 0 ];
     then
         curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "failure", "description": "No modifications found in release_notes.rst, bypass by adding [no release notes] to a commit in this PR", "context" : "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
