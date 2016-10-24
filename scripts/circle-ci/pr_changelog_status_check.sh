ee6f8ac1adf6c3e82c54b42c8e71a189f2f2d16a
John Boreiko <jboreiko@palantir.com>
2016-10-24 17:13:33 +0100

    Restructure if else blocks in chanlog_status script to increase readability

--- scripts/circle-ci/pr_changelog_status_check.sh
+++ scripts/circle-ci/pr_changelog_status_check.sh
@@ -13,16 +13,12 @@ cd $(dirname $0)
 
 CURRENT_REF=$(git log -1 --format="%H")
 CHANGELOG_COMMIT_FLAG=$(git log --pretty=format:%B origin/develop..HEAD | grep '\[no release notes\]' -q)$?
+CHANGELOG_MODIFIED=$(git log --name-only --pretty=format: origin/develop..HEAD | grep $CHANGELOG -q)$?
 
-if [ $CHANGELOG_COMMIT_FLAG -eq 0 ];
-then
+if [ $CHANGELOG_COMMIT_FLAG -eq 0 ]; then
     curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "success", "description": "Bypassed with commit flag", "context": "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
+elif [ $CHANGELOG_MODIFIED -eq 0 ]; then
+    curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "success", "description": "release_notes.rst updated.", "context": "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
 else
-    CHANGELOG_MODIFIED=$(git log --name-only --pretty=format: origin/develop..HEAD | grep $CHANGELOG -q)$?
-    if [ $CHANGELOG_MODIFIED -ne 0 ];
-    then
-        curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "failure", "description": "No modifications found in release_notes.rst, bypass by adding [no release notes] to a commit in this PR", "context" : "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
-    else
-        curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "success", "description": "release_notes.rst updated.", "context": "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
-    fi
+    curl -X POST -f --silent --header "Authorization: token $GITHUB_AUTH_TOKEN" -d '{"state": "failure", "description": "No modifications found in release_notes.rst, bypass by adding [no release notes] to a commit in this PR", "context" : "changelog"}' "https://api.github.com/repos/palantir/atlasdb/statuses/$CURRENT_REF" >/dev/null
 fi
