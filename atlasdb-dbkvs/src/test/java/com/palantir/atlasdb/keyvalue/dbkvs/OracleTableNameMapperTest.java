c5d851c61940d83eccd1d3a53dcf973ee303bd4e
Himangi Saraogi <hsaraogi@palantir.com>
2016-10-27 15:03:15 +0100

    Fix tests

--- atlasdb-dbkvs/src/test/java/com/palantir/atlasdb/keyvalue/dbkvs/OracleTableNameMapperTest.java
+++ atlasdb-dbkvs/src/test/java/com/palantir/atlasdb/keyvalue/dbkvs/OracleTableNameMapperTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Palantir Technologies
- *
+ * <p>
  * Licensed under the BSD-3 License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://opensource.org/licenses/BSD-3-Clause
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,20 +29,21 @@ import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.keyvalue.dbkvs.impl.DbKvs;
 
 public class OracleTableNameMapperTest {
-    OracleTableNameMapper nameMapper = new OracleTableNameMapper();
 
     private static final int TABLE_NAME_LENGTH = 30;
 
     private static final String TABLE_PREFIX = "a_";
     private static final TableReference SHORT_TABLE_NAME = TableReference.createFromFullyQualifiedName("ns.test_table");
-    private static final TableReference LONG_TABLE_NAME_WITHOUT_VOWELS = TableReference.createFromFullyQualifiedName("ns.Lng_tbl_nm_wtht_vwls_0123456789");
+    private static final TableReference LONG_TABLE_NAME_WITHOUT_VOWELS = TableReference.createFromFullyQualifiedName(
+            "ns.Lng_tbl_nm_wtht_vwls_0123456789");
 
     private static final String ORACLE_TABLE_NAME_REGEX = "^(?!_)^[a-zA-Z0-9_]*$";
 
     @Test
     public void shouldNotModifyNameForShortTableNames() {
         String fullTableName = TABLE_PREFIX + DbKvs.internalTableName(SHORT_TABLE_NAME);
-        Assert.assertThat(nameMapper.getShortPrefixedTableName(TABLE_PREFIX, SHORT_TABLE_NAME), is(fullTableName));
+        Assert.assertThat(OracleTableNameMapper.getShortPrefixedTableName(TABLE_PREFIX, SHORT_TABLE_NAME),
+                is(fullTableName));
     }
 
     @Test
@@ -52,7 +53,7 @@ public class OracleTableNameMapperTest {
                 randomQualifiedTableName);
         String fullTableName = TABLE_PREFIX + DbKvs.internalTableName(tableRef);
         Assert.assertThat(fullTableName.length(), is(TABLE_NAME_LENGTH));
-        Assert.assertThat(nameMapper.getShortPrefixedTableName(TABLE_PREFIX, tableRef), is(fullTableName));
+        Assert.assertThat(OracleTableNameMapper.getShortPrefixedTableName(TABLE_PREFIX, tableRef), is(fullTableName));
     }
 
     @Test
@@ -61,7 +62,7 @@ public class OracleTableNameMapperTest {
         final TableReference tableRef = TableReference.createFromFullyQualifiedName(
                 randomQualifiedTableName);
         String fullTableName = TABLE_PREFIX + DbKvs.internalTableName(tableRef);
-        final String shortTableName = nameMapper.getShortPrefixedTableName(TABLE_PREFIX, tableRef);
+        final String shortTableName = OracleTableNameMapper.getShortPrefixedTableName(TABLE_PREFIX, tableRef);
         Assert.assertThat(fullTableName.length(), is(TABLE_NAME_LENGTH + 1));
         Assert.assertThat(shortTableName.length(), is(TABLE_NAME_LENGTH));
         Assert.assertTrue(shortTableName.matches(ORACLE_TABLE_NAME_REGEX));
@@ -69,19 +70,23 @@ public class OracleTableNameMapperTest {
 
     @Test
     public void shouldReturnValidAndLongestPossibleOracleTableNamesForLongTableNames() {
-        Assert.assertThat(DbKvs.internalTableName(LONG_TABLE_NAME_WITHOUT_VOWELS).length(), greaterThan(TABLE_NAME_LENGTH));
-        String shortTableName = nameMapper.getShortPrefixedTableName(TABLE_PREFIX, LONG_TABLE_NAME_WITHOUT_VOWELS);
+        Assert.assertThat(DbKvs.internalTableName(LONG_TABLE_NAME_WITHOUT_VOWELS).length(),
+                greaterThan(TABLE_NAME_LENGTH));
+        String shortTableName = OracleTableNameMapper.getShortPrefixedTableName(TABLE_PREFIX,
+                LONG_TABLE_NAME_WITHOUT_VOWELS);
         Assert.assertThat(shortTableName.length(), equalTo(TABLE_NAME_LENGTH));
         Assert.assertTrue(shortTableName.matches(ORACLE_TABLE_NAME_REGEX));
     }
 
     @Test
     public void shouldReturnDifferentValidOracleTableNamesForAlmostSimilarLongTableNames() {
-        TableReference longTableName1 = TableReference.createFromFullyQualifiedName("ns." + LONG_TABLE_NAME_WITHOUT_VOWELS + "1");
-        TableReference longTableName2 = TableReference.createFromFullyQualifiedName("ns." + LONG_TABLE_NAME_WITHOUT_VOWELS + "2");
+        TableReference longTableName1 = TableReference.createFromFullyQualifiedName(
+                "ns." + LONG_TABLE_NAME_WITHOUT_VOWELS + "1");
+        TableReference longTableName2 = TableReference.createFromFullyQualifiedName(
+                "ns." + LONG_TABLE_NAME_WITHOUT_VOWELS + "2");
 
-        String shortTableName1 = nameMapper.getShortPrefixedTableName(TABLE_PREFIX, longTableName1);
-        String shortTableName2= nameMapper.getShortPrefixedTableName(TABLE_PREFIX, longTableName2);
+        String shortTableName1 = OracleTableNameMapper.getShortPrefixedTableName(TABLE_PREFIX, longTableName1);
+        String shortTableName2 = OracleTableNameMapper.getShortPrefixedTableName(TABLE_PREFIX, longTableName2);
 
         Assert.assertThat(shortTableName1.length(), is(TABLE_NAME_LENGTH));
         Assert.assertThat(shortTableName2.length(), is(TABLE_NAME_LENGTH));
@@ -90,9 +95,11 @@ public class OracleTableNameMapperTest {
 
     @Test
     public void shouldReturnMoreReadableTableNameForTableNamesWithVowels() {
-        TableReference longTableNameWithManyVowels = TableReference.createFromFullyQualifiedName("ns.eeeeeaaaaabbbbbeeeeeaaaaabbbbbaaaaa");
+        TableReference longTableNameWithManyVowels = TableReference.createFromFullyQualifiedName(
+                "ns.eeeeeaaaaabbbbbeeeeeaaaaabbbbbaaaaa");
 
-        String shortTableName = nameMapper.getShortPrefixedTableName(TABLE_PREFIX, longTableNameWithManyVowels);
+        String shortTableName = OracleTableNameMapper.getShortPrefixedTableName(TABLE_PREFIX,
+                longTableNameWithManyVowels);
         String expectedName = TABLE_PREFIX + "ns__eeeeeaaaabbbbbbbbbb";
 
         Assert.assertThat(shortTableName.length(), lessThanOrEqualTo(TABLE_NAME_LENGTH));
@@ -102,9 +109,12 @@ public class OracleTableNameMapperTest {
 
     @Test
     public void shouldReturnMoreReadableTableNameForTableNamesWithFewerVowels() {
-        TableReference longTableNameWithLessVowels = TableReference.createFromFullyQualifiedName("ns.eeeeebbbbbbbbbbccccccccccddddddcccc");
+        TableReference longTableNameWithLessVowels = TableReference.createFromFullyQualifiedName(
+                "ns.eeeeebbbbbbbbbbccccccccccddddddcccc");
 
-        String shortTableName = nameMapper.getShortPrefixedTableName(TABLE_PREFIX, longTableNameWithLessVowels);
+        String shortTableName = OracleTableNameMapper.getShortPrefixedTableName(
+                TABLE_PREFIX,
+                longTableNameWithLessVowels);
         String expectedName = TABLE_PREFIX + "ns__bbbbbbbbbbccccccccccddddddcccc".substring(0, 23);
 
         Assert.assertThat(shortTableName.length(), is(TABLE_NAME_LENGTH));
@@ -114,9 +124,12 @@ public class OracleTableNameMapperTest {
 
     @Test
     public void shouldReturnMoreReadableTableNameForTableNamesWithDroppingOnlyVowels() {
-        TableReference longTableNameWithExactVowels = TableReference.createFromFullyQualifiedName("ns.aaaaabbbbbbrrrrrrpppopppp");
+        TableReference longTableNameWithExactVowels = TableReference.createFromFullyQualifiedName(
+                "ns.aaaaabbbbbbrrrrrrpppopppp");
 
-        String shortTableName = nameMapper.getShortPrefixedTableName(TABLE_PREFIX, longTableNameWithExactVowels);
+        String shortTableName = OracleTableNameMapper.getShortPrefixedTableName(
+                TABLE_PREFIX,
+                longTableNameWithExactVowels);
         String expectedName = TABLE_PREFIX + "ns__bbbbbbrrrrrrppppppp";
 
         Assert.assertThat(shortTableName.length(), is(TABLE_NAME_LENGTH));
