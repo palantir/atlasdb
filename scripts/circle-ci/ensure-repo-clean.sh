220927d5092c8c6becdf3a73f4245a46d60a8001
John Boreiko <jboreiko@palantir.com>
2016-10-27 12:29:46 +0100

    Add script to check for repo cleanliness

---
+++ scripts/circle-ci/ensure-repo-clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#set -x
+
+if [[ $(git status --short) != "" ]]; then
+   git status
+   cat <<-EOF
+#######################################################################
+There are dirty files remaining in the AtlasDB repo after testing.
+These must be cleaned up or ignored so publishing can run successfully.
+^^^^^^^^^^^See the above git status to figure out the problem^^^^^^^^^^
+#######################################################################
+EOF
+fi
