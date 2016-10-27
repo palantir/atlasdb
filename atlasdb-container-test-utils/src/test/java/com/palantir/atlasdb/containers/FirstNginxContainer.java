0e0e0d89c4aa004dacf1d20487d948f0c4ac3403
Ben Grabham <Ben.Grabham@gmail.com>
2016-10-27 12:31:36 +0100

    Feature/run single test and no test suites (#1119)
    * Implementation of atlasdb-container-test-utils and initial switchover
    
    * Cleanup equals/hashCode, fix shutdown order of Containers
    
    * Fix replicationFactor for three node cassandra
    
    * Add tests for atlasdb-container-test-utils
    
    * Remove shutdown. It isn't really needed
    
    * [no release notes]
    
    * Remove test configuration so we don't run multiple tests
    
    * Store docker compose yamls to a temporary location so that it doesn't matter if they're stored in jars
    
    * Refactor methods to make things more clear

---
+++ atlasdb-container-test-utils/src/test/java/com/palantir/atlasdb/containers/FirstNginxContainer.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2016 Palantir Technologies
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.atlasdb.containers;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.palantir.docker.compose.connection.waiting.SuccessOrFailure;
+
+public class FirstNginxContainer extends Container {
+    @Override
+    public String getDockerComposeFile() {
+        return "/docker-compose-nginx1.yml";
+    }
+
+    @Override
+    public SuccessOrFailure isReady() {
+        return SuccessOrFailure.onResultOf(() -> {
+            URL url = new URL("http://nginx1/");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            return connection.getResponseCode() == 200;
+        });
+    }
+}
