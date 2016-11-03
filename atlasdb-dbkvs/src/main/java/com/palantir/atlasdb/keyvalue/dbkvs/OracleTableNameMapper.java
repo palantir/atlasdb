6f972cadee3652f104c3a1e0875d102846483d54
Himangi Saraogi <hsaraogi@palantir.com>
2016-11-03 14:47:17 +0000

    Fix query

--- atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/OracleTableNameMapper.java
+++ atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/OracleTableNameMapper.java
@@ -78,7 +78,7 @@ public class OracleTableNameMapper {
         AgnosticResultSet results = conns.get().selectResultSetUnregisteredQuery(
                 "SELECT short_table_name "
                 + "FROM " + AtlasDbConstants.ORACLE_NAME_MAPPING_TABLE
-                + "WHERE LOWER(short_table_name) LIKE LOWER(? || '\\______%') ESCAPE '\\'"
+                + " WHERE LOWER(short_table_name) LIKE LOWER(?||'\\______%') ESCAPE '\\'"
                 + " ORDER BY short_table_name DESC", truncatedTableName);
         return getTableNumberFromTableNames(truncatedTableName, results);
     }
