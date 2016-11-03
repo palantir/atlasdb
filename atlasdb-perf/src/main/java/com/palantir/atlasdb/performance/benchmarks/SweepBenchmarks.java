b8e04817f936a3fe2deb4995c4142b140f6610c4
Grgur Petric Maretic <gmaretic@palantir.com>
2016-11-03 17:04:31 +0000

    Fixed checkstyle

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/SweepBenchmarks.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/SweepBenchmarks.java
@@ -1,17 +1,14 @@
 /**
  * Copyright 2016 Palantir Technologies
- *
- * Licensed under the BSD-3 License (the "License");
- * you may not use this file except in compliance with the License.
+ * <p>
+ * Licensed under the BSD-3 License (the "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://opensource.org/licenses/BSD-3-Clause
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.palantir.atlasdb.performance.benchmarks;
 
@@ -73,7 +70,7 @@ public class SweepBenchmarks {
         SweepTaskRunner sweepTaskRunner = table.getSweepTaskRunner();
         SweepResults sweepResults = null;
         byte[] nextStartRow = null;
-        for (int i = 0; i < BATCH_SIZE; i++){
+        for (int i = 0; i < BATCH_SIZE; i++) {
             sweepResults = sweepTaskRunner.run(table.getTableRef(), 1, nextStartRow);
             nextStartRow = sweepResults.getNextStartRow().get();
             assertThat(sweepResults.getCellsDeleted(), is(DELETED_COUNT));
@@ -88,7 +85,7 @@ public class SweepBenchmarks {
         SweepTaskRunner sweepTaskRunner = table.getSweepTaskRunner();
         SweepResults sweepResults = null;
         byte[] nextStartRow = null;
-        for (int i = 0; i < BATCH_SIZE; i++){
+        for (int i = 0; i < BATCH_SIZE; i++) {
             sweepResults = sweepTaskRunner.run(table.getTableRef(), 1, nextStartRow);
             nextStartRow = sweepResults.getNextStartRow().get();
             assertThat(sweepResults.getCellsDeleted(), is(DELETED_COUNT));
