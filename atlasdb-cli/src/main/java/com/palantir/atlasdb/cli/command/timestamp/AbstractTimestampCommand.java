73b9c59df565a827420892a1d9f7fd6ae0a6790a
John Boreiko <jboreiko@palantir.com>
2016-11-08 14:23:44 +0000

    Add OutputPrinter class for clis to output to
    This allows printing to info or error which will both hit the slf4j impls as well as sysout and syserr
    respectively.  This is not perfect as some methods still use logging in other methods outside of the
    command class to inform the user of progress.  Notably: CleanCassLocksStateCommand
    
    Fixes #1116

--- atlasdb-cli/src/main/java/com/palantir/atlasdb/cli/command/timestamp/AbstractTimestampCommand.java
+++ atlasdb-cli/src/main/java/com/palantir/atlasdb/cli/command/timestamp/AbstractTimestampCommand.java
@@ -22,12 +22,12 @@ import java.nio.file.Files;
 import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Throwables;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
+import com.palantir.atlasdb.cli.output.OutputPrinter;
 import com.palantir.atlasdb.cli.command.SingleBackendCommand;
 import com.palantir.atlasdb.services.AtlasDbServices;
 
@@ -36,7 +36,7 @@ import io.airlift.airline.OptionType;
 
 public abstract class AbstractTimestampCommand extends SingleBackendCommand {
 
-    private static final Logger log = LoggerFactory.getLogger(AbstractTimestampCommand.class);
+    private static final OutputPrinter printer = new OutputPrinter(LoggerFactory.getLogger(AbstractTimestampCommand.class));
 
     @Option(name = {"-f", "--file"},
             title = "TIMESTAMP_FILE",
@@ -76,7 +76,7 @@ public abstract class AbstractTimestampCommand extends SingleBackendCommand {
         try {
             timestampString = StringUtils.strip(Iterables.getOnlyElement(Files.readAllLines(file.toPath())));
         } catch (IOException e) {
-            log.error("IOException thrown reading timestamp from file: {}", file.getPath());
+            printer.error("IOException thrown reading timestamp from file: {}", file.getPath());
             throw Throwables.propagate(e);
         }
         return Long.parseLong(timestampString);
@@ -90,7 +90,7 @@ public abstract class AbstractTimestampCommand extends SingleBackendCommand {
             }
             Files.write(file.toPath(), lines, StandardCharsets.UTF_8);
         } catch (IOException e) {
-            log.error("IOException thrown writing timestamp to file: {}", file.getPath());
+            printer.error("IOException thrown writing timestamp to file: {}", file.getPath());
             Throwables.propagate(e);
         }
     }
