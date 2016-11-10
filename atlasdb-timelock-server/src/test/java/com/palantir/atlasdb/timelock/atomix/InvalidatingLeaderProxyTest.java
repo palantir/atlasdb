dd496c8a4f140dea3b18fda5264f47dcdac939ff
Ben Grabham <bgrabham@palantir.com>
2016-11-10 14:17:03 +0000

    Switch out atomix for mocks in IPLT and improve error message

--- atlasdb-timelock-server/src/test/java/com/palantir/atlasdb/timelock/atomix/InvalidatingLeaderProxyTest.java
+++ atlasdb-timelock-server/src/test/java/com/palantir/atlasdb/timelock/atomix/InvalidatingLeaderProxyTest.java
@@ -17,111 +17,69 @@ package com.palantir.atlasdb.timelock.atomix;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.concurrent.CompletableFuture;
+
+import javax.annotation.Nullable;
 import javax.ws.rs.ServiceUnavailableException;
 
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.google.common.util.concurrent.Futures;
-
-import io.atomix.AtomixReplica;
-import io.atomix.catalyst.transport.Address;
-import io.atomix.catalyst.transport.local.LocalServerRegistry;
-import io.atomix.catalyst.transport.local.LocalTransport;
-import io.atomix.copycat.server.storage.Storage;
-import io.atomix.copycat.server.storage.StorageLevel;
-import io.atomix.group.DistributedGroup;
 import io.atomix.group.LocalMember;
 import io.atomix.variables.DistributedValue;
 
 public class InvalidatingLeaderProxyTest {
-    private static final Address LOCAL_ADDRESS = new Address("localhost", 8700);
-
-    private static final AtomixReplica ATOMIX_REPLICA = AtomixReplica.builder(LOCAL_ADDRESS)
-            .withStorage(Storage.builder()
-                    .withStorageLevel(StorageLevel.MEMORY)
-                    .build())
-            .withTransport(new LocalTransport(new LocalServerRegistry()))
-            .build();
-
-    private static final String GROUP_KEY = "groupKey";
-    private static final String LEADER_KEY = "groupKey/leader";
     private static final String TEST_VALUE = "testValue";
+    private static final String LOCAL_MEMBER_ID = "localId";
 
-    private static LocalMember localMember;
-    private static DistributedValue<String> leaderId;
+    private static final DistributedValue<String> LEADER_ID = mock(DistributedValue.class);
+    private static final LocalMember LOCAL_MEMBER = mock(LocalMember.class);
 
-    private AtomicString atomicString;
+    private final AtomicString atomicString = InvalidatingLeaderProxy.create(
+            LOCAL_MEMBER,
+            LEADER_ID,
+            SimpleAtomicString::new,
+            AtomicString.class);
 
     @BeforeClass
-    public static void startAtomix() {
-        ATOMIX_REPLICA.bootstrap().join();
-        leaderId = Futures.getUnchecked(ATOMIX_REPLICA.<String>getValue(LEADER_KEY));
-        DistributedGroup distributedGroup = Futures.getUnchecked(ATOMIX_REPLICA.getGroup(GROUP_KEY));
-        distributedGroup.election().onElection(
-                term -> Futures.getUnchecked(leaderId.set(term.leader().id()))
-        );
-        localMember = Futures.getUnchecked(distributedGroup.join());
-    }
-
-    @AfterClass
-    public static void stopAtomix() {
-        ATOMIX_REPLICA.leave();
-    }
-
-    @Before
-    public void setUp() {
-        atomicString = InvalidatingLeaderProxy.create(
-                localMember,
-                leaderId,
-                SimpleAtomicString::new,
-                AtomicString.class);
+    public static void setupMocks() {
+        when(LOCAL_MEMBER.id()).thenReturn(LOCAL_MEMBER_ID);
+        when(LEADER_ID.get()).thenReturn(null);
     }
 
     @Test
     public void shouldCallDelegateIfLeader() {
+        setLeader(LOCAL_MEMBER_ID);
         assertCanReadAndWriteValue(atomicString);
     }
 
     @Test
     public void shouldBeUnableToCallDelegateIfNotLeader() {
-        String oldLeader = getLeader();
-        try {
-            setLeader(null);
-            assertThatThrownBy(atomicString::get).isInstanceOf(ServiceUnavailableException.class);
-        } finally {
-            setLeader(oldLeader);
-        }
+        setLeader(null);
+        assertThatThrownBy(atomicString::get).isInstanceOf(ServiceUnavailableException.class);
     }
 
     @Test
     public void shouldBeAbleToCallDelegateAgainOnRegainingLeadership() {
-        String oldLeader = getLeader();
-        try {
-            assertCanReadAndWriteValue(atomicString);
-            setLeader(null);
-            assertThatThrownBy(atomicString::get).isInstanceOf(ServiceUnavailableException.class);
-            setLeader(oldLeader);
-            assertCanReadAndWriteValue(atomicString);
-        } finally {
-            setLeader(oldLeader);
-        }
-    }
-
-    private static String getLeader() {
-        return Futures.getUnchecked(leaderId.get());
-    }
-
-    private static void setLeader(String leader) {
-        Futures.getUnchecked(leaderId.set(leader));
+        setLeader(LOCAL_MEMBER_ID);
+        assertCanReadAndWriteValue(atomicString);
+        setLeader(null);
+        assertThatThrownBy(atomicString::get).isInstanceOf(ServiceUnavailableException.class);
+        setLeader(LOCAL_MEMBER_ID);
+        assertCanReadAndWriteValue(atomicString);
     }
 
     private void assertCanReadAndWriteValue(AtomicString container) {
+        setLeader(LOCAL_MEMBER_ID);
         container.set(TEST_VALUE);
         assertThat(container.get()).isEqualTo(TEST_VALUE);
+    }
+
+    private static void setLeader(@Nullable String leader) {
+        when(LEADER_ID.get()).thenReturn(CompletableFuture.completedFuture(leader));
     }
 
     private interface AtomicString {
