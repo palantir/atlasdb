fe21e84db7d8fc40edde53f66e5bbb75e64d32eb
John Boreiko <jboreiko@palantir.com>
2016-11-11 17:48:48 +0000

    Refactor into a StandardStreamUtility to wrap stdout and stderr

--- atlasdb-cli/src/test/java/com/palantir/atlasdb/cli/output/OutputPrinterTest.java
+++ atlasdb-cli/src/test/java/com/palantir/atlasdb/cli/output/OutputPrinterTest.java
@@ -17,53 +17,32 @@ package com.palantir.atlasdb.cli.output;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
+
+import com.palantir.atlasdb.cli.runner.StandardStreamUtilities;
 
 public class OutputPrinterTest {
     private static final OutputPrinter print = new OutputPrinter(LoggerFactory.getLogger(OutputPrinterTest.class));
 
-    private interface StandardStreamSetter {
-        void set(PrintStream ps);
-    }
-
-    private String wrapGenericStream(Runnable runnable, PrintStream original, StandardStreamSetter standardStreamSetter) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        standardStreamSetter.set(new PrintStream(baos));
-        try {
-            runnable.run();
-        } finally {
-            standardStreamSetter.set(original);
-        }
-        return baos.toString().replace("\n", " ").replace("\r", " ");
-    }
-
-    private String wrapSystemOut(Runnable runnable) {
-        return wrapGenericStream(runnable, System.out, System::setOut);
-    }
-
-    private String wrapSystemErr(Runnable runnable) {
-        return wrapGenericStream(runnable, System.err, System::setErr);
-    }
-
     @Test
     public void testInfoPrintingWorksWithSingleReplacement() {
-        String systemOut = wrapSystemOut(() -> print.info("Test this gets {}", "replaced"));
+        String systemOut = StandardStreamUtilities.wrapSystemOut(
+                () -> print.info("Test this gets {}", "replaced"));
         assertThat(systemOut).isEqualTo("Test this gets replaced ");
     }
 
     @Test
     public void testInfoPrintingWorksWithMultipleReplacement() {
-        String systemOut = wrapSystemOut(() -> print.info("Replace {} of {} {}.", "all", "these", "fields"));
+        String systemOut = StandardStreamUtilities.wrapSystemOut(
+                () -> print.info("Replace {} of {} {}.", "all", "these", "fields"));
         assertThat(systemOut).isEqualTo("Replace all of these fields. ");
     }
 
     @Test
     public void testErrorPrintingWorksWithMultipleReplacement() {
-        String systemErr = wrapSystemErr(() -> print.error("Replace {} of {} {}.", "all", "these", "fields"));
+        String systemErr = StandardStreamUtilities.wrapSystemErr(
+                () -> print.error("Replace {} of {} {}.", "all", "these", "fields"));
         assertThat(systemErr).isEqualTo("Replace all of these fields. ");
     }
 }
