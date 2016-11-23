29ac29205b2fe5c6d80107124799190570275673
John Carrino <carrino@palantir.com>
2016-11-22 16:09:38 -0800

    allow contiguous leader across terms

---
+++ atlasdb-timelock-server/src/main/java/com/palantir/atlasdb/timelock/atomix/LeaderAndTerm.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2016 Palantir Technologies
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.atlasdb.timelock.atomix;
+
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableLeaderAndTerm.class)
+@JsonDeserialize(as = ImmutableLeaderAndTerm.class)
+@JsonInclude(Include.NON_NULL)
+public interface LeaderAndTerm {
+    @Value.Parameter
+    long getTerm();
+
+    @Value.Parameter
+    String getLeader();
+
+    static LeaderAndTerm fromStoredString(String stored) {
+        if (stored == null) {
+            return null;
+        }
+        String[] split = stored.split("_", 2);
+        return ImmutableLeaderAndTerm.of(Long.parseLong(split[0]), split[1]);
+    }
+
+    default String toStoredString() {
+        return getTerm() + "_" + getLeader();
+    }
+}
+
