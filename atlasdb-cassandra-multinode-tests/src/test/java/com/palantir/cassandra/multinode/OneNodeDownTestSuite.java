75933bfb28b77d50ad44120df4c540963df5223e
Grgur Petric Maretic <gmaretic@palantir.com>
2016-11-25 12:42:20 +0000

    Added HA Cassandra tests. Still work in progress

--- atlasdb-cassandra-multinode-tests/src/test/java/com/palantir/cassandra/multinode/OneNodeDownTestSuite.java
+++ atlasdb-cassandra-multinode-tests/src/test/java/com/palantir/cassandra/multinode/OneNodeDownTestSuite.java
@@ -21,43 +21,81 @@ import java.util.concurrent.TimeUnit;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+import com.google.common.collect.ImmutableMap;
 import com.jayway.awaitility.Awaitility;
+import com.palantir.atlasdb.AtlasDbConstants;
 import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfigManager;
 import com.palantir.atlasdb.containers.CassandraContainer;
 import com.palantir.atlasdb.containers.Containers;
 import com.palantir.atlasdb.containers.ThreeNodeCassandraCluster;
+import com.palantir.atlasdb.keyvalue.api.Cell;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.keyvalue.cassandra.CassandraClientPool;
 import com.palantir.atlasdb.keyvalue.cassandra.CassandraKeyValueService;
 import com.palantir.docker.compose.connection.Container;
 import com.palantir.docker.compose.connection.DockerPort;
 
-public class DegradedClusterInitializationTest {
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        OneNodeDownGetTest.class,
+        OneNodeDownPutTest.class,
+        OneNodeDownMetadataTest.class,
+        OneNodeDownDeleteTest.class,
+        OneNodeDownTableManipulationTest.class
+    })
+public class OneNodeDownTestSuite {
     private static final String CASSANDRA_NODE_TO_KILL = ThreeNodeCassandraCluster.FIRST_CASSANDRA_CONTAINER_NAME;
 
+    public static final TableReference TEST_TABLE = TableReference.createWithEmptyNamespace("test_table");
+    public static final Cell CELL_1_1 = Cell.create("row1".getBytes(), "col1".getBytes());
+    public static final Cell CELL_1_2 = Cell.create("row1".getBytes(), "col2".getBytes());
+    public static final Cell CELL_2_1 = Cell.create("row2".getBytes(), "col1".getBytes());
+    public static final Cell CELL_2_2 = Cell.create("row2".getBytes(), "col2".getBytes());
+    public static final Cell CELL_3_1 = Cell.create("row3".getBytes(), "col1".getBytes());
+    public static final Cell CELL_3_2 = Cell.create("row3".getBytes(), "col2T".getBytes());
+    public static final byte[] FIRST_ROW = "row1".getBytes();
+    public static final byte[] SECOND_ROW = "row2".getBytes();
+    public static final byte[] DEFAULT_VALUE = "default_value".getBytes();
+    public static final long DEFAULT_TIMESTAMP = 2L;
+
+
+    public static CassandraKeyValueService db;
+
     @ClassRule
-    public static final Containers CONTAINERS = new Containers(DegradedClusterInitializationTest.class)
+    public static final Containers CONTAINERS = new Containers(OneNodeDownTestSuite.class)
             .with(new ThreeNodeCassandraCluster());
 
     @BeforeClass
     public static void initializeKvsAndDegradeCluster() throws IOException, InterruptedException {
-        createAndDiscardCassandraKvs();
+        setupTestTable();
         degradeCassandraCluster();
+        db = createCassandraKvs();
     }
 
     @AfterClass
     public static void bringClusterBack() throws IOException, InterruptedException {
-        startDeadCassandraNode();
+        db.close();
+        //startDeadCassandraNode();
     }
 
-    @Test
-    public void canCreateCassandraKvs()  {
-        createAndDiscardCassandraKvs();
+
+    private static void setupTestTable(){
+        db = createCassandraKvs();
+
+        db.createTable(TEST_TABLE, AtlasDbConstants.GENERIC_TABLE_METADATA);
+        db.put(TEST_TABLE, ImmutableMap.of(CELL_1_1, "old_value".getBytes()), 1);
+        db.put(TEST_TABLE, ImmutableMap.of(CELL_1_1, DEFAULT_VALUE), DEFAULT_TIMESTAMP);
+        db.put(TEST_TABLE, ImmutableMap.of(CELL_1_2, DEFAULT_VALUE), DEFAULT_TIMESTAMP);
+        db.put(TEST_TABLE, ImmutableMap.of(CELL_2_1, DEFAULT_VALUE), DEFAULT_TIMESTAMP);
+
+        db.close();
     }
 
-    private static void createAndDiscardCassandraKvs() {
-        CassandraKeyValueService.create(
+    protected static CassandraKeyValueService createCassandraKvs() {
+        return CassandraKeyValueService.create(
                 CassandraKeyValueServiceConfigManager.createSimpleManager(ThreeNodeCassandraCluster.KVS_CONFIG),
                 ThreeNodeCassandraCluster.LEADER_CONFIG);
     }
@@ -93,7 +131,7 @@ public class DegradedClusterInitializationTest {
     private static void waitUntilStartupChecksPass() {
         Awaitility.await()
                 .atMost(60, TimeUnit.SECONDS)
-                .until(DegradedClusterInitializationTest::startupChecksPass);
+                .until(OneNodeDownTestSuite::startupChecksPass);
     }
 
     private static boolean startupChecksPass() {
