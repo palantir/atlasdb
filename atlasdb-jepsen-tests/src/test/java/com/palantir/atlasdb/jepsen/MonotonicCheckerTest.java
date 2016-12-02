34e6a8ce953f44339f035cb7f0b569cdc896c41e
Harry Biddle <hbiddle@palantir.com>
2016-12-02 12:21:44 +0000

    Typo

--- atlasdb-jepsen-tests/src/test/java/com/palantir/atlasdb/jepsen/MonotonicCheckerTest.java
+++ atlasdb-jepsen-tests/src/test/java/com/palantir/atlasdb/jepsen/MonotonicCheckerTest.java
@@ -29,7 +29,7 @@ public class MonotonicCheckerTest {
     private static final int PROCESS_1 = 1;
 
     @Test
-    public void shouldSuceedOnNoEvents() {
+    public void shouldSucceedOnNoEvents() {
         CheckerResult result = runMonotonicChecker();
 
         assertThat(result.valid()).isTrue();
@@ -75,7 +75,7 @@ public class MonotonicCheckerTest {
     }
 
     @Test
-    public void shouldSuceedOnTwoProcessesOverlapping() {
+    public void shouldSucceedOnTwoProcessesOverlapping() {
         Event event1 = ImmutableOkEvent.builder()
                 .time(ZERO_TIME)
                 .process(PROCESS_0)
