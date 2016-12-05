61184e36e23d9c14b7de96fb948027031003683d
Ben Grabham <Ben.Grabham@gmail.com>
2016-12-05 10:08:00 +0000

    Rebalance cassandra integration tests on circle (#1331)
    * Move long running cassandra integration tests to container 3
    
    * Rebalance other projects
    
    * [no release notes]

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -13,11 +13,11 @@ CONTAINER_1=(':atlasdb-cassandra-integration-tests:check')
 
 CONTAINER_2=(':atlasdb-ete-tests:check')
 
-CONTAINER_3=(':lock-impl:check' ':atlasdb-dbkvs-tests:check' ':atlasdb-tests-shared:check' ':atlasdb-ete-test-utils:check' ':atlasdb-cassandra:check' ':atlasdb-api:check' ':atlasdb-jepsen-tests:check')
+CONTAINER_3=(':atlasdb-ete-test-utils:check' ':atlasdb-cassandra:check' ':atlasdb-api:check' ':atlasdb-jepsen-tests:check' ':atlasdb-cassandra-integration-tests:longTest')
 
 CONTAINER_4=(':atlasdb-dbkvs:check' ':atlasdb-cassandra-multinode-tests:check' ':atlasdb-impl-shared:check' ':atlasdb-dropwizard-bundle:check')
 
-CONTAINER_5=(':atlasdb-ete-tests:longTest')
+CONTAINER_5=(':atlasdb-ete-tests:longTest' ':lock-impl:check' ':atlasdb-dbkvs-tests:check' ':atlasdb-tests-shared:check')
 
 # Container 0 - runs tasks not found in the below containers
 CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}" "${CONTAINER_5[@]}")
@@ -38,7 +38,7 @@ fi
 
 case $CIRCLE_NODE_INDEX in
     0) ./gradlew $TEST_CONTAINER_ARGS check $CONTAINER_0_EXCLUDE_ARGS ;;
-    1) ./gradlew $TEST_CONTAINER_ARGS ${CONTAINER_1[@]} ;;
+    1) ./gradlew $TEST_CONTAINER_ARGS ${CONTAINER_1[@]} -x :atlasdb-cassandra-integration-tests:longTest ;;
     2) ./gradlew $TEST_CONTAINER_ARGS ${CONTAINER_2[@]} -x :atlasdb-ete-tests:longTest ;;
     3) ./gradlew $TEST_CONTAINER_ARGS ${CONTAINER_3[@]} -x :atlasdb-jepsen-tests:jepsenTest ;;
     4) ./gradlew $TEST_CONTAINER_ARGS ${CONTAINER_4[@]} ;;
