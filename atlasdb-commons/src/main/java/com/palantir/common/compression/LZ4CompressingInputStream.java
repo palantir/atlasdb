13e9ea41e5f2023da63021355143ff3ed593aea9
Sandor Van Wassenhove <sandorw@palantir.com>
2016-12-06 12:16:28 -0800

    Add LZ4 compressing input stream
    [no release notes]

---
+++ atlasdb-commons/src/main/java/com/palantir/common/compression/LZ4CompressingInputStream.java
@@ -0,0 +1,170 @@
+/**
+ * Copyright 2016 Palantir Technologies
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.common.compression;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.Checksum;
+
+import com.google.common.base.Preconditions;
+import com.google.common.io.ByteStreams;
+
+import net.jpountz.lz4.LZ4BlockOutputStream;
+import net.jpountz.lz4.LZ4Compressor;
+import net.jpountz.lz4.LZ4Factory;
+import net.jpountz.xxhash.XXHashFactory;
+
+/**
+ * {@link InputStream} that wraps a delegate InputStream, compressing its
+ * contents as they are read. Compression is managed via a compressing
+ * {@link OutputStream}. Reads pull data as necessary from the delegate
+ * InputStream, write the uncompressed data through the OutputStream, then
+ * serve the read from the resulting compressed buffer.
+ */
+public final class LZ4CompressingInputStream extends BufferedDelegateInputStream {
+
+    private static final LZ4Compressor COMPRESSOR = LZ4Factory.fastestInstance().fastCompressor();
+    private static final int DEFAULT_SEED = 0x9747b28c;
+    private static final int DEFAULT_BLOCK_SIZE = 1 << 16; // 64 KB
+
+    private final LZ4BlockOutputStream compressingStream;
+    private final int blockSize;
+
+    // Flag to prevent writes to the internal buffer while the
+    // compressing stream is reading from it.
+    private boolean readInProgress;
+    // Flag to indicate whether this stream has been exhausted.
+    private boolean finished;
+
+    public LZ4CompressingInputStream(InputStream delegate) throws IOException {
+        this(delegate, DEFAULT_BLOCK_SIZE);
+    }
+
+    public LZ4CompressingInputStream(InputStream delegate, int blockSize) throws IOException {
+        super(delegate);
+        this.blockSize = blockSize;
+        int compressedBufferSize = COMPRESSOR.maxCompressedLength(blockSize);
+        Checksum checksum = XXHashFactory.fastestInstance().newStreamingHash32(DEFAULT_SEED).asChecksum();
+        allocateBuffer(Math.max(blockSize, compressedBufferSize));
+        OutputStream delegateOutputStream = new InternalByteArrayOutputStream();
+        this.compressingStream = new LZ4BlockOutputStream(delegateOutputStream, blockSize, COMPRESSOR, checksum, true);
+        this.readInProgress = false;
+        this.finished = false;
+
+        // Flush the LZ4 header so that the compression buffer is empty
+        delegateOutputStream.flush();
+    }
+
+    @Override
+    protected boolean refill() throws IOException {
+        if (finished) {
+            return false;
+        }
+        position = 0;
+        bufferSize = 0;
+
+        // Read a block of data from the delegate input stream.
+        int bytesRead = ByteStreams.read(delegate, buffer, BUFFER_START, blockSize);
+        if (bytesRead == 0) {
+            // The delegate input stream has been exhausted, so we notify
+            // the compressing stream that there are no remaining bytes.
+            // Lastly, we flush the LZ4 footer into the internal buffer.
+            compressingStream.finish();
+            compressingStream.flush();
+            finished = true;
+            return true;
+        }
+
+        // Write the bytes to the compressing stream and flush it.
+        // The compressing stream flushes bytes when it contains more
+        // than blockSize bytes, so we manually flush after writing up to
+        // blockSize bytes to flush the compressed bytes to the internal
+        // buffer. The readInProgress flag protects against simultaneously
+        // reading and writing to the same buffer. The write call will read
+        // from the buffer but should not write to it - only the flush call
+        // should.
+        readInProgress = true;
+        compressingStream.write(buffer, BUFFER_START, bytesRead);
+        readInProgress = false;
+        compressingStream.flush();
+
+        return true;
+    }
+
+    private void write(int b) throws IOException {
+        ensureSafeForWrite();
+        ensureCapacity(bufferSize + 1);
+        buffer[bufferSize] = (byte) b;
+        bufferSize++;
+    }
+
+    private void write(byte b[], int off, int len) throws IOException {
+        ensureSafeForWrite();
+        if (b == null) {
+            throw new NullPointerException();
+        }
+        if ((off < 0) || (len < 0) || (off + len > b.length)) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (len == 0) {
+            return;
+        }
+        ensureCapacity(bufferSize + len);
+        System.arraycopy(b, off, buffer, bufferSize, len);
+        bufferSize += len;
+    }
+
+    private void ensureSafeForWrite() {
+        Preconditions.checkState(!readInProgress, "Cannot simultaneously read and write from the buffer.");
+    }
+
+    // Since the internal buffer size doesn't change, we throw if
+    // someone tries to write past the end of the buffer.
+    private void ensureCapacity(int size) {
+        Preconditions.checkState(buffer.length >= size, "Internal buffer overflow");
+    }
+
+    @Override
+    public void close() throws IOException {
+        delegate.close();
+        compressingStream.close();
+    }
+
+    /**
+     * Internal {@link OutputStream} that wraps the pre-existing buffer. This
+     * is an inner class since LZ4CompressingInputStream cannot extend both
+     * {@link BufferedDelegateInputStream} and {@link OutputStream}.
+     */
+    private final class InternalByteArrayOutputStream extends OutputStream {
+
+        public InternalByteArrayOutputStream() {
+            super();
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            LZ4CompressingInputStream.this.write(b);
+        }
+
+        @Override
+        public void write(byte b[], int off, int len) throws IOException {
+            LZ4CompressingInputStream.this.write(b, off, len);
+        }
+    }
+
+}
