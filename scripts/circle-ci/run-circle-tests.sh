85e613956c43ae07abc33b6c77cbf5aabf53e38a
Ben Grabham <bgrabham@palantir.com>
2016-12-13 18:02:25 +0000

    Re-balance nodes

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -14,14 +14,16 @@ CONTAINER_1=(':atlasdb-cassandra-integration-tests:check')
 
 CONTAINER_2=(':atlasdb-ete-tests:check')
 
-CONTAINER_3=(':atlasdb-ete-test-utils:check' ':atlasdb-cassandra:check' ':atlasdb-api:check' ':atlasdb-jepsen-tests:check' ':atlasdb-cassandra-integration-tests:longTest')
+CONTAINER_3=(':atlasdb-cassandra-integration-tests:longTest')
 
 CONTAINER_4=(':atlasdb-dbkvs:check' ':atlasdb-cassandra-multinode-tests:check' ':atlasdb-impl-shared:check' ':atlasdb-dropwizard-bundle:check')
 
 CONTAINER_5=(':atlasdb-ete-tests:longTest' ':lock-impl:check' ':atlasdb-dbkvs-tests:check' ':atlasdb-tests-shared:check')
 
+CONTAINER_6=(':atlasdb-ete-test-utils:check' ':atlasdb-cassandra:check' ':atlasdb-api:check' ':atlasdb-jepsen-tests:check')
+
 # Container 0 - runs tasks not found in the below containers
-CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}" "${CONTAINER_5[@]}")
+CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}" "${CONTAINER_5[@]}" "${CONTAINER_6[@]}")
 
 for task in "${CONTAINER_0_EXCLUDE[@]}"
 do
@@ -50,8 +52,8 @@ case $CIRCLE_NODE_INDEX in
     0) ./gradlew $BASE_GRADLE_ARGS $TEST_CONTAINER_ARGS check $CONTAINER_0_EXCLUDE_ARGS ;;
     1) ./gradlew $BASE_GRADLE_ARGS $TEST_CONTAINER_ARGS ${CONTAINER_1[@]} -x :atlasdb-cassandra-integration-tests:longTest ;;
     2) ./gradlew $BASE_GRADLE_ARGS $TEST_CONTAINER_ARGS ${CONTAINER_2[@]} -x :atlasdb-ete-tests:longTest ;;
-    3) ./gradlew $BASE_GRADLE_ARGS $TEST_CONTAINER_ARGS ${CONTAINER_3[@]} -x :atlasdb-jepsen-tests:jepsenTest ;;
+    3) ./gradlew $BASE_GRADLE_ARGS $TEST_CONTAINER_ARGS ${CONTAINER_3[@]} ;;
     4) ./gradlew $BASE_GRADLE_ARGS $TEST_CONTAINER_ARGS ${CONTAINER_4[@]} ;;
     5) ./gradlew $BASE_GRADLE_ARGS $TEST_CONTAINER_ARGS ${CONTAINER_5[@]} ;;
-    #6) ./gradlew $BASE_GRADLE_ARGS findbugsMain findbugsTest checkstyleMain checkstyleTest && checkDocsBuild ;;
+    6) ./gradlew $BASE_GRADLE_ARGS $TEST_CONTAINER_ARGS ${CONTAINER_6[@]} -x :atlasdb-jepsen-tests:jepsenTest && checkDocsBuild ;;
 esac
