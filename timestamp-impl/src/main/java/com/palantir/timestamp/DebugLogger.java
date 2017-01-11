d587a01cbf88afa6eca1af2cb83a5d6ee9844cac
Himangi Saraogi <hsaraogi@palantir.com>
2017-01-11 17:04:27 +0000

    Cleanup

--- timestamp-impl/src/main/java/com/palantir/timestamp/DebugLogger.java
+++ timestamp-impl/src/main/java/com/palantir/timestamp/DebugLogger.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.palantir.timestamp.impl;
+package com.palantir.timestamp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- timestamp-impl/src/main/java/com/palantir/timestamp/LastReturnedTimestamp.java
+++ timestamp-impl/src/main/java/com/palantir/timestamp/LastReturnedTimestamp.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.palantir.timestamp.impl;
+package com.palantir.timestamp;
 
 public class LastReturnedTimestamp {
     private long timestamp;
