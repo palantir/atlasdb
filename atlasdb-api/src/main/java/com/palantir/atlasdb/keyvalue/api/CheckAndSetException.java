2ea7550b256b446c297f108585bf52e4812cf7f2
Glenn Sheasby <gsheasby@palantir.com>
2017-01-18 21:26:01 +0000

    CheckAndSetExcpetion: log more detail than in the exception message
    Because we don't want the content in the log message to leak customer
    data (even if hex-encoded), via the exception message and another log
    message.

--- atlasdb-api/src/main/java/com/palantir/atlasdb/keyvalue/api/CheckAndSetException.java
+++ atlasdb-api/src/main/java/com/palantir/atlasdb/keyvalue/api/CheckAndSetException.java
@@ -18,9 +18,14 @@ package com.palantir.atlasdb.keyvalue.api;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.palantir.atlasdb.encoding.PtBytes;
 
 public class CheckAndSetException extends RuntimeException {
+    private static final Logger log = LoggerFactory.getLogger(CheckAndSetException.class);
+
     private static final long serialVersionUID = 1L;
 
     private final Cell key;
@@ -57,10 +62,10 @@ public class CheckAndSetException extends RuntimeException {
     }
 
     private static String getExceptionMessage(Cell key, TableReference table, byte[] expected, List<byte[]> actual) {
-        String template = "The cell %s in table %s has an unexpected value. "
-                + "Expected '%s' but got '%s'. "
-                + "If this is happening repeatedly, your program may be out of sync with the database.";
-        return String.format(template, key, table, PtBytes.encodeHexString(expected), encodeHexStrings(actual));
+        String repeatWarning = "If this is happening repeatedly, your program may be out of sync with the database.";
+        log.error("The cell {} in table {} has an unexpected value. Expected '{}' but got '{}'. {}",
+                key, table, PtBytes.encodeHexString(expected), encodeHexStrings(actual), repeatWarning);
+        return String.format("Unexpected value observed in table %s. %s", table, repeatWarning);
     }
 
     private static List<String> encodeHexStrings(List<byte[]> values) {
