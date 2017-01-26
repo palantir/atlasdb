05da34d7e3a0507b144fc108f0ef6edc63065940
Glenn Sheasby <gsheasby@palantir.com>
2017-01-26 11:19:25 +0000

    Javadoc for PersistentLockService

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/persistentlock/PersistentLockService.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/persistentlock/PersistentLockService.java
@@ -22,6 +22,10 @@ import com.google.common.annotations.VisibleForTesting;
 import com.palantir.atlasdb.keyvalue.api.CheckAndSetException;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 
+/**
+ * Provides endpoints for acquiring and releasing the Deletion Lock. This is intended to be used by backups and sweep,
+ * to ensure that only one of these operations is deleting data at once.
+ */
 @Path("/persistent-lock")
 public class PersistentLockService {
     private final LockStore lockStore;
@@ -36,11 +40,25 @@ public class PersistentLockService {
         return new PersistentLockService(lockStore);
     }
 
+    /**
+     * Attempt to acquire the lock.
+     * Call this method before performing any destructive operations.
+     * @param reason the reason for the lock, for logging purposes (e.g. "sweep")
+     * @return a {@link LockEntry} on success. The LockEntry will contain the given reason, and a unique ID. It is
+     *   essential that you retain a reference to this lock, as you will need it in order to release the lock.
+     * @throws CheckAndSetException if the lock was already taken.
+     */
     @Path("acquire")
     public LockEntry acquireLock(@PathParam("reason") String reason) throws CheckAndSetException {
         return lockStore.acquireLock(reason);
     }
 
+    /**
+     * Release a lock that you have previously acquired.
+     * Call this method as soon as you no longer need the lock (e.g. because you finished deleting stuff).
+     * @param lockEntry the {@link LockEntry} you were given when you called {@link #acquireLock(String)}
+     * @throws CheckAndSetException if the lock was no longer valid, most likely because it was already released.
+     */
     @Path("release")
     public void releaseLock(LockEntry lockEntry) throws CheckAndSetException {
         lockStore.releaseLock(lockEntry);
