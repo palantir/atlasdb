f6a66e316bdd4a1ea3ac0757f50f7c6bf0516cec
Jeremy Kong <jeremykong@hotmail.com>
2017-01-27 13:32:23 +0000

    Refactor C* timestamp store invalidator

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTimestampStoreInvalidator.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTimestampStoreInvalidator.java
@@ -15,17 +15,8 @@
  */
 package com.palantir.atlasdb.keyvalue.cassandra;
 
-import java.nio.ByteBuffer;
 import java.util.Map;
 import java.util.Optional;
-
-import javax.xml.bind.DatatypeConverter;
-
-import org.apache.cassandra.thrift.Compression;
-import org.apache.cassandra.thrift.ConsistencyLevel;
-import org.apache.thrift.TException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
@@ -33,81 +24,57 @@ import com.palantir.atlasdb.AtlasDbConstants;
 import com.palantir.atlasdb.encoding.PtBytes;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.Value;
-import com.palantir.common.base.Throwables;
 import com.palantir.timestamp.TimestampStoreInvalidator;
 
 public class CassandraTimestampStoreInvalidator implements TimestampStoreInvalidator {
-    private static final Logger log = LoggerFactory.getLogger(CassandraTimestampStoreInvalidator.class);
-    private static final long CASSANDRA_TIMESTAMP = 0L;
-
-    private static final String ROW_AND_COLUMN_NAME = "ts";
-    private static final byte[] ROW_AND_COLUMN_NAME_BYTES = PtBytes.toBytes(ROW_AND_COLUMN_NAME);
-    private static final Cell TIMESTAMP_CELL = Cell.create(ROW_AND_COLUMN_NAME_BYTES, ROW_AND_COLUMN_NAME_BYTES);
-
-    private static final String BACKUP_COLUMN_NAME = "oldTs";
-    private static final byte[] BACKUP_COLUMN_NAME_BYTES = PtBytes.toBytes(BACKUP_COLUMN_NAME);
-
-    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+    private static final long DEFAULT_TIMESTAMP_BOUND = 0L;
 
     private final CassandraKeyValueService rawCassandraKvs;
+    private final CassandraTimestampCqlExecutor cassandraTimestampCqlExecutor;
 
     public CassandraTimestampStoreInvalidator(CassandraKeyValueService rawCassandraKvs) {
         this.rawCassandraKvs = rawCassandraKvs;
+        this.cassandraTimestampCqlExecutor = new CassandraTimestampCqlExecutor(rawCassandraKvs);
     }
 
     @Override
     public void invalidateTimestampStore() {
         Optional<Long> bound = getCurrentTimestampBound();
-        bound.ifPresent(this::backupBound);
-    }
-
-    private Optional<Long> getCurrentTimestampBound() {
-        Map<Cell, Value> result = rawCassandraKvs.get(
-                AtlasDbConstants.TIMESTAMP_TABLE,
-                ImmutableMap.of(TIMESTAMP_CELL, Long.MAX_VALUE));
-        Value value = Iterables.getOnlyElement(result.values(), Value.create(PtBytes.toBytes(0L), CASSANDRA_TIMESTAMP));
-        try {
-            return Optional.of(PtBytes.toLong(value.getContents()));
-        } catch (IllegalArgumentException exception) {
-            // This can occur if the timestamp table was already invalidated.
-            return Optional.empty();
-        }
-    }
-
-    private void backupBound(long bound) {
-        rawCassandraKvs.clientPool.runWithRetry(client -> {
-            try {
-                String queryString = "BEGIN BATCH\n" +
-                        constructCqlInsertCommandForTimestampTable(BACKUP_COLUMN_NAME_BYTES, PtBytes.toBytes(bound)) +
-                        constructCqlInsertCommandForTimestampTable(ROW_AND_COLUMN_NAME_BYTES, EMPTY_BYTE_ARRAY) +
-                        "APPLY BATCH;";
-                ByteBuffer query = ByteBuffer.wrap(PtBytes.toBytes(queryString));
-                client.execute_cql3_query(query, Compression.NONE, ConsistencyLevel.QUORUM);
-            } catch (TException e) {
-                log.error("An error occurred whilst trying to write to Cassandra:", e);
-                throw Throwables.rewrapAndThrowUncheckedException(e);
-            }
-            return null;
-        });
-    }
-
-    private String constructCqlInsertCommandForTimestampTable(byte[] rowAndColumnName, byte[] value) {
-        String hexName = encodeCassandraHexValue(rowAndColumnName);
-        String hexValue = encodeCassandraHexValue(value);
-        return String.format(
-                "INSERT INTO %s (key, column1, column2, value) VALUES (%s, %s, -1, %s);\n",
-                "\"" + AtlasDbConstants.TIMESTAMP_TABLE.getQualifiedName() + "\"",
-                hexName,
-                hexName,
-                hexValue);
-    }
-
-    private String encodeCassandraHexValue(byte[] value) {
-        return "0x" + DatatypeConverter.printHexBinary(value);
+        bound.ifPresent(cassandraTimestampCqlExecutor::backupBound);
     }
 
     @Override
     public void revalidateTimestampStore() {
+        Optional<Long> bound = getBackupTimestampBound();
+        bound.ifPresent(cassandraTimestampCqlExecutor::restoreBoundFromBackup);
+    }
 
+    private Optional<Long> getCurrentTimestampBound() {
+        return getBoundFromKvs(CassandraTimestampCqlExecutor.ROW_AND_COLUMN_NAME_BYTES);
+    }
+
+    private Optional<Long> getBackupTimestampBound() {
+        return getBoundFromKvs(CassandraTimestampCqlExecutor.BACKUP_COLUMN_NAME_BYTES);
+    }
+
+    private Optional<Long> getBoundFromKvs(byte[] timestampCellName) {
+        Cell timestampCell = Cell.create(timestampCellName, timestampCellName);
+        Map<Cell, Value> result = rawCassandraKvs.get(
+                AtlasDbConstants.TIMESTAMP_TABLE,
+                ImmutableMap.of(timestampCell, Long.MAX_VALUE));
+        if (result.isEmpty()) {
+            return Optional.of(DEFAULT_TIMESTAMP_BOUND);
+        }
+
+        Value value = Iterables.getOnlyElement(result.values());
+        return getLongFromValue(value);
+    }
+
+    private Optional<Long> getLongFromValue(Value value) {
+        byte[] contents = value.getContents();
+        if (contents.length == 0) {
+            return Optional.empty();
+        }
+        return Optional.of(PtBytes.toLong(contents));
     }
 }
