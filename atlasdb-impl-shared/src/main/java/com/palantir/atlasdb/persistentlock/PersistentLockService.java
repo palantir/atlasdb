c149a610a10a8e7f21b8603cb7569efb41e17e63
Glenn Sheasby <gsheasby@palantir.com>
2017-02-01 17:10:45 +0000

    Fix PersistentLockServiceTest
    As a bonus, the user gets a nice little message when they release a
    lock successfully.

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/persistentlock/PersistentLockService.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/persistentlock/PersistentLockService.java
@@ -15,24 +15,19 @@
  */
 package com.palantir.atlasdb.persistentlock;
 
-import java.util.List;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Iterables;
 import com.palantir.atlasdb.keyvalue.api.CheckAndSetException;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 
@@ -62,7 +57,6 @@ public class PersistentLockService {
      * @param reason the reason for the lock, for logging purposes (e.g. "sweep")
      * @return a {@link LockEntry} on success. The LockEntry will contain the given reason, and a unique ID. It is
      *   essential that you retain a reference to this lock, as you will need it in order to release the lock.
-     * @throws CheckAndSetException if the lock was already taken.
      */
     @GET
     @Path("acquire/{reason}")
@@ -76,18 +70,19 @@ public class PersistentLockService {
      * Release a lock that you have previously acquired.
      * Call this method as soon as you no longer need the lock (e.g. because you finished deleting stuff).
      * @param lockEntry the {@link LockEntry} you were given when you called {@link #acquireLock(String)}
-     * @return OK (200) if the lock was successfully released, or CONFLICT (409) if there was a conflict.
+     * @return A success message if the lock was successfully released
+     * @throws CheckAndSetException if there was a conflict.
      */
     @POST
     @Path("release")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response releaseLock(LockEntry lockEntry) {
+    public String releaseLock(LockEntry lockEntry) throws CheckAndSetException {
         Preconditions.checkNotNull(lockEntry, "Please provide a LockEntry to release.");
 
         try {
             lockStore.releaseLock(lockEntry);
-            return Response.ok().build();
+            return "The lock was released successfully.\n";
         } catch (CheckAndSetException e) {
             log.error("Failed to release the persistent lock. This means that somebody already cleared this lock. "
                     + "You should investigate this, as this means your operation didn't necessarily hold the lock when "
