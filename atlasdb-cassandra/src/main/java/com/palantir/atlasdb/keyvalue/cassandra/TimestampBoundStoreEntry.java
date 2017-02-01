c31930556f07a7b4d182fc5e3764cd6593a8c4f0
Grgur Petric Maretic <gmaretic@palantir.com>
2017-02-01 18:01:22 +0000

    Extracted Entry class, still plan to add some unit tests, but should be good to go (once Jeremy massacres my style)

---
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/TimestampBoundStoreEntry.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2017 Palantir Technologies
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.atlasdb.keyvalue.cassandra;
+
+import java.util.UUID;
+
+import org.apache.cassandra.thrift.CASResult;
+import org.apache.cassandra.thrift.Column;
+import org.apache.commons.lang3.ArrayUtils;
+
+import com.google.common.collect.Iterables;
+import com.palantir.atlasdb.encoding.PtBytes;
+import com.palantir.atlasdb.table.description.ValueType;
+
+public final class TimestampBoundStoreEntry {
+    private final UUID id;
+    private final long timestamp;
+
+    protected TimestampBoundStoreEntry(UUID uuid, long timestamp) {
+        this.id = uuid;
+        this.timestamp = timestamp;
+    }
+
+    protected static TimestampBoundStoreEntry createFromBytes(byte[] values) {
+        if (values.length > 8) {
+            return new TimestampBoundStoreEntry((UUID) ValueType.UUID.convertToJava(values, 0),
+                    PtBytes.toLong(values, ValueType.UUID.sizeOf(null)));
+        } else {
+            return new TimestampBoundStoreEntry(null, PtBytes.toLong(values));
+        }
+    }
+
+    protected static TimestampBoundStoreEntry createFromColumn(Column column) {
+        return createFromBytes(column.getValue());
+    }
+
+    protected static TimestampBoundStoreEntry createFromCasResult(CASResult result) {
+        return createFromColumn(Iterables.getOnlyElement(result.getCurrent_values()));
+    }
+
+    protected static byte[] getByteValueForIdAndBound(UUID id, Long ts) {
+        if (ts == null) {
+            return null;
+        }
+        return (new TimestampBoundStoreEntry(id, ts)).getByteValue();
+    }
+
+    protected byte[] getByteValue() {
+        if (!hasId()) {
+            return PtBytes.toBytes(timestamp);
+        }
+        return ArrayUtils.addAll(ValueType.UUID.convertFromJava(id), PtBytes.toBytes(timestamp));
+    }
+
+    protected long getTimestamp() {
+        return timestamp;
+    }
+
+    protected boolean hasId() {
+        return id != null;
+    }
+
+    protected UUID getId() {
+        return id;
+    }
+}
+
