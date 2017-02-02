b4e8d428105ee83e3ccec7e2dfb94d6bca3b1525
Grgur Petric Maretic <gmaretic@palantir.com>
2017-02-02 23:11:31 +0000

    Made it work

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/backend/DockerizedDatabase.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/backend/DockerizedDatabase.java
@@ -27,6 +27,7 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 
 import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.configuration.ShutdownStrategy;
 import com.palantir.docker.compose.connection.DockerPort;
 import com.palantir.docker.compose.connection.waiting.HealthCheck;
 import com.palantir.docker.compose.connection.waiting.SuccessOrFailure;
@@ -35,7 +36,7 @@ public final class DockerizedDatabase implements Closeable {
 
     private static final String DOCKER_LOGS_DIR = "container-logs";
 
-    public static DockerizedDatabase start(KeyValueServiceType type) {
+    public static DockerizedDatabase start(KeyValueServiceInstrumentation type) {
         DockerComposeRule docker = DockerComposeRule.builder()
                 .file(getDockerComposeFileAbsolutePath(type.getDockerComposeResourceFileName()))
                 .waitingForHostNetworkedPort(type.getKeyValueServicePort(), toBeOpen())
