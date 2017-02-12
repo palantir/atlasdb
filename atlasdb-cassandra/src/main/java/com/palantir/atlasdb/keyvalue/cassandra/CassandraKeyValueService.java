a7b4b0711f62d4ae4255a23fb0e89c6c95e413c8
Grgur Petric Maretic <gmaretic@palantir.com>
2017-02-12 23:07:42 +0000

    Used new getter

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueService.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueService.java
@@ -145,7 +145,7 @@ public class CassandraKeyValueService extends AbstractKeyValueService {
 
     protected final CassandraKeyValueServiceConfigManager configManager;
     private final Optional<CassandraJmxCompactionManager> compactionManager;
-    protected final CassandraClientPool clientPool;
+    private final CassandraClientPool clientPool;
     private SchemaMutationLock schemaMutationLock;
     private final Optional<LeaderConfig> leaderConfig;
     private final HiddenTables hiddenTables;
