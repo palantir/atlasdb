9ef11eb126f9fb96e06b1d6f5cef467ef0f6cec1
Grgur Petric Maretic <gmaretic@palantir.com>
2017-02-14 18:37:35 +0000

    Current version

---
+++ atlasdb-jepsen-tests/src/jepsen/atlasdb.clj
@@ -0,0 +1,124 @@
+(ns jepsen.atlasdb
+  (:require [cheshire.core :as json]
+            [clj-http.client :as http]
+            [clojure.tools.logging :refer :all]
+            [jepsen.core :as jepsen]
+            [jepsen.checker :as checker]
+            [jepsen.checker.timeline :as timeline]
+            [jepsen.client :as client]
+            [jepsen.control :as c]
+            [jepsen.db :as db]
+            [jepsen.generator :as gen]
+            [jepsen.model :as model]
+            [jepsen.nemesis :as nemesis]
+            [jepsen.os.debian :as debian]
+            [jepsen.tests :as tests])
+  (:import com.palantir.atlasdb.AtlasDbEteServer))
+
+(def CASSANDRA_LOG_FILES ["/var/log/cassandra/system.log",
+                          "/var/log/cassandra/debug.log"])
+
+(def CASSANDRA_VERSION "2.2.8")
+
+(defn install-cassandra
+  [node version]
+  (c/su
+   (info node "installing Cassandra" version)
+   (debian/add-repo!
+    "cassandra"
+    "deb http://www.apache.org/dist/cassandra/debian 22x main")
+   (debian/install {:cassandra version})
+   (info node "starting Cassandra")
+   (c/upload "resources/cassandra/cassandra.yaml.template" "/etc/cassandra/")
+   (c/exec :sed (c/lit "\"s/{{LOCAL_ADDRESS}}/$(hostname -I)/g\"") "/etc/cassandra/cassandra.yaml.template" :> "/etc/cassandra/cassandra.yaml")
+   (c/upload "resources/cassandra/cassandra-env.sh" "/etc/cassandra/")
+   (c/exec :service :cassandra :start)
+   (info node "Cassandra is ready")))
+
+(defn teardown-cassandra
+  [node]
+  (c/su
+   (info node "tearing down Cassandra")
+   (c/exec :service :cassandra :stop)
+   (c/exec :rm "-rf" "/var/lib/cassandra/{saved_caches,data,commitlog}")))
+
+(defn db
+  "AtlasDB node setup."
+  []
+  (reify db/DB
+    (setup! [_ test node]
+        (install-cassandra node CASSANDRA_VERSION))
+
+    (teardown! [_ test node]
+        (teardown-cassandra node))
+
+    db/LogFiles
+    (log-files [_ test node]
+         CASSANDRA_LOG_FILES)))
+
+(defn atlasdb-get [node]
+  (json/parse-string (:body (http/get (format "http://%s:3828/cas" (name node)) {:content-type :json}))))
+
+(defn atlasdb-put! [node new-value]
+  (let [contents (json/generate-string new-value)]
+    (http/put (format "http://%s:3828/cas" (name node)) {:content-type :json :body contents})))
+
+(defn atlasdb-cas! [node old-value new-value]
+  (let [contents (json/generate-string {:oldValue old-value :newValue new-value})]
+    (json/parse-string (:body (http/patch (format "http://%s:3828/cas" (name node)) {:content-type :json :body contents})))))
+
+(defn create-client
+  "Creates an object that implements the client/Client protocol.
+   The object defines how you create a lock client, and how to request locks from it. The first call to this
+   function will return an invalid object: you should call 'setup' on the returned object to get a valid one.
+  "
+  [client-name]
+  (reify client/Client
+    (setup!
+      [this test node]
+      "Factory that returns an object implementing client/Client"
+      (AtlasDbEteServer/main "server" "resources/atlasdb/atlasdb-ete.yml.template")
+      (create-client
+        (name node)))
+
+    (invoke! [this test op]
+      (case (:f op)
+        :read (try (assoc op :type :ok :value (atlasdb-get node))
+                (catch Exception e
+                  (warn e "Read failed")
+                  (assoc op :type :fail)))
+
+        :write (do (atlasdb-put! node (:value op))
+                 (assoc op :type :ok))
+        :cas (let [[value value'] (:value op)
+                   ok? (atlasdb-cas! node value value')]
+               (assoc op :type (if ok? :ok :fail)))))
+
+    (teardown! [_ test])))
+
+(defn r   [_ _] {:type :invoke, :f :read, :value nil})
+(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
+
+(defn atlasdb-test
+  []
+  (assoc tests/noop-test
+         :name "atlasdb"
+         :nodes ["n1"]
+         :os debian/os
+         :db (db)
+         :client (create-client nil)
+         :nemesis (nemesis/partition-random-halves)
+         :model (model/cas-register)
+         :checker (checker/compose
+                   {:html (timeline/html)
+                    :perf (checker/perf)
+                    :linear checker/linearizable})
+         :generator (->> (gen/mix [r w cas])
+                         (gen/stagger 1)
+                         (gen/nemesis
+                          (gen/seq (cycle [(gen/sleep 5)
+                                           {:type :info, :f :start}
+                                           (gen/sleep 5)
+                                           {:type :info, :f :stop}])))
+                         (gen/time-limit 15))))
--- atlasdb-jepsen-tests/test/jepsen/atlasdb_test.clj
+++ atlasdb-jepsen-tests/test/jepsen/atlasdb_test.clj
@@ -1,14 +1,18 @@
 (ns jepsen.atlasdb-test
   (:require [clojure.test :refer :all]
-            [jepsen.atlasdb.lock :as lock]
-            [jepsen.atlasdb.timestamp :as timestamp]
+;            [jepsen.atlasdb.lock :as lock]
+;            [jepsen.atlasdb.timestamp :as timestamp]
+            [jepsen.atlasdb :as test]
             [jepsen.core :as jepsen]))
 
 ;; Using Clojure's testing framework, we initiate our test runs
 ;; Tests successful iff the value for the key ":valid?" is truthy
 
-(deftest timestamp-test
-   (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test))))))
+;(deftest timestamp-test
+;   (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test))))))
+;
+;(deftest lock-test
+;   (is (:valid? (:results (jepsen/run! (lock/lock-test))))))
 
-(deftest lock-test
-   (is (:valid? (:results (jepsen/run! (lock/lock-test))))))
+(deftest cassandra-test
+  (is (:valid? (:results (jepsen/run! (test/atlasdb-test))))))
