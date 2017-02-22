b612ae97ad3fb8e8f22f8f871a207fce4dadd017
Jeremy Kong <jeremykong@hotmail.com>
2017-02-22 17:51:41 +0000

    Baseline

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/PerformanceResults.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/PerformanceResults.java
@@ -63,25 +63,18 @@ public class PerformanceResults {
 
     private static List<ImmutablePerformanceResult> getPerformanceResults(Collection<RunResult> results) {
         long date = System.currentTimeMillis();
-        return results.stream().flatMap(rs -> {
-                    try {
-                        return Stream.of(ImmutablePerformanceResult.builder()
-                                .date(date)
-                                .benchmark(getBenchmarkName(rs.getParams()))
-                                .samples(rs.getPrimaryResult().getStatistics().getN())
-                                .std(rs.getPrimaryResult().getStatistics().getStandardDeviation())
-                                .mean(rs.getPrimaryResult().getStatistics().getMean())
-                                .data(getData(rs))
-                                .units(rs.getParams().getTimeUnit())
-                                .p50(rs.getPrimaryResult().getStatistics().getPercentile(50.0))
-                                .p90(rs.getPrimaryResult().getStatistics().getPercentile(90.0))
-                                .p99(rs.getPrimaryResult().getStatistics().getPercentile(99.0))
-                                .build());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        return Stream.of();
-                    }
-                }).collect(Collectors.toList());
+        return results.stream().flatMap(rs -> Stream.of(ImmutablePerformanceResult.builder()
+                .date(date)
+                .benchmark(getBenchmarkName(rs.getParams()))
+                .samples(rs.getPrimaryResult().getStatistics().getN())
+                .std(rs.getPrimaryResult().getStatistics().getStandardDeviation())
+                .mean(rs.getPrimaryResult().getStatistics().getMean())
+                .data(getData(rs))
+                .units(rs.getParams().getTimeUnit())
+                .p50(rs.getPrimaryResult().getStatistics().getPercentile(50.0))
+                .p90(rs.getPrimaryResult().getStatistics().getPercentile(90.0))
+                .p99(rs.getPrimaryResult().getStatistics().getPercentile(99.0))
+                .build())).collect(Collectors.toList());
     }
 
     @VisibleForTesting
