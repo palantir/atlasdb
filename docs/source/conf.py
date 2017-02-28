b6bbe94317b41ba25a176799c5a1e251ceeb6927
John Boreiko <jboreiko@users.noreply.github.com>
2017-02-28 13:52:00 +0100

    Add adrs to docs and remove them from the base of the repo (#1655)
    * Add adrs to docs and remove them from the base of the repo [no release notes]
    
    * Update contributing docs to reflect additional dep

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@ import sys
 import os
 import shlex
 
+from recommonmark.parser import CommonMarkParser
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -35,13 +37,17 @@ extensions = [
     'code-block-with-version-replacement'
 ]
 
+# Included 
+source_parsers = {
+  '.md': CommonMarkParser,
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
