799416f9b21726b0322c8710976f5cbc73dee27e
Jeremy Kong <jeremykong@hotmail.com>
2017-02-28 09:15:40 +0000

    Fix the build

--- scripts/circle-ci/install-docker.sh
+++ scripts/circle-ci/install-docker.sh
@@ -2,9 +2,11 @@
 
 set -x
 
-MIN_VERSION_WANTED=1.12.0
+MIN_VERSION_WANTED=1.12.3
 CURRENT_VERSION=$(docker version --format '{{.Client.Version}}')
 
 if [ "$MIN_VERSION_WANTED" != "$(echo -e "$CURRENT_VERSION\n$MIN_VERSION_WANTED" | sort -V | head -n 1)" ]; then
-    curl -sSL https://s3.amazonaws.com/circle-downloads/install-circleci-docker.sh | bash -s -- "$MIN_VERSION_WANTED"
+#    curl -sSL https://s3.amazonaws.com/circle-downloads/install-circleci-docker.sh | bash -s -- "$MIN_VERSION_WANTED"
+    curl -fsSLO "https://get.docker.com/builds/Linux/x86_64/docker-1.13.1.tgz" && sudo tar --strip-components=1 -xvzf docker-1.13.1.tgz -C /usr/bin
+    rm docker-1.13.1.tgz
 fi
