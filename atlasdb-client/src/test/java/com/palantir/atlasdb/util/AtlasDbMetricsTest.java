a762c16b8d6d7c472f103698d7f7cd5194f6cb53
David Schlosnagle <davids@palantir.com>
2017-03-06 17:47:53 -0500

    Basic AtlasDbMetrics unit tests

--- atlasdb-client/src/test/java/com/palantir/atlasdb/util/AtlasDbMetricsTest.java
+++ atlasdb-client/src/test/java/com/palantir/atlasdb/util/AtlasDbMetricsTest.java
@@ -20,12 +20,29 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.SharedMetricRegistries;
+import com.palantir.tritium.metrics.MetricRegistries;
 
 public class AtlasDbMetricsTest {
+
+    @Before
+    public void before() throws Exception {
+        AtlasDbMetrics.metrics = null;
+    }
+
+    @After
+    public void after() throws Exception {
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(AtlasDbMetrics.getMetricRegistry()).build();
+        reporter.report();
+        reporter.close();
+    }
+
     @Test
     public void metricsIsDefaultWhenNotSet() {
         AtlasDbMetrics.metrics = null;
@@ -44,4 +61,39 @@ public class AtlasDbMetricsTest {
     public void nullMetricsCannotBeSet() {
         AtlasDbMetrics.setMetricRegistry(null);
     }
+
+    @Test
+    public void instrumentWithDefaultName() throws Exception {
+        MetricRegistry metrics = MetricRegistries.createWithHdrHistogramReservoirs();
+        AtlasDbMetrics.setMetricRegistry(metrics);
+        TestService service = AtlasDbMetrics.instrument(TestService.class, () -> "pong");
+
+        String methodTimerName = MetricRegistry.name(TestService.class, "ping");
+
+        assertThat(metrics.timer(methodTimerName).getCount(), is(equalTo(0L)));
+
+        assertThat(service.ping(), is(equalTo("pong")));
+
+        assertThat(metrics.timer(methodTimerName).getCount(), is(equalTo(1L)));
+    }
+
+    @Test
+    public void instrumentWithCustomName() throws Exception {
+        MetricRegistry metrics = MetricRegistries.createWithHdrHistogramReservoirs();
+        AtlasDbMetrics.setMetricRegistry(metrics);
+        TestService service = AtlasDbMetrics.instrument(TestService.class, () -> "pong", "foo");
+
+        String methodTimerName = "foo.ping";
+
+        assertThat(metrics.timer(methodTimerName).getCount(), is(equalTo(0L)));
+
+        assertThat(service.ping(), is(equalTo("pong")));
+
+        assertThat(metrics.timer(methodTimerName).getCount(), is(equalTo(1L)));
+    }
+
+    public interface TestService {
+        String ping();
+    }
+
 }
