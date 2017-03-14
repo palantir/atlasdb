fb3c67179553a29f3b878f27e18ad48d43bfcbf8
Greg Bonik <gbonik@palantir.com>
2017-03-14 16:10:23 -0700

    Add explicit column names to make UNION ALL happy
    Also remove the 'includeValues' arg from getValueSubselectForGroupBy

--- atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/oracle/OracleQueryFactory.java
+++ atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/oracle/OracleQueryFactory.java
@@ -353,15 +353,16 @@ public abstract class OracleQueryFactory extends AbstractDbQueryFactory {
             long ts,
             BatchColumnRangeSelection columnRangeSelection) {
         String query = " /* GET_ROWS_COLUMN_RANGE (" + tableName + ") */ "
-                + "SELECT * FROM ( SELECT m.row_name, m.col_name, max(m.ts) as ts"
-                + getValueSubselectForGroupBy("m", true)
+                + "SELECT s.row_name, s.col_name, s.ts" + getValueSubselect("s", true)
+                + " FROM ( SELECT m.row_name, m.col_name, max(m.ts) as ts"
+                +         getValueSubselectForGroupBy("m")
                 + "   FROM " + tableName + " m"
                 + "  WHERE m.row_name = ?"
                 + "    AND m.ts < ? "
                 + (columnRangeSelection.getStartCol().length > 0 ? " AND m.col_name >= ?" : "")
                 + (columnRangeSelection.getEndCol().length > 0 ? " AND m.col_name < ?" : "")
                 + " GROUP BY m.row_name, m.col_name"
-                + " ORDER BY m.row_name ASC, m.col_name ASC ) WHERE rownum <= " + columnRangeSelection.getBatchHint();
+                + " ORDER BY m.row_name ASC, m.col_name ASC ) s WHERE rownum <= " + columnRangeSelection.getBatchHint();
         FullQuery fullQuery = new FullQuery(query).withArg(row).withArg(ts);
         if (columnRangeSelection.getStartCol().length > 0) {
             fullQuery = fullQuery.withArg(columnRangeSelection.getStartCol());
@@ -426,31 +427,27 @@ public abstract class OracleQueryFactory extends AbstractDbQueryFactory {
         }
     }
 
-    private String getValueSubselectForGroupBy(String tableAlias, boolean includeValue) {
-        if (includeValue) {
-            List<String> colNames = getValueColumnNames();
-            StringBuilder ret = new StringBuilder(70 * colNames.size());
-            for (String colName : colNames) {
-                // E.g., ", MAX(m.val) KEEP (DENSE_RANK LAST ORDER BY m.ts ASC) AS val".
-                // How this works, assuming we have a "GROUP BY row_name, col_name" clause:
-                //  1) Among each group of rows with the same (row_name, col_name) pair,
-                //     "KEEP (DENSE_RANK LAST ORDER BY m.ts ASC)" will select the subset
-                //     of rows with the biggest 'ts' value. Since (row_name, col_name, ts)
-                //     is the primary key, that subset will always contain exactly one
-                //     element in our case.
-                //  2) For that subset of rows, we take an aggregate function "MAX(m.val)".
-                //     It doesn't make a difference which function we use since our subset
-                //     is a singleton, so MAX will simply return the only element in that set.
-                //  To sum it up: for each (row_name, col_name) group, this will select
-                //  the value of the row that has the largest 'ts' value within that group.
-                ret.append(", MAX(").append(tableAlias).append('.').append(colName)
-                        .append(") KEEP (DENSE_RANK LAST ORDER BY ")
-                        .append(tableAlias).append(".ts ASC) AS ").append(colName);
-            }
-            return ret.toString();
-        } else {
-            return "";
+    private String getValueSubselectForGroupBy(String tableAlias) {
+        List<String> colNames = getValueColumnNames();
+        StringBuilder ret = new StringBuilder(70 * colNames.size());
+        for (String colName : colNames) {
+            // E.g., ", MAX(m.val) KEEP (DENSE_RANK LAST ORDER BY m.ts ASC) AS val".
+            // How this works, assuming we have a "GROUP BY row_name, col_name" clause:
+            //  1) Among each group of rows with the same (row_name, col_name) pair,
+            //     "KEEP (DENSE_RANK LAST ORDER BY m.ts ASC)" will select the subset
+            //     of rows with the biggest 'ts' value. Since (row_name, col_name, ts)
+            //     is the primary key, that subset will always contain exactly one
+            //     element in our case.
+            //  2) For that subset of rows, we take an aggregate function "MAX(m.val)".
+            //     It doesn't make a difference which function we use since our subset
+            //     is a singleton, so MAX will simply return the only element in that set.
+            //  To sum it up: for each (row_name, col_name) group, this will select
+            //  the value of the row that has the largest 'ts' value within that group.
+            ret.append(", MAX(").append(tableAlias).append('.').append(colName)
+                    .append(") KEEP (DENSE_RANK LAST ORDER BY ")
+                    .append(tableAlias).append(".ts ASC) AS ").append(colName);
         }
+        return ret.toString();
     }
 
     private List<String> getValueColumnNames() {
