a74f8cc62cc0491dd01c9060cef06a4e79c813e9
Grgur Petric Maretic <gmaretic@palantir.com>
2017-04-03 18:47:18 +0100

    Added benchmarks for get rows with 10k rows

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/table/ConsecutiveNarrowTable.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/table/ConsecutiveNarrowTable.java
@@ -15,6 +15,7 @@
  */
 package com.palantir.atlasdb.performance.benchmarks.table;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -72,6 +73,16 @@ public abstract class ConsecutiveNarrowTable {
     public abstract TableReference getTableRef();
 
     public abstract int getNumRows();
+
+    public List<byte[]> rows = populateRowNames();
+
+    private List<byte[]> populateRowNames() {
+        List<byte[]> list = new ArrayList<>();
+        for (int j = 0; j < DEFAULT_NUM_ROWS; ++j) {
+            list.add(Ints.toByteArray(j));
+        }
+        return list;
+    }
 
     protected abstract void setupData();
 
@@ -191,7 +202,7 @@ public abstract class ConsecutiveNarrowTable {
         }
         return requests;
     }
-
+    
     private static void storeDataInTable(ConsecutiveNarrowTable table, int numOverwrites) {
         IntStream.range(0, numOverwrites + 1).forEach($ -> {
             table.getTransactionManager().runTaskThrowOnConflict(txn -> {
