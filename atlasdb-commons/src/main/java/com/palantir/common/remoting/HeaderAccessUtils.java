d91edca34046d14f96ab9d556da7d5734faae209
Jeremy Kong <jeremykong@hotmail.com>
2017-04-06 21:46:35 +0100

    Parse retry-after headers of 503s correctly

--- atlasdb-commons/src/main/java/com/palantir/common/remoting/HeaderAccessUtils.java
+++ atlasdb-commons/src/main/java/com/palantir/common/remoting/HeaderAccessUtils.java
@@ -18,6 +18,8 @@ package com.palantir.common.remoting;
 import java.util.Collection;
 import java.util.Map;
 
+import com.google.common.collect.ImmutableList;
+
 /**
  * This class is useful for accessing HTTP headers in a case-insensitive manner.
  * This is necessary for compatibility with OkHttp 3.3.0+, as that lower-cases header names whereas we use constants
@@ -34,6 +36,9 @@ public final class HeaderAccessUtils {
      * the associated collection of strings from the map with the value, returning true iff this contains a match.
      * If no key matches, this method returns false.
      *
+     * This can be implemented in terms of shortcircuitingCaseInsensitiveGet, but suffered a performance regression
+     * in benchmarking. This is a hot code path, so speed is important.
+     *
      * As a precondition: the headers map should NOT contain distinct keys differing only in case.
      * (This is true as far as our use-case is concerned.)
      */
@@ -47,5 +52,24 @@ public final class HeaderAccessUtils {
             }
         }
         return false;
+    }
+
+    /**
+     * Compares the keys of the map to the header in a case-insensitive manner.
+     * Upon finding a match, returns the associated collection of strings.
+     * Returns an empty collection if the key is not present in the map.
+     *
+     * As a precondition: the headers map should NOT contain distinct keys differing only in case.
+     * (This is true as far as our use-case is concerned.)
+     */
+    public static Collection<String> shortcircuitingCaseInsensitiveGet(
+            Map<String, Collection<String>> headers,
+            String header) {
+        for (Map.Entry<String, Collection<String>> entry : headers.entrySet()) {
+            if (header.equalsIgnoreCase(entry.getKey())) {
+                return entry.getValue();
+            }
+        }
+        return ImmutableList.of();
     }
 }
