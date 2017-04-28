2b3b6fc676bc08ddb9f96a952374ec9e8b47ac93
Glenn Sheasby <gsheasby@palantir.com>
2017-04-28 10:36:57 +0100

    BREAKING: Rename classes to match checkstyle

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/service/TransactionKvsWrapper.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/service/TransactionKvsWrapper.java
@@ -26,14 +26,14 @@ import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.Value;
 import com.palantir.atlasdb.transaction.impl.TransactionConstants;
 
-public class TransactionKVSWrapper {
+public class TransactionKvsWrapper {
     // The maximum key-value store timestamp (exclusive) at which data is stored in transaction table.
     // All entries in transaction table are stored with timestamp 0
     private static final long MAX_TIMESTAMP = 1L;
 
     private final KeyValueService keyValueService;
 
-    public TransactionKVSWrapper(KeyValueService keyValueService) {
+    public TransactionKvsWrapper(KeyValueService keyValueService) {
         this.keyValueService = keyValueService;
     }
 
--- atlasdb-service/src/main/java/com/palantir/atlasdb/transaction/service/PaxosTransactionService.java
+++ atlasdb-service/src/main/java/com/palantir/atlasdb/transaction/service/PaxosTransactionService.java
@@ -26,13 +26,13 @@ import com.palantir.paxos.PaxosRoundFailureException;
 
 public class PaxosTransactionService implements TransactionService {
     private final PaxosProposer proposer;
-    private final TransactionKVSWrapper kvStore;
+    private final TransactionKvsWrapper kvStore;
 
-    public static TransactionService create(PaxosProposer proposer, TransactionKVSWrapper kvStore) {
+    public static TransactionService create(PaxosProposer proposer, TransactionKvsWrapper kvStore) {
         return new PaxosTransactionService(proposer, kvStore);
     }
 
-    private PaxosTransactionService(PaxosProposer proposer, TransactionKVSWrapper kvStore) {
+    private PaxosTransactionService(PaxosProposer proposer, TransactionKvsWrapper kvStore) {
         this.proposer = proposer;
         this.kvStore = kvStore;
     }
