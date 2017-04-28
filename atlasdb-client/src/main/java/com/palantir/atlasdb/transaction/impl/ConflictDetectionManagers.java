palantir/atlasdb

commit 15a5c5abf27c1523b174472c76660e7c629e2968

refactor conflict detection for high table scale (#1822)
* refactor conflict detection for high table scale

[no release notes]

will add release notes, going to see if all of the tests pass still

* Change snapshot transaction to correctly (directly) use the cache

Also revert some removed methods to reduce the size of the change

* fix test and some cleanup

* Refactor test-related stuff

* Don't use a default conflict handler

atlasdb-client/src/main/java/com/palantir/atlasdb/transaction/impl/ConflictDetectionManagers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Palantir Technologies
+ * Copyright 2017 Palantir Technologies
  *
  * Licensed under the BSD-3 License (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,69 +15,76 @@
  */
 package com.palantir.atlasdb.transaction.impl;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Functions;
-import com.google.common.collect.ImmutableMap;
+import com.google.common.cache.CacheLoader;
 import com.google.common.collect.Maps;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
-import com.palantir.atlasdb.table.description.Schema;
 import com.palantir.atlasdb.table.description.TableMetadata;
 import com.palantir.atlasdb.transaction.api.ConflictHandler;
 
-public class ConflictDetectionManagers {
-    private ConflictDetectionManagers() {
-        //
+public final class ConflictDetectionManagers {
+    private static final Logger log = LoggerFactory.getLogger(ConflictDetectionManagers.class);
+
+    private ConflictDetectionManagers() {}
+
+    public static ConflictDetectionManager createWithNoConflictDetection() {
+        return new ConflictDetectionManager(
+                new CacheLoader<TableReference, ConflictHandler>() {
+                    @Override
+                    public ConflictHandler load(TableReference tableReference) throws Exception {
+                        return ConflictHandler.IGNORE_ALL;
+                    }
+                });
     }
 
-    public static ConflictDetectionManager createDefault(KeyValueService kvs) {
-        return new ConflictDetectionManager(getTablesToConflictDetectSupplier(kvs));
+    public static ConflictDetectionManager create(KeyValueService kvs) {
+        return create(kvs, true);
     }
 
-    public static ConflictDetectionManager createFromSchema(Schema schema) {
-        return new ConflictDetectionManager(() -> getTablesToConflictDetect(schema));
+    public static ConflictDetectionManager createWithoutWarmingCache(KeyValueService kvs) {
+        return create(kvs, false);
     }
 
-    public static ConflictDetectionManager fromMap(final Map<TableReference, ConflictHandler> map) {
-        return new ConflictDetectionManager(() -> map);
-    }
-
-    /**
-     * Don't do conflict detection. Use this for read only transactions.
-     */
-    public static ConflictDetectionManager withoutConflictDetection(KeyValueService kvs) {
-        return new ConflictDetectionManager(getNoConflictDetectSupplier(kvs));
-    }
-
-    private static Supplier<Map<TableReference, ConflictHandler>> getNoConflictDetectSupplier(final KeyValueService kvs) {
-        return () -> {
-            Set<TableReference> tables = kvs.getAllTableNames();
-            return Maps.asMap(tables, Functions.constant(ConflictHandler.IGNORE_ALL));
-        };
-    }
-
-    private static Supplier<Map<TableReference, ConflictHandler>> getTablesToConflictDetectSupplier(final KeyValueService keyValueService) {
-        return () -> getTablesToConflictDetect(keyValueService);
-    }
-
-    private static Map<TableReference, ConflictHandler> getTablesToConflictDetect(KeyValueService kvs) {
-        return ImmutableMap.copyOf(Maps.transformValues(kvs.getMetadataForTables(), metadataForTable -> {
-            if (metadataForTable != null && metadataForTable.length > 0) {
-                return TableMetadata.BYTES_HYDRATOR.hydrateFromBytes(metadataForTable).getConflictHandler();
-            } else {
-                return ConflictHandler.RETRY_ON_WRITE_WRITE;
-            }
-        }));
-    }
-
-    private static Map<TableReference, ConflictHandler> getTablesToConflictDetect(Schema schema) {
-        Map<TableReference, ConflictHandler> ret = Maps.newHashMap();
-        for (Map.Entry<TableReference, TableMetadata> e : schema.getAllTablesAndIndexMetadata().entrySet()) {
-            ret.put(e.getKey(), e.getValue().getConflictHandler());
+    private static ConflictDetectionManager create(KeyValueService kvs, boolean warmCache) {
+        ConflictDetectionManager conflictDetectionManager = new ConflictDetectionManager(
+                new CacheLoader<TableReference, ConflictHandler>() {
+                    @Override
+                    public ConflictHandler load(TableReference tableReference) throws Exception {
+                        byte[] metadata = kvs.getMetadataForTable(tableReference);
+                        if (metadata == null) {
+                            log.error("Tried to make a transaction over a table that has no metadata: {}.",
+                                    tableReference);
+                            return null;
+                        } else {
+                            return getConflictHandlerFromMetadata(metadata);
+                        }
+                    }
+                });
+        if (warmCache) {
+            // kick off an async thread that attempts to fully warm this cache
+            // if it fails (e.g. probably this user has way too many tables), that's okay,
+            // we will be falling back on individually loading in tables as needed.
+            new Thread(() -> {
+                try {
+                    conflictDetectionManager.warmCacheWith(
+                            Maps.transformValues(kvs.getMetadataForTables(),
+                                    ConflictDetectionManagers::getConflictHandlerFromMetadata));
+                } catch (Throwable t) {
+                    log.warn("There was a problem with pre-warming the conflict detection cache;"
+                            + " if you have unusually high table scale, this might be expected."
+                            + " Performance may be degraded until normal usage adequately warms the cache.", t);
+                }
+            }, "ConflictDetectionManager Cache Async Pre-Warm").start();
         }
-        return ret;
+        return conflictDetectionManager;
     }
+
+    private static ConflictHandler getConflictHandlerFromMetadata(byte[] metadata) {
+        return TableMetadata.BYTES_HYDRATOR
+                .hydrateFromBytes(metadata).getConflictHandler();
+    }
+
 }

atlasdb-api/src/main/java/com/palantir/atlasdb/keyvalue/api/KeyValueService.java
@@ -479,6 +479,9 @@ public interface KeyValueService extends AutoCloseable {
 
     /**
      * Drop the table, and also delete its table metadata.
+     *
+     * Do not fall into the trap of performing drop & immediate re-create of tables;
+     * instead use 'truncate' for this task.
      */
     @DELETE
     @Path("drop-table")
@@ -492,6 +495,9 @@ public interface KeyValueService extends AutoCloseable {
      * Drops many tables in idempotent fashion. If you are dropping many tables at once,
      * use this call as the implementation can be much faster/less error-prone on some KVSs.
      * Also deletes corresponding table metadata.
+     *
+     * Do not fall into the trap of performing drop & immediate re-create of tables;
+     * instead use 'truncate' for this task.
      */
     @DELETE
     @Path("drop-tables")
@@ -548,6 +554,13 @@ public interface KeyValueService extends AutoCloseable {
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     byte[] getMetadataForTable(@QueryParam("tableRef") TableReference tableRef);
 
+    /**
+     * Gets the metadata for all known user-created Atlas tables.
+     * Consider not using this if you will be running against an Atlas instance with a large number of tables.
+     *
+     * @return a Map from TableReference to byte array representing the metadata for the table
+     * Consider {@link TableMetadata#BYTES_HYDRATOR} for hydrating
+     */
     @POST
     @Path("get-metadata-for-tables")
     @Produces(MediaType.APPLICATION_JSON)

atlasdb-client/src/main/java/com/palantir/atlasdb/transaction/impl/ConflictDetectionManager.java
@@ -15,56 +15,49 @@
  */
 package com.palantir.atlasdb.transaction.impl;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Supplier;
 
-import com.google.common.collect.Maps;
+import javax.annotation.Nullable;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.transaction.api.ConflictHandler;
 
 public class ConflictDetectionManager {
-    private final Supplier<Map<TableReference, ConflictHandler>> supplier;
-    private final Map<TableReference, ConflictHandler> overrides;
-    private final RecomputingSupplier<Map<TableReference, ConflictHandler>> cachedResult;
+    private final LoadingCache<TableReference, ConflictHandler> cache;
 
     /**
-     * @param supplier A {@link Supplier} which returns the current conflict detection
-     *                 configuration to the database. Note that the result is cached,
-     *                 so changes to configuration must be followed by a call to
-     *                 {@link #recompute()} in order to be picked up.
+     *  This class does not make the mistake of attempting cache invalidation,
+     *  so a table dropped by another instance may still be cached here.
+     *
+     *  This is okay in the case of a simple drop, but a same-name table drop
+     *  and re-addition with a different Conflict Handler
+     *  (where an external atlas instance handles both of these operations)
+     *  will be incorrect. This is an unrealistic workflow
+     *  and I'm fine with just documenting this behavior.
+     *
+     *  (This has always been the behavior of this class; I'm simply calling it out)
      */
-    ConflictDetectionManager(Supplier<Map<TableReference, ConflictHandler>> supplier) {
-        this.supplier = supplier;
-        this.overrides = Maps.newConcurrentMap();
-        this.cachedResult = RecomputingSupplier.create(() -> {
-            Map<TableReference, ConflictHandler> ret = new HashMap<>(this.supplier.get());
-            ret.putAll(overrides);
-            return Collections.unmodifiableMap(ret);
-        });
+    public ConflictDetectionManager(CacheLoader<TableReference, ConflictHandler> loader) {
+        this.cache = CacheBuilder.newBuilder()
+                .maximumSize(100_000)
+                .build(loader);
     }
 
-    public void setConflictDetectionMode(TableReference table, ConflictHandler handler) {
-        overrides.put(table, handler);
-        cachedResult.recompute();
+    public void warmCacheWith(Map<TableReference, ConflictHandler> preload) {
+        cache.putAll(preload);
     }
 
-    public void removeConflictDetectionMode(TableReference table) {
-        overrides.remove(table);
-        cachedResult.recompute();
+    @VisibleForTesting
+    public Map<TableReference, ConflictHandler> getCachedValues() {
+        return cache.asMap();
     }
 
-    public boolean isEmptyOrContainsTable(TableReference tableRef) {
-        Map<TableReference, ConflictHandler> tableToConflict = cachedResult.get();
-        return tableToConflict.isEmpty() || tableToConflict.containsKey(tableRef);
-    }
-
-    public Map<TableReference, ConflictHandler> get() {
-        return cachedResult.get();
-    }
-
-    public void recompute() {
-        cachedResult.recompute();
+    @Nullable
+    public ConflictHandler get(TableReference tableReference) {
+        return cache.getUnchecked(tableReference);
     }
 }

atlasdb-client/src/test/java/com/palantir/atlasdb/transaction/impl/ConflictDetectionManagerTest.java
@@ -1,135 +1,0 @@
-/*
- * Copyright 2015 Palantir Technologies
- *
- * Licensed under the BSD-3 License (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://opensource.org/licenses/BSD-3-Clause
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.palantir.atlasdb.transaction.impl;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import com.google.common.collect.ImmutableMap;
-import com.palantir.atlasdb.keyvalue.api.TableReference;
-import com.palantir.atlasdb.transaction.api.ConflictHandler;
-
-public class ConflictDetectionManagerTest {
-
-    private static final TableReference TABLE_1 = TableReference.fromString("default.table1");
-    private static final TableReference TABLE_2 = TableReference.fromString("default.table2");
-    private static final TableReference TABLE_3 = TableReference.fromString("default.table3");
-    private static final TableReference TABLE_4 = TableReference.fromString("default.table4");
-
-    private Map<TableReference, ConflictHandler> map;
-    private AtomicInteger callCount;
-    private ConflictDetectionManager manager;
-
-    @Before
-    public void setUp() {
-        map = new HashMap<>();
-        callCount = new AtomicInteger(0);
-        manager = new ConflictDetectionManager(() -> {
-            callCount.incrementAndGet();
-            return map;
-        });
-    }
-
-    @Test
-    public void testEmptyManager() {
-        Assert.assertTrue(manager.isEmptyOrContainsTable(TableReference.fromString("default.table")));
-        Assert.assertTrue(manager.get().isEmpty());
-        manager.recompute();
-        Assert.assertTrue(manager.get().isEmpty());
-    }
-
-    @Test
-    public void testRecomputeNoOverrides() {
-        map.put(TABLE_1, ConflictHandler.IGNORE_ALL);
-        map.put(TABLE_3, ConflictHandler.IGNORE_ALL);
-
-        // Initial get() loads supplier properly
-        Assert.assertEquals(ImmutableMap.of(
-                        TABLE_1, ConflictHandler.IGNORE_ALL,
-                        TABLE_3, ConflictHandler.IGNORE_ALL),
-                manager.get());
-        Assert.assertEquals(1, callCount.get());
-
-        map.clear();
-        map.put(TABLE_1, ConflictHandler.RETRY_ON_VALUE_CHANGED);
-        map.put(TABLE_2, ConflictHandler.IGNORE_ALL);
-
-        // results are cached
-        Assert.assertEquals(ImmutableMap.of(
-                        TABLE_1, ConflictHandler.IGNORE_ALL,
-                        TABLE_3, ConflictHandler.IGNORE_ALL),
-                manager.get());
-        Assert.assertEquals(1, callCount.get());
-
-        manager.recompute();
-
-        // recompute() recomputes the cached result
-        Assert.assertEquals(ImmutableMap.of(
-                        TABLE_1, ConflictHandler.RETRY_ON_VALUE_CHANGED,
-                        TABLE_2, ConflictHandler.IGNORE_ALL),
-                manager.get());
-        Assert.assertEquals(2, callCount.get());
-    }
-
-    @Test
-    public void testWithOverrides() {
-        map.put(TABLE_1, ConflictHandler.IGNORE_ALL);
-        map.put(TABLE_3, ConflictHandler.IGNORE_ALL);
-
-        Assert.assertEquals(ImmutableMap.of(
-                TABLE_1, ConflictHandler.IGNORE_ALL,
-                TABLE_3, ConflictHandler.IGNORE_ALL),
-                manager.get());
-
-        manager.setConflictDetectionMode(TABLE_1, ConflictHandler.RETRY_ON_VALUE_CHANGED);
-        manager.setConflictDetectionMode(TABLE_2, ConflictHandler.RETRY_ON_VALUE_CHANGED);
-
-        Assert.assertEquals(ImmutableMap.of(
-                TABLE_1, ConflictHandler.RETRY_ON_VALUE_CHANGED,
-                TABLE_2, ConflictHandler.RETRY_ON_VALUE_CHANGED,
-                TABLE_3, ConflictHandler.IGNORE_ALL),
-                manager.get());
-
-        manager.removeConflictDetectionMode(TABLE_1);
-
-        Assert.assertEquals(ImmutableMap.of(
-                TABLE_1, ConflictHandler.IGNORE_ALL,
-                TABLE_2, ConflictHandler.RETRY_ON_VALUE_CHANGED,
-                TABLE_3, ConflictHandler.IGNORE_ALL),
-                manager.get());
-    }
-
-    @Test
-    public void testIsEmptyOrContainsTable() {
-        map.put(TABLE_1, ConflictHandler.IGNORE_ALL);
-        map.put(TABLE_3, ConflictHandler.IGNORE_ALL);
-
-        manager.setConflictDetectionMode(TABLE_1, ConflictHandler.RETRY_ON_VALUE_CHANGED);
-        manager.setConflictDetectionMode(TABLE_2, ConflictHandler.RETRY_ON_VALUE_CHANGED);
-
-        int startCount = callCount.get();
-        Assert.assertTrue(manager.isEmptyOrContainsTable(TABLE_1));
-        Assert.assertTrue(manager.isEmptyOrContainsTable(TABLE_2));
-        Assert.assertTrue(manager.isEmptyOrContainsTable(TABLE_3));
-        Assert.assertFalse(manager.isEmptyOrContainsTable(TABLE_4));
-        Assert.assertEquals(startCount, callCount.get());
-    }
-}

atlasdb-config/src/main/java/com/palantir/atlasdb/factory/TransactionManagers.java
@@ -198,7 +198,7 @@ public final class TransactionManagers {
         PersistentLockService persistentLockService = createAndRegisterPersistentLockService(kvs, env);
 
         TransactionService transactionService = TransactionServices.createTransactionService(kvs);
-        ConflictDetectionManager conflictManager = ConflictDetectionManagers.createDefault(kvs);
+        ConflictDetectionManager conflictManager = ConflictDetectionManagers.create(kvs);
         SweepStrategyManager sweepStrategyManager = SweepStrategyManagers.createDefault(kvs);
 
         Set<Schema> allSchemas = ImmutableSet.<Schema>builder()

atlasdb-dagger/src/main/java/com/palantir/atlasdb/services/KeyValueServiceModule.java
@@ -76,7 +76,7 @@ public class KeyValueServiceModule {
     @Provides
     @Singleton
     public ConflictDetectionManager provideConflictDetectionManager(@Named("kvs") KeyValueService kvs) {
-        return ConflictDetectionManagers.createDefault(kvs);
+        return ConflictDetectionManagers.create(kvs);
     }
 
     @Provides

atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/memory/InMemoryAtlasDbFactory.java
@@ -122,7 +122,7 @@ public class InMemoryAtlasDbFactory implements AtlasDbFactory {
             }
         }));
         LockClient client = LockClient.of("in memory atlasdb instance");
-        ConflictDetectionManager conflictManager = ConflictDetectionManagers.createDefault(keyValueService);
+        ConflictDetectionManager conflictManager = ConflictDetectionManagers.createWithoutWarmingCache(keyValueService);
         SweepStrategyManager sweepStrategyManager = SweepStrategyManagers.createDefault(keyValueService);
 
         CleanupFollower follower = CleanupFollower.create(schemas);

atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SerializableTransaction.java
@@ -310,7 +310,9 @@ public class SerializableTransaction extends SnapshotTransaction {
     }
 
     boolean isSerializableTable(TableReference table) {
-        return getConflictHandlerForTable(table) == ConflictHandler.SERIALIZABLE;
+        // If the metadata is null, we assume that the conflict handler is not SERIALIZABLE.
+        // In that case the transaction will fail on commit if it has writes.
+        return conflictDetectionManager.get(table) == ConflictHandler.SERIALIZABLE;
     }
 
 
@@ -712,7 +714,7 @@ public class SerializableTransaction extends SnapshotTransaction {
                 defaultTransactionService,
                 NoOpCleaner.INSTANCE,
                 Suppliers.ofInstance(commitTs + 1),
-                ConflictDetectionManagers.withoutConflictDetection(keyValueService),
+                ConflictDetectionManagers.createWithNoConflictDetection(),
                 sweepStrategyManager,
                 immutableTimestamp,
                 Collections.emptyList(),

commit bb3fd3144eb6a9caef49e7b003f796087b53fb70

Bring back deleted factory method, but deprecated
Fixes downstream compile break caused by #1822.

atlasdb-client/src/main/java/com/palantir/atlasdb/transaction/impl/ConflictDetectionManagers.java
@@ -40,10 +40,25 @@ public final class ConflictDetectionManagers {
                 });
     }
 
+    /**
+     * @deprecated use {@link #create(KeyValueService)} instead. This constructor will be removed in a future release.
+     */
+    @Deprecated
+    public static ConflictDetectionManager createDefault(KeyValueService kvs) {
+        return create(kvs);
+    }
+
+    /**
+     * Creates a ConflictDetectionManager and kicks off an asynchronous thread to warm the cache that is used for
+     * conflict detection.
+     */
     public static ConflictDetectionManager create(KeyValueService kvs) {
         return create(kvs, true);
     }
 
+    /**
+     * Creates a ConflictDetectionManager without warming the cache.
+     */
     public static ConflictDetectionManager createWithoutWarmingCache(KeyValueService kvs) {
         return create(kvs, false);
     }