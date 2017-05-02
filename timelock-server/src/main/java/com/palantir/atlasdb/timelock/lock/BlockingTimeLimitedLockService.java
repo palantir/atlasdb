7d3d930b116ba82413a47ae2a9f563a2e5b992c5
Jeremy Kong <jeremykong@hotmail.com>
2017-05-01 19:54:40 -0700

    Baseline update

--- timelock-server/src/main/java/com/palantir/atlasdb/timelock/lock/BlockingTimeLimitedLockService.java
+++ timelock-server/src/main/java/com/palantir/atlasdb/timelock/lock/BlockingTimeLimitedLockService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Palantir Technologies
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
  *
  * Licensed under the BSD-3 License (the "License");
  * you may not use this file except in compliance with the License.
@@ -181,6 +181,7 @@ public class BlockingTimeLimitedLockService implements LockService {
         delegate.logCurrentState();
     }
 
+    @SuppressWarnings("Slf4jConstantLogMessage") // TODO (jkong): Implement an exception producer-and-logger
     private <T> T callWithTimeLimit(Callable<T> callable, LockRequestSpecification specification)
             throws InterruptedException {
         try {
