84f4a82290079d351096b0886a180bec7eb732bd
stefank-palantir <stefank@palantir.com>
2017-05-11 12:58:34 +0100

    Improve leader election logging (#1757)
    * Log getFreshTimestamp failure at warn
    
    * Improve leader election logging
    
    Add more granular leader election logging to facilitate debugging of
    failed or unexpected leadership transitions.
    
    * Code review markups.
    
    * Code review markups to reduce performance impact
    
    * Fix NPE in PaxosAcceptor.prepare
    
    Fix bug where new logging assumes a previously-accepted value exists at
    the point that prepare is called.
    
    * Fix another logging NPE
    
    * Tidy up log line
    
    Report (none) instead of null for the last accepted leader's UUID, if no leader has yet been accepted.

--- leader-election-impl/src/main/java/com/palantir/leader/PaxosLeaderElectionService.java
+++ leader-election-impl/src/main/java/com/palantir/leader/PaxosLeaderElectionService.java
@@ -151,6 +151,7 @@ public class PaxosLeaderElectionService implements PingableLeader, LeaderElectio
             return false;
         }
         final PingableLeader leader = maybeLeader.get();
+        final HostAndPort leaderName = potentialLeadersToHosts.get(leader);
 
         CompletionService<Boolean> pingCompletionService = new ExecutorCompletionService<Boolean>(
                 executor);
@@ -159,6 +160,7 @@ public class PaxosLeaderElectionService implements PingableLeader, LeaderElectio
         pingCompletionService.submit(new Callable<Boolean>() {
             @Override
             public Boolean call() throws Exception {
+                leaderLog.trace("Pinging suspected leader " + leaderName);
                 return leader.ping();
             }
         });
@@ -167,11 +169,25 @@ public class PaxosLeaderElectionService implements PingableLeader, LeaderElectio
             Future<Boolean> pingFuture = pingCompletionService.poll(
                     leaderPingResponseWaitMs,
                     TimeUnit.MILLISECONDS);
-            return pingFuture != null && pingFuture.get();
+            if (pingFuture == null) {
+                leaderLog.warn("Timed out pinging {} after {} ms", leaderName, leaderPingResponseWaitMs);
+                return false;
+            }
+            else if (pingFuture.get()) {
+                leaderLog.trace("Successfully pinged {}", leaderName);
+                return true;
+            }
+            else {
+                leaderLog.info("Pinged {} and it reported that it is no longer the leader", leaderName);
+                return false;
+            }
         } catch (InterruptedException e) {
+            log.warn("Interrupted while pinging {}", leaderName);
+            leaderLog.warn("Interrupted while pinging {}", leaderName);
             return false;
         } catch (ExecutionException e) {
-            log.warn("cannot ping leader", e);
+            log.warn("Cannot ping {}", leaderName, e);
+            leaderLog.warn("Cannot ping {}", leaderName, e);
             return false;
         }
     }
@@ -339,7 +355,7 @@ public class PaxosLeaderElectionService implements PingableLeader, LeaderElectio
                 seq = Defaults.defaultValue(long.class);
             }
 
-            leaderLog.info("Proposing leadership with sequence number " + seq);
+            leaderLog.info("Proposing leadership with sequence number {} (our UUID: {})", seq, getUUID());
             proposer.propose(seq, null);
         } catch (PaxosRoundFailureException e) {
             // We have failed trying to become the leader.
@@ -602,6 +618,7 @@ public class PaxosLeaderElectionService implements PingableLeader, LeaderElectio
             for (PaxosValue value : values) {
                 PaxosValue currentLearnedValue = knowledge.getLearnedValue(value.getRound());
                 if (currentLearnedValue == null) {
+                    leaderLog.info("Peers taught us about leader #{}, with UUID {}", value.getRound(), value.getLeaderUUID());
                     knowledge.learn(value.getRound(), value);
                     learned = true;
                 }
