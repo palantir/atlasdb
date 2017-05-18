71b65219efa371e9c738b39dad4e28d6bd50b4cc
Samuel Souza <ssouza@palantir.com>
2017-05-18 16:37:46 +0100

    Remove todo. Tracked in #1917

--- atlasdb-commons/src/main/java/com/palantir/common/concurrent/InterruptibleFuture.java
+++ atlasdb-commons/src/main/java/com/palantir/common/concurrent/InterruptibleFuture.java
@@ -45,7 +45,6 @@ import com.palantir.common.base.Throwables;
  * @author jtamer
  */
 public abstract class InterruptibleFuture<V> implements RunnableFuture<V> {
-    // TODO(ssouza): Move this class to the internal project.
 
     private static enum State { WAITING_TO_RUN, RUNNING, COMPLETED }
 
