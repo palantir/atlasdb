73d6d2204fb381cc234b33c9d0209feaf2cc268e
Jeremy Kong <jeremykong@hotmail.com>
2017-06-06 20:40:26 +0100

    SafeLoggableData

---
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/logging/SafeLoggableDataUtils.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.logging;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.table.description.NameComponentDescription;
+import com.palantir.atlasdb.table.description.NamedColumnDescription;
+import com.palantir.atlasdb.table.description.TableMetadata;
+
+public final class SafeLoggableDataUtils {
+    private SafeLoggableDataUtils() {
+        // utility
+    }
+
+    public static SafeLoggableData fromTableMetadata(Map<TableReference, byte[]> tableMetadataMap) {
+        ImmutableSafeLoggableData.Builder builder = ImmutableSafeLoggableData.builder();
+
+        tableMetadataMap.forEach(
+                (ref, metadataBytes) -> {
+                    TableMetadata tableMetadata = TableMetadata.BYTES_HYDRATOR.hydrateFromBytes(metadataBytes);
+                    addLoggableNamesToBuilder(builder, ref, tableMetadata);
+                });
+        return builder.build();
+    }
+
+    private static void addLoggableNamesToBuilder(
+            ImmutableSafeLoggableData.Builder builder,
+            TableReference ref,
+            TableMetadata tableMetadata) {
+        if (tableMetadata.isNameLoggable()) {
+            builder.addPermittedTableReferences(ref);
+        }
+
+        Set<NameComponentDescription> loggableRowParts = tableMetadata.getRowMetadata()
+                .getRowParts()
+                .stream()
+                .filter(NameComponentDescription::isNameLoggable)
+                .collect(Collectors.toSet());
+        builder.putPermittedRowComponents(ref, loggableRowParts);
+
+        Set<NamedColumnDescription> loggableColumnDescriptions = tableMetadata.getColumns()
+                .getNamedColumns()
+                .stream()
+                .filter(NamedColumnDescription::isNameLoggable)
+                .collect(Collectors.toSet());
+        builder.putPermittedColumnNames(ref, loggableColumnDescriptions);
+    }
+}
