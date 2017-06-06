0f1a47f71ad44bb7257e56d416782693a25728f2
Jeremy Kong <jeremykong@hotmail.com>
2017-06-06 20:40:26 +0100

    SafeLoggableData

---
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/logging/SimpleKeyValueServiceArgSupplier.java
@@ -0,0 +1,7 @@
+package com.palantir.atlasdb.logging;
+
+/**
+ * Created by jkong on 06/06/2017.
+ */
+public class SimpleKeyValueServiceArgSupplier {
+}
---
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/logging/SimpleKeyValueServiceLogArbitrator.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.logging;
+
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.table.description.NameComponentDescription;
+import com.palantir.atlasdb.table.description.NamedColumnDescription;
+
+public class SimpleKeyValueServiceLogArbitrator implements KeyValueServiceLogArbitrator {
+    private final SafeLoggableData safeLoggableData;
+
+    public SimpleKeyValueServiceLogArbitrator(SafeLoggableData safeLoggableData) {
+        this.safeLoggableData = safeLoggableData;
+    }
+
+    @Override
+    public boolean isTableReferenceLoggable(TableReference tableReference) {
+        return safeLoggableData.permittedTableReferences().contains(tableReference);
+    }
+
+    @Override
+    public boolean isRowComponentNameLoggable(TableReference tableReference,
+            NameComponentDescription nameComponentDescription) {
+        return safeLoggableData.permittedRowComponents().get(tableReference).contains(nameComponentDescription);
+    }
+
+    @Override
+    public boolean isColumnNameLoggable(TableReference tableReference, NamedColumnDescription namedColumnDescription) {
+        return safeLoggableData.permittedColumnNames().get(tableReference).contains(namedColumnDescription);
+    }
+}
