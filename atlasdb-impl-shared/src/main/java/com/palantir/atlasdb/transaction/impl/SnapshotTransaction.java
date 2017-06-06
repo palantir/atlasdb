f95cf2adf94017b2de40495a5d0078603e4a740c
nziebart <nathan.ziebart@gmail.com>
2017-06-06 22:51:55 +0100

    no remote calls in readonly txn (#1996)
    * no remote calls in readonly txn
    
    * changelog
    
    * unused file
    
    * Moving time check into logging block

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SnapshotTransaction.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SnapshotTransaction.java
@@ -1190,10 +1190,12 @@ public class SnapshotTransaction extends AbstractTransaction implements Constrai
 
             checkConstraints();
             commitWrites(transactionService);
-            long transactionMillis = TimeUnit.NANOSECONDS.toMillis(transactionTimerContext.stop());
-            perfLogger.debug("Committed transaction {} in {}ms",
-                    getStartTimestamp(),
-                    transactionMillis);
+            if (perfLogger.isDebugEnabled()) {
+                long transactionMillis = TimeUnit.NANOSECONDS.toMillis(transactionTimerContext.stop());
+                perfLogger.debug("Committed transaction {} in {}ms",
+                        getStartTimestamp(),
+                        transactionMillis);
+            }
             success = true;
         } finally {
             // Once we are in state committing, we need to try/finally to set the state to a terminal state.
