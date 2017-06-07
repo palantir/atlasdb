82acaba5d511abce43fc1cfd959cb1cc830447d7
Jeremy Kong <jeremykong@hotmail.com>
2017-06-07 07:39:44 +0100

    Loggability in TableMetadata Data Structures (Variably Secure Components - Part 1) (#1988)
    * update protobufs
    
    * Have data objects support loggable names
    
    * NamedColumnDescriptionTest
    
    * NameComponentDescriptionTest plus add explicit no cases to NamedColumnDescriptionTest
    
    * Add serialization and default-unsafe tests
    
    * Beef up serialization testing
    
    * Fix build protos problem
    
    * [no release notes]

---
+++ atlasdb-client/src/test/java/com/palantir/atlasdb/table/description/NameComponentDescriptionTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.table.description;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.palantir.atlasdb.protos.generated.TableMetadataPersistence;
+
+public class NameComponentDescriptionTest {
+    private static final String COMPONENT_NAME = "rowComponent";
+    private static final ValueType VALUE_TYPE = ValueType.UUID;
+    private static final TableMetadataPersistence.ValueByteOrder VALUE_BYTE_ORDER =
+            TableMetadataPersistence.ValueByteOrder.ASCENDING;
+    private static final UniformRowNamePartitioner UNIFORM_ROW_NAME_PARTITIONER =
+            new UniformRowNamePartitioner(VALUE_TYPE);
+
+    private static final NameComponentDescription DEFAULT_UNNAMED_DESCRIPTION = new NameComponentDescription();
+    private static final NameComponentDescription LOGGABILITY_UNSPECIFIED_DESCRIPTION =
+            new NameComponentDescription(
+                    COMPONENT_NAME,
+                    VALUE_TYPE,
+                    VALUE_BYTE_ORDER,
+                    UNIFORM_ROW_NAME_PARTITIONER,
+                    null);
+
+    private static final NameComponentDescription NAME_LOGGABLE_DESCRIPTION =
+            createWithSpecifiedLoggability(true);
+    private static final NameComponentDescription NAME_NOT_LOGGABLE_DESCRIPTION =
+            createWithSpecifiedLoggability(false);
+
+    @Test
+    public void nameIsNotLoggableInDefaultDescription() {
+        assertThat(DEFAULT_UNNAMED_DESCRIPTION.isNameLoggable()).isFalse();
+    }
+
+    @Test
+    public void nameIsNotLoggableIfNotSpecified() {
+        assertThat(LOGGABILITY_UNSPECIFIED_DESCRIPTION.isNameLoggable()).isFalse();
+    }
+
+    @Test
+    public void nameCanBeSpecifiedToBeLoggable() {
+        assertThat(NAME_LOGGABLE_DESCRIPTION.isNameLoggable()).isTrue();
+    }
+
+    @Test
+    public void nameCanBeSpecifiedToBeNotLoggable() {
+        assertThat(NAME_NOT_LOGGABLE_DESCRIPTION.isNameLoggable()).isFalse();
+    }
+
+    @Test
+    public void canSerializeAndDeserializeDefaultDescription() {
+        assertCanSerializeAndDeserializeWithLoggability(DEFAULT_UNNAMED_DESCRIPTION, false);
+    }
+
+    @Test
+    public void canSerializeAndDeserializeLoggabilityUnspecifiedDescription() {
+        assertCanSerializeAndDeserializeWithLoggability(LOGGABILITY_UNSPECIFIED_DESCRIPTION, false);
+    }
+
+    @Test
+    public void canSerializeAndDeserializeKeepingLoggability() {
+        assertCanSerializeAndDeserializeWithLoggability(NAME_LOGGABLE_DESCRIPTION, true);
+    }
+
+    @Test
+    public void canSerializeAndDeserializeKeepingNonLoggability() {
+        assertCanSerializeAndDeserializeWithLoggability(NAME_NOT_LOGGABLE_DESCRIPTION, false);
+    }
+
+    @Test
+    public void withPartitionersPreservesLoggabilityOfName() {
+        assertThat(NAME_LOGGABLE_DESCRIPTION.withPartitioners().isNameLoggable()).isTrue();
+    }
+
+    @Test
+    public void withPartitionersPreservesNonLoggabilityOfName() {
+        assertThat(LOGGABILITY_UNSPECIFIED_DESCRIPTION.withPartitioners().isNameLoggable()).isFalse();
+    }
+
+    private static void assertCanSerializeAndDeserializeWithLoggability(
+            NameComponentDescription componentDescription,
+            boolean loggable) {
+        TableMetadataPersistence.NameComponentDescription.Builder builder =
+                componentDescription.persistToProto();
+        assertThat(NameComponentDescription.hydrateFromProto(builder.build()))
+                .isEqualTo(componentDescription)
+                .matches(description -> description.isNameLoggable() == loggable);
+    }
+
+    private static NameComponentDescription createWithSpecifiedLoggability(boolean loggable) {
+        return new NameComponentDescription(
+                COMPONENT_NAME,
+                VALUE_TYPE,
+                VALUE_BYTE_ORDER,
+                UNIFORM_ROW_NAME_PARTITIONER,
+                null,
+                loggable);
+    }
+}
