4e332bd4ee0b86a2ec1e6016c1c4a8219fa70a92
Jeremy Kong <jeremykong@hotmail.com>
2017-06-29 17:03:06 +0100

    Expose ability to specify things as loggable

--- atlasdb-client/src/main/java/com/palantir/atlasdb/table/description/TableDefinition.java
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/table/description/TableDefinition.java
@@ -62,22 +62,48 @@ public class TableDefinition extends AbstractDefinition {
         state = State.DEFINING_CONSTRAINTS;
     }
 
-    public void column(String columnName, String shortName, Class<?> protoOrPersistable) {
-        column(columnName, shortName, protoOrPersistable, Compression.NONE);
+    public void tableNameIsSafeLoggable() {
+        Preconditions.checkState(state == State.NONE, "Specifying a table name is safe should be done outside of the"
+                + " subscopes of TableDefinition.");
+        tableNameLoggable = true;
     }
 
-    public void column(String columnName, String shortName, Class<?> protoOrPersistable, Compression compression) {
+    public void column(String columnName, String shortName, Class<?> protoOrPersistable) {
+        column(columnName, shortName, protoOrPersistable, false);
+    }
+
+    public void column(String columnName, String shortName, Class<?> protoOrPersistable, boolean columnNameLoggable) {
+        column(columnName, shortName, protoOrPersistable, Compression.NONE, columnNameLoggable);
+    }
+
+    public void column(
+            String columnName,
+            String shortName,
+            Class<?> protoOrPersistable,
+            Compression compression,
+            boolean columnNameLoggable) {
         Preconditions.checkState(state == State.DEFINING_COLUMNS);
         Preconditions.checkState(!noColumns);
         checkUniqueColumnNames(columnName, shortName);
-        fixedColumns.add(new NamedColumnDescription(shortName, columnName, getColumnValueDescription(protoOrPersistable, compression)));
+        fixedColumns.add(
+                new NamedColumnDescription(
+                        shortName,
+                        columnName,
+                        getColumnValueDescription(protoOrPersistable, compression),
+                        columnNameLoggable));
     }
 
     public void column(String columnName, String shortName, ValueType valueType) {
+        column(columnName, shortName, valueType, false);
+    }
+
+    public void column(String columnName, String shortName, ValueType valueType, boolean columnNameLoggable) {
         Preconditions.checkState(state == State.DEFINING_COLUMNS);
         Preconditions.checkState(!noColumns);
         checkUniqueColumnNames(columnName, shortName);
-        fixedColumns.add(new NamedColumnDescription(shortName, columnName, ColumnValueDescription.forType(valueType)));
+        fixedColumns.add(
+                new NamedColumnDescription(
+                        shortName, columnName, ColumnValueDescription.forType(valueType), columnNameLoggable));
     }
 
     public void noColumns() {
@@ -115,8 +141,13 @@ public class TableDefinition extends AbstractDefinition {
     }
 
     public void rowComponent(String componentName, ValueType valueType, ValueByteOrder valueByteOrder) {
+        rowComponent(componentName, valueType, valueByteOrder, false);
+    }
+
+    public void rowComponent(
+            String componentName, ValueType valueType, ValueByteOrder valueByteOrder, boolean rowNameLoggable) {
         Preconditions.checkState(state == State.DEFINING_ROW_NAME);
-        rowNameComponents.add(new NameComponentDescription(componentName, valueType, valueByteOrder));
+        rowNameComponents.add(new NameComponentDescription(componentName, valueType, valueByteOrder, rowNameLoggable));
     }
 
     /**
@@ -245,6 +276,7 @@ public class TableDefinition extends AbstractDefinition {
     private Set<String> fixedColumnShortNames = Sets.newHashSet();
     private Set<String> fixedColumnLongNames = Sets.newHashSet();
     private boolean noColumns = false;
+    private boolean tableNameLoggable = false;
 
     public TableMetadata toTableMetadata() {
         Preconditions.checkState(!rowNameComponents.isEmpty(), "No row name components defined.");
@@ -268,7 +300,8 @@ public class TableDefinition extends AbstractDefinition {
                 negativeLookups,
                 sweepStrategy,
                 expirationStrategy,
-                appendHeavyAndReadLight);
+                appendHeavyAndReadLight,
+                tableNameLoggable);
     }
 
     private ColumnMetadataDescription getColumnMetadataDescription() {
