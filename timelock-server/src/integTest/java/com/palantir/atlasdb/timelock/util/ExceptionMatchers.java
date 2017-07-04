2e366eb5cdcac6cd1fcbfe6b0bb2d240c332ae01
Himangi Saraogi <hsaraogi@palantir.com>
2017-07-04 16:34:18 +0100

    revert stray changes

--- timelock-server/src/integTest/java/com/palantir/atlasdb/timelock/util/ExceptionMatchers.java
+++ timelock-server/src/integTest/java/com/palantir/atlasdb/timelock/util/ExceptionMatchers.java
@@ -30,15 +30,4 @@ public final class ExceptionMatchers {
         assertThat(throwable.getClass().getName())
                 .contains("RetryableException");
     }
-
-    public static void isInternalServerError(Throwable throwable) {
-        assertThat(throwable)
-                .hasMessageContaining("method invoked on a non-leader");
-
-        // We shade Feign, so we can't rely on our client's RetryableException exactly matching ours.
-        assertThat(throwable.getClass().getName())
-                .contains("RetryableException");
-    }
-
-
 }
