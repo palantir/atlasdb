14ed2d6029c7a1d5bf1cf2527dc828c83268d2a5
Himangi Saraogi <hsaraogi@palantir.com>
2017-07-05 16:46:55 +0100

    Unit tests

--- leader-election-impl/src/test/java/com/palantir/leader/proxy/AwaitingLeadershipProxyTest.java
+++ leader-election-impl/src/test/java/com/palantir/leader/proxy/AwaitingLeadershipProxyTest.java
@@ -16,19 +16,27 @@
 package com.palantir.leader.proxy;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
 import org.junit.Test;
 
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.palantir.leader.LeaderElectionService;
+import com.palantir.leader.NotCurrentLeaderException;
+import com.palantir.leader.PaxosLeadershipToken;
 
 public class AwaitingLeadershipProxyTest {
+    private static final String TEST_MESSAGE = "test_message";
 
     @Test
     @SuppressWarnings("SelfEquals") // We're asserting that calling .equals on a proxy does not redirect the .equals call to the instance its being proxied.
@@ -47,6 +55,8 @@ public class AwaitingLeadershipProxyTest {
         assertThat(proxy.equals(proxy)).isTrue();
         assertThat(proxy.equals(null)).isFalse();
         assertThat(proxy.toString()).startsWith("com.palantir.leader.proxy.AwaitingLeadershipProxy@");
+        Runtime.getRuntime().gc();
+
     }
 
     @Test
@@ -66,5 +76,81 @@ public class AwaitingLeadershipProxyTest {
         assertThat(proxy.equals(proxy)).isTrue();
         assertThat(proxy.equals(null)).isFalse();
         assertThat(proxy.toString()).startsWith("com.palantir.leader.proxy.AwaitingLeadershipProxy@");
+        Runtime.getRuntime().gc();
+
+    }
+
+    @Test
+    public void shouldMapInterruptedExceptionToNCLEIfLeadingStatusChanges() throws InterruptedException {
+        TestRunnableWithCheckedException runnableWithInterruptedException = () -> {
+            throw new InterruptedException(TEST_MESSAGE);
+        };
+        Supplier<TestRunnableWithCheckedException> delegateSupplier = Suppliers.ofInstance(runnableWithInterruptedException);
+        LeaderElectionService mockLeader = mock(LeaderElectionService.class);
+        LeaderElectionService.LeadershipToken leadershipToken = mock(PaxosLeadershipToken.class);
+        when(mockLeader.blockOnBecomingLeader()).thenReturn(leadershipToken);
+
+        when(mockLeader.getSuspectedLeaderInMemory()).thenReturn(Optional.empty());
+
+        //Loses leadership
+        when(mockLeader.isStillLeading(leadershipToken)).thenReturn(
+                LeaderElectionService.StillLeadingStatus.LEADING,
+                LeaderElectionService.StillLeadingStatus.NOT_LEADING);
+
+        TestRunnableWithCheckedException proxy = AwaitingLeadershipProxy.newProxyInstance(
+                TestRunnableWithCheckedException.class, delegateSupplier, mockLeader);
+
+        assertThatThrownBy(proxy::run).isInstanceOf(NotCurrentLeaderException.class)
+                .hasMessage("received an interrupt due to leader election.")
+                .hasCauseExactlyInstanceOf(InterruptedException.class)
+                .hasStackTraceContaining(TEST_MESSAGE);
+        Runtime.getRuntime().gc();
+    }
+
+    @Test
+    public void shouldNotMapInterruptedExceptionToNCLEIfLeadingStatusDoesntChange() throws InterruptedException {
+        TestRunnableWithCheckedException runnableWithInterruptedException = () -> {
+            throw new InterruptedException(TEST_MESSAGE);
+        };
+        Supplier<TestRunnableWithCheckedException> delegateSupplier = Suppliers.ofInstance(runnableWithInterruptedException);
+        LeaderElectionService mockLeaderService = mock(LeaderElectionService.class);
+        LeaderElectionService.LeadershipToken leadershipToken = mock(PaxosLeadershipToken.class);
+        when(mockLeaderService.blockOnBecomingLeader()).thenReturn(leadershipToken);
+
+        when(mockLeaderService.getSuspectedLeaderInMemory()).thenReturn(Optional.empty());
+
+        //Always leading
+        when(mockLeaderService.isStillLeading(leadershipToken)).thenReturn(
+                LeaderElectionService.StillLeadingStatus.LEADING,
+                LeaderElectionService.StillLeadingStatus.LEADING);
+
+        TestRunnableWithCheckedException proxy = AwaitingLeadershipProxy.newProxyInstance(
+                TestRunnableWithCheckedException.class, delegateSupplier, mockLeaderService);
+
+        assertThatThrownBy(proxy::run).isInstanceOf(InterruptedException.class)
+                .hasMessage(TEST_MESSAGE);
+
+        Runtime.getRuntime().gc();
+    }
+
+
+    @Test
+    public void shouldNotMapOtherExceptionToNCLEIfLeadingStatusChanges() throws InterruptedException {
+        TestRunnableWithCheckedException runnableWithInterruptedException = () -> {
+            throw new IOException(TEST_MESSAGE);
+        };
+        Supplier<TestRunnableWithCheckedException> delegateSupplier = Suppliers.ofInstance(runnableWithInterruptedException);
+        LeaderElectionService mockLeader = mock(LeaderElectionService.class);
+        LeaderElectionService.LeadershipToken leadershipToken = mock(PaxosLeadershipToken.class);
+        when(mockLeader.blockOnBecomingLeader()).thenReturn(leadershipToken);
+
+        when(mockLeader.getSuspectedLeaderInMemory()).thenReturn(Optional.empty());
+        when(mockLeader.isStillLeading(leadershipToken)).thenReturn(LeaderElectionService.StillLeadingStatus.LEADING,
+                LeaderElectionService.StillLeadingStatus.NOT_LEADING);
+        TestRunnableWithCheckedException proxy = AwaitingLeadershipProxy.newProxyInstance(
+                TestRunnableWithCheckedException.class, delegateSupplier, mockLeader);
+
+        assertThatThrownBy(proxy::run).isNotInstanceOf(NotCurrentLeaderException.class).isInstanceOf(IOException.class);
+        Runtime.getRuntime().gc();
     }
 }
