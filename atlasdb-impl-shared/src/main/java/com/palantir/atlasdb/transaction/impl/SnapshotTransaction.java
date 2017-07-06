b2be587791316921b657ee90d57823ed8eccb754
Steven Berler <sberler@palantir.com>
2017-07-05 18:00:12 -0700

    fix reads of hidden tables from console
    This change more or less re-adds some very important lines of code
    that were incorrectly removed as part of removing support for temp
    tables nearly a year ago. As a result of these lines missing,
    reading hidden tables in console always resulted in all rows from
    the table being incorrectly deleted.

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SnapshotTransaction.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SnapshotTransaction.java
@@ -959,6 +959,17 @@ public class SnapshotTransaction extends AbstractTransaction implements Constrai
             }
         }
 
+        if (AtlasDbConstants.hiddenTables.contains(tableRef)) {
+            Preconditions.checkState(allowHiddenTableAccess, "hidden tables cannot be read in this transaction");
+            // hidden tables are used outside of the transaction protocol, and in general have invalid timestamps,
+            // so do not apply post-filtering as post-filtering would rollback (actually delete) the data incorrectly
+            // this case is hit when reading a hidden table from console
+            for (Map.Entry<Cell, Value> e : rawResults.entrySet()) {
+                results.put(e.getKey(), transformer.apply(e.getValue()));
+            }
+            return;
+        }
+
         Map<Cell, Value> remainingResultsToPostfilter = rawResults;
         while (!remainingResultsToPostfilter.isEmpty()) {
             remainingResultsToPostfilter = getWithPostFilteringInternal(
