6c01c2dbae133a278809b469410bdc46b1567ffd
Jeremy Kong <jeremykong@hotmail.com>
2017-07-11 10:40:13 +0100

    Jepsen Enhancements, Part 1: Parameterised Jepsen Lock Client (#2094)
    * Refactor to use JepsenLockClient interface
    
    * Fix broken tests
    
    * Improve test coverage slightly
    
    * Refactor clojure, part 1
    
    * Refactor out supplier
    
    * fix supplier syntax
    
    * add tests for default behaviours of unlock and refreshSingle
    
    * Fix incorrect method calls, remove todo block
    
    * Baseline, and a little bit more refactoring
    
    * [no release notes] plus rename tests to be consistent
    
    * Fix funky macro
    
    * PR comments

---
+++ atlasdb-jepsen-tests/src/main/java/com/palantir/atlasdb/http/SynchronousLockClient.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.atlasdb.http;
+
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableSortedMap;
+import com.google.common.collect.Sets;
+import com.palantir.lock.LockDescriptor;
+import com.palantir.lock.LockMode;
+import com.palantir.lock.LockRefreshToken;
+import com.palantir.lock.LockRequest;
+import com.palantir.lock.RemoteLockService;
+import com.palantir.lock.StringLockDescriptor;
+
+public class SynchronousLockClient implements JepsenLockClient<LockRefreshToken> {
+    private final RemoteLockService remoteLockService;
+
+    @VisibleForTesting
+    SynchronousLockClient(RemoteLockService remoteLockService) {
+        this.remoteLockService = remoteLockService;
+    }
+
+    public static JepsenLockClient<LockRefreshToken> create(List<String> hosts) {
+        return new SynchronousLockClient(TimelockUtils.createClient(hosts, RemoteLockService.class));
+    }
+
+    @Override
+    public LockRefreshToken lock(String client, String lockName) throws InterruptedException {
+        LockDescriptor descriptor = StringLockDescriptor.of(lockName);
+        LockRequest request = LockRequest.builder(ImmutableSortedMap.of(descriptor, LockMode.WRITE))
+                .doNotBlock()
+                .build();
+
+        return remoteLockService.lock(client, request);
+    }
+
+    @Override
+    public Set<LockRefreshToken> unlock(Set<LockRefreshToken> lockRefreshTokens) throws InterruptedException {
+        Set<LockRefreshToken> tokensUnlocked = Sets.newHashSet();
+        lockRefreshTokens.forEach(token -> {
+            if (remoteLockService.unlock(token)) {
+                tokensUnlocked.add(token);
+            }
+        });
+        return tokensUnlocked;
+    }
+
+    @Override
+    public Set<LockRefreshToken> refresh(Set<LockRefreshToken> lockRefreshTokens) throws InterruptedException {
+        return remoteLockService.refreshLockRefreshTokens(lockRefreshTokens);
+    }
+}
