47cec4d98b6bf8dc2d39590f87d4061d797976b4
Nathan Ziebart <nziebart@palantir.com>
2017-07-11 15:28:40 +0100

    checkstyle

--- atlasdb-feign/src/main/java/com/palantir/atlasdb/http/AtlasDbFeignTargetFactory.java
+++ atlasdb-feign/src/main/java/com/palantir/atlasdb/http/AtlasDbFeignTargetFactory.java
@@ -42,7 +42,8 @@ public final class AtlasDbFeignTargetFactory {
             .registerModule(new Jdk8Module());
     private static final Contract contract = new JAXRSContract();
     private static final Encoder encoder = new JacksonEncoder(mapper);
-    private static final Decoder decoder = new TextDelegateDecoder(new OptionalAwareDecoder(new JacksonDecoder(mapper)));
+    private static final Decoder decoder = new TextDelegateDecoder(
+            new OptionalAwareDecoder(new JacksonDecoder(mapper)));
     private static final ErrorDecoder errorDecoder = new AtlasDbErrorDecoder();
 
     private AtlasDbFeignTargetFactory() {
