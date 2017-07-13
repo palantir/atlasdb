4ccdd6661d617b38fdd7c64c049a39a62328ae4c
Tom Petracca <tpetracca@palantir.com>
2017-07-13 13:39:41 -0400

    delete things we've determined we don't need

--- timelock-agent/src/main/java/com/palantir/timelock/config/ClusterConfiguration.java
+++ timelock-agent/src/main/java/com/palantir/timelock/config/ClusterConfiguration.java
@@ -16,8 +16,6 @@ import com.palantir.remoting2.config.service.ServiceConfiguration;
 @JsonSerialize(as = ImmutableClusterConfiguration.class)
 @Value.Immutable
 public interface ClusterConfiguration {
-    // in remoting3 this class is officially public API
-    // TODO: do we want to live reload this? probably not? how do we make a timelock cluster bigger?
     ServiceConfiguration cluster();
 
     @JsonProperty("local-server")
--- timelock-agent/src/main/java/com/palantir/timelock/config/TimeLockRuntimeConfiguration.java
+++ timelock-agent/src/main/java/com/palantir/timelock/config/TimeLockRuntimeConfiguration.java
@@ -25,18 +25,6 @@ public abstract class TimeLockRuntimeConfiguration {
     private static final String CLIENT_NAME_REGEX = "[a-zA-Z0-9_-]+";
 
     abstract Optional<TimeLockAlgorithmRuntimeConfiguration> algorithm();
-//
-//    Per JKong: "TimeLimiter might not be needed"
-//    /**
-//     * Returns a value indicating the margin of error we leave before interrupting a long running request,
-//     * since we wish to perform this interruption and return a BlockingTimeoutException _before_ Jetty closes the
-//     * stream. This margin is specified as a ratio of the smallest idle timeout - hence it must be in (0, 1).
-//     */
-//    @JsonProperty("blocking-timeout-error-margin")
-//    @Value.Default
-//    Optional<Double> blockingTimeoutErrorMargin() {
-//        return Optional.of(0.03);
-//    }
 
     abstract Set<String> clients();
 
@@ -49,13 +37,6 @@ public abstract class TimeLockRuntimeConfiguration {
     long slowLockLogTriggerMillis() {
         return 10000;
     }
-//
-//    Per NZiebart: "I think we should just remove the ability to configure that going forward"
-//    @JsonProperty("use-client-request-limit")
-//    @Value.Default
-//    boolean useClientRequestLimit() {
-//        return false;
-//    }
 
     @Value.Check
     void check() {
@@ -66,9 +47,5 @@ public abstract class TimeLockRuntimeConfiguration {
         Preconditions.checkState(!clients().contains(PaxosTimeLockConstants.LEADER_ELECTION_NAMESPACE),
                 "The client name '%s' is reserved for the leader election service, and may not be used.",
                 PaxosTimeLockConstants.LEADER_ELECTION_NAMESPACE);
-//
-//        Preconditions.checkState(blockingTimeoutErrorMargin() > 0 && blockingTimeoutErrorMargin() < 1,
-//                "Lock service timeout margin must be strictly between 0 and 1. Illegal timeout: %s",
-//                blockingTimeoutErrorMargin());
     }
 }
