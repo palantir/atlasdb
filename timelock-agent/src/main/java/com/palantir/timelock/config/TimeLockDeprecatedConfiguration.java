8af3ec57cf054c5eacaebe5b938c31340e0c7976
Jeremy Kong <jeremykong@hotmail.com>
2017-07-18 15:58:04 +0100

    Add simple validation to TLDC and a test

--- timelock-agent/src/main/java/com/palantir/timelock/config/TimeLockDeprecatedConfiguration.java
+++ timelock-agent/src/main/java/com/palantir/timelock/config/TimeLockDeprecatedConfiguration.java
@@ -6,8 +6,11 @@ package com.palantir.timelock.config;
 
 import org.immutables.value.Value;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.base.Preconditions;
+import com.palantir.atlasdb.timelock.paxos.PaxosTimeLockConstants;
 
 /**
  * Items in {@link TimeLockServerConfiguration} that are in neither {@link TimeLockInstallConfiguration} nor
@@ -17,23 +20,35 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 @JsonSerialize(as = ImmutableTimeLockDeprecatedConfiguration.class)
 @Value.Immutable
 public abstract class TimeLockDeprecatedConfiguration {
+    @JsonProperty("use-client-request-limit")
     @Value.Default
     public boolean useClientRequestLimit() {
         return false;
     }
 
+    @JsonProperty("available-threads")
     @Value.Default
     public int availableThreads() {
         return 1024;
     }
 
+    @JsonProperty("use-lock-time-limiter")
     @Value.Default
     public boolean useLockTimeLimiter() {
         return false;
     }
 
+    @JsonProperty("blocking-timeout-in-ms")
     @Value.Default
     public long blockingTimeoutInMs() {
         return Long.MAX_VALUE;
+    }
+
+    @Value.Check
+    public void check() {
+        Preconditions.checkState(availableThreads() >= 0,
+                "Number of available threads must be nonnegative, but found %s", availableThreads());
+        Preconditions.checkState(blockingTimeoutInMs() >= 0,
+                "Blocking timeout must be nonnegative, but found %s", blockingTimeoutInMs());
     }
 }
