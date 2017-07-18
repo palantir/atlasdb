adff60d0a6eded699acf494fd1cce238ffff0cc4
Jeremy Kong <jeremykong@hotmail.com>
2017-07-18 14:40:53 +0100

    ProfilingKVS uses safe args

--- atlasdb-client/src/main/java/com/palantir/atlasdb/logging/LoggingArgs.java
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/logging/LoggingArgs.java
@@ -45,6 +45,13 @@ public final class LoggingArgs {
         logArbitrator = arbitrator;
     }
 
+    /**
+     * Returns a safe or unsafe arg corresponding to the supplied table reference, with name "tableRef".
+     */
+    public static Arg<TableReference> tableRef(TableReference tableReference) {
+        return tableRef("tableRef", tableReference);
+    }
+
     public static Arg<TableReference> tableRef(String argName, TableReference tableReference) {
         return getArg(argName, tableReference, logArbitrator.isTableReferenceSafe(tableReference));
     }
