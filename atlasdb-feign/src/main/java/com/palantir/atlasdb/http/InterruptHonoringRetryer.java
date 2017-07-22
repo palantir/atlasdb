08d63bbd7963c97a9bca4c068cf7a8322248f966
Nathan Ziebart <nziebart@palantir.com>
2017-07-21 20:43:32 -0700

    honor interrupts

---
+++ atlasdb-feign/src/main/java/com/palantir/atlasdb/http/InterruptHonoringRetryer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.http;
+
+import java.io.InterruptedIOException;
+
+import feign.RetryableException;
+import feign.Retryer;
+
+/**
+ * Simple wrapper around the default Feign retryer that propagates {@link InterruptedIOException}s
+ * rather than retrying them. This allows the request thread to be interrupted.
+ */
+public class InterruptHonoringRetryer implements Retryer {
+
+    private final Retryer delegate = new Retryer.Default();
+
+    @Override
+    public void continueOrPropagate(RetryableException e) {
+        if (e.getCause() instanceof InterruptedIOException) {
+            throw new RuntimeException(e.getCause());
+        }
+
+        delegate.continueOrPropagate(e);
+    }
+
+    @Override
+    public Retryer clone() {
+        return new InterruptHonoringRetryer();
+    }
+}
---
+++ atlasdb-feign/src/test/java/com/palantir/atlasdb/http/InterruptHonoringRetryerTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.http;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.InterruptedIOException;
+import java.sql.Date;
+import java.time.Instant;
+
+import org.junit.Test;
+
+import feign.RetryableException;
+
+public class InterruptHonoringRetryerTest {
+
+    private final InterruptHonoringRetryer retryer = new InterruptHonoringRetryer();
+
+    @Test
+    public void doesNotRetryInterruptedIoExceptions() {
+        RetryableException ex = retryable(new InterruptedIOException());
+
+        assertThatThrownBy(() -> retryer.continueOrPropagate(ex))
+                .hasCauseInstanceOf(InterruptedIOException.class);
+    }
+
+    @Test
+    public void retriesOtherRetryableExceptions() {
+        RetryableException ex = retryable(new RuntimeException());
+
+        retryer.continueOrPropagate(ex);
+    }
+
+    private RetryableException retryable(Throwable ex) {
+        return new RetryableException("", ex, Date.from(Instant.EPOCH));
+    }
+
+}
