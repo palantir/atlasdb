af587227743ba8e7cc93992520cf4d47e0f2cb03
amarzoca <marzoca.andreea@gmail.com>
2017-08-08 18:01:25 +0100

    Fix unused import

--- atlasdb-client/src/main/java/com/palantir/atlasdb/table/description/Schema.java
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/table/description/Schema.java
@@ -19,7 +19,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
