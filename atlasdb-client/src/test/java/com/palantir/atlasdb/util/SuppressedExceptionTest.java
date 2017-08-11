07f2b4210d4d3bcc278a08e4f8981d7a220f4c5a
Samuel Souza <ssouza@palantir.com>
2017-08-11 13:20:21 +0100

    Address pr comments

--- atlasdb-client/src/test/java/com/palantir/atlasdb/util/SuppressedExceptionTest.java
+++ atlasdb-client/src/test/java/com/palantir/atlasdb/util/SuppressedExceptionTest.java
@@ -17,13 +17,20 @@
 package com.palantir.atlasdb.util;
 
 import org.junit.Test;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SuppressedExceptionTest {
+    private static final Logger log = LoggerFactory.getLogger("AnnotatedCallable");
+
     @Test
     public void throwableSuppressingItselfShouldBeLoggable() {
+        // When logging exceptions with circular dependencies, logback can cause stack overflow. See
+        // https://jira.qos.ch/browse/LOGBACK-1027. This tests asserts that there are no circular dependencies when
+        // using SuppressedException, i.e., logback logs successfully.
+
         Throwable throwable = new Throwable("a exception");
         throwable.addSuppressed(SuppressedException.from(throwable));
-        LoggerFactory.getLogger("AnnotatedCallable").error("Error", throwable);
+        log.error("Error", throwable);
     }
 }
