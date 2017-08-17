34d999e9d90ccdb616cd63aa4da7cc2362d57771
Jeremy Kong <jeremykong@hotmail.com>
2017-08-17 11:17:27 +0100

    fix broken tests, part 1

--- atlasdb-dagger/src/main/java/com/palantir/atlasdb/services/ServicesConfig.java
+++ atlasdb-dagger/src/main/java/com/palantir/atlasdb/services/ServicesConfig.java
@@ -34,7 +34,10 @@ public abstract class ServicesConfig {
 
     @Value.Derived
     public ServiceDiscoveringAtlasSupplier atlasDbSupplier() {
-        return new ServiceDiscoveringAtlasSupplier(atlasDbConfig().keyValueService(), atlasDbConfig().leader());
+        return new ServiceDiscoveringAtlasSupplier(
+                atlasDbConfig().keyValueService(),
+                atlasDbConfig().leader(),
+                atlasDbConfig().namespace());
     }
 
     @Value.Default
