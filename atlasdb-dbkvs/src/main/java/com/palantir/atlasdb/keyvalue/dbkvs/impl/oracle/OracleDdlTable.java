48307dcde99b6ed809fcbd7d8175ff17a7206d92
Himangi Saraogi <hsaraogi@palantir.com>
2017-08-29 16:17:54 +0100

    Minor cleanup

--- atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/oracle/OracleDdlTable.java
+++ atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/oracle/OracleDdlTable.java
@@ -239,19 +239,23 @@ public final class OracleDdlTable implements DbDdlTable {
 
     @Override
     public void compactInternally() {
+        final String compactionFailureTemplate = "Tried to clean up {} bloat after a sweep operation,"
+                + " but underlying Oracle database or configuration does not support this {} feature online. "
+                + " Since this can't be automated in your configuration,"
+                + " good practice would be do to occasional offline manual maintenance of rebuilding"
+                + " IOT tables to compensate for bloat. You can contact Palantir Support if you'd"
+                + " like more information. Underlying error was: {}";
+
         if (config.enableOracleEnterpriseFeatures()) {
             try {
                 conns.get().executeUnregisteredQuery(
                         "ALTER TABLE " + oracleTableNameGetter.getInternalShortTableName(conns, tableRef)
                                 + " MOVE ONLINE");
             } catch (PalantirSqlException e) {
-                log.error("Tried to clean up {} bloat after a sweep operation,"
-                        + " but underlying Oracle database or configuration does not support this"
-                        + " (Enterprise Edition that requires this user to be able to perform DDL operations)"
-                        + " feature online. Since this can't be automated in your configuration,"
-                        + " good practice would be do to occasional offline manual maintenance of rebuilding"
-                        + " IOT tables to compensate for bloat. You can contact Palantir Support if you'd"
-                        + " like more information. Underlying error was: {}", tableRef, e.getMessage());
+                log.error(compactionFailureTemplate,
+                        tableRef,
+                        "(Enterprise Edition that requires this user to be able to perform DDL operations)",
+                        e.getMessage());
             } catch (TableMappingNotFoundException e) {
                 throw Throwables.propagate(e);
             }
@@ -264,15 +268,10 @@ public final class OracleDdlTable implements DbDdlTable {
                         "ALTER TABLE " + oracleTableNameGetter.getInternalShortTableName(conns, tableRef)
                                 + " SHRINK SPACE");
             } catch (PalantirSqlException e) {
-                log.error("Tried to clean up {} bloat after a sweep operation,"
-                                + " but underlying Oracle database or configuration does not support this"
-                                + " (If you are running against Enterprise Edition,"
-                                + " you can set enableOracleEnterpriseFeatures to true in the configuration.)"
-                                + " feature online. Since this can't be automated in your configuration,"
-                                + " good practice would be do to occasional offline manual maintenance of rebuilding"
-                                + " IOT tables to compensate for bloat. You can contact Palantir Support if you'd"
-                                + " like more information. Underlying error was: {}",
+                log.error(compactionFailureTemplate,
                         tableRef,
+                        "(If you are running against Enterprise Edition,"
+                                + " you can set enableOracleEnterpriseFeatures to true in the configuration.)",
                         e.getMessage());
             } catch (TableMappingNotFoundException e) {
                 throw new RuntimeException(e);
