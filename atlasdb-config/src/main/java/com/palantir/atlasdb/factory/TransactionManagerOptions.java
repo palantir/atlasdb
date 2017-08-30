214c1e11121cd8d3e2ca02d8ab31ff4ecb8b1c49
Tom Petracca <tpetracca@palantir.com>
2017-08-30 14:53:47 -0400

    introduce immutable options parament for TM creation

---
+++ atlasdb-config/src/main/java/com/palantir/atlasdb/factory/TransactionManagerOptions.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ */
+
+package com.palantir.atlasdb.factory;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
+
+import org.immutables.value.Value;
+
+import com.palantir.atlasdb.config.AtlasDbConfig;
+import com.palantir.atlasdb.config.AtlasDbRuntimeConfig;
+import com.palantir.atlasdb.factory.TransactionManagers.Environment;
+import com.palantir.atlasdb.http.UserAgents;
+import com.palantir.atlasdb.table.description.Schema;
+import com.palantir.lock.LockServerOptions;
+
+@Value.Immutable
+public interface TransactionManagerOptions {
+    AtlasDbConfig config();
+
+    Supplier<Optional<AtlasDbRuntimeConfig>> runtimeConfigSupplier();
+
+    Set<Schema> schemas();
+
+    Environment env();
+
+    @Value.Default
+    default LockServerOptions lockServerOptions() {
+        return LockServerOptions.DEFAULT;
+    }
+
+    @Value.Default
+    default boolean allowHiddenTableAccess() {
+        return false;
+    }
+
+    Optional<Class<?>> callingClass();
+
+    Optional<String> userAgent();
+
+    // directly specified -> inferred from caller -> default
+    @Value.Derived
+    default String derivedUserAgent() {
+        return userAgent().orElse(callingClass().map(UserAgents::fromClass).orElse(UserAgents.DEFAULT_USER_AGENT));
+    }
+
+    static Builder builder() {
+        return new Builder();
+    }
+
+    class Builder extends ImmutableTransactionManagerOptions.Builder {}
+}
