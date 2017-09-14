9186f677f52af9780c0ff863fab8fe0442bfcf62
Nathan Ziebart <nziebart@palantir.com>
2017-09-14 19:38:47 +0100

    checkstyle

--- timelock-server/src/integTest/java/com/palantir/atlasdb/timelock/TestableTimelockCluster.java
+++ timelock-server/src/integTest/java/com/palantir/atlasdb/timelock/TestableTimelockCluster.java
@@ -79,7 +79,6 @@ public class TestableTimelockCluster {
                         timestampService.getFreshTimestamp();
                         return true;
                     } catch (Throwable t) {
-                        t.printStackTrace();
                         return false;
                     }
                 });
