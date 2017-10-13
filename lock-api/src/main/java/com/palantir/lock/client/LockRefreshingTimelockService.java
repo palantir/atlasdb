5b987b5059b8258498d2f232ba905577b4593e0f
Ben Grabham <bgrabham@palantir.com>
2017-10-13 13:01:35 +0100

    [no release notes] Remove Exception on Lock close methods

--- lock-api/src/main/java/com/palantir/lock/client/LockRefreshingTimelockService.java
+++ lock-api/src/main/java/com/palantir/lock/client/LockRefreshingTimelockService.java
@@ -107,7 +107,7 @@ public class LockRefreshingTimelockService implements AutoCloseable, TimelockSer
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         lockRefresher.close();
     }
 }
