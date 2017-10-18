53b8cbf2f93cdc4bb42a32f40af1037a466f23e6
Greg Bonik <gregory@bonik.org>
2017-10-18 04:56:59 -0700

    DbKvs sweep refactors to address last PR comments (#2497)
    * DbKvs sweep refactors to address last PR comments
    
    As dxiao pointed out, CandidatePagingState does too many things. The
    solution is to make CellTsPairLoader return an iterator and move all
    paging logic inside that iterator, and remove CellTsPairLoaderFactory.
    Then we replace CandidatePagingState with CandidateGroupingIterator,
    which now doesn't have any paging logic and only takes care of grouping
    (cell, ts) pairs by cell.
    
    Also remove CandidatePageJoiningIterator because it's not used anymore.
    We could have done this earlier but I forgot.
    
    Plan for the future:
    - Since we gave up on the in-database filtering idea, we can replace
      KVS.getCandidateCellsForSweeping() with a simpler call like
      getCellTsPairs() which would do what CellTsPairLoader does now.
    - Implement the new call for the remaining KVS's. We need at least
      the InMemoryKVS. We should decide the destiny of JdbcKVS and CqlKVS.
    - Remove all remaining usages of getRangeOfTimestamps(). I think that's
      basically deleteRange() on Cassandra.
    - Remove KVS.getRangeOfTimestamps()!
    
    [no release notes]
    
    * Renames
    
    * Create OracleCellTsPageLoader.PageIterator.Token class
    
    * Create PostgresCellTsPageLaoder.PageIterator.Token
    
    * Fix Precondition
    
    having-next-ness is a state, not an argument
    
    * One Token To Rull Them All
    
    Well... two, actually. This one is a CellTsPairToken. We already had a
    Token class in DbKvs :-/
    
    * Make CellTsPairToken final; replace modification with factory methods
    
    * Have computeNextStartPosition return a token
    
    * Remove the TODO
    
    I don't think we can do it nicely
    
    * empty commit
    
    * Nits
    
    * Move duplicated precondition to CellTsPairToken and add error message
    * Renames
    
    * Make CellTsPairToken immutable
    
    * Sort timestamps array, just in case

--- atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/sweep/DbKvsGetCandidateCellsForSweeping.java
+++ atlasdb-dbkvs/src/main/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/sweep/DbKvsGetCandidateCellsForSweeping.java
@@ -18,61 +18,26 @@ package com.palantir.atlasdb.keyvalue.dbkvs.impl.sweep;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
-import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.Iterators;
 import com.palantir.atlasdb.keyvalue.api.CandidateCellForSweeping;
 import com.palantir.atlasdb.keyvalue.api.CandidateCellForSweepingRequest;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
-import com.palantir.atlasdb.keyvalue.dbkvs.impl.sweep.CandidatePagingState.StartingPosition;
 
 public class DbKvsGetCandidateCellsForSweeping {
 
-    private final CellTsPairLoaderFactory cellTsPairLoaderFactory;
+    private final CellTsPairLoader cellTsPairLoader;
 
-    public DbKvsGetCandidateCellsForSweeping(CellTsPairLoaderFactory cellTsPairLoaderFactory) {
-        this.cellTsPairLoaderFactory = cellTsPairLoaderFactory;
+    public DbKvsGetCandidateCellsForSweeping(CellTsPairLoader cellTsPairLoader) {
+        this.cellTsPairLoader = cellTsPairLoader;
     }
 
     public Iterator<List<CandidateCellForSweeping>> getCandidateCellsForSweeping(
             TableReference tableRef,
             CandidateCellForSweepingRequest request) {
-        CellTsPairLoader loader = cellTsPairLoaderFactory.createCellTsLoader(tableRef, request);
-        CandidatePagingState state = CandidatePagingState.create(request.startRowInclusive());
-        return Iterators.filter(new PageIterator(loader, state), page -> !page.isEmpty());
+        Iterator<List<CellTsPairInfo>> cellTsIter = cellTsPairLoader.createPageIterator(tableRef, request);
+        Iterator<List<CandidateCellForSweeping>> rawIter = CandidateGroupingIterator.create(cellTsIter);
+        return Iterators.filter(rawIter, page -> !page.isEmpty());
     }
 
-    private class PageIterator extends AbstractIterator<List<CandidateCellForSweeping>> {
-        private final CellTsPairLoader pageLoader;
-        private final CandidatePagingState state;
-
-        PageIterator(CellTsPairLoader pageLoader, CandidatePagingState state) {
-            this.pageLoader = pageLoader;
-            this.state = state;
-        }
-
-        @Override
-        protected List<CandidateCellForSweeping> computeNext() {
-            Optional<StartingPosition> startingPosition = state.getNextStartingPosition();
-            if (startingPosition.isPresent()) {
-                return loadNextPage(startingPosition.get());
-            } else {
-                return endOfData();
-            }
-        }
-
-        private List<CandidateCellForSweeping> loadNextPage(StartingPosition startingPosition) {
-            CellTsPairLoader.Page page = pageLoader.loadNextPage(
-                    startingPosition, state.getCellTsPairsExaminedInCurrentRow());
-            List<CandidateCellForSweeping> candidates = state.processBatch(
-                    page.entries, page.reachedEnd);
-            if (page.reachedEnd && page.scannedSingleRow) {
-                // If we reached the end of results in the single-row mode, it doesn't mean that we reached
-                // the end of the table. We need to restart from the next row in the normal mode.
-                state.restartFromNextRow();
-            }
-            return candidates;
-        }
-    }
 }
