766d2b023e37bac03dfdad5c37ba79b3c946cd35
Glenn Sheasby <gsheasby@palantir.com>
2017-11-01 14:50:52 +0000

    Add some RangeRequest.inRange tests

--- atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/keyvalue/impl/RangeRequestTest.java
+++ atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/keyvalue/impl/RangeRequestTest.java
@@ -16,38 +16,82 @@
 package com.palantir.atlasdb.keyvalue.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import com.palantir.atlasdb.encoding.PtBytes;
 import com.palantir.atlasdb.keyvalue.api.RangeRequest;
 import com.palantir.atlasdb.keyvalue.api.RangeRequests;
 
 public class RangeRequestTest {
+    private static final byte[] START = PtBytes.toBytes("commencement");
+    private static final byte[] END = PtBytes.toBytes("terminus");
+    private static final RangeRequest BOUNDED_REQUEST = RangeRequest.builder()
+            .startRowInclusive(START)
+            .endRowExclusive(END)
+            .build();
 
     @Test
     public void testPrefix() {
-        byte[] end = RangeRequest.builder().prefixRange(new byte[] {-1}).build().getEndExclusive();
-        assertEquals(0, end.length);
-        end = RangeRequest.builder().prefixRange(new byte[] {-2}).build().getEndExclusive();
-        assertEquals(1, end.length);
-        assertEquals(-1, end[0]);
+        byte[] endExclusive = RangeRequest.builder().prefixRange(new byte[] {-1}).build().getEndExclusive();
+        assertEquals(0, endExclusive.length);
+        endExclusive = RangeRequest.builder().prefixRange(new byte[] {-2}).build().getEndExclusive();
+        assertEquals(1, endExclusive.length);
+        assertEquals(-1, endExclusive[0]);
 
-        end = RangeRequest.builder().prefixRange(new byte[] {0, -1}).build().getEndExclusive();
-        assertEquals(1, end.length);
-        assertEquals(1, end[0]);
+        endExclusive = RangeRequest.builder().prefixRange(new byte[] {0, -1}).build().getEndExclusive();
+        assertEquals(1, endExclusive.length);
+        assertEquals(1, endExclusive[0]);
 
-        end = RangeRequest.builder().prefixRange(new byte[] {0, -1, 0}).build().getEndExclusive();
-        assertEquals(3, end.length);
-        assertEquals(1, end[2]);
+        endExclusive = RangeRequest.builder().prefixRange(new byte[] {0, -1, 0}).build().getEndExclusive();
+        assertEquals(3, endExclusive.length);
+        assertEquals(1, endExclusive[2]);
     }
 
     @Test
     public void testEmpty() {
-        RangeRequest request = RangeRequest.builder().endRowExclusive(RangeRequests.getFirstRowName()).build();
-        Assert.assertTrue(request.isEmptyRange());
-        request = RangeRequest.reverseBuilder().endRowExclusive(RangeRequests.getLastRowName()).build();
-        Assert.assertTrue(request.isEmptyRange());
+        RangeRequest emptyRequest = RangeRequest.builder().endRowExclusive(RangeRequests.getFirstRowName()).build();
+        assertTrue(emptyRequest.isEmptyRange());
+        emptyRequest = RangeRequest.reverseBuilder().endRowExclusive(RangeRequests.getLastRowName()).build();
+        assertTrue(emptyRequest.isEmptyRange());
     }
 
+    @Test
+    public void testValueInBoundedRange() {
+        assertTrue(BOUNDED_REQUEST.inRange(PtBytes.toBytes("middle")));
+    }
+
+    @Test
+    public void testValueBeforeBoundedRange() {
+        assertFalse(BOUNDED_REQUEST.inRange(PtBytes.toBytes("an early value")));
+    }
+
+    @Test
+    public void testValueAfterBoundedRange() {
+        assertFalse(BOUNDED_REQUEST.inRange(PtBytes.toBytes("too late")));
+    }
+
+    @Test
+    public void testStartIsInclusive() {
+        assertTrue(BOUNDED_REQUEST.inRange(START));
+    }
+
+    @Test
+    public void testEndIsExclusive() {
+        assertFalse(BOUNDED_REQUEST.inRange(END));
+    }
+
+    @Test
+    public void testValueBeforeRangeBoundedByStartOnly() {
+        RangeRequest request = RangeRequest.builder().startRowInclusive(START).build();
+        assertFalse(request.inRange(PtBytes.toBytes("an early value")));
+    }
+
+    @Test
+    public void testValueAfterRangeBoundedByStartOnly() {
+        RangeRequest request = RangeRequest.builder().startRowInclusive(START).build();
+        assertTrue(request.inRange(END));
+    }
 }
