2e2bb6f89dd3e45da380d3ace8adc115ddc42501
Jeremy Kong <jeremykong@hotmail.com>
2017-11-04 15:05:59 +0000

    0 nodes part 1

--- atlasdb-config/src/main/java/com/palantir/atlasdb/config/AtlasDbConfig.java
+++ atlasdb-config/src/main/java/com/palantir/atlasdb/config/AtlasDbConfig.java
@@ -279,6 +279,14 @@ public abstract class AtlasDbConfig {
     private void checkLockAndTimestampBlocks() {
         Preconditions.checkState(lock().isPresent() == timestamp().isPresent(),
                 "Lock and timestamp server blocks must either both be present or both be absent.");
+        checkServersListHasAtLeastOneServerIfPresent(lock());
+        checkServersListHasAtLeastOneServerIfPresent(timestamp());
+    }
+
+    private static void checkServersListHasAtLeastOneServerIfPresent(Optional<ServerListConfig> serverListOptional) {
+        serverListOptional.ifPresent(
+                serverList -> Preconditions.checkState(serverList.hasAtLeastOneServer(),
+                        "Server list must have at least one server."));
     }
 
     private void checkNamespaceConfig() {
