8e74682b0bccffc1579ea967f5132ff22552a5cc
Jeremy Kong <jeremykong@hotmail.com>
2017-11-09 18:08:52 +0000

    logging

--- atlasdb-feign/src/main/java/com/palantir/atlasdb/http/PollingRefreshable.java
+++ atlasdb-feign/src/main/java/com/palantir/atlasdb/http/PollingRefreshable.java
@@ -22,19 +22,31 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.annotations.VisibleForTesting;
 import com.palantir.common.concurrent.NamedThreadFactory;
+import com.palantir.logsafe.UnsafeArg;
 import com.palantir.remoting3.ext.refresh.Refreshable;
 
+/**
+ * A PollingRefreshable serves as a bridge between a {@link Supplier} and {@link Refreshable}, polling for changes
+ * in the value of the Supplier and, if detecting a change, pushing it to the linked Refreshable.
+ *
+ * @param <T> type of the value supplied / pushed to the Refreshable
+ */
 public final class PollingRefreshable<T> implements AutoCloseable {
     // TODO (jkong): Should this be configurable?
     @VisibleForTesting
     static final Duration POLL_INTERVAL = Duration.ofSeconds(5L);
 
+    private static final Logger log = LoggerFactory.getLogger(PollingRefreshable.class);
+
     private final Supplier<T> supplier;
     private final ScheduledExecutorService poller;
 
-    private final Refreshable<T> refreshable;
+    private final Refreshable<T> refreshable = Refreshable.empty();
 
     private volatile T lastSeenValue;
 
@@ -44,11 +56,11 @@ public final class PollingRefreshable<T> implements AutoCloseable {
 
         try {
             lastSeenValue = supplier.get();
+            refreshable.set(lastSeenValue);
         } catch (Exception e) {
+            log.info("Exception occurred in supplier when trying to populate the initial value.");
             lastSeenValue = null;
         }
-
-        refreshable = lastSeenValue == null ? Refreshable.empty() : Refreshable.of(lastSeenValue);
     }
 
     public static <T> PollingRefreshable<T> create(Supplier<T> supplier) {
@@ -69,10 +81,17 @@ public final class PollingRefreshable<T> implements AutoCloseable {
 
     private void scheduleUpdates() {
         poller.scheduleAtFixedRate(() -> {
-            T value = supplier.get();
-            if (!value.equals(lastSeenValue)) {
-                lastSeenValue = value;
-                refreshable.set(lastSeenValue);
+            try {
+                T value = supplier.get();
+                if (!value.equals(lastSeenValue)) {
+                    lastSeenValue = value;
+                    refreshable.set(lastSeenValue);
+                }
+            } catch (Exception e) {
+                log.info("Exception occurred in supplier when polling for a new value in our PollingRefreshable."
+                        + " The last value we saw was {}.",
+                        UnsafeArg.of("currentValue", lastSeenValue),
+                        e);
             }
         }, POLL_INTERVAL.getSeconds(), POLL_INTERVAL.getSeconds(), TimeUnit.SECONDS);
     }
