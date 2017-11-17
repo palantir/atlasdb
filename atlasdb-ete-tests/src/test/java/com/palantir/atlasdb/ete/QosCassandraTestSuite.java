08a07b2f503fce1fc9de205cd009be81c9aa2f7a
Himangi Saraogi <hsaraogi@palantir.com>
2017-11-17 20:36:26 +0000

    Test shouldFailIfReadingTooManyBytes

--- atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/QosCassandraTestSuite.java
+++ atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/QosCassandraTestSuite.java
@@ -19,21 +19,23 @@ import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
+import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.palantir.atlasdb.containers.CassandraEnvironment;
 import com.palantir.atlasdb.todo.ImmutableTodo;
 import com.palantir.atlasdb.todo.Todo;
 import com.palantir.atlasdb.todo.TodoResource;
-import com.palantir.atlasdb.util.AtlasDbMetrics;
 
 public class QosCassandraTestSuite extends EteSetup {
     private static final List<String> CLIENTS = ImmutableList.of("ete1");
-    private static final Todo TODO = ImmutableTodo.of(String.join("", Collections.nCopies(100_000, "a")));
 
     @ClassRule
     public static final RuleChain COMPOSITION_SETUP = EteSetup.setupComposition(
@@ -45,8 +47,29 @@ public class QosCassandraTestSuite extends EteSetup {
     @Test
     public void shouldFailIfWritingTooManyBytes() {
         TodoResource todoClient = EteSetup.createClientToSingleNode(TodoResource.class);
-        todoClient.addTodo(TODO);
-        assertThatThrownBy(() -> todoClient.addTodo(TODO))
-                .isInstanceOf(RuntimeException.class);
+        // Doesn't throw the first time as we estimate low values
+        todoClient.addTodo(getTodoOfSize(100_000));
+        assertThatThrownBy(() -> todoClient.addTodo(getTodoOfSize(100_000)))
+                .isInstanceOf(RuntimeException.class)
+                .as("Cannot write 100_000 bytes the second time as write limit of 1000 was consumed and the burst isnt enough either.");
+    }
+
+    @Test
+    public void shouldFailIfReadingTooManyBytes() throws InterruptedException {
+        TodoResource todoClient = EteSetup.createClientToSingleNode(TodoResource.class);
+        IntStream.range(0, 30).forEach(i -> todoClient.addTodo(getTodoOfSize(1_000)));
+
+        assertThatThrownBy(todoClient::getTodoList)
+                .isInstanceOf(RuntimeException.class)
+                .as("Cant read 30_000 bytes in 10 batches i.e. 3000 bytes multiple times when limit is 1000.");
+    }
+
+    @After
+    public void after() {
+        Uninterruptibles.sleepUninterruptibly(10, TimeUnit.SECONDS);
+    }
+
+    private Todo getTodoOfSize(int size) {
+        return ImmutableTodo.of(String.join("", Collections.nCopies(size, "a")));
     }
 }
