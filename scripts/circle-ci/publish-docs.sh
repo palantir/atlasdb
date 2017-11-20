257ea5ad487bb6eaa6dcb4a30692ecc75b9f93ec
Samuel Souza <ssouza@palantir.com>
2017-11-20 21:19:44 +0000

    Simplify internal docs publishing

--- scripts/circle-ci/publish-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO(ssouza): merge this file and publish-github-page.sh
-
-set -x -e
-
-[[ $CIRCLE_NODE_INDEX == 0 ]] && curl -s --fail $DOCS_URL | bash -s -- -r requirements.txt $CIRCLE_BRANCH
-
