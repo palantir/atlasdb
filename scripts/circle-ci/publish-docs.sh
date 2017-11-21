564cc6e8442a8674e07ba1cf706e2127c26e47e8
Samuel Souza <fsamuel-bs@users.noreply.github.com>
2017-11-21 07:40:50 +0000

    Fix the docs publication part 2 (#2722)
    * the curl should run the whole build
    
    * Simplify internal docs publishing
    
    * [no release notes]
    
    * Publish docs as a script

--- scripts/circle-ci/publish-docs.sh
+++ scripts/circle-ci/publish-docs.sh
@@ -1,33 +1,6 @@
 #!/usr/bin/env bash
 
-# TODO(ssouza): merge this file and publish-github-page.sh
-
 set -x -e
 
-# Just-in-time install of sphinx so that docs build
-sudo -H pip install --disable-pip-version-check --upgrade sphinx sphinx_rtd_theme requests recommonmark
+[[ $CIRCLE_NODE_INDEX == 0 ]] && curl -s --fail $DOCS_URL | bash -s -- -r requirements.txt $CIRCLE_BRANCH
 
-# Clone gh-pages into build
-cd docs/
-rm -rf build/
-git clone git@github.com:palantir/atlasdb.git -b gh-pages build
-
-# Rebuild the docs into the repo
-make html || { echo "doc build failed, build should not pass"; exit 1; }
-
-cd build/
-# Just to report status of repo in the build output
-git status
-git config user.email "pd-atlasdb-team@palantir.com"
-git config user.name $CIRCLE_PROJECT_USERNAME
-
-# Add and commmit changes to gh-pages
-git add .
-
-if ! git commit -m "circle-publish: [skip ci] from $CIRCLE_BRANCH by build $CIRCLE_BUILD_NUM, can be found at $CIRCLE_BUILD_URL"; then
-    echo "no docs changes, skipping deployment";
-    exit 0;
-fi
-
-cd ..
-curl -s --fail $DOCS_URL | bash -s -- -r requirements.txt $CIRCLE_BRANCH
