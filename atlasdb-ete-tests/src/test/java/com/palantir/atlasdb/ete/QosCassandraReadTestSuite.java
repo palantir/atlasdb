8eef5d5f2e7a374c842ad781f5f173753d143fd4
Himangi Saraogi <hsaraogi@palantir.com>
2017-11-21 14:26:22 +0000

    create tm in test

--- atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/QosCassandraReadTestSuite.java
+++ atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/QosCassandraReadTestSuite.java
@@ -15,56 +15,156 @@
  */
 package com.palantir.atlasdb.ete;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.InetSocketAddress;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
 
-import org.junit.After;
+import org.awaitility.Awaitility;
+import org.awaitility.Duration;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.rules.RuleChain;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.util.concurrent.Uninterruptibles;
-import com.palantir.atlasdb.containers.CassandraEnvironment;
+import com.google.common.collect.ImmutableMap;
+import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfig;
+import com.palantir.atlasdb.cassandra.ImmutableCassandraCredentialsConfig;
+import com.palantir.atlasdb.cassandra.ImmutableCassandraKeyValueServiceConfig;
+import com.palantir.atlasdb.config.AtlasDbConfig;
+import com.palantir.atlasdb.config.AtlasDbRuntimeConfig;
+import com.palantir.atlasdb.config.ImmutableAtlasDbConfig;
+import com.palantir.atlasdb.config.ImmutableAtlasDbRuntimeConfig;
+import com.palantir.atlasdb.config.ImmutableSweepConfig;
+import com.palantir.atlasdb.factory.TransactionManagers;
+import com.palantir.atlasdb.keyvalue.api.Cell;
+import com.palantir.atlasdb.keyvalue.api.RangeRequest;
+import com.palantir.atlasdb.keyvalue.api.RowResult;
+import com.palantir.atlasdb.qos.config.ImmutableQosClientConfig;
+import com.palantir.atlasdb.qos.config.ImmutableQosLimitsConfig;
+import com.palantir.atlasdb.table.description.ValueType;
 import com.palantir.atlasdb.todo.ImmutableTodo;
-import com.palantir.atlasdb.todo.Todo;
-import com.palantir.atlasdb.todo.TodoResource;
+import com.palantir.atlasdb.todo.TodoSchema;
+import com.palantir.atlasdb.transaction.impl.SerializableTransactionManager;
+import com.palantir.common.base.BatchingVisitable;
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.configuration.ShutdownStrategy;
+import com.palantir.docker.compose.connection.Container;
+import com.palantir.docker.compose.connection.DockerPort;
+import com.palantir.docker.compose.logging.LogDirectory;
 
 public class QosCassandraReadTestSuite extends EteSetup {
-    private static final List<String> CLIENTS = ImmutableList.of("ete1");
-    private TodoResource todoClient;
+    private static final Random random = new Random();
+    private static final int CASSANDRA_PORT_NUMBER = 9160;
+    private static SerializableTransactionManager serializableTransactionManager;
 
     @ClassRule
-    public static final RuleChain COMPOSITION_SETUP = EteSetup.setupComposition(
-            QosCassandraReadTestSuite.class,
-            "docker-compose.qos.cassandra.yml",
-            CLIENTS,
-            CassandraEnvironment.get());
+    public static DockerComposeRule docker = DockerComposeRule.builder()
+            .file("src/test/resources/cassandra-docker-compose.yml")
+            .waitingForService("cassandra", Container::areAllPortsOpen)
+            .saveLogsTo(LogDirectory.circleAwareLogDirectory(QosCassandraReadTestSuite.class))
+            .shutdownStrategy(ShutdownStrategy.AGGRESSIVE_WITH_NETWORK_CLEANUP)
+            .build();
 
     @Before
-    public void before() {
-        todoClient = EteSetup.createClientToSingleNode(TodoResource.class);
-        IntStream.range(0, 30).forEach(i -> todoClient.addTodo(getTodoOfSize(1_000)));
+    public void waitUntilTransactionManagersIsUp() {
+
+        serializableTransactionManager = TransactionManagers.builder()
+                .config(getAtlasDbConfig())
+                .runtimeConfigSupplier(() -> getAtlasDbRuntimeConfig())
+                .schemas(ImmutableList.of(TodoSchema.getSchema()))
+                .userAgent("qos-test")
+                .buildSerializable();
+
+        Awaitility.await()
+                .atMost(Duration.ONE_MINUTE)
+                .pollInterval(Duration.ONE_SECOND)
+                .until(serializableTransactionManager::isInitialized);
+
+        serializableTransactionManager.runTaskWithRetry((transaction) -> {
+            Cell thisCell = Cell.create(ValueType.FIXED_LONG.convertFromJava(random.nextLong()),
+                    TodoSchema.todoTextColumn());
+            Map<Cell, byte[]> write = ImmutableMap.of(thisCell, ValueType.STRING.convertFromJava(getTodoOfSize(100)));
+            transaction.put(TodoSchema.todoTable(), write);
+            return null;
+        });
     }
+
+    private AtlasDbConfig getAtlasDbConfig() {
+        DockerPort cassandraPort = docker.containers()
+                .container("cassandra")
+                .port(CASSANDRA_PORT_NUMBER);
+
+        InetSocketAddress cassandraAddress = new InetSocketAddress(cassandraPort.getIp(),
+                cassandraPort.getExternalPort());
+
+        CassandraKeyValueServiceConfig kvsConfig = ImmutableCassandraKeyValueServiceConfig.builder()
+                .servers(ImmutableList.of(cassandraAddress))
+                .credentials(ImmutableCassandraCredentialsConfig.builder()
+                        .username("cassandra")
+                        .password("cassandra")
+                        .build())
+                .ssl(false)
+                .replicationFactor(1)
+                .autoRefreshNodes(false)
+                .build();
+
+        return ImmutableAtlasDbConfig.builder()
+                .namespace("qosete")
+                .keyValueService(kvsConfig)
+                .initializeAsync(true)
+                .build();
+    }
+
+    private Optional<AtlasDbRuntimeConfig> getAtlasDbRuntimeConfig() {
+        return Optional.of(ImmutableAtlasDbRuntimeConfig.builder()
+                .sweep(ImmutableSweepConfig.builder().enabled(false).build())
+                .qos(ImmutableQosClientConfig.builder()
+                        .limits(ImmutableQosLimitsConfig.builder()
+                                .readBytesPerSecond(10_000)
+                                .writeBytesPerSecond(10_000)
+                                .build())
+                        .build())
+                .build());
+    }
+
+//    @Test
+//    public void shouldFailIfReadingTooManyBytes() throws InterruptedException {
+//        assertThatThrownBy(todoClient::getTodoList)
+//                .isInstanceOf(RuntimeException.class)
+//                .as("Cant read 30_000 bytes in 10 batches i.e. 3000 bytes multiple times when limit is 1000.");
+//    }
 
     @Test
-    public void shouldFailIfReadingTooManyBytes() throws InterruptedException {
-        assertThatThrownBy(todoClient::getTodoList)
-                .isInstanceOf(RuntimeException.class)
-                .as("Cant read 30_000 bytes in 10 batches i.e. 3000 bytes multiple times when limit is 1000.");
+    public void shouldBeAbleToReadSmallAmounts() {
+        ImmutableList<RowResult<byte[]>> results = serializableTransactionManager.runTaskWithRetry((transaction) -> {
+            BatchingVisitable<RowResult<byte[]>> rowResultBatchingVisitable = transaction.getRange(
+                    TodoSchema.todoTable(), RangeRequest.all());
+            ImmutableList.Builder<RowResult<byte[]>> rowResults = ImmutableList.builder();
+
+            rowResultBatchingVisitable.batchAccept(10, items -> {
+                rowResults.addAll(items);
+                return true;
+            });
+
+            return rowResults.build();
+        });
+
+        List<ImmutableTodo> collect = results.stream()
+                .map(RowResult::getOnlyColumnValue)
+                .map(ValueType.STRING::convertToString)
+                .map(ImmutableTodo::of)
+                .collect(Collectors.toList());
+
+        assertThat(collect).hasSize(1);
     }
 
-    @After
-    public void after() {
-        Uninterruptibles.sleepUninterruptibly(5, TimeUnit.SECONDS);
-    }
-
-    private Todo getTodoOfSize(int size) {
-        return ImmutableTodo.of(String.join("", Collections.nCopies(size, "a")));
+    private static String getTodoOfSize(int size) {
+        return String.join("", Collections.nCopies(size, "a"));
     }
 }
