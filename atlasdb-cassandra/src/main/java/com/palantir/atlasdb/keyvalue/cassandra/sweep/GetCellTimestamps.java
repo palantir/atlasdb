11192dd657f63090d0fe526dd28ac1de43c66442
nziebart <nathan.ziebart@gmail.com>
2017-11-28 12:33:16 -0600

    Nziebart/cell timestamps qos (#2745)
    * handle qos exceptions in cell timestamp loader [no release notes]
    
    * actually just remove checked exception
    
    * Remove the throws in the method signature

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/sweep/GetCellTimestamps.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/sweep/GetCellTimestamps.java
@@ -34,7 +34,6 @@ import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.keyvalue.cassandra.CqlExecutor;
 import com.palantir.atlasdb.keyvalue.cassandra.paging.RowGetter;
 import com.palantir.atlasdb.keyvalue.cassandra.thrift.SlicePredicates;
-import com.palantir.common.base.Throwables;
 
 public class GetCellTimestamps {
 
@@ -110,16 +109,12 @@ public class GetCellTimestamps {
     private Optional<byte[]> determineSafeRangeEnd(byte[] rangeStart) {
         KeyRange keyRange = new KeyRange().setStart_key(rangeStart).setEnd_key(new byte[0]).setCount(batchHint);
         SlicePredicate slicePredicate = SlicePredicates.create(SlicePredicates.Range.ALL, SlicePredicates.Limit.ONE);
-        try {
-            List<KeySlice> rows = rowGetter.getRows("getCandidateCellsForSweeping", keyRange, slicePredicate);
-            if (rows.isEmpty()) {
-                return Optional.empty();
-            } else {
-                return Optional.of(Iterables.getLast(rows).getKey());
-            }
-        } catch (Exception e) {
-            // TODO(nziebart): handle QoS exceptions here
-            throw Throwables.throwUncheckedException(e);
+
+        List<KeySlice> rows = rowGetter.getRows("getCandidateCellsForSweeping", keyRange, slicePredicate);
+        if (rows.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(Iterables.getLast(rows).getKey());
         }
     }
 
