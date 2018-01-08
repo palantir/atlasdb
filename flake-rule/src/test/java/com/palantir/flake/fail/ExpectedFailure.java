611b7a4bc6a79e5a6df10ca053c954a3a655e9bf
Jeremy Kong <jeremykong@hotmail.com>
2018-01-08 17:06:44 +0000

    expected failure rules

---
+++ flake-rule/src/test/java/com/palantir/flake/fail/ExpectedFailure.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.flake.fail;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // Used at runtime by ExpectedFailureRule to make decisions
+@Target(ElementType.METHOD)
+public @interface ExpectedFailure {
+}
