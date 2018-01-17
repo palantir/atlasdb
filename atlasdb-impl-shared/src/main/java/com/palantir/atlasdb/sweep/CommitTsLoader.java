242b970f761d0700bcad0e33b4b0006e318431c3
Himangi Saraogi <hsaraogi@palantir.com>
2018-01-17 14:53:34 +0000

    startTs logging

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/CommitTsLoader.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/CommitTsLoader.java
@@ -22,6 +22,7 @@ import com.palantir.atlasdb.keyvalue.api.KeyAlreadyExistsException;
 import com.palantir.atlasdb.transaction.api.TransactionFailedRetriableException;
 import com.palantir.atlasdb.transaction.impl.TransactionConstants;
 import com.palantir.atlasdb.transaction.service.TransactionService;
+import com.palantir.logsafe.SafeArg;
 
 import gnu.trove.TDecorators;
 import gnu.trove.map.TLongLongMap;
@@ -76,6 +77,7 @@ public final class CommitTsLoader {
                     new TransactionFailedRetriableException(msg, e));
         }
 
+        log.warn("Trying to fetch the commitTs for the startTs {}", SafeArg.of("startTs", startTs));
         return transactionService.get(startTs);
     }
 }
