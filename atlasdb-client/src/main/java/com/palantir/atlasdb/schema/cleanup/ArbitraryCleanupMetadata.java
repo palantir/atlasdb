931a7379f392e82d032b695f97cf1278aec47bf6
Jeremy Kong <jeremykong@hotmail.com>
2018-01-17 12:35:51 +0000

    Cleanup Requirements Part 1.5: Stream Store Cleanup Metadata (#2847)
    * Update structures to include enough information for Nimbus to sweep things
    
    * [no release notes]
    
    * We also need the stream ID type
    
    * fully qualify import name
    
    * cleanup names
    
    * Cleanup Metadata Structures
    
    * rename cases in SMP
    
    * update classes so that schema metadata can be hydrated/persisted
    
    * fix compile breaks
    
    * cleanup sdtm test
    
    * visitor test
    
    * add individual type level tests
    
    * SCREAMING_SNAKE_CASE
    
    * CR comments.
    
    * Correct test artifact

---
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/schema/cleanup/ArbitraryCleanupMetadata.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.schema.cleanup;
+
+import com.google.common.base.Throwables;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.palantir.atlasdb.protos.generated.SchemaMetadataPersistence;
+
+public class ArbitraryCleanupMetadata implements CleanupMetadata {
+    public static Hydrator<ArbitraryCleanupMetadata> BYTES_HYDRATOR = message -> {
+        try {
+            return hydrateFromProto(SchemaMetadataPersistence.ArbitraryCleanupMetadata.parseFrom(message));
+        } catch (InvalidProtocolBufferException e) {
+            throw Throwables.propagate(e);
+        }
+    };
+
+    public static ArbitraryCleanupMetadata hydrateFromProto(
+            SchemaMetadataPersistence.ArbitraryCleanupMetadata unused) {
+        // This is correct while the ArbitraryCleanupMetadata message has no fields.
+        return new ArbitraryCleanupMetadata();
+    }
+
+    @Override
+    public byte[] persistToBytes() {
+        return persistToProto().toByteArray();
+    }
+
+    public SchemaMetadataPersistence.ArbitraryCleanupMetadata persistToProto() {
+        return SchemaMetadataPersistence.ArbitraryCleanupMetadata.newBuilder().build();
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this == other || (other != null && this.getClass() == other.getClass());
+    }
+
+    @Override
+    public int hashCode() {
+        return ArbitraryCleanupMetadata.class.hashCode();
+    }
+}
