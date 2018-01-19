298c95c6ef2dd7fa0f0575dcf3273c5791a8b11a
Jeremy Kong <jeremykong@hotmail.com>
2018-01-19 11:34:16 +0000

    more async init stuff

--- atlasdb-ete-tests/src/main/java/com/palantir/atlasdb/schema/CleanupMetadataResourceImpl.java
+++ atlasdb-ete-tests/src/main/java/com/palantir/atlasdb/schema/CleanupMetadataResourceImpl.java
@@ -19,38 +19,36 @@ package com.palantir.atlasdb.schema;
 import java.util.Optional;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.schema.metadata.SchemaMetadataService;
 import com.palantir.atlasdb.schema.metadata.SchemaMetadataServiceImpl;
 import com.palantir.atlasdb.transaction.api.TransactionManager;
 import com.palantir.atlasdb.transaction.impl.SerializableTransactionManager;
-import com.palantir.common.base.Throwables;
+import com.palantir.exception.NotInitializedException;
 
 public class CleanupMetadataResourceImpl implements CleanupMetadataResource {
-    private final SchemaMetadataService schemaMetadataService;
+    private final Supplier<SchemaMetadataService> schemaMetadataServiceSupplier;
 
     public CleanupMetadataResourceImpl(TransactionManager transactionManager, boolean initializeAsync) {
         Preconditions.checkState(transactionManager instanceof SerializableTransactionManager,
                 "Cannot create a CleanupMetadataResourceImpl from a non-SerializableTransactionManager");
-        // TODO (jkong): Implement async initialization
-        while (!transactionManager.isInitialized()) {
-            try {
-                Thread.sleep(1_000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw Throwables.rewrapAndThrowUncheckedException(e);
+        schemaMetadataServiceSupplier = Suppliers.memoize(() -> {
+            if (!transactionManager.isInitialized()) {
+                throw new NotInitializedException("not initialized");
             }
-        }
-        schemaMetadataService = SchemaMetadataServiceImpl.create(
-                ((SerializableTransactionManager) transactionManager).getKeyValueService(),
-                initializeAsync);
+            return SchemaMetadataServiceImpl.create(
+                    ((SerializableTransactionManager) transactionManager).getKeyValueService(),
+                    initializeAsync);
+        });
     }
 
     @Override
     public Optional<SerializableCleanupMetadata> get(
             String schemaName,
             String tableName) {
-        return schemaMetadataService.loadSchemaMetadata(schemaName)
+        return schemaMetadataServiceSupplier.get().loadSchemaMetadata(schemaName)
                 .map(SchemaMetadata::schemaDependentTableMetadata)
                 .flatMap(metadataMap -> Optional.ofNullable(metadataMap.get(
                         TableReference.createFromFullyQualifiedName(tableName))))
