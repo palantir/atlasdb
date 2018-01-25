61c59bb2903d4b7393dd6f6d60ba18b47c098659
Glenn Sheasby <gsheasby@palantir.com>
2018-01-25 15:30:37 +0000

    Generify javadoc

--- qos-service-impl/src/main/java/com/palantir/atlasdb/qos/metrics/MetricsService.java
+++ qos-service-impl/src/main/java/com/palantir/atlasdb/qos/metrics/MetricsService.java
@@ -27,20 +27,15 @@ import javax.ws.rs.core.MediaType;
 
 import com.palantir.logsafe.Safe;
 
-/**
- * Copied from internal sls Cassandra project.
- */
-
 @Path("/metrics")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface MetricsService {
     /**
-     * Get the value for a particular Cassandra metric. The possible metric combinations can be found in
-     * http://cassandra.apache.org/doc/latest/operating/metrics.html.
+     * Get the value for a particular metric.
      *
-     * @param type The type of the Cassandra metric, mostly a sub-type of the metric name eg: CommitLog
-     * @param name The name of the Cassandra metric eg: PendingTasks
+     * @param type The type of the metric, mostly a sub-type of the metric name eg: CommitLog
+     * @param name The name of the metric eg: PendingTasks
      * @param attr The attribute you require for the metric eg: Value, p99 etc.
      * @param additionalParams Any other parameters to make the metric more specific eg: scope:keyspace
      * @return the metric object for the queried metric
