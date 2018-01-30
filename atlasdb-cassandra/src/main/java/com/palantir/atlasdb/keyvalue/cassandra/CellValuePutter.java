5971de915c5ae681f72e483ddc607eafeb08c371
Glenn Sheasby <gsheasby@palantir.com>
2018-01-30 17:48:27 +0000

    Fix clean cass locks state cli, Part 1: Extract all the things (#2943)
    * Use supplier instead of passing UniqueSchemaMutationLockTable
    
    The USMLT object enforces that we only have one lock table, meaning it
    cannot be used by the clean-cass-locks-state CLI. That CLI needs to
    call SchemaMutationLock.cleanLockState for a specific TableReference,
    hence SML just needs a Supplier<TableReference>.
    
    * Pass supplier in as constructor parameter
    
    * Move host-partitioning methods to HostPartitioner
    
    * Inline CKVSI.partitionMapByHost
    
    * Move batchMutate/multigetInternal to WrappingQueryRunner
    
    * Move runAllTasksCancelOnFailure to TaskRunner
    
    Is there a better name than TaskRunner?
    
    This class can probably be moved to a more generic place (and tested!),
    but it also does some custom-looking exception wrapping.
    
    * Move loadWithTs to CellLoader
    
    There is some log-based logic here (isTraceEnabled); since we're in the
    same package, I'm assuming that the same rules will apply. Is
    assumption this valid in practice?
    
    * Delegate getAllTimestamps to TimestampsLoader
    
    This new object could potentially be combined with CellLoader.
    
    * Delegate CKVSI.delete to CellDeleter
    
    * Move iterable-partitioning utility methods to IterablePartitioner
    
    There's some weirdness here with passing loggers around, but to
    minimise the footprint of this change, I preserved it.
    
    * Minor cleanup in AbstractKeyValueService
    
    Also, avoided a possible NPE in the entry-sizing function
    
    * Move putInternal to CellValuePutter
    
    * Move dropTablesInternal to CassandraTableDropper
    
    Also moved:
    - fromInternalTableName from AbstractKVS to TableReference
    - remaining static utility methods from CKVSI to
    CassandraKeyValueServices
    
    * Make HostPartitioner static
    
    * Merge TimestampsLoader into CellLoader
    
    * Fixup javadoc
    
    [no release notes]

---
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CellValuePutter.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2018 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.keyvalue.cassandra;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import org.apache.cassandra.thrift.Column;
+import org.apache.cassandra.thrift.ColumnOrSuperColumn;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.apache.cassandra.thrift.Mutation;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfig;
+import com.palantir.atlasdb.keyvalue.api.Cell;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.keyvalue.api.Value;
+import com.palantir.atlasdb.keyvalue.cassandra.thrift.MutationMap;
+import com.palantir.atlasdb.keyvalue.impl.Cells;
+import com.palantir.atlasdb.keyvalue.impl.IterablePartitioner;
+import com.palantir.atlasdb.util.AnnotatedCallable;
+import com.palantir.atlasdb.util.AnnotationType;
+import com.palantir.common.base.FunctionCheckedException;
+
+public class CellValuePutter {
+    private static final Function<Map.Entry<Cell, Value>, Long> ENTRY_SIZING_FUNCTION = input ->
+            input.getValue().getContents().length + 4L + Cells.getApproxSizeOfCell(input.getKey());
+
+    private CassandraKeyValueServiceConfig config;
+    private CassandraClientPool clientPool;
+    private TaskRunner taskRunner;
+    private WrappingQueryRunner queryRunner;
+    private ConsistencyLevel writeConsistency;
+
+    public CellValuePutter(CassandraKeyValueServiceConfig config,
+            CassandraClientPool clientPool,
+            TaskRunner taskRunner,
+            WrappingQueryRunner queryRunner,
+            ConsistencyLevel writeConsistency) {
+        this.config = config;
+        this.clientPool = clientPool;
+        this.taskRunner = taskRunner;
+        this.queryRunner = queryRunner;
+        this.writeConsistency = writeConsistency;
+    }
+
+    void put(final String kvsMethodName,
+            final TableReference tableRef,
+            final Iterable<Map.Entry<Cell, Value>> values) throws Exception {
+        Map<InetSocketAddress, Map<Cell, Value>> cellsByHost = HostPartitioner.partitionMapByHost(clientPool, values);
+        List<Callable<Void>> tasks = Lists.newArrayListWithCapacity(cellsByHost.size());
+        for (final Map.Entry<InetSocketAddress, Map<Cell, Value>> entry : cellsByHost.entrySet()) {
+            tasks.add(AnnotatedCallable.wrapWithThreadName(AnnotationType.PREPEND,
+                    "Atlas put " + entry.getValue().size()
+                            + " cell values to " + tableRef + " on " + entry.getKey(),
+                    () -> {
+                        putForSingleHost(kvsMethodName, entry.getKey(), tableRef, entry.getValue().entrySet());
+                        clientPool.markWritesForTable(entry.getValue(), tableRef);
+                        return null;
+                    }));
+        }
+        taskRunner.runAllTasksCancelOnFailure(tasks);
+    }
+
+    private void putForSingleHost(String kvsMethodName,
+            final InetSocketAddress host,
+            final TableReference tableRef,
+            final Iterable<Map.Entry<Cell, Value>> values) throws Exception {
+        clientPool.runWithRetryOnHost(host,
+                new FunctionCheckedException<CassandraClient, Void, Exception>() {
+                    @Override
+                    public Void apply(CassandraClient client) throws Exception {
+                        int mutationBatchCount = config.mutationBatchCount();
+                        int mutationBatchSizeBytes = config.mutationBatchSizeBytes();
+                        for (List<Map.Entry<Cell, Value>> partition : IterablePartitioner.partitionByCountAndBytes(
+                                values,
+                                mutationBatchCount,
+                                mutationBatchSizeBytes,
+                                tableRef,
+                                ENTRY_SIZING_FUNCTION)) {
+                            MutationMap map = new MutationMap();
+                            for (Map.Entry<Cell, Value> e : partition) {
+                                Cell cell = e.getKey();
+                                Column col = CassandraKeyValueServices.createColumn(cell, e.getValue());
+
+                                ColumnOrSuperColumn colOrSup = new ColumnOrSuperColumn();
+                                colOrSup.setColumn(col);
+                                Mutation mutation = new Mutation();
+                                mutation.setColumn_or_supercolumn(colOrSup);
+
+                                map.addMutationForCell(cell, tableRef, mutation);
+                            }
+
+                            queryRunner.batchMutate(kvsMethodName, client, ImmutableSet.of(tableRef), map,
+                                    writeConsistency);
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "batch_mutate(" + host + ", " + tableRef.getQualifiedName() + ", "
+                                + Iterables.size(values) + " values)";
+                    }
+                });
+    }
+}
---
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/HostPartitioner.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the BSD-3 License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.keyvalue.cassandra;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimaps;
+import com.palantir.atlasdb.keyvalue.api.Cell;
+
+final class HostPartitioner {
+    private HostPartitioner() {
+        // Static class
+    }
+
+    static <V> Map<InetSocketAddress, Map<Cell, V>> partitionMapByHost(CassandraClientPool clientPool,
+            Iterable<Map.Entry<Cell, V>> cells) {
+        Map<InetSocketAddress, List<Map.Entry<Cell, V>>> partitionedByHost =
+                partitionByHost(clientPool, cells, entry -> entry.getKey().getRowName());
+        Map<InetSocketAddress, Map<Cell, V>> cellsByHost = Maps.newHashMap();
+        for (Map.Entry<InetSocketAddress, List<Map.Entry<Cell, V>>> hostAndCells : partitionedByHost.entrySet()) {
+            Map<Cell, V> cellsForHost = Maps.newHashMapWithExpectedSize(hostAndCells.getValue().size());
+            for (Map.Entry<Cell, V> entry : hostAndCells.getValue()) {
+                cellsForHost.put(entry.getKey(), entry.getValue());
+            }
+            cellsByHost.put(hostAndCells.getKey(), cellsForHost);
+        }
+        return cellsByHost;
+    }
+
+    static <V> Map<InetSocketAddress, List<V>> partitionByHost(CassandraClientPool clientPool,
+            Iterable<V> iterable,
+            Function<V, byte[]> keyExtractor) {
+        // Ensure that the same key goes to the same partition. This is important when writing multiple columns
+        // to the same row, since this is a normally a single write in cassandra, whereas splitting the columns
+        // into different requests results in multiple writes.
+        ListMultimap<ByteBuffer, V> partitionedByKey = ArrayListMultimap.create();
+        for (V value : iterable) {
+            partitionedByKey.put(ByteBuffer.wrap(keyExtractor.apply(value)), value);
+        }
+        ListMultimap<InetSocketAddress, V> valuesByHost = ArrayListMultimap.create();
+        for (ByteBuffer key : partitionedByKey.keySet()) {
+            InetSocketAddress host = clientPool.getRandomHostForKey(key.array());
+            valuesByHost.putAll(host, partitionedByKey.get(key));
+        }
+        return Multimaps.asMap(valuesByHost);
+    }
+}
