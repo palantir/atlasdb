9b46ead7b42eeb16ae513b5c5536d5413afee5c2
Jeremy Kong <jeremykong@hotmail.com>
2018-02-06 10:25:36 +0000

    parameterize SSSDT

--- atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/external/SchemalessStreamStoreDeleterTest.java
+++ atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/external/SchemalessStreamStoreDeleterTest.java
@@ -17,38 +17,69 @@
 package com.palantir.atlasdb.sweep.external;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.protobuf.ByteString;
 import com.palantir.atlasdb.keyvalue.api.Namespace;
 import com.palantir.atlasdb.protos.generated.StreamPersistence;
+import com.palantir.atlasdb.protos.generated.StreamPersistence.StreamMetadata;
 import com.palantir.atlasdb.schema.cleanup.ImmutableStreamStoreCleanupMetadata;
+import com.palantir.atlasdb.schema.cleanup.StreamStoreCleanupMetadata;
 import com.palantir.atlasdb.schema.stream.StreamTableType;
 import com.palantir.atlasdb.table.description.ValueType;
 import com.palantir.atlasdb.transaction.api.Transaction;
 
+@RunWith(Parameterized.class)
 public class SchemalessStreamStoreDeleterTest {
     private static final Namespace NAMESPACE = Namespace.DEFAULT_NAMESPACE;
     private static final String SHORT_NAME = "s";
 
-    private final SchemalessStreamStoreDeleter deleter = new SchemalessStreamStoreDeleter(
-            NAMESPACE,
-            SHORT_NAME,
-            ImmutableStreamStoreCleanupMetadata.builder()
-                    .numHashedRowComponents(1)
-                    .streamIdType(ValueType.VAR_LONG).build());
+    private static final ByteString HASH = ByteString.copyFrom(new byte[32]);
+
+    private final StreamStoreCleanupMetadata cleanupMetadata;
+    private final GenericStreamIdentifier streamIdentifier;
+
+    private final GenericStreamStoreCellCreator cellCreator;
+    private final SchemalessStreamStoreDeleter deleter;
+
+    // Hacky. Unfortunately a transform on the objects for the naming doesn't seem to be easily allowed
+    @Parameterized.Parameters(name = "{index} components hashed")
+    public static Iterable<StreamStoreCleanupMetadata> streamStoreCleanupMetadata() {
+        return ImmutableList.of(
+                ImmutableStreamStoreCleanupMetadata.builder()
+                        .numHashedRowComponents(0)
+                        .streamIdType(ValueType.VAR_SIGNED_LONG)
+                        .build(),
+                ImmutableStreamStoreCleanupMetadata.builder()
+                        .numHashedRowComponents(1)
+                        .streamIdType(ValueType.VAR_LONG)
+                        .build(),
+                ImmutableStreamStoreCleanupMetadata.builder()
+                        .numHashedRowComponents(2)
+                        .streamIdType(ValueType.FIXED_LONG)
+                        .build());
+    }
+
+    public SchemalessStreamStoreDeleterTest(StreamStoreCleanupMetadata cleanupMetadata) {
+        this.cleanupMetadata = cleanupMetadata;
+        this.streamIdentifier = ImmutableGenericStreamIdentifier.of(
+                cleanupMetadata.streamIdType(), cleanupMetadata.streamIdType().convertFromJava(1L));
+        this.cellCreator = new GenericStreamStoreCellCreator(cleanupMetadata);
+        this.deleter = new SchemalessStreamStoreDeleter(NAMESPACE, SHORT_NAME, cleanupMetadata);
+    }
 
     @Test
-
-    public void deleteStreamsDoesNotMakeDatabaseCallsIfDeletingAnEmptyStream() {
+    public void deleteStreamsDoesNotMakeDatabaseCallsIfDeletingNothing() {
         Transaction tx = mock(Transaction.class);
         deleter.deleteStreams(tx, ImmutableSet.of());
         verifyNoMoreInteractions(tx);
@@ -56,32 +87,32 @@ public class SchemalessStreamStoreDeleterTest {
 
     @Test
     public void deleteStreamsWorksForSingleBlockStreams() {
-        GenericStreamStoreCellCreator cellCreator = new GenericStreamStoreCellCreator(
-                ImmutableStreamStoreCleanupMetadata.builder()
-                        .numHashedRowComponents(1)
-                        .streamIdType(ValueType.VAR_LONG)
-                        .build());
-        StreamPersistence.StreamMetadata streamMetadata = StreamPersistence.StreamMetadata.newBuilder()
-                .setLength(1L)
-                .setStatus(StreamPersistence.Status.STORED)
-                .setHash(ByteString.copyFrom(new byte[32]))
-                .build();
         Transaction tx = mock(Transaction.class);
-        when(tx.get(any(), any())).thenReturn(
-                ImmutableMap.of(cellCreator.constructMetadataTableCell(ImmutableGenericStreamIdentifier.of(ValueType.VAR_LONG, ValueType.VAR_LONG.convertFromJava(1L))),
-                        streamMetadata.toByteArray()));
+        when(tx.get(any(), any())).thenReturn(ImmutableMap.of(
+                cellCreator.constructMetadataTableCell(streamIdentifier), getStreamMetadataForStreamOfLength(1L)));
 
-        deleter.deleteStreams(tx,
-                ImmutableSet.of(
-                        ImmutableGenericStreamIdentifier.of(ValueType.VAR_LONG, ValueType.VAR_LONG.convertFromJava(1L))
-                ));
-        verify(tx).delete(eq(StreamTableType.VALUE.getTableReference(NAMESPACE, SHORT_NAME)),
-                eq(new GenericStreamStoreCellCreator(ImmutableStreamStoreCleanupMetadata.builder().numHashedRowComponents(1).streamIdType(ValueType.VAR_LONG).build())
-                        .constructValueTableCellSet(ImmutableGenericStreamIdentifier.of(ValueType.VAR_LONG, ValueType.VAR_LONG.convertFromJava(1L)), 1)));
+        deleter.deleteStreams(tx, ImmutableSet.of(streamIdentifier));
+        verify(tx).delete(
+                StreamTableType.VALUE.getTableReference(NAMESPACE, SHORT_NAME),
+                cellCreator.constructValueTableCellSet(streamIdentifier, 1));
+        verify(tx).delete(
+                StreamTableType.METADATA.getTableReference(NAMESPACE, SHORT_NAME),
+                ImmutableSet.of(cellCreator.constructMetadataTableCell(streamIdentifier)));
+        verify(tx).delete(
+                StreamTableType.HASH.getTableReference(NAMESPACE, SHORT_NAME),
+                ImmutableSet.of(cellCreator.constructHashTableCell(streamIdentifier, HASH)));
     }
 
     @Test
     public void deleteStreamsWorksForMultiBlockStreams() {
-        foo
+    }
+
+    private byte[] getStreamMetadataForStreamOfLength(long length) {
+        return StreamMetadata.newBuilder()
+                .setLength(length)
+                .setStatus(StreamPersistence.Status.STORED)
+                .setHash(HASH)
+                .build()
+                .toByteArray();
     }
 }
