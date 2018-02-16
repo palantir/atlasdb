673ab038da7ac9adf627c1f495a6e81324539ab5
Samuel Souza <ssouza@palantir.com>
2018-02-16 15:13:07 +0000

    Basic documentation for the Service*Transaction

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SerializableTransactionManager.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SerializableTransactionManager.java
@@ -19,20 +19,32 @@ package com.palantir.atlasdb.transaction.impl;
 import com.palantir.atlasdb.cleaner.Cleaner;
 import com.palantir.atlasdb.transaction.api.PreCommitCondition;
 import com.palantir.atlasdb.transaction.api.TransactionManager;
+import com.palantir.atlasdb.transaction.api.TransactionTask;
 
 public interface SerializableTransactionManager extends TransactionManager {
+    /**
+     * @return A {@link SerializableTransaction} that can be used for reads and writes.
+     * This behaves as the transactions used in the {@link TransactionManager#runTaskWithRetry(TransactionTask)}.
+     */
     <C extends PreCommitCondition, E extends Exception> ServiceWriteTransaction getWriteTransaction(
             C condition) throws E;
 
+    /**
+     * @return A transaction that can just be used for readonly transactions.
+     * Note that this transaction does not lock the immutable timestamp.
+     * Therefore, it is possible that sweep has deleted the values this transaction would read,
+     * causing correctness issues. The burden for locking the immutable timestamp, and preventing correctness problems
+     * is on the client.
+     */
     <C extends PreCommitCondition, E extends Exception> ServiceReadOnlyTransaction getReadTransaction(
             C condition) throws E;
 
-        /**
-         * Registers a Runnable that will be run when the transaction manager is closed, provided no callback already
-         * submitted throws an exception.
-         *
-         * Concurrency: If this method races with close(), then closingCallback may not be called.
-         */
+    /**
+     * Registers a Runnable that will be run when the transaction manager is closed, provided no callback already
+     * submitted throws an exception.
+     *
+     * Concurrency: If this method races with close(), then closingCallback may not be called.
+     */
     void registerClosingCallback(Runnable closingCallback);
 
     Cleaner getCleaner();
