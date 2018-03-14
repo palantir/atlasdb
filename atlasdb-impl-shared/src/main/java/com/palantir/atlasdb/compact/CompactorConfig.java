5fbbd5657dd96b573975afdbb0dc2924e358e243
Jeremy Kong <jeremykong@hotmail.com>
2018-03-14 13:50:46 +0000

    [Oracle Compaction][Part 7] Configurable Compactions (#3053)
    * Switch to modelling based on CompactorConfigs rather than on bools
    
    * Actually respect disabled
    
    * Test compact intervals
    
    * Nonnegative intervals
    
    * greater than 0 -> nonnegative

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/compact/CompactorConfig.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/compact/CompactorConfig.java
@@ -16,13 +16,58 @@
 
 package com.palantir.atlasdb.compact;
 
+import java.util.concurrent.TimeUnit;
+
 import org.immutables.value.Value;
+
+import com.google.common.base.Preconditions;
 
 @Value.Immutable
 public interface CompactorConfig {
-    boolean inSafeHours();
+    long DEFAULT_COMPACT_PAUSE_ON_FAILURE_MILLIS = TimeUnit.SECONDS.toMillis(1800);
+    long DEFAULT_COMPACT_PAUSE_MILLIS = TimeUnit.SECONDS.toMillis(10);
 
-    static CompactorConfig defaultCompactorConfig() {
-        return ImmutableCompactorConfig.builder().inSafeHours(true).build();
+    /**
+     * Indicates whether background compaction should run at all.
+     */
+    @Value.Default
+    default boolean enableCompaction() {
+        return false;
+    }
+
+    /**
+     * Indicates whether the background compactor is configured to run in maintenance mode.
+     *
+     * Compactors that are running in maintenance mode may perform more aggressive operations that could have
+     * significant impacts on other queries to the underlying key-value service (such as blocking, un-cancelable
+     * calls that acquire a table lock).
+     */
+    @Value.Default
+    default boolean inMaintenanceMode() {
+        return false;
+    }
+
+    /**
+     * Indicates the time interval to wait after a failed compaction before trying again.
+     */
+    @Value.Default
+    default long compactPauseOnFailureMillis() {
+        return DEFAULT_COMPACT_PAUSE_ON_FAILURE_MILLIS;
+    }
+
+    /**
+     * Indicates the time interval to wait after a successful compaction before trying again (e.g. on a new table).
+     */
+    @Value.Default
+    default long compactPauseMillis() {
+        return DEFAULT_COMPACT_PAUSE_MILLIS;
+    }
+
+    @Value.Check
+    default void checkIntervalsNonnegative() {
+        Preconditions.checkState(compactPauseOnFailureMillis() >= 0,
+                "Compact pause-on-failure interval must be nonnegative, but found %s", compactPauseOnFailureMillis());
+        Preconditions.checkState(compactPauseMillis() >= 0,
+                "Compact pause interval must be nonnegative, but found %s", compactPauseMillis());
     }
 }
