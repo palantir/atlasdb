7d03d3774bd15900db980d620732aabbd3b8b467
Grgur Petric Maretic <gmaretic@palantir.com>
2018-03-14 15:04:28 +0000

    Fixed sweep metrics to be updated by iteration results instead of cumulative results.

--- atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/BackgroundSweeperFastTest.java
+++ atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/BackgroundSweeperFastTest.java
@@ -221,17 +221,10 @@ public class BackgroundSweeperFastTest extends SweeperTestSetup {
                 .timeInMillis(20L)
                 .timeSweepStarted(50L)
                 .build();
-        SweepResults fullResults = ImmutableSweepResults.builder()
-                .staleValuesDeleted(5)
-                .cellTsPairsExamined(21)
-                .minSweptTimestamp(4567L)
-                .timeInMillis(30L)
-                .timeSweepStarted(20L)
-                .build();
 
         setupTaskRunner(intermediateResults);
         backgroundSweeper.runOnce();
-        Mockito.verify(sweepMetricsManager).updateMetrics(fullResults, TABLE_REF);
+        Mockito.verify(sweepMetricsManager).updateMetrics(intermediateResults, TABLE_REF);
     }
 
     @Test
