eadddd3e749fea01f6ffa397d02cd9d7d79161bc
James Baker <jbaker@palantir.com>
2018-03-19 15:37:13 +0000

    Stop using GCM in any circumstances
    Also rename esoteric 'cypher' to 'cipher'.
    
    Basically this says that a server or client 'may' reject other ciphers as insecure.
    However, both internally and externally we do not do this on either
    clients or servers.

--- atlasdb-feign/src/main/java/com/palantir/atlasdb/http/FeignOkHttpClients.java
+++ atlasdb-feign/src/main/java/com/palantir/atlasdb/http/FeignOkHttpClients.java
@@ -49,18 +49,10 @@ public final class FeignOkHttpClients {
     @Deprecated
     public static volatile Consumer<okhttp3.OkHttpClient.Builder> globalClientSettings = (client) -> { };
 
-    public static final ImmutableList<ConnectionSpec> CONNECTION_SPEC_WITH_CYPHER_SUITES = ImmutableList.of(
+    public static final ImmutableList<ConnectionSpec> CONNECTION_SPEC_WITH_CIPHER_SUITES = ImmutableList.of(
             new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
                     .tlsVersions(TlsVersion.TLS_1_2)
                     .cipherSuites(
-                            // This GCM cipher suite is for HTTP/2 over TLS1.2 as clients have to
-                            // enable at least one cipher suite not in the blacklist.
-                            // (https://http2.github.io/http2-spec/index.html#BadCipherSuites)
-                            // Timelock server will support HTTP/2 connections, and this will ensure
-                            // all AtlasDB clients have one supported cipher suite.
-                            // See also:
-                            //    - https://http2.github.io/http2-spec/index.html#rfc.section.9.2.2
-                            CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
                             // In an ideal world, we'd use GCM suites, but they're an order of
                             // magnitude slower than the CBC suites, which have JVM optimizations
                             // already. We should revisit with JDK9.
@@ -84,6 +76,8 @@ public final class FeignOkHttpClients {
                             CipherSuite.TLS_ECDH_RSA_WITH_AES_128_CBC_SHA,
                             CipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA,
                             CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+                            CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+                            CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
                             CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV)
                     .build(),
             ConnectionSpec.CLEARTEXT);
@@ -122,7 +116,7 @@ public final class FeignOkHttpClients {
             String userAgent) {
         // Don't allow retrying on connection failures - see ticket #2194
         okhttp3.OkHttpClient.Builder builder = new okhttp3.OkHttpClient.Builder()
-                .connectionSpecs(CONNECTION_SPEC_WITH_CYPHER_SUITES)
+                .connectionSpecs(CONNECTION_SPEC_WITH_CIPHER_SUITES)
                 .connectionPool(new ConnectionPool(CONNECTION_POOL_SIZE, KEEP_ALIVE_TIME_MILLIS, TimeUnit.MILLISECONDS))
                 .proxySelector(proxySelector.orElse(ProxySelector.getDefault()))
                 .retryOnConnectionFailure(false);
