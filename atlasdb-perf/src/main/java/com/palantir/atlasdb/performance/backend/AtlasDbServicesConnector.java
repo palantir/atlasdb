0dce2bfeb29379078cc64fc30f66a51c7609fef0
Grgur Petric Maretic <gmaretic@palantir.com>
2018-03-22 17:42:20 +0000

    Working version

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/backend/AtlasDbServicesConnector.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/backend/AtlasDbServicesConnector.java
@@ -17,17 +17,27 @@
 package com.palantir.atlasdb.performance.backend;
 
 import java.io.Closeable;
+import java.net.InetSocketAddress;
+import java.nio.file.Paths;
 
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
+import com.google.common.collect.ImmutableSet;
+import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfig;
+import com.palantir.atlasdb.cassandra.ImmutableCassandraKeyValueServiceConfig;
+import com.palantir.atlasdb.config.AtlasDbConfig;
+import com.palantir.atlasdb.config.AtlasDbRuntimeConfig;
 import com.palantir.atlasdb.config.ImmutableAtlasDbConfig;
 import com.palantir.atlasdb.config.ImmutableAtlasDbRuntimeConfig;
+import com.palantir.atlasdb.config.ImmutableServerListConfig;
+import com.palantir.atlasdb.config.ImmutableTimeLockRuntimeConfig;
+import com.palantir.atlasdb.config.ServerListConfig;
 import com.palantir.atlasdb.services.AtlasDbServices;
 import com.palantir.atlasdb.services.DaggerAtlasDbServices;
 import com.palantir.atlasdb.services.ServicesConfigModule;
-import com.palantir.atlasdb.spi.KeyValueServiceConfig;
+import com.palantir.remoting.api.config.ssl.SslConfiguration;
 
 @State(Scope.Benchmark)
 public class AtlasDbServicesConnector implements Closeable {
@@ -45,12 +55,41 @@ public class AtlasDbServicesConnector implements Closeable {
             throw new IllegalStateException("connect() has already been called");
         }
 
-        DockerizedDatabaseUri dburi = DockerizedDatabaseUri.fromUriString(uri);
-        KeyValueServiceConfig config = dburi.getKeyValueServiceInstrumentation()
-                .getKeyValueServiceConfig(dburi.getAddress());
-        ImmutableAtlasDbConfig atlasDbConfig = ImmutableAtlasDbConfig.builder().keyValueService(config).build();
-        ImmutableAtlasDbRuntimeConfig runtimeConfig = ImmutableAtlasDbRuntimeConfig.defaultRuntimeConfig();
-        ServicesConfigModule servicesConfigModule = ServicesConfigModule.create(atlasDbConfig, runtimeConfig);
+        InetSocketAddress first = InetSocketAddress.createUnresolved("il-pg-alpha-2822832.use1.palantir.global", 9160);
+        InetSocketAddress second = InetSocketAddress.createUnresolved("il-pg-alpha-2822833.use1.palantir.global", 9160);
+        InetSocketAddress third = InetSocketAddress.createUnresolved("il-pg-alpha-2822834.use1.palantir.global", 9160);
+
+
+        SslConfiguration ssl = SslConfiguration.builder()
+                .keyStorePath(Paths.get("/opt/palantir/services/.1378/var/security/keystore.jks"))
+                .trustStorePath(Paths.get("/opt/palantir/services/.1378/var/security/truststore.jks"))
+                .keyStorePassword("upVkPSqf4heABZXA")
+                .build();
+
+        CassandraKeyValueServiceConfig cassandraConfig = ImmutableCassandraKeyValueServiceConfig.builder()
+                .servers(ImmutableSet.of(first, second, third))
+                .replicationFactor(3)
+                .sslConfiguration(ssl)
+                .build();
+
+        AtlasDbConfig installConfig = ImmutableAtlasDbConfig.builder()
+                .keyValueService(cassandraConfig)
+                .namespace("benchmarks")
+                .build();
+
+        ServerListConfig servers = ImmutableServerListConfig.builder()
+                .servers(ImmutableSet.of(
+                    "https://il-pg-alpha-2822832.use1.palantir.global:8421/timelock/api",
+                    "https://il-pg-alpha-2822833.use1.palantir.global:8421/timelock/api",
+                    "https://il-pg-alpha-2822834.use1.palantir.global:8421/timelock/api"))
+                .sslConfiguration(ssl)
+                .build();
+
+        AtlasDbRuntimeConfig runtimeConfig = ImmutableAtlasDbRuntimeConfig.builder()
+                .timelockRuntime(ImmutableTimeLockRuntimeConfig.builder().serversList(servers).build())
+                .build();
+
+        ServicesConfigModule servicesConfigModule = ServicesConfigModule.create(installConfig, runtimeConfig);
 
         services = DaggerAtlasDbServices.builder()
                 .servicesConfigModule(servicesConfigModule)
