d401f1351780e89af46ad7835446fd2d71d473ce
Glenn Sheasby <gsheasby@palantir.com>
2018-04-24 11:39:41 +0100

    Move TodoSchema to atlasdb-ete-test-utils

--- atlasdb-ete-test-utils/src/main/java/com/palantir/atlasdb/todo/TodoSchema.java
+++ atlasdb-ete-test-utils/src/main/java/com/palantir/atlasdb/todo/TodoSchema.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ * Copyright 2018 Palantir Technologies, Inc. All rights reserved.
  *
  * Licensed under the BSD-3 License (the "License");
  * you may not use this file except in compliance with the License.
