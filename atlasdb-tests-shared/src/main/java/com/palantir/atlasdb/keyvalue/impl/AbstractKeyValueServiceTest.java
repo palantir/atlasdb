68633e519992758bdc4a77ef721502f424c6f272
James Baker <j.baker@outlook.com>
2018-05-23 11:58:44 +0100

    Row paging hands back in hashmap order from the second page and beyond (#3185)
    * Failing test for issue
    
    * Release notes
    
    * Fix for Cassandra
    
    * FIx test

--- atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/keyvalue/impl/AbstractKeyValueServiceTest.java
+++ atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/keyvalue/impl/AbstractKeyValueServiceTest.java
@@ -25,6 +25,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,6 +38,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.SortedMap;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.After;
@@ -57,6 +59,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
+import com.google.common.collect.Streams;
 import com.google.common.primitives.UnsignedBytes;
 import com.palantir.atlasdb.AtlasDbConstants;
 import com.palantir.atlasdb.encoding.PtBytes;
@@ -95,6 +98,10 @@ public abstract class AbstractKeyValueServiceTest {
     protected static final byte[] column0 = PtBytes.toBytes("column0");
     protected static final byte[] column1 = PtBytes.toBytes("column1");
     protected static final byte[] column2 = PtBytes.toBytes("column2");
+    protected static final byte[] column3 = PtBytes.toBytes("column3");
+    protected static final byte[] column4 = PtBytes.toBytes("column4");
+    protected static final byte[] column5 = PtBytes.toBytes("column5");
+    protected static final byte[] column6 = PtBytes.toBytes("column6");
     protected static final byte[] value00 = PtBytes.toBytes("value00");
     protected static final byte[] value01 = PtBytes.toBytes("value01");
     protected static final byte[] value10 = PtBytes.toBytes("value10");
@@ -253,6 +260,38 @@ public abstract class AbstractKeyValueServiceTest {
         assertEquals(2, batchValues.size());
         assertArrayEquals(batchValues.get(Cell.create(row1, column0)).getContents(), value10);
         assertArrayEquals(batchValues.get(Cell.create(row1, column2)).getContents(), value12);
+    }
+
+    @Test
+    public void testGetRowColumnRange_pagesInOrder() {
+        // reg test for bug where HashMap led to reorder, batch 3 to increase chance of that happening if HashMap change
+        Map<Cell, byte[]> values = new HashMap<>();
+        values.put(Cell.create(row1, column0), value10);
+        values.put(Cell.create(row1, column1), value10);
+        values.put(Cell.create(row1, column2), value10);
+        values.put(Cell.create(row1, column3), value10);
+        values.put(Cell.create(row1, column4), value10);
+        values.put(Cell.create(row1, column5), value10);
+        values.put(Cell.create(row1, column6), value10);
+        keyValueService.put(TEST_TABLE, values, TEST_TIMESTAMP);
+
+        RowColumnRangeIterator iterator = keyValueService.getRowsColumnRange(
+                TEST_TABLE,
+                ImmutableList.of(row1),
+                BatchColumnRangeSelection.create(null, null, 3),
+                TEST_TIMESTAMP + 1).get(row1);
+        List<ByteBuffer> columns = Streams.stream(iterator)
+                .map(entry -> entry.getKey().getColumnName())
+                .map(ByteBuffer::wrap)
+                .collect(Collectors.toList());
+        assertEquals(ImmutableList.of(
+                ByteBuffer.wrap(column0),
+                ByteBuffer.wrap(column1),
+                ByteBuffer.wrap(column2),
+                ByteBuffer.wrap(column3),
+                ByteBuffer.wrap(column4),
+                ByteBuffer.wrap(column5),
+                ByteBuffer.wrap(column6)), columns);
     }
 
     @Test
