9b5b5ed4e8713556efb363fdcbb723b29dc24d3b
Jeremy Kong <jeremykong@hotmail.com>
2018-06-05 11:21:58 +0100

    baseline

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/memory/InMemoryAtlasDbFactory.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/memory/InMemoryAtlasDbFactory.java
@@ -17,7 +17,6 @@ package com.palantir.atlasdb.memory;
 
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.LongSupplier;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
