6619c031565bae8318c5593e0e336dc78dacb98d
Jeremy Kong <jeremykong@hotmail.com>
2018-06-08 13:35:33 +0100

    Add API documentation for CMTP

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/cassandra/CassandraMutationTimestampProvider.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/cassandra/CassandraMutationTimestampProvider.java
@@ -20,12 +20,24 @@ import java.util.function.LongUnaryOperator;
 
 /**
  * This interface produces Cassandra timestamps for mutations, where it makes sense for these to be overridden.
- * Generally, to ensure that Cassandra is able to compact tombstones smoothly, these values should increase over
- * time and should largely be in line with AtlasDB's notion of logical time.
+ * The values returned by this provider must observe the following semantics.
+ *
+ *   - Sweep sentinels: These must be strictly greater than the Cassandra timestamps of any previous deletions or range
+ *     tombstones covering the sweep sentinel. (We may want to write sweep sentinels to a table where deletions or range
+ *     tombstones already exist, in the case of tables shifting between THOROUGH to CONSERVATIVE sweep.)
+ *   - Deletions: These must be strictly greater than the Cassandra timestamp at which the original value being deleted
+ *     was written.
+ *   - Range tombstones: These must be strictly greater than the Cassandra timestamps of all values covered by the
+ *     range tombstone (including sweep sentinels, if applicable.)
+ *
+ * In addition to the above semantics, these values should increase over time and should largely be in line with
+ * AtlasDB's notion of logical time, to ensure smooth compaction of tables.
  */
 public interface CassandraMutationTimestampProvider {
     /**
      * Returns the Cassandra timestamp at which sweep sentinels should be written.
+     *
+     * This is called once per API call to CassandraKeyValueServiceImpl.addGarbageCollectionSentinelValues().
      */
     long getSweepSentinelWriteTimestamp();
 
@@ -35,13 +47,16 @@ public interface CassandraMutationTimestampProvider {
      * The operator is intended to be use for a single batch deletion, and must not be re-used outside of the
      * context of a single batch delete.
      *
-     * This operator may be invoked once per cell involved in a batch delete, and thus must be cheap to invoke.
+     * The operator is obtained once per API call to CassandraKeyValueServiceImpl.delete().
+     * The operator is invoked once per cell involved in such an API call, and thus must be cheap to invoke.
      */
     LongUnaryOperator getDeletionTimestampOperatorForBatchDelete();
 
     /**
      * Cassandra timestamp at which a range tombstone should be written.
      * @param maximumAtlasTimestampExclusive Maximum timestamp, exclusive, of the range tombstone.
+     *
+     * This is called once per API call to CassandraKeyValueServiceImpl.deleteAllTimestamps().
      */
     long getRangeTombstoneTimestamp(long maximumAtlasTimestampExclusive);
 }
