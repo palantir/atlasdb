ecfe6898a3df709b8f89e092d09e89713c5736e0
James Baker <jbaker@palantir.com>
2018-06-17 10:22:56 +0100

    Use a daemon executor
    Internally we've had a few cases recently where we've failed to shut
    down properly. All the executors _we_ create are properly shut down
    when a termination signal is seen, and this is the case almost all
    of the time in AtlasDB, except in this place, which is brand new.
    
    I've diffed our dependencies, and this seems like the most likely
    place for our issues to show up.

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SerializableTransactionManager.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SerializableTransactionManager.java
@@ -18,7 +18,6 @@ package com.palantir.atlasdb.transaction.impl;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -327,7 +326,7 @@ public class SerializableTransactionManager extends SnapshotTransactionManager {
                 concurrentGetRangesThreadPoolSize,
                 defaultGetRangesConcurrency,
                 sweepQueueWriter,
-                Executors.newSingleThreadExecutor()
+                PTExecutors.newSingleThreadExecutor(true)
         );
     }
 
