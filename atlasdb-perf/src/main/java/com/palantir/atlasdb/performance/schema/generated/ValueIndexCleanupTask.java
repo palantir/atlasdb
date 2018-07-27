fe3e372fff0af464ea8d91bdbe5126d991264888
Jeremy Kong <jeremykong@hotmail.com>
2018-07-27 13:36:42 +0100

    Regenerate schemas

--- atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/schema/generated/ValueIndexCleanupTask.java
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/schema/generated/ValueIndexCleanupTask.java
@@ -1,16 +1,25 @@
 package com.palantir.atlasdb.performance.schema.generated;
 
+import java.util.Map;
 import java.util.Set;
 
-import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import com.palantir.atlasdb.cleaner.api.OnCleanupTask;
+import com.palantir.atlasdb.encoding.PtBytes;
+import com.palantir.atlasdb.keyvalue.api.BatchColumnRangeSelection;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.Namespace;
+import com.palantir.atlasdb.performance.schema.generated.ValueStreamIdxTable.ValueStreamIdxColumnValue;
+import com.palantir.atlasdb.performance.schema.generated.ValueStreamIdxTable.ValueStreamIdxRow;
 import com.palantir.atlasdb.table.description.ValueType;
 import com.palantir.atlasdb.transaction.api.Transaction;
+import com.palantir.common.base.BatchingVisitable;
 
 public class ValueIndexCleanupTask implements OnCleanupTask {
+    private static final BatchColumnRangeSelection ONE_COLUMN = BatchColumnRangeSelection.create(
+            PtBytes.EMPTY_BYTE_ARRAY,
+            PtBytes.EMPTY_BYTE_ARRAY,
+            1);
 
     private final StreamTestTableFactory tables;
 
@@ -25,9 +34,28 @@ public class ValueIndexCleanupTask implements OnCleanupTask {
         for (Cell cell : cells) {
             rows.add(ValueStreamIdxTable.ValueStreamIdxRow.of((Long) ValueType.VAR_LONG.convertToJava(cell.getRowName(), 0)));
         }
-        Multimap<ValueStreamIdxTable.ValueStreamIdxRow, ValueStreamIdxTable.ValueStreamIdxColumnValue> rowsInDb = usersIndex.getRowsMultimap(rows);
-        Set<Long> toDelete = Sets.newHashSetWithExpectedSize(rows.size() - rowsInDb.keySet().size());
-        for (ValueStreamIdxTable.ValueStreamIdxRow rowToDelete : Sets.difference(rows, rowsInDb.keySet())) {
+
+        Map<ValueStreamIdxRow, BatchingVisitable<ValueStreamIdxColumnValue>> presentRowsMultimap
+                = usersIndex.getRowsColumnRange(rows, ONE_COLUMN);
+        Set<ValueStreamIdxRow> presentRows = Sets.newHashSetWithExpectedSize(cells.size());
+        for (Map.Entry<ValueStreamIdxRow, BatchingVisitable<ValueStreamIdxColumnValue>> rowVisitable
+                : presentRowsMultimap.entrySet()) {
+            rowVisitable.getValue().batchAccept(1, columnValue -> {
+                if (!columnValue.isEmpty()) {
+                    presentRows.add(rowVisitable.getKey());
+                }
+                return false;
+            });
+        }
+
+        // Loads all refs, which seems unnecessary!
+//        Multimap<ValueStreamIdxTable.ValueStreamIdxRow, ValueStreamIdxTable.ValueStreamIdxColumnValue> rowsInDb = usersIndex.getRowsMultimap(rows);
+//        Set<Long> toDelete = Sets.newHashSetWithExpectedSize(rows.size() - rowsInDb.keySet().size());
+//        for (ValueStreamIdxTable.ValueStreamIdxRow rowToDelete : Sets.difference(rows, rowsInDb.keySet())) {
+//            toDelete.add(rowToDelete.getId());
+
+        Set<Long> toDelete = Sets.newHashSetWithExpectedSize(rows.size() - presentRows.size());
+        for (ValueStreamIdxTable.ValueStreamIdxRow rowToDelete : Sets.difference(rows, presentRows)) {
             toDelete.add(rowToDelete.getId());
         }
         ValueStreamStore.of(tables).deleteStreams(t, toDelete);
--- atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/schema/stream/generated/TestHashComponentsIndexCleanupTask.java
+++ atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/schema/stream/generated/TestHashComponentsIndexCleanupTask.java
@@ -1,14 +1,18 @@
 package com.palantir.atlasdb.schema.stream.generated;
 
+import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import com.palantir.atlasdb.cleaner.api.OnCleanupTask;
+import com.palantir.atlasdb.encoding.PtBytes;
+import com.palantir.atlasdb.keyvalue.api.BatchColumnRangeSelection;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.Namespace;
 import com.palantir.atlasdb.table.description.ValueType;
 import com.palantir.atlasdb.transaction.api.Transaction;
+import com.palantir.common.base.BatchingVisitable;
 
 public class TestHashComponentsIndexCleanupTask implements OnCleanupTask {
 
@@ -25,9 +29,21 @@ public class TestHashComponentsIndexCleanupTask implements OnCleanupTask {
         for (Cell cell : cells) {
             rows.add(TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxRow.BYTES_HYDRATOR.hydrateFromBytes(cell.getRowName()));
         }
-        Multimap<TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxRow, TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxColumnValue> rowsInDb = usersIndex.getRowsMultimap(rows);
-        Set<Long> toDelete = Sets.newHashSetWithExpectedSize(rows.size() - rowsInDb.keySet().size());
-        for (TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxRow rowToDelete : Sets.difference(rows, rowsInDb.keySet())) {
+        BatchColumnRangeSelection oneColumn = BatchColumnRangeSelection.create(
+                PtBytes.EMPTY_BYTE_ARRAY, PtBytes.EMPTY_BYTE_ARRAY, 1);
+        Map<TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxRow, BatchingVisitable<TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxColumnValue>> existentRows
+                = usersIndex.getRowsColumnRange(rows, oneColumn);
+        Set<TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxRow> rowsInDb = Sets.newHashSetWithExpectedSize(cells.size());
+        for (Map.Entry<TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxRow, BatchingVisitable<TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxColumnValue>> rowVisitable
+                : existentRows.entrySet())
+        rowVisitable.getValue().batchAccept(1, columnValues -> {
+            if (!columnValues.isEmpty()) {
+                rowsInDb.add(rowVisitable.getKey());
+            }
+            return false;
+        });
+        Set<Long> toDelete = Sets.newHashSetWithExpectedSize(rows.size() - rowsInDb.size());
+        for (TestHashComponentsStreamIdxTable.TestHashComponentsStreamIdxRow rowToDelete : Sets.difference(rows, rowsInDb)) {
             toDelete.add(rowToDelete.getId());
         }
         TestHashComponentsStreamStore.of(tables).deleteStreams(t, toDelete);
