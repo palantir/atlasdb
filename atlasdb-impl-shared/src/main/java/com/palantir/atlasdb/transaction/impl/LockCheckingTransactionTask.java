ec4d13f7d71a667e21fa1cddb83a26249e8d4036
Gokcan Ozakdag <gozakdag@palantir.com>
2018-08-16 19:30:05 +0100

    release notes

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/LockCheckingTransactionTask.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/LockCheckingTransactionTask.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.palantir.atlasdb.transaction.impl;
 
 import com.google.common.collect.ImmutableSet;
@@ -26,9 +25,15 @@ import com.palantir.lock.v2.TimelockService;
 
 /**
  *  Best effort attempt to keep backwards compatibility while making immutableTs lock validation optional on reads.
- *  Validating immutableTs lock only on commits rather than on every read may cause the TransactionTask to throw
- *  an unexpected non-retriable error as reads are not idempotent. This wrapper task will convert the exception thrown
- *  by the underlying task into a retriable lock timeout exception if immutableTs lock is not valid anymore.
+ *  <p>
+ *  If a read is performed without validating immutableTs lock on a thoroughly swept table, there is no guarantee on
+ *  consistency of data read; as sweep may remove data that is being read. This will not cause any correctness
+ *  issues as immutableTs lock will be checked on commit time, and transaction will be aborted if lock is invalid.
+ *  Although this prevents committing corrupted values, reading inconsistent data may cause tasks to throw
+ *  non-retriable exceptions; causing a possible behaviour change.
+ *  <p>
+ *  This wrapper task will convert the exception thrown by the underlying task into a retriable lock timeout exception
+ *  if immutableTs lock is not valid anymore.
  */
 public class LockCheckingTransactionTask<T, E extends Exception> implements TransactionTask<T, E> {
     private final TransactionTask<T, E> delegate;
@@ -41,7 +46,6 @@ public class LockCheckingTransactionTask<T, E extends Exception> implements Tran
         this.delegate = delegate;
         this.timelockService = timelockService;
         this.immutableTsLock = immutableTsLock;
-
     }
 
     public T execute(Transaction transaction) throws E {
