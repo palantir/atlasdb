fc739787e692445e028cf8efd59bdf36c12bfc7a
Jeremy Kong <jeremykong@hotmail.com>
2018-08-20 18:03:31 +0100

    fix script

--- atlasdb-jepsen-tests/scripts/ensure-date-util-present.sh
+++ atlasdb-jepsen-tests/scripts/ensure-date-util-present.sh
@@ -2,19 +2,16 @@
 
 set -x
 
-if command -v python &>/dev/null; then
+if command -v python >/dev/null 2>&1; then
     echo "Python is present."
 else
     echo "Python not found, installing."
     sudo apt-get install python
 fi
 
-python -c "import dateutil" &>/dev/null
-dateutilAbsent=$?
-
-if [[ ${dateutilAbsent} -ne 0 ]]; then
-    echo "DateUtil not found, installing."
-    sudo apt-get install python-dateutil
-else
+if python -c "import dateutil" >/dev/null 2>&1; then
     echo "DateUtil is present."
+else
+    echo "DateUtil not found. Installing."
+    sudo apt-get install python-dateutil
 fi
