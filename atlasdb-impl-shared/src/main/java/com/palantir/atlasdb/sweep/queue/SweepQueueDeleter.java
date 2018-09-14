ebb44e028ea29544be96e01af67689f4b27dc733
David Xiao <dxiao@palantir.com>
2018-09-13 23:43:19 -0700

    Targeted sweeper ignores queued writes where the table has been deleted.

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/queue/SweepQueueDeleter.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/queue/SweepQueueDeleter.java
@@ -21,13 +21,20 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.collect.Iterables;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.keyvalue.impl.TableMappingNotFoundException;
+import com.palantir.atlasdb.logging.LoggingArgs;
 import com.palantir.atlasdb.sweep.Sweeper;
 
 public class SweepQueueDeleter {
+    private static final Logger log = LoggerFactory.getLogger(SweepQueueDeleter.class);
+
     private final KeyValueService kvs;
     private final TargetedSweepFollower follower;
 
@@ -48,19 +55,31 @@ public class SweepQueueDeleter {
     public void sweep(Collection<WriteInfo> writes, Sweeper sweeper) {
         Map<TableReference, Map<Cell, Long>> maxTimestampByCell = writesPerTable(writes, sweeper);
         for (Map.Entry<TableReference, Map<Cell, Long>> entry : maxTimestampByCell.entrySet()) {
-            Iterables.partition(entry.getValue().keySet(), SweepQueueUtils.BATCH_SIZE_KVS)
-                    .forEach(cells -> {
-                        Map<Cell, Long> maxTimestampByCellPartition = cells.stream()
-                                .collect(Collectors.toMap(Function.identity(), entry.getValue()::get));
-                        follower.run(entry.getKey(), maxTimestampByCellPartition.keySet());
-                        if (sweeper.shouldAddSentinels()) {
-                            kvs.addGarbageCollectionSentinelValues(entry.getKey(),
-                                    maxTimestampByCellPartition.keySet());
-                            kvs.deleteAllTimestamps(entry.getKey(), maxTimestampByCellPartition, false);
-                        } else {
-                            kvs.deleteAllTimestamps(entry.getKey(), maxTimestampByCellPartition, true);
-                        }
-                    });
+            try {
+                Iterables.partition(entry.getValue().keySet(), SweepQueueUtils.BATCH_SIZE_KVS)
+                        .forEach(cells -> {
+                            Map<Cell, Long> maxTimestampByCellPartition = cells.stream()
+                                    .collect(Collectors.toMap(Function.identity(), entry.getValue()::get));
+                            follower.run(entry.getKey(), maxTimestampByCellPartition.keySet());
+                            if (sweeper.shouldAddSentinels()) {
+                                kvs.addGarbageCollectionSentinelValues(entry.getKey(),
+                                        maxTimestampByCellPartition.keySet());
+                                kvs.deleteAllTimestamps(entry.getKey(), maxTimestampByCellPartition, false);
+                            } else {
+                                kvs.deleteAllTimestamps(entry.getKey(), maxTimestampByCellPartition, true);
+                            }
+                        });
+            } catch (IllegalArgumentException e) {
+                // hack because InMemoryKVS doesn't throw a TableMappingNotFoundException...
+                if (e.getCause() instanceof TableMappingNotFoundException || e.getMessage().endsWith("does not exist")) {
+                    log.debug("Could not resolve full name for table reference {}, "
+                            + "assuming table has been deleted and therefore relevant cells as well.",
+                            LoggingArgs.tableRef(entry.getKey()));
+                    log.trace("Stack trace follows:", e);
+                } else {
+                    throw e;
+                }
+            }
         }
     }
 
