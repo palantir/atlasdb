08fefbc58f7d3d9950d93a41554137aa7857dc0b
Felix de Souza <fdesouza@palantir.com>
2018-10-02 10:42:21 +0100

    Test that adds coverage.

--- atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/transaction/impl/AbstractSerializableTransactionTest.java
+++ atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/transaction/impl/AbstractSerializableTransactionTest.java
@@ -702,6 +702,25 @@ public abstract class AbstractSerializableTransactionTest extends AbstractTransa
         t1.commit();
     }
 
+    @Test
+    public void testMultipleReadsToSameColumnRangeAcrossRows() {
+        byte[] row1 = PtBytes.toBytes("row1");
+        byte[] row2 = PtBytes.toBytes("row2");
+
+        Transaction transaction = startTransaction();
+        Map<byte[], BatchingVisitable<Map.Entry<Cell, byte[]>>> columnRange =
+                transaction.getRowsColumnRange(TEST_TABLE, ImmutableList.of(row1),
+                        BatchColumnRangeSelection.create(PtBytes.toBytes("col"), PtBytes.toBytes("col0"), 1));
+        columnRange.values().forEach(visitable -> visitable.batchAccept(10, t -> true));
+        Map<byte[], BatchingVisitable<Map.Entry<Cell, byte[]>>> columnRangeDifferentRow =
+                transaction.getRowsColumnRange(TEST_TABLE, ImmutableList.of(row2),
+                        BatchColumnRangeSelection.create(PtBytes.toBytes("col"), PtBytes.toBytes("col0"), 1));
+        columnRangeDifferentRow.values().forEach(visitable -> visitable.batchAccept(10, t -> true));
+        put(transaction, "mutation to ensure", "conflict", "handling");
+        transaction.commit();
+
+    }
+
     private void writeColumns() {
         Transaction t1 = startTransaction();
         int totalPuts = 101;
