fbf54b427e3b2b08d18e151c8ecac4e6e4cd5555
Jeremy Kong <jeremykong@hotmail.com>
2018-10-18 14:00:31 +0100

    need to ignore derived property in JSON

--- atlasdb-dbkvs-hikari/src/main/java/com/palantir/nexus/db/pool/config/ConnectionConfig.java
+++ atlasdb-dbkvs-hikari/src/main/java/com/palantir/nexus/db/pool/config/ConnectionConfig.java
@@ -110,6 +110,7 @@ public abstract class ConnectionConfig {
         return "db-pool";
     }
 
+    @JsonIgnore
     @Value.Derived
     public String getConnectionPoolName() {
         return String.format(
