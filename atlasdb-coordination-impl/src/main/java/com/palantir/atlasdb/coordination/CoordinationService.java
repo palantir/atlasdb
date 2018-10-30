2f3099fde50623a123a703e34c65c3d927aab0c3
Jeremy Kong <jeremykong@hotmail.com>
2018-10-30 14:43:24 +0000

    Coordination Service and simple impl

---
+++ atlasdb-coordination-impl/src/main/java/com/palantir/atlasdb/coordination/CoordinationService.java
@@ -0,0 +1,56 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.coordination;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * A {@link CoordinationService} is used to agree on values being relevant or correct at a given timestamp.
+ * The sequence of values being agreed should evolve in a backwards consistent manner; that is, if I read a value
+ * for some timestamp TS and make a decision at TS, all future values written to the {@link CoordinationService} should
+ * also ensure that no decision that would be inconsistent with the decision made at TS would be made at TS. In general,
+ * this means that one should avoid changes that affect behaviour of values below the validity bound.
+ *
+ * It is the responsibility of the caller to preserve the above property.
+ */
+public interface CoordinationService<T> {
+    /**
+     * Returns a value that the coordination service has agreed is appropriate at the provided timestamp.
+     *
+     * The value returned by this method may change over time. However, values returned by this method would
+     * be consistent at this timestamp - that is, the service should never return values for the same timestamp
+     * that would be contradictory or result in different operation for uses at this timestamp.
+     *
+     * This operation will return an empty Optional if no value has been agreed upon yet, or if the current value agreed
+     * by the coordination service is not valid at the provided timestamp.
+     *
+     * @param timestamp timestamp to retrieve the coordinated value for
+     * @return value associated with that timestamp
+     */
+    Optional<ValueAndBound<T>> getValueForTimestamp(long timestamp);
+
+    /**
+     * Updates the value that this coordination service stores, marking this value as valid up to a certain timestamp.
+     *
+     * Evolutions of the value must be compatible in terms of backwards consistency as defined in the class docs.
+     *
+     * @param transform transformation to apply to the existing value and bound the coordination service agrees on
+     * @return true if and only if the transformation was applied
+     */
+    boolean tryTransformCurrentValue(Function<Optional<ValueAndBound<T>>, ValueAndBound<T>> transform);
+}
---
+++ atlasdb-coordination-impl/src/main/java/com/palantir/atlasdb/coordination/CoordinationServiceImpl.java
@@ -0,0 +1,123 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.coordination;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+import java.util.function.LongSupplier;
+
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.palantir.remoting3.ext.jackson.ObjectMappers;
+
+import okio.ByteString;
+
+public class CoordinationServiceImpl<T> implements CoordinationService<T> {
+    private static final ObjectMapper OBJECT_MAPPER = ObjectMappers.newServerObjectMapper();
+
+    private final CoordinationStore store;
+    private final AtomicReference<SequenceAndValueAndBound<T>> cache = new AtomicReference<>();
+    private final Class<T> clazz;
+    private final LongSupplier sequenceNumberSupplier;
+
+    public CoordinationServiceImpl(CoordinationStore store, Class<T> clazz, LongSupplier sequenceNumberSupplier) {
+        this.store = store;
+        this.clazz = clazz;
+        this.sequenceNumberSupplier = sequenceNumberSupplier;
+    }
+
+    @Override
+    public Optional<ValueAndBound<T>> getValueForTimestamp(long timestamp) {
+        SequenceAndValueAndBound<T> cachedReference = cache.get();
+        if (cachedReference == null || cachedReference.bound() < timestamp) {
+            return store.getCoordinationValue()
+                    .filter(sequenceAndBound -> sequenceAndBound.bound() < timestamp)
+                    .map(this::getCorrespondingValueFromStore);
+        }
+        // TODO (jkong): Intern if performance is bad
+        return Optional.of(ImmutableValueAndBound.of(cachedReference.value(), cachedReference.bound()));
+    }
+
+    private void accumulateCachedValue(SequenceAndBound presentSequenceAndBound, Optional<T> valueOptional) {
+        ImmutableSequenceAndValueAndBound<T> newCacheValue =
+                ImmutableSequenceAndValueAndBound.of(
+                        presentSequenceAndBound.sequence(),
+                        valueOptional,
+                        presentSequenceAndBound.bound());
+        cache.accumulateAndGet(newCacheValue, (svb1, svb2) -> {
+            if (svb1.bound() > svb2.bound()) {
+                return svb1;
+            }
+            return svb2;
+        });
+    }
+
+    @Override
+    public boolean tryTransformCurrentValue(
+            Function<Optional<ValueAndBound<T>>, ValueAndBound<T>> transform) {
+        Optional<SequenceAndBound> sequenceAndBound = store.getCoordinationValue();
+        Optional<ValueAndBound<T>> valueAndBound = sequenceAndBound.map(this::getCorrespondingValueFromStore);
+
+        ValueAndBound<T> target = transform.apply(valueAndBound);
+        long sequenceNumber = sequenceNumberSupplier.getAsLong();
+        store.putValue(sequenceNumber, serializeByteString(
+                target.value().orElseThrow(() -> new RuntimeException("Cannot put an empty value"))));
+        return store.checkAndSetCoordinationValue(sequenceAndBound,
+                ImmutableSequenceAndBound.of(sequenceNumber, target.bound()))
+                .successful();
+    }
+
+    private ValueAndBound<T> getCorrespondingValueFromStore(SequenceAndBound sequenceAndBound) {
+        return ImmutableValueAndBound.of(readValueFromStore(sequenceAndBound), sequenceAndBound.bound());
+    }
+
+    private Optional<T> readValueFromStore(SequenceAndBound presentSequenceAndBound) {
+        Optional<T> valueOptional = store.getValue(presentSequenceAndBound.sequence()).map(this::deserializeByteString);
+        accumulateCachedValue(presentSequenceAndBound, valueOptional);
+        return valueOptional;
+    }
+
+    private ByteString serializeByteString(T object) {
+        try {
+            return ByteString.of(OBJECT_MAPPER.writeValueAsBytes(object));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private T deserializeByteString(ByteString byteString) {
+        try {
+            return OBJECT_MAPPER.readValue(byteString.toByteArray(), clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Value.Immutable
+    interface SequenceAndValueAndBound<T> {
+        @Value.Parameter
+        long sequence();
+        @Value.Parameter
+        Optional<T> value();
+        @Value.Parameter
+        long bound();
+    }
+}
