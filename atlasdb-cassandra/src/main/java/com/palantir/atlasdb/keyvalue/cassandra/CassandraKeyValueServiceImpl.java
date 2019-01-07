2f06a89a071d5e8f4f137d4b28d6369f56ceaed4
Jeremy Kong <jeremykong@hotmail.com>
2019-01-07 17:17:40 +0000

    fix bug with timestamps

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueServiceImpl.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueServiceImpl.java
@@ -1679,7 +1679,10 @@ public class CassandraKeyValueServiceImpl extends AbstractKeyValueService implem
     }
 
     private static Column prepareColumnForPutUnlessExists(Entry<Cell, byte[]> insertion) {
-        return new Column(ByteBuffer.wrap(insertion.getKey().getColumnName()))
+        return new Column(
+                CassandraKeyValueServices.makeCompositeBuffer(
+                        insertion.getKey().getColumnName(),
+                        CassandraConstants.CAS_TABLE_TIMESTAMP))
                 .setValue(insertion.getValue());
     }
 
