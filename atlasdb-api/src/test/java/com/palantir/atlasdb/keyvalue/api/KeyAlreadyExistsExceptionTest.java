f9ac7dec4968dd40a0a1721963f9306820652b49
Jeremy Kong <jeremykong@hotmail.com>
2019-01-15 18:56:43 +0000

    legacy deserialization test

---
+++ atlasdb-api/src/test/java/com/palantir/atlasdb/keyvalue/api/KeyAlreadyExistsExceptionTest.java
@@ -0,0 +1,46 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.keyvalue.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.URL;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+import com.palantir.atlasdb.encoding.PtBytes;
+
+public class KeyAlreadyExistsExceptionTest {
+    @Test
+    public void canDeserializeLegacyVersionsOfException() throws IOException, ClassNotFoundException {
+        URL exceptionSerializedFormUrl = KeyAlreadyExistsExceptionTest.class.getClassLoader()
+                .getResource("serializedLegacyKeyAlreadyExistsException.dat");
+        FileInputStream fileInputStream = new FileInputStream(new File(exceptionSerializedFormUrl.getPath()));
+        KeyAlreadyExistsException deserialized = (KeyAlreadyExistsException)
+                new ObjectInputStream(fileInputStream).readObject();
+
+        assertThat(deserialized.getMessage()).isEqualTo("aaa");
+        assertThat(deserialized.getExistingKeys()).containsExactly(
+                Cell.create(PtBytes.toBytes("row"), PtBytes.toBytes("col")));
+        assertThat(deserialized.getKnownSuccessfullyCommittedKeys()).isEqualTo(ImmutableList.of());
+    }
+}
