4f714f35e8749dd34bc95f9cad8ae74141530a31
Grgur Petric Maretic <gmaretic@palantir.com>
2019-01-22 15:27:44 +0000

    [no release notes] Fix and simplify benchmarking orchestration

--- push-benchmark-cluster.sh
+++ push-benchmark-cluster.sh
@@ -5,13 +5,18 @@ source scripts/benchmarks/servers.txt
 
 ./scripts/benchmarks/render-configs.sh
 
+SCRIPTS_FOLDER="scripts/benchmarks"
 SERVER_SIDE_SCRIPT="run-on-il.sh"
+REMOTE_DIR="/tmp/timelock-benchmark"
+CASSANDRA_KEYSTORE="keystore.jks"
+CASSANDRA_TRUSTSTORE="truststore.jks"
+
 declare -a SERVERS=("$SERVER1" "$SERVER2" "$SERVER3")
 
 rm -rf timelock-server-benchmark-cluster/build/distributions/
 ./gradlew --parallel timelock-server-benchmark-cluster:distTar
 
-SLS_FILE="$(find ./timelock-server-benchmark-cluster/build/distributions/ -name '*sls.tgz' | tail -1)"
+SLS_FILE="$(find ./timelock-server-benchmark-cluster/build/distributions -name '*sls.tgz' | tail -1)"
 
 echo "sls file: $SLS_FILE ..."
 
@@ -20,14 +25,18 @@ for SERVER in "${SERVERS[@]}"
 do
 {
   echo "server: $SERVER"
-
   echo "copying sls file"
-  REMOTE_DIR="/opt/palantir/timelock"
-  ssh $SERVER "rm -rf $REMOTE_DIR/*"
+
+  ssh $SERVER "rm -rf $REMOTE_DIR && mkdir $REMOTE_DIR"
   scp $SLS_FILE "$SERVER:$REMOTE_DIR"
 
+  echo "copying keystore and truststore"
+
+  scp "$SCRIPTS_FOLDER/$CASSANDRA_KEYSTORE" "$SERVER:$REMOTE_DIR"
+  scp "$SCRIPTS_FOLDER/$CASSANDRA_TRUSTSTORE" "$SERVER:$REMOTE_DIR"
+
   echo "running remote script"
-  scp scripts/benchmarks/$SERVER_SIDE_SCRIPT "$SERVER:$REMOTE_DIR"
+  scp "$SCRIPTS_FOLDER/$SERVER_SIDE_SCRIPT" "$SERVER:$REMOTE_DIR"
 
   YML_FILE="timelock-remote$SERVER_INDEX.yml"
   ssh $SERVER "$REMOTE_DIR/$SERVER_SIDE_SCRIPT $YML_FILE"
--- scripts/benchmarks/render-configs.sh
+++ scripts/benchmarks/render-configs.sh
@@ -10,7 +10,7 @@ declare -a configs=("timelock-remote0.yml" "timelock-remote1.yml" "timelock-remo
 
 for config in "${configs[@]}"
 do
-  cat "$TEMPLATE_DIR/$config" | sed "s/SERVER1/$SERVER1/g" | sed "s/SERVER2/$SERVER2/g" | sed "s/SERVER3/$SERVER3/g" > "$RENDERED_DIR/$config"
+  cat "$TEMPLATE_DIR/$config" | sed "s/SERVER1/$SERVER1/g" | sed "s/SERVER2/$SERVER2/g" | sed "s/SERVER3/$SERVER3/g" | sed "s/KEYSTORE_PASSWORD/$KEYSTORE_PASSWORD/g" > "$RENDERED_DIR/$config"
 done
 
 # timelock-server-benchmark-client
@@ -19,5 +19,5 @@ RENDERED_DIR="./timelock-server-benchmark-client/var/conf"
 
 config="benchmark-server.yml"
 
-cat "$TEMPLATE_DIR/$config" | sed "s/SERVER1/$SERVER1/g" | sed "s/SERVER2/$SERVER2/g" | sed "s/SERVER3/$SERVER3/g" | sed "s/CASSANDRA1/$CASSANDRA1/g" | sed "s/CASSANDRA2/$CASSANDRA2/g" | sed "s/CASSANDRA3/$CASSANDRA3/g" > "$RENDERED_DIR/$config"
+cat "$TEMPLATE_DIR/$config" | sed "s/SERVER1/$SERVER1/g" | sed "s/SERVER2/$SERVER2/g" | sed "s/SERVER3/$SERVER3/g" | sed "s/CASSANDRA1/$CASSANDRA1/g" | sed "s/CASSANDRA2/$CASSANDRA2/g" | sed "s/CASSANDRA3/$CASSANDRA3/g" | sed "s/KEYSTORE_PASSWORD/$KEYSTORE_PASSWORD/g" > "$RENDERED_DIR/$config"
 
