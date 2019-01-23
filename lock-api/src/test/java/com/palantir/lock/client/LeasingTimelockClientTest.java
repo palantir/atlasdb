82a2126e9ffbdcc8f4336e699fb10cdca3d7f175
Gokcan Ozakdag <gozakdag@palantir.com>
2019-01-23 11:28:49 +0000

    smoke tests for leasing timelock client

---
+++ lock-api/src/test/java/com/palantir/lock/client/LeasingTimelockClientTest.java
@@ -0,0 +1,102 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.lock.client;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.Duration;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableSet;
+import com.palantir.lock.v2.ContractedLockResponse;
+import com.palantir.lock.v2.ContractedRefreshLockResponse;
+import com.palantir.lock.v2.ContractedStartIdentifiedAtlasDbTransactionResponse;
+import com.palantir.lock.v2.LockImmutableTimestampResponse;
+import com.palantir.lock.v2.LockRequest;
+import com.palantir.lock.v2.LockResponse;
+import com.palantir.lock.v2.LockToken;
+import com.palantir.lock.v2.StartIdentifiedAtlasDbTransactionRequest;
+import com.palantir.lock.v2.StartIdentifiedAtlasDbTransactionResponse;
+import com.palantir.lock.v2.TimelockService;
+import com.palantir.lock.v2.TimestampAndPartition;
+
+public class LeasingTimelockClientTest {
+    private TimelockServerInterface timelockService = mock(TimelockServerInterface.class);
+    private TimelockService timelockClient;
+
+    private static final Duration leaseDuration = Duration.ofSeconds(1);
+    private static final LockToken LOCK_TOKEN = LockToken.of(UUID.randomUUID());
+    private static final LockToken LOCK_TOKEN_2 = LockToken.of(UUID.randomUUID());
+    private static final LockResponse LOCK_RESPONSE = LockResponse.successful(LOCK_TOKEN);
+
+    @Before
+    public void setUp() {
+        timelockClient = LeasingTimelockClient.create(timelockService);
+    }
+
+    @Test
+    public void delegatesLockRequest() {
+        LockRequest lockRequest = mock(LockRequest.class);
+        when(timelockService.contractedLock(lockRequest)).thenReturn(
+                ContractedLockResponse.of(LOCK_RESPONSE, leaseDuration));
+
+        LockResponse lockResponse = timelockClient.lock(lockRequest);
+
+        verify(timelockService).contractedLock(lockRequest);
+        assertEquals(LOCK_RESPONSE, lockResponse);
+    }
+
+    @Test
+    public void delegatesRefreshLockRequest() {
+        Set<LockToken> lockTokens = ImmutableSet.of(LOCK_TOKEN, LOCK_TOKEN_2);
+        when(timelockService.contractedRefreshLockLeases(lockTokens)).thenReturn(
+                ContractedRefreshLockResponse.of(lockTokens, leaseDuration));
+
+        Set<LockToken> refreshedTokens = timelockClient.refreshLockLeases(lockTokens);
+
+        verify(timelockService).contractedRefreshLockLeases(refreshedTokens);
+        assertEquals(lockTokens, refreshedTokens);
+    }
+
+    @Test
+    public void delegatesStartAtlasdbTransactionRequest() {
+        StartIdentifiedAtlasDbTransactionRequest request = mock(StartIdentifiedAtlasDbTransactionRequest.class);
+        StartIdentifiedAtlasDbTransactionResponse response = startTransactionResponseWith(LOCK_TOKEN);
+        when(timelockService.contractedStartIdentifiedAtlasDbTransaction(request)).thenReturn(
+                ContractedStartIdentifiedAtlasDbTransactionResponse.of(response, leaseDuration));
+
+        StartIdentifiedAtlasDbTransactionResponse clientResponse =
+                timelockClient.startIdentifiedAtlasDbTransaction(request);
+
+        verify(timelockService).contractedStartIdentifiedAtlasDbTransaction(request);
+        assertEquals(response, clientResponse);
+    }
+
+    private StartIdentifiedAtlasDbTransactionResponse startTransactionResponseWith(LockToken lockToken) {
+        return StartIdentifiedAtlasDbTransactionResponse.of(
+                LockImmutableTimestampResponse.of(1L, lockToken),
+                mock(TimestampAndPartition.class)
+        );
+    }
+}
