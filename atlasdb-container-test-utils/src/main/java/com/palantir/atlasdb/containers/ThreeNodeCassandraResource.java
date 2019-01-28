c3233c21353d42669d7cb2be393637931d828a5b
Grgur Petric Maretic <gmaretic@palantir.com>
2019-01-28 15:52:53 +0000

    Wire up OS palantir cassandra, implement cas using multicas, add tests that are currently ignored pending new docker image

---
+++ atlasdb-container-test-utils/src/main/java/com/palantir/atlasdb/containers/ThreeNodeCassandraResource.java
@@ -0,0 +1,82 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.containers;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.junit.rules.ExternalResource;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import com.palantir.atlasdb.keyvalue.api.KeyValueService;
+import com.palantir.atlasdb.keyvalue.cassandra.CassandraKeyValueService;
+import com.palantir.atlasdb.keyvalue.cassandra.CassandraKeyValueServiceImpl;
+import com.palantir.atlasdb.keyvalue.impl.KvsManager;
+import com.palantir.atlasdb.keyvalue.impl.TestResourceManager;
+import com.palantir.atlasdb.keyvalue.impl.TransactionManagerManager;
+import com.palantir.atlasdb.transaction.api.TransactionManager;
+
+public class ThreeNodeCassandraResource extends ExternalResource implements KvsManager, TransactionManagerManager {
+    private final Supplier<KeyValueService> supplier;
+    private Containers containers;
+    private TestResourceManager testResourceManager;
+
+    public ThreeNodeCassandraResource() {
+        this.supplier = () -> CassandraKeyValueServiceImpl.createForTesting(
+                ThreeNodeCassandraCluster.KVS_CONFIG, Optional.empty());
+    }
+
+    public Statement apply(Statement base, Description description) {
+        containers = new Containers(description.getTestClass()).with(new ThreeNodeCassandraCluster());
+        testResourceManager = new TestResourceManager(supplier);
+        return super.apply(base, description);
+    }
+
+    @Override
+    public void before() throws Throwable {
+        containers.before();
+    }
+
+    @Override
+    public void after() {
+        testResourceManager.after();
+    }
+
+    /**
+     * Returns the memoized instance of the {@link CassandraKeyValueService} given by the supplier from the constructor.
+     */
+    @Override
+    public CassandraKeyValueService getDefaultKvs() {
+        return (CassandraKeyValueService) testResourceManager.getDefaultKvs();
+    }
+
+    @Override
+    public void registerKvs(KeyValueService kvs) {
+        testResourceManager.registerKvs(kvs);
+    }
+
+    @Override
+    public void registerTransactionManager(TransactionManager manager) {
+        testResourceManager.registerTransactionManager(manager);
+    }
+
+    @Override
+    public Optional<TransactionManager> getLastRegisteredTransactionManager() {
+        return testResourceManager.getLastRegisteredTransactionManager();
+    }
+}
---
+++ atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/keyvalue/impl/AbstractMultiCasTest.java
@@ -0,0 +1,73 @@
+package com.palantir.atlasdb.keyvalue.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+import com.palantir.atlasdb.AtlasDbConstants;
+import com.palantir.atlasdb.encoding.PtBytes;
+import com.palantir.atlasdb.keyvalue.api.Cell;
+import com.palantir.atlasdb.keyvalue.api.KeyAlreadyExistsException;
+import com.palantir.atlasdb.keyvalue.api.KeyValueService;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+
+public class AbstractMultiCasTest {
+    private static final byte[] ROW_1 = PtBytes.toBytes("row1");
+    private static final byte[] COL_1 = PtBytes.toBytes("col1");
+    public static final Cell FIRST_CELL = Cell.create(ROW_1, COL_1);
+    private static final byte[] COL_2 = PtBytes.toBytes("col2");
+    private static final byte[] COL_3 = PtBytes.toBytes("col3");
+    private static final byte[] DATA = PtBytes.toBytes("data");
+
+    private static final TableReference TEST_TABLE = TableReference.createFromFullyQualifiedName("test.table");
+
+    private final KvsManager kvsManager;
+    private KeyValueService kvs;
+
+    public AbstractMultiCasTest(KvsManager kvsManager) {
+        this.kvsManager = kvsManager;
+    }
+
+    @Before
+    public void setup() {
+        kvs = kvsManager.getDefaultKvs();
+        kvs.createTable(TEST_TABLE, AtlasDbConstants.GENERIC_TABLE_METADATA);
+    }
+
+    @After
+    public void cleanup() {
+        kvs.truncateTable(TEST_TABLE);
+    }
+
+    @Test
+    public void atomicallyFailsForSingleRowWhenColumnExists() {
+        kvs.put(TEST_TABLE, ImmutableMap.of(Cell.create(ROW_1, COL_3), DATA), 0);
+        assertThatThrownBy(() -> kvs.putUnlessExists(TEST_TABLE, putDataInCols(COL_1, COL_2, COL_3)))
+                .isInstanceOf(KeyAlreadyExistsException.class);
+
+        assertThat(kvs.get(TEST_TABLE, ImmutableMap.of(FIRST_CELL, Long.MAX_VALUE))).isEmpty();
+    }
+
+    @Test
+    public void suceedsWhenThereIsDataInOtherColumns() {
+        kvs.put(TEST_TABLE, ImmutableMap.of(Cell.create(ROW_1, COL_3), DATA), 0);
+        kvs.putUnlessExists(TEST_TABLE, putDataInCols(COL_1, COL_2));
+
+        assertThat(kvs.get(TEST_TABLE, ImmutableMap.of(FIRST_CELL, Long.MAX_VALUE)).get(FIRST_CELL).getContents())
+                .contains(DATA);
+    }
+
+    private static Map<Cell, byte[]> putDataInCols(byte[]... cols) {
+        return Stream.of(cols)
+                .map(column -> Cell.create(ROW_1, column))
+                .collect(Collectors.toMap(cell -> cell, ignore -> DATA));
+    }
+}
