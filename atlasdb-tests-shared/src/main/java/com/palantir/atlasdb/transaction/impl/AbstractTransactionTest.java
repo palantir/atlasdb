f9e066a71f673e6bb837a840333ad5cac9b3ce1d
Sandor Van Wassenhove <sandorw@palantir.com>
2019-02-13 10:57:57 +0000

    extra test case

--- atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/transaction/impl/AbstractTransactionTest.java
+++ atlasdb-tests-shared/src/main/java/com/palantir/atlasdb/transaction/impl/AbstractTransactionTest.java
@@ -273,7 +273,7 @@ public abstract class AbstractTransactionTest extends TransactionTestSetup {
                 new ColumnRangeSelection(PtBytes.EMPTY_BYTE_ARRAY, PtBytes.EMPTY_BYTE_ARRAY),
                 1,
                 1);
-        assertFalse(iterator.hasNext());
+        assertThat(iterator.hasNext()).isFalse();
     }
 
     @Test
@@ -291,7 +291,28 @@ public abstract class AbstractTransactionTest extends TransactionTestSetup {
                 1,
                 1);
         assertThat(PtBytes.toBytes("v3")).isEqualTo(iterator.next().getValue().getContents());
-        assertFalse(iterator.hasNext());
+        assertThat(iterator.hasNext()).isFalse();
+    }
+
+    @Test
+    public void testRowsColumnRange_forwardProgressAfterValidResult() {
+        putDirect("row1", "col1", "v1a", 1);
+        putDirect("row1", "col1", "v1b", 2);
+        putDirect("row1", "col1", "v1c", 3);
+        putDirect("row1", "col1", "v1d", 4);
+        putDirect("row1", "col1", "v1e", 5);
+        putDirect("row1", "col2", "v2", 5);
+
+        byte[] rowBytes = PtBytes.toBytes("row1");
+        RowColumnRangeIterator iterator = keyValueService.getRowsColumnRange(
+                TEST_TABLE,
+                ImmutableList.of(rowBytes),
+                new ColumnRangeSelection(PtBytes.EMPTY_BYTE_ARRAY, PtBytes.EMPTY_BYTE_ARRAY),
+                1,
+                6);
+        assertThat(PtBytes.toBytes("v1e")).isEqualTo(iterator.next().getValue().getContents());
+        assertThat(PtBytes.toBytes("v2")).isEqualTo(iterator.next().getValue().getContents());
+        assertThat(iterator.hasNext()).isFalse();
     }
 
     @Test
