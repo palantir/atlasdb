d510fd0628a165d7539f3d0f2e7c000de1b58fd2
Jeremy Kong <jeremykong@hotmail.com>
2019-02-28 10:33:27 +0000

    [_transactions2] Part 23: Tuning Tables for Cassandra (#3776)
    * TableMetadataPersistence field
    
    * hasDenselyAccessedWideRows
    
    * Change transactions2 metadata
    
    * refactor to separate CassandraTableOptions + use new param
    
    * Extract compression options
    
    * CFD update and tests
    
    * Cassandra Table Options test
    
    * Transactions2 test
    
    * [no release notes]
    
    * Add missing return falses and tests to catch these
    
    * CR: extract variables for readability
    
    * CR: Optional.map(), and only persist true for hasDenselyAccessed...
    
    * Compare objects. A bit sad.
    
    * Revert "Compare objects. A bit sad."
    
    This reverts commit 2e5e9fd9130c9fe455cbd06b9c07a2c02b13a1b5.

---
+++ atlasdb-cassandra/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTableOptionsTest.java
@@ -0,0 +1,71 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.keyvalue.cassandra;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.palantir.atlasdb.table.description.TableMetadata;
+
+public class CassandraTableOptionsTest {
+    private static final TableMetadata DENSELY_ACCESSED_WIDE_ROWS_METADATA
+            = TableMetadata.internal().denselyAccessedWideRows(true).build();
+    private static final TableMetadata DENSELY_ACCESSED_WIDE_ROWS_APPEND_HEAVY_READ_LIGHT_METADATA
+            = TableMetadata.internal().denselyAccessedWideRows(true).appendHeavyAndReadLight(true).build();
+    private static final TableMetadata APPEND_HEAVY_READ_LIGHT_METADATA
+            = TableMetadata.internal().appendHeavyAndReadLight(true).build();
+    private static final TableMetadata NEGATIVE_LOOKUPS_METADATA
+            = TableMetadata.internal().negativeLookups(true).build();
+    private static final TableMetadata APPEND_HEAVY_READ_LIGHT_NEGATIVE_LOOKUPS_METADATA
+            = TableMetadata.internal().appendHeavyAndReadLight(true).negativeLookups(true).build();
+
+    @Test
+    public void tablesWithDenselyAccessedWideRowsAlwaysHaveLowBloomFilterFpChance() {
+        assertThat(CassandraTableOptions.bloomFilterFpChance(DENSELY_ACCESSED_WIDE_ROWS_METADATA))
+                .isEqualTo(CassandraConstants.DENSELY_ACCESSED_WIDE_ROWS_BLOOM_FILTER_FP_CHANCE);
+        assertThat(CassandraTableOptions.bloomFilterFpChance(
+                DENSELY_ACCESSED_WIDE_ROWS_APPEND_HEAVY_READ_LIGHT_METADATA))
+                .isEqualTo(CassandraConstants.DENSELY_ACCESSED_WIDE_ROWS_BLOOM_FILTER_FP_CHANCE);
+    }
+
+    @Test
+    public void tablesWithAppendHeavyReadLightHaveSizeTieredBasedBloomFilterFpChance() {
+        assertThat(CassandraTableOptions.bloomFilterFpChance(APPEND_HEAVY_READ_LIGHT_METADATA))
+                .isEqualTo(CassandraConstants.DEFAULT_SIZE_TIERED_COMPACTION_BLOOM_FILTER_FP_CHANCE);
+        assertThat(CassandraTableOptions.bloomFilterFpChance(APPEND_HEAVY_READ_LIGHT_NEGATIVE_LOOKUPS_METADATA))
+                .isEqualTo(CassandraConstants.NEGATIVE_LOOKUPS_SIZE_TIERED_BLOOM_FILTER_FP_CHANCE);
+    }
+
+    @Test
+    public void tablesWithNegativeLookupsHaveDifferentBloomFilterFpChance() {
+        assertThat(CassandraTableOptions.bloomFilterFpChance(NEGATIVE_LOOKUPS_METADATA))
+                .isEqualTo(CassandraConstants.NEGATIVE_LOOKUPS_BLOOM_FILTER_FP_CHANCE);
+        assertThat(CassandraTableOptions.bloomFilterFpChance(TableMetadata.allDefault()))
+                .isEqualTo(CassandraConstants.DEFAULT_LEVELED_COMPACTION_BLOOM_FILTER_FP_CHANCE);
+    }
+
+    @Test
+    public void tablesWithDenselyAccessedWideRowsHaveReducedIndexIntervals() {
+        assertThat(CassandraTableOptions.minIndexInterval(DENSELY_ACCESSED_WIDE_ROWS_METADATA))
+                .isEqualTo(CassandraConstants.DENSELY_ACCESSED_WIDE_ROWS_INDEX_INTERVAL)
+                .isLessThan(CassandraConstants.DEFAULT_MIN_INDEX_INTERVAL);
+        assertThat(CassandraTableOptions.maxIndexInterval(DENSELY_ACCESSED_WIDE_ROWS_METADATA))
+                .isEqualTo(CassandraConstants.DENSELY_ACCESSED_WIDE_ROWS_INDEX_INTERVAL)
+                .isLessThan(CassandraConstants.DEFAULT_MAX_INDEX_INTERVAL);
+    }
+}
