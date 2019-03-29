f6450c2bbf661d7e9816a5832c7b57a82a1a280f
Jeremy Kong <jeremykong@hotmail.com>
2019-03-29 20:02:10 +0000

    Untangle broken CKVSImpl method

--- atlasdb-cassandra-integration-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueServiceTransactionIntegrationTest.java
+++ atlasdb-cassandra-integration-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueServiceTransactionIntegrationTest.java
@@ -15,18 +15,35 @@
  */
 package com.palantir.atlasdb.keyvalue.cassandra;
 
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TestRule;
 
 import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.Futures;
 import com.palantir.atlasdb.cassandra.CassandraMutationTimestampProviders;
 import com.palantir.atlasdb.containers.CassandraResource;
+import com.palantir.atlasdb.keyvalue.api.Cell;
+import com.palantir.atlasdb.keyvalue.api.KeyAlreadyExistsException;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
+import com.palantir.atlasdb.transaction.encoding.TicketsEncodingStrategy;
 import com.palantir.atlasdb.transaction.impl.AbstractTransactionTest;
+import com.palantir.atlasdb.transaction.impl.TransactionConstants;
+import com.palantir.atlasdb.transaction.impl.TransactionTables;
+import com.palantir.atlasdb.transaction.service.SimpleTransactionService;
+import com.palantir.atlasdb.transaction.service.TransactionService;
+import com.palantir.atlasdb.transaction.service.WriteBatchingTransactionService;
 import com.palantir.exception.NotInitializedException;
 import com.palantir.flake.FlakeRetryingRule;
 import com.palantir.flake.ShouldRetry;
@@ -58,6 +75,46 @@ public class CassandraKeyValueServiceTransactionIntegrationTest extends Abstract
     @Override
     protected KeyValueService getKeyValueService() {
         return kvsSupplier.get();
+    }
+
+    @Test
+    public void f178923401274891() {
+        TransactionTables.createTables(keyValueService);
+        TransactionService s = SimpleTransactionService.createV2(keyValueService);
+        s.putUnlessExists(15, 666);
+        keyValueService.putUnlessExists(
+                TransactionConstants.TRANSACTIONS2_TABLE,
+                ImmutableMap.<Cell, byte[]>builder()
+                .put(TicketsEncodingStrategy.INSTANCE.encodeStartTimestampAsCell(31),
+                        TicketsEncodingStrategy.INSTANCE.encodeCommitTimestampAsValue(31, 999))
+                .build());
+
+    }
+
+    @Test
+    public void f1234567() {
+        TransactionTables.createTables(keyValueService);
+        TransactionTables.truncateTables(keyValueService);
+        TransactionService s = WriteBatchingTransactionService.create(
+                SimpleTransactionService.createV2(keyValueService));
+        ExecutorService es = Executors.newCachedThreadPool();
+        List<Future<?>> f = LongStream.range(0, 100)
+                .mapToObj(unused -> {
+                    return es.submit(() -> {
+                        try {
+                            s.putUnlessExists(unused, unused + 1);
+                            s.putUnlessExists(unused + 1, unused + 3);
+                            s.putUnlessExists(unused - 1, unused + 5);
+                            s.putUnlessExists(unused + 2, unused + 7);
+                            s.putUnlessExists(unused - 2, unused + 9);
+                        } catch (KeyAlreadyExistsException ex) {
+                            // bleh
+                            System.out.println(ex.getExistingKeys());
+                        }
+                    });
+                })
+                .collect(Collectors.toList());
+        f.forEach(Futures::getUnchecked);
     }
 
     private KeyValueService createAndRegisterKeyValueService() {
