90ad5932b6c9c6ce3b2309ebc7aacf06415a3d97
Grgur Petric Maretic <gmaretic@palantir.com>
2019-04-16 15:18:51 +0100

    Initial removal

--- timelock-agent/src/main/java/com/palantir/timelock/paxos/TimeLockAgent.java
+++ timelock-agent/src/main/java/com/palantir/timelock/paxos/TimeLockAgent.java
@@ -44,7 +44,6 @@ import com.palantir.conjure.java.config.ssl.TrustContext;
 import com.palantir.leader.PaxosLeaderElectionService;
 import com.palantir.lock.LockService;
 import com.palantir.timelock.TimeLockStatus;
-import com.palantir.timelock.clock.ClockSkewMonitorCreator;
 import com.palantir.timelock.config.DatabaseTsBoundPersisterConfiguration;
 import com.palantir.timelock.config.PaxosTsBoundPersisterConfiguration;
 import com.palantir.timelock.config.TimeLockDeprecatedConfiguration;
@@ -169,8 +168,6 @@ public class TimeLockAgent {
                 this::createInvalidatingTimeLockServices,
                 Suppliers.compose(TimeLockRuntimeConfiguration::maxNumberOfClients, runtime::get));
         registrar.accept(resource);
-
-        ClockSkewMonitorCreator.create(metricsManager, install, registrar).registerClockServices();
     }
 
     @SuppressWarnings("unused") // used by external health checks
