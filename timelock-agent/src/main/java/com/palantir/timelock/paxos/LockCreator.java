a4ca8820301a781ab3a399ce8139b4c0e76b03ac
Grgur Petric Maretic <gmaretic@palantir.com>
2019-05-03 11:50:10 +0100

    Test if blocking is what's breaking us

--- timelock-agent/src/main/java/com/palantir/timelock/paxos/LockCreator.java
+++ timelock-agent/src/main/java/com/palantir/timelock/paxos/LockCreator.java
@@ -47,8 +47,8 @@ public class LockCreator {
                 .build();
 
         LockServiceImpl rawLockService = LockServiceImpl.create(lockServerOptions, sharedExecutor);
-        CloseableLockService lockService = BlockingTimeLimitedLockService.create(rawLockService, blockingTimeoutMs);
+//        CloseableLockService lockService = BlockingTimeLimitedLockService.create(rawLockService, blockingTimeoutMs);
 
-        return new ThreadPooledLockService(lockService, -1, sharedThreadPool);
+        return new ThreadPooledLockService(rawLockService, -1, sharedThreadPool);
     }
 }
