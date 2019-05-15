c23455f733ef90a3ac6bbc59b2d84900cd06b6b6
Jeremy Kong <jeremykong@hotmail.com>
2019-05-15 16:14:54 +0100

    Bounded work queue

---
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/DefaultTaskExecutors.java
@@ -0,0 +1,48 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.transaction.impl;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import com.palantir.common.concurrent.NamedThreadFactory;
+import com.palantir.common.concurrent.PTExecutors;
+
+final class DefaultTaskExecutors {
+    private static final Duration DEFAULT_IDLE_TIMEOUT = Duration.ofSeconds(5);
+    private static final int SINGLE_THREAD = 1;
+
+    private static final int DEFAULT_QUEUE_CAPACITY = 10_000;
+
+    private DefaultTaskExecutors() {
+        // factory
+    }
+
+    static ExecutorService createDefaultDeleteExecutor() {
+        return PTExecutors.newThreadPoolExecutor(
+                SINGLE_THREAD,
+                SINGLE_THREAD,
+                DEFAULT_IDLE_TIMEOUT.toMillis(),
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(DEFAULT_QUEUE_CAPACITY),
+                new NamedThreadFactory("atlas-delete-executor", true),
+                new ThreadPoolExecutor.DiscardPolicy()); // Delete executor isn't required for correctness
+    }
+}
