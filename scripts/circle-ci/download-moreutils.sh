1621b7778ef3f2b1f3facf3681e4091c2d02d583
Jeremy Kong <jeremykong@hotmail.com>
2019-05-15 15:32:46 +0100

    Update moreutils link (#4035)
    * moreutils
    
    * [no release notes]

--- scripts/circle-ci/download-moreutils.sh
+++ scripts/circle-ci/download-moreutils.sh
@@ -2,8 +2,7 @@
 
 set -x -e
 
-MOREUTILS="moreutils_0.60-1_amd64.deb"
-
-curl -O "http://snapshot.debian.org/archive/debian/20170123T091837Z/pool/main/m/moreutils/$MOREUTILS"
+MOREUTILS="moreutils_0.63-1_amd64.deb"
+curl -O "http://http.us.debian.org/debian/pool/main/m/moreutils/$MOREUTILS"
 ar p $MOREUTILS data.tar.xz | sudo tar xJ --strip-components=3 -C /usr/bin/ ./usr/bin/ts
 rm $MOREUTILS
