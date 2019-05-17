81087c7f6cd24c7f2fe31c7672dac5fe00d9c8fc
Gokcan Ozakdag <gozakdag@palantir.com>
2019-05-17 13:59:35 +0300

    add AuthRequirement enum class

---
+++ timelock-impl/src/main/java/com/palantir/atlasdb/timelock/auth/impl/AuthRequirement.java
@@ -0,0 +1,35 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.timelock.auth.impl;
+
+public enum AuthRequirement {
+    /**
+     * All namespaces are locked, only privileged clients have access.
+     */
+    ALWAYS,
+
+    /**
+     * Non of the namespaces are locked
+     */
+    NEVER,
+
+    /**
+     * A namespace is locked only if a client claimed exclusive privileges for that namespace. Only privileged
+     * clients have access to locked namespaces.
+     */
+    PRIVILEGE_BASED
+}
