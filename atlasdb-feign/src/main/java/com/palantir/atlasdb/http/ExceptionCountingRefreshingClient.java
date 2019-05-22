f963078c33dd7892ff1289f6486c32f760fad6c5
Gokcan Ozakdag <gozakdag@palantir.com>
2019-05-22 11:09:30 +0100

    don't close if successful

--- atlasdb-feign/src/main/java/com/palantir/atlasdb/http/ExceptionCountingRefreshingClient.java
+++ atlasdb-feign/src/main/java/com/palantir/atlasdb/http/ExceptionCountingRefreshingClient.java
@@ -55,10 +55,15 @@ public class ExceptionCountingRefreshingClient implements Client {
 
     @Override
     public Response execute(Request request, Request.Options options) throws IOException {
-        try (Response response = delegate().execute(request, options)) {
+        Response response = null;
+        try {
+            response = delegate().execute(request, options);
             counter.set(0);
             return response;
         } catch (Exception e) {
+            if (response != null) {
+                response.close();
+            }
             counter.incrementAndGet();
             throw e;
         }
