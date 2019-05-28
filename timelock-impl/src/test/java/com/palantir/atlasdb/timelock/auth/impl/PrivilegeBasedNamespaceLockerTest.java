6c3feb31c8885a73d15b63f848c10bc4645167b0
Gokcan Ozakdag <gozakdag@palantir.com>
2019-05-28 17:50:16 +0100

    Timelock Auth Part 1: Introducing Authorizer (#3917)
    * auth api
    
    * authorizer tests
    
    * tests for authRequirer
    
    * [no release notes]
    
    * rename user to client
    
    * cr comments
    
    * cr comments
    
    * remove authenticator
    
    * adding TimelockNamespace
    
    * cr comments
    
    * move timelockNamespace to api
    
    * move admin knowledge to authorizer
    
    * rename name field
    
    * rename id builder field
    
    * [no release notes]
    
    * add privileges interface
    
    * add AuthRequirement enum class
    
    * use auth requirement while constructing authorizer
    
    * pass map of client to privileges
    
    * remove NamespaceMatcher
    
    * better naming
    
    * few more renames

---
+++ timelock-impl/src/test/java/com/palantir/atlasdb/timelock/auth/impl/PrivilegeBasedNamespaceLockerTest.java
@@ -0,0 +1,48 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.timelock.auth.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+import com.palantir.atlasdb.timelock.auth.api.Privileges;
+import com.palantir.lock.TimelockNamespace;
+import com.palantir.atlasdb.timelock.auth.api.Client;
+
+public class PrivilegeBasedNamespaceLockerTest {
+    private static final Client CLIENT = Client.create("user");
+
+    private static final TimelockNamespace NAMESPACE_1 = TimelockNamespace.of("namespace_1");
+    private static final TimelockNamespace NAMESPACE_2 = TimelockNamespace.of("namespace_2");
+
+    @Test
+    public void shouldRequireAuthIfPrivilegeIsSpecified() {
+        Map<Client, Privileges> privilege = ImmutableMap.of(CLIENT, createPrivilegesFor(NAMESPACE_1));
+        NamespaceLocker namespaceLocker = NamespaceLocker.deriveFromPrivileges(privilege);
+
+        assertThat(namespaceLocker.isLocked(NAMESPACE_1)).isTrue();
+        assertThat(namespaceLocker.isLocked(NAMESPACE_2)).isFalse();
+    }
+
+    private Privileges createPrivilegesFor(TimelockNamespace namespace) {
+        return otherNamespace -> otherNamespace.equals(namespace);
+    }
+}
