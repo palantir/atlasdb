cdf57b48f7fbefd549aed14841ca2d2ae92c7e10
Gokcan Ozakdag <gozakdag@palantir.com>
2019-05-30 15:39:02 +0100

    add caching to authorizer

--- timelock-impl/src/main/java/com/palantir/atlasdb/timelock/auth/impl/SimpleAuthorizer.java
+++ timelock-impl/src/main/java/com/palantir/atlasdb/timelock/auth/impl/SimpleAuthorizer.java
@@ -18,6 +18,10 @@ package com.palantir.atlasdb.timelock.auth.impl;
 
 import java.util.Map;
 
+import org.immutables.value.Value;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 import com.palantir.atlasdb.timelock.auth.api.ClientId;
@@ -29,11 +33,15 @@ import com.palantir.atlasdb.timelock.auth.api.AuthenticatedClient;
 public class SimpleAuthorizer implements Authorizer {
     private final Map<ClientId, Privileges> privileges;
     private final NamespaceLocker namespaceLocker;
+    private final LoadingCache<CacheKey, Boolean> cache;
 
     @VisibleForTesting
     SimpleAuthorizer(Map<ClientId, Privileges> privileges, NamespaceLocker namespaceLocker) {
         this.privileges = privileges;
         this.namespaceLocker = namespaceLocker;
+        this.cache = Caffeine.newBuilder()
+                .maximumSize(1000)
+                .build(key -> isAuthorizedInternal(key.authenticatedClient(), key.timelockNamespace()));
     }
 
     public static Authorizer of(Map<ClientId, Privileges> privileges, AuthRequirement authRequirement) {
@@ -45,6 +53,10 @@ public class SimpleAuthorizer implements Authorizer {
 
     @Override
     public boolean isAuthorized(AuthenticatedClient authenticatedClient, TimelockNamespace namespace) {
+        return cache.get(ImmutableCacheKey.of(authenticatedClient, namespace));
+    }
+
+    private boolean isAuthorizedInternal(AuthenticatedClient authenticatedClient, TimelockNamespace namespace) {
         return !namespaceLocker.isLocked(namespace)
                 || privileges.getOrDefault(authenticatedClient.id(), Privileges.EMPTY).hasPrivilege(namespace);
     }
@@ -62,5 +74,15 @@ public class SimpleAuthorizer implements Authorizer {
             default:
                 throw new IllegalStateException("unknown auth requirement option " + authRequirement);
         }
+    }
+
+
+    @Value.Immutable
+    interface CacheKey {
+        @Value.Parameter
+        AuthenticatedClient authenticatedClient();
+
+        @Value.Parameter
+        TimelockNamespace timelockNamespace();
     }
 }
