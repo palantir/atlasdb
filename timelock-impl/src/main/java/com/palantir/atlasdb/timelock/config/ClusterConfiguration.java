c321671d37da66ac92674db4faa40eb7b6511cf1
Jeremy Kong <jeremykong@hotmail.com>
2019-07-05 15:24:50 +0100

    Illegal forbidden config

--- timelock-impl/src/main/java/com/palantir/atlasdb/timelock/config/ClusterConfiguration.java
+++ timelock-impl/src/main/java/com/palantir/atlasdb/timelock/config/ClusterConfiguration.java
@@ -36,11 +36,33 @@ public abstract class ClusterConfiguration {
     @Size(min = 1)
     public abstract Set<String> servers();
 
+    @Value.Default
+    public boolean enableNonstandardAndPossiblyDangerousTopology() {
+        return false;
+    }
+
     @Value.Check
     protected void check() {
         Preconditions.checkArgument(servers().contains(localServer()),
                 "The localServer '%s' must be included in the server entries %s.", localServer(), servers());
         checkServersAreWellFormed();
+        if (!enableNonstandardAndPossiblyDangerousTopology()) {
+            checkTopologyOffersHighAvailability();
+        }
+    }
+
+    protected void checkTopologyOffersHighAvailability() {
+        Preconditions.checkArgument(servers().size() >= 3,
+                "This TimeLock cluster is set up to use %s (<3) servers, which is not a standard configuration!"
+                        + " With fewer than three servers, your service will not have high availability. In the"
+                        + " event a node goes down, timelock will become unresponsive, meaning that ALL your AtlasDB"
+                        + " clients will become unable to perform transactions. Furthermore your TimeLock cluster has"
+                        + " NO resilience to failures of the underlying storage layer; if ANY node loses its logs,"
+                        + " the timestamp information may be IRRECOVERABLY COMPROMISED, meaning that your AtlasDB"
+                        + " deployments may become completely unusable."
+                        + " If you know what you are doing and you want to run in this configuration, you must set"
+                        + " 'enableNonstandardAndPossiblyDangerousTopology' to true.",
+                servers().size());
     }
 
     private void checkServersAreWellFormed() {
--- timelock-impl/src/test/java/com/palantir/atlasdb/timelock/config/ClusterConfigurationTest.java
+++ timelock-impl/src/test/java/com/palantir/atlasdb/timelock/config/ClusterConfigurationTest.java
@@ -15,13 +15,20 @@
  */
 package com.palantir.atlasdb.timelock.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
 public class ClusterConfigurationTest {
     private static final String ADDRESS_1 = "localhost:1";
     private static final String ADDRESS_2 = "localhost:2";
+    private static final String ADDRESS_3 = "localhost:3";
+    private static final String ADDRESS_4 = "localhost:4";
 
     @Test
     public void shouldThrowIfLocalServerNotSpecified() {
@@ -76,6 +83,43 @@ public class ClusterConfigurationTest {
         assertAddressNotValid("localhost:foo");
     }
 
+    @Test
+    public void shouldThrowIfConfigurationContainsOneServerAndDisclaimerNotEnabled() {
+        assertConfigurationWithFixedNumberOfServersIsInvalidWithoutDisclaimer(1);
+    }
+
+    @Test
+    public void shouldThrowIfConfigurationContainsTwoServersAndDisclaimerNotEnabled() {
+        assertConfigurationWithFixedNumberOfServersIsInvalidWithoutDisclaimer(2);
+    }
+
+    @Test
+    public void shouldPermitConfigurationContainingThreeServersWithoutDisclaimer() {
+        assertConfigurationWithFixedNumberOfServersIsValidWithoutDisclaimer(3);
+    }
+
+    @Test
+    public void shouldPermitConfigurationContainingMoreThanThreeServersWithoutDisclaimer() {
+        assertConfigurationWithFixedNumberOfServersIsValidWithoutDisclaimer(4);
+        assertConfigurationWithFixedNumberOfServersIsValidWithoutDisclaimer(77);
+    }
+
+    @Test
+    public void shouldPermitConfigurationContainingOneServerIfDisclaimerSpecified() {
+        assertConfigurationWithFixedNumberOfServersIsValidWithDisclaimer(1);
+    }
+
+    @Test
+    public void shouldPermitConfigurationContainingTwoServersIfDisclaimerSpecified() {
+        assertConfigurationWithFixedNumberOfServersIsValidWithDisclaimer(2);
+    }
+
+    @Test
+    public void shouldPermitConfigurationContainingThreeOrMoreServersEvenIfDisclaimerSpecified() {
+        assertConfigurationWithFixedNumberOfServersIsValidWithDisclaimer(3);
+        assertConfigurationWithFixedNumberOfServersIsValidWithDisclaimer(314);
+    }
+
     private void assertAddressValid(String address) {
         ClusterConfiguration.checkHostPortString(address);
     }
@@ -83,5 +127,33 @@ public class ClusterConfigurationTest {
     private void assertAddressNotValid(String address) {
         assertThatThrownBy(() -> ClusterConfiguration.checkHostPortString(address))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void assertConfigurationWithFixedNumberOfServersIsValidWithoutDisclaimer(int numServers) {
+        ClusterConfiguration configuration = createClusterConfiguration(numServers, false);
+        assertThat(configuration).isNotNull();
+    }
+
+    private void assertConfigurationWithFixedNumberOfServersIsValidWithDisclaimer(int numServers) {
+        ClusterConfiguration configuration = createClusterConfiguration(numServers, true);
+        assertThat(configuration).isNotNull();
+    }
+
+    private ClusterConfiguration createClusterConfiguration(int numServers, boolean disclaimer) {
+        List<String> servers = IntStream.range(0, numServers)
+                .mapToObj(index -> "server" + index + ":42")
+                .collect(Collectors.toList());
+        return ImmutableClusterConfiguration.builder()
+                .addAllServers(servers)
+                .localServer(servers.get(0))
+                .enableNonstandardAndPossiblyDangerousTopology(disclaimer)
+                .build();
+    }
+
+    private void assertConfigurationWithFixedNumberOfServersIsInvalidWithoutDisclaimer(int numServers) {
+        assertThatThrownBy(() -> createClusterConfiguration(numServers, false))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("not a standard configuration!")
+                .hasMessageContaining("IRRECOVERABLY COMPROMISED");
     }
 }
