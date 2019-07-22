86cda05fe8254f82f55ac7c66ee6c46498ad9558
Jelena Cvitanovic <jelenac@palantir.com>
2019-07-22 17:45:04 +0100

    Simple test green.

--- atlasdb-cassandra/src/test/java/com/palantir/atlasdb/migration/LiveMigratorTest.java
+++ atlasdb-cassandra/src/test/java/com/palantir/atlasdb/migration/LiveMigratorTest.java
@@ -17,47 +17,85 @@
 package com.palantir.atlasdb.migration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.palantir.atlasdb.AtlasDbConstants;
 import com.palantir.atlasdb.encoding.PtBytes;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.keyvalue.impl.TestResourceManager;
+import com.palantir.atlasdb.transaction.api.TransactionManager;
 import com.palantir.atlasdb.transaction.impl.TransactionTestSetup;
 
 public class LiveMigratorTest extends TransactionTestSetup {
     private static final TableReference OLD_TABLE_REF = TableReference.createFromFullyQualifiedName("old.table");
     private static final TableReference NEW_TABLE_REF = TableReference.createFromFullyQualifiedName("new.table");
+    private static final Cell CELL = createCell("rowName", "columnName");
 
     @ClassRule
     public static final TestResourceManager TRM = TestResourceManager.inMemory();
+
+    private KeyValueService kvs;
+    private TransactionManager transactionManager;
+
+    private final ProgressCheckPoint checkPoint = mock(ProgressCheckPoint.class);
+
+    private LiveMigrator liveMigrator;
 
     public LiveMigratorTest() {
         super(TRM, TRM);
     }
 
-    @Test
-    public void test() {
-        System.out.println("blablabla");
-        KeyValueService kvs = TRM.getDefaultKvs();
-
+    @Before
+    public void before() {
+        kvs = TRM.getDefaultKvs();
         kvs.createTable(OLD_TABLE_REF, AtlasDbConstants.GENERIC_TABLE_METADATA);
-
-        Cell cell = createCell("rowName", "columnName");
-        kvs.put(OLD_TABLE_REF,
-                ImmutableMap.of(cell, PtBytes.toBytes("value")),
-                10L);
-
-        assertThat(kvs.get(OLD_TABLE_REF, ImmutableMap.of(cell, 11L)).get(cell).getContents())
-                .containsExactly(PtBytes.toBytes("value"));
+        kvs.createTable(NEW_TABLE_REF, AtlasDbConstants.GENERIC_TABLE_METADATA);
+        transactionManager = getManager();
+        liveMigrator = new LiveMigrator(transactionManager, OLD_TABLE_REF, NEW_TABLE_REF, checkPoint);
     }
 
-    Cell createCell(String rowName, String columnName) {
+    @Test
+    public void testMigrationRuns() {
+        transactionManager.runTaskWithRetry(transaction -> {
+            transaction.put(OLD_TABLE_REF,
+                    ImmutableMap.of(CELL, PtBytes.toBytes("value")));
+
+            return null;
+        });
+
+
+        byte[] value = transactionManager.runTaskWithRetry(
+                transaction -> transaction.get(OLD_TABLE_REF, ImmutableSet.of(CELL)).get(CELL));
+
+        assertThat(value)
+                .containsExactly(PtBytes.toBytes("value"));
+
+        when(checkPoint.getNextStartRow())
+                .thenReturn(Optional.of(PtBytes.EMPTY_BYTE_ARRAY))
+                .thenReturn(Optional.empty());
+
+        liveMigrator.startMigration();
+
+        byte[] valueInNewTable = transactionManager.runTaskWithRetry(
+                transaction -> transaction.get(NEW_TABLE_REF, ImmutableSet.of(CELL)).get(CELL));
+
+        assertThat(valueInNewTable)
+                .containsExactly(PtBytes.toBytes("value"));
+
+    }
+
+    private static Cell createCell(String rowName, String columnName) {
         return Cell.create(PtBytes.toBytes(rowName), PtBytes.toBytes(columnName));
     }
 }
