c2f67fbf20e148931ebbd6b8185e4af83f2fbb2a
jelenac <cvitanovic.jelena@gmail.com>
2019-07-30 10:56:16 +0100

    [Hack19]Coordination tests (#4151)
    * More refactor for testing.
    
    * Suppress
    
    * testTransformCallsCoordinationServiceWithCorrectArguments in MigrationStateTransformerTest
    
    * More tests
    
    * Complete tests for MigrationStateTransformerTest
    
    * Checkstyle
    
    * Use constants
    
    * Better name
    
    * Tests for get migration state
    
    * Migration operation test.

--- atlasdb-table-migration/src/test/java/com/palantir/atlasdb/migration/MigrationCoordinationServiceImplTest.java
+++ atlasdb-table-migration/src/test/java/com/palantir/atlasdb/migration/MigrationCoordinationServiceImplTest.java
@@ -16,11 +16,118 @@
 
 package com.palantir.atlasdb.migration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static com.palantir.atlasdb.migration.MigrationCoordinationServiceImpl.DEFAULT_TABLE_MIGRATION_STATE;
+
+import java.util.Optional;
+
 import org.junit.Test;
 
+import com.palantir.atlasdb.coordination.CoordinationServiceImpl;
+import com.palantir.atlasdb.coordination.ValueAndBound;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+
+@SuppressWarnings("unchecked") // Mocks of generic types
 public class MigrationCoordinationServiceImplTest {
+    private static final TableReference TABLE = TableReference.fromString("table.some");
+    private static final TableReference TARGET_TABLE = TableReference.fromString("table.target");
+    private static final TableReference OTHER_TABLE = TableReference.fromString("table.other");
+    private static final MigrationState STATE = MigrationState.WRITE_FIRST_ONLY;
+    private static final MigrationState OTHER_STATE = MigrationState.WRITE_BOTH_READ_FIRST;
+    private static final TableMigrationStateMap STATE_MAP = TableMigrationStateMap.builder()
+            .putTableMigrationStateMap(TABLE, TableMigrationState.builder()
+                    .migrationsState(STATE)
+                    .build())
+            .putTableMigrationStateMap(OTHER_TABLE, TableMigrationState.builder()
+                    .migrationsState(OTHER_STATE)
+                    .build())
+            .build();
+    private static final long TIMESTAMP = 10L;
+    private static final boolean TRANSFORMATION_RESULT = true;
+
+    private final CoordinationServiceImpl<TableMigrationStateMap> coordinationService =
+            mock(CoordinationServiceImpl.class);
+    private final MigrationCoordinationStateTransformer stateTransformer =
+            mock(MigrationCoordinationStateTransformer.class);
+
+    private final MigrationCoordinationService migrationCoordinationService =
+            new MigrationCoordinationServiceImpl(coordinationService, stateTransformer);
+
     @Test
-    public void test() {
-        System.out.println("blah");
+    public void testStartMigrationCallsStateTransformerWithCorrectArgs() {
+        when(stateTransformer.transformMigrationStateForTable(
+                TABLE,
+                Optional.of(TARGET_TABLE),
+                MigrationState.WRITE_BOTH_READ_FIRST)
+        ).thenReturn(TRANSFORMATION_RESULT);
+
+        assertThat(migrationCoordinationService.startMigration(TABLE, TARGET_TABLE))
+                .isEqualTo(TRANSFORMATION_RESULT);
+    }
+
+    @Test
+    public void testEndMigrationCallsStateTransformerWithCorrectArgs() {
+        when(stateTransformer.transformMigrationStateForTable(
+                TABLE,
+                Optional.empty(),
+                MigrationState.WRITE_BOTH_READ_SECOND)
+        ).thenReturn(TRANSFORMATION_RESULT);
+
+        assertThat(migrationCoordinationService.endMigration(TABLE))
+                .isEqualTo(TRANSFORMATION_RESULT);
+    }
+
+    @Test
+    public void testEndDualWriteCallsStateTransformerWithCorrectArgs() {
+        when(stateTransformer.transformMigrationStateForTable(
+                TABLE,
+                Optional.empty(),
+                MigrationState.WRITE_SECOND_READ_SECOND)
+        ).thenReturn(TRANSFORMATION_RESULT);
+
+        assertThat(migrationCoordinationService.endDualWrite(TABLE))
+                .isEqualTo(TRANSFORMATION_RESULT);
+    }
+
+    @Test
+    public void testGetMigrationStateReturnsDefaultStateWhenStateTableNotInitialized() {
+        ValueAndBound<TableMigrationStateMap> currentValue = ValueAndBound.of(Optional.empty(), TIMESTAMP);
+
+        when(coordinationService.getValueForTimestamp(TIMESTAMP)).thenReturn(Optional.of(currentValue));
+
+        assertThat(migrationCoordinationService.getMigrationState(TABLE, TIMESTAMP))
+                .isEqualTo(DEFAULT_TABLE_MIGRATION_STATE);
+    }
+
+    @Test
+    public void testGetMigrationStateReturnsDefaultStateWhenStateTableDoestHaveTheRequestedTableState() {
+        TableMigrationStateMap stateMapWithoutRequestedTable = TableMigrationStateMap.builder()
+                .putTableMigrationStateMap(OTHER_TABLE, TableMigrationState.builder()
+                        .migrationsState(OTHER_STATE)
+                        .build())
+                .build();
+        ValueAndBound<TableMigrationStateMap> currentValue =
+                ValueAndBound.of(Optional.of(stateMapWithoutRequestedTable), TIMESTAMP);
+
+        when(coordinationService.getValueForTimestamp(TIMESTAMP)).thenReturn(Optional.of(currentValue));
+
+        assertThat(migrationCoordinationService.getMigrationState(TABLE, TIMESTAMP))
+                .isEqualTo(DEFAULT_TABLE_MIGRATION_STATE);
+    }
+
+    @Test
+    public void testGetMigrationStateReturnsExpectedValueFromCoordinationService() {
+        ValueAndBound<TableMigrationStateMap> currentValue = ValueAndBound.of(STATE_MAP, TIMESTAMP);
+
+        when(coordinationService.getValueForTimestamp(TIMESTAMP)).thenReturn(Optional.of(currentValue));
+
+        TableMigrationState expected = TableMigrationState.of(STATE);
+
+        assertThat(migrationCoordinationService.getMigrationState(TABLE, TIMESTAMP))
+                .isEqualTo(expected);
     }
 }
+
---
+++ atlasdb-table-migration/src/test/java/com/palantir/atlasdb/migration/MigrationCoordinationStateTransformerTest.java
@@ -0,0 +1,203 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.migration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static com.palantir.atlasdb.migration.MigrationCoordinationStateTransformer.EMPTY_TABLE_MIGRATION_STATE_MAP;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import com.google.common.collect.ImmutableList;
+import com.palantir.atlasdb.coordination.CoordinationServiceImpl;
+import com.palantir.atlasdb.coordination.ValueAndBound;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.keyvalue.impl.CheckAndSetResult;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
+
+@SuppressWarnings("unchecked") // Mocks of generic types
+public class MigrationCoordinationStateTransformerTest {
+    private static final TableReference TABLE = TableReference.fromString("table.some");
+    private static final TableReference OTHER_TABLE = TableReference.fromString("table.other");
+    private static final MigrationState STATE = MigrationState.WRITE_FIRST_ONLY;
+    private static final MigrationState TARGET_STATE = MigrationState.WRITE_BOTH_READ_FIRST;
+
+    private static final Optional<TableReference> MAYBE_TARGET_TABLE =
+            Optional.of(TableReference.fromString("table.target"));
+
+    private static final TableMigrationStateMap STATE_MAP = TableMigrationStateMap.builder()
+            .putTableMigrationStateMap(TABLE, TableMigrationState.builder()
+                    .migrationsState(STATE)
+                    .build())
+            .putTableMigrationStateMap(OTHER_TABLE, TableMigrationState.builder()
+                    .migrationsState(STATE)
+                    .build())
+            .build();
+    private static final TableMigrationStateMap NEW_STATE_MAP = TableMigrationStateMap.builder()
+            .putTableMigrationStateMap(TABLE, TableMigrationState.builder()
+                    .migrationsState(TARGET_STATE)
+                    .targetTable(TableReference.fromString("table.targetTable"))
+                    .build())
+            .putTableMigrationStateMap(OTHER_TABLE, TableMigrationState.builder()
+                    .migrationsState(STATE)
+                    .build())
+            .build();
+    private static final long TIMESTAMP = 10L;
+    private static final boolean SUCCESSFUL = true;
+    private static final boolean FAILED = false;
+
+    private final CoordinationServiceImpl<TableMigrationStateMap> coordinationService =
+            mock(CoordinationServiceImpl.class);
+    private final MigrationStateTransitioner stateTransitioner = mock(MigrationStateTransitioner.class);
+    private final MigrationCoordinationStateTransformer stateTransformer =
+            new MigrationCoordinationStateTransformer(stateTransitioner, coordinationService);
+
+    @Test
+    public void testTransformCallsCoordinationServiceWithCorrectArguments() {
+        ValueAndBound<TableMigrationStateMap> currentValue = ValueAndBound.of(STATE_MAP, TIMESTAMP);
+        ValueAndBound<TableMigrationStateMap> transformedValue = ValueAndBound.of(NEW_STATE_MAP, TIMESTAMP);
+
+        CheckAndSetResult<ValueAndBound<TableMigrationStateMap>> result =
+                CheckAndSetResult.of(SUCCESSFUL, ImmutableList.of(transformedValue));
+
+        when(coordinationService.tryTransformCurrentValue(any()))
+                .thenReturn(result);
+
+        ArgumentCaptor<Function> functionArgumentCaptor = ArgumentCaptor.forClass(Function.class);
+
+        stateTransformer.transformMigrationStateForTable(TABLE, MAYBE_TARGET_TABLE, TARGET_STATE);
+        verify(coordinationService).tryTransformCurrentValue(functionArgumentCaptor.capture());
+
+        functionArgumentCaptor.getValue().apply(currentValue);
+        verify(stateTransitioner).updateTableMigrationStateForTable(STATE_MAP, TABLE, MAYBE_TARGET_TABLE, TARGET_STATE);
+    }
+
+    @Test
+    public void testTransformCreatesInitialStateTable() {
+        ValueAndBound<TableMigrationStateMap> currentValue = ValueAndBound.of(Optional.empty(), TIMESTAMP);
+        ValueAndBound<TableMigrationStateMap> transformedValue = ValueAndBound.of(NEW_STATE_MAP, TIMESTAMP);
+
+        CheckAndSetResult<ValueAndBound<TableMigrationStateMap>> result =
+                CheckAndSetResult.of(SUCCESSFUL, ImmutableList.of(transformedValue));
+
+        when(coordinationService.tryTransformCurrentValue(any()))
+                .thenReturn(result);
+
+        ArgumentCaptor<Function> functionArgumentCaptor = ArgumentCaptor.forClass(Function.class);
+
+        stateTransformer.transformMigrationStateForTable(TABLE, MAYBE_TARGET_TABLE, TARGET_STATE);
+        verify(coordinationService).tryTransformCurrentValue(functionArgumentCaptor.capture());
+
+        functionArgumentCaptor.getValue().apply(currentValue);
+        verify(stateTransitioner)
+                .updateTableMigrationStateForTable(
+                        EMPTY_TABLE_MIGRATION_STATE_MAP,
+                        TABLE,
+                        MAYBE_TARGET_TABLE,
+                        TARGET_STATE);
+    }
+
+    @Test
+    public void testTransformReturnsTrueWhenTransformationSucceededAndFinalResultIsWhatWeWanted() {
+        ValueAndBound<TableMigrationStateMap> transformedValue = ValueAndBound.of(NEW_STATE_MAP, TIMESTAMP);
+
+        CheckAndSetResult<ValueAndBound<TableMigrationStateMap>> result =
+                CheckAndSetResult.of(SUCCESSFUL, ImmutableList.of(transformedValue));
+
+        when(coordinationService.tryTransformCurrentValue(any()))
+                .thenReturn(result);
+
+        assertThat(stateTransformer.transformMigrationStateForTable(TABLE, MAYBE_TARGET_TABLE, TARGET_STATE))
+                .isEqualTo(SUCCESSFUL);
+    }
+
+    @Test
+    public void testTransformReturnsFalseWhenTransformationSucceededButFinalStateIsNotWhatWeWanted() {
+        ValueAndBound<TableMigrationStateMap> transformedValue = ValueAndBound.of(STATE_MAP, TIMESTAMP);
+
+        CheckAndSetResult<ValueAndBound<TableMigrationStateMap>> result =
+                CheckAndSetResult.of(SUCCESSFUL, ImmutableList.of(transformedValue));
+
+        when(coordinationService.tryTransformCurrentValue(any()))
+                .thenReturn(result);
+
+        assertThat(stateTransformer.transformMigrationStateForTable(TABLE, MAYBE_TARGET_TABLE, TARGET_STATE))
+                .isEqualTo(FAILED);
+    }
+
+    @Test
+    public void testTransformReturnsTrueWhenTransformationFailedButFinalStateIsWhatWeWanted() {
+        ValueAndBound<TableMigrationStateMap> transformedValue = ValueAndBound.of(NEW_STATE_MAP, TIMESTAMP);
+
+        CheckAndSetResult<ValueAndBound<TableMigrationStateMap>> result =
+                CheckAndSetResult.of(FAILED, ImmutableList.of(transformedValue));
+
+        when(coordinationService.tryTransformCurrentValue(any()))
+                .thenReturn(result);
+
+        assertThat(stateTransformer.transformMigrationStateForTable(TABLE, MAYBE_TARGET_TABLE, TARGET_STATE))
+                .isEqualTo(SUCCESSFUL);
+    }
+
+    @Test
+    public void testTransformThrowsWhenTheResultHasNoTableStateMap() {
+        ValueAndBound<TableMigrationStateMap> transformedValue = ValueAndBound.of(Optional.empty(), TIMESTAMP);
+
+        CheckAndSetResult<ValueAndBound<TableMigrationStateMap>> result =
+                CheckAndSetResult.of(SUCCESSFUL, ImmutableList.of(transformedValue));
+
+        when(coordinationService.tryTransformCurrentValue(any()))
+                .thenReturn(result);
+
+        assertThatExceptionOfType(SafeIllegalStateException.class)
+                .isThrownBy(() ->
+                        stateTransformer.transformMigrationStateForTable(TABLE, MAYBE_TARGET_TABLE, TARGET_STATE));
+
+    }
+
+    @Test
+    public void testTransformThrowsWhenTheResultHasNoTableStateForUpdatedTable() {
+        TableMigrationStateMap stateMapWithoutUpdatedTable = TableMigrationStateMap.builder()
+                .putTableMigrationStateMap(OTHER_TABLE, TableMigrationState.builder()
+                        .migrationsState(STATE)
+                        .build())
+                .build();
+
+        ValueAndBound<TableMigrationStateMap> transformedValue =
+                ValueAndBound.of(stateMapWithoutUpdatedTable, TIMESTAMP);
+
+        CheckAndSetResult<ValueAndBound<TableMigrationStateMap>> result =
+                CheckAndSetResult.of(SUCCESSFUL, ImmutableList.of(transformedValue));
+
+        when(coordinationService.tryTransformCurrentValue(any()))
+                .thenReturn(result);
+
+        assertThatExceptionOfType(SafeIllegalStateException.class)
+                .isThrownBy(() ->
+                        stateTransformer.transformMigrationStateForTable(TABLE, MAYBE_TARGET_TABLE, TARGET_STATE));
+
+    }
+}
