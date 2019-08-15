97556eddeebec8e2047aa189cc8f05322f8621c2
Felix de Souza <fdesouza@palantir.com>
2019-08-15 17:21:22 +0100

    Handle empty optional return types in the server

--- timelock-server/src/main/java/com/palantir/atlasdb/timelock/TimeLockServerLauncher.java
+++ timelock-server/src/main/java/com/palantir/atlasdb/timelock/TimeLockServerLauncher.java
@@ -18,6 +18,10 @@ package com.palantir.atlasdb.timelock;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.SharedMetricRegistries;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -31,6 +35,7 @@ import com.palantir.timelock.paxos.TimeLockAgent;
 import com.palantir.tritium.metrics.registry.DefaultTaggedMetricRegistry;
 
 import io.dropwizard.Application;
+import io.dropwizard.jersey.optional.EmptyOptionalException;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -58,6 +63,7 @@ public class TimeLockServerLauncher extends Application<CombinedTimeLockServerCo
                 .registerModule(new Jdk8Module())
                 .registerModule(new JavaTimeModule());
         environment.jersey().register(ConjureJerseyFeature.INSTANCE);
+        environment.jersey().register(new EmptyOptionalTo204ExceptionMapper());
 
         MetricsManager metricsManager = MetricsManagers.of(environment.metrics(), new DefaultTaggedMetricRegistry());
         Consumer<Object> registrar = component -> environment.jersey().register(component);
@@ -69,5 +75,13 @@ public class TimeLockServerLauncher extends Application<CombinedTimeLockServerCo
                 CombinedTimeLockServerConfiguration.threadPoolSize(),
                 CombinedTimeLockServerConfiguration.blockingTimeoutMs(),
                 registrar);
+    }
+
+    @Provider
+    private final static class EmptyOptionalTo204ExceptionMapper implements ExceptionMapper<EmptyOptionalException> {
+        @Override
+        public Response toResponse(EmptyOptionalException exception) {
+            return Response.noContent().build();
+        }
     }
 }
