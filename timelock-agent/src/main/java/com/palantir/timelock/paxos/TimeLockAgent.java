fb1dcae5ad39d13f01b919f35aa6942277d71ba7
Jeremy Kong <jeremykong@hotmail.com>
2019-09-03 15:19:23 +0100

    Retry targeter

--- timelock-agent/src/main/java/com/palantir/timelock/paxos/TimeLockAgent.java
+++ timelock-agent/src/main/java/com/palantir/timelock/paxos/TimeLockAgent.java
@@ -200,7 +200,7 @@ public class TimeLockAgent {
 
     private void registerExceptionMappers() {
         registrar.accept(new BlockingTimeoutExceptionMapper());
-        registrar.accept(new NotCurrentLeaderExceptionMapper());
+        registrar.accept(new NotCurrentLeaderExceptionMapper(redirectRetryTargeter));
         registrar.accept(new TooManyRequestsExceptionMapper());
     }
 
