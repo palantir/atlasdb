1a56f6765a2e1a2636b11cf3d7c285b8a0f2401a
Felix de Souza <fdesouza@palantir.com>
2019-09-12 14:32:24 +0100

    [Timelock Partitioning] Part 26: Timestamp paxos test suite. (#4186)
    * Add dependency on parameterized suites.
    
    * Parameter injector class that allows us to run via the suite and via the idea.
    
    * batch paxos configs
    
    * PaxosSuite which runs batched and non batched paxos.
    
    * More intuitive logging for the server.
    
    * Introduce suite test set.
    
    * Separate test library and suite implementations.
    
    * Better logging/debug experience.
    
    * Checkstyle.
    
    * better names
    
    * new line.
    
    * Clearer parameter injection usage.
    
    * inline previous impl
    
    * Checkstyle.
    
    * fix up the build.

---
+++ timelock-server/src/testCommon/java/com/palantir/atlasdb/timelock/util/ParameterInjector.java
@@ -0,0 +1,60 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.timelock.util;
+
+import java.util.function.Supplier;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import com.github.peterwippermann.junit4.parameterizedsuite.ParameterContext;
+import com.google.common.collect.ImmutableSet;
+
+public final class ParameterInjector<T extends TestRule> implements TestRule {
+
+    private final Supplier<T> factory;
+    private boolean outsideSuite;
+    private T currentInstance = null;
+
+    private ParameterInjector(Supplier<T> factory) {
+        this.factory = factory;
+    }
+
+    public static <T extends TestRule> ParameterInjector<T> withFallBackConfiguration(Supplier<T> factory) {
+        return new ParameterInjector<>(factory);
+    }
+
+    public Iterable<T> getParameter() {
+        if (ParameterContext.isParameterSet()) {
+            return ImmutableSet.of(ParameterContext.getParameter());
+        } else {
+            outsideSuite = true;
+            currentInstance = factory.get();
+            return ImmutableSet.of(currentInstance);
+        }
+    }
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        if (outsideSuite) {
+            return currentInstance.apply(base, description);
+        } else {
+            return base;
+        }
+    }
+}
