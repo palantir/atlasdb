2cc65835dfaf9f10c95317803d4d38f017c8e657
Jeremy Kong <jeremykong@hotmail.com>
2019-09-30 19:22:15 +0100

    Revert "PollingRefreshable transforms"
    This reverts commit 53bad854fe3a9b6f346959c9e50ad51d924f7213.

--- atlasdb-feign/src/main/java/com/palantir/atlasdb/http/AtlasDbFeignTargetFactory.java
+++ atlasdb-feign/src/main/java/com/palantir/atlasdb/http/AtlasDbFeignTargetFactory.java
@@ -147,7 +147,7 @@ public final class AtlasDbFeignTargetFactory implements TargetFactory {
             Class<T> type,
             String userAgent,
             boolean limitPayload) {
-        PollingRefreshable<ServerListConfig, ServerListConfig> configPollingRefreshable =
+        PollingRefreshable<ServerListConfig> configPollingRefreshable =
                 PollingRefreshable.create(serverListConfigSupplier);
         return Reflection.newProxy(
                 type,
