e17e2ca388d11db1ba213856f383e657af3ffcd1
jakubk <jakubk@palantir.com>
2019-10-22 14:11:08 +0100

    Fixup

--- atlasdb-api/src/main/java/com/palantir/atlasdb/metrics/Timed.java
+++ atlasdb-api/src/main/java/com/palantir/atlasdb/metrics/Timed.java
@@ -22,7 +22,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD})
+@Target(ElementType.METHOD)
 public @interface Timed {
 }
 
