79c791c0660ab25031aab0c9a1f77794b5832b7b
Jakub Kozlowski <jkozlowski@users.noreply.github.com>
2019-10-25 14:37:16 +0100

    Fix CassandraClient instrumentation. (#4348)
    * Fix CassandraClient instrumentation.
    * AutoDelegate destroys the instrumentation, because it overrides
      methods with default implementation, without copying the annotations.
    * Sending a fix for the specific case first, until I figure out
      a better solution.
    
    * Add generated changelog entries

--- atlasdb-api/src/main/java/com/palantir/atlasdb/metrics/Timed.java
+++ atlasdb-api/src/main/java/com/palantir/atlasdb/metrics/Timed.java
@@ -17,10 +17,12 @@
 package com.palantir.atlasdb.metrics;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Timed {
