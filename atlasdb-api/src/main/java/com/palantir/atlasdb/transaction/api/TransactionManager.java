709316ba30d0511a7d860e699efbbdb0abe578e4
Grgur Petric Maretic <gmaretic@palantir.com>
2019-11-07 12:18:17 +0000

    Modify TM interface for more context

--- atlasdb-api/src/main/java/com/palantir/atlasdb/transaction/api/TransactionManager.java
+++ atlasdb-api/src/main/java/com/palantir/atlasdb/transaction/api/TransactionManager.java
@@ -315,6 +315,11 @@ public interface TransactionManager extends AutoCloseable {
      */
     TimelockService getTimelockService();
 
+    // todo(gmaretic): implement
+    default KvsLockWatchingService getLockWatchingService() {
+        return null;
+    }
+
     /**
      * Returns the timestamp service used by this transaction manager.
      *
