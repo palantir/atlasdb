ada1203e629bdfd65cb9d0072233682481168123
Felix de Souza <fdesouza@palantir.com>
2019-11-14 01:41:29 +0000

    checkstyle again..

--- timelock-server/src/integTest/java/com/palantir/atlasdb/timelock/PaxosTimeLockServerIntegrationTest.java
+++ timelock-server/src/integTest/java/com/palantir/atlasdb/timelock/PaxosTimeLockServerIntegrationTest.java
@@ -75,8 +75,8 @@ public class PaxosTimeLockServerIntegrationTest {
     private static final TestableTimelockServer TIMELOCK =
             new TestableTimelockServer("https://localhost", TIMELOCK_SERVER_HOLDER);
 
-    private static NamespacedClients NAMESPACE_1;
-    private static NamespacedClients NAMESPACE_2;
+    private static NamespacedClients namespace1;
+    private static NamespacedClients namespace2;
 
     @ClassRule
     public static final RuleChain ruleChain = RuleChain.outerRule(TEMPORARY_FOLDER)
@@ -85,8 +85,8 @@ public class PaxosTimeLockServerIntegrationTest {
 
     @BeforeClass
     public static void waitForClusterToStabilize() {
-        NAMESPACE_1 = TIMELOCK.client(CLIENT_1);
-        NAMESPACE_2 = TIMELOCK.client(CLIENT_2);
+        namespace1 = TIMELOCK.client(CLIENT_1);
+        namespace2 = TIMELOCK.client(CLIENT_2);
         PingableLeader leader = TIMELOCK.pingableLeader();
         Awaitility.await()
                 .atMost(30, TimeUnit.SECONDS)
@@ -107,7 +107,7 @@ public class PaxosTimeLockServerIntegrationTest {
 
     @Test
     public void lockServiceShouldAllowUsToTakeOutLocks() throws InterruptedException {
-        LockService lockService = NAMESPACE_1.legacyLockService();
+        LockService lockService = namespace1.legacyLockService();
 
         LockRefreshToken token = lockService.lock(LOCK_CLIENT_NAME, com.palantir.lock.LockRequest.builder(LOCK_MAP)
                 .doNotBlock()
@@ -120,8 +120,8 @@ public class PaxosTimeLockServerIntegrationTest {
 
     @Test
     public void lockServiceShouldAllowUsToTakeOutSameLockInDifferentNamespaces() throws InterruptedException {
-        LockService lockService1 = NAMESPACE_1.legacyLockService();
-        LockService lockService2 = NAMESPACE_2.legacyLockService();
+        LockService lockService1 = namespace1.legacyLockService();
+        LockService lockService2 = namespace2.legacyLockService();
 
         LockRefreshToken token1 = lockService1.lock(LOCK_CLIENT_NAME, com.palantir.lock.LockRequest.builder(LOCK_MAP)
                 .doNotBlock()
@@ -139,8 +139,8 @@ public class PaxosTimeLockServerIntegrationTest {
 
     @Test
     public void lockServiceShouldNotAllowUsToRefreshLocksFromDifferentNamespaces() throws InterruptedException {
-        LockService lockService1 = NAMESPACE_1.legacyLockService();
-        LockService lockService2 = NAMESPACE_2.legacyLockService();
+        LockService lockService1 = namespace1.legacyLockService();
+        LockService lockService2 = namespace2.legacyLockService();
 
         com.palantir.lock.LockRequest request = com.palantir.lock.LockRequest.builder(LOCK_MAP)
                 .doNotBlock()
@@ -157,46 +157,46 @@ public class PaxosTimeLockServerIntegrationTest {
 
     @Test
     public void asyncLockServiceShouldAllowUsToTakeOutLocks() {
-        LockToken token = NAMESPACE_1.lock(newLockV2Request(LOCK_1)).getToken();
-        assertThat(NAMESPACE_1.unlock(token)).isTrue();
+        LockToken token = namespace1.lock(newLockV2Request(LOCK_1)).getToken();
+        assertThat(namespace1.unlock(token)).isTrue();
     }
 
     @Test
     public void asyncLockServiceShouldAllowUsToTakeOutSameLockInDifferentNamespaces() {
-        LockToken token1 = NAMESPACE_1.lock(newLockV2Request(LOCK_1)).getToken();
-        LockToken token2 = NAMESPACE_2.lock(newLockV2Request(LOCK_1)).getToken();
+        LockToken token1 = namespace1.lock(newLockV2Request(LOCK_1)).getToken();
+        LockToken token2 = namespace2.lock(newLockV2Request(LOCK_1)).getToken();
 
-        NAMESPACE_1.unlock(token1);
-        NAMESPACE_2.unlock(token2);
+        namespace1.unlock(token1);
+        namespace2.unlock(token2);
     }
 
     @Test
     public void asyncLockServiceShouldNotAllowUsToRefreshLocksFromDifferentNamespaces() {
-        LockToken token = NAMESPACE_1.lock(newLockV2Request(LOCK_1)).getToken();
+        LockToken token = namespace1.lock(newLockV2Request(LOCK_1)).getToken();
 
-        assertThat(NAMESPACE_1.refreshLockLease(token)).isTrue();
-        assertThat(NAMESPACE_2.refreshLockLease(token)).isFalse();
+        assertThat(namespace1.refreshLockLease(token)).isTrue();
+        assertThat(namespace2.refreshLockLease(token)).isFalse();
 
-        NAMESPACE_1.unlock(token);
+        namespace1.unlock(token);
     }
 
     @Test
     public void timestampServiceShouldGiveUsIncrementalTimestamps() {
-        long timestamp1 = NAMESPACE_1.getFreshTimestamp();
-        long timestamp2 = NAMESPACE_1.getFreshTimestamp();
+        long timestamp1 = namespace1.getFreshTimestamp();
+        long timestamp2 = namespace1.getFreshTimestamp();
 
         assertThat(timestamp1).isLessThan(timestamp2);
     }
 
     @Test
     public void timestampServiceShouldRespectDistinctClientsWhenIssuingTimestamps() {
-        long firstServiceFirstTimestamp = NAMESPACE_1.getFreshTimestamp();
-        long secondServiceFirstTimestamp = NAMESPACE_2.getFreshTimestamp();
+        long firstServiceFirstTimestamp = namespace1.getFreshTimestamp();
+        long secondServiceFirstTimestamp = namespace2.getFreshTimestamp();
 
-        getFortyTwoFreshTimestamps(NAMESPACE_1.timelockService());
+        getFortyTwoFreshTimestamps(namespace1.timelockService());
 
-        long firstServiceSecondTimestamp = NAMESPACE_1.getFreshTimestamp();
-        long secondServiceSecondTimestamp = NAMESPACE_2.getFreshTimestamp();
+        long firstServiceSecondTimestamp = namespace1.getFreshTimestamp();
+        long secondServiceSecondTimestamp = namespace2.getFreshTimestamp();
 
         assertThat(firstServiceSecondTimestamp - firstServiceFirstTimestamp).isGreaterThanOrEqualTo(FORTY_TWO);
         assertThat(secondServiceSecondTimestamp - secondServiceFirstTimestamp).isBetween(0L, (long) FORTY_TWO);
@@ -204,25 +204,25 @@ public class PaxosTimeLockServerIntegrationTest {
 
     @Test
     public void timestampServiceRespectsTimestampManagementService() {
-        long currentTimestampIncrementedByOneMillion = NAMESPACE_1.getFreshTimestamp() + ONE_MILLION;
-        NAMESPACE_1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByOneMillion);
-        assertThat(NAMESPACE_1.getFreshTimestamp())
+        long currentTimestampIncrementedByOneMillion = namespace1.getFreshTimestamp() + ONE_MILLION;
+        namespace1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByOneMillion);
+        assertThat(namespace1.getFreshTimestamp())
                 .isGreaterThan(currentTimestampIncrementedByOneMillion);
     }
 
     @Test
     public void timestampManagementServiceRespectsTimestampService() {
-        long currentTimestampIncrementedByOneMillion = NAMESPACE_1.getFreshTimestamp() + ONE_MILLION;
-        NAMESPACE_1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByOneMillion);
-        getFortyTwoFreshTimestamps(NAMESPACE_1.timelockService());
-        NAMESPACE_1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByOneMillion + 1);
-        assertThat(NAMESPACE_1.getFreshTimestamp())
+        long currentTimestampIncrementedByOneMillion = namespace1.getFreshTimestamp() + ONE_MILLION;
+        namespace1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByOneMillion);
+        getFortyTwoFreshTimestamps(namespace1.timelockService());
+        namespace1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByOneMillion + 1);
+        assertThat(namespace1.getFreshTimestamp())
                 .isGreaterThan(currentTimestampIncrementedByOneMillion + FORTY_TWO);
     }
 
     @Test
     public void lockServiceShouldDisallowGettingMinLockedInVersionId() {
-        LockService lockService = NAMESPACE_1.legacyLockService();
+        LockService lockService = namespace1.legacyLockService();
 
         // Catching any exception since this currently is an error deserialization exception
         // until we stop requiring http-remoting2 errors
@@ -238,23 +238,23 @@ public class PaxosTimeLockServerIntegrationTest {
 
     @Test
     public void fastForwardRespectsDistinctClients() {
-        TimestampManagementService anotherClientTimestampManagementService = NAMESPACE_2.timestampManagementService();
+        TimestampManagementService anotherClientTimestampManagementService = namespace2.timestampManagementService();
 
-        long currentTimestamp = NAMESPACE_1.getFreshTimestamp();
+        long currentTimestamp = namespace1.getFreshTimestamp();
         anotherClientTimestampManagementService.fastForwardTimestamp(currentTimestamp + ONE_MILLION);
-        assertThat(NAMESPACE_1.getFreshTimestamp())
+        assertThat(namespace1.getFreshTimestamp())
                 .isBetween(currentTimestamp + 1, currentTimestamp + ONE_MILLION - 1);
     }
 
     @Test
     public void fastForwardToThePastDoesNothing() {
-        long currentTimestamp = NAMESPACE_1.getFreshTimestamp();
+        long currentTimestamp = namespace1.getFreshTimestamp();
         long currentTimestampIncrementedByOneMillion = currentTimestamp + ONE_MILLION;
         long currentTimestampIncrementedByTwoMillion = currentTimestamp + TWO_MILLION;
 
-        NAMESPACE_1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByTwoMillion);
-        NAMESPACE_1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByOneMillion);
-        assertThat(NAMESPACE_1.getFreshTimestamp()).isGreaterThan(currentTimestampIncrementedByTwoMillion);
+        namespace1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByTwoMillion);
+        namespace1.timestampManagementService().fastForwardTimestamp(currentTimestampIncrementedByOneMillion);
+        assertThat(namespace1.getFreshTimestamp()).isGreaterThan(currentTimestampIncrementedByTwoMillion);
     }
 
     @Test
