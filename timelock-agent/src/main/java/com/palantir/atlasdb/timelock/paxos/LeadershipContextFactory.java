7083325df476326ff7c10132035f4ccbbb12a239
Felix de Souza <fdesouza@palantir.com>
2019-12-05 12:29:39 +0000

    Revert "[Timelock Partitioning] Part 44b: Ensure same instance of `Lead… (#4448)"
    This reverts commit 80c84b2391b35ea45645cd38d1753eda00dc7ebc.

--- timelock-agent/src/main/java/com/palantir/atlasdb/timelock/paxos/LeadershipContextFactory.java
+++ timelock-agent/src/main/java/com/palantir/atlasdb/timelock/paxos/LeadershipContextFactory.java
@@ -23,10 +23,13 @@ import org.immutables.value.Value;
 
 import com.palantir.atlasdb.timelock.paxos.LeadershipComponents.LeadershipContext;
 import com.palantir.atlasdb.timelock.paxos.NetworkClientFactories.Factory;
+import com.palantir.leader.LeaderElectionService;
+import com.palantir.leader.LeaderElectionServiceBuilder;
 import com.palantir.leader.PaxosLeadershipEventRecorder;
 import com.palantir.leader.PingableLeader;
 import com.palantir.paxos.LeaderPinger;
 import com.palantir.paxos.PaxosLearner;
+import com.palantir.paxos.PaxosProposer;
 import com.palantir.timelock.paxos.LeaderPingHealthCheck;
 
 @Value.Immutable
@@ -81,22 +84,13 @@ public abstract class LeadershipContextFactory implements
         return leaderPingHealthCheckFactory().create(this);
     }
 
-    @Value.Derived
-    LeaderElectionServiceFactory leaderElectionServiceFactory() {
-        return new LeaderElectionServiceFactory();
-    }
-
     @Override
     public LeadershipContext create(Client client) {
-        ClientAwareComponents clientAwareComponents = ImmutableClientAwareComponents.builder()
+        return ImmutableClientAwareComponents.builder()
                 .from(this)
                 .proxyClient(client)
-                .build();
-
-        return ImmutableLeadershipContext.builder()
-                .leadershipMetrics(clientAwareComponents.leadershipMetrics())
-                .leaderElectionService(leaderElectionServiceFactory().create(clientAwareComponents))
-                .build();
+                .build()
+                .leadershipContext();
     }
 
     @Value.Derived
@@ -107,18 +101,17 @@ public abstract class LeadershipContextFactory implements
     @Value.Immutable
     abstract static class ClientAwareComponents implements
             Dependencies.ClientAwareComponents,
-            Dependencies.LeaderElectionService,
             Dependencies.LeadershipMetrics {
 
         public abstract Client proxyClient();
 
         @Value.Derived
-        public Client paxosClient() {
+        Client paxosClient() {
             return useCase().resolveClient(proxyClient());
         }
 
         @Value.Derived
-        public PaxosLearner localLearner() {
+        PaxosLearner localLearner() {
             return components().learner(paxosClient());
         }
 
@@ -135,6 +128,37 @@ public abstract class LeadershipContextFactory implements
         @Value.Derived
         public PaxosLeadershipEventRecorder eventRecorder() {
             return leadershipMetrics().eventRecorder();
+        }
+
+        @Value.Derived
+        LeaderElectionService leaderElectionService() {
+            return new LeaderElectionServiceBuilder()
+                    .leaderPinger(leaderPinger())
+                    .leaderUuid(leaderUuid())
+                    .pingRate(runtime().get().pingRate())
+                    .randomWaitBeforeProposingLeadership(runtime().get().maximumWaitBeforeProposingLeadership())
+                    .eventRecorder(eventRecorder())
+                    .knowledge(localLearner())
+                    .acceptorClient(networkClientFactories().acceptor().create(paxosClient()))
+                    .learnerClient(networkClientFactories().learner().create(paxosClient()))
+                    .decorateProposer(this::instrumentProposer)
+                    .build();
+        }
+
+        @Value.Derived
+        LeadershipContext leadershipContext() {
+            return ImmutableLeadershipContext.builder()
+                    .leadershipMetrics(leadershipMetrics())
+                    .leaderElectionService(leaderElectionService())
+                    .build();
+        }
+
+        private PaxosProposer instrumentProposer(PaxosProposer uninstrumentedPaxosProposer) {
+            return metrics().instrument(
+                    PaxosProposer.class,
+                    uninstrumentedPaxosProposer,
+                    "paxos-proposer",
+                    paxosClient());
         }
 
     }
