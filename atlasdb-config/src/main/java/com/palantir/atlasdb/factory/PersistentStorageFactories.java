0e9e1f4bb399249cdbbf9f6f5a9055b852cd74a4
Stevan Ognjanovic <sognjanovic@palantir.com>
2020-01-16 14:13:36 +0000

    Safer deleting.

--- atlasdb-config/src/main/java/com/palantir/atlasdb/factory/PersistentStorageFactories.java
+++ atlasdb-config/src/main/java/com/palantir/atlasdb/factory/PersistentStorageFactories.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -28,18 +29,21 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.base.MoreObjects;
 import com.palantir.logsafe.Preconditions;
 import com.palantir.logsafe.SafeArg;
 
 public final class PersistentStorageFactories {
+    private static final Logger log = LoggerFactory.getLogger(PersistentStorageFactories.class);
     private static final Pattern UUID_PATTERN = Pattern.compile(
             "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
 
     private static final Set<String> SANITIZED_PATHS = new HashSet<>();
 
     private PersistentStorageFactories() {}
-
 
     /**
      * For the given path does the following: 1) it is sanitized only once per VM lifetime 2) if it exists checks that
@@ -68,16 +72,42 @@ public final class PersistentStorageFactories {
                 "Storage path has to point to a directory",
                 SafeArg.of("storageDirectory", storageDirectory.getAbsolutePath()));
 
-        for (File file : MoreObjects.firstNonNull(storageDirectory.listFiles(), new File[0])) {
-            if (file.isDirectory() && (UUID_PATTERN.matcher(file.getName()).matches())) {
-                try {
-                    deletePath(file.toPath().toAbsolutePath());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+        List<File> uuidSubDirectories = uuidNamedSubDirectories(
+                MoreObjects.firstNonNull(storageDirectory.listFiles(), new File[0]));
+        List<File> directoryContentToDelete = limitFoldersToDelete(uuidSubDirectories);
+        deleteDirectories(directoryContentToDelete);
+
+        SANITIZED_PATHS.add(storagePath);
+    }
+
+    private static List<File> uuidNamedSubDirectories(File[] files) {
+        return Arrays.stream(files)
+                .filter(PersistentStorageFactories::folderFilter)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean folderFilter(File file) {
+        return file.isDirectory() && (UUID_PATTERN.matcher(file.getName()).matches());
+    }
+
+    private static List<File> limitFoldersToDelete(List<File> folders) {
+        if (folders.size() > 2) {
+            log.warn(
+                    "You are trying to delete more that two UUID named folders during persistent storage start-up. "
+                            + "Only two will be deleted.",
+                    SafeArg.of("number", folders.size()));
+        }
+        return folders.stream().limit(2).collect(Collectors.toList());
+    }
+
+    private static void deleteDirectories(List<File> directoryContentToDelete) {
+        for (File directoryToDelete : directoryContentToDelete) {
+            try {
+                deletePath(directoryToDelete.toPath().toAbsolutePath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
-        SANITIZED_PATHS.add(storagePath);
     }
 
     private static void deletePath(Path absolutePath) throws IOException {
@@ -85,6 +115,7 @@ public final class PersistentStorageFactories {
                 absolutePath.isAbsolute(),
                 "Deletion path needs to be absolute",
                 SafeArg.of("path", absolutePath.toString()));
+        log.info("Deleted folder during sanitization.", SafeArg.of("path", absolutePath));
 
         try (Stream<Path> stream = Files.walk(absolutePath)) {
             List<Path> sortedPaths = stream.sorted(Comparator.reverseOrder()).collect(Collectors.toList());
