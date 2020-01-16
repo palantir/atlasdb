3d34fd4d78c39fa9638610d3e8524ed9fc0f5025
Jeremy Kong <jeremykong@hotmail.com>
2020-01-16 20:25:11 +0000

    More unusedness

--- leader-election-impl/src/main/java/com/palantir/paxos/PaxosQuorumChecker.java
+++ leader-election-impl/src/main/java/com/palantir/paxos/PaxosQuorumChecker.java
@@ -25,7 +25,6 @@ import java.util.concurrent.Future;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
