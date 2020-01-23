2b673d69d277d8f87c240850a94ea3bd1524a0be
Sudiksha Nanda <snanda@palantir.com>
2020-01-23 12:55:39 +0000

    Address comments(2) | add comment

--- atlasdb-client/src/main/java/com/palantir/atlasdb/annotation/Reusable.java
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/annotation/Reusable.java
@@ -1,3 +1,18 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.palantir.atlasdb.annotation;
 
 import java.lang.annotation.ElementType;
@@ -5,6 +20,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Use to annotate persister which is not stateful in order to avoid
+ * unnecessary object allocation (in the implementation of bytes hydrator).
+ *
+ * Note: Persister must be thread safe
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Reusable {
