05a665172a2cb7efcbe60f096b55289848214ea9
Jeremy Kong <jeremykong@hotmail.com>
2020-02-04 21:34:14 +0000

    stash

--- timelock-agent/src/main/java/com/palantir/timelock/invariants/NoSimultaneousServiceCheck.java
+++ timelock-agent/src/main/java/com/palantir/timelock/invariants/NoSimultaneousServiceCheck.java
@@ -16,23 +16,76 @@
 
 package com.palantir.timelock.invariants;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.palantir.atlasdb.timelock.TimeLockResource;
+import com.google.common.util.concurrent.Uninterruptibles;
+import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
 
 public class NoSimultaneousServiceCheck {
     private static final Logger log = LoggerFactory.getLogger(NoSimultaneousServiceCheck.class);
 
     private static final int REQUIRED_CONSECUTIVE_VIOLATIONS_BEFORE_FAIL = 5;
+    private static final Duration BACKOFF = Duration.ofMillis(1337);
 
-    private final TimeLockResource timeLockResource;
+    private final List<TimeLockActivityChecker> timeLockActivityCheckers;
+    private final Consumer<String> failureMechanism;
 
-    public NoSimultaneousServiceCheck(TimeLockResource timeLockResource) {
-        this.timeLockResource = timeLockResource;
+    private NoSimultaneousServiceCheck(
+            List<TimeLockActivityChecker> timeLockActivityCheckers,
+            Consumer<String> failureMechanism) {
+        this.timeLockActivityCheckers = timeLockActivityCheckers;
+        this.failureMechanism = failureMechanism;
+    }
+
+    public static NoSimultaneousServiceCheck create(List<TimeLockActivityChecker> timeLockActivityCheckers) {
+        return new NoSimultaneousServiceCheck(timeLockActivityCheckers,
+                client -> {
+                    throw ServerKiller.killMeNow(new SafeIllegalStateException(
+                            "We observed that multiple services were consistently all serving timestamps! This is"
+                                    + " potentially indicative of SEVERE DATA CORRUPTION, and should never happen in a"
+                                    + " correct TimeLock implementation. If you see this message, please check the"
+                                    + " frequency of leader elections on your stack. If there were very many leader"
+                                    + " elections around this time, consider increasing the leader election timeout."
+                                    + " Otherwise, please contact support - your stack may have been compromised."
+                    ));
+                });
     }
 
     public void performCheckOnSpecificClient(String client) {
+        // Only fail on repeated violations, since it is possible for there to be a leader election between checks that
+        // could legitimately cause false positives if we failed after one such issue. However, given the number of
+        // checks it is unlikely that *that* many elections would occur.
+        for (int attempt = 1; attempt <= REQUIRED_CONSECUTIVE_VIOLATIONS_BEFORE_FAIL; attempt++) {
+            long numberOfNodesServingTimestamps = timeLockActivityCheckers.stream()
+                    .map(timeLockActivityChecker ->
+                            timeLockActivityChecker.isThisNodeActivelyServingTimestampsForClient(client))
+                    .filter(x -> x)
+                    .count();
+            if (numberOfNodesServingTimestamps <= 1) {
+                // Accept 0: the cluster being in such a bad state is not a terminal condition, could just be a
+                // network partition or legitimate no-quorum situation. No reason to kill the server then.
+                log.info("We don't think services were simultaneously serving timestamps for client {}",
+                        SafeArg.of("client", client));
+                return;
+            }
 
+            if (attempt < REQUIRED_CONSECUTIVE_VIOLATIONS_BEFORE_FAIL) {
+                log.info("We observed on attempt {} of {} that multiple services were serving timestamps. We'll try"
+                                + " again in {} ms to see if this remains the case.",
+                        SafeArg.of("attemptNumber", attempt),
+                        SafeArg.of("maximumAttempts", REQUIRED_CONSECUTIVE_VIOLATIONS_BEFORE_FAIL),
+                        SafeArg.of("backoffMillis", BACKOFF.toMillis()));
+                Uninterruptibles.sleepUninterruptibly(BACKOFF.toMillis(), TimeUnit.MILLISECONDS);
+            } else {
+                failureMechanism.accept(client);
+            }
+        }
     }
 }
---
+++ timelock-agent/src/main/java/com/palantir/timelock/invariants/TimeLockActivityCheckerFactory.java
@@ -0,0 +1,74 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.timelock.invariants;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.palantir.atlasdb.config.AuxiliaryRemotingParameters;
+import com.palantir.atlasdb.config.ImmutableRemotingClientConfig;
+import com.palantir.atlasdb.config.ImmutableServerListConfig;
+import com.palantir.atlasdb.config.RemotingClientConfig;
+import com.palantir.atlasdb.config.RemotingClientConfigs;
+import com.palantir.atlasdb.config.ServerListConfig;
+import com.palantir.atlasdb.config.ServerListConfigs;
+import com.palantir.atlasdb.factory.ServiceCreator;
+import com.palantir.atlasdb.http.AtlasDbHttpClients;
+import com.palantir.atlasdb.util.MetricsManager;
+import com.palantir.conjure.java.api.config.service.UserAgent;
+import com.palantir.conjure.java.config.ssl.SslSocketFactories;
+import com.palantir.lock.v2.TimelockRpcClient;
+import com.palantir.lock.v2.TimelockService;
+import com.palantir.timelock.config.ClusterConfiguration;
+
+public class TimeLockActivityCheckerFactory {
+    private final ClusterConfiguration timelockClusterConfiguration;
+    private final MetricsManager metricsManager;
+    private final UserAgent userAgent;
+
+    public TimeLockActivityCheckerFactory(
+            ClusterConfiguration timelockClusterConfiguration, MetricsManager metricsManager, UserAgent userAgent) {
+        this.timelockClusterConfiguration = timelockClusterConfiguration;
+        this.metricsManager = metricsManager;
+        this.userAgent = userAgent;
+    }
+
+    public List<TimeLockActivityChecker> getTimeLockActivityCheckers() {
+        return timelockClusterConfiguration.clusterMembers()
+                .stream()
+                .map(this::createServiceCreatorForRemote)
+                .map(creator -> creator.createService(TimelockRpcClient.class))
+                .map(TimeLockActivityChecker::new)
+                .collect(Collectors.toList());
+    }
+
+    private ServiceCreator createServiceCreatorForRemote(String remoteUrl) {
+        return ServiceCreator.withPayloadLimiter(
+                metricsManager,
+                () -> getServerListConfig(remoteUrl),
+                userAgent,
+                () -> RemotingClientConfigs.ALWAYS_USE_CONJURE);
+    }
+
+    private ServerListConfig getServerListConfig(String remoteUrl) {
+        return ImmutableServerListConfig.builder()
+                .addServers(remoteUrl)
+                .sslConfiguration(timelockClusterConfiguration.cluster().security())
+                .proxyConfiguration(timelockClusterConfiguration.cluster().proxyConfiguration())
+                .build();
+    }
+}
