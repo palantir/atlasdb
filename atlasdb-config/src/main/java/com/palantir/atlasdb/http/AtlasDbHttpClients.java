eaa0c615d8013ae1e0fc0306b9cc11f33bdceac9
Jeremy Kong <jeremykong@hotmail.com>
2020-03-25 13:16:30 +0000

    Fix checkstyle

--- atlasdb-config/src/main/java/com/palantir/atlasdb/http/AtlasDbHttpClients.java
+++ atlasdb-config/src/main/java/com/palantir/atlasdb/http/AtlasDbHttpClients.java
@@ -18,11 +18,9 @@ package com.palantir.atlasdb.http;
 import java.net.SocketTimeoutException;
 import java.time.Duration;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Suppliers;
 import com.palantir.atlasdb.config.AuxiliaryRemotingParameters;
 import com.palantir.atlasdb.config.ServerListConfig;
 import com.palantir.atlasdb.http.v2.ConjureJavaRuntimeTargetFactory;
