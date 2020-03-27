4c6c49e986b11df46a496bd61ab304a95c0ac5f3
Jolyon Shah <jshah@palantir.com>
2020-03-27 13:11:35 +0000

    use non-retriable txns

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/schema/KeyValueServiceValidator.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/schema/KeyValueServiceValidator.java
@@ -115,7 +115,7 @@ public class KeyValueServiceValidator {
     private void validateTable(final TableReference table) {
         final int limit = getBatchSize(table);
         // read only, but need to use a write tx in case the source table has SweepStrategy.THOROUGH
-        validationFromTransactionManager.runTaskWithRetry(
+        validationFromTransactionManager.runTaskThrowOnConflict(
                 (TransactionTask<Map<Cell, byte[]>, RuntimeException>) t1 -> {
                     validateTable(table, limit, t1);
                     return null;
@@ -126,7 +126,7 @@ public class KeyValueServiceValidator {
 
     private void validateTable(final TableReference table, final int limit, final Transaction t1) {
         // read only, but need to use a write tx in case the source table has SweepStrategy.THOROUGH
-        validationToTransactionManager.runTaskWithRetry(
+        validationToTransactionManager.runTaskThrowOnConflict(g
                 (TransactionTask<Map<Cell, byte[]>, RuntimeException>) t2 -> {
                     validateTable(table, limit, t1, t2);
                     return null;
