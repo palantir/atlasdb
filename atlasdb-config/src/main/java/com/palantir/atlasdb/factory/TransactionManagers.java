9801f770b3dcb474b3769d75e6a9251b4525571e
jakubk <jakubk@palantir.com>
2020-04-27 09:11:00 +0100

    Remove deprecation until downstream services catch up.

--- atlasdb-config/src/main/java/com/palantir/atlasdb/factory/TransactionManagers.java
+++ atlasdb-config/src/main/java/com/palantir/atlasdb/factory/TransactionManagers.java
@@ -205,9 +205,8 @@ public abstract class TransactionManagers {
     abstract Optional<Refreshable<Optional<AtlasDbRuntimeConfig>>> runtimeConfig();
 
     /**
-     * @deprecated use {@link #runtimeConfig} instead.
+     * Use {@link #runtimeConfig} instead.
      */
-    @Deprecated
     abstract Optional<Supplier<Optional<AtlasDbRuntimeConfig>>> runtimeConfigSupplier();
 
     abstract Set<Schema> schemas();
