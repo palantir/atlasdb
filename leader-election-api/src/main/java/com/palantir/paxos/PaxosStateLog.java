c0d3b7f78f73ed7e0efac99fcca85ec0eaadc2e6
Grgur Petric Maretic <grgapm@gmail.com>
2020-04-28 11:13:44 +0100

    Checkstyle is a joke

--- leader-election-api/src/main/java/com/palantir/paxos/PaxosStateLog.java
+++ leader-election-api/src/main/java/com/palantir/paxos/PaxosStateLog.java
@@ -35,10 +35,9 @@ public interface PaxosStateLog<V extends Persistable & Versionable> {
 
     /**
      * Implementations of this method MUST obey the following:
-     * 1) The end state of the log should be equivalent to applying the operations in 
-     *    iteration order.
-     * 2) Implementations need not be atomic. However, if they fail, they should apply only
-     *    prefixes of the input iterable.
+     * 1) The end state of the log should be equivalent to applying the operations in iteration order.
+     * 2) Implementations need not be atomic. However, if they fail, they should apply only prefixes of the input
+     * iterable.
      */
     default void writeBatchOfRounds(Iterable<PaxosRound<V>> rounds) {
         rounds.forEach(round -> writeRound(round.sequence(), round.value()));
