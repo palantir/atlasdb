b9cb67290dede360f9b3b39f8e1cb4dfb6c965c9
gmaretic <gmaretic@palantir.com>
2020-04-28 11:57:40 +0100

    [PaxosLog] Expose batch write method (#4737)
    * Use JDBI
    
    * Package private
    
    * Prevent test connection from closing so we don't need to have funky factory in production code
    
    * Add batching support
    
    * Address CR
    
    * Oops
    
    * Update leader-election-api/src/main/java/com/palantir/paxos/PaxosStateLog.java
    
    Co-Authored-By: Jeremy Kong <jeremykong@hotmail.com>
    
    * Checkstyle is a joke
    
    Co-authored-by: Jeremy Kong <jeremykong@hotmail.com>

--- leader-election-impl/src/main/java/com/palantir/paxos/SqlitePaxosStateLog.java
+++ leader-election-impl/src/main/java/com/palantir/paxos/SqlitePaxosStateLog.java
@@ -22,10 +22,13 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.immutables.JdbiImmutables;
 import org.jdbi.v3.sqlobject.SingleValue;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindPojo;
 import org.jdbi.v3.sqlobject.customizer.Define;
+import org.jdbi.v3.sqlobject.statement.SqlBatch;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
@@ -43,6 +46,7 @@ public final class SqlitePaxosStateLog<V extends Persistable & Versionable> impl
     public static <V extends Persistable & Versionable> PaxosStateLog<V> create(String namespace,
             Supplier<Connection> connectionSupplier) {
         Jdbi jdbi = Jdbi.create(connectionSupplier::get).installPlugin(new SqlObjectPlugin());
+        jdbi.getConfig(JdbiImmutables.class).registerImmutable(PaxosRound.class);
         SqlitePaxosStateLog<V> log = new SqlitePaxosStateLog<>(namespace, jdbi);
         log.initialize();
         return log;
@@ -55,6 +59,11 @@ public final class SqlitePaxosStateLog<V extends Persistable & Versionable> impl
     @Override
     public void writeRound(long seq, V round) {
         execute(dao -> dao.writeRound(namespace, seq, round.persistToBytes()));
+    }
+
+    @Override
+    public void writeBatchOfRounds(Iterable<PaxosRound<V>> rounds) {
+        execute(dao -> dao.writeBatchOfRounds(namespace, rounds));
     }
 
     @Override
@@ -100,5 +109,9 @@ public final class SqlitePaxosStateLog<V extends Persistable & Versionable> impl
 
         @SqlUpdate("DELETE FROM <table> WHERE seq <= :seq")
         boolean truncate(@Define("table") String table, @Bind("seq") long seq);
+
+        @SqlBatch("INSERT OR REPLACE INTO <table> (seq, val) VALUES (:round.sequence, :round.valueBytes)")
+        <V extends Persistable & Versionable> boolean[] writeBatchOfRounds(@Define("table") String table,
+                @BindPojo("round") Iterable<PaxosRound<V>> rounds);
     }
 }
