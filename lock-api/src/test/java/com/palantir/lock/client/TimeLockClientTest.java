2bee6ba068054e2586147d8efab8f4a59aed06db
Jolyon Shah <jshah@palantir.com>
2020-05-05 11:42:49 +0100

    Remove unused import

--- lock-api/src/test/java/com/palantir/lock/client/TimeLockClientTest.java
+++ lock-api/src/test/java/com/palantir/lock/client/TimeLockClientTest.java
@@ -19,7 +19,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
