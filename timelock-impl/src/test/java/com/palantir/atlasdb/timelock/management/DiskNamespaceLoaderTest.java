fda8c53e67066cb8865fc1e56eda137ca0c634fc
Sudiksha Nanda <snanda@palantir.com>
2020-05-11 14:14:28 +0100

    Tests

---
+++ timelock-impl/src/test/java/com/palantir/atlasdb/timelock/management/DiskNamespaceLoaderTest.java
@@ -0,0 +1,66 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.timelock.management;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.palantir.atlasdb.timelock.paxos.PaxosTimeLockConstants;
+
+public class DiskNamespaceLoaderTest {
+    private static final String NAMESPACE_1 = "testNamespace";
+
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    private DiskNamespaceLoader diskNamespaceLoader;
+
+    @Before
+    public void setup() {
+        createDirectory(NAMESPACE_1);
+        createDirectory(PaxosTimeLockConstants.LEADER_PAXOS_NAMESPACE);
+        diskNamespaceLoader = new DiskNamespaceLoader(tempFolder.getRoot().toPath());
+    }
+
+    @Test
+    public void doesNotLoadLeaderPaxosAsNamespace() {
+        Set<String> namespaces = diskNamespaceLoader.getNamespaces();
+        assertThat(namespaces).containsExactly(NAMESPACE_1);
+    }
+
+    private void createDirectory(String namespace) {
+        new File(tempFolder.getRoot().toPath() + "/" +
+                Paths.get(PaxosTimeLockConstants.LEADER_PAXOS_NAMESPACE,
+                        PaxosTimeLockConstants.MULTI_LEADER_PAXOS_NAMESPACE).toString() + "/" +
+                namespace).mkdirs();
+    }
+
+    @After
+    public void cleanup() {
+        tempFolder.delete();
+    }
+}
