93ce6cd1ba63a9933e89fcbfde32be2eaa81a387
Jeremy Kong <jeremykong@hotmail.com>
2020-05-13 18:34:53 +0100

    MCTT

---
+++ atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/keyvalue/MemoryCachingTransactionTest.java
@@ -0,0 +1,31 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.keyvalue;
+
+import com.palantir.atlasdb.transaction.api.Transaction;
+import com.palantir.atlasdb.transaction.impl.CachingTransaction;
+
+public class MemoryCachingTransactionTest extends MemoryTransactionTest {
+    public MemoryCachingTransactionTest() {
+        super();
+    }
+
+    @Override
+    protected Transaction startTransaction() {
+        return new CachingTransaction(super.startTransaction());
+    }
+}
