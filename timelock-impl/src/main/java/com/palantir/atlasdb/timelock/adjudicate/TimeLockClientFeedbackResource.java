ae9e123310468f2e30ccf55d0e7bc8fa8c27a698
Sudiksha Nanda <snanda@palantir.com>
2020-06-24 18:57:09 +0100

    Address comments

--- timelock-impl/src/main/java/com/palantir/atlasdb/timelock/adjudicate/TimeLockClientFeedbackResource.java
+++ timelock-impl/src/main/java/com/palantir/atlasdb/timelock/adjudicate/TimeLockClientFeedbackResource.java
@@ -16,7 +16,7 @@
 
 package com.palantir.atlasdb.timelock.adjudicate;
 
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -29,26 +29,26 @@ import com.palantir.timelock.feedback.ConjureTimeLockClientFeedback;
 import com.palantir.tokens.auth.AuthHeader;
 
 public class TimeLockClientFeedbackResource implements UndertowTimeLockClientFeedbackService {
-    private Function<Client, Boolean> leadershipCheck;
+    private Predicate<Client> leadershipCheck;
 
-    private TimeLockClientFeedbackResource(Function<Client, Boolean> leadershipCheck) {
+    private TimeLockClientFeedbackResource(Predicate<Client> leadershipCheck) {
         this.leadershipCheck = leadershipCheck;
     }
 
-    public static TimeLockClientFeedbackResource create(Function<Client, Boolean> leadershipCheck) {
+    public static TimeLockClientFeedbackResource create(Predicate<Client> leadershipCheck) {
         return new TimeLockClientFeedbackResource(leadershipCheck);
     }
 
-    public static UndertowService undertow(Function<Client, Boolean> leadershipCheck) {
+    public static UndertowService undertow(Predicate<Client> leadershipCheck) {
         return TimeLockClientFeedbackServiceEndpoints.of(TimeLockClientFeedbackResource.create(leadershipCheck));
     }
 
-    public static TimeLockClientFeedbackService jersey(Function<Client, Boolean> leadershipCheck) {
+    public static TimeLockClientFeedbackService jersey(Predicate<Client> leadershipCheck) {
         return new JerseyAdapter(TimeLockClientFeedbackResource.create(leadershipCheck));
     }
     @Override
     public ListenableFuture<Void> reportFeedback(AuthHeader authHeader, ConjureTimeLockClientFeedback feedbackReport) {
-        if (leadershipCheck.apply(Client.of(feedbackReport.getServiceName()))) {
+        if (leadershipCheck.test(Client.of(feedbackReport.getServiceName()))) {
             // process feedback
         }
         return Futures.immediateVoidFuture();
