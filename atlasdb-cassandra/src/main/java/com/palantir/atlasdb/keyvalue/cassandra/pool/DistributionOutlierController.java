416df4b1dcd7fabb6e71fae296c886d077c4d0aa
Jeremy Kong <jeremykong@hotmail.com>
2020-07-07 21:33:45 +0100

    Baseline

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/pool/DistributionOutlierController.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/pool/DistributionOutlierController.java
@@ -17,7 +17,6 @@
 package com.palantir.atlasdb.keyvalue.cassandra.pool;
 
 import java.time.Duration;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
