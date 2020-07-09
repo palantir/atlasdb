7120bfcbeab9b0bd3a9cea82e03997f3a4b0acbe
Jeremy Kong <jeremykong@hotmail.com>
2020-07-09 10:33:40 +0100

    Toplist Delta

---
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/metrics/ToplistDeltaFilteringTableLevelMetricsController.java
@@ -0,0 +1,118 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.transaction.impl.metrics;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import com.codahale.metrics.CachedGauge;
+import com.codahale.metrics.Clock;
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.metrics.MetricPublicationFilter;
+import com.palantir.atlasdb.util.MetricsManager;
+import com.palantir.atlasdb.util.TopNMetricPublicationController;
+
+/**
+ * Makes publication decisions for a given metric, as follows: for a given String identifier, filters out all but the
+ * highest {@code maximumNumberOfTables} values. In the event of ties (e.g. a top-list of 10 where the 10th and 11th
+ * highest values are equal), all tying values are published.
+ *
+ * This controller makes decisions based on deltas (in an attempt to be able to detect load spikes) measured over the
+ * last {@@code REFRESH_INTERVAL} period.
+ */
+public final class ToplistDeltaFilteringTableLevelMetricsController implements TableLevelMetricsController {
+    private static final int DEFAULT_MAX_TABLES_TO_PUBLISH_METRICS = 10;
+    private static final String CONTROLLER_GENERATED = "controllerGenerated";
+    private static final String TRUE = "true";
+
+    @VisibleForTesting
+    static final Duration REFRESH_INTERVAL = Duration.ofSeconds(30);
+
+    private final Map<String, TopNMetricPublicationController<Long>> metricNameToPublicationController;
+    private final MetricsManager metricsManager;
+    private final int maximumNumberOfTables;
+    private final Clock clock;
+
+    @VisibleForTesting
+    ToplistDeltaFilteringTableLevelMetricsController(
+            MetricsManager metricsManager,
+            int maximumNumberOfTables,
+            Clock clock) {
+        this.metricNameToPublicationController = Maps.newConcurrentMap();
+        this.metricsManager = metricsManager;
+        this.maximumNumberOfTables = maximumNumberOfTables;
+        this.clock = clock;
+    }
+
+    public static TableLevelMetricsController create(MetricsManager metricsManager) {
+        return new ToplistDeltaFilteringTableLevelMetricsController(
+                metricsManager,
+                DEFAULT_MAX_TABLES_TO_PUBLISH_METRICS,
+                Clock.defaultClock());
+    }
+
+    @Override
+    public <T> Counter createAndRegisterCounter(Class<T> clazz, String metricName, TableReference tableReference) {
+        Counter counter = metricsManager.registerOrGetTaggedCounter(
+                clazz,
+                metricName,
+                getTagsForTableReference(tableReference));
+        metricsManager.addMetricFilter(
+                clazz,
+                metricName,
+                getTagsForTableReference(tableReference),
+                MetricPublicationFilter.NEVER_PUBLISH);
+
+
+        Gauge<Long> gauge = new ZeroBasedDeltaGauge(counter::getCount);
+        Gauge<Long> memoizedGauge = new CachedGauge<Long>(clock, REFRESH_INTERVAL.toNanos(), TimeUnit.NANOSECONDS) {
+            @Override
+            protected Long loadValue() {
+                return gauge.getValue();
+            }
+        };
+
+        MetricPublicationFilter filter = metricNameToPublicationController.computeIfAbsent(metricName,
+                _name -> TopNMetricPublicationController.create(maximumNumberOfTables))
+                .registerAndCreateFilter(memoizedGauge);
+        metricsManager.addMetricFilter(
+                clazz,
+                metricName,
+                metricsManager.getTableNameTagFor(tableReference),
+                filter);
+        metricsManager.registerOrGet(
+                clazz,
+                metricName,
+                gauge,
+                metricsManager.getTableNameTagFor(tableReference));
+
+        return counter;
+    }
+
+    private Map<String, String> getTagsForTableReference(TableReference tableReference) {
+        return ImmutableMap.<String, String>builder()
+                .putAll(metricsManager.getTableNameTagFor(tableReference))
+                .put(CONTROLLER_GENERATED, TRUE)
+                .build();
+    }
+}
---
+++ atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/transaction/impl/metrics/ToplistDeltaFilteringTableLevelMetricsControllerTest.java
@@ -0,0 +1,110 @@
+
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.transaction.impl.metrics;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.codahale.metrics.Clock;
+import com.codahale.metrics.Counter;
+import com.google.common.collect.ImmutableMap;
+import com.palantir.atlasdb.keyvalue.api.Namespace;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.util.MetricsManager;
+import com.palantir.atlasdb.util.MetricsManagers;
+import com.palantir.tritium.metrics.registry.MetricName;
+
+public class ToplistDeltaFilteringTableLevelMetricsControllerTest {
+    private final Clock mockClock = mock(Clock.class);
+    private final MetricsManager metricsManager = MetricsManagers.createAlwaysSafeAndFilteringForTests();
+    private final ToplistDeltaFilteringTableLevelMetricsController controller
+            = new ToplistDeltaFilteringTableLevelMetricsController(metricsManager, 3, mockClock);
+
+    @Before
+    public void setUpClock() {
+        when(mockClock.getTick()).thenReturn(0L);
+    }
+
+    @Test
+    public void letsSingularMetricsThrough() {
+        Counter counter = controller.createAndRegisterCounter(
+                Class.class,
+                "metricName",
+                TableReference.create(Namespace.create("namespace"), "table"));
+
+        counter.inc();
+        assertThat(metricsManager.getPublishableMetrics().getMetrics())
+                .hasSize(1)
+                .containsKey(getMetricName("table"));
+    }
+
+    @Test
+    public void selectsOnlyHighestMetricsForPublication() {
+        List<Counter> counters = IntStream.range(0, 5)
+                .mapToObj(index -> controller.createAndRegisterCounter(
+                        Class.class,
+                        "metricName",
+                        TableReference.create(Namespace.create("namespace"), "table" + index)))
+                .collect(Collectors.toList());
+
+        IntStream.range(0, 5)
+                .forEach(index -> counters.get(index).inc(index));
+        assertThat(metricsManager.getPublishableMetrics().getMetrics())
+                .containsOnlyKeys(getMetricName("table2"), getMetricName("table3"), getMetricName("table4"));
+    }
+
+    @Test
+    public void detectsSpikeInMetrics() {
+        List<Counter> counters = IntStream.range(0, 5)
+                .mapToObj(index -> controller.createAndRegisterCounter(
+                        Class.class,
+                        "metricName",
+                        TableReference.create(Namespace.create("namespace"), "table" + index)))
+                .collect(Collectors.toList());
+
+        IntStream.range(0, 5)
+                .forEach(index -> counters.get(index).inc(index));
+
+        when(mockClock.getTick()).thenReturn(
+                ToplistDeltaFilteringTableLevelMetricsController.REFRESH_INTERVAL.toNanos() + 1);
+        counters.get(1).inc(1_000_000L);
+        assertThat(metricsManager.getPublishableMetrics().getMetrics())
+                .containsKey(getMetricName("table1"));
+
+        when(mockClock.getTick()).thenReturn(
+                ToplistDeltaFilteringTableLevelMetricsController.REFRESH_INTERVAL.toNanos() * 2 + 1);
+        IntStream.range(0, 5).forEach(index -> counters.get(index).inc(index));
+        assertThat(metricsManager.getPublishableMetrics().getMetrics())
+                .doesNotContainKey(getMetricName("table1"));
+    }
+
+    private static MetricName getMetricName(String tableName) {
+        return MetricName.builder().safeName(Class.class.getName() + ".metricName")
+                .safeTags(ImmutableMap.of("tableName", tableName))
+                .build();
+    }
+
+}
