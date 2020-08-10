3a3515a6ff1e4a23f8e99faba19c0e039c459335
Sudiksha Nanda <snanda@palantir.com>
2020-08-10 17:19:30 +0100

    Bump TimeLock Schema Version

--- timelock-agent/src/main/java/com/palantir/timelock/paxos/TimeLockAgent.java
+++ timelock-agent/src/main/java/com/palantir/timelock/paxos/TimeLockAgent.java
@@ -76,7 +76,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @SuppressWarnings("checkstyle:FinalClass") // This is mocked internally
 public class TimeLockAgent {
-    private static final Long SCHEMA_VERSION = 2L;
+    private static final Long SCHEMA_VERSION = 3L;
 
     private final MetricsManager metricsManager;
     private final TimeLockInstallConfiguration install;
