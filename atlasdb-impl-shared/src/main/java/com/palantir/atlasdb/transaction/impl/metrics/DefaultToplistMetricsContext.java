0d68bb4fce6a9157646d4d9a68d4c71e0bf1e6b5
Jeremy Kong <jeremykong@hotmail.com>
2020-08-11 12:46:01 +0100

    Shared contexts

---
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/metrics/DefaultToplistMetricsContext.java
@@ -0,0 +1,41 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.transaction.impl.metrics;
+
+import java.util.Map;
+
+import com.codahale.metrics.Gauge;
+import com.google.common.collect.Maps;
+import com.palantir.atlasdb.metrics.MetricPublicationFilter;
+import com.palantir.atlasdb.util.TopNMetricPublicationController;
+
+public class DefaultToplistMetricsContext implements ToplistMetricsContext {
+    private final int toplistSize;
+    private final Map<String, TopNMetricPublicationController<Long>> keyToPublicationController;
+
+    DefaultToplistMetricsContext(int toplistSize) {
+        this.keyToPublicationController = Maps.newConcurrentMap();
+        this.toplistSize = toplistSize;
+    }
+
+    @Override
+    public MetricPublicationFilter registerAndCreateTopNFilter(String key, Gauge<Long> gauge) {
+        return keyToPublicationController.computeIfAbsent(key,
+                _name -> TopNMetricPublicationController.create(toplistSize))
+                .registerAndCreateFilter(gauge);
+    }
+}
