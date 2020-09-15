3520a5affb635d2e856241aa529338562c234190
sudiksha nanda <58551710+sudiksha27@users.noreply.github.com>
2020-09-15 14:57:58 +0100

    [Part 1] | TimeLock shoots itself (#4982)
    * TimeLock corruption pingers
    
    * Refactor
    
    * Redesign
    
    * Refactor
    
    * Address comments - 1
    
    * Parallel notification
    
    * Volatile state
    
    * Revert "Parallel notification"
    
    This reverts commit 285139c428f6e70f057608e4ba33069d43fc02eb.
    
    * Do not kill

---
+++ timelock-impl/src/main/java/com/palantir/atlasdb/timelock/corruption/LocalCorruptionDetector.java
@@ -0,0 +1,76 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.timelock.corruption;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.palantir.common.concurrent.NamedThreadFactory;
+import com.palantir.common.concurrent.PTExecutors;
+import com.palantir.timelock.corruption.TimeLockCorruptionNotifier;
+
+public class LocalCorruptionDetector implements CorruptionDetector {
+    private static final Duration TIMELOCK_CORRUPTION_ANALYSIS_INTERVAL = Duration.ofMinutes(5);
+    private static final String CORRUPTION_DETECTOR_THREAD_PREFIX = "timelock-corruption-detector";
+
+    private final ScheduledExecutorService executor = PTExecutors.newSingleThreadScheduledExecutor(
+            new NamedThreadFactory(CORRUPTION_DETECTOR_THREAD_PREFIX, true));
+    private final LocalCorruptionHandler corruptionHandler;
+
+    private volatile CorruptionStatus localCorruptionState = CorruptionStatus.HEALTHY;
+
+    public static LocalCorruptionDetector create(List<TimeLockCorruptionNotifier> corruptionNotifiers) {
+        LocalCorruptionDetector localCorruptionDetector
+                = new LocalCorruptionDetector(corruptionNotifiers);
+
+//        TODO(snanda) - uncomment when TL corruption detection goes live
+//        timeLockLocalCorruptionDetector.scheduleWithFixedDelay();
+        return localCorruptionDetector;
+    }
+
+    private LocalCorruptionDetector(List<TimeLockCorruptionNotifier> corruptionNotifiers) {
+        this.corruptionHandler = new LocalCorruptionHandler(corruptionNotifiers);
+    }
+
+    private void scheduleWithFixedDelay() {
+        executor.scheduleWithFixedDelay(() -> {
+                    if (detectedSignsOfCorruption()) {
+                        killTimeLock();
+                    }
+                },
+                TIMELOCK_CORRUPTION_ANALYSIS_INTERVAL.getSeconds(),
+                TIMELOCK_CORRUPTION_ANALYSIS_INTERVAL.getSeconds(),
+                TimeUnit.SECONDS);
+    }
+
+    private void killTimeLock() {
+        localCorruptionState = CorruptionStatus.CORRUPTION;
+        corruptionHandler.notifyRemoteServersOfCorruption();
+    }
+
+    private boolean detectedSignsOfCorruption() {
+        // no op for now
+        return false;
+    }
+
+    @Override
+    public boolean hasDetectedCorruption() {
+        return localCorruptionState.hasCorruption();
+    }
+}
