2fc57200ba99ac9eb5a9ed0b5f9db6db724fb35f
Jolyon Shah <jshah@palantir.com>
2020-10-01 14:57:07 +0100

    help it go green

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/api/watch/LockWatchEventLog.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/api/watch/LockWatchEventLog.java
@@ -59,7 +59,6 @@ final class LockWatchEventLog {
      *                     - the earliest version to which the events may be condensed to in the case of a snapshot.
      * @return lock watch events that occurred from (exclusive) the provided version, up to the end version (inclusive);
      *         this may begin with a snapshot if the latest version is too far behind.
-     * This may begin with a snapshot if the latest version is too far behind.
      */
     public ClientLogEvents getEventsBetweenVersions(VersionBounds versionBounds) {
         Optional<LockWatchVersion> startVersion = versionBounds.startVersion().map(this::createStartVersion);
