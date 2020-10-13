cdee853fa3437016c0a7879c42962ff4ae599e96
Jeremy Kong <jeremykong@hotmail.com>
2020-10-13 14:03:44 +0100

    fix imports

--- timelock-agent/src/main/java/com/palantir/timelock/paxos/TimeLockAgent.java
+++ timelock-agent/src/main/java/com/palantir/timelock/paxos/TimeLockAgent.java
@@ -35,7 +35,6 @@ import com.palantir.atlasdb.config.ServerListConfig;
 import com.palantir.atlasdb.http.BlockingTimeoutExceptionMapper;
 import com.palantir.atlasdb.http.NotCurrentLeaderExceptionMapper;
 import com.palantir.atlasdb.http.RedirectRetryTargeter;
-import com.palantir.atlasdb.spi.KeyValueServiceConfig;
 import com.palantir.atlasdb.timelock.AsyncTimelockService;
 import com.palantir.atlasdb.timelock.ConjureLockWatchingResource;
 import com.palantir.atlasdb.timelock.ConjureTimelockResource;
