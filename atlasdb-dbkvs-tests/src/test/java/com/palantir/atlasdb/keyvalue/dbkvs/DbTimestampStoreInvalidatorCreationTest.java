c1c061fb2d170a057b0e8f13f07d2f40fbc39e57
Sudiksha Nanda <snanda@palantir.com>
2020-10-15 17:06:59 +0100

    Revert "Fix build"
    This reverts commit fd5e1c13363c568d5262ad708bebadc73b7587de.

--- atlasdb-dbkvs-tests/src/test/java/com/palantir/atlasdb/keyvalue/dbkvs/DbTimestampStoreInvalidatorCreationTest.java
+++ atlasdb-dbkvs-tests/src/test/java/com/palantir/atlasdb/keyvalue/dbkvs/DbTimestampStoreInvalidatorCreationTest.java
@@ -80,7 +80,7 @@ public class DbTimestampStoreInvalidatorCreationTest {
     @Test
     public void doesNotInvalidateMultiSeriesTable() {
         assertThatThrownBy(() -> storeUpperLimitAndGetTimestampStoreInvalidator(
-                Optional.of(DbTimestampCreationSettings.multipleSeries(otherTable,
+                Optional.of(DbTimestampCreationSettings.multipleSeries(Optional.of(otherTable),
                         TimestampSeries.of("test")))))
                 .isInstanceOf(SafeIllegalStateException.class)
                 .hasMessageContaining("Invalidator must only be called by embedded DB timeLock that does not support "
