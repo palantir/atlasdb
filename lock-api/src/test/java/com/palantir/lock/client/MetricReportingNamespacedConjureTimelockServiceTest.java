dc5e523072eb9581d1e10e27ab7c69b38efef4e6
Jolyon Shah <jshah@palantir.com>
2020-10-19 16:21:54 +0100

    It is not an object

--- lock-api/src/test/java/com/palantir/lock/client/MetricReportingNamespacedConjureTimelockServiceTest.java
+++ lock-api/src/test/java/com/palantir/lock/client/MetricReportingNamespacedConjureTimelockServiceTest.java
@@ -17,6 +17,7 @@
 package com.palantir.lock.client;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -84,6 +85,6 @@ public class MetricReportingNamespacedConjureTimelockServiceTest {
         timelockService.startTransactions(startTransactionsRequest);
         timelockService.startTransactions(startTransactionsRequest);
         verify(metricRegistry).timer(any());
-        verify(mockedTimer).update(any(), eq(TimeUnit.NANOSECONDS));
+        verify(mockedTimer).update(anyLong(), eq(TimeUnit.NANOSECONDS));
     }
 }
