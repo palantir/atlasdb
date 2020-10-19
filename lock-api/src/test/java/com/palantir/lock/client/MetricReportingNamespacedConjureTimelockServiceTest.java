485a861e0d41cbcaf1bb3004572d7012b1e82cd6
Grgur Petric Maretic <grgapm@gmail.com>
2020-10-19 18:55:02 +0200

    Clean up and add test

--- lock-api/src/test/java/com/palantir/lock/client/MetricReportingNamespacedConjureTimelockServiceTest.java
+++ lock-api/src/test/java/com/palantir/lock/client/MetricReportingNamespacedConjureTimelockServiceTest.java
@@ -18,14 +18,14 @@ package com.palantir.lock.client;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -35,56 +35,72 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.palantir.atlasdb.timelock.api.ConjureStartTransactionsRequest;
 import com.palantir.atlasdb.timelock.api.ConjureStartTransactionsResponse;
+import com.palantir.atlasdb.timelock.api.GetCommitTimestampsRequest;
+import com.palantir.atlasdb.timelock.api.GetCommitTimestampsResponse;
 import com.palantir.lock.watch.LockWatchStateUpdate;
 import com.palantir.tritium.metrics.registry.TaggedMetricRegistry;
 
 public class MetricReportingNamespacedConjureTimelockServiceTest {
-
     private static final UUID LEADER_1 = UUID.randomUUID();
-    private ConjureStartTransactionsRequest startTransactionsRequest =
-            mock(ConjureStartTransactionsRequest.class);
-    private ConjureStartTransactionsResponse startTransactionsResponse =
-            mock(ConjureStartTransactionsResponse.class);
-    private LockWatchStateUpdate stateUpdate =
-            LockWatchStateUpdate.success(LEADER_1, -1L, ImmutableList.of());
-    private NamespacedConjureTimelockService delegate = mock(NamespacedConjureTimelockService.class);
-    private TaggedMetricRegistry metricRegistry = mock(TaggedMetricRegistry.class);
+    private static final LockWatchStateUpdate UPDATE = LockWatchStateUpdate.success(LEADER_1, -1L, ImmutableList.of());
+
+    private ConjureStartTransactionsRequest startTransactionsRequest = mock(ConjureStartTransactionsRequest.class);
+    private ConjureStartTransactionsResponse startTransactionsResponse = mock(ConjureStartTransactionsResponse.class);
+    private GetCommitTimestampsRequest commitTimestampsRequest = mock(GetCommitTimestampsRequest.class);
+    private GetCommitTimestampsResponse commitTimestampsResponse = mock(GetCommitTimestampsResponse.class);
+    private NamespacedConjureTimelockService mockedDelegeate = mock(NamespacedConjureTimelockService.class);
+    private TaggedMetricRegistry mockedRegistry = mock(TaggedMetricRegistry.class);
     private Timer mockedTimer = mock(Timer.class);
 
     private NamespacedConjureTimelockService timelockService;
 
     @Before
     public void before() {
-        timelockService = new MetricReportingNamespacedConjureTimelockService(delegate, metricRegistry);
-        when(metricRegistry.timer(any())).thenReturn(mockedTimer);
+        timelockService = new MetricReportingNamespacedConjureTimelockService(mockedDelegeate, mockedRegistry);
+        when(mockedDelegeate.startTransactions(any())).thenReturn(startTransactionsResponse);
+        when(mockedDelegeate.getCommitTimestamps(any())).thenReturn(commitTimestampsResponse);
+        when(mockedRegistry.timer(any())).thenReturn(mockedTimer);
+
+        when(startTransactionsResponse.getLockWatchUpdate()).thenReturn(UPDATE);
     }
 
     @Test
     public void firstCallDoesNotReportMetrics() {
-        when(delegate.startTransactions(any())).thenReturn(startTransactionsResponse);
-        when(startTransactionsResponse.getLockWatchUpdate()).thenReturn(stateUpdate);
         timelockService.startTransactions(startTransactionsRequest);
-        verifyNoInteractions(metricRegistry);
+        verifyNoInteractions(mockedRegistry);
     }
 
     @Test
     public void sameLeaderDoesNotReportMetrics() {
-        when(delegate.startTransactions(any())).thenReturn(startTransactionsResponse);
-        when(startTransactionsResponse.getLockWatchUpdate()).thenReturn(stateUpdate);
         timelockService.startTransactions(startTransactionsRequest);
         timelockService.startTransactions(startTransactionsRequest);
-        verifyNoInteractions(metricRegistry);
+        verifyNoInteractions(mockedRegistry);
     }
 
     @Test
-    public void differentLeaderReportsMetrics() {
-        when(delegate.startTransactions(any())).thenReturn(startTransactionsResponse);
+    public void reportMetricsOnLeaderElection() {
+        LockWatchStateUpdate.Snapshot secondUpdate =
+                LockWatchStateUpdate.snapshot(UUID.randomUUID(), 1L, ImmutableSet.of(), ImmutableSet.of());
         when(startTransactionsResponse.getLockWatchUpdate())
-                .thenReturn(stateUpdate)
-                .thenReturn(LockWatchStateUpdate.snapshot(UUID.randomUUID(), 1L, ImmutableSet.of(), ImmutableSet.of()));
+                .thenReturn(UPDATE)
+                .thenReturn(secondUpdate)
+                .thenReturn(secondUpdate)
+                .thenReturn(LockWatchStateUpdate.success(UUID.randomUUID(), 5L, ImmutableList.of()));
         timelockService.startTransactions(startTransactionsRequest);
         timelockService.startTransactions(startTransactionsRequest);
-        verify(metricRegistry).timer(any());
-        verify(mockedTimer).update(anyLong(), eq(TimeUnit.NANOSECONDS));
+        timelockService.startTransactions(startTransactionsRequest);
+        timelockService.startTransactions(startTransactionsRequest);
+        verify(mockedRegistry, atLeast(1)).timer(any());
+        verify(mockedTimer, times(2)).update(anyLong(), any());
+    }
+
+    @Test
+    public void leaderIsInitialisedForAllMethods() {
+        when(commitTimestampsResponse.getLockWatchUpdate())
+                .thenReturn(LockWatchStateUpdate.success(UUID.randomUUID(), 3L, ImmutableList.of()));
+        timelockService.getCommitTimestamps(commitTimestampsRequest);
+        timelockService.startTransactions(startTransactionsRequest);
+        verify(mockedRegistry).timer(any());
+        verify(mockedTimer).update(anyLong(), any());
     }
 }
