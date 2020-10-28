085ceb503114178256a6d2ee5714be00d439b290
Jolyon Shah <jshah@palantir.com>
2020-10-28 11:21:14 +0000

    revert

--- timelock-impl/src/main/java/com/palantir/atlasdb/timelock/adjudicate/LeaderElectionMetricAggregator.java
+++ timelock-impl/src/main/java/com/palantir/atlasdb/timelock/adjudicate/LeaderElectionMetricAggregator.java
@@ -19,6 +19,7 @@ package com.palantir.atlasdb.timelock.adjudicate;
 import com.palantir.atlasdb.util.CurrentValueMetric;
 import com.palantir.atlasdb.util.MetricsManager;
 import com.palantir.atlasdb.util.SlidingWindowWeightedMeanGauge;
+import com.palantir.conjure.java.lib.SafeLong;
 import com.palantir.timelock.feedback.LeaderElectionStatistics;
 
 public final class LeaderElectionMetricAggregator {
@@ -47,8 +48,7 @@ public final class LeaderElectionMetricAggregator {
         weightedGaugeMean.update(statistics.getMean(), count);
         weightedGaugeP95.update(statistics.getP95(), count);
         weightedGaugeP99.update(statistics.getP99(), count);
-        statistics
-                .getDurationEstimate()
-                .ifPresent(durationEstimate -> leaderElectionEstimate.setValue(durationEstimate));
+        leaderElectionEstimate.setValue(
+                statistics.getDurationEstimate().map(SafeLong::longValue).orElse(0L));
     }
 }
