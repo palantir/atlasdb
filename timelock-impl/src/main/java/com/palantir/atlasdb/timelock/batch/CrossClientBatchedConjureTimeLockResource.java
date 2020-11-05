fe4fd6918ce680826f8c20b64c236097aa876d81
Sudiksha Nanda <snanda@palantir.com>
2020-11-05 12:45:43 +0000

    getCommitTimestamps

--- timelock-impl/src/main/java/com/palantir/atlasdb/timelock/batch/CrossClientBatchedConjureTimeLockResource.java
+++ timelock-impl/src/main/java/com/palantir/atlasdb/timelock/batch/CrossClientBatchedConjureTimeLockResource.java
@@ -21,9 +21,14 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.palantir.atlasdb.timelock.AsyncTimelockService;
 import com.palantir.atlasdb.timelock.ConjureResourceExceptionHandler;
+import com.palantir.atlasdb.timelock.api.ConjureIdentifiedVersion;
+import com.palantir.atlasdb.timelock.api.GetCommitTimestampsResponse;
+import com.palantir.atlasdb.timelock.api.NamespacedGetCommitTimestampsRequest;
+import com.palantir.atlasdb.timelock.api.NamespacedGetCommitTimestampsResponse;
 import com.palantir.atlasdb.timelock.api.NamespacedLeaderTime;
 import com.palantir.atlasdb.timelock.batch.api.UndertowCrossClientBatchedConjureTimelockService;
 import com.palantir.lock.v2.LeaderTime;
+import com.palantir.lock.watch.LockWatchVersion;
 import com.palantir.tokens.auth.AuthHeader;
 import java.util.List;
 import java.util.function.Function;
@@ -42,24 +47,58 @@ public class CrossClientBatchedConjureTimeLockResource implements UndertowCrossC
 
     @Override
     public ListenableFuture<List<NamespacedLeaderTime>> leaderTimes(AuthHeader authHeader, List<String> namespaces) {
-        List<ListenableFuture<NamespacedLeaderTime>> futures =
-                namespaces.stream().map(namespace -> {
-                    ListenableFuture<LeaderTime> leaderTimeListenableFuture = forNamespace(namespace).leaderTime();
-                    return Futures.transform(leaderTimeListenableFuture,
-                            leaderTime -> NamespacedLeaderTime.of(namespace,
-                            leaderTime),
-                            MoreExecutors.directExecutor());
-                }).collect(Collectors.toList());
+        List<ListenableFuture<NamespacedLeaderTime>> futures = namespaces.stream()
+                .map(this::getNamespacedLeaderTimeListenableFuture)
+                .collect(Collectors.toList());
 
         // todo(snanda) failing the entire batch right now
         return handleExceptions(() -> Futures.allAsList(futures));
     }
 
+    @Override
+    public ListenableFuture<List<NamespacedGetCommitTimestampsResponse>> getCommitTimestamps(
+            AuthHeader authHeader, List<NamespacedGetCommitTimestampsRequest> requests) {
+        List<ListenableFuture<NamespacedGetCommitTimestampsResponse>> futures = requests.stream()
+                .map(this::getNamespacedGetCommitTimestampsResponseListenableFuture)
+                .collect(Collectors.toList());
+
+        return handleExceptions(() -> Futures.allAsList(futures));
+    }
+
+    private ListenableFuture<NamespacedLeaderTime> getNamespacedLeaderTimeListenableFuture(String namespace) {
+        ListenableFuture<LeaderTime> leaderTimeListenableFuture =
+                forNamespace(namespace).leaderTime();
+        return Futures.transform(
+                leaderTimeListenableFuture,
+                leaderTime -> NamespacedLeaderTime.of(namespace, leaderTime),
+                MoreExecutors.directExecutor());
+    }
+
+    private ListenableFuture<NamespacedGetCommitTimestampsResponse>
+            getNamespacedGetCommitTimestampsResponseListenableFuture(NamespacedGetCommitTimestampsRequest request) {
+        ListenableFuture<GetCommitTimestampsResponse> commitTimestamps = forNamespace(request.getNamespace())
+                .getCommitTimestamps(
+                        request.getGetCommitTimestampsRequest().getNumTimestamps(),
+                        request.getGetCommitTimestampsRequest()
+                                .getLastKnownVersion()
+                                .map(this::toIdentifiedVersion));
+        return Futures.transform(
+                commitTimestamps,
+                commitTimestampsResponse ->
+                        NamespacedGetCommitTimestampsResponse.of(request.getNamespace(), commitTimestampsResponse),
+                MoreExecutors.directExecutor());
+    }
+
+    // todo (snanda) refactor copy pasted code
     private AsyncTimelockService forNamespace(String namespace) {
         return timelockServices.apply(namespace);
     }
 
     private <T> ListenableFuture<T> handleExceptions(Supplier<ListenableFuture<T>> supplier) {
         return exceptionHandler.handleExceptions(supplier);
+    }
+
+    private LockWatchVersion toIdentifiedVersion(ConjureIdentifiedVersion conjureIdentifiedVersion) {
+        return LockWatchVersion.of(conjureIdentifiedVersion.getId(), conjureIdentifiedVersion.getVersion());
     }
 }
