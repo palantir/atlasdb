70772abc9e8c58f63e78c6f9889825a6f41955f1
Sudiksha Nanda <snanda@palantir.com>
2020-11-11 16:34:29 +0000

    Fix serialization bug

--- timelock-agent/src/main/java/com/palantir/timelock/config/CorruptionCheckConfig.java
+++ timelock-agent/src/main/java/com/palantir/timelock/config/CorruptionCheckConfig.java
@@ -16,9 +16,13 @@
 
 package com.palantir.timelock.config;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 @Value.Immutable
+@JsonSerialize(as = ImmutableCorruptionCheckConfig.class)
+@JsonDeserialize(as = ImmutableCorruptionCheckConfig.class)
 public interface CorruptionCheckConfig {
     long timelockCorruptionAnalysisIntervalSeconds();
 
