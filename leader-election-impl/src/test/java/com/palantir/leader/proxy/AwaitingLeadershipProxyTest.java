80143d970b7a6174ea305f42e771c016d99d2b64
Sudiksha Nanda <snanda@palantir.com>
2020-12-09 11:08:48 +0000

    Fix test

--- leader-election-impl/src/test/java/com/palantir/leader/proxy/AwaitingLeadershipProxyTest.java
+++ leader-election-impl/src/test/java/com/palantir/leader/proxy/AwaitingLeadershipProxyTest.java
@@ -60,6 +60,7 @@ public class AwaitingLeadershipProxyTest {
     private final LeaderElectionService leaderElectionService = mock(LeaderElectionService.class);
     private final Runnable mockRunnable = mock(Runnable.class);
     private final Supplier<Runnable> delegateSupplier = Suppliers.ofInstance(mockRunnable);
+    private final AwaitingLeadership awaitingLeadership = AwaitingLeadership.create(leaderElectionService);
 
     @Rule
     public final ExpectedException expect = ExpectedException.none();
@@ -298,7 +299,7 @@ public class AwaitingLeadershipProxyTest {
     }
 
     private AwaitingLeadership getAwaitingLeadership() {
-        return AwaitingLeadership.create(leaderElectionService);
+        return awaitingLeadership;
     }
 
     private void waitForLeadershipToBeGained() throws InterruptedException {
