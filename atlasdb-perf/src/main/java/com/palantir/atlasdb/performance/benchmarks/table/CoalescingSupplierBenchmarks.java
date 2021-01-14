92885ea11f01916188faebf5354094cf010a0e6e
Sudiksha Nanda <snanda@palantir.com>
2021-01-14 16:23:15 +0000

    coalescingSupplier

---
+++ atlasdb-perf/src/main/java/com/palantir/atlasdb/performance/benchmarks/table/CoalescingSupplierBenchmarks.java
@@ -0,0 +1,57 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.performance.benchmarks.table;
+
+import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.Futures;
+import com.palantir.atlasdb.futures.AtlasFutures;
+import com.palantir.common.concurrent.CoalescingSupplier;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Benchmark)
+public class CoalescingSupplierBenchmarks {
+    private static final Map<Integer, CoalescingSupplier<Integer>> map = Maps.newConcurrentMap();
+    // Atomic integer containing the next thread ID to be assigned
+    private static final AtomicInteger nextId = new AtomicInteger(0);
+
+    // Thread local variable containing each thread's ID
+    private static final ThreadLocal<Integer> threadId =
+            new ThreadLocal<Integer>() {
+                @Override protected Integer initialValue() {
+                    return nextId.getAndIncrement();
+                }
+            };
+
+    @Benchmark
+    @Threads(256)
+    @Warmup(time = 2)
+    @Measurement(time = 6)
+    public int coalescing() {
+        return AtlasFutures.getUnchecked(Futures.immediateFuture(getTimer(threadId.get()).get()));
+    }
+
+    private CoalescingSupplier<Integer> getTimer(Integer id) {
+        return map.computeIfAbsent(id, u -> new CoalescingSupplier(() -> 1));
+    }
+}
