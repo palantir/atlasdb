873338fad6cd28e9e31f31a28765706522fc67b1
Jeremy Kong <jeremykong@hotmail.com>
2021-01-18 14:27:52 +0000

    beef up tests

--- lock-api/src/test/java/com/palantir/lock/client/BroadsideLeaderPollerTest.java
+++ lock-api/src/test/java/com/palantir/lock/client/BroadsideLeaderPollerTest.java
@@ -27,21 +27,27 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
 import com.google.common.util.concurrent.Futures;
 import com.palantir.atlasdb.timelock.api.LeaderTimes;
 import com.palantir.atlasdb.timelock.api.Namespace;
 import com.palantir.common.concurrent.PTExecutors;
+import com.palantir.common.streams.KeyedStream;
 import com.palantir.common.time.NanoTime;
 import com.palantir.lock.v2.LeaderTime;
 import com.palantir.lock.v2.LeadershipId;
 import com.palantir.logsafe.exceptions.SafeIllegalStateException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.Test;
 
-@SuppressWarnings("unchecked") // Mocked generics
 public class BroadsideLeaderPollerTest {
     private static final Namespace NAMESPACE_1 = Namespace.of("tom");
     private static final Namespace NAMESPACE_2 = Namespace.of("jeremy");
@@ -72,7 +78,7 @@ public class BroadsideLeaderPollerTest {
     }
 
     @Test
-    public void buildsRequests() {
+    public void constructsUnionOfRequestHistory() {
         when(authenticatedService.leaderTimes(any()))
                 .thenReturn(LeaderTimes.builder()
                         .leaderTimes(NAMESPACE_1, LEADER_TIME_1)
@@ -102,6 +108,19 @@ public class BroadsideLeaderPollerTest {
     }
 
     @Test
+    public void recoversIfResponseAddsNamespaceOnLaterAttempt() {
+        when(authenticatedService.leaderTimes(any()))
+                .thenReturn(LeaderTimes.builder()
+                        .leaderTimes(NAMESPACE_3, LEADER_TIME_3)
+                        .build())
+                .thenReturn(LeaderTimes.builder()
+                        .leaderTimes(NAMESPACE_1, LEADER_TIME_1)
+                        .build());
+
+        assertThat(serviceBackedPoller.get(NAMESPACE_1)).isEqualTo(LEADER_TIME_1);
+    }
+
+    @Test
     public void simulation() {
         when(authenticatedService.leaderTimes(any()))
                 .thenReturn(LeaderTimes.builder()
@@ -119,6 +138,37 @@ public class BroadsideLeaderPollerTest {
         assertThat(mockingDetails(authenticatedService).getInvocations().size())
                 .as("some requests were autobatched")
                 .isLessThan(512)
+                .isGreaterThanOrEqualTo(1);
+    }
+
+    @Test
+    public void multiClientSimulation() {
+        Map<Namespace, LeaderTime> canonicalTimes = KeyedStream.of(
+                IntStream.range(0, 256).boxed().collect(Collectors.toList()))
+                .mapKeys(index -> Namespace.of("n" + index))
+                .map(unused -> LeaderTime.of(LeadershipId.random(), NanoTime.now()))
+                .collectToMap();
+
+        when(authenticatedService.leaderTimes(any())).thenAnswer(invocationOnMock -> {
+            Set<Namespace> namespaces = invocationOnMock.getArgument(0);
+            return LeaderTimes.of(KeyedStream.of(namespaces.stream())
+                    .map(canonicalTimes::get)
+                    .collectToMap());
+        });
+
+        ExecutorService executorService = PTExecutors.newFixedThreadPool(16);
+        Multimap<Namespace, Future<LeaderTime>> leaderTimeFutures = MultimapBuilder.hashKeys().arrayListValues().build();
+        for (int request = 0; request < 1024; request++) {
+            Namespace namespace = Namespace.of("n" + (request % 256));
+            leaderTimeFutures.put(namespace, executorService.submit(() -> serviceBackedPoller.get(namespace)));
+        }
+
+        leaderTimeFutures.forEach(
+                (namespace, future) -> assertThat(Futures.getUnchecked(future)).isEqualTo(canonicalTimes.get(namespace)));
+
+        assertThat(mockingDetails(authenticatedService).getInvocations().size())
+                .as("some requests were autobatched")
+                .isLessThan(1024)
                 .isGreaterThanOrEqualTo(1);
     }
 }
