d212028b1533c2fa9f1eeabb709288766b485152
Jolyon Shah <jshah@palantir.com>
2021-01-25 10:25:21 +0000

    don't use object equals for primitives

--- lock-api/src/main/java/com/palantir/lock/LockServerOptions.java
+++ lock-api/src/main/java/com/palantir/lock/LockServerOptions.java
@@ -129,15 +129,15 @@ public class LockServerOptions implements Serializable {
             return false;
         }
         LockServerOptions other = (LockServerOptions) obj;
-        return Objects.equals(isStandaloneServer(), other.isStandaloneServer())
+        return isStandaloneServer() == other.isStandaloneServer()
+                && slowLogTriggerMillis() == other.slowLogTriggerMillis()
+                && getRandomBitCount() == other.getRandomBitCount()
                 && Objects.equals(getMaxAllowedLockTimeout(), other.getMaxAllowedLockTimeout())
                 && Objects.equals(getMaxAllowedClockDrift(), other.getMaxAllowedClockDrift())
                 && Objects.equals(getMaxAllowedBlockingDuration(), other.getMaxAllowedBlockingDuration())
                 && Objects.equals(getMaxNormalLockAge(), other.getMaxNormalLockAge())
-                && Objects.equals(getRandomBitCount(), other.getRandomBitCount())
                 && Objects.equals(getStuckTransactionTimeout(), other.getStuckTransactionTimeout())
-                && Objects.equals(getLockStateLoggerDir(), other.getLockStateLoggerDir())
-                && Objects.equals(slowLogTriggerMillis(), other.slowLogTriggerMillis());
+                && Objects.equals(getLockStateLoggerDir(), other.getLockStateLoggerDir());
     }
 
     @Override
