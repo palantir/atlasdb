5afffb03cb1247d7b03c02957f7b9bb4eb0210b1
sudiksha nanda <58551710+sudiksha27@users.noreply.github.com>
2021-02-18 11:51:46 +0000

    ALP Hotfix (#5268)

--- leader-election-impl/src/main/java/com/palantir/leader/proxy/LeadershipStateManager.java
+++ leader-election-impl/src/main/java/com/palantir/leader/proxy/LeadershipStateManager.java
@@ -160,6 +160,12 @@ public class LeadershipStateManager<T> {
     interface LeadershipState<T> {
         LeadershipToken leadershipToken();
 
+        // It is legal to have delegate set to null while the leadershipTokenRef has not been cleared i.e. when the
+        // proxy is closed.
+        //
+        // Making this nullable instead of optional as this object will be called by 1000s of threads and we want to
+        // avoid the cost of setting and getting the optional.
+        @Nullable
         T delegate();
     }
 }
