0b4339fe7301d342a5d43ad2361da4f7f227c937
Sudiksha Nanda <snanda@palantir.com>
2021-02-18 17:49:50 +0000

    WIP

--- lock-api/src/main/java/com/palantir/lock/client/MultiClientBatchingIdentifiedAtlasDbTransactionStarter.java
+++ lock-api/src/main/java/com/palantir/lock/client/MultiClientBatchingIdentifiedAtlasDbTransactionStarter.java
@@ -17,6 +17,7 @@
 package com.palantir.lock.client;
 
 import static com.palantir.lock.client.LockLeaseService.toConjure;
+import static java.util.stream.Collectors.toCollection;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
@@ -30,37 +31,35 @@ import com.palantir.atlasdb.timelock.api.Namespace;
 import com.palantir.common.streams.KeyedStream;
 import com.palantir.lock.v2.StartIdentifiedAtlasDbTransactionResponse;
 import com.palantir.lock.watch.LockWatchVersion;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Queue;
 import java.util.UUID;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.immutables.value.Value;
 
 public class MultiClientBatchingIdentifiedAtlasDbTransactionStarter implements AutoCloseable {
     private final DisruptorAutobatcher<
-                    Map.Entry<Namespace, StartTransactionsRequestParams>,
-                    List<StartIdentifiedAtlasDbTransactionResponse>>
+                    NamespacedStartTransactionsRequestParams, List<StartIdentifiedAtlasDbTransactionResponse>>
             autobatcher;
 
     public MultiClientBatchingIdentifiedAtlasDbTransactionStarter(
             DisruptorAutobatcher<
-                            Map.Entry<Namespace, StartTransactionsRequestParams>,
-                            List<StartIdentifiedAtlasDbTransactionResponse>>
+                            NamespacedStartTransactionsRequestParams, List<StartIdentifiedAtlasDbTransactionResponse>>
                     autobatcher) {
         this.autobatcher = autobatcher;
     }
 
     static MultiClientBatchingIdentifiedAtlasDbTransactionStarter create(
             InternalMultiClientConjureTimelockService delegate) {
-        DisruptorAutobatcher<
-                        Map.Entry<Namespace, StartTransactionsRequestParams>,
-                        List<StartIdentifiedAtlasDbTransactionResponse>>
+        DisruptorAutobatcher<NamespacedStartTransactionsRequestParams, List<StartIdentifiedAtlasDbTransactionResponse>>
                 autobatcher = Autobatchers.independent(consumer(delegate, UUID.randomUUID()))
                         .safeLoggablePurpose("multi-client-transaction-starter")
                         .build();
@@ -69,66 +68,122 @@ public class MultiClientBatchingIdentifiedAtlasDbTransactionStarter implements A
 
     public List<StartIdentifiedAtlasDbTransactionResponse> startTransactions(
             Namespace namespace, int request, Supplier<Optional<LockWatchVersion>> lockWatchVersionSuppplier) {
-        return AtlasFutures.getUnchecked(autobatcher.apply(
-                Maps.immutableEntry(namespace, StartTransactionsRequestParams.of(request, lockWatchVersionSuppplier))));
+        return AtlasFutures.getUnchecked(autobatcher.apply(NamespacedStartTransactionsRequestParams.of(
+                namespace, StartTransactionsRequestParams.of(request, lockWatchVersionSuppplier))));
     }
 
     private static Consumer<
                     List<
                             BatchElement<
-                                    Map.Entry<Namespace, StartTransactionsRequestParams>,
+                                    NamespacedStartTransactionsRequestParams,
                                     List<StartIdentifiedAtlasDbTransactionResponse>>>>
             consumer(InternalMultiClientConjureTimelockService delegate, UUID requestorId) {
-        return batch -> serviceAllRequests(delegate, requestorId, batch);
+        return batch -> {
+
+            // todo - do the following two in one iteration on the batch
+            // Map of namespace to pending requests
+
+            Map<
+                            Namespace,
+                            Queue<
+                                    BatchElement<
+                                            NamespacedStartTransactionsRequestParams,
+                                            List<StartIdentifiedAtlasDbTransactionResponse>>>>
+                    namespaceWisePendingRequests = batch.stream()
+                            .collect(Collectors.groupingBy(
+                                    elem -> elem.argument().namespace(), toCollection(LinkedList::new)));
+
+            // Map of namespace to pending requests params
+            Map<Namespace, StartTransactionsRequestParams> namespaceWiseRequestParams =
+                    getNamespaceWiseRequestParams(batch);
+
+            Map<Namespace, List<StartIdentifiedAtlasDbTransactionResponse>> transientResult = new HashMap<>();
+            Map<Namespace, Integer> progressTracker = new HashMap<>();
+
+            while (!namespaceWiseRequestParams.isEmpty()) {
+                Map<Namespace, List<StartIdentifiedAtlasDbTransactionResponse>> startTransactionResponses =
+                        getStartTransactionResponses(namespaceWiseRequestParams, delegate, requestorId);
+                // update pending request state
+                namespaceWiseRequestParams =
+                        getUpdatedPendingRequestsMap(namespaceWiseRequestParams, startTransactionResponses);
+
+                // responses received
+                transientResult = getAllResponses(transientResult, startTransactionResponses);
+
+                // set futures where can
+                KeyedStream.stream(transientResult).forEach((namespace, responseList) -> {
+                    Queue<
+                                    BatchElement<
+                                            NamespacedStartTransactionsRequestParams,
+                                            List<StartIdentifiedAtlasDbTransactionResponse>>>
+                            pendings = namespaceWisePendingRequests.get(namespace);
+
+                    int start = progressTracker.getOrDefault(namespace, 0);
+                    int end = start;
+                    while (!pendings.isEmpty()) {
+                        int numRequired = pendings.peek().argument().params().numTransactions();
+                        if (start + numRequired > responseList.size()) {
+                            break;
+                        }
+                        end = start + numRequired; // // Todo Ahh clean this
+                        BatchElement<
+                                        NamespacedStartTransactionsRequestParams,
+                                        List<StartIdentifiedAtlasDbTransactionResponse>>
+                                batchElement = pendings.poll();
+                        batchElement.result().set(ImmutableList.copyOf(responseList.subList(start, end)));
+                    }
+                    progressTracker.put(namespace, end);
+                    // transientResult.put(namespace, responseList.subList(end, responseList.size())); // Todo is this
+                    // allowed
+                });
+            }
+        };
     }
 
-    private static void serviceAllRequests(
-            InternalMultiClientConjureTimelockService delegate,
-            UUID requestorId,
+    private static Map<Namespace, List<StartIdentifiedAtlasDbTransactionResponse>> getAllResponses(
+            Map<Namespace, List<StartIdentifiedAtlasDbTransactionResponse>> transientResult,
+            Map<Namespace, List<StartIdentifiedAtlasDbTransactionResponse>> startTransactionResponses) {
+        return Stream.concat(transientResult.entrySet().stream(), startTransactionResponses.entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (l1, l2) -> {
+                    if (l1.size() > l2.size()) {
+                        l1.addAll(l2);
+                        return l1;
+                    }
+                    l2.addAll(l1);
+                    return l2;
+                }));
+    }
+
+    private static Map<Namespace, StartTransactionsRequestParams> getNamespaceWiseRequestParams(
             List<
                             BatchElement<
-                                    Entry<Namespace, StartTransactionsRequestParams>,
+                                    NamespacedStartTransactionsRequestParams,
                                     List<StartIdentifiedAtlasDbTransactionResponse>>>
                     batch) {
+        return batch.stream()
+                .map(batchElement -> batchElement.argument())
+                .collect(Collectors.toMap(
+                        NamespacedStartTransactionsRequestParams::namespace,
+                        NamespacedStartTransactionsRequestParams::params,
+                        StartTransactionsRequestParams::coalesce));
+    }
 
-        List<
-                        BatchElement<
-                                Entry<Namespace, StartTransactionsRequestParams>,
-                                List<StartIdentifiedAtlasDbTransactionResponse>>>
-                pendingRequests = new ArrayList<>(batch);
-
-        while (!pendingRequests.isEmpty()) {
-            Map<Namespace, StartTransactionsRequestParams> namespaceWiseRequestParams = pendingRequests.stream()
-                    .map(batchElement -> batchElement.argument())
-                    .collect(
-                            Collectors.toMap(Entry::getKey, Entry::getValue, StartTransactionsRequestParams::coalesce));
-
-            Map<Namespace, List<StartIdentifiedAtlasDbTransactionResponse>> result =
-                    getStartTransactionResponses(namespaceWiseRequestParams, delegate, requestorId);
-
-            Map<Namespace, Integer> responseTracker = new HashMap<>();
-            for (BatchElement<
-                            Entry<Namespace, StartTransactionsRequestParams>,
-                            List<StartIdentifiedAtlasDbTransactionResponse>>
-                    batchElement : pendingRequests) {
-                Entry<Namespace, StartTransactionsRequestParams> argument = batchElement.argument();
-                Namespace namespace = argument.getKey();
-                int start = responseTracker.putIfAbsent(namespace, 0);
-
-                if (start > result.get(namespace).size()) {
-                    continue;
-                }
-
-                int end = start + argument.getValue().numTransactions();
-                batchElement
-                        .result()
-                        .set(ImmutableList.copyOf(result.get(namespace).subList(start, end)));
-                responseTracker.put(namespace, end);
-            }
-
-            pendingRequests =
-                    pendingRequests.stream().filter(x -> !x.result().isDone()).collect(Collectors.toList());
-        }
+    private static Map<Namespace, StartTransactionsRequestParams> getUpdatedPendingRequestsMap(
+            Map<Namespace, StartTransactionsRequestParams> namespaceWiseRequestParams,
+            Map<Namespace, List<StartIdentifiedAtlasDbTransactionResponse>> startTransactionResponses) {
+        return KeyedStream.stream(startTransactionResponses)
+                .map(List::size)
+                .mapEntries((namespace, size) -> {
+                    StartTransactionsRequestParams params = namespaceWiseRequestParams.get(namespace);
+                    int numTransactions = params.numTransactions();
+                    StartTransactionsRequestParams updatedParams = size < numTransactions
+                            ? StartTransactionsRequestParams.of(
+                                    numTransactions - size, params.lockWatchVersionSupplier())
+                            : null;
+                    return Maps.immutableEntry(namespace, updatedParams);
+                })
+                .filter(Objects::nonNull)
+                .collectToMap();
     }
 
     private static Map<Namespace, List<StartIdentifiedAtlasDbTransactionResponse>> getStartTransactionResponses(
@@ -170,6 +225,19 @@ public class MultiClientBatchingIdentifiedAtlasDbTransactionStarter implements A
     @Override
     public void close() {
         autobatcher.close();
+    }
+
+    @Value.Immutable
+    interface NamespacedStartTransactionsRequestParams {
+        @Value.Parameter
+        Namespace namespace();
+
+        @Value.Parameter
+        StartTransactionsRequestParams params();
+
+        static NamespacedStartTransactionsRequestParams of(Namespace namespace, StartTransactionsRequestParams params) {
+            return ImmutableNamespacedStartTransactionsRequestParams.of(namespace, params);
+        }
     }
 
     @Value.Immutable
