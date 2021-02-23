930da6419efb17cc8ecef734df7e749788e8aeec
sudiksha nanda <58551710+sudiksha27@users.noreply.github.com>
2021-02-23 13:23:04 +0000

    [CCB] | MultiClientTransactionStarter refactor - part 1 (#5271)
    * Extract interfact for TransactionStarter
    
    * Refactor + fix build
    
    * Move utility methods
    
    * Address comments

---
+++ lock-api/src/main/java/com/palantir/lock/client/BatchingIdentifiedAtlasDbTransactionStarter.java
@@ -0,0 +1,108 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.lock.client;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.palantir.atlasdb.autobatch.Autobatchers;
+import com.palantir.atlasdb.autobatch.BatchElement;
+import com.palantir.atlasdb.autobatch.DisruptorAutobatcher;
+import com.palantir.atlasdb.futures.AtlasFutures;
+import com.palantir.atlasdb.timelock.api.ConjureStartTransactionsResponse;
+import com.palantir.common.base.Throwables;
+import com.palantir.lock.v2.StartIdentifiedAtlasDbTransactionResponse;
+import com.palantir.lock.watch.LockWatchEventCache;
+import com.palantir.lock.watch.LockWatchVersion;
+import com.palantir.logsafe.Preconditions;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public final class BatchingIdentifiedAtlasDbTransactionStarter implements IdentifiedAtlasDbTransactionStarter {
+    private final DisruptorAutobatcher<Integer, List<StartIdentifiedAtlasDbTransactionResponse>> autobatcher;
+
+    private BatchingIdentifiedAtlasDbTransactionStarter(
+            DisruptorAutobatcher<Integer, List<StartIdentifiedAtlasDbTransactionResponse>> autobatcher) {
+        this.autobatcher = autobatcher;
+    }
+
+    static BatchingIdentifiedAtlasDbTransactionStarter create(
+            LockLeaseService lockLeaseService, LockWatchEventCache lockWatchEventCache) {
+        DisruptorAutobatcher<Integer, List<StartIdentifiedAtlasDbTransactionResponse>> autobatcher =
+                Autobatchers.independent(consumer(lockLeaseService, lockWatchEventCache))
+                        .safeLoggablePurpose("transaction-starter")
+                        .build();
+        return new BatchingIdentifiedAtlasDbTransactionStarter(autobatcher);
+    }
+
+    @Override
+    public List<StartIdentifiedAtlasDbTransactionResponse> startIdentifiedAtlasDbTransactionBatch(int count) {
+        Preconditions.checkArgument(count > 0, "Cannot start 0 or fewer transactions");
+        return AtlasFutures.getUnchecked(autobatcher.apply(count));
+    }
+
+    @VisibleForTesting
+    static Consumer<List<BatchElement<Integer, List<StartIdentifiedAtlasDbTransactionResponse>>>> consumer(
+            LockLeaseService lockLeaseService, LockWatchEventCache lockWatchEventCache) {
+        return batch -> {
+            int numTransactions =
+                    batch.stream().mapToInt(BatchElement::argument).sum();
+
+            List<StartIdentifiedAtlasDbTransactionResponse> startTransactionResponses =
+                    getStartTransactionResponses(lockLeaseService, lockWatchEventCache, numTransactions);
+
+            int start = 0;
+            for (BatchElement<Integer, List<StartIdentifiedAtlasDbTransactionResponse>> batchElement : batch) {
+                int end = start + batchElement.argument();
+                batchElement.result().set(ImmutableList.copyOf(startTransactionResponses.subList(start, end)));
+                start = end;
+            }
+        };
+    }
+
+    private static List<StartIdentifiedAtlasDbTransactionResponse> getStartTransactionResponses(
+            LockLeaseService lockLeaseService, LockWatchEventCache lockWatchEventCache, int numberOfTransactions) {
+        List<StartIdentifiedAtlasDbTransactionResponse> result = new ArrayList<>();
+        while (result.size() < numberOfTransactions) {
+            try {
+                Optional<LockWatchVersion> requestedVersion = lockWatchEventCache.lastKnownVersion();
+                ConjureStartTransactionsResponse response = lockLeaseService.startTransactionsWithWatches(
+                        requestedVersion, numberOfTransactions - result.size());
+                lockWatchEventCache.processStartTransactionsUpdate(
+                        response.getTimestamps().stream().boxed().collect(Collectors.toSet()),
+                        response.getLockWatchUpdate());
+                result.addAll(TransactionStarterHelper.split(response));
+                LockWatchLogUtility.logTransactionEvents(requestedVersion, response.getLockWatchUpdate());
+            } catch (Throwable t) {
+                TransactionStarterHelper.unlock(
+                        result.stream()
+                                .map(response -> response.immutableTimestamp().getLock())
+                                .collect(Collectors.toSet()),
+                        lockLeaseService);
+                throw Throwables.throwUncheckedException(t);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void close() {
+        autobatcher.close();
+    }
+}
---
+++ lock-api/src/main/java/com/palantir/lock/client/TransactionStarterHelper.java
@@ -0,0 +1,114 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.lock.client;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.google.common.collect.Streams;
+import com.palantir.atlasdb.timelock.api.ConjureStartTransactionsResponse;
+import com.palantir.lock.v2.LockImmutableTimestampResponse;
+import com.palantir.lock.v2.LockToken;
+import com.palantir.lock.v2.PartitionedTimestamps;
+import com.palantir.lock.v2.StartIdentifiedAtlasDbTransactionResponse;
+import com.palantir.lock.v2.TimestampAndPartition;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class TransactionStarterHelper {
+    private TransactionStarterHelper() {
+        // Do not instantiate helper class
+    }
+
+    static Set<LockToken> unlock(Set<LockToken> tokens, LockLeaseService lockLeaseService) {
+        Set<LockToken> lockTokens = filterOutTokenShares(tokens);
+
+        Set<LockTokenShare> lockTokenShares = filterLockTokenShares(tokens);
+
+        Set<LockToken> toUnlock = reduceForUnlock(lockTokenShares);
+        Set<LockToken> toRefresh = getLockTokensToRefresh(lockTokenShares, toUnlock);
+
+        Set<LockToken> refreshed = lockLeaseService.refreshLockLeases(toRefresh);
+        Set<LockToken> unlocked = lockLeaseService.unlock(Sets.union(toUnlock, lockTokens));
+
+        Set<LockTokenShare> resultLockTokenShares = Sets.filter(
+                lockTokenShares,
+                t -> unlocked.contains(t.sharedLockToken()) || refreshed.contains(t.sharedLockToken()));
+        Set<LockToken> resultLockTokens = Sets.intersection(lockTokens, unlocked);
+
+        return ImmutableSet.copyOf(Sets.union(resultLockTokenShares, resultLockTokens));
+    }
+
+    static Set<LockTokenShare> filterLockTokenShares(Set<LockToken> tokens) {
+        return tokens.stream()
+                .filter(TransactionStarterHelper::isLockTokenShare)
+                .map(LockTokenShare.class::cast)
+                .collect(Collectors.toSet());
+    }
+
+    static Set<LockToken> filterOutTokenShares(Set<LockToken> tokens) {
+        return tokens.stream().filter(t -> !isLockTokenShare(t)).collect(Collectors.toSet());
+    }
+
+    /**
+     * Calling unlock on a set of LockTokenShares only calls unlock on shared token iff all references to shared token
+     * are unlocked.
+     *
+     * {@link com.palantir.lock.v2.TimelockService#unlock(Set)} has a guarantee that returned tokens were valid until
+     * calling unlock. To keep that guarantee, we need to check if LockTokenShares were valid (by calling refresh with
+     * referenced shared token) even if we don't unlock the underlying shared token.
+     */
+    private static Set<LockToken> getLockTokensToRefresh(
+            Set<LockTokenShare> lockTokenShares, Set<LockToken> sharedTokensToUnlock) {
+        return lockTokenShares.stream()
+                .map(LockTokenShare::sharedLockToken)
+                .filter(token -> !sharedTokensToUnlock.contains(token))
+                .collect(Collectors.toSet());
+    }
+
+    private static Set<LockToken> reduceForUnlock(Set<LockTokenShare> lockTokenShares) {
+        return lockTokenShares.stream()
+                .map(LockTokenShare::unlock)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toSet());
+    }
+
+    private static boolean isLockTokenShare(LockToken lockToken) {
+        return lockToken instanceof LockTokenShare;
+    }
+
+    static List<StartIdentifiedAtlasDbTransactionResponse> split(ConjureStartTransactionsResponse response) {
+        PartitionedTimestamps partitionedTimestamps = response.getTimestamps();
+        int partition = partitionedTimestamps.partition();
+
+        LockToken immutableTsLock = response.getImmutableTimestamp().getLock();
+        long immutableTs = response.getImmutableTimestamp().getImmutableTimestamp();
+
+        Stream<LockImmutableTimestampResponse> immutableTsAndLocks = LockTokenShare.share(
+                        immutableTsLock, partitionedTimestamps.count())
+                .map(tokenShare -> LockImmutableTimestampResponse.of(immutableTs, tokenShare));
+
+        Stream<TimestampAndPartition> timestampAndPartitions =
+                partitionedTimestamps.stream().mapToObj(timestamp -> TimestampAndPartition.of(timestamp, partition));
+
+        return Streams.zip(immutableTsAndLocks, timestampAndPartitions, StartIdentifiedAtlasDbTransactionResponse::of)
+                .collect(Collectors.toList());
+    }
+}
