310e8b337b97cd5dd0ea91bc74f01c0c2695e213
Grgur Petric Maretic <grgapm@gmail.com>
2021-03-22 17:37:01 +0000

    I know what I am doing

--- atlasdb-console/src/main/groovy/com/palantir/atlasdb/console/module/Table.groovy
+++ atlasdb-console/src/main/groovy/com/palantir/atlasdb/console/module/Table.groovy
@@ -270,7 +270,7 @@ class Table {
         }
         else {
             data = entryList.collect { entry ->
-                def result = [row: convertToListIfMapConvertValuesToList(entry['row'])]
+                Map<String, List> result = [row: convertToListIfMapConvertValuesToList(entry['row'])]
                 Map cols = entry['cols'] as Map<String, List>
                 Set colNames = this.columnNames().toSet()
                 cols.each { key, value ->
