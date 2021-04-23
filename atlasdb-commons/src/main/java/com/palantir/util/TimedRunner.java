d4c80ff92d7f26033b2689ad23f7dd569a0f1c8c
Jolyon Shah <jshah@palantir.com>
2021-04-23 11:42:27 +0100

    majestic horn

---
+++ atlasdb-commons/src/main/java/com/palantir/util/TimedRunner.java
@@ -0,0 +1,102 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.util;
+
+import com.palantir.common.concurrent.PTExecutors;
+import com.palantir.logsafe.exceptions.SafeRuntimeException;
+import java.time.Duration;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.immutables.value.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class TimedRunner {
+    private static final Logger log = LoggerFactory.getLogger(SafeShutdownRunner.class);
+
+    private final ExecutorService executor;
+    private final Duration timeoutDuration;
+
+    private TimedRunner(ExecutorService executor, Duration timeoutDuration) {
+        this.executor = executor;
+        this.timeoutDuration = timeoutDuration;
+    }
+
+    public static TimedRunner create(Duration timeoutDuration) {
+        return new TimedRunner(PTExecutors.newSingleThreadExecutor(), timeoutDuration);
+    }
+
+    public <T> T run(TaskContext<T> taskContext) {
+        Future<T> future = executor.submit(() -> taskContext.task().call());
+        final Throwable failure;
+        try {
+            return future.get(timeoutDuration.toMillis(), TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            failure = e;
+        } catch (ExecutionException e) {
+            failure = e.getCause();
+        } catch (TimeoutException e) {
+            future.cancel(true);
+            failure = e;
+        }
+
+        SafeRuntimeException exception = new SafeRuntimeException();
+        exception.addSuppressed(failure);
+        try {
+            taskContext.taskFailureHandler().run();
+        } catch (Throwable t) {
+            log.warn("Shutdown failure handler threw an exception itself!", t);
+            exception.addSuppressed(t);
+        }
+        throw exception;
+    }
+
+    @Value.Immutable
+    public interface TaskContext<T> {
+        /**
+         * Executed to shut down an object.
+         */
+        Callable<T> task();
+
+        /**
+         * Executed only if the shutdownCallback times out or otherwise has an exception.
+         */
+        Runnable taskFailureHandler();
+
+        static <T> TaskContext<T> create(Callable<T> task, Runnable taskFailureHandler) {
+            return ImmutableTaskContext.<T>builder()
+                    .task(task)
+                    .taskFailureHandler(taskFailureHandler)
+                    .build();
+        }
+
+        static TaskContext<Void> createRunnable(Runnable task, Runnable taskFailureHandler) {
+            return ImmutableTaskContext.<Void>builder()
+                    .task(() -> {
+                        task.run();
+                        return null;
+                    })
+                    .taskFailureHandler(taskFailureHandler)
+                    .build();
+        }
+    }
+}
---
+++ atlasdb-commons/src/test/java/com/palantir/util/TimedRunnerTest.java
@@ -0,0 +1,82 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.palantir.logsafe.exceptions.SafeRuntimeException;
+import com.palantir.util.TimedRunner.TaskContext;
+import java.time.Duration;
+import java.util.concurrent.Callable;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class TimedRunnerTest {
+    private static final RuntimeException EXCEPTION_1 = new RuntimeException("test");
+    private static final RuntimeException EXCEPTION_2 = new RuntimeException("bleh");
+
+    private Runnable noOpRunnable = mock(Runnable.class);
+    private Runnable throwingRunnable = mock(Runnable.class);
+
+    @Before
+    public void setupMocks() {
+        doThrow(EXCEPTION_1).when(throwingRunnable).run();
+    }
+
+    @Test
+    public void runnerRunsOneTask() throws Exception {
+        TimedRunner runner = TimedRunner.create(Duration.ofSeconds(1));
+
+        Callable<Integer> task = mock(Callable.class);
+        when(task.call()).thenReturn(5);
+        int result = runner.run(TaskContext.create(task, noOpRunnable));
+
+        assertThat(result).isEqualTo(5);
+        verify(task).call();
+        verify(noOpRunnable, never()).run();
+    }
+
+    @Test
+    public void exceptionsAreThrownWhenRunningSingleton() {
+        TimedRunner runner = TimedRunner.create(Duration.ofSeconds(1));
+        Runnable failureHandler = mock(Runnable.class);
+
+        assertThatThrownBy(() -> runner.run(TaskContext.createRunnable(throwingRunnable, failureHandler)))
+                .isInstanceOf(SafeRuntimeException.class)
+                .hasSuppressedException(EXCEPTION_1);
+        verify(failureHandler).run();
+    }
+
+    @Test
+    public void correctSuppressedExceptionsAreThrownIfFailureHandlerThrows() {
+        TimedRunner runner = TimedRunner.create(Duration.ofSeconds(1));
+        Runnable failureHandler = mock(Runnable.class);
+        doThrow(EXCEPTION_2).when(failureHandler).run();
+
+        assertThatThrownBy(() -> runner.run(TaskContext.createRunnable(throwingRunnable, failureHandler)))
+                .isInstanceOf(SafeRuntimeException.class)
+                .hasSuppressedException(EXCEPTION_1)
+                .hasSuppressedException(EXCEPTION_2);
+        verify(failureHandler).run();
+    }
+}
