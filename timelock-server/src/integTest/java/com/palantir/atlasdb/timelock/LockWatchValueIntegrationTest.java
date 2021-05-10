34ddc04c0084fb9d46d4caf36aacb2b2d2761916
Jolyon Shah <jshah@palantir.com>
2021-05-10 14:30:56 +0100

    MORE TESTING

--- timelock-server/src/integTest/java/com/palantir/atlasdb/timelock/LockWatchValueIntegrationTest.java
+++ timelock-server/src/integTest/java/com/palantir/atlasdb/timelock/LockWatchValueIntegrationTest.java
@@ -18,6 +18,7 @@ package com.palantir.atlasdb.timelock;
 
 import static com.palantir.atlasdb.timelock.AbstractAsyncTimelockServiceIntegrationTest.DEFAULT_SINGLE_SERVER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -38,6 +39,7 @@ import com.palantir.atlasdb.table.description.ValueType;
 import com.palantir.atlasdb.transaction.api.ConflictHandler;
 import com.palantir.atlasdb.transaction.api.Transaction;
 import com.palantir.atlasdb.transaction.api.TransactionManager;
+import com.palantir.atlasdb.transaction.api.TransactionSerializableConflictException;
 import com.palantir.lock.watch.LockWatchVersion;
 import java.time.Duration;
 import java.util.Map;
@@ -54,10 +56,13 @@ public final class LockWatchValueIntegrationTest {
     private static final String TEST_PACKAGE = "package";
     private static final byte[] DATA_1 = "foo".getBytes();
     private static final byte[] DATA_2 = "Caecilius est in horto".getBytes();
+    private static final byte[] DATA_3 = "canis est in via".getBytes();
     private static final Cell CELL_1 = Cell.create("bar".getBytes(), "baz".getBytes());
     private static final Cell CELL_2 = Cell.create("eggs".getBytes(), "spam".getBytes());
     private static final String TABLE = "table";
+    private static final String TABLE_2 = "vesuvius";
     private static final TableReference TABLE_REF = TableReference.create(Namespace.DEFAULT_NAMESPACE, TABLE);
+    private static final TableReference TABLE_REF_2 = TableReference.create(Namespace.DEFAULT_NAMESPACE, TABLE);
     private static final CellReference TABLE_CELL_1 = CellReference.of(TABLE_REF, CELL_1);
     private static final CellReference TABLE_CELL_2 = CellReference.of(TABLE_REF, CELL_2);
     private static final TestableTimelockCluster CLUSTER =
@@ -70,20 +75,14 @@ public final class LockWatchValueIntegrationTest {
 
     @Before
     public void before() {
-        Schema schema = createSchema();
-        txnManager = createTransactionManager(schema, 0.0);
+        txnManager = createTransactionManager(0.0);
     }
 
     @Test
     public void effectivelyReadOnlyTransactionsPublishValuesToCentralCache() {
-        txnManager.runTaskWithRetry(txn -> {
-            txn.put(TABLE_REF, ImmutableMap.of(CELL_1, DATA_1));
-            return null;
-        });
+        putValue();
 
-        awaitUnlock();
-
-        Map<Cell, byte[]> result = txnManager.runTaskWithRetry(txn -> {
+        Map<Cell, byte[]> result = txnManager.runTaskThrowOnConflict(txn -> {
             Map<Cell, byte[]> values = txn.get(TABLE_REF, ImmutableSet.of(CELL_1, CELL_2));
             assertHitValues(txn, ImmutableSet.of());
             assertLoadedValues(
@@ -96,7 +95,7 @@ public final class LockWatchValueIntegrationTest {
 
         disableTable();
 
-        Map<Cell, byte[]> result2 = txnManager.runTaskWithRetry(txn -> {
+        Map<Cell, byte[]> result2 = txnManager.runTaskThrowOnConflict(txn -> {
             Map<Cell, byte[]> values = txn.get(TABLE_REF, ImmutableSet.of(CELL_1, CELL_2));
             assertHitValues(txn, ImmutableSet.of(TABLE_CELL_1, TABLE_CELL_2));
             assertLoadedValues(txn, ImmutableMap.of());
@@ -109,14 +108,9 @@ public final class LockWatchValueIntegrationTest {
 
     @Test
     public void readWriteTransactionsPublishValuesToCentralCache() {
-        txnManager.runTaskWithRetry(txn -> {
-            txn.put(TABLE_REF, ImmutableMap.of(CELL_1, DATA_1));
-            return null;
-        });
+        putValue();
 
-        awaitUnlock();
-
-        Map<Cell, byte[]> result = txnManager.runTaskWithRetry(txn -> {
+        Map<Cell, byte[]> result = txnManager.runTaskThrowOnConflict(txn -> {
             txn.put(TABLE_REF, ImmutableMap.of(CELL_2, DATA_2));
             Map<Cell, byte[]> values = txn.get(TABLE_REF, ImmutableSet.of(CELL_1, CELL_2));
             assertHitValues(txn, ImmutableSet.of());
@@ -126,7 +120,7 @@ public final class LockWatchValueIntegrationTest {
 
         awaitUnlock();
 
-        Map<Cell, byte[]> result2 = txnManager.runTaskWithRetry(txn -> {
+        Map<Cell, byte[]> result2 = txnManager.runTaskThrowOnConflict(txn -> {
             Map<Cell, byte[]> values = txn.get(TABLE_REF, ImmutableSet.of(CELL_1, CELL_2));
             assertHitValues(txn, ImmutableSet.of(TABLE_CELL_1));
             assertLoadedValues(txn, ImmutableMap.of(TABLE_CELL_2, CacheValue.of(DATA_2)));
@@ -136,6 +130,36 @@ public final class LockWatchValueIntegrationTest {
         assertThat(result).containsEntry(CELL_1, DATA_1);
         assertThat(result).containsEntry(CELL_2, DATA_2);
         assertThat(result).containsExactlyInAnyOrderEntriesOf(result2);
+    }
+
+    @Test
+    public void serializableTransactionsThrowOnCachedReadWriteConflicts() {
+        putValue();
+
+        // Nested transactions are normally discouraged, but it is easier to create a conflict this way (compared to
+        // using executors, which is much more heavy-handed).
+        assertThatThrownBy(() -> txnManager.runTaskThrowOnConflict(txn -> {
+                    txn.get(TABLE_REF, ImmutableSet.of(CELL_1));
+                    txn.put(TABLE_REF, ImmutableMap.of(CELL_2, DATA_2));
+
+                    txnManager.runTaskThrowOnConflict(txn2 -> {
+                        txn2.put(TABLE_REF, ImmutableMap.of(CELL_1, DATA_3));
+                        return null;
+                    });
+                    awaitUnlock();
+                    return null;
+                }))
+                .isExactlyInstanceOf(TransactionSerializableConflictException.class)
+                .hasMessageContaining("There was a read-write conflict on table");
+    }
+
+    private void putValue() {
+        txnManager.runTaskThrowOnConflict(txn -> {
+            txn.put(TABLE_REF, ImmutableMap.of(CELL_1, DATA_1));
+            return null;
+        });
+
+        awaitUnlock();
     }
 
     private void assertHitValues(Transaction transaction, Set<CellReference> expectedCells) {
@@ -196,7 +220,7 @@ public final class LockWatchValueIntegrationTest {
         return extractValueCache().getOrCreateTransactionScopedCache(txn.getTimestamp());
     }
 
-    private static TransactionManager createTransactionManager(Schema schema, double validationProbability) {
+    private static TransactionManager createTransactionManager(double validationProbability) {
         return TimeLockTestUtils.createTransactionManager(
                         CLUSTER,
                         UUID.randomUUID().toString(),
@@ -206,7 +230,7 @@ public final class LockWatchValueIntegrationTest {
                                         .validationProbability(validationProbability)
                                         .build()),
                         Optional.empty(),
-                        schema)
+                        createSchema())
                 .transactionManager();
     }
 
