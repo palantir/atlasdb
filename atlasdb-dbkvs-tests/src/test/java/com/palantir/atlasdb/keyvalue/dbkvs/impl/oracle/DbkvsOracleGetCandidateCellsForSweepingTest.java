a70fdf7251d5347912ae707e429db53edd5f2263
Jeremy Kong <jeremykong@hotmail.com>
2021-05-27 13:04:12 +0100

    fix

--- atlasdb-dbkvs-tests/src/test/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/oracle/DbkvsOracleGetCandidateCellsForSweepingTest.java
+++ atlasdb-dbkvs-tests/src/test/java/com/palantir/atlasdb/keyvalue/dbkvs/impl/oracle/DbkvsOracleGetCandidateCellsForSweepingTest.java
@@ -16,7 +16,6 @@
 
 package com.palantir.atlasdb.keyvalue.dbkvs.impl.oracle;
 
-import com.palantir.atlasdb.keyvalue.dbkvs.impl.oracle.DbKvsOracleTestSuite;
 import com.palantir.atlasdb.keyvalue.impl.AbstractGetCandidateCellsForSweepingTest;
 import com.palantir.atlasdb.keyvalue.impl.TestResourceManager;
 import org.junit.ClassRule;
