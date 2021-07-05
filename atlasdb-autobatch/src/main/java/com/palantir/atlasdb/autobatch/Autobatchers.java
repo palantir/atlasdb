a4dbea08fac5232d6e72904c6c8ee7daaa99109c
Jeremy Kong <jeremykong@hotmail.com>
2021-07-05 13:47:20 +0100

    autobatchers timeout

--- atlasdb-autobatch/src/main/java/com/palantir/atlasdb/autobatch/Autobatchers.java
+++ atlasdb-autobatch/src/main/java/com/palantir/atlasdb/autobatch/Autobatchers.java
@@ -17,21 +17,32 @@
 package com.palantir.atlasdb.autobatch;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.SimpleTimeLimiter;
+import com.google.common.util.concurrent.TimeLimiter;
 import com.google.errorprone.annotations.CompileTimeConstant;
 import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.WaitStrategy;
+import com.palantir.common.concurrent.PTExecutors;
 import com.palantir.logsafe.Preconditions;
+import com.palantir.logsafe.SafeArg;
 import com.palantir.tracing.Observability;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalInt;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import javax.annotation.Nullable;
+import org.immutables.value.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class Autobatchers {
+    private static final Logger log = LoggerFactory.getLogger(Autobatchers.class);
 
     private static final int DEFAULT_BUFFER_SIZE = 4096;
 
@@ -63,7 +74,8 @@ public final class Autobatchers {
      * @see CoalescingRequestConsumer
      */
     public static <I, O> AutobatcherBuilder<I, O> coalescing(CoalescingRequestFunction<I, O> function) {
-        return new AutobatcherBuilder<>(bufferSize -> new CoalescingBatchingEventHandler<>(function, bufferSize));
+        return new AutobatcherBuilder<>(parameters -> new CoalescingBatchingEventHandler<>(
+                maybeWrapWithTimeout(function, parameters), parameters.batchSize()));
     }
 
     public static <O> AutobatcherBuilder<SupplierKey, O> coalescing(Supplier<O> supplier) {
@@ -79,25 +91,89 @@ public final class Autobatchers {
      * the result for that input element
      * @param <I> type of input element
      * @param <O> type of output element
-     * @return builder where the autobatch can be further customised
+     * @return builder where the autobatcher can be further customised
      */
     public static <I, O> AutobatcherBuilder<I, O> independent(Consumer<List<BatchElement<I, O>>> batchFunction) {
-        return new AutobatcherBuilder<>(bufferSize -> new IndependentBatchingEventHandler<>(batchFunction, bufferSize));
+        return new AutobatcherBuilder<>(parameters -> new IndependentBatchingEventHandler<>(
+                maybeWrapWithTimeout(batchFunction, parameters), parameters.batchSize()));
+    }
+
+    private static <I, O> Consumer<List<BatchElement<I, O>>> maybeWrapWithTimeout(
+            Consumer<List<BatchElement<I, O>>> batchFunction, EventHandlerParameters parameters) {
+        return parameters
+                .batchFunctionTimeout()
+                .map(timeout -> wrapWithTimeout(batchFunction, parameters.safeLoggablePurpose(), timeout))
+                .orElse(batchFunction);
+    }
+
+    private static <I, O> CoalescingRequestFunction<I, O> maybeWrapWithTimeout(
+            CoalescingRequestFunction<I, O> coalescingFunction, EventHandlerParameters parameters) {
+        return parameters
+                .batchFunctionTimeout()
+                .map(timeout -> wrapWithTimeout(coalescingFunction, parameters.safeLoggablePurpose(), timeout))
+                .orElse(coalescingFunction);
+    }
+
+    private static <I, O> Consumer<List<BatchElement<I, O>>> wrapWithTimeout(
+            Consumer<List<BatchElement<I, O>>> delegate, String safeLoggablePurpose, Duration maxRuntime) {
+        TimeLimiter limiter = createTimeLimiter(safeLoggablePurpose);
+        return elements -> {
+            try {
+                limiter.runWithTimeout(() -> delegate.accept(elements), maxRuntime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (TimeoutException e) {
+                log.info(
+                        "Autobatcher timed out and request had to be interrupted",
+                        SafeArg.of("safeLoggablePurpose", safeLoggablePurpose),
+                        e);
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private static <I, O> CoalescingRequestFunction<I, O> wrapWithTimeout(
+            CoalescingRequestFunction<I, O> delegate, String safeLoggablePurpose, Duration maxRuntime) {
+        TimeLimiter limiter = createTimeLimiter(safeLoggablePurpose);
+        return elements -> {
+            try {
+                return limiter.callWithTimeout(() -> delegate.apply(elements), maxRuntime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (TimeoutException e) {
+                log.info(
+                        "Autobatcher timed out and request had to be interrupted",
+                        SafeArg.of("safeLoggablePurpose", safeLoggablePurpose),
+                        e);
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e.getCause());
+            }
+        };
+    }
+
+    private static TimeLimiter createTimeLimiter(String safeLoggablePurpose) {
+        TimeLimiter limiter = SimpleTimeLimiter.create(
+                PTExecutors.newCachedThreadPool("autobatcher." + safeLoggablePurpose + "-timeout"));
+        return limiter;
     }
 
     public static final class AutobatcherBuilder<I, O> {
 
-        private final Function<Integer, EventHandler<BatchElement<I, O>>> handlerFactory;
+        private final Function<EventHandlerParameters, EventHandler<BatchElement<I, O>>> handlerFactory;
         private final ImmutableMap.Builder<String, String> safeTags = ImmutableMap.builder();
 
         private Observability observability = Observability.UNDECIDED;
         private OptionalInt bufferSize = OptionalInt.empty();
+        private Optional<Duration> batchFunctionTimeout = Optional.empty();
         private Optional<WaitStrategy> waitStrategy = Optional.empty();
 
         @Nullable
         private String purpose;
 
-        private AutobatcherBuilder(Function<Integer, EventHandler<BatchElement<I, O>>> handlerFactory) {
+        private AutobatcherBuilder(Function<EventHandlerParameters, EventHandler<BatchElement<I, O>>> handlerFactory) {
             this.handlerFactory = handlerFactory;
         }
 
@@ -126,19 +202,40 @@ public final class Autobatchers {
             return this;
         }
 
+        public AutobatcherBuilder<I, O> batchFunctionTimeout(Duration duration) {
+            this.batchFunctionTimeout = Optional.of(duration);
+            return this;
+        }
+
         public DisruptorAutobatcher<I, O> build() {
             Preconditions.checkArgument(purpose != null, "purpose must be provided");
 
-            int bufferSizeValue = bufferSize.orElse(DEFAULT_BUFFER_SIZE);
+            ImmutableEventHandlerParameters.Builder parametersBuilder = ImmutableEventHandlerParameters.builder();
+            bufferSize.ifPresent(parametersBuilder::batchSize);
+            parametersBuilder.safeLoggablePurpose(purpose).batchFunctionTimeout(batchFunctionTimeout);
+            EventHandlerParameters parameters = parametersBuilder.build();
 
-            EventHandler<BatchElement<I, O>> handler = this.handlerFactory.apply(bufferSizeValue);
+            EventHandler<BatchElement<I, O>> handler = this.handlerFactory.apply(parameters);
 
-            EventHandler<BatchElement<I, O>> tracingHandler = new TracingEventHandler<>(handler, bufferSizeValue);
+            EventHandler<BatchElement<I, O>> tracingHandler =
+                    new TracingEventHandler<>(handler, parameters.batchSize());
 
             EventHandler<BatchElement<I, O>> profiledHandler =
                     new ProfilingEventHandler<>(tracingHandler, purpose, safeTags.build());
 
-            return DisruptorAutobatcher.create(profiledHandler, bufferSizeValue, purpose, waitStrategy);
+            return DisruptorAutobatcher.create(profiledHandler, parameters.batchSize(), purpose, waitStrategy);
         }
+    }
+
+    @Value.Immutable
+    interface EventHandlerParameters {
+        @Value.Default
+        default int batchSize() {
+            return DEFAULT_BUFFER_SIZE;
+        }
+
+        String safeLoggablePurpose();
+
+        Optional<Duration> batchFunctionTimeout();
     }
 }
