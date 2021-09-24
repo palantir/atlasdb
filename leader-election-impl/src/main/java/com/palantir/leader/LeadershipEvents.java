6000745d4239cf294a93c0c0e1bd8d31f3b4ff48
Jeremy Kong <jeremykong@hotmail.com>
2021-09-24 15:30:59 +0100

    fix

--- leader-election-impl/src/main/java/com/palantir/leader/LeadershipEvents.java
+++ leader-election-impl/src/main/java/com/palantir/leader/LeadershipEvents.java
@@ -31,6 +31,7 @@ import org.slf4j.LoggerFactory;
 class LeadershipEvents {
 
     private static final String LEADER_LOG_NAME = "leadership";
+    @SuppressWarnings("PreferSafeLogger") // Some refactoring required
     private static final Logger leaderLog = LoggerFactory.getLogger(LEADER_LOG_NAME);
     private final Object[] contextArgs;
     private final LeaderElectionServiceMetrics leaderElectionServiceMetrics;
