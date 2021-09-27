ba9eafa3801768f35e799b5e6c6c730bd0ea0f6a
Jolyon <jshah@palantir.com>
2021-09-27 10:27:24 +0100

    [LW] Add additional fail-safe mechanisms around stored state (#5658)

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/api/watch/TimestampStateStore.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/api/watch/TimestampStateStore.java
@@ -27,6 +27,10 @@ import com.palantir.lock.v2.LockToken;
 import com.palantir.lock.watch.LockWatchVersion;
 import com.palantir.lock.watch.TransactionUpdate;
 import com.palantir.logsafe.Preconditions;
+import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
+import com.palantir.logsafe.logger.SafeLogger;
+import com.palantir.logsafe.logger.SafeLoggerFactory;
 import java.util.Collection;
 import java.util.NavigableMap;
 import java.util.Optional;
@@ -36,10 +40,14 @@ import org.immutables.value.Value;
 
 @NotThreadSafe
 final class TimestampStateStore {
+    private static final SafeLogger log = SafeLoggerFactory.get(TimestampStateStore.class);
+    private static final int MAXIMUM_SIZE = 20_000;
     private final NavigableMap<StartTimestamp, MapEntry> timestampMap = new TreeMap<>();
     private final SortedSetMultimap<Sequence, StartTimestamp> livingVersions = TreeMultimap.create();
 
     void putStartTimestamps(Collection<Long> startTimestamps, LockWatchVersion version) {
+        validateStateSize();
+
         startTimestamps.stream().map(StartTimestamp::of).forEach(startTimestamp -> {
             MapEntry previous = timestampMap.putIfAbsent(startTimestamp, MapEntry.of(version));
             Preconditions.checkArgument(previous == null, "Start timestamp already present in map");
@@ -93,8 +101,22 @@ final class TimestampStateStore {
                 .build();
     }
 
-    public Optional<Sequence> getEarliestLiveSequence() {
+    Optional<Sequence> getEarliestLiveSequence() {
         return Optional.ofNullable(Iterables.getFirst(livingVersions.keySet(), null));
+    }
+
+    private void validateStateSize() {
+        if (timestampMap.size() > MAXIMUM_SIZE || livingVersions.size() > MAXIMUM_SIZE) {
+            log.warn(
+                    "Timestamp state store has exceeded its maximum size. This likely indicates a memory leak",
+                    SafeArg.of("timestampMapSize", timestampMap.size()),
+                    SafeArg.of("livingVersionsSize", livingVersions.size()),
+                    SafeArg.of("maximumSize", MAXIMUM_SIZE),
+                    SafeArg.of("minimumLiveTimestamp", timestampMap.firstEntry()),
+                    SafeArg.of("maximumLiveTimestamp", timestampMap.lastEntry()),
+                    SafeArg.of("minimumLiveVersion", getEarliestLiveSequence()));
+            throw new SafeIllegalStateException("Exceeded maximum timestamp state store size");
+        }
     }
 
     @Value.Immutable
