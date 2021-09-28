d14312011a1cfde0ad6f324c946697018674163e
Jolyon Shah <jshah@palantir.com>
2021-09-28 15:48:32 +0100

    small tidy

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/api/cache/LockWatchValueScopingCacheImpl.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/api/cache/LockWatchValueScopingCacheImpl.java
@@ -148,7 +148,7 @@ public final class LockWatchValueScopingCacheImpl implements LockWatchValueScopi
 
     /**
      * Retrieval of transaction scoped caches (read-only or otherwise) does not need to be synchronised. The main
-     * reason for this is that, the only race condition that could conceivably occur is for the state to not exist here
+     * reason for this is that the only race condition that could conceivably occur is for the state to not exist here
      * when it should. However, in all of these cases, a no-op cache is used instead, and thus the transaction will
      * simply go ahead but with caching disabled (which is guaranteed to be safe).
      */
