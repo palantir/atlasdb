45ba34ad746a83aa339de8f93efc7263f02cb4cd
Glenn Sheasby <gsheasby@palantir.com>
2021-10-06 13:24:36 +0100

    move more stuff

--- atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/config/InMemoryAsyncAtlasDbConfig.java
+++ atlasdb-tests-shared/src/test/java/com/palantir/atlasdb/config/InMemoryAsyncAtlasDbConfig.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.palantir.atlasdb.memory;
+package com.palantir.atlasdb.config;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeName;
