e6301b97e4807a6df96d0b4cd227c24e0b17aec0
Glenn Sheasby <gsheasby@palantir.com>
2021-10-08 10:19:49 +0100

    restrict visibility of InMemoryTimestampService

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/memory/InMemoryDbTimeLockFactory.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/memory/InMemoryDbTimeLockFactory.java
@@ -29,7 +29,6 @@ import com.palantir.atlasdb.spi.KeyValueServiceRuntimeConfig;
 import com.palantir.atlasdb.timestamp.DbTimeLockFactory;
 import com.palantir.atlasdb.util.MetricsManager;
 import com.palantir.refreshable.Refreshable;
-import com.palantir.timestamp.InMemoryTimestampService;
 import com.palantir.timestamp.ManagedTimestampService;
 import java.util.HashMap;
 import java.util.Map;
