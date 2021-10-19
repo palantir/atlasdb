a829403dce23fbda259f814ebe23cf5772f69640
Jolyon <jshah@palantir.com>
2021-10-19 11:46:04 +0100

    Refactor TimestampCorroboratingTimelockService (#5707)

--- lock-api/src/main/java/com/palantir/lock/client/TimestampCorroboratingTimelockService.java
+++ lock-api/src/main/java/com/palantir/lock/client/TimestampCorroboratingTimelockService.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,87 +14,112 @@
  * limitations under the License.
  */
 
-package com.palantir.atlasdb.factory.timelock;
+package com.palantir.lock.client;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.palantir.atlasdb.correctness.TimestampCorrectnessMetrics;
-import com.palantir.lock.v2.AutoDelegate_TimelockService;
-import com.palantir.lock.v2.StartIdentifiedAtlasDbTransactionResponse;
-import com.palantir.lock.v2.TimelockService;
+import com.palantir.atlasdb.timelock.api.ConjureGetFreshTimestampsRequest;
+import com.palantir.atlasdb.timelock.api.ConjureGetFreshTimestampsResponse;
+import com.palantir.atlasdb.timelock.api.ConjureLockRequest;
+import com.palantir.atlasdb.timelock.api.ConjureLockResponse;
+import com.palantir.atlasdb.timelock.api.ConjureRefreshLocksRequest;
+import com.palantir.atlasdb.timelock.api.ConjureRefreshLocksResponse;
+import com.palantir.atlasdb.timelock.api.ConjureStartTransactionsRequest;
+import com.palantir.atlasdb.timelock.api.ConjureStartTransactionsResponse;
+import com.palantir.atlasdb.timelock.api.ConjureUnlockRequest;
+import com.palantir.atlasdb.timelock.api.ConjureUnlockResponse;
+import com.palantir.atlasdb.timelock.api.ConjureWaitForLocksResponse;
+import com.palantir.atlasdb.timelock.api.GetCommitTimestampsRequest;
+import com.palantir.atlasdb.timelock.api.GetCommitTimestampsResponse;
+import com.palantir.lock.v2.LeaderTime;
 import com.palantir.logsafe.SafeArg;
 import com.palantir.logsafe.exceptions.SafeRuntimeException;
 import com.palantir.logsafe.logger.SafeLogger;
 import com.palantir.logsafe.logger.SafeLoggerFactory;
-import com.palantir.timestamp.TimestampRange;
 import com.palantir.tritium.metrics.registry.TaggedMetricRegistry;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
 import java.util.function.ToLongFunction;
-import java.util.stream.Collectors;
 import org.immutables.value.Value;
 
-/**
- * A timelock service decorator for introducing runtime validity checks on received timestamps.
- */
-public final class TimestampCorroboratingTimelockService implements AutoDelegate_TimelockService {
+public final class TimestampCorroboratingTimelockService implements NamespacedConjureTimelockService {
     private static final SafeLogger log = SafeLoggerFactory.get(TimestampCorroboratingTimelockService.class);
     private static final String CLOCKS_WENT_BACKWARDS_MESSAGE = "It appears that clocks went backwards!";
 
     private final Runnable timestampViolationCallback;
-    private final TimelockService delegate;
+    private final NamespacedConjureTimelockService delegate;
     private final AtomicLong lowerBoundFromTimestamps = new AtomicLong(Long.MIN_VALUE);
     private final AtomicLong lowerBoundFromTransactions = new AtomicLong(Long.MIN_VALUE);
 
     @VisibleForTesting
-    TimestampCorroboratingTimelockService(Runnable timestampViolationCallback, TimelockService delegate) {
+    TimestampCorroboratingTimelockService(
+            Runnable timestampViolationCallback, NamespacedConjureTimelockService delegate) {
         this.timestampViolationCallback = timestampViolationCallback;
         this.delegate = delegate;
     }
 
-    public static TimelockService create(
-            Optional<String> userNamespace, TaggedMetricRegistry taggedMetricRegistry, TimelockService delegate) {
+    public static NamespacedConjureTimelockService create(
+            String userNamespace,
+            TaggedMetricRegistry taggedMetricRegistry,
+            NamespacedConjureTimelockService delegate) {
         return new TimestampCorroboratingTimelockService(
                 () -> TimestampCorrectnessMetrics.of(taggedMetricRegistry)
-                        .timestampsGoingBackwards(userNamespace.orElse("[unknown or un-namespaced]"))
+                        .timestampsGoingBackwards(userNamespace)
                         .inc(),
                 delegate);
     }
 
     @Override
-    public TimelockService delegate() {
-        return delegate;
+    public ConjureUnlockResponse unlock(ConjureUnlockRequest request) {
+        return delegate.unlock(request);
     }
 
     @Override
-    public long getFreshTimestamp() {
-        return checkAndUpdateLowerBound(delegate::getFreshTimestamp, x -> x, x -> x, OperationType.TIMESTAMP);
+    public ConjureRefreshLocksResponse refreshLocks(ConjureRefreshLocksRequest request) {
+        return delegate.refreshLocks(request);
     }
 
     @Override
-    public TimestampRange getFreshTimestamps(int numTimestampsRequested) {
+    public ConjureWaitForLocksResponse waitForLocks(ConjureLockRequest request) {
+        return delegate.waitForLocks(request);
+    }
+
+    @Override
+    public ConjureLockResponse lock(ConjureLockRequest request) {
+        return delegate.lock(request);
+    }
+
+    @Override
+    public LeaderTime leaderTime() {
+        return delegate.leaderTime();
+    }
+
+    @Override
+    public GetCommitTimestampsResponse getCommitTimestamps(GetCommitTimestampsRequest request) {
         return checkAndUpdateLowerBound(
-                () -> delegate.getFreshTimestamps(numTimestampsRequested),
-                TimestampRange::getLowerBound,
-                TimestampRange::getUpperBound,
+                () -> delegate.getCommitTimestamps(request),
+                GetCommitTimestampsResponse::getInclusiveLower,
+                GetCommitTimestampsResponse::getInclusiveUpper,
                 OperationType.TIMESTAMP);
     }
 
     @Override
-    public List<StartIdentifiedAtlasDbTransactionResponse> startIdentifiedAtlasDbTransactionBatch(int count) {
+    public ConjureGetFreshTimestampsResponse getFreshTimestamps(ConjureGetFreshTimestampsRequest request) {
         return checkAndUpdateLowerBound(
-                () -> delegate.startIdentifiedAtlasDbTransactionBatch(count),
-                responses -> Collections.min(getTimestampsFromResponses(responses)),
-                responses -> Collections.max(getTimestampsFromResponses(responses)),
-                OperationType.TRANSACTION);
+                () -> delegate.getFreshTimestamps(request),
+                ConjureGetFreshTimestampsResponse::getInclusiveLower,
+                ConjureGetFreshTimestampsResponse::getInclusiveUpper,
+                OperationType.TIMESTAMP);
     }
 
-    private List<Long> getTimestampsFromResponses(List<StartIdentifiedAtlasDbTransactionResponse> responses) {
-        return responses.stream()
-                .map(response -> response.startTimestampAndPartition().timestamp())
-                .collect(Collectors.toList());
+    @Override
+    public ConjureStartTransactionsResponse startTransactions(ConjureStartTransactionsRequest request) {
+        return checkAndUpdateLowerBound(
+                () -> delegate.startTransactions(request),
+                r -> r.getTimestamps().start(),
+                r -> r.getTimestamps().start()
+                        + ((r.getTimestamps().count() - 1L) * r.getTimestamps().interval()),
+                OperationType.TRANSACTION);
     }
 
     private <T> T checkAndUpdateLowerBound(
@@ -112,7 +137,8 @@ public final class TimestampCorroboratingTimelockService implements AutoDelegate
         return timestampContainer;
     }
 
-    private TimestampBounds getTimestampBounds() {
+    @VisibleForTesting
+    TimestampBounds getTimestampBounds() {
         long threadLocalLowerBoundFromTimestamps = lowerBoundFromTimestamps.get();
         long threadLocalLowerBoundFromTransactions = lowerBoundFromTransactions.get();
         return ImmutableTimestampBounds.of(threadLocalLowerBoundFromTimestamps, threadLocalLowerBoundFromTransactions);
