f4b6d16f385bc0e78681723543056ba091afd519
Glenn Sheasby <gsheasby@palantir.com>
2021-10-20 14:44:56 +0100

    api stuff

--- timelock-impl/src/main/java/com/palantir/atlasdb/backup/AtlasBackupResource.java
+++ timelock-impl/src/main/java/com/palantir/atlasdb/backup/AtlasBackupResource.java
@@ -17,132 +17,118 @@
 package com.palantir.atlasdb.backup;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.palantir.atlasdb.backup.api.AtlasBackupService;
-import com.palantir.atlasdb.config.ServerListConfig;
-import com.palantir.atlasdb.factory.AtlasDbDialogueServiceProvider;
 import com.palantir.atlasdb.timelock.api.BackupToken;
 import com.palantir.atlasdb.timelock.api.CheckBackupIsValidRequest;
 import com.palantir.atlasdb.timelock.api.CheckBackupIsValidResponse;
 import com.palantir.atlasdb.timelock.api.CompleteBackupRequest;
 import com.palantir.atlasdb.timelock.api.CompleteBackupResponse;
-import com.palantir.atlasdb.timelock.api.ConjureGetFreshTimestampsRequest;
-import com.palantir.atlasdb.timelock.api.ConjureGetFreshTimestampsResponse;
-import com.palantir.atlasdb.timelock.api.ConjureLockImmutableTimestampResponse;
-import com.palantir.atlasdb.timelock.api.ConjureLockImmutableTimestampResponse.Visitor;
-import com.palantir.atlasdb.timelock.api.ConjureLockToken;
-import com.palantir.atlasdb.timelock.api.ConjureRefreshLocksRequest;
-import com.palantir.atlasdb.timelock.api.ConjureRefreshLocksResponse;
-import com.palantir.atlasdb.timelock.api.ConjureTimelockService;
-import com.palantir.atlasdb.timelock.api.ConjureUnlockRequest;
 import com.palantir.atlasdb.timelock.api.Namespace;
 import com.palantir.atlasdb.timelock.api.PrepareBackupRequest;
 import com.palantir.atlasdb.timelock.api.PrepareBackupResponse;
-import com.palantir.atlasdb.timelock.api.SuccessfulLockImmutableTimestampResponse;
-import com.palantir.atlasdb.timelock.api.SuccessfulPrepareBackupResponse;
-import com.palantir.atlasdb.timelock.api.UnsuccessfulLockImmutableTimestampResponse;
-import com.palantir.atlasdb.timelock.api.UnsuccessfulPrepareBackupResponse;
-import com.palantir.conjure.java.api.config.service.UserAgent;
-import com.palantir.dialogue.clients.DialogueClients;
+import com.palantir.lock.v2.LockImmutableTimestampResponse;
 import com.palantir.lock.v2.LockToken;
-import com.palantir.refreshable.Refreshable;
+import com.palantir.lock.v2.TimelockService;
+import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.logger.SafeLogger;
+import com.palantir.logsafe.logger.SafeLoggerFactory;
 import com.palantir.tokens.auth.AuthHeader;
-import com.palantir.tritium.metrics.registry.TaggedMetricRegistry;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import org.jetbrains.annotations.NotNull;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class AtlasBackupResource implements AtlasBackupService {
-    private final ConjureTimelockService timelockService;
+    private static final SafeLogger log = SafeLoggerFactory.get(AtlasBackupResource.class);
+    private final Function<String, TimelockService> timelockServices;
 
     @VisibleForTesting
-    AtlasBackupResource(ConjureTimelockService timelockService) {
-        this.timelockService = timelockService;
+    AtlasBackupResource(Function<String, TimelockService> timelockServices) {
+        this.timelockServices = timelockServices;
     }
 
-    public static AtlasBackupResource create(
-            Refreshable<ServerListConfig> serverListConfig,
-            DialogueClients.ReloadingFactory reloadingFactory,
-            UserAgent userAgent,
-            TaggedMetricRegistry taggedMetricRegistry) {
-        AtlasDbDialogueServiceProvider serviceProvider = AtlasDbDialogueServiceProvider.create(
-                serverListConfig, reloadingFactory, userAgent, taggedMetricRegistry);
-        ConjureTimelockService timelockService = serviceProvider.getConjureTimelockService();
-
-        return new AtlasBackupResource(timelockService);
-    }
+    // public static AtlasBackupResource create(
+    //         Refreshable<ServerListConfig> serverListConfig,
+    //         DialogueClients.ReloadingFactory reloadingFactory,
+    //         UserAgent userAgent,
+    //         TaggedMetricRegistry taggedMetricRegistry) {
+    //     AtlasDbDialogueServiceProvider serviceProvider = AtlasDbDialogueServiceProvider.create(
+    //             serverListConfig, reloadingFactory, userAgent, taggedMetricRegistry);
+    //     ConjureTimelockService timelockService = serviceProvider.getConjureTimelockService();
+    //
+    //     return new AtlasBackupResource(timelockService);
+    // }
 
     @Override
     public PrepareBackupResponse prepareBackup(AuthHeader authHeader, PrepareBackupRequest request) {
-        Namespace namespace = getAny(request.getNamespaces());
-        ConjureLockImmutableTimestampResponse response =
-                timelockService.lockImmutableTimestamp(authHeader, namespace.get());
-        return response.accept(new Visitor<>() {
-            @Override
-            public PrepareBackupResponse visitSuccessful(SuccessfulLockImmutableTimestampResponse value) {
-                LockToken lockToken = LockToken.of(value.getLockToken().getRequestId());
-                BackupToken backupToken = BackupToken.of(namespace, value.getImmutableTimestamp(), lockToken);
-                return PrepareBackupResponse.successful(SuccessfulPrepareBackupResponse.builder()
-                        .backupTokens(backupToken)
-                        .build());
+        // TODO(gs): map?
+        Set<BackupToken> backupTokens = new HashSet<>();
+        Set<Namespace> unsuccessfulNamespaces = new HashSet<>();
+        for (Namespace namespace : request.getNamespaces()) {
+            try {
+                LockImmutableTimestampResponse response = timelock(namespace).lockImmutableTimestamp();
+                BackupToken backupToken =
+                        BackupToken.of(namespace, response.getImmutableTimestamp(), response.getLock());
+                backupTokens.add(backupToken);
+            } catch (Exception e) {
+                log.info("Failed to prepare backup for namespace", SafeArg.of("namespace", namespace), e);
+                unsuccessfulNamespaces.add(namespace);
             }
+        }
 
-            @Override
-            public PrepareBackupResponse visitUnsuccessful(UnsuccessfulLockImmutableTimestampResponse value) {
-                return PrepareBackupResponse.unsuccessful(UnsuccessfulPrepareBackupResponse.of());
-            }
-
-            @Override
-            public PrepareBackupResponse visitUnknown(String unknownType) {
-                return PrepareBackupResponse.unsuccessful(UnsuccessfulPrepareBackupResponse.of());
-            }
-        });
-    }
-
-    @NotNull
-    private <T> T getAny(Set<T> input) {
-        // single ns for now
-        return input.stream().findAny().orElseThrow();
+        return PrepareBackupResponse.builder()
+                .successful(backupTokens)
+                .unsuccessful(unsuccessfulNamespaces)
+                .build();
     }
 
     @Override
     public Map<Namespace, Long> getFreshTimestamps(AuthHeader authHeader, Set<Namespace> namespaces) {
-        ConjureGetFreshTimestampsRequest request = ConjureGetFreshTimestampsRequest.of(1);
-        Namespace namespace = getAny(namespaces);
-        ConjureGetFreshTimestampsResponse response =
-                timelockService.getFreshTimestamps(authHeader, namespace.get(), request);
-        return ImmutableMap.of(namespace, response.getInclusiveLower());
+        return namespaces.stream()
+                .collect(Collectors.toMap(ns -> ns, ns -> timelock(ns).getFreshTimestamp()));
     }
 
     @Override
     public CheckBackupIsValidResponse checkBackupIsValid(AuthHeader authHeader, CheckBackupIsValidRequest request) {
-        BackupToken backupToken = getAny(request.getBackupTokens());
-        ConjureLockToken conjureLockToken = getConjureLockToken(backupToken);
-        ConjureRefreshLocksRequest conjureRequest = ConjureRefreshLocksRequest.of(Set.of(conjureLockToken));
-        ConjureRefreshLocksResponse response = timelockService.refreshLocks(
-                authHeader, backupToken.getNamespace().get(), conjureRequest);
-        return response.getRefreshedTokens().contains(conjureLockToken)
-                ? CheckBackupIsValidResponse.of(ImmutableSet.of(backupToken), ImmutableSet.of())
-                : CheckBackupIsValidResponse.of(ImmutableSet.of(), ImmutableSet.of(backupToken));
+        Map<Boolean, List<BackupToken>> validAndInvalidTokens =
+                request.getBackupTokens().stream().collect(Collectors.partitioningBy(this::isValid));
+
+        return CheckBackupIsValidResponse.builder()
+                .validBackupTokens(validAndInvalidTokens.get(true))
+                .invalidBackupTokens(validAndInvalidTokens.get(false))
+                .build();
+    }
+
+    private boolean isValid(BackupToken backupToken) {
+        LockToken lockToken = backupToken.getLockToken();
+        return timelock(backupToken.getNamespace())
+                .refreshLockLeases(Set.of(lockToken))
+                .contains(lockToken);
     }
 
     @Override
     public CompleteBackupResponse completeBackup(AuthHeader authHeader, CompleteBackupRequest request) {
-        BackupToken backupToken = getAny(request.getBackupTokens());
-        ConjureLockToken conjureLockToken = getConjureLockToken(backupToken);
-        ConjureUnlockRequest conjureRequest = ConjureUnlockRequest.of(Set.of(conjureLockToken));
-        Set<ConjureLockToken> unlockedTokens = timelockService
-                .unlock(authHeader, backupToken.getNamespace().get(), conjureRequest)
-                .getTokens();
-        Set<Namespace> oneNamespace = ImmutableSet.of(backupToken.getNamespace());
-        return unlockedTokens.contains(conjureLockToken)
-                ? CompleteBackupResponse.of(oneNamespace, ImmutableSet.of())
-                : CompleteBackupResponse.of(ImmutableSet.of(), oneNamespace);
+        Map<Boolean, List<BackupToken>> results =
+                request.getBackupTokens().stream().collect(Collectors.partitioningBy(this::completeBackup));
+
+        return CompleteBackupResponse.builder()
+                .successfulBackups(namespaces(results.get(true)))
+                .unsuccessfulBackups(namespaces(results.get(false)))
+                .build();
     }
 
-    @NotNull
-    private ConjureLockToken getConjureLockToken(BackupToken backupToken) {
-        return ConjureLockToken.of(backupToken.getLockToken().getRequestId());
+    private boolean completeBackup(BackupToken backupToken) {
+        LockToken lockToken = backupToken.getLockToken();
+        return timelock(backupToken.getNamespace()).unlock(Set.of(lockToken)).contains(lockToken);
+    }
+
+    private TimelockService timelock(Namespace namespace) {
+        return timelockServices.apply(namespace.get());
+    }
+
+    private static Set<Namespace> namespaces(List<BackupToken> tokens) {
+        return tokens.stream().map(BackupToken::getNamespace).collect(Collectors.toSet());
     }
 }
