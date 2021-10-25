492f4d72f41cf915fd096a06e8915c7827fe6cb5
Jolyon Shah <jshah@palantir.com>
2021-10-25 15:53:34 +0100

    make thread safe

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/api/watch/LockWatchEventLog.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/api/watch/LockWatchEventLog.java
@@ -27,12 +27,27 @@ import com.palantir.lock.watch.LockWatchVersion;
 import com.palantir.logsafe.Preconditions;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Supplier;
 import javax.annotation.concurrent.NotThreadSafe;
 
+/**
+ * This class is *not* thread-safe due to the fact that it is *technically* possible to race multiple success updates
+ * at once. In practice, this is not a concern, due to:
+ *
+ * 1. Concurrent successes should have one success be a subset of the other in terms of events;
+ * 2. The {@link LockWatchEventCacheImpl} protects against concurrency concerns when processing elements as all event
+ *    cache updates are done in sychronized blocks.
+ *
+ * Assuming that both of the above hold, this class should otherwise remain internally consistent during concurrent
+ * execution.
+ */
 @NotThreadSafe
 final class LockWatchEventLog {
     private final ClientLockWatchSnapshot snapshot;
     private final VersionedEventStore eventStore;
+    private final ReadWriteLock eventLock;
     private Optional<LockWatchVersion> latestVersion = Optional.empty();
 
     static LockWatchEventLog create(CacheMetrics metrics, int minEvents, int maxEvents) {
@@ -42,10 +57,11 @@ final class LockWatchEventLog {
     private LockWatchEventLog(ClientLockWatchSnapshot snapshot, CacheMetrics metrics, int minEvents, int maxEvents) {
         this.snapshot = snapshot;
         this.eventStore = new VersionedEventStore(metrics, minEvents, maxEvents);
+        this.eventLock = new ReentrantReadWriteLock(true);
     }
 
     CacheUpdate processUpdate(LockWatchStateUpdate update) {
-        if (!latestVersion.isPresent()
+        if (latestVersion.isEmpty()
                 || !update.logId().equals(latestVersion.get().id())) {
             return update.accept(new NewLeaderVisitor());
         } else {
@@ -54,11 +70,40 @@ final class LockWatchEventLog {
     }
 
     /**
+     * Get events between provided versions, taking out a read lock in order to guarantee internal consistency. This
+     * is critical, as when events are retentioned, they are removed from the event store before they are added to
+     * the snapshot; if events were to be read within this window, the call will fail due to a violation of internal
+     * consistency.
+     *
      * @return lock watch events that occurred from (exclusive) the provided version, up to the end version (inclusive);
      *         this may begin with a snapshot if the latest version is too far behind, and this snapshot may be
      *         condensed.
      */
     ClientLogEvents getEventsBetweenVersions(VersionBounds versionBounds) {
+        return runWithReadLock(() -> getEventsBetweenVersionsInternal(versionBounds));
+    }
+
+    /**
+     * Remove events up to, but not including, the provided earliest sequence. If no sequence is provided, then no
+     * events will be retentioned based on sequence number (note that the minimum number of events will always be
+     * retained, and events over the maximum number will always be removed).
+     *
+     * This method *must* be executed with a write lock in order to guarantee internal consistency: it modifies both
+     * the event store and the snapshot, and thus must perform this operation with mutual exclusion to any threads
+     * that may attempt to read from both of these data structures.
+     */
+    void retentionEvents(Optional<Sequence> earliestSequence) {
+        runWithWriteLock(() -> getLatestKnownVersion().ifPresent(version -> {
+            LockWatchEvents eventsToBeRemoved = eventStore.retentionEvents(earliestSequence);
+            snapshot.processEvents(eventsToBeRemoved, version.id());
+        }));
+    }
+
+    Optional<LockWatchVersion> getLatestKnownVersion() {
+        return latestVersion;
+    }
+
+    private ClientLogEvents getEventsBetweenVersionsInternal(VersionBounds versionBounds) {
         Optional<LockWatchVersion> startVersion = versionBounds.startVersion().map(this::createStartVersion);
         LockWatchVersion currentVersion = getLatestVersionAndVerify(versionBounds.endVersion());
 
@@ -98,17 +143,6 @@ final class LockWatchEventLog {
         }
     }
 
-    void retentionEvents(Optional<Sequence> earliestSequence) {
-        getLatestKnownVersion().ifPresent(version -> {
-            LockWatchEvents eventsToBeRemoved = eventStore.retentionEvents(earliestSequence);
-            snapshot.processEvents(eventsToBeRemoved, version.id());
-        });
-    }
-
-    Optional<LockWatchVersion> getLatestKnownVersion() {
-        return latestVersion;
-    }
-
     @VisibleForTesting
     LockWatchEventLogState getStateForTesting() {
         return ImmutableLockWatchEventLogState.builder()
@@ -116,6 +150,24 @@ final class LockWatchEventLog {
                 .eventStoreState(eventStore.getStateForTesting())
                 .snapshotState(snapshot.getStateForTesting())
                 .build();
+    }
+
+    private <T> T runWithReadLock(Supplier<T> task) {
+        try {
+            eventLock.readLock().lock();
+            return task.get();
+        } finally {
+            eventLock.readLock().unlock();
+        }
+    }
+
+    private void runWithWriteLock(Runnable task) {
+        try {
+            eventLock.writeLock().lock();
+            task.run();
+        } finally {
+            eventLock.writeLock().unlock();
+        }
     }
 
     private LockWatchEvent getCompressedSnapshot(VersionBounds versionBounds) {
@@ -152,7 +204,20 @@ final class LockWatchEventLog {
         return currentVersion;
     }
 
+    /**
+     * While processing snapshots and failures require write locks (as they modify both the snapshot and the event
+     * store), a success only modifies the event store as no retention is performed. As such, a read lock is *mostly*
+     * sufficient: it technically is possible to race on concurrent successes, particularly if they have different
+     * events, but this should be guarded against by the caller of {@link #processUpdate(LockWatchStateUpdate)}.
+     */
     private void processSuccess(LockWatchStateUpdate.Success success) {
+        runWithReadLock(() -> {
+            processSuccessInternal(success);
+            return null;
+        });
+    }
+
+    private void processSuccessInternal(LockWatchStateUpdate.Success success) {
         Preconditions.checkState(latestVersion.isPresent(), "Must have a known version to process successful updates");
         Optional<LockWatchVersion> snapshotVersion = snapshot.getSnapshotVersion();
         Preconditions.checkState(
@@ -179,15 +244,19 @@ final class LockWatchEventLog {
     }
 
     private void processSnapshot(LockWatchStateUpdate.Snapshot snapshotUpdate) {
-        eventStore.clear();
-        snapshot.resetWithSnapshot(snapshotUpdate);
-        latestVersion = Optional.of(LockWatchVersion.of(snapshotUpdate.logId(), snapshotUpdate.lastKnownVersion()));
+        runWithWriteLock(() -> {
+            eventStore.clear();
+            snapshot.resetWithSnapshot(snapshotUpdate);
+            latestVersion = Optional.of(LockWatchVersion.of(snapshotUpdate.logId(), snapshotUpdate.lastKnownVersion()));
+        });
     }
 
     private void processFailed() {
-        eventStore.clear();
-        snapshot.reset();
-        latestVersion = Optional.empty();
+        runWithWriteLock(() -> {
+            eventStore.clear();
+            snapshot.reset();
+            latestVersion = Optional.empty();
+        });
     }
 
     private final class ProcessingVisitor implements LockWatchStateUpdate.Visitor<CacheUpdate> {
