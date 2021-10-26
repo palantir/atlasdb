4e001ea2e6319586c07f14a757d9cd4c6aabacf7
Anjor Kanekar <anjor@umd.edu>
2021-10-26 16:56:25 +0100

    Update run-circle-tests.sh

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -8,7 +8,7 @@ function checkDocsBuild {
      pyenv install 3.5.4
      pyenv global 3.5.4
      # these versions are quite old, but the docs all stop building on newer versions. Since the docs publish has now been failing for like 6 months, I'm trying to maintain the status quo.
-     pip3 install sphinx==4.2.0 sphinx_rtd_theme==0.5.0 requests==2.24.0 recommonmark==0.6.0 pygments==2.5.2 markupsafe==1.1.1 jinja2==2.11.2 alabaster==0.7.12 babel==2.8.0 certifi==2020.6.20 sphinxcontrib-websupport==1.1.2 setuptools==44.1.1 typing==3.7.4.3 urllib3==1.25.10 snowballstemmer==2.0.0 six==1.15.0
+     pip3 install sphinx==4.0.2 sphinx_rtd_theme==0.5.0 requests==2.24.0 recommonmark==0.6.0 pygments==2.5.2 markupsafe==1.1.1 jinja2==2.11.2 alabaster==0.7.12 babel==2.8.0 certifi==2020.6.20 sphinxcontrib-websupport==1.1.2 setuptools==44.1.1 typing==3.7.4.3 urllib3==1.25.10 snowballstemmer==2.0.0 six==1.15.0
      cd docs/
      make html
 }
