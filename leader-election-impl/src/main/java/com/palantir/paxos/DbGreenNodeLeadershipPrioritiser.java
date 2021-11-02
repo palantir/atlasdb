df245a0fb0a15218bc188905d8865f7a2cf82b25
Glenn Sheasby <gsheasby@palantir.com>
2021-11-02 15:29:09 +0000

    Add DbGreenNodeLeadershipPrioritiser

---
+++ leader-election-impl/src/main/java/com/palantir/paxos/DbGreenNodeLeadershipPrioritiser.java
@@ -0,0 +1,61 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.paxos;
+
+import com.palantir.common.time.Clock;
+import com.palantir.sls.versions.OrderableSlsVersion;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class DbGreenNodeLeadershipPrioritiser implements GreenNodeLeadershipPrioritiser {
+    private final Optional<OrderableSlsVersion> timeLockVersion;
+    private final Supplier<Duration> leadershipAttemptBackoff;
+    private final GreenNodeLeadershipState greenNodeLeadershipState;
+    private final Clock clock;
+
+    public DbGreenNodeLeadershipPrioritiser(
+            Optional<OrderableSlsVersion> timeLockVersion,
+            Supplier<Duration> leadershipAttemptBackoff,
+            GreenNodeLeadershipState greenNodeLeadershipState,
+            Clock clock) {
+        this.timeLockVersion = timeLockVersion;
+        this.leadershipAttemptBackoff = leadershipAttemptBackoff;
+        this.greenNodeLeadershipState = greenNodeLeadershipState;
+        this.clock = clock;
+    }
+
+    @Override
+    public boolean shouldGreeningNodeBecomeLeader() {
+        OrderableSlsVersion currentVersion = timeLockVersion.orElse(null);
+        Optional<Long> latestAttemptTime = greenNodeLeadershipState.getLatestAttemptTime(currentVersion);
+        if (latestAttemptTime.isEmpty()) {
+            greenNodeLeadershipState.setLatestAttemptTime(currentVersion, clock.getTimeMillis());
+            return true;
+        }
+
+        long latestAttemptMillis = latestAttemptTime.get();
+        long currentTime = clock.getTimeMillis();
+        long backoffMillis = leadershipAttemptBackoff.get().toMillis();
+        if (currentTime - latestAttemptMillis > backoffMillis) {
+            greenNodeLeadershipState.setLatestAttemptTime(currentVersion, currentTime);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ leader-election-impl/src/main/java/com/palantir/paxos/GreenNodeLeadershipState.java
@@ -0,0 +1,79 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.paxos;
+
+import com.palantir.sls.versions.OrderableSlsVersion;
+import java.util.Optional;
+import java.util.function.Function;
+import javax.sql.DataSource;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.immutables.JdbiImmutables;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+public class GreenNodeLeadershipState {
+    private static final int NODE_ID = 0;
+
+    private final Jdbi jdbi;
+
+    private GreenNodeLeadershipState(Jdbi jdbi) {
+        this.jdbi = jdbi;
+    }
+
+    public static GreenNodeLeadershipState create(DataSource dataSource) {
+        Jdbi jdbi = Jdbi.create(dataSource).installPlugin(new SqlObjectPlugin());
+        jdbi.getConfig(JdbiImmutables.class).registerImmutable(Client.class);
+        GreenNodeLeadershipState state = new GreenNodeLeadershipState(jdbi);
+        state.initialise();
+        return state;
+    }
+
+    private void initialise() {
+        execute(Queries::createGreenNodeLeadershipStateTable);
+    }
+
+    public Optional<Long> getLatestAttemptTime(OrderableSlsVersion currentVersion) {
+        return execute(dao -> dao.getLatestAttemptTime(NODE_ID, currentVersion.getValue()));
+    }
+
+    public boolean setLatestAttemptTime(OrderableSlsVersion currentVersion, Long attemptTimeMillis) {
+        return execute(dao -> dao.updateLatestAttemptTime(NODE_ID, currentVersion.getValue(), attemptTimeMillis));
+    }
+
+    private <T> T execute(Function<GreenNodeLeadershipState.Queries, T> call) {
+        return jdbi.withExtension(GreenNodeLeadershipState.Queries.class, call::apply);
+    }
+
+    public interface Queries {
+        @SqlUpdate("CREATE TABLE IF NOT EXISTS greenNodeLeadershipState (nodeId INTEGER, productVersion TEXT, "
+                + "attemptTime BIGINT, PRIMARY KEY(nodeId))")
+        boolean createGreenNodeLeadershipStateTable();
+
+        @SqlUpdate("INSERT OR REPLACE INTO greenNodeLeadershipState (node, productVersion, attemptTime) VALUES "
+                + "(:nodeId, :productVersion, :attemptTime)")
+        boolean updateLatestAttemptTime(
+                @Bind("nodeId") int nodeId,
+                @Bind("productVersion") String productVersion,
+                @Bind("attemptTime") long attemptTime);
+
+        @SqlQuery("SELECT attemptTime FROM greenNodeLeadershipState "
+                + "WHERE nodeId = :nodeId AND productVersion = :productVersion")
+        Optional<Long> getLatestAttemptTime(@Bind("nodeId") int nodeId, @Bind("productVersion") String productVersion);
+    }
+}
