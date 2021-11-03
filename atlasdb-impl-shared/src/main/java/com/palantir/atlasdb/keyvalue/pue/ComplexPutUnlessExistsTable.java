78e983c18ecad6fd9c0f9e852bd0df854adc8d5f
Jeremy Kong <jeremykong@hotmail.com>
2021-11-03 17:11:42 +0800

    cleanup cpuet

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/pue/ComplexPutUnlessExistsTable.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/keyvalue/pue/ComplexPutUnlessExistsTable.java
@@ -22,18 +22,14 @@ import com.google.common.collect.Streams;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
-import com.palantir.atlasdb.futures.AtlasFutures;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.KeyAlreadyExistsException;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.common.streams.KeyedStream;
 import com.palantir.conjure.java.lib.Bytes;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public final class ComplexPutUnlessExistsTable implements PutUnlessExistsTable {
@@ -63,11 +59,8 @@ public final class ComplexPutUnlessExistsTable implements PutUnlessExistsTable {
                 store.get(Streams.stream(cells).collect(Collectors.toSet()));
         return Futures.transform(
                 currentState,
-                state -> KeyedStream.stream(state)
-                        .filter(it -> it.commitState() == CommitState.COMMITTED)
-                        .map(it -> it.value().asNewByteArray())
-                        .collectToMap(),
-                MoreExecutors.directExecutor());
+                this::resolvePendingDecisions,
+                MoreExecutors.directExecutor()); // TODO (jkong): Naughty
     }
 
     @Override
@@ -77,72 +70,44 @@ public final class ComplexPutUnlessExistsTable implements PutUnlessExistsTable {
 
     @Override
     public void putUnlessExistsMultiple(Map<Cell, byte[]> values) throws KeyAlreadyExistsException {
-        try {
-            store.putUnlessExists(KeyedStream.stream(values)
-                    .map(userValue -> (PutUnlessExistsState) ImmutablePutUnlessExistsState.builder()
-                            .value(Bytes.from(userValue))
-                            .commitState(CommitState.PENDING)
-                            .build())
-                    .collectToMap());
-            store.put(KeyedStream.stream(values)
-                    .map(userValue -> (PutUnlessExistsState) ImmutablePutUnlessExistsState.builder()
-                            .value(Bytes.from(userValue))
-                            .commitState(CommitState.COMMITTED)
-                            .build())
-                    .collectToMap());
-        } catch (KeyAlreadyExistsException e) {
-            makeDecisionOnStates(e, values);
-        }
+        store.putUnlessExists(KeyedStream.stream(values)
+                .map(userValue -> (PutUnlessExistsState) ImmutablePutUnlessExistsState.builder()
+                        .value(Bytes.from(userValue))
+                        .commitState(CommitState.PENDING)
+                        .build())
+                .collectToMap());
+        store.put(KeyedStream.stream(values)
+                .map(userValue -> (PutUnlessExistsState) ImmutablePutUnlessExistsState.builder()
+                        .value(Bytes.from(userValue))
+                        .commitState(CommitState.COMMITTED)
+                        .build())
+                .collectToMap());
     }
 
-    private void makeDecisionOnStates(KeyAlreadyExistsException initialException, Map<Cell, byte[]> userValues) {
-        // Acceptable: this call is part of a putUnlessExists, which is blocking API.
-        Map<Cell, PutUnlessExistsState> existingStates =
-                AtlasFutures.getUnchecked(store.get(initialException.getExistingKeys()));
-        Set<Cell> alreadyCommittedValues = new HashSet<>();
-        Map<Cell, byte[]> valuesToTryPutting = new HashMap<>();
+    private Map<Cell, byte[]> resolvePendingDecisions(Map<Cell, PutUnlessExistsState> initialRawDbRead) {
+        Map<Cell, PutUnlessExistsState> pendingCells = KeyedStream.stream(initialRawDbRead)
+                .filter(read -> read.commitState() == CommitState.PENDING)
+                .collectToMap();
 
-        for (Map.Entry<Cell, PutUnlessExistsState> existingState : existingStates.entrySet()) {
-            if (existingState.getValue().commitState() == CommitState.COMMITTED) {
-                alreadyCommittedValues.add(existingState.getKey());
-            } else {
-                // Pending. We can try to overwrite this.
-                byte[] userValue = userValues.get(existingState.getKey());
-                valuesToTryPutting.put(existingState.getKey(), userValue);
-            }
-        }
-
-        if (!alreadyCommittedValues.isEmpty()) {
-            throw new KeyAlreadyExistsException(
-                    initialException.getMessage(),
-                    alreadyCommittedValues,
-                    initialException.getKnownSuccessfullyCommittedKeys());
-        }
-        // We can try resolving decisions
-        // No multi-CAS :(
-        for (Map.Entry<Cell, byte[]> valueToTryPutting : valuesToTryPutting.entrySet()) {
+        // TODO (jkong): Replace with multi-cas
+        for (Map.Entry<Cell, PutUnlessExistsState> pendingCell : pendingCells.entrySet()) {
             store.checkAndSet(
-                    valueToTryPutting.getKey(),
-                    existingStates.get(valueToTryPutting.getKey()),
+                    pendingCell.getKey(),
+                    pendingCell.getValue(),
                     ImmutablePutUnlessExistsState.builder()
-                            .value(Bytes.from(valueToTryPutting.getValue()))
+                            .value(pendingCell.getValue().value())
                             .commitState(CommitState.PENDING)
                             .build());
             store.put(
-                    valueToTryPutting.getKey(),
+                    pendingCell.getKey(),
                     ImmutablePutUnlessExistsState.builder()
-                            .value(Bytes.from(valueToTryPutting.getValue()))
+                            .value(pendingCell.getValue().value())
                             .commitState(CommitState.COMMITTED)
                             .build());
         }
 
-        Map<Cell, byte[]> remainingUserValues = KeyedStream.stream(userValues)
-                .filterKeys(c -> !valuesToTryPutting.containsKey(c))
-                .filterKeys(c ->
-                        !initialException.getKnownSuccessfullyCommittedKeys().contains(c))
+        return KeyedStream.stream(initialRawDbRead)
+                .map(read -> read.value().asNewByteArray())
                 .collectToMap();
-        if (!remainingUserValues.isEmpty()) {
-            putUnlessExistsMultiple(remainingUserValues);
-        }
     }
 }
