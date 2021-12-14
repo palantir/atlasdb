f5c00e3881e4cd7fceec5ec1f46d570cae906d77
Glenn Sheasby <gsheasby@palantir.com>
2021-12-14 08:40:47 +0000

    ABR4: repairInternalTables (#5787)

--- atlasdb-container-test-utils/src/main/java/com/palantir/atlasdb/containers/ThreeNodeCassandraCluster.java
+++ atlasdb-container-test-utils/src/main/java/com/palantir/atlasdb/containers/ThreeNodeCassandraCluster.java
@@ -19,7 +19,7 @@ import com.palantir.atlasdb.cassandra.CassandraCredentialsConfig;
 import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfig;
 import com.palantir.atlasdb.cassandra.ImmutableCassandraCredentialsConfig;
 import com.palantir.atlasdb.cassandra.ImmutableCassandraKeyValueServiceConfig;
-import com.palantir.atlasdb.cassandra.ImmutableDefaultConfig;
+import com.palantir.atlasdb.cassandra.ImmutableCqlCapableConfig;
 import com.palantir.atlasdb.keyvalue.cassandra.CassandraKeyValueServiceImpl;
 import com.palantir.docker.compose.DockerComposeRule;
 import com.palantir.docker.compose.connection.waiting.SuccessOrFailure;
@@ -42,25 +42,39 @@ public class ThreeNodeCassandraCluster extends Container {
             .password("password")
             .build();
 
-    public static final CassandraKeyValueServiceConfig KVS_CONFIG = ImmutableCassandraKeyValueServiceConfig.builder()
-            .servers(ImmutableDefaultConfig.builder()
-                    .addThriftHosts(
-                            InetSocketAddress.createUnresolved(
-                                    FIRST_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_THRIFT_PORT),
-                            InetSocketAddress.createUnresolved(
-                                    SECOND_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_THRIFT_PORT),
-                            InetSocketAddress.createUnresolved(
-                                    THIRD_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_THRIFT_PORT))
-                    .build())
-            .poolSize(20)
-            .keyspace("atlasdb")
-            .replicationFactor(3)
-            .mutationBatchCount(10000)
-            .mutationBatchSizeBytes(10000000)
-            .fetchBatchCount(1000)
-            .autoRefreshNodes(false)
-            .credentials(CREDENTIALS)
-            .build();
+    private static final int DEFAULT_REPLICATION_FACTOR = 3;
+    public static final CassandraKeyValueServiceConfig KVS_CONFIG = getKvsConfig(DEFAULT_REPLICATION_FACTOR);
+
+    @SuppressWarnings("DnsLookup")
+    public static CassandraKeyValueServiceConfig getKvsConfig(int replicationFactor) {
+        return ImmutableCassandraKeyValueServiceConfig.builder()
+                .servers(ImmutableCqlCapableConfig.builder()
+                        .addThriftHosts(
+                                new InetSocketAddress(
+                                        FIRST_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_THRIFT_PORT),
+                                new InetSocketAddress(
+                                        SECOND_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_THRIFT_PORT),
+                                new InetSocketAddress(
+                                        THIRD_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_THRIFT_PORT))
+                        .addCqlHosts(
+                                new InetSocketAddress(
+                                        FIRST_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_CQL_PORT),
+                                new InetSocketAddress(
+                                        SECOND_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_CQL_PORT),
+                                new InetSocketAddress(
+                                        THIRD_CASSANDRA_CONTAINER_NAME, CassandraContainer.CASSANDRA_CQL_PORT))
+                        .build())
+                .poolSize(20)
+                .keyspace("atlasdb")
+                .replicationFactor(replicationFactor)
+                .ignoreNodeTopologyChecks(true)
+                .mutationBatchCount(10000)
+                .mutationBatchSizeBytes(10000000)
+                .fetchBatchCount(1000)
+                .autoRefreshNodes(false)
+                .credentials(CREDENTIALS)
+                .build();
+    }
 
     @Override
     public String getDockerComposeFile() {
