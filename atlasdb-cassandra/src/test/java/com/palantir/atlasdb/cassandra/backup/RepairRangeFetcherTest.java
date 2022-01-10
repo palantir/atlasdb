8af635d9751b801d9c5e63cec9667bb79893dfdc
Glenn Sheasby <gsheasby@palantir.com>
2022-01-10 16:42:30 +0000

    add first RepairRangeFetcherTest

--- atlasdb-cassandra/src/test/java/com/palantir/atlasdb/cassandra/backup/RepairRangeFetcherTest.java
+++ atlasdb-cassandra/src/test/java/com/palantir/atlasdb/cassandra/backup/RepairRangeFetcherTest.java
@@ -16,42 +16,47 @@
 
 package com.palantir.atlasdb.cassandra.backup;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import com.datastax.driver.core.KeyspaceMetadata;
 import com.datastax.driver.core.TableMetadata;
+import com.datastax.driver.core.policies.DefaultRetryPolicy;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableRangeSet;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Range;
 import com.google.common.collect.RangeSet;
 import com.palantir.atlasdb.cassandra.CassandraKeyValueServiceConfig;
 import com.palantir.atlasdb.cassandra.CassandraServersConfigs;
 import com.palantir.atlasdb.cassandra.ImmutableCqlCapableConfig;
+import com.palantir.atlasdb.cassandra.backup.transaction.Transactions1TableInteraction;
+import com.palantir.atlasdb.cassandra.backup.transaction.TransactionsTableInteraction;
 import com.palantir.atlasdb.keyvalue.cassandra.LightweightOppToken;
-import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
+import com.palantir.atlasdb.transaction.impl.TransactionConstants;
+import com.palantir.timestamp.FullyBoundedTimestampRange;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+// TODO(gs): deduplicate mockery
 @RunWith(MockitoJUnitRunner.class)
-@SuppressWarnings({"DnsLookup", "UnstableApiUsage"})
-public class TokenRangeFetcherTest {
+public class RepairRangeFetcherTest {
     private static final InetSocketAddress HOST_1 = new InetSocketAddress("cassandra-1", 9042);
     private static final InetSocketAddress HOST_2 = new InetSocketAddress("cassandra-2", 9042);
     private static final InetSocketAddress HOST_3 = new InetSocketAddress("cassandra-3", 9042);
     private static final ImmutableList<InetSocketAddress> HOSTS = ImmutableList.of(HOST_1, HOST_2, HOST_3);
     private static final String KEYSPACE_NAME = "keyspace";
-    private static final String TABLE_NAME = "table";
+    private static final DefaultRetryPolicy POLICY = DefaultRetryPolicy.INSTANCE;
+    private static final String TXN_1 = TransactionConstants.TRANSACTION_TABLE.getTableName();
 
     private static final LightweightOppToken TOKEN_1 = new LightweightOppToken("1111".getBytes(StandardCharsets.UTF_8));
     private static final LightweightOppToken TOKEN_2 = new LightweightOppToken("2222".getBytes(StandardCharsets.UTF_8));
@@ -71,23 +76,26 @@ public class TokenRangeFetcherTest {
     @Mock
     private CassandraKeyValueServiceConfig config;
 
-    private TokenRangeFetcher tokenRangeFetcher;
+    private RepairRangeFetcher repairRangeFetcher;
 
     @Before
     public void setUp() {
-        TableMetadata tableMetadata = mock(TableMetadata.class);
+        TableMetadata txn1Metadata = mock(TableMetadata.class);
 
         KeyspaceMetadata keyspaceMetadata = mock(KeyspaceMetadata.class);
         when(keyspaceMetadata.getName()).thenReturn(KEYSPACE_NAME);
-        when(keyspaceMetadata.getTable(TABLE_NAME)).thenReturn(tableMetadata);
-        when(tableMetadata.getKeyspace()).thenReturn(keyspaceMetadata);
-        when(tableMetadata.getName()).thenReturn(TABLE_NAME);
+        when(keyspaceMetadata.getTables()).thenReturn(ImmutableList.of(txn1Metadata));
+        when(txn1Metadata.getKeyspace()).thenReturn(keyspaceMetadata);
+        when(txn1Metadata.getName()).thenReturn(TXN_1);
         when(cqlMetadata.getKeyspaceMetadata(KEYSPACE_NAME)).thenReturn(keyspaceMetadata);
 
+        when(cqlSession.getMetadata()).thenReturn(cqlMetadata);
         when(cqlSession.retrieveRowKeysAtConsistencyAll(anyList()))
-                .thenReturn(ImmutableSet.of(TOKEN_1, TOKEN_2, TOKEN_3));
+                .thenReturn(ImmutableSet.of(token("1111"), token("5555")));
+
         when(cqlMetadata.getTokenRanges())
                 .thenReturn(ImmutableSet.of(RANGE_AT_MOST_1, RANGE_1_TO_2, RANGE_2_TO_3, RANGE_GREATER_THAN_3));
+        when(cqlMetadata.getReplicas(eq(KEYSPACE_NAME), any())).thenReturn(ImmutableSet.copyOf(HOSTS));
         when(cqlSession.getMetadata()).thenReturn(cqlMetadata);
 
         CassandraServersConfigs.CqlCapableConfig cqlCapableConfig = ImmutableCqlCapableConfig.builder()
@@ -97,31 +105,25 @@ public class TokenRangeFetcherTest {
         when(config.servers()).thenReturn(cqlCapableConfig);
         when(config.getKeyspaceOrThrow()).thenReturn(KEYSPACE_NAME);
 
-        tokenRangeFetcher = new TokenRangeFetcher(cqlSession, config);
+        repairRangeFetcher = new RepairRangeFetcher(cqlSession, config);
     }
 
     @Test
-    public void allNodesGetAllRangesWithThreeNodesAndRF3() {
-        when(cqlMetadata.getReplicas(eq(KEYSPACE_NAME), any())).thenReturn(ImmutableSet.copyOf(HOSTS));
+    public void testRepairOnlyTxn1() {
+        List<TransactionsTableInteraction> interactions =
+                ImmutableList.of(new Transactions1TableInteraction(range(1L, 10_000_000L), POLICY));
 
-        Map<InetSocketAddress, RangeSet<LightweightOppToken>> tokenRange = tokenRangeFetcher.getTokenRange(TABLE_NAME);
-        assertThat(tokenRange.keySet()).containsExactlyInAnyOrder(HOST_1, HOST_2, HOST_3);
-        HOSTS.forEach(host -> assertThat(tokenRange.get(host)).isEqualTo(ImmutableRangeSet.of(Range.all())));
+        Map<String, Map<InetSocketAddress, RangeSet<LightweightOppToken>>> rangesForRepair =
+                repairRangeFetcher.getTransactionTableRangesForRepair(interactions);
+        assertThat(rangesForRepair.keySet()).containsExactly(TXN_1);
     }
 
-    @Test
-    public void testGetTokenRangesWithRF2() {
-        when(cqlMetadata.getReplicas(KEYSPACE_NAME, RANGE_AT_MOST_1)).thenReturn(ImmutableSet.of(HOST_3, HOST_1));
-        when(cqlMetadata.getReplicas(KEYSPACE_NAME, RANGE_GREATER_THAN_3)).thenReturn(ImmutableSet.of(HOST_3, HOST_1));
-        when(cqlMetadata.getReplicas(KEYSPACE_NAME, RANGE_1_TO_2)).thenReturn(ImmutableSet.of(HOST_1, HOST_2));
-        when(cqlMetadata.getReplicas(KEYSPACE_NAME, RANGE_2_TO_3)).thenReturn(ImmutableSet.of(HOST_2, HOST_3));
+    private static FullyBoundedTimestampRange range(long lower, long upper) {
+        return FullyBoundedTimestampRange.of(Range.closed(lower, upper));
+    }
 
-        Map<InetSocketAddress, RangeSet<LightweightOppToken>> tokenRange = tokenRangeFetcher.getTokenRange(TABLE_NAME);
-        assertThat(tokenRange.keySet()).containsExactlyInAnyOrder(HOST_1, HOST_2, HOST_3);
-        assertThat(tokenRange.get(HOST_1).asRanges())
-                .containsExactly(Range.atMost(TOKEN_2), Range.greaterThan(TOKEN_3));
-        assertThat(tokenRange.get(HOST_2).asRanges()).containsExactly(Range.openClosed(TOKEN_1, TOKEN_3));
-        assertThat(tokenRange.get(HOST_3).asRanges())
-                .containsExactly(Range.atMost(TOKEN_1), Range.greaterThan(TOKEN_2));
+    // TODO(gs): refine/unify test token generation?
+    private static LightweightOppToken token(String hexString) {
+        return new LightweightOppToken(hexString.getBytes(StandardCharsets.UTF_8));
     }
 }
