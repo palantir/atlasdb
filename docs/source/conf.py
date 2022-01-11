d61c3426e76c1cf4d138658b7b99e340c397f88b
Jeremy Kong <jeremykong@hotmail.com>
2022-01-11 18:42:54 +0800

    [Transactions3] A Delightful/Disastrous/Documentary Reading (#5826)
    * begin transactions3
    
    * criteria
    
    * more writeup stuff
    
    * Theory
    
    * cfs
    
    * there will be consequences
    
    * byte addition
    
    * testing
    
    * accepted
    
    * standardise nomenclature (_transactions2 workstream, monospace for table)
    
    * reorg
    
    * a bit of hackery, a way of life
    
    * buffoonery
    
    * Suppress header warnings as we don't always want h1s
    
    * really
    
    * fix publish
    
    * Apply suggestions from code review
    
    Co-authored-by: Glenn Sheasby <gsheasby@palantir.com>
    
    * CR feedback
    
    Co-authored-by: Glenn Sheasby <gsheasby@palantir.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -114,7 +114,9 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = [
+    'miscellaneous/doc/adr/*.md'
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
