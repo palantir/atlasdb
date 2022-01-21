4ab415be22ae38c7abec2f4bfe094027128a34a8
Glenn Sheasby <gsheasby@palantir.com>
2022-01-21 09:54:28 +0000

    my intellij is playing up

--- timelock-impl/src/test/java/com/palantir/atlasdb/timelock/TimelockNamespacesTest.java
+++ timelock-impl/src/test/java/com/palantir/atlasdb/timelock/TimelockNamespacesTest.java
@@ -21,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
