7360661f5d0dc29649b34c08304dc1a8a30da8e3
David Schlosnagle <davids@palantir.com>
2022-01-25 12:12:30 -0500

    Use ThreadNames.setName (#5878)

--- commons-db/src/main/java/com/palantir/nexus/db/sql/BasicSQLUtils.java
+++ commons-db/src/main/java/com/palantir/nexus/db/sql/BasicSQLUtils.java
@@ -15,6 +15,7 @@
  */
 package com.palantir.nexus.db.sql;
 
+import com.palantir.common.concurrent.ThreadNames;
 import com.palantir.common.concurrent.ThreadNamingCallable;
 import com.palantir.exception.PalantirSqlException;
 import com.palantir.logsafe.Preconditions;
@@ -293,7 +294,8 @@ public class BasicSQLUtils {
         T result = null;
         final String oldName = Thread.currentThread().getName();
         final String currentTimestamp = DateTimeFormat.forPattern("HH:mm:ss").print(System.currentTimeMillis());
-        Thread.currentThread().setName(oldName + " blocking on " + threadString + " started at " + currentTimestamp);
+        ThreadNames.setThreadName(
+                Thread.currentThread(), oldName + " blocking on " + threadString + " started at " + currentTimestamp);
         try {
             long startTime = System.currentTimeMillis();
             while (true) {
@@ -308,7 +310,7 @@ public class BasicSQLUtils {
                 }
             }
         } finally {
-            Thread.currentThread().setName(oldName);
+            ThreadNames.setThreadName(Thread.currentThread(), oldName);
             if (interrupted) {
                 Thread.currentThread().interrupt();
             }
