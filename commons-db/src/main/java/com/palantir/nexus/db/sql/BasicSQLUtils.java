b41ae50df1ebe846604796f103b732e4855176d0
Jeremy Kong <jeremykong@hotmail.com>
2022-01-31 12:07:00 +0000

    Exclusion Hunter: ReturnValueIgnored and ReadReturnedValueIgnored (#5875)

--- commons-db/src/main/java/com/palantir/nexus/db/sql/BasicSQLUtils.java
+++ commons-db/src/main/java/com/palantir/nexus/db/sql/BasicSQLUtils.java
@@ -318,6 +318,7 @@ public class BasicSQLUtils {
         return result;
     }
 
+    @SuppressWarnings("ReadReturnValueIgnored") // Read is from a byte array input stream; will read everything
     public static void toStringSqlArgs(final StringBuilder sb, Object[] args) {
         if (args instanceof Object[][]) {
             // then we're doing a batch query
