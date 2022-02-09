76b9efdd95cf1b126c23f665c127a7a6a9f63585
Glenn Sheasby <gsheasby@palantir.com>
2022-02-09 10:11:06 +0000

    for local etes

--- atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/CassandraTimeLockTestSuite.java
+++ atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/CassandraTimeLockTestSuite.java
@@ -17,8 +17,6 @@ package com.palantir.atlasdb.ete;
 
 import com.google.common.collect.ImmutableList;
 import com.palantir.atlasdb.containers.CassandraEnvironment;
-import com.palantir.atlasdb.ete.coordination.CoordinationEteTest;
-import com.palantir.atlasdb.ete.coordination.MultipleSchemaVersionsCoordinationEteTest;
 import org.junit.ClassRule;
 import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
@@ -27,12 +25,12 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     TodoEteTest.class,
-    TargetedSweepEteTest.class,
-    CassandraTimestampsEteTest.class,
-    TimestampManagementEteTest.class,
-    CoordinationEteTest.class,
-    MultipleSchemaVersionsCoordinationEteTest.class,
-    LockWithTimelockEteTest.class
+    //    TargetedSweepEteTest.class,
+    //    CassandraTimestampsEteTest.class,
+    //    TimestampManagementEteTest.class,
+    //    CoordinationEteTest.class,
+    //    MultipleSchemaVersionsCoordinationEteTest.class,
+    //    LockWithTimelockEteTest.class
 })
 public class CassandraTimeLockTestSuite extends EteSetup {
     private static final ImmutableList<String> CLIENTS = ImmutableList.of("ete1");
