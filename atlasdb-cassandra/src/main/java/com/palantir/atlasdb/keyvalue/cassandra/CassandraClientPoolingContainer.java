726c96060438f211788bc81d333660924e402817
Glenn Sheasby <gsheasby@palantir.com>
2022-02-17 14:46:10 +0000

    remove debug code

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraClientPoolingContainer.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraClientPoolingContainer.java
@@ -28,7 +28,6 @@ import com.palantir.common.concurrent.ThreadNames;
 import com.palantir.common.pooling.PoolingContainer;
 import com.palantir.logsafe.SafeArg;
 import com.palantir.logsafe.UnsafeArg;
-import com.palantir.logsafe.exceptions.SafeRuntimeException;
 import com.palantir.logsafe.logger.SafeLogger;
 import com.palantir.logsafe.logger.SafeLoggerFactory;
 import com.palantir.util.TimedRunner;
@@ -227,8 +226,6 @@ public class CassandraClientPoolingContainer implements PoolingContainer<Cassand
 
     @Override
     public void shutdownPooling() {
-        SafeRuntimeException stackTrace = new SafeRuntimeException("I exist to show you the stack trace");
-        log.warn("Shutting down pooling", stackTrace);
         clientPool.close();
     }
 
