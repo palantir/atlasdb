02071458f8435a87f52a203ade694a5784467716
Mohammed Daudali <mdaudali@palantir.com>
2022-05-09 17:35:55 +0100

    Create live reloading closeable container, for use in Async KVS

---
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/cassandra/backup/ReloadingCloseableContainer.java
@@ -0,0 +1,222 @@
+/*
+ * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.cassandra.backup;
+
+import com.palantir.common.base.CallableCheckedException;
+import com.palantir.common.base.FunctionCheckedException;
+import com.palantir.common.base.RunnableCheckedException;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
+import com.palantir.logsafe.logger.SafeLogger;
+import com.palantir.logsafe.logger.SafeLoggerFactory;
+import com.palantir.refreshable.Disposable;
+import com.palantir.refreshable.Refreshable;
+import com.palantir.util.Pair;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Function;
+import javax.annotation.concurrent.GuardedBy;
+
+/**
+ * Guarantees:
+ * 1. A resource will never be closed while that resource is in scope of an invocation of {@link #runWithResource}.
+ * 2. After a refresh, the new resource created must reflect the refreshed changes and not be closed.
+ * 3. After a refresh, there exists a time T when all resources except the latest resource is closed.
+ * 4. After {@link #close} has returned, the latest resource must be closed, and no further resources will be created.
+ *    (With 3, there exists a time T after {@link #close} has been called where all the resources are closed)
+ * 5. After {@link #close} has returned, all subsequent calls to runWith will fail.
+ *
+ * <b>When should you use this class?</b>
+ * You should use this when:
+ * 1. A resource is created from some live reloaded value, and new resources should be created when said value is
+ * updated.
+ * 2. The cost of retrying if the underlying resource is closed underneath you in an operation is expensive / not
+ * possible, OR there's risk of starvation (e.g with Cassandra, the topology changing in quick succession could
+ * starve existing connections if they're closed on every change)
+ * 3. It's acceptable for inflight consumers to use a stale resource (e.g at a temporary performance penalty for
+ * redirects)
+ *
+ */
+public final class ReloadingCloseableContainer<T extends Closeable> implements Closeable {
+    private static final SafeLogger log = SafeLoggerFactory.get(ReloadingCloseableContainer.class);
+
+    // This lock is marked fair to prevent starving #close. After close is called, no further #runWith
+    // calls will acquire the resource lock.
+    private final ReadWriteLock isClosedLock = new ReentrantReadWriteLock(true);
+    private final AtomicReference<Optional<Pair<ReadWriteLock, T>>> currentResourceWithLock =
+            new AtomicReference<>(Optional.empty());
+
+    private final Refreshable<Pair<ReadWriteLock, T>> refreshableResourceWithLock;
+    private final Disposable refreshableSubscriptionDisposable;
+
+    @GuardedBy("isClosedLock")
+    private volatile boolean isClosed;
+
+    public static <R, T extends Closeable> ReloadingCloseableContainer<T> of(
+            Function<R, T> resourceFactory, Refreshable<R> refreshableFactoryParameter) {
+        return new ReloadingCloseableContainer<>(resourceFactory, refreshableFactoryParameter);
+    }
+
+    private <R> ReloadingCloseableContainer(
+            Function<R, T> resourceFactory, Refreshable<R> refreshableFactoryParameter) {
+        isClosed = false;
+        refreshableResourceWithLock = refreshableFactoryParameter.map(factoryParameter -> runIfNotClosed(() -> {
+            T resource = resourceFactory.apply(factoryParameter);
+            // This lock does not need to be fair, since the isClosed lock is fair (preventing #close from being
+            // starved) and since subscribe happens after a map (and thus a new resource is created before attempting
+            // to close the old), there will not be any more readers of a resource about to be closed.
+            ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+            return new Pair<>(readWriteLock, resource);
+        }));
+
+        // The old resource is closed in a subscriber (rather than map) so that attempting to close the old resource
+        // happens after the refreshable was updated to use a new resource. This avoids a situation where
+        // a #runWithResource call using a resource that is closed, violating guarantee 1 (between the close lock
+        // being released and map completing the update).
+
+        // Furthermore, we don't want updates to have lock contention with a #runWithResource call:
+
+        // Consider the case of a long-running #runWithResource call. The current resource lock is held.
+        // If we attempt to close the resource in the map, then this must occur before refreshableResourceWithLock is
+        // updated (assuming we don't run close asynchronously). As close holds the write lock for the
+        // resource, subsequent #runWithResource calls would now block on acquiring the lock. This would be doubly
+        // damaging if the resource lock was fair, as now #runWithResource calls are blocked until all calls prior to
+        // closing the resource have finished.
+
+        // To avoid this, we must not block an update with close. This can either be done with another thread, or
+        // simply using a subscriber on the updated resource.
+        refreshableSubscriptionDisposable = refreshableResourceWithLock.subscribe(resourceWithLock -> {
+            Optional<Pair<ReadWriteLock, T>> maybeResourceWithLockToClose =
+                    currentResourceWithLock.getAndSet(Optional.of(resourceWithLock));
+            maybeResourceWithLockToClose.ifPresent(this::closeResourceAndLock);
+        });
+    }
+
+    private void closeResourceAndLock(Pair<ReadWriteLock, T> resourceWithLockToClose) {
+        Lock writeLock = resourceWithLockToClose.getLhSide().writeLock();
+        runWithLock(writeLock, () -> {
+            try {
+                T resourceToClose = resourceWithLockToClose.getRhSide();
+                resourceToClose.close();
+            } catch (IOException e) {
+                log.warn("Failed to close resource. This may result in a resource leak", e);
+            }
+        });
+    }
+
+    /***
+     * Closes the container and the latest created resource.
+     * After this method has returned, no further resources will be created in this container, and all subsequent
+     * {@link #runWithResource} invocations will fail with a {@link SafeIllegalStateException}.
+     *
+     * Note: This method does <b>block</b> on any inflight {@link #runWithResource} invocations.
+     *
+     * Any {@link IOException} from closing the underlying resource will not be propagated.
+     *
+     * See {@link Closeable#close()}.
+     */
+    @Override
+    public void close() {
+        Lock isClosedWriteLock = isClosedLock.writeLock();
+        runWithLock(isClosedWriteLock, () -> {
+            isClosed = true;
+            currentResourceWithLock.get().ifPresent(this::closeResourceAndLock);
+            refreshableSubscriptionDisposable.dispose();
+        });
+    }
+
+    /***
+     * Returns the result of `task` applied with the current resource.
+     * This resource is guaranteed not to be closed within scope of this function <i>by this container</i>.
+     * There are no guarantees on the resource being closed externally e.g. if there is a shared reference, nor if the
+     * resource is closed within scope by the task itself.
+     *
+     * If an update to the refreshable occurs during an invocation, any <i>inflight</i> invocations will <i>not</i> use
+     * the new resource.
+     *
+     * <b>Do not</b> block on close or update the refreshable within the scope of runWith. Doing so will result in a
+     * deadlock.
+     *
+     * There are no timeouts applied to these invocations. If required, consider using the
+     * {@link com.google.common.util.concurrent.TimeLimiter} utilities. Long-running invocations will block close.
+     *
+     * @param task The function to execute using the current resource. This function may throw a checked exception,
+     *             and this exception will propagate.
+     * @param <V> The return type of the task
+     * @param <K> The checked exception type, if any. If multiple checked exceptions are thrown, this will resolve to
+     *          the lowest common ancestor, which may be {@link Exception}
+     * @return The result of `task`
+     * @throws K Any checked exceptions thrown by the underlying task.
+     */
+    public <V, K extends Exception> V runWithResource(FunctionCheckedException<T, V, K> task) throws K {
+        return runIfNotClosed(() -> {
+            Pair<ReadWriteLock, T> resourceWithLock = refreshableResourceWithLock.get();
+            Lock readLock = resourceWithLock.getLhSide().readLock();
+            return runWithLock(readLock, () -> task.apply(resourceWithLock.getRhSide()));
+        });
+    }
+
+    /**
+     * Utility method for {@link #runWithResource} for tasks that have no return value.
+     *
+     * See {@link #runWithResource} for semantics, guarantees and conditions.
+     *
+     */
+    public <K extends Exception> void executeWithResource(ConsumerCheckedException<T, K> task) throws K {
+        runWithResource(resource -> {
+            task.accept(resource);
+            return null;
+        });
+    }
+
+    private static <V, K extends Exception> V runWithLock(Lock lock, CallableCheckedException<V, K> task) throws K {
+        lock.lock();
+        try {
+            return task.call();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private static <K extends Exception> void runWithLock(Lock lock, RunnableCheckedException<K> task) throws K {
+        runWithLock(lock, () -> {
+            task.run();
+            return null;
+        });
+    }
+
+    private <V, K extends Exception> V runIfNotClosed(CallableCheckedException<V, K> task) throws K {
+        Lock readLock = isClosedLock.readLock();
+        return runWithLock(readLock, () -> {
+            if (isClosed) {
+                // This exception bubbles up to the caller of #runWithResource, and blocks an update for the
+                // Refreshable#map call.
+                throw new SafeIllegalStateException("Attempted to use resource after the container was closed.");
+            }
+            return task.call();
+        });
+    }
+
+    @FunctionalInterface
+    public interface ConsumerCheckedException<T, K extends Exception> {
+        void accept(T input) throws K;
+    }
+}
---
+++ atlasdb-cassandra/src/test/java/com/palantir/atlasdb/cassandra/backup/ReloadingCloseableContainerTest.java
@@ -0,0 +1,327 @@
+/*
+ * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.cassandra.backup;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import com.palantir.common.concurrent.PTExecutors;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
+import com.palantir.refreshable.Refreshable;
+import com.palantir.refreshable.SettableRefreshable;
+import java.io.Closeable;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import org.awaitility.Awaitility;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReloadingCloseableContainerTest {
+    private static final ExecutorService executor =
+            PTExecutors.newCachedThreadPool(ReloadingCloseableContainerTest.class.getName());
+
+    private static final int INITIAL_VALUE = 0;
+    private static final int UPDATED_VALUE = 1;
+
+    @Mock
+    private Closeable mockCloseable;
+
+    @Mock
+    private Closeable mockCloseableExtra;
+
+    @Mock
+    private Function<Integer, Closeable> closeableFactory;
+
+    private SettableRefreshable<Integer> settableRefreshable;
+
+    @Before
+    public void setUp() {
+        settableRefreshable = Refreshable.create(INITIAL_VALUE);
+        when(closeableFactory.apply(INITIAL_VALUE)).thenReturn(mockCloseable);
+        when(closeableFactory.apply(UPDATED_VALUE)).thenReturn(mockCloseableExtra);
+    }
+
+    @Test
+    public void closeBlocksDuringRunWithInvocation() throws BrokenBarrierException, InterruptedException {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+        CyclicBarrier runWithFutureRunsFirstBarrier = new CyclicBarrier(2);
+        CyclicBarrier manuallyFinishRunWith = new CyclicBarrier(2);
+
+        executor.submit(() -> container.executeWithResource(closeable -> {
+            awaitOrFail(runWithFutureRunsFirstBarrier);
+            awaitOrFail(manuallyFinishRunWith);
+            verifyNoInteractions(closeable);
+        }));
+
+        // This test could pass spuriously, if close is just not called.
+        // The likelihood if this is minimised by
+        // 1. waiting on a barrier for the executeWithResource and,
+        // 2. Requiring a whole second of close not completing.
+        Future<?> closeFuture = executor.submit(() -> {
+            awaitOrFail(runWithFutureRunsFirstBarrier);
+            container.close();
+        });
+        Awaitility.await().during(Duration.ofSeconds(1)).until(() -> !closeFuture.isDone());
+        verifyNoInteractions(mockCloseable);
+
+        manuallyFinishRunWith.await();
+        Awaitility.await().atMost(Duration.ofSeconds(1)).until(closeFuture::isDone);
+    }
+
+    @Test
+    public void refreshCloseBlockedDuringRunWithInvocation()
+            throws IOException, BrokenBarrierException, InterruptedException {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+
+        CyclicBarrier runWithFutureRunsFirstBarrier = new CyclicBarrier(2);
+        CyclicBarrier manuallyFinishRunWith = new CyclicBarrier(2);
+
+        executor.submit(() -> container.executeWithResource(closeable -> {
+            awaitOrFail(runWithFutureRunsFirstBarrier);
+            awaitOrFail(manuallyFinishRunWith);
+            verifyNoInteractions(closeable);
+        }));
+
+        Future<?> refreshableFuture = executor.submit(() -> {
+            awaitOrFail(runWithFutureRunsFirstBarrier);
+            settableRefreshable.update(UPDATED_VALUE);
+        });
+
+        Awaitility.await().during(Duration.ofSeconds(1)).until(() -> !refreshableFuture.isDone());
+        verifyNoInteractions(mockCloseable);
+
+        manuallyFinishRunWith.await();
+
+        Awaitility.await().atMost(Duration.ofSeconds(1)).until(refreshableFuture::isDone);
+        verify(mockCloseable).close();
+    }
+
+    @Test
+    public void refreshUpdatesResource() {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+
+        container.executeWithResource(closeable -> assertThat(closeable).isEqualTo(mockCloseable));
+
+        settableRefreshable.update(UPDATED_VALUE);
+        container.executeWithResource(closeable -> assertThat(closeable).isEqualTo(mockCloseableExtra));
+    }
+
+    @Test
+    public void previousResourceClosedAfterRefresh() throws IOException {
+        // Creating a container has the side effect of managing all resources created within it.
+        getContainer();
+
+        verifyNoInteractions(mockCloseable);
+        settableRefreshable.update(UPDATED_VALUE);
+        verify(mockCloseable).close();
+    }
+
+    @Test
+    public void latestResourceClosedAfterClose() throws IOException {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+
+        verifyNoInteractions(mockCloseable);
+        settableRefreshable.update(UPDATED_VALUE);
+        container.close();
+
+        verify(mockCloseableExtra).close();
+    }
+
+    @Test
+    public void noFurtherResourceCreatedAfterClose() {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+
+        verifyNoInteractions(mockCloseable);
+        container.close();
+        settableRefreshable.update(UPDATED_VALUE);
+
+        verify(closeableFactory, times(1)).apply(any());
+    }
+
+    @Test
+    public void runWithResourceFailsAfterClose() {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+
+        container.close();
+
+        assertThatThrownBy(() -> container.runWithResource(_closeable -> null))
+                .isInstanceOf(SafeIllegalStateException.class);
+    }
+
+    @Test
+    public void closeReleasesLocksAfterException() throws IOException {
+        doThrow(new SafeIllegalStateException()).when(mockCloseable).close();
+
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+        assertThatThrownBy(container::close).isInstanceOf(SafeIllegalStateException.class);
+
+        Future<?> future = executor.submit(container::close);
+        Awaitility.await().atMost(Duration.ofSeconds(1)).until(future::isDone);
+    }
+
+    @Test
+    public void runWithResourceReleasesLocksAfterExceptionInTask() {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+
+        assertThatThrownBy(() -> container.executeWithResource(_closeable -> {
+                    throw new SafeIllegalStateException("Test");
+                }))
+                .isInstanceOf(SafeIllegalStateException.class);
+
+        Future<?> closeFuture = executor.submit(container::close);
+        Awaitility.await().atMost(Duration.ofSeconds(1)).until(closeFuture::isDone);
+    }
+
+    @Test
+    public void concurrentRunWithWithoutBlocking() {
+        int numberOfRuns = 100;
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+        CyclicBarrier allEnteredRunWith = new CyclicBarrier(numberOfRuns);
+
+        CompletableFuture<?> allFutures = runManyTimesAndGetSingleFuture(
+                numberOfRuns, () -> container.executeWithResource(_closeable -> awaitOrFail(allEnteredRunWith)));
+
+        Awaitility.await().atMost(Duration.ofSeconds(2)).until(allFutures::isDone);
+    }
+
+    @Test
+    public void closeSuppressesUnderlyingIoException() throws IOException {
+        doThrow(new IOException()).when(mockCloseable).close();
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+        container.close();
+    }
+
+    @Test
+    public void blockingMethodsDontDeadlockUnderConcurrentLoad() {
+        int numberOfRuns = 2000;
+
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+
+        AtomicInteger refreshableValue = new AtomicInteger(INITIAL_VALUE);
+        CompletableFuture<?> allFutures =
+                runManyTimesAndGetSingleFuture(numberOfRuns, () -> triggerEvent(container, refreshableValue));
+
+        Awaitility.await().atMost(Duration.ofSeconds(2)).until(allFutures::isDone);
+
+        Future<?> future = executor.submit(container::close);
+        Awaitility.await().atMost(Duration.ofSeconds(1)).until(future::isDone);
+    }
+
+    @Test
+    public void runWithResourceReturnsFunctionValue() {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+        int expectedValue = 3213;
+        int returnedValue = container.runWithResource(_closeable -> expectedValue);
+        assertThat(returnedValue).isEqualTo(expectedValue);
+    }
+
+    @Test
+    public void runWithResourceRunsWithLatestResource() {
+        ReloadingCloseableContainer<Closeable> container =
+                ReloadingCloseableContainer.of(closeableFactory, settableRefreshable);
+        settableRefreshable.update(UPDATED_VALUE);
+        container.executeWithResource(closeable -> assertThat(closeable).isEqualTo(mockCloseableExtra));
+    }
+
+    @Test
+    public void runWithResourcePropagatesException() {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+        assertThatThrownBy(() -> container.runWithResource(_closeable -> {
+                    throw new SafeIllegalStateException("Test");
+                }))
+                .isInstanceOf(SafeIllegalStateException.class);
+    }
+
+    @Test
+    public void runWithDoesNotBlockResourceUpdates() {
+        ReloadingCloseableContainer<Closeable> container = getContainer();
+        CyclicBarrier ensureInitialExecuteHasStartedBarrier = new CyclicBarrier(2);
+        CyclicBarrier manuallyFinishRunWithBarrier = new CyclicBarrier(2);
+        executor.submit(() -> container.executeWithResource(closeable -> {
+            awaitOrFail(ensureInitialExecuteHasStartedBarrier);
+            awaitOrFail(manuallyFinishRunWithBarrier);
+            assertThat(closeable).isEqualTo(mockCloseable);
+        }));
+
+        awaitOrFail(ensureInitialExecuteHasStartedBarrier);
+        // Map and subscriber updates happen sequentially, but on the same thread. Therefore, the next call won't
+        // complete until after the execute above completes, even if the map itself has finished.
+        executor.submit(() -> settableRefreshable.update(UPDATED_VALUE));
+
+        // The earlier executeWithResource call cannot complete yet, and so if the update was blocked, this following
+        // assertion would fail.
+
+        // It is possible for this to fail, if this call ends up scheduled before the update has started...
+        Future<?> executeWithResourceFuture = executor.submit(() ->
+                container.executeWithResource(closeable -> assertThat(closeable).isEqualTo(mockCloseableExtra)));
+
+        Awaitility.await().atMost(Duration.ofSeconds(1)).until(executeWithResourceFuture::isDone);
+        awaitOrFail(manuallyFinishRunWithBarrier);
+    }
+
+    private void triggerEvent(ReloadingCloseableContainer<Closeable> container, AtomicInteger refreshableValue) {
+        double random = ThreadLocalRandom.current().nextDouble();
+        if (random < 0.2) {
+            settableRefreshable.update(refreshableValue.incrementAndGet());
+        } else {
+            try {
+                container.executeWithResource(_closeable -> {});
+            } catch (Throwable t) {
+                fail("Triggering an event caused an exception");
+            }
+        }
+    }
+
+    private CompletableFuture<?> runManyTimesAndGetSingleFuture(int numberOfRuns, Runnable task) {
+        CompletableFuture<?>[] allFutures = IntStream.range(0, numberOfRuns)
+                .mapToObj(_id -> CompletableFuture.runAsync(task, executor))
+                .toArray(CompletableFuture[]::new);
+        return CompletableFuture.anyOf(allFutures);
+    }
+
+    private ReloadingCloseableContainer<Closeable> getContainer() {
+        return ReloadingCloseableContainer.of(closeableFactory, settableRefreshable);
+    }
+
+    private static void awaitOrFail(CyclicBarrier barrier) {
+        try {
+            barrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            fail("Failed to await barrier");
+        }
+    }
+}
