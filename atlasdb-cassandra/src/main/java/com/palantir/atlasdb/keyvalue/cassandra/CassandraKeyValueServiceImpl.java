d3d8d1be6fbfffa25fbebd397ac132d6069ac1cd
Mohammed Daudali <mdaudali@palantir.com>
2022-05-18 17:25:19 +0100

    runtime exception catching

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueServiceImpl.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueServiceImpl.java
@@ -2094,7 +2094,7 @@ public class CassandraKeyValueServiceImpl extends AbstractKeyValueService implem
             try {
                 return Futures.catching(
                         asyncKeyValueService.getAsync(tableRef, timestampByCell),
-                        IllegalStateException.class,
+                        RuntimeException.class,
                         e -> {
                             log.warn(
                                     "CQL Client closed during getAsync. Delegating to synchronous get. This should be"
