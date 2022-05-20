f8febe4b86401b623a0b1d45769e0b686813bc09
Glenn Sheasby <gsheasby@palantir.com>
2022-05-20 17:21:40 +0100

    useful log

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/cassandra/backup/CassandraRepairHelper.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/cassandra/backup/CassandraRepairHelper.java
@@ -121,7 +121,9 @@ public class CassandraRepairHelper {
                 getRangesForRepairByTable(atlasService, transactionsTableInteractions);
 
         tokenRangesForRepair.forEach((table, ranges) -> {
-            log.info("Repairing ranges for table", SafeArg.of("table", table));
+            log.info("Repairing ranges for table",
+                    SafeArg.of("namespace", atlasService.getNamespace()),
+                    SafeArg.of("table", table));
             repairTable.accept(table, ranges);
         });
     }
