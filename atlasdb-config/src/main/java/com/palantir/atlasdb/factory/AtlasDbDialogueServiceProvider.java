5b9c4da365dc85110922c9d40e8834f49ac98355
Jolyon Shah <jshah@palantir.com>
2022-05-31 11:28:44 +0100

    config dump on unknown remote exception

--- atlasdb-config/src/main/java/com/palantir/atlasdb/factory/AtlasDbDialogueServiceProvider.java
+++ atlasdb-config/src/main/java/com/palantir/atlasdb/factory/AtlasDbDialogueServiceProvider.java
@@ -32,6 +32,7 @@ import com.palantir.atlasdb.http.v2.FastFailoverProxy;
 import com.palantir.atlasdb.http.v2.ImmutableRemoteServiceConfiguration;
 import com.palantir.atlasdb.http.v2.RemoteServiceConfiguration;
 import com.palantir.atlasdb.http.v2.RetryOnSocketTimeoutExceptionProxy;
+import com.palantir.atlasdb.http.v2.UnknownRemoteDebuggingProxy;
 import com.palantir.atlasdb.timelock.api.ConjureTimelockService;
 import com.palantir.atlasdb.timelock.api.ConjureTimelockServiceBlocking;
 import com.palantir.atlasdb.timelock.api.MultiClientConjureTimelockServiceBlocking;
@@ -44,6 +45,7 @@ import com.palantir.conjure.java.client.config.ClientConfiguration;
 import com.palantir.conjure.java.client.config.NodeSelectionStrategy;
 import com.palantir.dialogue.Channel;
 import com.palantir.dialogue.clients.DialogueClients;
+import com.palantir.dialogue.clients.DialogueClients.ReloadingFactory;
 import com.palantir.lock.ConjureLockV1ServiceBlocking;
 import com.palantir.lock.LockRpcClient;
 import com.palantir.lock.client.ConjureTimelockServiceBlockingMetrics;
@@ -72,11 +74,15 @@ public final class AtlasDbDialogueServiceProvider {
 
     private final DialogueClients.ReloadingFactory dialogueClientFactory;
     private final TaggedMetricRegistry taggedMetricRegistry;
+    private final Refreshable<ServerListConfig> timeLockServerListConfig;
 
     private AtlasDbDialogueServiceProvider(
-            DialogueClients.ReloadingFactory dialogueClientFactory, TaggedMetricRegistry taggedMetricRegistry) {
+            ReloadingFactory dialogueClientFactory,
+            TaggedMetricRegistry taggedMetricRegistry,
+            Refreshable<ServerListConfig> timeLockServerListConfig) {
         this.dialogueClientFactory = dialogueClientFactory;
         this.taggedMetricRegistry = taggedMetricRegistry;
+        this.timeLockServerListConfig = timeLockServerListConfig;
     }
 
     public static AtlasDbDialogueServiceProvider create(
@@ -92,7 +98,7 @@ public final class AtlasDbDialogueServiceProvider {
                         baseFactory, timeLockRemoteConfigurations)
                 .withUserAgent(versionedAgent);
 
-        return new AtlasDbDialogueServiceProvider(reloadingFactory, taggedMetricRegistry);
+        return new AtlasDbDialogueServiceProvider(reloadingFactory, taggedMetricRegistry, timeLockServerListConfig);
     }
 
     public ConjureTimelockService getConjureTimelockService() {
@@ -185,9 +191,12 @@ public final class AtlasDbDialogueServiceProvider {
         return AtlasDbHttpClients.createDialogueProxy(taggedMetricRegistry, type, channel);
     }
 
-    private static <T> T wrapInProxy(Class<T> type, T service) {
-        return RetryOnSocketTimeoutExceptionProxy.newProxyInstance(
-                type, () -> FastFailoverProxy.newProxyInstance(type, () -> service));
+    private <T> T wrapInProxy(Class<T> type, T service) {
+        return UnknownRemoteDebuggingProxy.newProxyInstance(
+                type,
+                timeLockServerListConfig.map(ServerListConfig::servers),
+                RetryOnSocketTimeoutExceptionProxy.newProxyInstance(
+                        type, () -> FastFailoverProxy.newProxyInstance(type, () -> service)));
     }
 
     private static ImmutableMap<String, RemoteServiceConfiguration> getServiceConfigurations(
---
+++ atlasdb-conjure/src/main/java/com/palantir/atlasdb/http/v2/UnknownRemoteDebuggingProxy.java
@@ -0,0 +1,65 @@
+/*
+ * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.http.v2;
+
+import com.google.common.reflect.AbstractInvocationHandler;
+import com.google.common.util.concurrent.RateLimiter;
+import com.palantir.conjure.java.api.errors.UnknownRemoteException;
+import com.palantir.logsafe.Safe;
+import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.logger.SafeLogger;
+import com.palantir.logsafe.logger.SafeLoggerFactory;
+import com.palantir.refreshable.Refreshable;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import javax.annotation.CheckForNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public final class UnknownRemoteDebuggingProxy<T, V> extends AbstractInvocationHandler {
+    private static final SafeLogger log = SafeLoggerFactory.get(UnknownRemoteDebuggingProxy.class);
+    private final Refreshable<T> safeLoggableRefreshable;
+    private final RateLimiter rateLimiter;
+    private final V delegate;
+
+    private UnknownRemoteDebuggingProxy(@Safe Refreshable<T> safeLoggableRefreshable, V delegate) {
+        this.safeLoggableRefreshable = safeLoggableRefreshable;
+        this.delegate = delegate;
+        this.rateLimiter = RateLimiter.create(0.05);
+    }
+
+    @CheckForNull
+    @Override
+    protected Object handleInvocation(Object proxy, Method method, @Nullable Object[] args) throws Throwable {
+        try {
+            return method.invoke(delegate, args);
+        } catch (UnknownRemoteException e) {
+            if (rateLimiter.tryAcquire()) {
+                log.warn(
+                        "Encountered UnknownRemoteException; logging current state of refreshable",
+                        SafeArg.of("safeLoggableRefreshable", safeLoggableRefreshable.get()));
+            }
+            throw e;
+        }
+    }
+
+    public static <T, U> U newProxyInstance(
+            Class<U> interfaceClass, @Safe Refreshable<T> safeLoggableRefreshable, U delegate) {
+        UnknownRemoteDebuggingProxy<T, U> proxy = new UnknownRemoteDebuggingProxy<>(safeLoggableRefreshable, delegate);
+
+        return (U) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class<?>[] {interfaceClass}, proxy);
+    }
+}
