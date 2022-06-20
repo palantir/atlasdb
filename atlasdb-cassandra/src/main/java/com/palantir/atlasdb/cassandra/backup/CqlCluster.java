b331e7cd2d619313e42439311f12b7b294b934c7
Glenn Sheasby <gsheasby@palantir.com>
2022-06-20 15:08:22 +0100

    retry session creation

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/cassandra/backup/CqlCluster.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/cassandra/backup/CqlCluster.java
@@ -17,6 +17,11 @@
 package com.palantir.atlasdb.cassandra.backup;
 
 import com.datastax.driver.core.Cluster;
+import com.github.rholder.retry.Attempt;
+import com.github.rholder.retry.RetryException;
+import com.github.rholder.retry.Retryer;
+import com.github.rholder.retry.StopStrategies;
+import com.github.rholder.retry.WaitStrategies;
 import com.google.common.collect.RangeSet;
 import com.palantir.atlasdb.cassandra.CassandraServersConfigs.CassandraServersConfig;
 import com.palantir.atlasdb.cassandra.backup.transaction.TransactionsTableInteraction;
@@ -24,22 +29,33 @@ import com.palantir.atlasdb.keyvalue.cassandra.LightweightOppToken;
 import com.palantir.atlasdb.keyvalue.cassandra.async.client.creation.ClusterFactory;
 import com.palantir.atlasdb.keyvalue.cassandra.async.client.creation.ClusterFactory.CassandraClusterConfig;
 import com.palantir.atlasdb.timelock.api.Namespace;
+import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public final class CqlCluster implements Closeable {
+    private static final int RETRY_COUNT = 5;
+
     private final Cluster cluster;
     private final CassandraServersConfig cassandraServersConfig;
     private final Namespace namespace;
+    private final Retryer<CqlSession> cqlSessionRetryer;
 
     // VisibleForTesting
     public CqlCluster(Cluster cluster, CassandraServersConfig cassandraServersConfig, Namespace namespace) {
         this.cluster = cluster;
         this.cassandraServersConfig = cassandraServersConfig;
         this.namespace = namespace;
+        this.cqlSessionRetryer = new Retryer<>(
+                StopStrategies.stopAfterAttempt(RETRY_COUNT),
+                WaitStrategies.fixedWait(10L, TimeUnit.SECONDS),
+                Attempt::hasResult);
     }
 
     public static CqlCluster create(
@@ -57,22 +73,34 @@ public final class CqlCluster implements Closeable {
     }
 
     public Map<InetSocketAddress, RangeSet<LightweightOppToken>> getTokenRanges(String tableName) {
-        try (CqlSession session = new CqlSession(cluster.connect())) {
+        try (CqlSession session = createSessionWithRetry()) {
             return new TokenRangeFetcher(session, namespace, cassandraServersConfig).getTokenRange(tableName);
         }
     }
 
     public Map<String, Map<InetSocketAddress, RangeSet<LightweightOppToken>>> getTransactionsTableRangesForRepair(
             List<TransactionsTableInteraction> transactionsTableInteractions) {
-        try (CqlSession session = new CqlSession(cluster.connect())) {
+        try (CqlSession session = createSessionWithRetry()) {
             return new RepairRangeFetcher(session, namespace, cassandraServersConfig)
                     .getTransactionTableRangesForRepair(transactionsTableInteractions);
         }
     }
 
     public void abortTransactions(long timestamp, List<TransactionsTableInteraction> transactionsTableInteractions) {
-        try (CqlSession session = new CqlSession(cluster.connect())) {
+        try (CqlSession session = createSessionWithRetry()) {
             new TransactionAborter(session, namespace).abortTransactions(timestamp, transactionsTableInteractions);
+        }
+    }
+
+    private CqlSession createSessionWithRetry() {
+        try {
+            return cqlSessionRetryer.call(() -> new CqlSession(cluster.connect()));
+        } catch (ExecutionException e) {
+            throw new SafeIllegalStateException(
+                    "Failed to execute CqlSession connect", e, SafeArg.of("namespace", namespace));
+        } catch (RetryException e) {
+            throw new SafeIllegalStateException(
+                    "Failed to execute CqlSession connect even with retry", e, SafeArg.of("namespace", namespace));
         }
     }
 }
