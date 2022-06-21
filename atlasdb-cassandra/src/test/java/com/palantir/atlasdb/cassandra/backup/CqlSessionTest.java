bf345974dc677553b8dede5e50e77b59e60a371b
Glenn Sheasby <gsheasby@palantir.com>
2022-06-21 13:32:06 +0100

    more retry testing

--- atlasdb-cassandra/src/test/java/com/palantir/atlasdb/cassandra/backup/CqlSessionTest.java
+++ atlasdb-cassandra/src/test/java/com/palantir/atlasdb/cassandra/backup/CqlSessionTest.java
@@ -24,10 +24,12 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.KeyspaceMetadata;
 import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.TableMetadata;
 import com.datastax.driver.core.exceptions.NoHostAvailableException;
 import com.google.common.collect.ImmutableMap;
 import com.palantir.atlasdb.timelock.api.Namespace;
 import java.time.Duration;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -35,25 +37,57 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CqlSessionTest {
+    private static final Namespace NAMESPACE = Namespace.of("namespace");
+
     @Mock
     private Cluster cluster;
 
-    @Test
-    public void retriesCreation() {
-        Namespace namespace = Namespace.of("namespace");
+    @Mock
+    private Session session;
 
-        Session session = mock(Session.class);
-        Metadata metadata = mock(Metadata.class);
+    @Mock
+    private Metadata metadata;
+
+    @Mock
+    private KeyspaceMetadata keyspaceMetadata;
+
+    @Before
+    public void before() {
         when(session.getCluster()).thenReturn(cluster);
         when(cluster.getMetadata()).thenReturn(metadata);
-        KeyspaceMetadata keyspaceMetadata = mock(KeyspaceMetadata.class);
-        when(metadata.getKeyspace(namespace.value())).thenReturn(keyspaceMetadata);
+    }
 
+    @Test
+    public void retriesCreation() {
+        when(metadata.getKeyspace(NAMESPACE.value())).thenReturn(keyspaceMetadata);
         when(cluster.connect())
                 .thenThrow(new NoHostAvailableException(ImmutableMap.of()))
                 .thenReturn(session);
 
-        assertThatCode(() -> CqlSession.create(cluster, namespace, Duration.ofMillis(50L)))
+        assertThatCode(() -> CqlSession.create(cluster, NAMESPACE, Duration.ofMillis(50L)))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void retriesCreationWhenKeyspaceMetadataNotFound() {
+        when(cluster.connect()).thenReturn(session);
+        when(metadata.getKeyspace(NAMESPACE.value())).thenReturn(null).thenReturn(keyspaceMetadata);
+
+        assertThatCode(() -> CqlSession.create(cluster, NAMESPACE, Duration.ofMillis(50L)))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void retriesFetchingTableMetadata() {
+        String tableName = "table";
+
+        when(cluster.connect()).thenReturn(session);
+        when(metadata.getKeyspace(NAMESPACE.value())).thenReturn(keyspaceMetadata);
+        TableMetadata tableMetadata = mock(TableMetadata.class);
+        when(keyspaceMetadata.getTable(tableName)).thenReturn(null).thenReturn(tableMetadata);
+
+        CqlSession cqlSession = CqlSession.create(cluster, NAMESPACE, Duration.ofMillis(50L));
+
+        assertThatCode(() -> cqlSession.getTableMetadata(tableName)).doesNotThrowAnyException();
     }
 }
