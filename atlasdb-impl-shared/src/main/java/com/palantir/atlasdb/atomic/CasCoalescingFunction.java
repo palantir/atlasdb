158fe76f223c860dafb51de5db1bfb510d09ffe1
Sudiksha Nanda <snanda@palantir.com>
2022-08-25 13:38:42 +0100

    wip test

---
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/atomic/CasCoalescingFunction.java
@@ -0,0 +1,2 @@
+package com.palantir.atlasdb.atomic;public class CasCoalescingFunction {
+}
--- atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/atomic/MarkAndCasConsensusForgettingStoreTest.java
+++ atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/atomic/MarkAndCasConsensusForgettingStoreTest.java
@@ -18,30 +18,47 @@ package com.palantir.atlasdb.atomic;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.palantir.atlasdb.encoding.PtBytes;
 import com.palantir.atlasdb.keyvalue.api.Cell;
 import com.palantir.atlasdb.keyvalue.api.CheckAndSetException;
+import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.keyvalue.api.Value;
 import com.palantir.atlasdb.keyvalue.impl.InMemoryKeyValueService;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.palantir.atlasdb.transaction.impl.TransactionConstants;
 import org.junit.Test;
 
 public class MarkAndCasConsensusForgettingStoreTest {
     private static final byte[] SAD = PtBytes.toBytes("sad");
     private static final byte[] HAPPY = PtBytes.toBytes("happy");
-    private static final Cell CELL = Cell.create(PtBytes.toBytes("a"), PtBytes.toBytes("b"));
+    private static final Cell CELL = Cell.create(PtBytes.toBytes("r"), PtBytes.toBytes("col1"));
+    private static final Cell CELL_2 = Cell.create(PtBytes.toBytes("r"), PtBytes.toBytes("col2"));
     public static final TableReference TABLE = TableReference.createFromFullyQualifiedName("test.table");
-    private static final byte[] IN_PROGRESS_MARKER = new byte[0];
+    private static final byte[] IN_PROGRESS_MARKER = new byte[] {0};
 
-    private final InMemoryKeyValueService kvs = new InMemoryKeyValueService(true);
+    private final InMemoryKeyValueService kvs = spy(new InMemoryKeyValueService(true));
     private final MarkAndCasConsensusForgettingStore store =
             new MarkAndCasConsensusForgettingStore(IN_PROGRESS_MARKER, kvs, TABLE);
+//    private final CasCoalescingFunction casCoalescingFunction =
 
     @Test
     public void canMarkCell() throws ExecutionException, InterruptedException {
-
         store.mark(CELL);
         assertThat(store.get(CELL).get()).hasValue(IN_PROGRESS_MARKER);
         assertThat(kvs.getAllTimestamps(TABLE, ImmutableSet.of(CELL), Long.MAX_VALUE)
@@ -57,6 +74,15 @@ public class MarkAndCasConsensusForgettingStoreTest {
     }
 
     @Test
+    public void updatesMultipleMarkedCells() throws ExecutionException, InterruptedException {
+        store.mark(ImmutableSet.of(CELL, CELL_2));
+
+        store.atomicUpdate(ImmutableMap.of(CELL, HAPPY, CELL_2, HAPPY));
+        assertThat(store.get(CELL).get()).hasValue(HAPPY);
+        assertThat(store.get(CELL_2).get()).hasValue(HAPPY);
+    }
+
+    @Test
     public void cannotUpdateUnmarkedCell() throws ExecutionException, InterruptedException {
         assertThatThrownBy(() -> store.atomicUpdate(CELL, SAD))
                 .isInstanceOf(CheckAndSetException.class)
@@ -64,5 +90,73 @@ public class MarkAndCasConsensusForgettingStoreTest {
                         "Unexpected value observed in table test.table. If this is happening repeatedly, your program"
                                 + " may be out of sync with the database");
         assertThat(store.get(CELL).get()).isEmpty();
+    }
+
+    @Test
+    public void coalescesMultipleMcasCalls() throws ExecutionException, InterruptedException {
+        KeyValueService kvs = mock(KeyValueService.class);
+        MarkAndCasConsensusForgettingStore store =
+                new MarkAndCasConsensusForgettingStore(IN_PROGRESS_MARKER, kvs, TABLE);
+
+        ExecutorService service = Executors.newCachedThreadPool();
+        CountDownLatch latch = new CountDownLatch(1);
+        CountDownLatch done = new CountDownLatch(100);
+        store.mark(CELL);
+        when(kvs.get(any(), any())).thenReturn(ImmutableMap.of(CELL, Value.create(IN_PROGRESS_MARKER, 0)));
+
+        byte[] stagingCommitTs = {1, 1, 1, 0};
+
+        for (int i = 0; i < 100; i++) {
+            service.execute(() -> {
+                try {
+                    latch.await();
+                    store.atomicUpdate(CELL, stagingCommitTs);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                } finally {
+                    done.countDown();
+                }
+            });
+        }
+        latch.countDown();
+        done.await();
+        verify(kvs).multiCheckAndSet(any());
+        assertThat(store.get(CELL).get()).hasValue(IN_PROGRESS_MARKER);
+    }
+
+    @Test
+    public void choosesCommitOverAbort() throws ExecutionException, InterruptedException {
+        store.mark(CELL);
+        ExecutorService service = Executors.newCachedThreadPool();
+        CountDownLatch latch = new CountDownLatch(1);
+        CountDownLatch done = new CountDownLatch(1);
+        byte[] stagingCommitTs = {1, 1, 1, 0};
+
+        service.execute(() -> {
+            try {
+                latch.await();
+                store.atomicUpdate(CELL, stagingCommitTs);
+            } catch (InterruptedException e) {
+                // Do nothing
+            } finally {
+                done.countDown();
+            }
+        });
+
+//        service.execute(() -> {
+//            try {
+//                latch.await();
+//                store.atomicUpdate(CELL, TransactionConstants.TICKETS_ENCODING_ABORTED_TRANSACTION_VALUE);
+//            } catch (InterruptedException e) {
+//                // Do nothing
+//            } finally {
+//                done.countDown();
+//            }
+//        });
+
+        latch.countDown();
+        done.await();
+//        verify(kvs).multiCheckAndSet(any());
+        assertThat(store.get(CELL).get()).hasValue(stagingCommitTs);
     }
 }
