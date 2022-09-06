d1b78051195a10e8098f434d144a74f5f239461f
Sudiksha Nanda <snanda@palantir.com>
2022-09-06 13:47:17 +0100

    womp

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SnapshotTransaction.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/SnapshotTransaction.java
@@ -15,6 +15,8 @@
  */
 package com.palantir.atlasdb.transaction.impl;
 
+import static java.util.stream.Collectors.toList;
+
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Timer;
@@ -137,10 +139,6 @@ import com.palantir.logsafe.logger.SafeLoggerFactory;
 import com.palantir.tracing.CloseableTracer;
 import com.palantir.util.AssertUtils;
 import com.palantir.util.paging.TokenBackedBasicResultsPage;
-import org.apache.commons.lang3.Validate;
-import org.apache.commons.lang3.tuple.Pair;
-
-import javax.annotation.Nullable;
 import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
@@ -177,8 +175,9 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
-
-import static java.util.stream.Collectors.toList;
+import javax.annotation.Nullable;
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * This implements snapshot isolation for transactions.
@@ -2538,7 +2537,8 @@ public class SnapshotTransaction extends AbstractTransaction
     }
 
     private Map<Long, Long> getCommitTimestampsFromCache(Iterable<Long> startTimestamps) {
-        Set<Long> startTsSet = StreamSupport.stream(startTimestamps.spliterator(), false).collect(Collectors.toSet());
+        Set<Long> startTsSet =
+                StreamSupport.stream(startTimestamps.spliterator(), false).collect(Collectors.toSet());
         return KeyedStream.of(startTsSet)
                 .filter(ts -> !isReadOnlyTransactionReadingSweepableTransaction(ts))
                 .map(timestampValidationReadCache::getCommitTimestampIfPresent)
