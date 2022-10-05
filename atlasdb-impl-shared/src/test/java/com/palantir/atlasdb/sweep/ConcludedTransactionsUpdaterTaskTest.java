ef6a155ab70f6a64acad4a3e208bc73822ffbffb
Sudiksha Nanda <snanda@palantir.com>
2022-10-05 18:47:41 +0100

    undo

--- atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/ConcludedTransactionsUpdaterTaskTest.java
+++ atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/ConcludedTransactionsUpdaterTaskTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.palantir.atlasdb.sweep.knowledge;
+package com.palantir.atlasdb.sweep;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -25,7 +25,6 @@ import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Range;
-import com.palantir.atlasdb.sweep.ConcludedTransactionsUpdaterTask;
 import com.palantir.atlasdb.sweep.queue.ShardAndStrategy;
 import com.palantir.atlasdb.sweep.queue.ShardProgress;
 import com.palantir.atlasdb.table.description.SweeperStrategy;
