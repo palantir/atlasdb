a97e8979b983546cd11a85bcd88b94c79aba3edf
Steve Capetta <scapetta@palantir.com>
2022-10-06 10:00:23 -0400

    fixed unused imports

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/TaskRunner.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/TaskRunner.java
@@ -15,18 +15,14 @@
  */
 package com.palantir.atlasdb.keyvalue.cassandra;
 
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.palantir.common.base.Throwables;
-import com.palantir.tracing.DetachedSpan;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 
 class TaskRunner {
