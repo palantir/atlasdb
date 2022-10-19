5e4713e9e327c7f8d79122dd1a1aa7eca6bb59a2
Ahmed Alouane <aalouane@palantir.com>
2022-10-19 23:31:09 +0100

    track some bytes

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/expectations/KeyValueServiceDataTracker.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/expectations/KeyValueServiceDataTracker.java
@@ -18,43 +18,84 @@ package com.palantir.atlasdb.transaction.impl.expectations;
 
 import com.google.common.collect.Comparators;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.AtomicLongMap;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.transaction.api.expectations.ImmutableKvsCallReadInfo;
 import com.palantir.atlasdb.transaction.api.expectations.ImmutableTransactionReadInfo;
 import com.palantir.atlasdb.transaction.api.expectations.KvsCallReadInfo;
 import com.palantir.atlasdb.transaction.api.expectations.TransactionReadInfo;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public final class KeyValueServiceDataTracker {
-    AtomicLongMap<TableReference> bytesReadByTable = AtomicLongMap.create();
-    AtomicLongMap<TableReference> kvsCallsByTable = AtomicLongMap.create();
+    private final AtomicLongMap<TableReference> bytesReadByTable = AtomicLongMap.create();
+    private final AtomicLongMap<TableReference> kvsCallsByTable = AtomicLongMap.create();
 
-    ConcurrentMap<TableReference, KvsCallReadInfo> maximumBytesKvsCallInfoByTable = new ConcurrentHashMap<>();
+    private final ConcurrentMap<TableReference, KvsCallReadInfo> maximumBytesKvsCallInfoByTable =
+            new ConcurrentHashMap<>();
 
     // we can keep separate tallies for these, not sure if it's worth the extra bookkeeping
     TransactionReadInfo getReadInfo() {
+        // todo(aalouane) check Comparator.naturalOrder() does what I think it does
         return ImmutableTransactionReadInfo.builder()
                 .bytesRead(bytesReadByTable.sum())
                 .kvsCalls(kvsCallsByTable.sum())
+                .maximumBytesKvsCallInfo(
+                        maximumBytesKvsCallInfoByTable.values().stream().max(Comparator.naturalOrder()))
                 .build();
     }
 
+    // below method may be too jank
+    // takes immutable snapshots of maps and uses only the common keys to construct a coherent return value
+    // we could keep a ConcurrentMap<TableReference, TransactionReadInfo>, but the update would be slower
+    // (creating immutables instead of updating atomic longs) and that
+    // happens more frequently than the method below (only ran after transactions close on callback)
     ImmutableMap<TableReference, TransactionReadInfo> getReadInfoByTable() {
-        return null;
+        ImmutableMap<TableReference, Long> bytesRead = ImmutableMap.copyOf(bytesReadByTable.asMap());
+        ImmutableMap<TableReference, Long> kvsCalls = ImmutableMap.copyOf(kvsCallsByTable.asMap());
+        ImmutableMap<TableReference, KvsCallReadInfo> maximumBytesKvsCall =
+                ImmutableMap.copyOf(maximumBytesKvsCallInfoByTable);
+
+        // omitting third map as it doesn't always have info (e.g. kvs iterator gets)
+        Set<TableReference> tableRefs = Sets.intersection(bytesRead.keySet(), kvsCalls.keySet());
+
+        // ugly!
+        Builder<TableReference, TransactionReadInfo> mapBuilder = ImmutableMap.builder();
+        for (TableReference tableRef : tableRefs) {
+            mapBuilder = mapBuilder.put(
+                    tableRef,
+                    ImmutableTransactionReadInfo.builder()
+                            .bytesRead(bytesRead.get(tableRef))
+                            .kvsCalls(kvsCalls.get(tableRef))
+                            .maximumBytesKvsCallInfo(Optional.ofNullable(maximumBytesKvsCall.get(tableRef)))
+                            .build());
+        }
+
+        return mapBuilder.build();
     }
 
-    void trackBytesRead(TableReference tableRef, String methodName, long bytesRead) {
+    void trackKvsGetMethodRead(TableReference tableRef, String methodName, long bytesRead) {
         bytesReadByTable.addAndGet(tableRef, bytesRead);
         kvsCallsByTable.incrementAndGet(tableRef);
-
         maximumBytesKvsCallInfoByTable.merge(
                 tableRef,
                 ImmutableKvsCallReadInfo.builder()
                         .bytesRead(bytesRead)
                         .kvsMethodName(methodName)
                         .build(),
-                (x, y) -> Comparators.max(x, y))
+                Comparators::max);
+    }
+
+    void trackKvsGetPartialRead(TableReference tableRef, long bytesRead) {
+        bytesReadByTable.addAndGet(tableRef, bytesRead);
+    }
+
+    void incrementKvsGetCallCount(TableReference tableRef) {
+        kvsCallsByTable.incrementAndGet(tableRef);
     }
 }
--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/expectations/TrackingKeyValueServiceImpl.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/expectations/TrackingKeyValueServiceImpl.java
@@ -17,10 +17,15 @@
 package com.palantir.atlasdb.transaction.impl.expectations;
 
 import com.google.common.collect.ImmutableMap;
+import com.palantir.atlasdb.keyvalue.api.Cell;
+import com.palantir.atlasdb.keyvalue.api.ColumnSelection;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.keyvalue.api.Value;
 import com.palantir.atlasdb.keyvalue.impl.ForwardingKeyValueService;
 import com.palantir.atlasdb.transaction.api.expectations.TransactionReadInfo;
+import com.palantir.common.streams.KeyedStream;
+import java.util.Map;
 
 public class TrackingKeyValueServiceImpl extends ForwardingKeyValueService implements TrackingKeyValueService {
     KeyValueService delegate;
@@ -43,5 +48,49 @@ public class TrackingKeyValueServiceImpl extends ForwardingKeyValueService imple
     @Override
     public ImmutableMap<TableReference, TransactionReadInfo> getReadInfoByTable() {
         return tracker.getReadInfoByTable();
+    }
+
+    @Override
+    public Map<Cell, Value> getRows(
+            TableReference tableRef, Iterable<byte[]> rows, ColumnSelection columnSelection, long timestamp) {
+        Map<Cell, Value> result = delegate.getRows(tableRef, rows, columnSelection, timestamp);
+        tracker.trackKvsGetMethodRead(tableRef, "getRows", byteSize(result));
+        return result;
+    }
+
+    @Override
+    public Map<Cell, Value> get(TableReference tableRef, Map<Cell, Long> timestampByCell) {
+        Map<Cell, Value> result = delegate.get(tableRef, timestampByCell);
+        tracker.trackKvsGetMethodRead(tableRef, "get", byteSize(result));
+        return result;
+    }
+
+    @Override
+    public Map<Cell, Long> getLatestTimestamps(TableReference tableRef, Map<Cell, Long> timestampByCell) {
+        Map<Cell, Long> result = delegate.getLatestTimestamps(tableRef, timestampByCell);
+        tracker.trackKvsGetMethodRead(tableRef, "getLatestTimestamps", byteSizeTs(result));
+        return result;
+    }
+
+    private static long byteSizeTs(Map<Cell, Long> timestampByCell) {
+        return timestampByCell.keySet().stream()
+                .mapToLong(cell -> byteSize(cell) + 8)
+                .sum();
+    }
+
+    private static long byteSize(Map<Cell, Value> valueByCell) {
+        return KeyedStream.stream(valueByCell)
+                .map((cell, value) -> byteSize(cell) + byteSize(value))
+                .values()
+                .mapToLong(i -> i)
+                .sum();
+    }
+
+    private static long byteSize(Cell cell) {
+        return cell.getRowName().length + cell.getColumnName().length;
+    }
+
+    private static long byteSize(Value value) {
+        return value.getContents().length;
     }
 }
