65d48959c1f94982b797192b916c116b713fa84f
Ahmed Alouane <aalouane@palantir.com>
2022-10-25 13:48:52 +0100

    checkpoint

---
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/transaction/impl/expectations/ExpectationsCallbackManager.java
@@ -0,0 +1,35 @@
+/*
+ * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.transaction.impl.expectations;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
+
+public final class ExpectationsCallbackManager {
+    private final List<Consumer<Object>> callbacks = new CopyOnWriteArrayList<>();
+    private final AtomicBoolean ranCallbacks
+
+    public void registerCallback(Consumer<Object> callback) {
+        callbacks.add(callback);
+    }
+
+    public void runCallbacksOnce(Object stats) {
+        callbacks.forEach(consumer -> consumer.accept(stats));
+    }
+}
