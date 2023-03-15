bbb6e9b27b123bbdd154f28434c9cd972e5f4441
Jeremy Kong <jkong@palantir.com>
2023-03-15 19:29:55 +0000

    Add witnessed transaction actions

--- atlasdb-workload-server/src/test/java/com/palantir/atlasdb/workload/workflow/SingleRowTwoCellsWorkflowsTest.java
+++ atlasdb-workload-server/src/test/java/com/palantir/atlasdb/workload/workflow/SingleRowTwoCellsWorkflowsTest.java
@@ -18,13 +18,37 @@ package com.palantir.atlasdb.workload.workflow;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.palantir.atlasdb.factory.TransactionManagers;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
+import com.palantir.atlasdb.workload.store.AtlasDbTransactionStore;
+import com.palantir.atlasdb.workload.store.IsolationLevel;
+import com.palantir.atlasdb.workload.store.TransactionStore;
 import com.palantir.atlasdb.workload.transaction.DeleteTransactionAction;
 import com.palantir.atlasdb.workload.transaction.ReadTransactionAction;
 import com.palantir.atlasdb.workload.transaction.WriteTransactionAction;
+import com.palantir.atlasdb.workload.transaction.witnessed.WitnessedDeleteTransactionAction;
+import com.palantir.atlasdb.workload.transaction.witnessed.WitnessedReadTransactionAction;
+import com.palantir.atlasdb.workload.transaction.witnessed.WitnessedWriteTransactionAction;
+import com.palantir.atlasdb.workload.util.AtlasDbUtils;
+import com.palantir.common.concurrent.PTExecutors;
+import java.util.Optional;
 import org.junit.Test;
 
 public class SingleRowTwoCellsWorkflowsTest {
     private static final String TABLE_NAME = "coffee";
+    private static final SingleRowTwoCellsWorkflowConfiguration CONFIGURATION =
+            ImmutableSingleRowTwoCellsWorkflowConfiguration.builder()
+                    .tableConfiguration(ImmutableTableConfiguration.builder()
+                            .tableName(TABLE_NAME)
+                            .isolationLevel(IsolationLevel.SERIALIZABLE)
+                            .build())
+                    .rateLimit(Double.MAX_VALUE)
+                    .iterationCount(1)
+                    .build();
 
     @Test
     public void shouldWriteToFirstCellOnEvenIndices() {
@@ -78,5 +102,33 @@ public class SingleRowTwoCellsWorkflowsTest {
                         WriteTransactionAction.of(TABLE_NAME, SingleRowTwoCellsWorkflows.SECOND_CELL, 6021023),
                         ReadTransactionAction.of(TABLE_NAME, SingleRowTwoCellsWorkflows.FIRST_CELL),
                         ReadTransactionAction.of(TABLE_NAME, SingleRowTwoCellsWorkflows.SECOND_CELL));
+    }
+
+    @Test
+    public void workflowHistoryPreservesStoreAndCapturesWitnessedActions() {
+        TransactionStore memoryStore = AtlasDbTransactionStore.create(
+                TransactionManagers.createInMemory(ImmutableSet.of()),
+                ImmutableMap.of(
+                        TableReference.createWithEmptyNamespace(TABLE_NAME),
+                        AtlasDbUtils.tableMetadata(IsolationLevel.SERIALIZABLE)));
+        WorkflowHistory history = SingleRowTwoCellsWorkflows.createSingleRowTwoCell(
+                        memoryStore, CONFIGURATION, MoreExecutors.listeningDecorator(PTExecutors.newFixedThreadPool(1)))
+                .run();
+
+        assertThat(history.transactionStore())
+                .as("should return the provided store")
+                .isEqualTo(memoryStore);
+        assertThat(Iterables.getOnlyElement(history.history()).actions())
+                .containsExactly(
+                        WitnessedReadTransactionAction.of(
+                                TABLE_NAME, SingleRowTwoCellsWorkflows.FIRST_CELL, Optional.empty()),
+                        WitnessedReadTransactionAction.of(
+                                TABLE_NAME, SingleRowTwoCellsWorkflows.SECOND_CELL, Optional.empty()),
+                        WitnessedWriteTransactionAction.of(TABLE_NAME, SingleRowTwoCellsWorkflows.FIRST_CELL, 0),
+                        WitnessedDeleteTransactionAction.of(TABLE_NAME, SingleRowTwoCellsWorkflows.SECOND_CELL),
+                        WitnessedReadTransactionAction.of(
+                                TABLE_NAME, SingleRowTwoCellsWorkflows.FIRST_CELL, Optional.empty()),
+                        WitnessedReadTransactionAction.of(
+                                TABLE_NAME, SingleRowTwoCellsWorkflows.SECOND_CELL, Optional.of(0)));
     }
 }
