45c36ae5f1012b136b0e91a58a6cca47f92f7c5a
Sam-Kramer <sam.c.kramer@gmail.com>
2023-03-17 10:16:27 +0000

    Add workload server interfaces to help modularity (#6471)
    Adds interfaces to the workload server which makes integration with dropwizard or other server frameworks.

---
+++ atlasdb-workload-server-api/src/main/java/com/palantir/atlasdb/workload/workflow/WorkflowRunner.java
@@ -0,0 +1,30 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.workload.workflow;
+
+import com.palantir.atlasdb.workload.invariant.InvariantReporter;
+import java.util.List;
+
+/**
+ * Run the provided workflow and execute the provided invariant reporters.
+ *
+ * The idea of this class is that long-term we will want to have multiple ways to run/execute workflows,
+ * rather than biasing towards the present case (just once).
+ */
+public interface WorkflowRunner<WorkflowType extends Workflow> {
+    void run(WorkflowType workflow, List<InvariantReporter<?>> invariantReporters);
+}
