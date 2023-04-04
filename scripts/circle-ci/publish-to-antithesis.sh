157f6ab25629d95e50b6de28f4fa361dd2287687
Sam Kramer <skramer@palantir.com>
2023-04-04 14:20:58 +0100

    create script to easily push to antithesis

---
+++ scripts/circle-ci/publish-to-antithesis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+printenv ANTITHESIS_LOGIN_JSON | base64 -d | docker login -u _json_key https://${ANTITHESIS_REPO_URL} --password-stdin
+
+./gradlew docker
+
+docker pull palantirtechnologies/docker-cassandra-atlasdb:atlasdb-testing-palantir-cassandra
+docker tag palantirtechnologies/docker-cassandra-atlasdb:atlasdb-testing-palantir-cassandra ${ANTITHESIS_REPO_URL}/docker-cassandra-atlasdb:atlasdb-testing-palantir-cassandra
+docker push ${ANTITHESIS_REPO_URL}/docker-cassandra-atlasdb:atlasdb-testing-palantir-cassandra
+
+docker tag palantirtechnologies/timelock-server-distribution:unspecified ${ANTITHESIS_REPO_URL}/timelock-server-distribution:unspecified
+docker push ${ANTITHESIS_REPO_URL}/timelock-server-distribution:unspecified
+
+docker tag palantirtechnologies/atlasdb-workload-server-distribution:unspecified ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:unspecified
+docker push ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:unspecified
+
+LATEST_ANTITHESIS_TAG=$(docker image ls palantirtechnologies/atlasdb-workload-server-antithesis --format "{{.Tag}}")
+docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${LATEST_ANTITHESIS_TAG} ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:${LATEST_ANTITHESIS_TAG}
+docker push ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:${LATEST_ANTITHESIS_TAG}
