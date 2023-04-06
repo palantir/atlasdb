0afb522e66943a9e2139218e23ad4c7a39bf1f54
Sam Kramer <skramer@palantir.com>
2023-04-06 10:18:02 +0100

    check if var exists

--- scripts/circle-ci/publish-to-antithesis.sh
+++ scripts/circle-ci/publish-to-antithesis.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ -z "${ANTITHESIS_REPO_URL}" ]]; then
+  echo "Antithesis image repository URL is not set as an environment variable, exiting."
+  exit 1
+fi
+
+if [[ -z "${ANTITHESIS_LOGIN_JSON}" ]]; then
+  echo "Antithesis json key is not set as an environment variable, exiting."
+  exit 1
+fi
+
 printenv ANTITHESIS_LOGIN_JSON | base64 -d | docker login -u _json_key https://${ANTITHESIS_REPO_URL} --password-stdin
 
 ./gradlew docker
