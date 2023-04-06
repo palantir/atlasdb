b48b80fc886b4054e26e30b37c1280ecfa2d2a93
Sam-Kramer <sam.c.kramer@gmail.com>
2023-04-06 18:47:21 +0100

    Fix bug in workload server when failing to fetch commit timestamp due to network errors (#6510)
    Obtain commit timestamp from the transaction itself to be resilient to network failures after a transaction has been witnessed.

--- atlasdb-workload-server/src/test/java/com/palantir/atlasdb/workload/store/AtlasDbTransactionStoreTest.java
+++ atlasdb-workload-server/src/test/java/com/palantir/atlasdb/workload/store/AtlasDbTransactionStoreTest.java
@@ -27,14 +27,19 @@ import static com.palantir.atlasdb.workload.transaction.WorkloadTestHelpers.WORK
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Iterables;
 import com.palantir.atlasdb.factory.TransactionManagers;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
+import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.transaction.api.ConflictHandler;
+import com.palantir.atlasdb.transaction.api.Transaction;
 import com.palantir.atlasdb.transaction.api.TransactionManager;
+import com.palantir.atlasdb.workload.store.AtlasDbTransactionStore.CommitTimestampProvider;
 import com.palantir.atlasdb.workload.transaction.DeleteTransactionAction;
 import com.palantir.atlasdb.workload.transaction.ReadTransactionAction;
 import com.palantir.atlasdb.workload.transaction.WitnessToActionVisitor;
@@ -45,15 +50,26 @@ import com.palantir.atlasdb.workload.transaction.witnessed.WitnessedTransactionA
 import com.palantir.atlasdb.workload.transaction.witnessed.WitnessedWriteTransactionAction;
 import com.palantir.atlasdb.workload.util.AtlasDbUtils;
 import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.Test;
 
 public final class AtlasDbTransactionStoreTest {
+
+    private static final long START_TS = 1;
+    private static final long COMMIT_TS = 2;
+
+    private static final Map<TableReference, byte[]> TABLES = Map.of(
+            TABLE_REFERENCE,
+            AtlasDbUtils.tableMetadata(ConflictHandler.SERIALIZABLE),
+            INDEX_REFERENCE,
+            AtlasDbUtils.indexMetadata(ConflictHandler.SERIALIZABLE));
 
     private TransactionManager manager;
 
@@ -62,13 +78,7 @@ public final class AtlasDbTransactionStoreTest {
     @Before
     public void before() {
         manager = TransactionManagers.createInMemory(Set.of());
-        store = AtlasDbTransactionStore.create(
-                manager,
-                Map.of(
-                        TABLE_REFERENCE,
-                        AtlasDbUtils.tableMetadata(ConflictHandler.SERIALIZABLE),
-                        INDEX_REFERENCE,
-                        AtlasDbUtils.indexMetadata(ConflictHandler.SERIALIZABLE)));
+        store = AtlasDbTransactionStore.create(manager, TABLES);
     }
 
     @Test
@@ -144,5 +154,40 @@ public final class AtlasDbTransactionStoreTest {
                 store.readWrite(List.of(ReadTransactionAction.of(TABLE_1, WORKLOAD_CELL_ONE)));
         assertThat(witnessedTransaction).isPresent();
         assertThat(witnessedTransaction.get().commitTimestamp()).isEmpty();
+    }
+
+    @Test
+    public void abortedTransactionsReturnEmpty() {
+        TransactionManager onlyAbortsManager = spy(manager);
+        Transaction abortedTransaction = mock(Transaction.class);
+        when(abortedTransaction.isAborted()).thenReturn(true);
+        doReturn(abortedTransaction).when(onlyAbortsManager).runTaskWithConditionWithRetry(any(Supplier.class), any());
+        AtlasDbTransactionStore onlyAbortsStore = AtlasDbTransactionStore.create(onlyAbortsManager, TABLES);
+        assertThat(onlyAbortsStore.readWrite(List.of(ReadTransactionAction.of(TABLE_1, WORKLOAD_CELL_ONE))))
+                .isEmpty();
+    }
+
+    @Test
+    public void getCommitTimestampIsEmptyWhenMatchesStartTimestamp() {
+        CommitTimestampProvider commitTimestampProvider = new CommitTimestampProvider();
+        commitTimestampProvider.throwIfConditionInvalid(START_TS);
+        assertThat(commitTimestampProvider.getCommitTimestampOrThrowIfMaybeNotCommitted(START_TS))
+                .isEmpty();
+    }
+
+    @Test
+    public void getCommitTimestampThrowsWhenNoTimestampSet() {
+        assertThatThrownBy(() -> new CommitTimestampProvider().getCommitTimestampOrThrowIfMaybeNotCommitted(START_TS))
+                .isInstanceOf(SafeIllegalStateException.class)
+                .hasMessageContaining(
+                        "Timestamp has not been set, which means the pre commit condition has never been executed");
+    }
+
+    @Test
+    public void getCommitTimestampReturnsPresentWhenCommitAndStartTimestampMismatch() {
+        CommitTimestampProvider commitTimestampProvider = new CommitTimestampProvider();
+        commitTimestampProvider.throwIfConditionInvalid(COMMIT_TS);
+        assertThat(commitTimestampProvider.getCommitTimestampOrThrowIfMaybeNotCommitted(START_TS))
+                .hasValue(COMMIT_TS);
     }
 }
