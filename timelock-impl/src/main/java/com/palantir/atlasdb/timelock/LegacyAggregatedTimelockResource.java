bb560d91ca79c925d7bba16296cfbe85b3d28d37
Jeremy Kong <jkong@palantir.com>
2023-04-09 16:16:50 +0800

    lock updates

--- timelock-impl/src/main/java/com/palantir/atlasdb/timelock/LegacyAggregatedTimelockResource.java
+++ timelock-impl/src/main/java/com/palantir/atlasdb/timelock/LegacyAggregatedTimelockResource.java
@@ -16,6 +16,9 @@
 
 package com.palantir.atlasdb.timelock;
 
+import com.palantir.annotations.remoting.CancelableServerCall;
+import com.palantir.common.annotation.Idempotent;
+import com.palantir.common.annotation.NonIdempotent;
 import com.palantir.conjure.java.undertow.annotations.Handle;
 import com.palantir.conjure.java.undertow.annotations.HttpMethod;
 import com.palantir.conjure.java.undertow.annotations.SerializerFactory;
@@ -23,14 +26,25 @@ import com.palantir.conjure.java.undertow.lib.Endpoint;
 import com.palantir.conjure.java.undertow.lib.Serializer;
 import com.palantir.conjure.java.undertow.lib.TypeMarker;
 import com.palantir.conjure.java.undertow.lib.UndertowRuntime;
+import com.palantir.lock.HeldLocksGrant;
+import com.palantir.lock.HeldLocksToken;
+import com.palantir.lock.LockClient;
+import com.palantir.lock.LockRequest;
+import com.palantir.lock.LockResponse;
+import com.palantir.lock.LockServerOptions;
+import com.palantir.lock.SimpleHeldLocksToken;
 import com.palantir.logsafe.Safe;
 import com.palantir.timestamp.TimestampRange;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.util.Set;
 import javax.annotation.CheckReturnValue;
+import javax.annotation.Nullable;
 import javax.annotation.meta.When;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -75,13 +89,203 @@ public class LegacyAggregatedTimelockResource {
     }
 
     @POST // This has to be POST because we can't allow caching.
-    @Path("fresh-timestamps")
+    @Path("/{namespace}/timestamp/fresh-timestamps")
     @Produces(MediaType.APPLICATION_JSON)
     @Handle(method = HttpMethod.POST, path = "/{namespace}/timestamp/fresh-timestamps")
     public TimestampRange getFreshTimestamps(
             @Safe @PathParam("namespace") @Handle.PathParam String namespace,
             @Safe @QueryParam("number") int numTimestampsRequested) {
         return namespaces.get(namespace).getTimestampService().getFreshTimestamps(numTimestampsRequested);
+    }
+
+    // Lock v1
+    @POST
+    @Path("/{namespace}/lock/lock-with-full-response/{client}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @CancelableServerCall
+    @NonIdempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/lock-with-full-response/{client}")
+    public LockResponse lockWithFullLockResponse(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace,
+            @Safe @PathParam("client") @Handle.PathParam LockClient client,
+            @Handle.Body LockRequest request)
+            throws InterruptedException {
+        return namespaces.get(namespace).getLockService().lockWithFullLockResponse(client, request);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/unlock-deprecated")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Deprecated
+    @NonIdempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/unlock-deprecated")
+    public boolean unlock(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace, @Handle.Body HeldLocksToken token) {
+        return namespaces.get(namespace).getLockService().unlock(token);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/unlock-simple")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @NonIdempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/unlock-simple")
+    public boolean unlockSimple(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace, @Handle.Body SimpleHeldLocksToken token) {
+        return namespaces.get(namespace).getLockService().unlockSimple(token);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/unlock-and-freeze")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @NonIdempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/unlock-and-freeze")
+    public boolean unlockAndFreeze(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace, @Handle.Body HeldLocksToken token) {
+        return namespaces.get(namespace).getLockService().unlockAndFreeze(token);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/get-tokens/{client}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @CancelableServerCall
+    @NonIdempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/get-tokens/{client}")
+    public Set<HeldLocksToken> getTokens(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace,
+            @Safe @PathParam("client") @Handle.PathParam LockClient client) {
+        return namespaces.get(namespace).getLockService().getTokens(client);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/refresh-tokens")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Deprecated
+    @Idempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/refresh-tokens")
+    public Set<HeldLocksToken> refreshTokens(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace,
+            @Handle.Body Iterable<HeldLocksToken> tokens) {
+        return namespaces.get(namespace).getLockService().refreshTokens(tokens);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/refresh-grant")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Idempotent
+    @Nullable
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/refresh-grant")
+    public HeldLocksGrant refreshGrant(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace, @Handle.Body HeldLocksGrant grant) {
+        return namespaces.get(namespace).getLockService().refreshGrant(grant);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/refresh-grant-id")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Idempotent
+    @Nullable
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/refresh-grant-id")
+    public HeldLocksGrant refreshGrant(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace, @Handle.Body BigInteger grantId) {
+        return namespaces.get(namespace).getLockService().refreshGrant(grantId);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/convert-to-grant")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @NonIdempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/convert-to-grant")
+    public HeldLocksGrant convertToGrant(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace, @Handle.Body HeldLocksToken token) {
+        return namespaces.get(namespace).getLockService().convertToGrant(token);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/use-grant/{client}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @NonIdempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/use-grant/{client}")
+    public HeldLocksToken useGrant(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace,
+            @Safe @PathParam("client") @Handle.PathParam LockClient client,
+            @Handle.Body HeldLocksGrant grant) {
+        return namespaces.get(namespace).getLockService().useGrant(client, grant);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/use-grant-id/{client}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @NonIdempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/use-grant-id/{client}")
+    public HeldLocksToken useGrant(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace,
+            @Safe @PathParam("client") @Handle.PathParam LockClient client,
+            @Handle.Body BigInteger grantId) {
+        return namespaces.get(namespace).getLockService().useGrant(client, grantId);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/min-locked-in-version-id")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Deprecated
+    @Idempotent
+    @Nullable
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/min-locked-in-version-id")
+    public Long getMinLockedInVersionId(@Safe @PathParam("namespace") @Handle.PathParam String namespace) {
+        return namespaces.get(namespace).getLockService().getMinLockedInVersionId(namespace);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/min-locked-in-version-id-for-client/{client}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/min-locked-in-version-id-for-client/{client}")
+    public Long getMinLockedInVersionId(
+            @Safe @PathParam("namespace") @Handle.PathParam String namespace,
+            @Safe @PathParam("client") @Handle.PathParam LockClient client) {
+        return namespaces.get(namespace).getLockService().getMinLockedInVersionId(client);
+    }
+
+    @POST
+    @Path("/{namespace}/lock/lock-server-options")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Idempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/lock-server-options")
+    public LockServerOptions getLockServerOptions(@Safe @PathParam("namespace") @Handle.PathParam String namespace) {
+        return namespaces.get(namespace).getLockService().getLockServerOptions();
+    }
+
+    @POST
+    @Path("/{namespace}/lock/current-time-millis")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Idempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/current-time-millis")
+    public long currentTimeMillis(@Safe @PathParam("namespace") @Handle.PathParam String namespace) {
+        return namespaces.get(namespace).getLockService().currentTimeMillis();
+    }
+
+    @POST
+    @Path("/{namespace}/lock/log-current-state")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Idempotent
+    @Handle(method = HttpMethod.POST, path = "/{namespace}/lock/log-current-state")
+    public void logCurrentState(@Safe @PathParam("namespace") @Handle.PathParam String namespace) {
+        namespaces.get(namespace).getLockService().logCurrentState();
     }
 
     // Timestamp management
