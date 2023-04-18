23de54946e19ce8cc71cdf53e565bb9b1241a593
Sam Kramer <skramer@palantir.com>
2023-04-18 14:37:55 +0100

    fix tests

--- atlasdb-workload-server/src/test/java/com/palantir/atlasdb/workload/transaction/witnessed/OnlyCommittedWitnessedTransactionVisitorTest.java
+++ atlasdb-workload-server/src/test/java/com/palantir/atlasdb/workload/transaction/witnessed/OnlyCommittedWitnessedTransactionVisitorTest.java
@@ -18,6 +18,7 @@ package com.palantir.atlasdb.workload.transaction.witnessed;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import com.palantir.atlasdb.workload.store.ReadOnlyTransactionStore;
@@ -42,7 +43,7 @@ public class OnlyCommittedWitnessedTransactionVisitorTest {
 
     @Test
     public void fullyCommittedTransactionsAreNeverChecked() {
-        when(transactionStore.isCommitted(anyLong())).thenThrow(new RuntimeException());
+        lenient().when(transactionStore.isCommitted(anyLong())).thenThrow(new RuntimeException());
         assertThat(READ_ONLY_WITNESSED_TRANSACTION.accept(
                         new OnlyCommittedWitnessedTransactionVisitor(transactionStore)))
                 .contains(READ_ONLY_WITNESSED_TRANSACTION);
