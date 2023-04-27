355775c5f173c0844ebe85791f507507062f10f8
aalouane <30903736+ergo14@users.noreply.github.com>
2023-04-27 16:35:51 +0100

    [CBT] Retry flaky tests (#6541)

--- atlasdb-api/src/test/java/com/palantir/atlasdb/keyvalue/api/CellReferenceTest.java
+++ atlasdb-api/src/test/java/com/palantir/atlasdb/keyvalue/api/CellReferenceTest.java
@@ -21,18 +21,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.math.IntMath;
 import com.palantir.atlasdb.encoding.PtBytes;
+import com.palantir.flake.FlakeRetryingRule;
+import com.palantir.flake.ShouldRetry;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.assertj.core.data.Percentage;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class CellReferenceTest {
     private static final TableReference TABLE = createFromFullyQualifiedName("fixed.table");
     private static final byte[] ROW = PtBytes.toBytes("row");
     private static final byte[] COL = PtBytes.toBytes("col");
+
+    @Rule
+    public final FlakeRetryingRule retryingRule = new FlakeRetryingRule();
 
     @Test
     public void dynamicSequentialColumns() {
@@ -44,6 +50,7 @@ public class CellReferenceTest {
         assertDistributionUniform(hashes, totalCount);
     }
 
+    @ShouldRetry
     @Test
     public void dynamicRandomColumns() {
         ThreadLocalRandom random = ThreadLocalRandom.current();
--- atlasdb-cli/src/test/java/com/palantir/atlasdb/cli/command/TestTimestampCommand.java
+++ atlasdb-cli/src/test/java/com/palantir/atlasdb/cli/command/TestTimestampCommand.java
@@ -37,6 +37,8 @@ import com.palantir.atlasdb.services.ServicesConfigModule;
 import com.palantir.atlasdb.services.test.DaggerTestAtlasDbServices;
 import com.palantir.atlasdb.services.test.TestAtlasDbServices;
 import com.palantir.common.time.Clock;
+import com.palantir.flake.FlakeRetryingRule;
+import com.palantir.flake.ShouldRetry;
 import com.palantir.lock.LockClient;
 import com.palantir.lock.LockDescriptor;
 import com.palantir.lock.LockMode;
@@ -60,6 +62,7 @@ import org.joda.time.format.ISODateTimeFormat;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -70,6 +73,9 @@ public class TestTimestampCommand {
     private static LockDescriptor lock;
     private static AtlasDbServicesFactory moduleFactory;
     private static List<String> cliArgs;
+
+    @Rule
+    public FlakeRetryingRule flakeRetryingRule = new FlakeRetryingRule();
 
     @BeforeClass
     public static void oneTimeSetup() throws Exception {
@@ -181,6 +187,7 @@ public class TestTimestampCommand {
         runAndVerifyCliForFile(inputFileString);
     }
 
+    @ShouldRetry
     @Test
     public void testWithFileWithDir() throws Exception {
         String inputFileString = "existing-dir/test.timestamp";
