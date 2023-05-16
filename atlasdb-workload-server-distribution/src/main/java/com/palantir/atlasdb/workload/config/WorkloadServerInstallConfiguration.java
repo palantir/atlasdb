c59f74d9034a87abc8743ecf69c8ff5e88058ed1
Jeremy Kong <jkong@palantir.com>
2023-05-16 22:58:04 +0100

    cleaning

--- atlasdb-workload-server-distribution/src/main/java/com/palantir/atlasdb/workload/config/WorkloadServerInstallConfiguration.java
+++ atlasdb-workload-server-distribution/src/main/java/com/palantir/atlasdb/workload/config/WorkloadServerInstallConfiguration.java
@@ -19,6 +19,7 @@ package com.palantir.atlasdb.workload.config;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.palantir.atlasdb.config.AtlasDbConfig;
+import com.palantir.atlasdb.workload.workflow.SingleBusyCellWorkflowConfiguration;
 import com.palantir.atlasdb.workload.workflow.SingleRowTwoCellsWorkflowConfiguration;
 import com.palantir.atlasdb.workload.workflow.ring.RingWorkflowConfiguration;
 import org.immutables.value.Value;
@@ -32,6 +33,8 @@ public interface WorkloadServerInstallConfiguration {
     SingleRowTwoCellsWorkflowConfiguration singleRowTwoCellsConfig();
 
     RingWorkflowConfiguration ringConfig();
+
+    SingleBusyCellWorkflowConfiguration singleBusyCellConfig();
 
     boolean exitAfterRunning();
 }
