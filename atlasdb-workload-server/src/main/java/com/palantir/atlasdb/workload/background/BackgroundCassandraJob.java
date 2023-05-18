d9e9ed30e5bc5a6d7a7afce109138ffcf2c6ad02
Sam-Kramer <sam.c.kramer@gmail.com>
2023-05-18 16:08:10 +0100

    Skramer/cassandra chaos (#6570)

---
+++ atlasdb-workload-server/src/main/java/com/palantir/atlasdb/workload/background/BackgroundCassandraJob.java
@@ -0,0 +1,44 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.workload.background;
+
+import com.google.common.collect.Iterators;
+import com.palantir.atlasdb.buggify.api.BuggifyFactory;
+import com.palantir.atlasdb.workload.resource.CassandraSidecarResource;
+import java.util.Iterator;
+import java.util.List;
+
+public class BackgroundCassandraJob implements Runnable {
+
+    private final CassandraSidecarResource cassandraSidecarResource;
+    private final Iterator<String> cassandraHosts;
+
+    private final BuggifyFactory buggify;
+
+    public BackgroundCassandraJob(
+            List<String> cassandraHosts, CassandraSidecarResource cassandraSidecarResource, BuggifyFactory buggify) {
+        this.cassandraSidecarResource = cassandraSidecarResource;
+        this.cassandraHosts = Iterators.cycle(cassandraHosts);
+        this.buggify = buggify;
+    }
+
+    @Override
+    public void run() {
+        buggify.maybe(0.20).run(() -> cassandraSidecarResource.flush(cassandraHosts.next()));
+        buggify.maybe(0.20).run(() -> cassandraSidecarResource.compact(cassandraHosts.next()));
+    }
+}
