730db668b0486dbebe9afbb25a4609052d62cac5
Sam-Kramer <sam.c.kramer@gmail.com>
2023-05-22 12:56:57 +0100

    Fix antithesis publish script to publish latest C*  (#6578)

--- scripts/circle-ci/publish-to-antithesis.sh
+++ scripts/circle-ci/publish-to-antithesis.sh
@@ -16,9 +16,9 @@ printenv ANTITHESIS_LOGIN_JSON | base64 -d | docker login -u _json_key https://$
 
 VERSION=$(./gradlew -q printVersion)
 
-docker pull palantirtechnologies/docker-cassandra-atlasdb:atlasdb-testing-palantir-cassandra
-docker tag palantirtechnologies/docker-cassandra-atlasdb:atlasdb-testing-palantir-cassandra ${ANTITHESIS_REPO_URL}/docker-cassandra-atlasdb:atlasdb-testing-palantir-cassandra
-docker push ${ANTITHESIS_REPO_URL}/docker-cassandra-atlasdb:atlasdb-testing-palantir-cassandra
+docker pull palantirtechnologies/cassandra:2.2.18-1.112.0-rc5
+docker tag palantirtechnologies/cassandra:2.2.18-1.112.0-rc5 ${ANTITHESIS_REPO_URL}/cassandra:2.2.18-1.112.0-rc5
+docker push ${ANTITHESIS_REPO_URL}/cassandra:2.2.18-1.112.0-rc5
 
 docker tag palantirtechnologies/timelock-server-distribution:${VERSION} ${ANTITHESIS_REPO_URL}/timelock-server-distribution:${VERSION}
 docker push ${ANTITHESIS_REPO_URL}/timelock-server-distribution:${VERSION}
