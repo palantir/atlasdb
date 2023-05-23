319538c088318a3ed50aa9d8d1e0b36a2aba0295
aalouane <30903736+ergo14@users.noreply.github.com>
2023-05-23 15:48:47 +0100

    [CBT] Split up container 3 and add more max heap to timelock integTest (#6584)

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -17,7 +17,7 @@ CONTAINER_1=(':atlasdb-cassandra-integration-tests:testSubset1')
 
 CONTAINER_2=(':atlasdb-ete-tests:check')
 
-CONTAINER_3=(':atlasdb-dbkvs:check' ':atlasdb-cassandra:check' ':timelock-server:integTest')
+CONTAINER_3=(':timelock-server:integTest')
 
 CONTAINER_4=(':atlasdb-cassandra-multinode-tests:check' ':atlasdb-tests-shared:check' ':atlasdb-perf:check' ':atlasdb-ete-tests:dbkvsTest')
 
@@ -35,15 +35,17 @@ CONTAINER_10=('atlasdb-dbkvs-tests:oracleTest')
 
 CONTAINER_11=(':atlasdb-impl-shared:check' )
 
-CONTAINER_12=(':atlasdb-ete-tests:timeLockMigrationTest')
+CONTAINER_12=(':atlasdb-dbkvs:check' ':atlasdb-cassandra:check' )
 
-CONTAINER_13=('compileJava' 'compileTestJava')
+CONTAINER_13=(':atlasdb-ete-tests:timeLockMigrationTest')
+
+CONTAINER_14=('compileJava' 'compileTestJava')
 
 # Excluded as it is split into two subsets
 EXCLUDED=(':atlasdb-cassandra-integration-tests:check')
 
 # Container 0 - runs tasks not found in the below containers
-CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}" "${CONTAINER_5[@]}" "${CONTAINER_6[@]}" "${CONTAINER_7[@]}" "${CONTAINER_8[@]}" "${CONTAINER_9[@]}" "${CONTAINER_10[@]}" "${CONTAINER_11[@]}" "${CONTAINER_12[@]}" "${EXCLUDED[@]}")
+CONTAINER_0_EXCLUDE=("${CONTAINER_1[@]}" "${CONTAINER_2[@]}" "${CONTAINER_3[@]}" "${CONTAINER_4[@]}" "${CONTAINER_5[@]}" "${CONTAINER_6[@]}" "${CONTAINER_7[@]}" "${CONTAINER_8[@]}" "${CONTAINER_9[@]}" "${CONTAINER_10[@]}" "${CONTAINER_11[@]}" "${CONTAINER_12[@]}" "${CONTAINER_13[@]}" "${EXCLUDED[@]}")
 
 for task in "${CONTAINER_0_EXCLUDE[@]}"
 do
@@ -73,8 +75,11 @@ JAVA_GC_LOGGING_OPTIONS="${JAVA_GC_LOGGING_OPTIONS} -Xlog:class+unload=off"
 JAVA_GC_LOGGING_OPTIONS="${JAVA_GC_LOGGING_OPTIONS} -Xlog:gc:build-%t-%p.gc.log"
 
 # External builds have a 16gb limit.
-if [ "$test_suite_index" -eq "13" ]; then
+if [ "$test_suite_index" -eq "14" ]; then
     export _JAVA_OPTIONS="-Xms2g -Xmx4g -XX:ActiveProcessorCount=8 ${JAVA_GC_LOGGING_OPTIONS}"
+elif [ "$test_suite_index" -eq "3" ]; then
+    export _JAVA_OPTIONS="-Xms8g -Xmx8g -XX:ActiveProcessorCount=8 ${JAVA_GC_LOGGING_OPTIONS}"
+    BASE_GRADLE_ARGS+=" --scan --parallel"
 else
     export _JAVA_OPTIONS="-Xmx4g ${JAVA_GC_LOGGING_OPTIONS}"
     BASE_GRADLE_ARGS+=" --scan --parallel"
@@ -96,5 +101,6 @@ case "$test_suite_index" in
     10) ./gradlew $BASE_GRADLE_ARGS ${CONTAINER_10[@]} ;;
     11) ./gradlew $BASE_GRADLE_ARGS ${CONTAINER_11[@]} ;;
     12) ./gradlew $BASE_GRADLE_ARGS ${CONTAINER_12[@]} ;;
-    13) ./gradlew $BASE_GRADLE_ARGS ${CONTAINER_13[@]} --stacktrace -PenableErrorProne=true && checkDocsBuild ;;
+    13) ./gradlew $BASE_GRADLE_ARGS ${CONTAINER_13[@]} ;;
+    14) ./gradlew $BASE_GRADLE_ARGS ${CONTAINER_14[@]} --stacktrace -PenableErrorProne=true && checkDocsBuild ;;
 esac
