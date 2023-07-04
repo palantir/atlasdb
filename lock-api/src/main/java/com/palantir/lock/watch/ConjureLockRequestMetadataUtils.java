276ddcf61016788924376a171a755977a3d822d9
Tom Papke <tpapke@palantir.com>
2023-07-04 16:37:01 +0100

    Add conjure objects for metadata Add conversion utility class with tests

---
+++ lock-api/src/main/java/com/palantir/lock/watch/ConjureLockRequestMetadataUtils.java
@@ -0,0 +1,132 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.lock.watch;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.palantir.atlasdb.timelock.api.ConjureChangeMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureCreatedChangeMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureDeletedChangeMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureLockRequestMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureUnchangedChangeMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureUpdatedChangeMetadata;
+import com.palantir.conjure.java.lib.Bytes;
+import com.palantir.lock.LockDescriptor;
+import com.palantir.lock.watch.ChangeMetadata.Created;
+import com.palantir.lock.watch.ChangeMetadata.Deleted;
+import com.palantir.lock.watch.ChangeMetadata.Unchanged;
+import com.palantir.lock.watch.ChangeMetadata.Updated;
+import com.palantir.util.IndexEncodingUtils;
+import com.palantir.util.IndexEncodingUtils.ChecksumType;
+import com.palantir.util.IndexEncodingUtils.IndexEncodingResult;
+import com.palantir.util.IndexEncodingUtils.KeyListChecksum;
+import com.palantir.util.Pair;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public final class ConjureLockRequestMetadataUtils {
+    private ConjureLockRequestMetadataUtils() {}
+
+    @VisibleForTesting
+    static final ChecksumType DEFAULT_CHECKSUM_TYPE = ChecksumType.CRC32_OF_DETERMINISTIC_HASHCODE;
+
+    public static Pair<List<LockDescriptor>, ConjureLockRequestMetadata> toConjureIndexEncoded(
+            Set<LockDescriptor> lockDescriptors, LockRequestMetadata metadata) {
+        ChangeMetadataToConjureVisitor toConjureVisitor = new ChangeMetadataToConjureVisitor();
+        IndexEncodingResult<LockDescriptor, ConjureChangeMetadata> encoded = IndexEncodingUtils.encode(
+                lockDescriptors,
+                metadata.lockDescriptorToChangeMetadata(),
+                changeMetadata -> changeMetadata.accept(toConjureVisitor),
+                DEFAULT_CHECKSUM_TYPE);
+        KeyListChecksum checksum = encoded.keyListChecksum();
+        ConjureLockRequestMetadata conjureLockRequestMetadata = ConjureLockRequestMetadata.of(
+                encoded.indexToValue(), checksum.type().getId(), Bytes.from(checksum.value()));
+        return Pair.create(encoded.keyList(), conjureLockRequestMetadata);
+    }
+
+    public static LockRequestMetadata fromConjureIndexEncoded(
+            List<LockDescriptor> keyList, ConjureLockRequestMetadata conjureMetadata) {
+        ChangeMetadataFromConjureVisitor fromConjureVisitor = new ChangeMetadataFromConjureVisitor();
+        ChecksumType checksumType = ChecksumType.valueOf(conjureMetadata.getChecksumType());
+        KeyListChecksum checksum = KeyListChecksum.of(
+                checksumType, conjureMetadata.getChecksumValue().asNewByteArray());
+        IndexEncodingResult<LockDescriptor, ConjureChangeMetadata> encoded =
+                IndexEncodingResult.of(keyList, conjureMetadata.getIndexToChangeMetadata(), checksum);
+        Map<LockDescriptor, ChangeMetadata> changeMetadata = IndexEncodingUtils.decode(
+                encoded, conjureChangeMetadata -> conjureChangeMetadata.accept(fromConjureVisitor));
+        // visitUnknown() will return null
+        changeMetadata.values().removeIf(Objects::isNull);
+        return LockRequestMetadata.of(changeMetadata);
+    }
+
+    private static final class ChangeMetadataToConjureVisitor implements ChangeMetadata.Visitor<ConjureChangeMetadata> {
+
+        @Override
+        public ConjureChangeMetadata visit(Unchanged unchanged) {
+            return ConjureChangeMetadata.unchanged(ConjureUnchangedChangeMetadata.of());
+        }
+
+        @Override
+        public ConjureChangeMetadata visit(Updated updated) {
+            return ConjureChangeMetadata.updated(
+                    ConjureUpdatedChangeMetadata.of(Bytes.from(updated.oldValue()), Bytes.from(updated.newValue())));
+        }
+
+        @Override
+        public ConjureChangeMetadata visit(Deleted deleted) {
+            return ConjureChangeMetadata.deleted(ConjureDeletedChangeMetadata.of(Bytes.from(deleted.oldValue())));
+        }
+
+        @Override
+        public ConjureChangeMetadata visit(Created created) {
+            return ConjureChangeMetadata.created(ConjureCreatedChangeMetadata.of(Bytes.from(created.newValue())));
+        }
+    }
+
+    private static final class ChangeMetadataFromConjureVisitor
+            implements ConjureChangeMetadata.Visitor<ChangeMetadata> {
+
+        @Override
+        public ChangeMetadata visitUnchanged(ConjureUnchangedChangeMetadata unchanged) {
+            return ChangeMetadata.unchanged();
+        }
+
+        @Override
+        public ChangeMetadata visitUpdated(ConjureUpdatedChangeMetadata updated) {
+            return ChangeMetadata.updated(
+                    updated.getOldValue().asNewByteArray(),
+                    updated.getNewValue().asNewByteArray());
+        }
+
+        @Override
+        public ChangeMetadata visitDeleted(ConjureDeletedChangeMetadata deleted) {
+            return ChangeMetadata.deleted(deleted.getOldValue().asNewByteArray());
+        }
+
+        @Override
+        public ChangeMetadata visitCreated(ConjureCreatedChangeMetadata created) {
+            return ChangeMetadata.created(created.getNewValue().asNewByteArray());
+        }
+
+        @Override
+        public ChangeMetadata visitUnknown(String unknownType) {
+            // caller should handle this case
+            return null;
+        }
+    }
+}
---
+++ lock-api/src/test/java/com/palantir/lock/watch/ConjureLockRequestMetadataUtilsTest.java
@@ -0,0 +1,94 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.lock.watch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.palantir.atlasdb.encoding.PtBytes;
+import com.palantir.atlasdb.timelock.api.ConjureChangeMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureCreatedChangeMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureDeletedChangeMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureLockRequestMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureUnchangedChangeMetadata;
+import com.palantir.atlasdb.timelock.api.ConjureUpdatedChangeMetadata;
+import com.palantir.conjure.java.lib.Bytes;
+import com.palantir.lock.LockDescriptor;
+import com.palantir.lock.StringLockDescriptor;
+import com.palantir.util.IndexEncodingUtils;
+import com.palantir.util.IndexEncodingUtils.KeyListChecksum;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ConjureLockRequestMetadataUtilsTest {
+    private static final LockDescriptor LOCK_1 = StringLockDescriptor.of("lock1");
+    private static final LockDescriptor LOCK_2 = StringLockDescriptor.of("lock2");
+    private static final LockDescriptor LOCK_3 = StringLockDescriptor.of("lock3");
+    private static final LockDescriptor LOCK_4 = StringLockDescriptor.of("lock4");
+    private static final Set<LockDescriptor> LOCK_SET = ImmutableSet.of(LOCK_1, LOCK_2, LOCK_3, LOCK_4);
+    private static final List<LockDescriptor> LOCK_LIST = ImmutableList.of(LOCK_1, LOCK_2, LOCK_3, LOCK_4);
+
+    // Although this is quite verbose, we explicitly want to test all possible types of change metadata and ensure
+    // that we do the conversion right for each of them.
+    private static final Map<LockDescriptor, ChangeMetadata> LOCKS_WITH_METADATA = ImmutableMap.of(
+            LOCK_1,
+            ChangeMetadata.unchanged(),
+            LOCK_2,
+            ChangeMetadata.updated(PtBytes.toBytes("old"), PtBytes.toBytes("new")),
+            LOCK_3,
+            ChangeMetadata.deleted(PtBytes.toBytes("deleted")),
+            LOCK_4,
+            ChangeMetadata.created(PtBytes.toBytes("created")));
+    private static final LockRequestMetadata LOCK_REQUEST_METADATA = LockRequestMetadata.of(LOCKS_WITH_METADATA);
+
+    private static final Map<Integer, ConjureChangeMetadata> CONJURE_LOCKS_WITH_METADATA = ImmutableMap.of(
+            0,
+            ConjureChangeMetadata.unchanged(ConjureUnchangedChangeMetadata.of()),
+            1,
+            ConjureChangeMetadata.updated(ConjureUpdatedChangeMetadata.of(
+                    Bytes.from(PtBytes.toBytes("old")), Bytes.from(PtBytes.toBytes("new")))),
+            2,
+            ConjureChangeMetadata.deleted(ConjureDeletedChangeMetadata.of(Bytes.from(PtBytes.toBytes("deleted")))),
+            3,
+            ConjureChangeMetadata.created(ConjureCreatedChangeMetadata.of(Bytes.from(PtBytes.toBytes("created")))));
+    private static ConjureLockRequestMetadata conjureLockRequestMetadata;
+
+    @BeforeClass
+    public static void setup() {
+        KeyListChecksum checksum =
+                IndexEncodingUtils.computeChecksum(ConjureLockRequestMetadataUtils.DEFAULT_CHECKSUM_TYPE, LOCK_LIST);
+        conjureLockRequestMetadata = ConjureLockRequestMetadata.of(
+                CONJURE_LOCKS_WITH_METADATA, checksum.type().getId(), Bytes.from(checksum.value()));
+    }
+
+    @Test
+    public void convertsToConjureCorrectly() {
+        assertThat(ConjureLockRequestMetadataUtils.toConjureIndexEncoded(LOCK_SET, LOCK_REQUEST_METADATA).rhSide)
+                .isEqualTo(conjureLockRequestMetadata);
+    }
+
+    @Test
+    public void convertsFromConjureCorrectly() {
+        assertThat(ConjureLockRequestMetadataUtils.fromConjureIndexEncoded(LOCK_LIST, conjureLockRequestMetadata))
+                .isEqualTo(LOCK_REQUEST_METADATA);
+    }
+}
