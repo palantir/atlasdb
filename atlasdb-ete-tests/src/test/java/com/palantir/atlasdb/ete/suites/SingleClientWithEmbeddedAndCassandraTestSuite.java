5cc159c4fe06b636de38ed0158deea26042d8d86
Tom Petracca <tpetracca@palantir.com>
2023-07-24 15:29:57 -0400

    embedded and cassandra

--- atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/suites/SingleClientWithEmbeddedAndCassandraTestSuite.java
+++ atlasdb-ete-tests/src/test/java/com/palantir/atlasdb/ete/suites/SingleClientWithEmbeddedAndCassandraTestSuite.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.palantir.atlasdb.ete;
+package com.palantir.atlasdb.ete.suites;
 
 import com.google.common.collect.ImmutableList;
 import com.palantir.atlasdb.containers.CassandraEnvironment;
+import com.palantir.atlasdb.ete.EteSetup;
+import com.palantir.atlasdb.ete.LockWithoutTimelockEteTest;
+import com.palantir.atlasdb.ete.MultiCassandraDoubleNodeDownEteTest;
+import com.palantir.atlasdb.ete.MultiCassandraSingleNodeDownEteTest;
+import com.palantir.atlasdb.ete.ServiceExposureEteTest;
+import com.palantir.atlasdb.ete.TimestampManagementEteTest;
+import com.palantir.atlasdb.ete.TodoEteTest;
 import com.palantir.atlasdb.ete.coordination.CoordinationEteTest;
 import com.palantir.atlasdb.ete.coordination.MultipleSchemaVersionsCoordinationEteTest;
 import org.junit.ClassRule;
@@ -35,13 +42,11 @@ import org.junit.runners.Suite;
     MultipleSchemaVersionsCoordinationEteTest.class,
     LockWithoutTimelockEteTest.class
 })
-public class MultiCassandraTestSuite extends EteSetup {
-    private static final ImmutableList<String> CLIENTS = ImmutableList.of("ete1");
-
+public class SingleClientWithEmbeddedAndCassandraTestSuite extends EteSetup {
     @ClassRule
     public static final RuleChain COMPOSITION_SETUP = EteSetup.setupComposition(
-            MultiCassandraTestSuite.class,
-            "docker-compose.multiple-cassandra.yml",
-            CLIENTS,
+            SingleClientWithEmbeddedAndCassandraTestSuite.class,
+            "docker-compose.single-client-with-embedded-and-cassandra.yml",
+            TestSuites.SINGLE_CLIENT,
             CassandraEnvironment.get());
 }
