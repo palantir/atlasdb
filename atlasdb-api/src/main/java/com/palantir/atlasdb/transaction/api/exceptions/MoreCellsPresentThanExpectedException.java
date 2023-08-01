2ab80f953b402de71813333655631d3f75bfb411
Lucas Meireles <meireles31@msn.com>
2023-08-01 13:38:21 +0100

    Make MoreCellsPresentThanExpectedException SafeLoggable

--- atlasdb-api/src/main/java/com/palantir/atlasdb/transaction/api/exceptions/MoreCellsPresentThanExpectedException.java
+++ atlasdb-api/src/main/java/com/palantir/atlasdb/transaction/api/exceptions/MoreCellsPresentThanExpectedException.java
@@ -17,26 +17,50 @@
 package com.palantir.atlasdb.transaction.api.exceptions;
 
 import com.palantir.atlasdb.keyvalue.api.Cell;
+import com.palantir.logsafe.Arg;
+import com.palantir.logsafe.Safe;
 import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.SafeLoggable;
+import com.palantir.logsafe.Unsafe;
 import com.palantir.logsafe.UnsafeArg;
-import com.palantir.logsafe.exceptions.SafeIllegalStateException;
+import com.palantir.logsafe.exceptions.SafeExceptions;
+import java.util.List;
 import java.util.Map;
 
-public class MoreCellsPresentThanExpectedException extends IllegalStateException {
+public final class MoreCellsPresentThanExpectedException extends IllegalStateException implements SafeLoggable {
+
+    private static final String MESSAGE =
+            "KeyValueService returned more results than Get expected. This means there is a bug"
+                    + "either in the SnapshotTransaction implementation or in how the client is "
+                    + "using such method.";
+    private final List<Arg<?>> arguments;
     private final Map<Cell, byte[]> fetchedCells;
 
     public MoreCellsPresentThanExpectedException(Map<Cell, byte[]> fetchedCells, long expectedNumberOfCells) {
-        super(new SafeIllegalStateException(
-                "KeyValueService returned more results than Get expected. This means there is a bug"
-                        + "either in the SnapshotTransaction implementation or in how the client is "
-                        + "using such method.",
-                SafeArg.of("expectedNumberOfCells", expectedNumberOfCells),
-                SafeArg.of("numberOfCellsRetrieved", fetchedCells.size()),
-                UnsafeArg.of("retrievedCells", fetchedCells)));
+        super(SafeExceptions.renderMessage(
+                MESSAGE, argsFrom(fetchedCells, expectedNumberOfCells).toArray(Arg<?>[]::new)));
         this.fetchedCells = fetchedCells;
+        this.arguments = argsFrom(fetchedCells, expectedNumberOfCells);
     }
 
     public Map<Cell, byte[]> getFetchedCells() {
         return fetchedCells;
+    }
+
+    @Override
+    public @Safe String getLogMessage() {
+        return MESSAGE;
+    }
+
+    @Override
+    public List<Arg<?>> getArgs() {
+        return arguments;
+    }
+
+    @Unsafe private static List<Arg<?>> argsFrom(Map<Cell, byte[]> retrievedCells, long expectedNumberOfCells) {
+        return List.of(
+                SafeArg.of("expectedNumberOfCells", expectedNumberOfCells),
+                SafeArg.of("numberOfCellsRetrieved", retrievedCells.size()),
+                UnsafeArg.of("retrievedCells", retrievedCells));
     }
 }
