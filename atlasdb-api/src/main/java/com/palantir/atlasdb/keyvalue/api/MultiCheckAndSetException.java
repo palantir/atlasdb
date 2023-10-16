61da171dbf6f36f85b33781fbe8546b2d1348f26
Mohammed Daudali <mdaudali@palantir.com>
2023-10-16 19:35:09 +0100

    safe MultiCheckAndSetException exception

--- atlasdb-api/src/main/java/com/palantir/atlasdb/keyvalue/api/MultiCheckAndSetException.java
+++ atlasdb-api/src/main/java/com/palantir/atlasdb/keyvalue/api/MultiCheckAndSetException.java
@@ -16,27 +16,45 @@
 
 package com.palantir.atlasdb.keyvalue.api;
 
+import com.google.common.collect.ImmutableList;
 import com.palantir.logsafe.Arg;
-import com.palantir.logsafe.exceptions.SafeRuntimeException;
+import com.palantir.logsafe.Safe;
+import com.palantir.logsafe.SafeLoggable;
+import com.palantir.logsafe.UnsafeArg;
+import com.palantir.logsafe.exceptions.SafeExceptions;
+import java.util.List;
 import java.util.Map;
 
-public class MultiCheckAndSetException extends RuntimeException {
+public class MultiCheckAndSetException extends RuntimeException implements SafeLoggable {
+    private static final String MESSAGE =
+            "Current values in the database do not match the expected values specified in multi-checkAndSet"
+                    + " request.";
     private final byte[] rowName;
     private final Map<Cell, byte[]> expectedValues;
     private final Map<Cell, byte[]> actualValues;
+
+    private final List<Arg<?>> args;
 
     public MultiCheckAndSetException(
             Arg<String> tableReference,
             byte[] rowName,
             Map<Cell, byte[]> expectedValue,
-            Map<Cell, byte[]> actualValues) {
-        super(new SafeRuntimeException(
-                "Current values in the database do not match the expected values specified in multi-checkAndSet"
-                        + " request.",
-                tableReference));
+            Map<Cell, byte[]> actualValues,
+            Arg<?>... args) {
+        this(
+                rowName,
+                expectedValue,
+                actualValues,
+                toArgListWithTableNameRowAndValues(tableReference, rowName, expectedValue, actualValues, args));
+    }
+
+    private MultiCheckAndSetException(
+            byte[] rowName, Map<Cell, byte[]> expectedValue, Map<Cell, byte[]> actualValues, List<Arg<?>> args) {
+        super(SafeExceptions.renderMessage(MESSAGE, args.toArray(new Arg[0])));
         this.rowName = rowName;
         this.expectedValues = expectedValue;
         this.actualValues = actualValues;
+        this.args = args;
     }
 
     public byte[] getRowName() {
@@ -49,5 +67,30 @@ public class MultiCheckAndSetException extends RuntimeException {
 
     public Map<Cell, byte[]> getActualValues() {
         return actualValues;
+    }
+
+    @Override
+    public @Safe String getLogMessage() {
+        return MESSAGE;
+    }
+
+    @Override
+    public List<Arg<?>> getArgs() {
+        return args;
+    }
+
+    private static List<Arg<?>> toArgListWithTableNameRowAndValues(
+            Arg<String> tableReference,
+            byte[] rowName,
+            Map<Cell, byte[]> expectedValue,
+            Map<Cell, byte[]> actualValues,
+            Arg<?>... args) {
+        return ImmutableList.<Arg<?>>builderWithExpectedSize(args.length + 4)
+                .add(args)
+                .add(tableReference)
+                .add(UnsafeArg.of("rowName", rowName))
+                .add(UnsafeArg.of("expectedValue", expectedValue))
+                .add(UnsafeArg.of("actualValues", actualValues))
+                .build();
     }
 }
