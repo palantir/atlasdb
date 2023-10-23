6dddb0a053c517ef44ec8eb8a76721316b755a11
annieli <annieli@palantir.com>
2023-10-23 11:39:47 -0700

    update sql format

--- atlasdb-dbkvs-tests/src/test/resources/docker-startup/01_create_user.sql
+++ atlasdb-dbkvs-tests/src/test/resources/docker-startup/01_create_user.sql
@@ -1,4 +1,15 @@
 CREATE USER palantir IDENTIFIED BY "7_SeeingStones_7";
-grant create session, create procedure, create sequence, create table, create view, create type to palantir;
-ALTER USER palantir QUOTA UNLIMITED ON USERS;
-exit;
+
+GRANT
+    CREATE SESSION,
+    CREATE PROCEDURE,
+    CREATE SEQUENCE,
+    CREATE TABLE,
+    CREATE VIEW,
+    CREATE TYPE
+TO palantir;
+
+ALTER USER palantir
+    QUOTA UNLIMITED ON users;
+
+EXIT;
