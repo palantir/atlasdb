b831d64cf9ada2568f03ddbb3f538cf6e81581f8
Lucas Meireles <meireles31@msn.com>
2023-11-15 11:07:27 +0000

    Tag images to antithesis on Publish step and schedule triggering on a recurring basis

--- scripts/circle-ci/publish-images-to-antithesis.sh
+++ scripts/circle-ci/publish-images-to-antithesis.sh
@@ -10,11 +10,6 @@ if [[ -z "${ANTITHESIS_LOGIN_JSON}" ]]; then
   exit 1
 fi
 
-if [[ -z "${ANTITHESIS_WEBHOOK_PASSWORD}" ]]; then
-  echo "Antithesis webhook password is not set as an environment variable, exiting."
-  exit 1
-fi
-
 printenv ANTITHESIS_LOGIN_JSON | base64 -d | docker login -u _json_key https://${ANTITHESIS_REPO_URL} --password-stdin
 
 ./gradlew --scan dockerTag
@@ -34,7 +29,3 @@ docker push ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:${EXPECT
 
 docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${VERSION} ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TAG}
 docker push ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TAG}
-
-WEBHOOK_LOCATOR="palantir_atlasdb__smoketest__latest"
-echo "Triggering simulation on Antithesis via the ${WEBHOOK_LOCATOR} webhook."
-curl -v -u "palantir:${ANTITHESIS_WEBHOOK_PASSWORD}" -X POST https://palantir.antithesis.com/api/v1/launch_experiment/${WEBHOOK_LOCATOR}
