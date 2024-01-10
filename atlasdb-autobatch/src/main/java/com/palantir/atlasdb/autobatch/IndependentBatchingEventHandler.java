0d9df7ed74ec0df22141725744781ce5d8a4064a
Ahmed Alouane <aalouane@palantir.com>
2024-01-10 17:38:03 +0000

    IndependentBatchingEventHandler supports multiple concurrent batches, test write-batching-transaction-service with that

--- atlasdb-autobatch/src/main/java/com/palantir/atlasdb/autobatch/IndependentBatchingEventHandler.java
+++ atlasdb-autobatch/src/main/java/com/palantir/atlasdb/autobatch/IndependentBatchingEventHandler.java
@@ -16,35 +16,79 @@
 
 package com.palantir.atlasdb.autobatch;
 
+import com.google.common.collect.ImmutableList;
 import com.lmax.disruptor.EventHandler;
+import com.palantir.common.concurrent.PTExecutors;
+import com.palantir.logsafe.Preconditions;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
 import java.util.function.Consumer;
 
 final class IndependentBatchingEventHandler<T, R> implements EventHandler<BatchElement<T, R>> {
     private final Consumer<List<BatchElement<T, R>>> batchFunction;
     private final List<BatchElement<T, R>> pending;
+    private final Optional<ExecutorService> executor;
 
-    IndependentBatchingEventHandler(Consumer<List<BatchElement<T, R>>> batchFunction, int bufferSize) {
+    IndependentBatchingEventHandler(
+            Consumer<List<BatchElement<T, R>>> batchFunction, int bufferSize, Optional<ExecutorService> executor) {
         this.batchFunction = batchFunction;
         this.pending = new ArrayList<>(bufferSize);
+        this.executor = executor;
+    }
+
+    static <T, R> IndependentBatchingEventHandler<T, R> create(
+            Consumer<List<BatchElement<T, R>>> batchFunction, int bufferSize, int maxParallelBatches) {
+        Preconditions.checkArgument(
+                (maxParallelBatches >= 1) && (maxParallelBatches <= 5),
+                "Up to 5 parallel batches can run but not more");
+        if (maxParallelBatches == 1) {
+            return new IndependentBatchingEventHandler<>(batchFunction, bufferSize, Optional.empty());
+        } else {
+            return new IndependentBatchingEventHandler<>(
+                    batchFunction,
+                    bufferSize,
+                    Optional.of(PTExecutors.newFixedThreadPool(
+                            maxParallelBatches, "independent-batching-event-handler-flush")));
+        }
     }
 
     @Override
     public void onEvent(BatchElement<T, R> event, long sequence, boolean endOfBatch) {
         pending.add(event);
         if (endOfBatch) {
-            flush();
+            signalFlush();
         }
     }
 
-    private void flush() {
-        try {
-            batchFunction.accept(Collections.unmodifiableList(pending));
-        } catch (Throwable t) {
-            pending.forEach(p -> p.result().setException(t));
+    private void signalFlush() {
+        if (executor.isPresent()) {
+            List<BatchElement<T, R>> pendingCopy = ImmutableList.copyOf(pending);
+            asyncFlush(batchFunction, pendingCopy, executor.get());
+        } else {
+            List<BatchElement<T, R>> pendingView = Collections.unmodifiableList(pending);
+            syncFlush(batchFunction, pendingView);
         }
+
         pending.clear();
+    }
+
+    private static <T, R> void asyncFlush(
+            Consumer<List<BatchElement<T, R>>> batchFunction,
+            List<BatchElement<T, R>> elementsToFlush,
+            ExecutorService executor) {
+        executor.execute(() -> syncFlush(batchFunction, elementsToFlush));
+    }
+
+    private static <T, R> void syncFlush(
+            Consumer<List<BatchElement<T, R>>> batchFunction, List<BatchElement<T, R>> elementsToFlush) {
+        elementsToFlush.forEach(batchElement -> batchElement.result().running());
+        try {
+            batchFunction.accept(elementsToFlush);
+        } catch (Throwable t) {
+            elementsToFlush.forEach(p -> p.result().setException(t));
+        }
     }
 }
