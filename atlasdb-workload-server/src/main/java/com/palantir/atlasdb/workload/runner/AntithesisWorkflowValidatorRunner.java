07bc2dec5a5e3a89138f631f04f14b3c7b817201
Jeremy Kong <jkong@palantir.com>
2024-01-10 12:31:29 +0100

    clarify

--- atlasdb-workload-server/src/main/java/com/palantir/atlasdb/workload/runner/AntithesisWorkflowValidatorRunner.java
+++ atlasdb-workload-server/src/main/java/com/palantir/atlasdb/workload/runner/AntithesisWorkflowValidatorRunner.java
@@ -51,7 +51,8 @@ public final class AntithesisWorkflowValidatorRunner implements WorkflowValidato
                     .get();
             log.info("antithesis: stop_faults");
             log.info("antithesis: start_validation");
-            // TODO (jkong): Do we need to wait for the cluster to stabilise here?
+            // TODO (jkong): Do we need to wait for the cluster to stabilise here? Possible race condition if
+            // a server fault was still live when this validation began.
             workflowHistoryValidators.forEach(this::checkAndReportInvariantViolations);
             log.info("antithesis: end_validation");
         } catch (InterruptedException e) {
