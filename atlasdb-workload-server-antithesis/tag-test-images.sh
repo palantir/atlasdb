6fcdbcf2d96a21df5f223fa3290b1ad53593446f
Lucas Meireles <meireles31@msn.com>
2024-01-24 17:27:49 +0000

    reference test tag

--- atlasdb-workload-server-antithesis/tag-test-images.sh
+++ atlasdb-workload-server-antithesis/tag-test-images.sh
@@ -8,7 +8,7 @@ VERSION=$(./gradlew -q printVersion)
 EXPECTED_ANTITHESIS_TEST_TAG="unspecified"
 
 docker pull palantirtechnologies/cassandra:2.2.18-1.116.0
-docker tag palantirtechnologies/cassandra:2.2.18-1.116.0 cassandra:${EXPECTED_ANTITHESIS_TAG}
-docker tag palantirtechnologies/timelock-server-distribution:${VERSION} timelock-server-distribution:${EXPECTED_ANTITHESIS_TAG}
-docker tag palantirtechnologies/atlasdb-workload-server-distribution:${VERSION} atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TAG}
-docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${VERSION} atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TAG}
+docker tag palantirtechnologies/cassandra:2.2.18-1.116.0 cassandra:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/timelock-server-distribution:${VERSION} timelock-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/atlasdb-workload-server-distribution:${VERSION} atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${VERSION} atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TEST_TAG}
