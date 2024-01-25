7d8033d40b8e37fea5a08fdc70aea4a5416b3d66
Lucas Meireles <meireles31@msn.com>
2024-01-25 10:58:18 +0000

    try to use latest

--- atlasdb-workload-server-antithesis/tag-test-images.sh
+++ atlasdb-workload-server-antithesis/tag-test-images.sh
@@ -7,6 +7,6 @@ EXPECTED_ANTITHESIS_TEST_TAG="unspecified"
 
 docker pull palantirtechnologies/cassandra:2.2.18-1.116.0
 docker tag palantirtechnologies/cassandra:2.2.18-1.116.0 palantirtechnologies/cassandra:${EXPECTED_ANTITHESIS_TEST_TAG}
-docker tag palantirtechnologies/timelock-server-distribution:${VERSION} palantirtechnologies/timelock-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
-docker tag palantirtechnologies/atlasdb-workload-server-distribution:${VERSION} palantirtechnologies/atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/timelock-server-distribution:latest palantirtechnologies/timelock-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/atlasdb-workload-server-distribution:latest palantirtechnologies/atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
 docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${VERSION} palantirtechnologies/atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TEST_TAG}
