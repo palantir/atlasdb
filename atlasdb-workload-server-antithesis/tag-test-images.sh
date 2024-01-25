af04b21da32e03a9b0cacc7d3f51efe33d814583
Lucas Meireles <meireles31@msn.com>
2024-01-25 10:29:00 +0000

    add palantir prefix to tagged

--- atlasdb-workload-server-antithesis/tag-test-images.sh
+++ atlasdb-workload-server-antithesis/tag-test-images.sh
@@ -6,7 +6,7 @@ VERSION=$1
 EXPECTED_ANTITHESIS_TEST_TAG="unspecified"
 
 docker pull palantirtechnologies/cassandra:2.2.18-1.116.0
-docker tag palantirtechnologies/cassandra:2.2.18-1.116.0 cassandra:${EXPECTED_ANTITHESIS_TEST_TAG}
-docker tag palantirtechnologies/timelock-server-distribution:${VERSION} timelock-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
-docker tag palantirtechnologies/atlasdb-workload-server-distribution:${VERSION} atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
-docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${VERSION} atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/cassandra:2.2.18-1.116.0 palantirtechnologies/cassandra:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/timelock-server-distribution:${VERSION} palantirtechnologies/timelock-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/atlasdb-workload-server-distribution:${VERSION} palantirtechnologies/atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TEST_TAG}
+docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${VERSION} palantirtechnologies/atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TEST_TAG}
