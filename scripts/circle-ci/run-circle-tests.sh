d1ef8172aa623b81c2fb0540c25b6e2c10920f78
Lucas Meireles <meireles31@msn.com>
2024-01-25 11:21:55 +0000

    don't run 14 in parallel

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -79,6 +79,9 @@ JAVA_GC_LOGGING_OPTIONS="${JAVA_GC_LOGGING_OPTIONS} -Xlog:gc:build-%t-%p.gc.log"
 # External builds have a 16gb limit.
 if [ "$test_suite_index" -eq "15" ]; then
     export _JAVA_OPTIONS="-Xms2g -Xmx4g -XX:ActiveProcessorCount=8 ${JAVA_GC_LOGGING_OPTIONS}"
+if [ "$test_suite_index" -eq "14" ]; then
+    export _JAVA_OPTIONS="-Xmx4g ${JAVA_GC_LOGGING_OPTIONS}"
+    BASE_GRADLE_ARGS+=" --scan"
 elif [ "$test_suite_index" -eq "4" ]; then
     export _JAVA_OPTIONS="-Xms8g -Xmx8g -XX:ActiveProcessorCount=8 ${JAVA_GC_LOGGING_OPTIONS}"
 elif [ "$test_suite_index" -eq "3" ]; then
