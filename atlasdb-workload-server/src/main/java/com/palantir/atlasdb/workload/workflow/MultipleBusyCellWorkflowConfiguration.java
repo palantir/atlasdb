5573cb57f31e9f31977f9878dcf3cb73a993f714
Mohammed Daudali <mdaudali@palantir.com>
2024-03-11 11:52:41 +0000

    Initial workflow

---
+++ atlasdb-workload-server/src/main/java/com/palantir/atlasdb/workload/workflow/MultipleBusyCellWorkflowConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.workload.workflow;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableRandomWorkflowConfiguration.class)
+@JsonDeserialize(as = ImmutableRandomWorkflowConfiguration.class)
+@JsonTypeName(MultipleBusyCellWorkflowConfiguration.TYPE)
+public interface MultipleBusyCellWorkflowConfiguration extends WorkflowConfiguration {
+    String TYPE = "multiple-busy-cell";
+
+    TableConfiguration tableConfiguration();
+
+    @Value.Default
+    default int maxCells() {
+        return 5;
+    }
+
+    @Value.Default
+    default double deleteProbability() {
+        return 0.2;
+    }
+
+    @Value.Default
+    default int maxWritesPerCell() {
+        return 1000;
+    }
+
+    @Value.Default
+    default int maxReadsPerCell() {
+        return 50;
+    }
+}
---
+++ atlasdb-workload-server/src/main/java/com/palantir/atlasdb/workload/workflow/MultipleBusyCellWorkflows.java
@@ -0,0 +1,114 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.workload.workflow;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.palantir.atlasdb.buggify.impl.DefaultNativeSamplingSecureRandomFactory;
+import com.palantir.atlasdb.workload.store.ImmutableWorkloadCell;
+import com.palantir.atlasdb.workload.store.InteractiveTransactionStore;
+import com.palantir.atlasdb.workload.store.ReadOnlyTransactionStore;
+import com.palantir.atlasdb.workload.store.WorkloadCell;
+import com.palantir.atlasdb.workload.transaction.DeleteTransactionAction;
+import com.palantir.atlasdb.workload.transaction.WriteTransactionAction;
+import com.palantir.atlasdb.workload.transaction.witnessed.WitnessedTransaction;
+import com.palantir.atlasdb.workload.transaction.witnessed.WitnessedTransactions;
+import java.security.SecureRandom;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * This workflow attempts to heavily manipulate a few cells across different rows in an attempt to create a large number
+ * of tombstones across multiple DB nodes. This aims to verify that Sweep successfully remains up to date as much as
+ * possible, even as Cassandra nodes die, without overwhelming the cluster. This differs from SingleRow/SingleCell
+ * workflows as it attempts to spread data across nodes, and also intends to do a far greater number of writes to
+ * create a large number of AtlasDB tombstones.
+ * TODO: Check that we don't run just a 3 node Cassandra cluster
+ */
+public final class MultipleBusyCellWorkflows {
+    private static final SecureRandom SECURE_RANDOM = DefaultNativeSamplingSecureRandomFactory.INSTANCE.create();
+
+    private MultipleBusyCellWorkflows() {
+        // utility
+    }
+
+    public static Workflow create(
+            InteractiveTransactionStore store,
+            MultipleBusyCellWorkflowConfiguration configuration,
+            ListeningExecutorService readExecutor,
+            ListeningExecutorService writeExecutor) {
+        return () -> {
+            List<WorkloadCell> cells = IntStream.range(0, configuration.maxCells())
+                    .mapToObj(_x -> ImmutableWorkloadCell.of(SECURE_RANDOM.nextInt(), SECURE_RANDOM.nextInt()))
+                    .collect(Collectors.toList());
+            List<ListenableFuture<Optional<WitnessedTransaction>>> writes = cells.stream()
+                    .flatMap(cell -> scheduleWrites(store, cell, configuration, writeExecutor).stream())
+                    .collect(Collectors.toList());
+            List<ListenableFuture<Optional<WitnessedTransaction>>> reads = cells.stream()
+                    .flatMap(cell -> scheduleReads(store, cell, configuration, readExecutor).stream())
+                    .collect(Collectors.toList());
+            ListenableFuture<List<WitnessedTransaction>> witnessedTransactions = Futures.transform(
+                    Futures.allAsList(
+                            Stream.of(reads, writes).flatMap(Collection::stream).collect(Collectors.toList())),
+                    maybeWitnessedTransactions -> maybeWitnessedTransactions.stream()
+                            .flatMap(Optional::stream)
+                            .collect(Collectors.toList()),
+                    MoreExecutors.directExecutor());
+            ReadOnlyTransactionStore readOnlyTransactionStore = new ReadOnlyTransactionStore(store);
+            return ImmutableWorkflowHistory.builder()
+                    .addAllHistory(WitnessedTransactions.sortAndFilterTransactions(
+                            readOnlyTransactionStore, Futures.getUnchecked(witnessedTransactions)))
+                    .transactionStore(readOnlyTransactionStore)
+                    .build();
+        };
+    }
+
+    private static List<ListenableFuture<Optional<WitnessedTransaction>>> scheduleReads(
+            InteractiveTransactionStore store,
+            WorkloadCell cell,
+            MultipleBusyCellWorkflowConfiguration configuration,
+            ListeningExecutorService readExecutor) {
+        return IntStream.range(0, configuration.maxReadsPerCell())
+                .mapToObj(idx -> readExecutor.submit(() -> store.readWrite(
+                        txn -> txn.read(configuration.tableConfiguration().tableName(), cell))))
+                .collect(Collectors.toList());
+    }
+
+    private static List<ListenableFuture<Optional<WitnessedTransaction>>> scheduleWrites(
+            InteractiveTransactionStore store,
+            WorkloadCell cell,
+            MultipleBusyCellWorkflowConfiguration configuration,
+            ListeningExecutorService writeExecutor) {
+        return IntStream.range(0, configuration.maxWritesPerCell())
+                .mapToObj(index -> {
+                    if (SECURE_RANDOM.nextDouble() < configuration.deleteProbability()) {
+                        return writeExecutor.submit(() -> store.readWrite(List.of(DeleteTransactionAction.of(
+                                configuration.tableConfiguration().tableName(), cell))));
+                    } else {
+                        return writeExecutor.submit(() -> store.readWrite(List.of(WriteTransactionAction.of(
+                                configuration.tableConfiguration().tableName(), cell, index))));
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+}
