8f9c1d56808fe5a6e03a9cd51d234aa20f4da6bf
Mohammed Daudali <mdaudali@palantir.com>
2024-04-26 14:00:48 +0100

    set publish

--- scripts/circle-ci/publish-images-to-antithesis.sh
+++ scripts/circle-ci/publish-images-to-antithesis.sh
@@ -15,17 +15,23 @@ printenv ANTITHESIS_LOGIN_JSON | base64 -d | docker login -u _json_key https://$
 ./gradlew --scan dockerTag
 
 VERSION=$(./gradlew -q printVersion)
-EXPECTED_ANTITHESIS_TAG="latest"
+EXPECTED_ANTITHESIS_TAG_DC1="migration-dc1"
+EXPECTED_ANTITHESIS_TAG_DC2="migration-dc1"
+EXPECTED_ANTITHESIS_TAG_OTHER="migration-latest"
 
-docker pull palantirtechnologies/cassandra:2.2.18-1.143.0-rc5
-docker tag palantirtechnologies/cassandra:2.2.18-1.143.0-rc5 ${ANTITHESIS_REPO_URL}/cassandra:${EXPECTED_ANTITHESIS_TAG}
-docker push ${ANTITHESIS_REPO_URL}/cassandra:${EXPECTED_ANTITHESIS_TAG}
+docker pull palantirtechnologies/cassandra:2.2.18-1.143.0-rc11
+docker tag palantirtechnologies/cassandra:2.2.18-1.143.0-rc11 ${ANTITHESIS_REPO_URL}/cassandra:${EXPECTED_ANTITHESIS_TAG_DC1}
+docker push ${ANTITHESIS_REPO_URL}/cassandra:${EXPECTED_ANTITHESIS_TAG_DC1}
 
-docker tag palantirtechnologies/timelock-server-distribution:${VERSION} ${ANTITHESIS_REPO_URL}/timelock-server-distribution:${EXPECTED_ANTITHESIS_TAG}
-docker push ${ANTITHESIS_REPO_URL}/timelock-server-distribution:${EXPECTED_ANTITHESIS_TAG}
+docker pull palantirtechnologies/cassandra:2.2.18-1.143.0-rc14
+docker tag palantirtechnologies/cassandra:2.2.18-1.143.0-rc14 ${ANTITHESIS_REPO_URL}/cassandra:${EXPECTED_ANTITHESIS_TAG_DC2}
+docker push ${ANTITHESIS_REPO_URL}/cassandra:${EXPECTED_ANTITHESIS_TAG_DC2}
 
-docker tag palantirtechnologies/atlasdb-workload-server-distribution:${VERSION} ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TAG}
-docker push ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TAG}
+docker tag palantirtechnologies/timelock-server-distribution:${VERSION} ${ANTITHESIS_REPO_URL}/timelock-server-distribution:${EXPECTED_ANTITHESIS_TAG_OTHER}
+docker push ${ANTITHESIS_REPO_URL}/timelock-server-distribution:${EXPECTED_ANTITHESIS_TAG_OTHER}
 
-docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${VERSION} ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TAG}
-docker push ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TAG}
+docker tag palantirtechnologies/atlasdb-workload-server-distribution:${VERSION} ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TAG_OTHER}
+docker push ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-distribution:${EXPECTED_ANTITHESIS_TAG_OTHER}
+
+docker tag palantirtechnologies/atlasdb-workload-server-antithesis:${VERSION} ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TAG_OTHER}
+docker push ${ANTITHESIS_REPO_URL}/atlasdb-workload-server-antithesis:${EXPECTED_ANTITHESIS_TAG_OTHER}
