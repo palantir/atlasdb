3d7d786357fe886b7bbd16b88b29494c0fd95fb8
Mohammed Daudali <mdaudali@palantir.com>
2024-07-12 19:22:02 +0100

    [ASTS] feat(bucket-retrieval): Lockables

---
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/locks/Lockable.java
@@ -0,0 +1,133 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.sweep.asts.locks;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.errorprone.annotations.CheckReturnValue;
+import com.palantir.lock.LockDescriptor;
+import com.palantir.lock.v2.LockRequest;
+import com.palantir.lock.v2.LockToken;
+import com.palantir.lock.v2.TimelockService;
+import com.palantir.logsafe.UnsafeArg;
+import com.palantir.logsafe.logger.SafeLogger;
+import com.palantir.logsafe.logger.SafeLoggerFactory;
+import com.palantir.refreshable.Refreshable;
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+// See the LockableFactory comment for why keeping the value in the class ends up making things simpler from a local
+// lock management perspective.
+// Separately:
+// I, personally, am much more used to working with locks being attached to the data object. It's, imo, harder to screw
+// up and forget to lock / unlock things when a) you can't access the data without the lock and b) the error checker
+// will shout at you for not unlocking
+// TODO: Make the error checker shout at you
+// It also made the local isLocked check easier to manage rather than having a separate mapping (or, you don't have it
+// at all, but with so many threads and relatively high chance of some collisions, it seems like wasted RPCs otherwise)
+public final class Lockable<T> {
+    private static final SafeLogger log = SafeLoggerFactory.get(Lockable.class);
+    private final LockDescriptor lockDescriptor;
+    private final T inner;
+    private final TimelockService timeLock;
+    private final Refreshable<Duration> lockTimeout;
+
+    // Consider whether this should be a lock. It's simply an optimisation to avoid a check for timelock
+    // each time you want to test if it's locked by something locally, but we must make sure there are no false
+    // negatives. It's allowed that something local is working on this item and Timelock has released the lock due to
+    // expiry and this is still True (since that still lets us minimise conflicts by not working on it locally),
+    // but it should not be true if something local has stopped working on this item and closed the inner class
+    private final AtomicBoolean isLocked = new AtomicBoolean(false);
+
+    private Lockable(
+            T inner, LockDescriptor lockDescriptor, TimelockService timeLock, Refreshable<Duration> lockTimeout) {
+        this.timeLock = timeLock;
+        this.inner = inner;
+        this.lockTimeout = lockTimeout;
+        this.lockDescriptor = lockDescriptor;
+    }
+
+    public static <T> Lockable<T> create(
+            T inner, LockDescriptor lockDescriptor, TimelockService timeLock, Refreshable<Duration> lockTimeout) {
+        return new Lockable<>(inner, lockDescriptor, timeLock, lockTimeout);
+    }
+
+    // For code owners:
+    // Do not use this for multiple locks!! If you're not careful, you can make it really easy to deadlock
+    // If you end up using this class and have a need for batch locks, implement a separate method that uses
+    // Timelocks batch locking methods and ensure there's a consistent ordering when taking out local locks
+    // to avoid circular waiting.
+    //    @MustBeClosed TODO: How do I get this working?? I'll track looking into this for another time.
+    @CheckReturnValue
+    public Optional<Inner> tryLock() {
+        if (isLocked.compareAndSet(false, true)) {
+            // We do not want the timeout to be too low to avoid a race condition where we give up too soon
+            LockRequest request = LockRequest.of(
+                    ImmutableSet.of(lockDescriptor), lockTimeout.get().toMillis());
+
+            try {
+                return timeLock.lock(request).getTokenOrEmpty().map(lockToken -> new Inner(lockToken));
+            } catch (Exception e) {
+                log.error("Failed to acquire lock", UnsafeArg.of("lockDescriptor", lockDescriptor), e);
+                isLocked.set(false);
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    // Expected to be used in a try closeable.
+    public final class Inner implements AutoCloseable {
+        private final LockToken lockToken;
+
+        private Inner(LockToken lockToken) {
+            this.lockToken = lockToken;
+        }
+
+        public T getInner() {
+            return inner;
+        }
+
+        public Lockable<T> getOuter() {
+            return Lockable.this;
+        }
+
+        @Override
+        public void close() {
+            try {
+                timeLock.unlock(ImmutableSet.of(lockToken));
+            } finally {
+                isLocked.set(false); // We end up holding the local lock ever so slightly longer than necessary.
+            }
+        }
+    }
+
+    public static class LockableComparator<T> implements Comparator<Lockable<T>> {
+        private final Comparator<T> comparator;
+
+        public LockableComparator(Comparator<T> comparator) {
+            this.comparator = comparator;
+        }
+
+        @Override
+        public int compare(Lockable<T> o1, Lockable<T> o2) {
+            return comparator.compare(o1.inner, o2.inner);
+        }
+    }
+}
---
+++ atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/asts/locks/LockableTest.java
@@ -0,0 +1,178 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.sweep.asts.locks;
+
+import static com.palantir.logsafe.testing.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.ImmutableSet;
+import com.palantir.atlasdb.sweep.asts.locks.Lockable.LockableComparator;
+import com.palantir.lock.v2.LockRequest;
+import com.palantir.lock.v2.LockResponse;
+import com.palantir.lock.v2.LockToken;
+import com.palantir.lock.v2.TimelockService;
+import com.palantir.refreshable.Refreshable;
+import com.palantir.refreshable.SettableRefreshable;
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
+
+@ExtendWith(MockitoExtension.class)
+public final class LockableTest {
+    private static final TestLockable TEST_LOCKABLE = TestLockable.of(1);
+    private final SettableRefreshable<Duration> lockTimeout = Refreshable.create(Duration.ofSeconds(1));
+    private Lockable<TestLockable> lockable;
+
+    @Mock
+    private TimelockService timelockService;
+
+    @BeforeEach
+    public void setup() {
+        lockable = createLockable(TEST_LOCKABLE);
+    }
+
+    @Test
+    public void comparatorUsesProvidedComparatorForComparison() {
+        TestLockable otherLockable = TestLockable.of(2);
+        Lockable<TestLockable> lockOne = createLockable(TEST_LOCKABLE);
+        Lockable<TestLockable> lockTwo = createLockable(otherLockable);
+        Comparator<TestLockable> comparator = mock(Comparator.class);
+        LockableComparator<TestLockable> lockableComparator = new LockableComparator<>(comparator);
+
+        int _result = lockableComparator.compare(lockOne, lockTwo);
+        verify(comparator).compare(TEST_LOCKABLE, otherLockable);
+    }
+
+    @Test
+    public void tryLockLocksObjectLocally() {
+        withLockMock().thenReturn(lockResponse(lockToken()));
+        Optional<Lockable<TestLockable>.Inner> lockedValue = lockable.tryLock();
+        assertThatValueIsLocked(lockedValue);
+
+        Optional<Lockable<TestLockable>.Inner> attemptTwo = lockable.tryLock();
+        assertThat(attemptTwo).isEmpty();
+    }
+
+    @Test
+    public void tryLockLocksObjectInTimelock() {
+        withLockMock().thenReturn(lockResponse(lockToken()));
+        Optional<Lockable<TestLockable>.Inner> lockedValue = lockable.tryLock();
+        verify(timelockService)
+                .lock(LockRequest.of(
+                        ImmutableSet.of(TEST_LOCKABLE.lockDescriptor()),
+                        lockTimeout.get().toMillis()));
+    }
+
+    @Test
+    public void tryLockReturnsEmptyAndUnlocksLocallyIfTimelockCallFails() {
+        withLockMock().thenThrow(new RuntimeException("Timelock is down")).thenReturn(lockResponse(lockToken()));
+        Optional<Lockable<TestLockable>.Inner> lockedValue = lockable.tryLock();
+        assertThat(lockedValue).isEmpty();
+
+        Optional<Lockable<TestLockable>.Inner> attemptTwo = lockable.tryLock();
+        assertThatValueIsLocked(attemptTwo);
+    }
+
+    @Test
+    public void closingInnerCallsTimelockUnlock() {
+        LockToken lockToken = lockToken();
+        withLockMock().thenReturn(lockResponse(lockToken));
+        Optional<Lockable<TestLockable>.Inner> lockedValue = lockable.tryLock();
+        assertThatValueIsLocked(lockedValue);
+
+        withUnlockMock(lockToken).thenReturn(ImmutableSet.of(lockToken));
+        lockedValue.orElseThrow().close();
+        verify(timelockService).unlock(ImmutableSet.of(lockToken));
+    }
+
+    @Test
+    public void closingInnerRemovesIsLockedFlagEvenIfTimelockUnlockFails() {
+        LockToken lockToken = lockToken();
+
+        withLockMock().thenReturn(lockResponse(lockToken));
+        Optional<Lockable<TestLockable>.Inner> lockedValue = lockable.tryLock();
+        assertThatValueIsLocked(lockedValue);
+
+        RuntimeException exception = new RuntimeException("Timelock is down");
+        withUnlockMock(lockToken).thenThrow(exception);
+        assertThatThrownBy(lockedValue.orElseThrow()::close).isEqualTo(exception);
+        assertThat(lockable.tryLock()).isPresent();
+    }
+
+    @Test
+    public void requestsUseCurrentLockTimeout() {
+        lockTimeout.update(Duration.ofSeconds(2));
+        LockToken lockToken = lockToken();
+        withLockMock().thenReturn(lockResponse(lockToken));
+        Optional<Lockable<TestLockable>.Inner> lockedValueTwoSeconds = lockable.tryLock();
+
+        verify(timelockService)
+                .lock(LockRequest.of(
+                        ImmutableSet.of(TEST_LOCKABLE.lockDescriptor()),
+                        Duration.ofSeconds(2).toMillis()));
+
+        lockedValueTwoSeconds.orElseThrow().close();
+
+        lockTimeout.update(Duration.ofSeconds(3));
+        withLockMock().thenReturn(lockResponse(lockToken()));
+        Optional<Lockable<TestLockable>.Inner> _lockedValueThreeSeconds = lockable.tryLock();
+
+        verify(timelockService)
+                .lock(LockRequest.of(
+                        ImmutableSet.of(TEST_LOCKABLE.lockDescriptor()),
+                        Duration.ofSeconds(3).toMillis()));
+    }
+
+    private Lockable<TestLockable> createLockable(TestLockable value) {
+        return Lockable.create(value, value.lockDescriptor(), timelockService, lockTimeout);
+    }
+
+    private OngoingStubbing<LockResponse> withLockMock() {
+        return when(timelockService.lock(LockRequest.of(
+                ImmutableSet.of(LockableTest.TEST_LOCKABLE.lockDescriptor()),
+                lockTimeout.get().toMillis())));
+    }
+
+    private OngoingStubbing<Set<LockToken>> withUnlockMock(LockToken lockToken) {
+        return when(timelockService.unlock(ImmutableSet.of(lockToken)));
+    }
+
+    private LockToken lockToken() {
+        return LockToken.of(UUID.randomUUID());
+    }
+
+    private LockResponse lockResponse(LockToken lockToken) {
+        return LockResponse.successful(lockToken);
+    }
+
+    private void assertThatValueIsLocked(Optional<Lockable<TestLockable>.Inner> maybeLocked) {
+        assertThat(maybeLocked).hasValueSatisfying(inner -> Assertions.assertThat(inner.getInner())
+                .isEqualTo(TEST_LOCKABLE));
+    }
+}
