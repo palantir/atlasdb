a964245cc9585ed1369b991885d35cf3c0d057e1
Jeremy Kong <jkong@palantir.com>
2024-07-22 16:36:08 +0100

    downscale

--- atlasdb-client/src/main/java/com/palantir/atlasdb/schema/TargetedSweepSchema.java
+++ atlasdb-client/src/main/java/com/palantir/atlasdb/schema/TargetedSweepSchema.java
@@ -119,24 +119,6 @@ public enum TargetedSweepSchema implements AtlasSchema {
                 conflictHandler(ConflictHandler.IGNORE_ALL);
             }
         });
-
-        schema.addTableDefinition("sweepProgressPerBucket", new TableDefinition() {
-            {
-                javaTableName("SweepBucketProgress");
-                allSafeForLoggingByDefault();
-                rowName();
-                hashFirstNRowComponents(3);
-                rowComponent("shard", ValueType.VAR_SIGNED_LONG);
-                rowComponent("bucket_identifier", ValueType.VAR_LONG);
-                rowComponent("sweep_conservative", ValueType.BLOB);
-                columns();
-                column("bucket_progress", "p", ValueType.BLOB);
-
-                // we do our own cleanup
-                sweepStrategy(TableMetadataPersistence.SweepStrategy.NOTHING);
-                conflictHandler(ConflictHandler.IGNORE_ALL);
-            }
-        });
     }
 
     private static void addTableIdentifierTables(Schema schema) {
