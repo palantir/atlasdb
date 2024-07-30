1ef7cb876831c3f0e9eac5f6e3c8ec2e2051a4ce
Jeremy Kong <jkong@palantir.com>
2024-07-30 10:51:28 +0100

    prototyping

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/progress/DefaultGlobalSweepProgressUpdatingTask.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/progress/DefaultGlobalSweepProgressUpdatingTask.java
@@ -16,85 +16,34 @@
 
 package com.palantir.atlasdb.sweep.asts.progress;
 
-import com.palantir.atlasdb.sweep.asts.SweepStateCoordinator.SweepableBucket;
 import com.palantir.atlasdb.sweep.queue.ShardAndStrategy;
-import com.palantir.atlasdb.sweep.queue.ShardProgress;
 import com.palantir.atlasdb.sweep.queue.SweepQueueCleaner;
-import com.palantir.atlasdb.sweep.queue.SweepQueueUtils;
-import com.palantir.atlasdb.sweep.queue.SweepableTimestamps;
 import com.palantir.logsafe.logger.SafeLogger;
 import com.palantir.logsafe.logger.SafeLoggerFactory;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
 import java.util.function.LongSupplier;
 
 public class DefaultGlobalSweepProgressUpdatingTask implements GlobalSweepProgressUpdatingTask {
     private static final SafeLogger log = SafeLoggerFactory.get(DefaultGlobalSweepProgressUpdatingTask.class);
 
-    private static final int MAX_PARTITIONS_TO_ADVANCE_PROGRESS_PER_ITERATION = 100;
-
-    private final ShardProgress shardProgress;
-    private final BucketProgressStore bucketProgressStore;
-    private final SweepableTimestamps sweepableTimestamps;
-    private final SweepQueueCleaner sweepQueueCleaner;
     private final LongSupplier sweepTimestampSupplier;
+    private final ShardProgressUpdateProposer shardProgressUpdateProposer;
+    private final SweepQueueCleaner sweepQueueCleaner;
 
     public DefaultGlobalSweepProgressUpdatingTask(
-            ShardProgress shardProgress,
-            BucketProgressStore bucketProgressStore,
-            SweepableTimestamps sweepableTimestamps,
-            SweepQueueCleaner sweepQueueCleaner,
-            LongSupplier sweepTimestampSupplier) {
-        this.shardProgress = shardProgress;
-        this.bucketProgressStore = bucketProgressStore;
-        this.sweepableTimestamps = sweepableTimestamps;
-        this.sweepQueueCleaner = sweepQueueCleaner;
+            LongSupplier sweepTimestampSupplier,
+            ShardProgressUpdateProposer shardProgressUpdateProposer,
+            SweepQueueCleaner sweepQueueCleaner) {
         this.sweepTimestampSupplier = sweepTimestampSupplier;
+        this.shardProgressUpdateProposer = shardProgressUpdateProposer;
+        this.sweepQueueCleaner = sweepQueueCleaner;
     }
 
     @Override
     public void updateProgress(ShardAndStrategy shardAndStrategy) {
         long currentSweepTimestamp = sweepTimestampSupplier.getAsLong();
-        long currentTimestampPointer = shardProgress.getLastSweptTimestamp(shardAndStrategy);
-
-        int numPartitionsProcessed = 0;
-        boolean shouldContinueProcessing = true;
-
-        Set<Long> completedPartitions = new HashSet<>();
-
-        // TODO (jkong): This is a mouthful.
-        while (numPartitionsProcessed < MAX_PARTITIONS_TO_ADVANCE_PROGRESS_PER_ITERATION && shouldContinueProcessing) {
-            Optional<Long> nextFinePartition = sweepableTimestamps.nextTimestampPartition(
-                    shardAndStrategy, currentTimestampPointer, currentSweepTimestamp);
-            if (nextFinePartition.isEmpty()) {
-                // No more timestamp partitions!
-                shouldContinueProcessing = false;
-                currentTimestampPointer = currentSweepTimestamp;
-            } else {
-                numPartitionsProcessed++;
-                long presentNextFinePartition = nextFinePartition.get();
-                Optional<BucketProgress> partitionProgress = bucketProgressStore.getBucketProgress(
-                        SweepableBucket.of(shardAndStrategy, presentNextFinePartition));
-                if (partitionProgress.isEmpty()) {
-                    // This partition was not started!
-                    shouldContinueProcessing = false;
-                } else {
-                    BucketProgress presentProgress = partitionProgress.get();
-                    boolean complete = presentProgress.timestampOffset() == SweepQueueUtils.TS_FINE_GRANULARITY;
-                    if (!complete) {
-                        shouldContinueProcessing = false;
-                        currentTimestampPointer = SweepQueueUtils.minTsForFinePartition(presentNextFinePartition)
-                                + presentProgress.timestampOffset();
-                    } else {
-                        completedPartitions.add(presentNextFinePartition);
-                        currentTimestampPointer = SweepQueueUtils.minTsForFinePartition(presentNextFinePartition + 1);
-                    }
-                }
-            }
-        }
-
+        ShardProgressUpdate progressUpdate = shardProgressUpdateProposer.proposeUpdate(currentSweepTimestamp);
         // Deferring to today's cleaner.
-        sweepQueueCleaner.backgroundClean(shardAndStrategy, completedPartitions, currentTimestampPointer);
+        sweepQueueCleaner.backgroundClean(
+                shardAndStrategy, progressUpdate.completedFinePartitions(), progressUpdate.progressTimestamp());
     }
 }
---
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/progress/DefaultShardProgressUpdateProposer.java
@@ -0,0 +1,121 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.sweep.asts.progress;
+
+import com.palantir.atlasdb.sweep.asts.SweepStateCoordinator.SweepableBucket;
+import com.palantir.atlasdb.sweep.queue.ShardAndStrategy;
+import com.palantir.atlasdb.sweep.queue.ShardProgress;
+import com.palantir.atlasdb.sweep.queue.SweepQueueUtils;
+import com.palantir.atlasdb.sweep.queue.SweepableTimestamps;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class DefaultShardProgressUpdateProposer implements ShardProgressUpdateProposer {
+    // At perhaps 5 millis per call to sweepableTimestamps#nextTimestampPartition, this will mean an iteration of
+    // this call will take 5 seconds.
+    private static final int MAX_PARTITIONS_TO_ADVANCE_PROGRESS_PER_ITERATION = 1000;
+
+    private final ShardAndStrategy shardAndStrategy;
+    private final ShardProgress shardProgress;
+    private final BucketProgressStore bucketProgressStore;
+    private final SweepableTimestamps sweepableTimestamps;
+
+    public DefaultShardProgressUpdateProposer(
+            ShardAndStrategy shardAndStrategy,
+            ShardProgress shardProgress,
+            BucketProgressStore bucketProgressStore,
+            SweepableTimestamps sweepableTimestamps) {
+        this.shardAndStrategy = shardAndStrategy;
+        this.shardProgress = shardProgress;
+        this.bucketProgressStore = bucketProgressStore;
+        this.sweepableTimestamps = sweepableTimestamps;
+    }
+
+    @Override
+    public ShardProgressUpdate proposeUpdate(long currentSweepTimestamp) {
+        long currentTimestampPointer = shardProgress.getLastSweptTimestamp(shardAndStrategy);
+
+        int numPartitionsProcessed = 0;
+        Set<Long> completedPartitions = new HashSet<>();
+
+        // TODO (jkong): This is a mouthful.
+        while (numPartitionsProcessed < MAX_PARTITIONS_TO_ADVANCE_PROGRESS_PER_ITERATION) {
+            Optional<Long> nextFinePartition = sweepableTimestamps.nextTimestampPartition(
+                    shardAndStrategy, currentTimestampPointer, currentSweepTimestamp);
+            if (nextFinePartition.isEmpty()) {
+                // No more timestamp partitions in our range.
+                return ShardProgressUpdate.builder()
+                        .completedFinePartitions(completedPartitions)
+                        .progressTimestamp(currentSweepTimestamp)
+                        .build();
+            } else {
+                numPartitionsProcessed++;
+                long presentNextFinePartition = nextFinePartition.get();
+                Optional<BucketProgress> partitionProgress = bucketProgressStore.getBucketProgress(
+                        SweepableBucket.of(shardAndStrategy, presentNextFinePartition));
+                if (partitionProgress.isEmpty()) {
+                    // This partition was not started. In most cases, we can say that we stopped at the end of the
+                    // previous partition safely. However, I'm not convinced we want to rewind the sweep progress if
+                    // we're already ahead of the start (e.g., because our last update might have ended on this
+                    // partition and there might have been nothing, so we might have set sweep timestamp inside this
+                    // partition).
+                    return ShardProgressUpdate.builder()
+                            .completedFinePartitions(completedPartitions)
+                            .progressTimestamp(Math.max(
+                                    SweepQueueUtils.maxTsForFinePartition(presentNextFinePartition - 1),
+                                    currentTimestampPointer))
+                            .build();
+                } else {
+                    BucketProgress presentProgress = partitionProgress.get();
+                    boolean complete = presentProgress.timestampOffset() == SweepQueueUtils.TS_FINE_GRANULARITY;
+                    if (!complete) {
+                        // As in the empty case, we do not want to rewind the sweep progress if we're already
+                        // ahead of the start. This seems less likely, but I don't want to rule out us writing
+                        // a zero marker or something to that effect.
+                        return ShardProgressUpdate.builder()
+                                .completedFinePartitions(completedPartitions)
+                                .progressTimestamp(Math.max(
+                                        SweepQueueUtils.minTsForFinePartition(presentNextFinePartition)
+                                                + presentProgress.timestampOffset(),
+                                        currentTimestampPointer))
+                                .build();
+                    } else {
+                        completedPartitions.add(presentNextFinePartition);
+                        currentTimestampPointer = SweepQueueUtils.minTsForFinePartition(presentNextFinePartition + 1);
+                    }
+                }
+            }
+        }
+
+        // We finished the max number of iterations, and identified MAX_PARTITIONS_TO_ADVANCE partitions.
+        // We have to be careful here: currentTimestampPointer tells us where we would start our search for the next
+        // partition. But, we don't actually know that that timestamp is processed.
+        // We could just do currentTimestampPointer - 1, though that feels a bit too fragile for my liking.
+        long highestCompletedPartition = completedPartitions.stream()
+                .max(Comparator.naturalOrder())
+                .orElseThrow(() -> new SafeIllegalStateException(
+                        "Finished max iterations of shard progress update proposal, but no partitions were"
+                                + " found. This is unexpected"));
+        return ShardProgressUpdate.builder()
+                .completedFinePartitions(completedPartitions)
+                .progressTimestamp(SweepQueueUtils.maxTsForFinePartition(highestCompletedPartition))
+                .build();
+    }
+}
