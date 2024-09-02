3f28a260e86f3fd084fc1f4e8870010dbc06f5b4
Mohammed Daudali <mdaudali@palantir.com>
2024-09-02 17:13:15 +0100

    [ASTS] A somewhat random iterator, for the coordinator. (#7227)

---
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/ProbingRandomIterator.java
@@ -0,0 +1,65 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.sweep.asts;
+
+import java.util.BitSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ThreadLocalRandom;
+import javax.annotation.concurrent.NotThreadSafe;
+
+// We could use something similar to a Linear Congruential Generator to generate a pseudo-random sequence of
+// numbers across the range [0, len(remainingBuckets))
+// I'm not the first to think of this, see
+// https://lemire.me/blog/2017/09/18/visiting-all-values-in-an-array-exactly-once-in-random-order/
+// But, this is sufficient, and ideally, we'd just have an endpoint in Timelock that says give me the first unlocked
+// thing in this set.
+@NotThreadSafe
+public final class ProbingRandomIterator<T> implements Iterator<T> {
+    private final List<T> list;
+    private final BitSet visited;
+    private int probedElements = 0;
+
+    public ProbingRandomIterator(List<T> list) {
+        this.list = list;
+        this.visited = new BitSet(list.size());
+    }
+
+    @Override
+    public boolean hasNext() {
+        return probedElements < list.size();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int nextStart = ThreadLocalRandom.current().nextInt(list.size());
+        int nextClearBit = visited.nextClearBit(nextStart);
+        // This is annoyingly undocumented in the nextClearBit API, but nextClearBit
+        // can return size(bitSet) if there's no clear bit after the start index.
+        // If we didn't do the next check, we'd end up with an IndexOutOfBoundsException
+        if (nextClearBit >= list.size()) {
+            nextClearBit = visited.nextClearBit(0);
+        }
+        probedElements++;
+        visited.set(nextClearBit);
+        return list.get(nextClearBit);
+    }
+}
---
+++ atlasdb-impl-shared/src/test/java/com/palantir/atlasdb/sweep/asts/ProbingRandomIteratorTest.java
@@ -0,0 +1,74 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.sweep.asts;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.apache.commons.lang3.stream.Streams;
+import org.junit.jupiter.api.Test;
+
+public final class ProbingRandomIteratorTest {
+    private static final List<Integer> LARGE_INT_LIST =
+            IntStream.range(0, 1000).boxed().collect(Collectors.toList());
+
+    @Test
+    public void iteratorReturnsAllElements() {
+        ProbingRandomIterator<Integer> iterator = new ProbingRandomIterator<>(LARGE_INT_LIST);
+        List<Integer> elements = ImmutableList.copyOf(iterator);
+
+        // containsExactlyInAnyOrderElementsOf is actually quite slow with large lists (>10000 elements)
+        // From a quick glance, it seems to use a O(n^2) algorithm
+        assertThat(elements).containsExactlyInAnyOrderElementsOf(LARGE_INT_LIST);
+    }
+
+    @Test
+    public void iteratorReturnsElementsInDifferentOrderEachTime() {
+        List<Integer> elements1 = ImmutableList.copyOf(new ProbingRandomIterator<>(LARGE_INT_LIST));
+        List<Integer> elements2 = ImmutableList.copyOf(new ProbingRandomIterator<>(LARGE_INT_LIST));
+        assertThat(elements1).isNotEqualTo(elements2);
+    }
+
+    @Test
+    public void iteratorDoesNotModifyUnderlyingList() {
+        List<Integer> originalList = ImmutableList.copyOf(LARGE_INT_LIST);
+        // Consume the entire iterator
+        Streams.of(new ProbingRandomIterator<>(LARGE_INT_LIST)).forEach(x -> {});
+        assertThat(originalList).containsExactlyElementsOf(LARGE_INT_LIST);
+    }
+
+    @Test
+    public void iteratorHasNextDoesNotFailOnEmptyList() {
+        List<Integer> emptyList = ImmutableList.of();
+        ProbingRandomIterator<Integer> iterator = new ProbingRandomIterator<>(emptyList);
+        assertThat(iterator.hasNext()).isFalse();
+    }
+
+    @Test
+    public void iteratorNextThrowsNoSuchElementExceptionWhenNoMoreElements() {
+        ProbingRandomIterator<Integer> iterator = new ProbingRandomIterator<>(ImmutableList.of(1));
+        assertThat(iterator.hasNext()).isTrue();
+        iterator.next();
+        assertThat(iterator.hasNext()).isFalse();
+        assertThatThrownBy(iterator::next).isInstanceOf(NoSuchElementException.class);
+    }
+}
