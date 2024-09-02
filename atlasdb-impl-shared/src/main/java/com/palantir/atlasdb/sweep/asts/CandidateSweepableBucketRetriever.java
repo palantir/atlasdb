d5a4cdb01db08d35008faa9433ee11e84be31aa6
Mohammed Daudali <mdaudali@palantir.com>
2024-09-02 16:50:59 +0100

    [ASTS][Bucket Retriever] Debouncing bucket retrieval requests (#7220)

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/CandidateSweepableBucketRetriever.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/CandidateSweepableBucketRetriever.java
@@ -18,14 +18,25 @@ package com.palantir.atlasdb.sweep.asts;
 
 import com.palantir.atlasdb.sweep.asts.SweepStateCoordinator.SweepableBucket;
 import com.palantir.refreshable.Disposable;
-import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
 
 // A push model - decouples asking for updates from the logic of debouncing requests.
 // Simplifies the coordinator, since it can just repeatedly _ask_ for updates whenever it thinks it's useful to do so
 // but we can rate limit it down.
 public interface CandidateSweepableBucketRetriever {
+    /**
+     * Requests an update to the set of buckets. This method will return immediately. There is no guarantee that
+     * a refresh will occur after the method is called, as this is simply a hint.
+     */
     void requestUpdate();
 
-    Disposable subscribeToChanges(Consumer<List<SweepableBucket>> task);
+    /**
+     * Registers a callback that will execute after each refresh, including whenever the set of buckets remains constant
+     * across refreshes.
+     * <p>
+     * There are no guarantees in the order that callbacks are executed. If any callback fails, it is not guaranteed
+     * that other callbacks will be executed. Moreover, slow callbacks may delay subsequent refreshes.
+     */
+    Disposable subscribeToChanges(Consumer<Set<SweepableBucket>> task);
 }
