f481e57f3d2a80fc6697cf00d686ec95c247a433
Baris Ogun Yoruk <boyoruk@palantir.com>
2024-10-07 16:08:00 +0100

    remove palantirtechnologies/

--- scripts/circle-ci/trigger-antithesis-simulation.sh
+++ scripts/circle-ci/trigger-antithesis-simulation.sh
@@ -15,8 +15,8 @@ TEST_DURATION=$([ -n "${CIRCLE_TAG}" ] && echo "12" || echo "3")
 echo "Triggering simulation on Antithesis via the ${WEBHOOK_LOCATOR} webhook with a test duration of ${TEST_DURATION} hours."
 curl -v -u "palantir:${ANTITHESIS_WEBHOOK_PASSWORD}" -X POST https://palantir.antithesis.com/api/v1/launch_experiment/${WEBHOOK_LOCATOR} -d \
 '{ "params": {
-    "antithesis.images":"palantirtechnologies/cassandra:latest;palantirtechnologies/timelock-server-distribution:latest;palantirtechnologies/atlasdb-workload-server-distribution:latest",
-    "antithesis.config_image":"palantirtechnologies/atlasdb-workload-server-antithesis:latest",
+    "antithesis.images":"cassandra:latest;timelock-server-distribution:latest;atlasdb-workload-server-distribution:latest",
+    "antithesis.config_image":"atlasdb-workload-server-antithesis:latest",
     "custom.duration":"'${TEST_DURATION}'",
     "antithesis.report.recipients":"'${ANTITHESIS_REPORT_RECIPIENT}'"
 } }'
