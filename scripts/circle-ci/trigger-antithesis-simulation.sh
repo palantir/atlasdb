11fa5758bbf193a9ca7b8f65d5d088ff12c1c059
Baris Ogun Yoruk <boyoruk@palantir.com>
2024-10-07 14:19:54 +0100

    if release 12, otherwise 3

--- scripts/circle-ci/trigger-antithesis-simulation.sh
+++ scripts/circle-ci/trigger-antithesis-simulation.sh
@@ -11,11 +11,12 @@ if [[ -z "${ANTITHESIS_REPORT_RECIPIENT}" ]]; then
 fi
 
 WEBHOOK_LOCATOR="atlasdb"
+DURATION=$([ "$CIRCLE_TAG" ] && echo "12" || echo "3")
 echo "Triggering simulation on Antithesis via the ${WEBHOOK_LOCATOR} webhook."
 curl -v -u "palantir:${ANTITHESIS_WEBHOOK_PASSWORD}" -X POST https://palantir.antithesis.com/api/v1/launch_experiment/${WEBHOOK_LOCATOR} -d \
 '{ "params": {
     "antithesis.images":"palantirtechnologies/cassandra:latest;palantirtechnologies/timelock-server-distribution:latest;palantirtechnologies/atlasdb-workload-server-distribution:latest",
     "antithesis.config_image":"palantirtechnologies/atlasdb-workload-server-antithesis:latest",
-    "custom.duration":"12",
+    "custom.duration":"'${DURATION}'",
     "antithesis.report.recipients":"'${ANTITHESIS_REPORT_RECIPIENT}'"
 } }'
