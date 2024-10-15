bf9b64b466cbc913fc7651d06a22a9d85e955f08
Tilly Oswell Wheeler <tillyow@palantir.com>
2024-10-15 14:57:02 +0100

    Added CassandraTException

---
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTExceptions.java
@@ -0,0 +1,49 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.atlasdb.keyvalue.cassandra;
+
+import com.google.errorprone.annotations.CompileTimeConstant;
+import com.palantir.atlasdb.keyvalue.api.InsufficientConsistencyException;
+import com.palantir.common.exception.AtlasDbDependencyException;
+import com.palantir.logsafe.Arg;
+import org.apache.cassandra.thrift.TimedOutException;
+import org.apache.cassandra.thrift.UnavailableException;
+
+public final class CassandraTExceptions {
+    private CassandraTExceptions() {}
+
+    public static AtlasDbDependencyException mapToUncheckedException(
+            @CompileTimeConstant final String logMessage, Throwable throwable, Arg<?>... args) {
+        if (throwable instanceof TimedOutException) {
+            return new CassandraTimedOutException(throwable, args);
+        }
+        if (throwable instanceof UnavailableException) {
+            return new InsufficientConsistencyException(logMessage, throwable);
+        }
+        return new AtlasDbDependencyException(throwable);
+    }
+
+    public static AtlasDbDependencyException mapToUncheckedException(Throwable throwable, Arg<?>... args) {
+        if (throwable instanceof TimedOutException) {
+            return new CassandraTimedOutException(throwable, args);
+        }
+        if (throwable instanceof UnavailableException) {
+            return new InsufficientConsistencyException(throwable);
+        }
+        return new AtlasDbDependencyException(throwable);
+    }
+}
--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/paging/RowGetter.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/paging/RowGetter.java
@@ -15,10 +15,10 @@
  */
 package com.palantir.atlasdb.keyvalue.cassandra.paging;
 
-import com.palantir.atlasdb.keyvalue.api.InsufficientConsistencyException;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.keyvalue.cassandra.CassandraClient;
 import com.palantir.atlasdb.keyvalue.cassandra.CassandraClientPool;
+import com.palantir.atlasdb.keyvalue.cassandra.CassandraTExceptions;
 import com.palantir.atlasdb.keyvalue.cassandra.TracingQueryRunner;
 import com.palantir.atlasdb.keyvalue.cassandra.pool.CassandraServer;
 import com.palantir.atlasdb.keyvalue.cassandra.thrift.SlicePredicates;
@@ -61,8 +61,7 @@ public class RowGetter {
                             () -> client.get_range_slices(
                                     kvsMethodName, tableRef, slicePredicate, keyRange, consistency));
                 } catch (UnavailableException e) {
-                    throw new InsufficientConsistencyException(
-                            "get_range_slices requires " + consistency + " Cassandra nodes to be up and available.", e);
+                    throw CassandraTExceptions.mapToUncheckedException("get_range_slices requires Cassandra nodes to be up and available. Check Cassandra nodes: ", e, SafeArg.of("consistency", consistency));
                 } catch (Exception e) {
                     throw Throwables.unwrapAndThrowAtlasDbDependencyException(e);
                 }
