1f026b0c2f7add9988d9dc330e9ba54034c37735
Tilly Oswell Wheeler <tillyow@palantir.com>
2024-10-17 15:08:52 +0100

    fix compileErrors

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraClientFactory.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraClientFactory.java
@@ -111,13 +111,13 @@ public class CassandraClientFactory extends BasePooledObjectFactory<CassandraCli
                 throw new ClientCreationFailedException(
                         "Failed to construct client for {}/{} over SSL",
                         e,
-                        SafeArg.of("proxy", CassandraLogHelper.host(cassandraServer.proxy())));
+                        SafeArg.of("proxy", CassandraLogHelper.host(cassandraServer.proxy())),
                         SafeArg.of("Keyspace", clientConfig.keyspace()));
             }
             throw new ClientCreationFailedException(
                     "Failed to construct client for {}/{}",
                     e,
-                    SafeArg.of("proxy", CassandraLogHelper.host(cassandraServer.proxy())));
+                    SafeArg.of("proxy", CassandraLogHelper.host(cassandraServer.proxy())),
                     SafeArg.of("Keyspace", clientConfig.keyspace()));
         }
     }
