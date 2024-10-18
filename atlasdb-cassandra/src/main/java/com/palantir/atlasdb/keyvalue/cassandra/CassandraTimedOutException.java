b0ee926fdbe034f68424cac7868022d4e05f162a
Tilly Oswell Wheeler <tillyow@palantir.com>
2024-10-18 13:36:24 +0100

    spoteless

--- atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTimedOutException.java
+++ atlasdb-cassandra/src/main/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraTimedOutException.java
@@ -28,14 +28,14 @@ public class CassandraTimedOutException extends AtlasDbDependencyException {
 
     private static final String LOG_MESSAGE =
             "Cassandra query threw a TimedOut exception. Possible reasons and actions to resolve include:\n"
-                    + "1. Reason: AtlasDB clients are requesting too much data from Cassandra.\n"
-                    + "   Resolution: Change the query to request less data.\n"
-                    + "2. Reason: Data that has been deleted is being read in the query (e.g. A large amount of"
-                    + " tombstones).\n"
-                    + "   Resolution: Run a compaction on your Cassandra server.\n"
-                    + "3. Reason: Cassandra is struggling, possibly due to another large query, server health issues, or a"
-                    + " network outage.\n"
-                    + "   Resolution: Ask your CassandraOps to check the state of the Cassandra server.";
+                + "1. Reason: AtlasDB clients are requesting too much data from Cassandra.\n"
+                + "   Resolution: Change the query to request less data.\n"
+                + "2. Reason: Data that has been deleted is being read in the query (e.g. A large amount of"
+                + " tombstones).\n"
+                + "   Resolution: Run a compaction on your Cassandra server.\n"
+                + "3. Reason: Cassandra is struggling, possibly due to another large query, server health issues, or a"
+                + " network outage.\n"
+                + "   Resolution: Ask your CassandraOps to check the state of the Cassandra server.";
 
     public CassandraTimedOutException(Throwable throwable, Arg<?>... args) {
         super(SafeExceptions.renderMessage(LOG_MESSAGE, args), throwable);
