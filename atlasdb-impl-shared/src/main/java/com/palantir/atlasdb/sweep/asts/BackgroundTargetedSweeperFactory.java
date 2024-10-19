3abf312f6f5db4ae193ef2a25b0d71d590902b32
Mohammed Daudali <mdaudali@palantir.com>
2024-10-19 15:27:06 +0100

    [ASTS] Wire up bootstrapping task

--- atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/BackgroundTargetedSweeperFactory.java
+++ atlasdb-impl-shared/src/main/java/com/palantir/atlasdb/sweep/asts/BackgroundTargetedSweeperFactory.java
@@ -149,6 +149,11 @@ public final class BackgroundTargetedSweeperFactory {
         DefaultSweepAssignedBucketStore sweepAssignedBucketStore =
                 DefaultSweepAssignedBucketStore.create(keyValueService);
         BucketProgressStore bucketProgressStore = DefaultBucketProgressStore.create(keyValueService);
+
+        new InitialBucketAssignerStateMachineBootstrapTask(
+                        components.shardProgress(), sweepAssignedBucketStore, timelockService::getFreshTimestamp)
+                .run();
+
         LockableFactory<ExclusiveTask> exclusiveTaskLockableFactory = LockableFactory.create(
                 timelockService,
                 // We will just retry later if we fail to acquire the lock at this time - liveness on a given sweeper
