fda40b59e815de025d87b0b4a94ff30557a6c596
Tilly Oswell Wheeler <tillyow@palantir.com>
2024-10-23 14:19:35 +0100

    change exception caught

--- atlasdb-cassandra-multinode-tests/src/test/java/com/palantir/cassandra/multinode/AbstractDegradedClusterTest.java
+++ atlasdb-cassandra-multinode-tests/src/test/java/com/palantir/cassandra/multinode/AbstractDegradedClusterTest.java
@@ -24,13 +24,13 @@ import com.google.common.collect.Iterables;
 import com.palantir.atlasdb.AtlasDbConstants;
 import com.palantir.atlasdb.encoding.PtBytes;
 import com.palantir.atlasdb.keyvalue.api.Cell;
-import com.palantir.atlasdb.keyvalue.api.InsufficientConsistencyException;
 import com.palantir.atlasdb.keyvalue.api.KeyValueService;
 import com.palantir.atlasdb.keyvalue.api.TableReference;
 import com.palantir.atlasdb.keyvalue.api.Value;
 import com.palantir.atlasdb.keyvalue.cassandra.CassandraClient;
 import com.palantir.atlasdb.keyvalue.cassandra.CassandraKeyValueService;
 import com.palantir.atlasdb.keyvalue.cassandra.CassandraKeyValueServices;
+import com.palantir.atlasdb.keyvalue.cassandra.CassandraTExceptions;
 import com.palantir.common.base.RunnableCheckedException;
 import com.palantir.common.exception.AtlasDbDependencyException;
 import java.util.HashMap;
@@ -107,7 +107,7 @@ public abstract class AbstractDegradedClusterTest {
     }
 
     void assertThrowsInsufficientConsistencyExceptionAndDoesNotChangeCassandraSchema(RunnableCheckedException<?> task) {
-        assertThatThrownBy(task::run).isInstanceOf(InsufficientConsistencyException.class);
+        assertThatThrownBy(task::run).isInstanceOf(CassandraTExceptions.class);
         assertCassandraSchemaUnchanged();
     }
 
