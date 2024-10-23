dd18a6af89111058c57e19448d3cf9a11b6ecff7
Jakub Kozlowski <jkozlowski@users.noreply.github.com>
2024-10-23 12:59:08 +0100

    Remove Jepsen tests. (#7390)
    Remove Jepsen tests.

--- atlasdb-jepsen-tests/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject jepsen.atlasdb "0.1.0"
-  :resource-paths ["resources/atlasdb/atlasdb-jepsen-tests-all.jar"]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jepsen "0.1.3"]
-                 [cheshire "5.6.1"]
-                 [clj-http "3.1.0"]])
