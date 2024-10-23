e04191a1b990436ea8fd86065def351f32ce8047
Jakub Kozlowski <jakubk@palantir.com>
2024-10-23 10:45:47 +0000

    Timestamp leases in LegacyTimelockService.

--- lock-impl/src/main/java/com/palantir/lock/impl/LegacyTimelockService.java
+++ lock-impl/src/main/java/com/palantir/lock/impl/LegacyTimelockService.java
@@ -16,24 +16,30 @@
 package com.palantir.lock.impl;
 
 import com.google.common.collect.ImmutableSortedMap;
+import com.palantir.atlasdb.timelock.api.ConjureTimestampRange;
 import com.palantir.atlasdb.timelock.api.TimestampLeaseName;
 import com.palantir.common.base.Throwables;
 import com.palantir.lock.AtlasTimestampLockDescriptor;
+import com.palantir.lock.ConjureTimestampRangeTimestampSupplier;
+import com.palantir.lock.LimitingLongSupplier;
 import com.palantir.lock.LockClient;
 import com.palantir.lock.LockDescriptor;
 import com.palantir.lock.LockMode;
 import com.palantir.lock.LockRefreshToken;
 import com.palantir.lock.LockService;
 import com.palantir.lock.SimpleTimeDuration;
+import com.palantir.lock.logger.ClientId;
 import com.palantir.lock.v2.ClientLockingOptions;
 import com.palantir.lock.v2.LockImmutableTimestampResponse;
 import com.palantir.lock.v2.LockRequest;
 import com.palantir.lock.v2.LockResponse;
 import com.palantir.lock.v2.LockToken;
 import com.palantir.lock.v2.TimelockService;
+import com.palantir.lock.v2.TimestampLeaseResult;
 import com.palantir.lock.v2.TimestampLeaseResults;
 import com.palantir.lock.v2.WaitForLocksRequest;
 import com.palantir.lock.v2.WaitForLocksResponse;
+import com.palantir.logsafe.Preconditions;
 import com.palantir.timestamp.TimestampRange;
 import com.palantir.timestamp.TimestampService;
 import java.util.HashSet;
@@ -42,12 +48,19 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.function.LongSupplier;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+import one.util.streamex.EntryStream;
+import one.util.streamex.StreamEx;
 
 /**
  * A {@link TimelockService} implementation that delegates to a {@link LockService} and {@link TimestampService}.
  */
 public class LegacyTimelockService implements TimelockService {
+
+    private static final ClientId TIMESTAMP_LEASES_CLIENT_ID = ClientId.of("timestamp-leases");
 
     private final TimestampService timestampService;
     private final LockService lockService;
@@ -82,39 +95,13 @@ public class LegacyTimelockService implements TimelockService {
 
     @Override
     public LockImmutableTimestampResponse lockImmutableTimestamp() {
-        long immutableLockTs = timestampService.getFreshTimestamp();
-        LockDescriptor lockDesc = AtlasTimestampLockDescriptor.of(immutableLockTs);
-        com.palantir.lock.LockRequest lockRequest = com.palantir.lock.LockRequest.builder(
-                        ImmutableSortedMap.of(lockDesc, LockMode.READ))
-                .withLockedInVersionId(immutableLockTs)
-                .build();
-        LockRefreshToken lock;
-
-        try {
-            lock = lockService.lock(immutableTsLockClient.getClientId(), lockRequest);
-        } catch (InterruptedException e) {
-            throw Throwables.throwUncheckedException(e);
-        }
-
-        try {
-            return LockImmutableTimestampResponse.of(
-                    getImmutableTimestampInternal(immutableLockTs), LockTokenConverter.toTokenV2(lock));
-        } catch (Throwable e) {
-            if (lock != null) {
-                try {
-                    lockService.unlock(lock);
-                } catch (Throwable unlockThrowable) {
-                    e.addSuppressed(unlockThrowable);
-                }
-            }
-            throw Throwables.rewrapAndThrowUncheckedException(e);
-        }
+        return withLeaseFreshTimestamp(immutableTsLockClient.getClientId(), response -> response);
     }
 
     @Override
     public long getImmutableTimestamp() {
         long ts = timestampService.getFreshTimestamp();
-        return getImmutableTimestampInternal(ts);
+        return getTimestampLeaseInternal(immutableTsLockClient.getClientId(), ts);
     }
 
     @Override
@@ -186,18 +173,72 @@ public class LegacyTimelockService implements TimelockService {
 
     @Override
     public TimestampLeaseResults acquireTimestampLeases(Map<TimestampLeaseName, Integer> requests) {
-        // TODO(aalouane): implement!
-        throw new UnsupportedOperationException("Not implemented yet!");
+        // This implementation here treats ALL the timestamp leases the same, meaning they will all progress
+        // at the same rate. This is only a perf concern (all timestamps progress together), not correctness.
+        // TODO(jakubk): This should reject timestamp lease names that conflict with the immutable timestamp lease
+        // or we should fix the implementation in timelock to allow these to conflict.
+        return withLeaseFreshTimestamp(TIMESTAMP_LEASES_CLIENT_ID.get(), response -> {
+            Map<TimestampLeaseName, TimestampLeaseResult> results = EntryStream.of(requests)
+                    .mapValues(numFreshTimestamps -> {
+                        TimestampRange timestampRange = getFreshTimestamps(numFreshTimestamps);
+                        Preconditions.checkState(
+                                timestampRange.size() >= numFreshTimestamps, "Didn't get enough timestamps");
+                        LongSupplier freshTimestamps = new LimitingLongSupplier(
+                                new ConjureTimestampRangeTimestampSupplier(ConjureTimestampRange.of(
+                                        timestampRange.getLowerBound(), timestampRange.size())),
+                                numFreshTimestamps);
+                        return TimestampLeaseResult.of(response.getImmutableTimestamp(), freshTimestamps);
+                    })
+                    .toMap();
+            return TimestampLeaseResults.of(response.getLock(), results);
+        });
     }
 
     @Override
     public Map<TimestampLeaseName, Long> getMinLeasedTimestamps(Set<TimestampLeaseName> timestampNames) {
-        // TODO(aalouane): implement!
-        throw new UnsupportedOperationException("Not implemented yet!");
+        long minLeasedTimestamp =
+                getTimestampLeaseInternal(TIMESTAMP_LEASES_CLIENT_ID.get(), timestampService.getFreshTimestamp());
+        return StreamEx.of(timestampNames)
+                .mapToEntry(_ignored -> minLeasedTimestamp)
+                .toMap();
     }
 
-    private long getImmutableTimestampInternal(long ts) {
-        Long minLocked = lockService.getMinLockedInVersionId(immutableTsLockClient.getClientId());
+    private <R> R withLeaseFreshTimestamp(String timestampName, Function<LockImmutableTimestampResponse, R> mapper) {
+        long immutableLockTs = timestampService.getFreshTimestamp();
+        LockDescriptor lockDesc = AtlasTimestampLockDescriptor.of(immutableLockTs);
+        com.palantir.lock.LockRequest lockRequest = com.palantir.lock.LockRequest.builder(
+                        ImmutableSortedMap.of(lockDesc, LockMode.READ))
+                .withLockedInVersionId(immutableLockTs)
+                .build();
+        LockRefreshToken lock;
+
+        try {
+            lock = lockService.lock(timestampName, lockRequest);
+        } catch (InterruptedException e) {
+            throw Throwables.throwUncheckedException(e);
+        }
+
+        try {
+            return mapper.apply(LockImmutableTimestampResponse.of(
+                    getTimestampLeaseInternal(timestampName, immutableLockTs), LockTokenConverter.toTokenV2(lock)));
+        } catch (Throwable e) {
+            throw unlockAndRethrow(e, lock);
+        }
+    }
+
+    private RuntimeException unlockAndRethrow(Throwable e, @Nullable LockRefreshToken lock) {
+        if (lock != null) {
+            try {
+                lockService.unlock(lock);
+            } catch (Throwable unlockThrowable) {
+                e.addSuppressed(unlockThrowable);
+            }
+        }
+        throw Throwables.rewrapAndThrowUncheckedException(e);
+    }
+
+    private long getTimestampLeaseInternal(String timestampName, long ts) {
+        Long minLocked = lockService.getMinLockedInVersionId(timestampName);
         return minLocked == null ? ts : minLocked;
     }
 
